41283a7c0e115bb07ad81766ae6c165cf97c4c9d
Naor Matania <naorm1991@fb.com>
2019-02-25 07:50:05 -0800

    Add menu dropdown with DELETE to images (#522)
    Summary:
    Pull Request resolved: https://github.com/facebookexternal/fbc/pull/522
    
    Add the menu option Delete for image
    
    Reviewed By: noamsch
    
    Differential Revision: D14167607
    
    fbshipit-source-id: ce1edf70c571cc643693af5107b2d7e6786d777b

--- nms/fbcnms-packages/fbcnms-ui/theme/colors.js
+++ nms/fbcnms-packages/fbcnms-ui/theme/colors.js
@@ -17,6 +17,8 @@ export const gray0 = '#f2f3f5';
 export const gray1 = '#979797';
 export const gray2 = '#4d4d4e';
 export const gray3 = '#dadde1';
+export const gray4 = 'rgba(0,0,0,0.5)';
+export const gray5 = 'rgba(0,0,0,0.3)';
 export const sea100 = '#6ca0b6';
 export const sea200 = '#0097a7';
 export const sea300 = '#455a64';
@@ -28,6 +30,8 @@ export default {
   gray1,
   gray2,
   gray3,
+  gray4,
+  gray5,
   purple,
   sea100,
   sea200,
