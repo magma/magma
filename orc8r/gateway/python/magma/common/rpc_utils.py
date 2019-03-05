737fe6b38aa64758cdc43cb678c30d436268d590
Marie Bremner <marwhal@fb.com>
2019-03-05 13:33:27 -0800

    Checkin_manager now uses asyncio not callback model so that job.py can be used later (#20)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/20
    
    Converting checkin_manager.py so that it uses await and futures instead of relying on callbacks. This will later make it easier to use the loop based Job interface, defined in common/job.py.
    
    Reviewed By: xjtian
    
    Differential Revision: D14215189
    
    fbshipit-source-id: 336468732b500368498cc5fdf89ee7bb0dc87f66

--- orc8r/gateway/python/magma/common/rpc_utils.py
+++ orc8r/gateway/python/magma/common/rpc_utils.py
@@ -6,7 +6,9 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree. An additional grant
 of patent rights can be found in the PATENTS file in the same directory.
 """
+# pylint: disable=broad-except
 
+import asyncio
 import grpc
 from orc8r.protos import common_pb2
 
@@ -71,3 +73,32 @@ def cloud_grpc_wrapper(func):
             print("Error! [%s] %s" % (err.code(), err.details()))
             exit(1)
     return wrapper
+
+
+def _grpc_async_wrapper(f, gf):
+    try:
+        f.set_result(gf.result())
+    except Exception as e:
+        f.set_exception(e)
+
+
+def grpc_async_wrapper(gf, loop=None):
+    """
+    Wraps a GRPC result in a future that can be yielded by asyncio
+
+    Usage:
+
+    async def my_fn(param):
+        result =
+            await grpc_async_wrapper(stub.function_name.future(param, timeout))
+
+    Code taken and modified from:
+        https://github.com/grpc/grpc/wiki/Integration-with-tornado-(python)
+    """
+    f = asyncio.Future()
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    gf.add_done_callback(
+        lambda _: loop.call_soon_threadsafe(_grpc_async_wrapper, f, gf)
+    )
+    return f
