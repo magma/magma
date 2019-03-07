e7f4630a27b1009aa0b77a20f80574e2981aa828
Naor Matania <naorm1991@fb.com>
2019-03-07 05:25:51 -0800

    Add files section to documents tab
    Summary:
    1. Add table view for files that will show that same as images (right now)
    2. support adding file and download & delete
    3. files table only visible for `testEnv`
    
    Reviewed By: noamsch
    
    Differential Revision: D14241191
    
    fbshipit-source-id: a94bf0a829072ff0fbb260fbf747c0aefcdbbbb8

--- nms/fbcnms-packages/fbcnms-ui/utils/displayUtils.js
+++ nms/fbcnms-packages/fbcnms-ui/utils/displayUtils.js
@@ -8,5 +8,25 @@
  * @format
  */
 
+const KB = 1024;
+const MB = 1024 * 1024;
+const GB = 1024 * 1024 * 1024;
+
 export const sortLexicographically = (a: string, b: string) =>
   a.localeCompare(b, 'en', {numeric: true});
+
+export const formatFileSize = (sizeInBytes: number) => {
+  if (sizeInBytes === 0) {
+    return '0MB';
+  }
+
+  if (sizeInBytes >= GB) {
+    return `${(sizeInBytes / GB).toFixed(2)}GB`;
+  } else if (sizeInBytes >= MB) {
+    return `${(sizeInBytes / MB).toFixed(2)}MB`;
+  } else if (sizeInBytes >= KB) {
+    return `${Math.round(sizeInBytes / KB)}KB`;
+  } else {
+    return `${sizeInBytes}B`;
+  }
+};
