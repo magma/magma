2e55abbd085fe7d12473849b4ed24dbc1ca28d8a
Vik Goel <vikg@fb.com>
2019-03-07 18:12:35 -0800

    Add SWx integration tests for authorization verification
    Summary: Add integration tests between the HSS and SWx Proxy which test that the verifyAuthorization flag works correctly.
    
    Reviewed By: mpgermano
    
    Differential Revision: D14249929
    
    fbshipit-source-id: 8d977136f557dd58464893b3921eec4aa985fae1

--- feg/gateway/services/testcore/hss/servicers/test/hss_swx_proxy_integration_test.go
+++ feg/gateway/services/testcore/hss/servicers/test/hss_swx_proxy_integration_test.go
@@ -4,20 +4,28 @@ import (
 	"context"
 	"testing"
 
-	"magma/feg/cloud/go/protos"
+	fegprotos "magma/feg/cloud/go/protos"
 	"magma/feg/gateway/diameter"
-	"magma/feg/gateway/services/swx_proxy/servicers"
+	swx "magma/feg/gateway/services/swx_proxy/servicers"
 	"magma/feg/gateway/services/testcore/hss/crypto"
+	"magma/feg/gateway/services/testcore/hss/servicers"
+	lteprotos "magma/lte/cloud/go/protos"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMAR_Successful(t *testing.T) {
-	swxProxy := getTestSwxProxy(t)
-	mar := &protos.AuthenticationRequest{
+	testMARSuccessful(t, true)
+	testMARSuccessful(t, false)
+}
+
+func testMARSuccessful(t *testing.T, verifyAuthorization bool) {
+	hss := getTestHSSDiameterServer(t)
+	swxProxy := getTestSwxProxy(t, hss, verifyAuthorization)
+	mar := &fegprotos.AuthenticationRequest{
 		UserName:             "sub1",
 		SipNumAuthVectors:    5,
-		AuthenticationScheme: protos.AuthenticationScheme_EAP_AKA,
+		AuthenticationScheme: fegprotos.AuthenticationScheme_EAP_AKA,
 	}
 	maa, err := swxProxy.Authenticate(context.Background(), mar)
 	assert.NoError(t, err)
@@ -25,7 +33,7 @@ func TestMAR_Successful(t *testing.T) {
 	assert.Equal(t, "sub1", maa.GetUserName())
 	assert.Equal(t, 5, len(maa.GetSipAuthVectors()))
 	for _, vector := range maa.GetSipAuthVectors() {
-		assert.Equal(t, protos.AuthenticationScheme_EAP_AKA, vector.AuthenticationScheme)
+		assert.Equal(t, fegprotos.AuthenticationScheme_EAP_AKA, vector.AuthenticationScheme)
 		assert.Equal(t, crypto.ConfidentialityKeyBytes, len(vector.ConfidentialityKey))
 		assert.Equal(t, crypto.IntegrityKeyBytes, len(vector.IntegrityKey))
 		assert.Equal(t, crypto.RandChallengeBytes+crypto.AutnBytes, len(vector.RandAutn))
@@ -33,12 +41,33 @@ func TestMAR_Successful(t *testing.T) {
 	}
 }
 
+func TestMAR_AuthRejected(t *testing.T) {
+	hss := getTestHSSDiameterServer(t)
+	subscriber, err := hss.GetSubscriberData(context.Background(), &lteprotos.SubscriberID{Id: "sub1"})
+	assert.NoError(t, err)
+	subscriber.Non_3Gpp.Non_3GppIpAccess = lteprotos.Non3GPPUserProfile_NON_3GPP_SUBSCRIPTION_BARRED
+	_, err = hss.UpdateSubscriber(context.Background(), subscriber)
+	assert.NoError(t, err)
+
+	swxProxy := getTestSwxProxy(t, hss, true)
+	mar := &fegprotos.AuthenticationRequest{
+		UserName:             "sub1",
+		SipNumAuthVectors:    5,
+		AuthenticationScheme: fegprotos.AuthenticationScheme_EAP_AKA,
+	}
+	maa, err := swxProxy.Authenticate(context.Background(), mar)
+	assert.EqualError(t, err, "rpc error: code = PermissionDenied desc = User sub1 is not authorized for Non-3GPP Subscription Access")
+	assert.Equal(t, "sub1", maa.GetUserName())
+	assert.Equal(t, 0, len(maa.GetSipAuthVectors()))
+}
+
 func TestMAR_UnknownIMSI(t *testing.T) {
-	swxProxy := getTestSwxProxy(t)
-	mar := &protos.AuthenticationRequest{
+	hss := getTestHSSDiameterServer(t)
+	swxProxy := getTestSwxProxy(t, hss, false)
+	mar := &fegprotos.AuthenticationRequest{
 		UserName:             "sub_unknown",
 		SipNumAuthVectors:    1,
-		AuthenticationScheme: protos.AuthenticationScheme_EAP_AKA,
+		AuthenticationScheme: fegprotos.AuthenticationScheme_EAP_AKA,
 	}
 	maa, err := swxProxy.Authenticate(context.Background(), mar)
 	assert.EqualError(t, err, "rpc error: code = Code(5001) desc = Diameter Error: 5001 (USER_UNKNOWN)")
@@ -47,23 +76,24 @@ func TestMAR_UnknownIMSI(t *testing.T) {
 }
 
 func TestSAR_SuccessfulRegistration(t *testing.T) {
-	swxProxy := getTestSwxProxy(t)
-	sar := &protos.RegistrationRequest{UserName: "sub1"}
+	hss := getTestHSSDiameterServer(t)
+	swxProxy := getTestSwxProxy(t, hss, false)
+	sar := &fegprotos.RegistrationRequest{UserName: "sub1"}
 	_, err := swxProxy.Register(context.Background(), sar)
 	assert.NoError(t, err)
 }
 
 func TestSAR_UnknownIMSI(t *testing.T) {
-	swxProxy := getTestSwxProxy(t)
-	sar := &protos.RegistrationRequest{UserName: "sub_unknown"}
+	hss := getTestHSSDiameterServer(t)
+	swxProxy := getTestSwxProxy(t, hss, false)
+	sar := &fegprotos.RegistrationRequest{UserName: "sub_unknown"}
 	_, err := swxProxy.Register(context.Background(), sar)
 	assert.EqualError(t, err, "rpc error: code = Code(5001) desc = Diameter Error: 5001 (USER_UNKNOWN)")
 }
 
 // getTestSwxProxy creates a SWx Proxy server and test HSS Diameter
 // server which are configured to communicate with each other.
-func getTestSwxProxy(t *testing.T) protos.SwxProxyServer {
-	hss := getTestHSSDiameterServer(t)
+func getTestSwxProxy(t *testing.T, hss *servicers.HomeSubscriberServer, verifyAuthorization bool) fegprotos.SwxProxyServer {
 	serverCfg := hss.Config.Server
 
 	// Create an swx proxy server.
@@ -85,12 +115,12 @@ func getTestSwxProxy(t *testing.T) protos.SwxProxyServer {
 		DestHost:  serverCfg.DestHost,
 		DestRealm: serverCfg.DestRealm,
 	}
-	swxProxyConfig := &servicers.SwxProxyConfig{
+	swxProxyConfig := &swx.SwxProxyConfig{
 		ClientCfg:           clientCfg,
 		ServerCfg:           diameterServerCfg,
-		VerifyAuthorization: false,
+		VerifyAuthorization: verifyAuthorization,
 	}
-	swxProxy, err := servicers.NewSwxProxy(swxProxyConfig)
+	swxProxy, err := swx.NewSwxProxy(swxProxyConfig)
 	assert.NoError(t, err)
 	return swxProxy
 }
