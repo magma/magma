c5df3e48add82438b3943e8cb2d9f387b4efbe78
Murtadha Al-Tameemi <murtadha@fb.com>
2019-03-11 21:18:34 -0700

    Add test for organization detection
    Reviewed By: rckclmbr
    
    Differential Revision: D14313745
    
    fbshipit-source-id: 13c244ec38465faf1ec210d5b0b67fad63b40432

---
+++ nms/fbcnms-packages/fbcnms-express-middleware/__tests__/organization_test.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import {getOrganization} from '@fbcnms/express-middleware/organization';
+import {find} from 'lodash-es';
+
+const ORGS = [
+  {
+    id: '1',
+    name: 'custom_domain_org',
+    customDomains: ['subdomain.localtest.me'],
+  },
+  {
+    id: '2',
+    name: 'subdomain',
+    customDomains: [],
+  },
+];
+
+const MockOrganization = {
+  findOne: ({where}) => {
+    if (where.name) {
+      return find(ORGS, where);
+    }
+
+    return find(ORGS, {customDomains: [JSON.parse(where.args[1])]});
+  },
+};
+
+describe('organization tests', () => {
+  it('should allow custom domain', async () => {
+    const request = {
+      get: () => 'subdomain.localtest.me',
+    };
+
+    const org = await getOrganization(request, MockOrganization);
+    expect(org.name).toBe(ORGS[0].name);
+  });
+
+  it('should allow org by subdomain', async () => {
+    const request = {
+      get: () => 'subdomain.phbcloud.io',
+    };
+
+    const org = await getOrganization(request, MockOrganization);
+    expect(org.name).toBe(ORGS[1].name);
+  });
+
+  it('should throw an exception when no org is found', async () => {
+    const request = {
+      get: () => 'unknowndomain.com',
+    };
+
+    await expect(getOrganization(request, MockOrganization)).rejects.toThrow();
+  });
+});
