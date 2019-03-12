7cfecec9ae91dae09c8e29870ddc945439b98523
Andrei Lee <andreilee@fb.com>
2019-03-11 17:28:52 -0700

    Add parameter value transform for bandwidth (#48)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/48
    
    Some equivalent parameters are formatted differently across devices, or between a device and eNB. Transform functions are made to convert between the format that an eNB and Magma enodebd expects. This revision adds a transform function for bandwidth.
    
    Reviewed By: sciencemanx
    
    Differential Revision: D14382381
    
    fbshipit-source-id: 714c5beec6d5888fb8b4fd3e58a3d22bea258ae1

--- lte/gateway/python/magma/enodebd/data_models/transform_for_magma.py
+++ lte/gateway/python/magma/enodebd/data_models/transform_for_magma.py
@@ -8,22 +8,34 @@ of patent rights can be found in the PATENTS file in the same directory.
 """
 
 import textwrap
+from typing import Optional
+from magma.enodebd.exceptions import ConfigurationError
+
 
 DUPLEX_MAP = {
     '01': 'TDDMode',
     '02': 'FDDMode'
 }
 
+BANDWIDTH_RBS_TO_MHZ_MAP = {
+    'n6': 1.4,
+    'n15': 3,
+    'n25': 5,
+    'n50': 10,
+    'n75': 15,
+    'n100': 20,
+}
 
-def duplex_mode(value):
+
+def duplex_mode(value: str) -> Optional[str]:
     return DUPLEX_MAP.get(value)
 
 
-def band_capability(value):
+def band_capability(value: str) -> str:
     return ','.join([str(int(b, 16)) for b in textwrap.wrap(value, 2)])
 
 
-def gps_tr181(value):
+def gps_tr181(value: str) -> str:
     """Convert GPS value (lat or lng) to float
 
     Per TR-181 specification, coordinates are returned in degrees,
@@ -37,3 +49,21 @@ def gps_tr181(value):
     if value.isnumeric():
         return str(float(value) / 1e6)
     return value
+
+
+def bandwidth(bandwidth_rbs: str) -> float:
+    """
+
+    Map bandwidth in number of RBs to MHz
+    TODO: TR-196 spec says this should be '6' rather than 'n6', but
+    BaiCells eNodeB uses 'n6'. Need to resolve this.
+
+    Args:
+        bandwidth_rbs (str): Bandwidth in number of RBs
+    Returns:
+        str: Bandwidth in MHz
+    """
+    if bandwidth_rbs not in BANDWIDTH_RBS_TO_MHZ_MAP:
+        raise ConfigurationError('Unknown bandwidth_rbs (%s)' %
+                                 str(bandwidth_rbs))
+    return BANDWIDTH_RBS_TO_MHZ_MAP[bandwidth_rbs]
