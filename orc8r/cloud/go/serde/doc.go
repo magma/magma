cbb682593470ee479df715558024ab6bf91fcd66
Jacky Tian <xjtian@fb.com>
2019-03-14 12:32:00 -0700

    Create common serde interface and registry (#67)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/67
    
    - It's becoming clearer now that the primary surface of integration with orc8r services is at the data serialization/deserialization level
    - For e.g. upcoming inventory service, entity service (for configs), and state service will all rely on a registry for serializing and deserializing data defined in plugins
    - Formalize this with a generic Serde (SERializer-DEserializer) interface and a registry-of-registries to unify this pattern
    
    Differential Revision: D14449704
    
    fbshipit-source-id: ae79cca768883759ae88b8131127860fc6e80b7e

---
+++ orc8r/cloud/go/serde/doc.go
@@ -0,0 +1,6 @@
+// Package serde contains the definition of a SERializer-DEserializer concept.
+// This package also includes a global registry of serdes for applications to
+// delegate implementation-agnostic serialization and deserialization to.
+// Serdes are one of the primary plugin interfaces exposed by orc8r to extend
+// services with domain-specific data models and logic.
+package serde
---
+++ orc8r/cloud/go/serde/registry_test.go
@@ -0,0 +1,60 @@
+package serde_test
+
+import (
+	"testing"
+
+	"magma/orc8r/cloud/go/serde"
+	"magma/orc8r/cloud/go/serde/mocks"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestSerialize(t *testing.T) {
+	serde.UnregisterAllSerdes(t)
+	defer func() {
+		serde.UnregisterAllSerdes(t)
+	}()
+
+	mockSerde := &mocks.Serde{}
+	mockSerde.On("GetDomain").Return("foo")
+	mockSerde.On("GetType").Return("bar")
+	mockSerde.On("Serialize", mock.Anything).Return([]byte("hello world"), nil)
+
+	err := serde.RegisterSerdes([]serde.Serde{mockSerde})
+	assert.NoError(t, err)
+	actual, err := serde.Serialize("foo", "bar", "baz")
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("hello world"), actual)
+
+	_, err = serde.Serialize("bar", "foo", "baz")
+	assert.EqualError(t, err, "No serdes registered for domain bar")
+
+	_, err = serde.Serialize("foo", "baz", "bar")
+	assert.EqualError(t, err, "No Serde found for type baz")
+}
+
+func TestDeserialize(t *testing.T) {
+	serde.UnregisterAllSerdes(t)
+	defer func() {
+		serde.UnregisterAllSerdes(t)
+	}()
+
+	mockSerde := &mocks.Serde{}
+	mockSerde.On("GetDomain").Return("foo")
+	mockSerde.On("GetType").Return("bar")
+	mockSerde.On("Deserialize", mock.Anything).Return("hello world", nil)
+
+	err := serde.RegisterSerdes([]serde.Serde{mockSerde})
+	assert.NoError(t, err)
+	actual, err := serde.Deserialize("foo", "bar", []byte("baz"))
+	assert.NoError(t, err)
+	assert.Equal(t, "hello world", actual)
+
+	_, err = serde.Serialize("bar", "foo", "baz")
+	assert.EqualError(t, err, "No serdes registered for domain bar")
+
+	_, err = serde.Serialize("foo", "baz", "bar")
+	assert.EqualError(t, err, "No Serde found for type baz")
+
+}
