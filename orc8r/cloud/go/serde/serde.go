5dd265695961d81a3e5addb26ae9c31f5f9ed9f3
Jacky Tian <xjtian@fb.com>
2019-03-14 17:54:01 -0700

    Create common serde interface and registry
    Differential Revision: D14449704
    
    fbshipit-source-id: fe19b297b510ba7dc2c8ea2b41535c7d18aa7d49

---
+++ orc8r/cloud/go/serde/serde.go
@@ -0,0 +1,21 @@
+package serde
+
+// Serde (SERializer-DEserializer) implements logic to serialize/deserialize
+// a specific piece of data.
+type Serde interface {
+	// GetDomain returns a globally unique key which represents the domain of
+	// this Serde. Serde types are unique within each domain but not across
+	// domains.
+	GetDomain() string
+
+	// GetType returns a unique key within the domain for the specific Serde
+	// implementation. This represents the type of data that the Serde will be
+	// responsible for serializing and deserialing.
+	GetType() string
+
+	// Serialize a piece of data
+	Serialize(interface{}) ([]byte, error)
+
+	// Deserialize a piece of data
+	Deserialize([]byte) (interface{}, error)
+}
