a369ec7c886c8ef497e46217c2b747354407bf72
Vik Goel <vikg@fb.com>
2019-03-19 16:11:08 -0700

    Refactor HSS auth logic to make it reusable by the EPS authentication service (#90)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/90
    
    This change moves the authentication functions from testcore/hss to the eps authentication service. The functions have been modified to not require a HomeSubscriberServer and instead take in the arguments that they need directly. This makes the code possible to re-use by the eps authentication service.
    
    Reviewed By: xjtian
    
    Differential Revision: D14390092
    
    fbshipit-source-id: df21f91744e0efd29dd30e7eb77746ff0f67d123

--- feg/gateway/services/testcore/hss/servicers/util.go
+++ feg/gateway/services/testcore/hss/servicers/util.go
@@ -8,16 +8,6 @@ LICENSE file in the root directory of this source tree.
 
 package servicers
 
-// AllZero returns true if and only if the slice contains only zero bytes.
-func AllZero(bytes []byte) bool {
-	for _, b := range bytes {
-		if b != 0 {
-			return false
-		}
-	}
-	return true
-}
-
 // BoolToInt converts true to a 1 and false to a 0.
 func BoolToInt(b bool) int {
 	if b {
