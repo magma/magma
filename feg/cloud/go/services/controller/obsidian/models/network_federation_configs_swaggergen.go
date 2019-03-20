c7953fb56553a24918cb19a16ad56b84ee91e6d6
Jacky Tian <xjtian@fb.com>
2019-03-20 02:17:54 -0700

    Update swagger codegen for orc8r
    Summary:
    - Looks like updating golang.org/x/tools in the diff which adds Serde changes how go-swagger organizes the imports in swaggergen files
    - For organization, this updates the generated files which were modified in the stacked diff
    
    Reviewed By: vikg-fb
    
    Differential Revision: D14497243
    
    fbshipit-source-id: 7dfbe96917575b5cbad04fb654e4e864898d35ef

--- feg/cloud/go/services/controller/obsidian/models/network_federation_configs_swaggergen.go
+++ feg/cloud/go/services/controller/obsidian/models/network_federation_configs_swaggergen.go
@@ -9,8 +9,9 @@ import (
 	"encoding/json"
 	"strconv"
 
-	"github.com/go-openapi/errors"
 	strfmt "github.com/go-openapi/strfmt"
+
+	"github.com/go-openapi/errors"
 	"github.com/go-openapi/swag"
 	"github.com/go-openapi/validate"
 )
