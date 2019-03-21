37e5f884a040ba647c0a6c018dc26edbf4232130
Vik Goel <vikg@fb.com>
2019-03-21 16:03:10 -0700

    Implement PurgeUE function
    Summary: This change implements the PurgeUE gRPC function in the EPS Authentication service. PurgeUE is one of the functions in the S6a Proxy interface which the EPS Authentication service implements. See 3GPP TS 29.272 section 5.2.1.3 for more details.
    
    Reviewed By: fishlinghu
    
    Differential Revision: D14549064
    
    fbshipit-source-id: cdc873fd0e151cf30e829d31b63a1dda27e15fca

--- lte/cloud/go/services/eps_authentication/servicers/pur.go
+++ lte/cloud/go/services/eps_authentication/servicers/pur.go
@@ -9,13 +9,37 @@ LICENSE file in the root directory of this source tree.
 package servicers
 
 import (
+	"errors"
+
+	"magma/feg/cloud/go/protos"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	"magma/feg/cloud/go/protos"
 )
 
 func (srv *EPSAuthServer) PurgeUE(ctx context.Context, purge *protos.PurgeUERequest) (*protos.PurgeUEAnswer, error) {
-	return nil, status.Errorf(codes.Unimplemented, "purge UE not implemented")
+	if err := validatePUR(purge); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
+	}
+	networkID, err := getNetworkID(ctx)
+	if err != nil {
+		return nil, err
+	}
+	_, errorCode, err := srv.lookupSubscriber(purge.UserName, networkID)
+	if err != nil {
+		return &protos.PurgeUEAnswer{ErrorCode: errorCode}, err
+	}
+	return &protos.PurgeUEAnswer{ErrorCode: protos.ErrorCode_SUCCESS}, nil
+}
+
+// validatePUR returns an error iff the PUR is invalid.
+func validatePUR(purge *protos.PurgeUERequest) error {
+	if purge == nil {
+		return errors.New("received a nil PurgeUERequest")
+	}
+	if len(purge.UserName) == 0 {
+		return errors.New("user name was empty")
+	}
+	return nil
 }
