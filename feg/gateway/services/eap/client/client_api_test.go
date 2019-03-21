6612131aca9a7f83c81f584cd12d1d999052a1b1
Evgeniy Makeev <evgeniym@fb.com>
2019-03-20 17:15:18 -0700

    EAP-AKA Challenge Resp Verification (#107)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/107
    
    EAP-AKA Challenge response handler & supporting utilities, fixes and unit tests
    This change also includes an addition of dedicated gomod for eap/client
    
    Reviewed By: jrallapalli
    
    Differential Revision: D14536033
    
    fbshipit-source-id: f94a1320981d5eb0e6dec17c38f6d90e09d18d6e

--- feg/gateway/services/eap/client/client_api_test.go
+++ feg/gateway/services/eap/client/client_api_test.go
@@ -10,9 +10,14 @@ package client_test
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
+	"magma/feg/gateway/services/eap"
+	"magma/feg/gateway/services/eap/providers/aka"
 	"magma/feg/cloud/go/protos"
 	"magma/feg/gateway/registry"
 	"magma/feg/gateway/services/eap/client"
@@ -24,6 +29,7 @@ import (
 
 type testSwxProxy struct{}
 
+var rpcResponseDelay time.Duration
 // Test SwxProxyServer implementation
 //
 // Authenticate sends MAR (code 303) over diameter connection,
@@ -32,6 +38,9 @@ func (s testSwxProxy) Authenticate(
 	ctx context.Context,
 	req *protos.AuthenticationRequest,
 ) (*protos.AuthenticationAnswer, error) {
+
+	time.Sleep(rpcResponseDelay)
+
 	return &protos.AuthenticationAnswer{
 		UserName: req.GetUserName(),
 		SipAuthVectors: []*protos.AuthenticationAnswer_SIPAuthVector{
@@ -62,9 +71,16 @@ var (
 		"\x30\x31\x30\x30\x30\x30\x30\x30\x30\x30\x35\x35\x40\x77\x6c\x61" +
 		"\x6e\x2e\x6d\x6e\x63\x30\x30\x31\x2e\x6d\x63\x63\x30\x30\x31\x2e" +
 		"\x33\x67\x70\x70\x6e\x65\x74\x77\x6f\x72\x6b\x2e\x6f\x72\x67\x00")
-	expectedResp = []byte{1, 1, 0, 68, 23, 1, 0, 0, 1, 5, 0, 0, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69,
-		103, 137, 171, 205, 239, 2, 5, 0, 0, 84, 171, 100, 74, 144, 81, 185, 185, 94, 133, 193, 34, 62, 14, 241,
-		76, 11, 5, 0, 0, 113, 89, 171, 136, 187, 183, 17, 173, 129, 81, 120, 214, 133, 52, 0, 117}
+	expectedTestEap = []byte{1, 2, 0, 68, 23, 1, 0, 0, 1, 5, 0, 0, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69,
+		103,137, 171, 205, 239, 2, 5, 0, 0, 84, 171, 100, 74, 144, 81, 185, 185, 94, 133, 193, 34, 62, 14, 241,
+		76, 11, 5, 0, 0, 187, 28, 77, 175, 111, 216, 83, 74, 247, 124, 169, 254, 40, 141, 169, 189}
+
+	testEapChallengeResp = []byte("\x02\x02\x00\x28\x17\x01\x00\x00\x0b\x05\x00\x00\xfd\x2b\x50\xbd" +
+		"\x32\x24\x7a\xd7\x32\x9d\x9d\x26\x41\x60\x44\x46\x03\x03\x00\x40\x29\x5c\x00\xea\xe3\x88\x93\x0d")
+	expectedChallengeResp = []byte{1, 2, 0, 68, 23, 1, 0, 0, 1, 5, 0, 0, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103,
+		137, 171, 205, 239, 2, 5, 0, 0, 84, 171, 100, 74, 144, 81, 185, 185, 94, 133, 193, 34, 62, 14, 241, 76, 11, 5,
+		0, 0, 180, 191, 23, 199, 219, 210, 244, 54, 3, 41, 254, 37, 158, 216, 47, 19}
+	successEAP = []byte{3, 2, 0, 4}
 )
 
 func TestEAPClientApi(t *testing.T) {
@@ -82,13 +98,50 @@ func TestEAPClientApi(t *testing.T) {
 	eap_protos.RegisterEapServiceServer(eapSrv.GrpcServer, servicer)
 	go eapSrv.RunTest(eapLis)
 
-	peap, err := client.Handle(&eap_protos.Eap{Payload: testEapIdentityResp})
+	eapCtx := &eap_protos.EapContext{SessionId: eap.CreateSessionId()}
+	peap, err := client.Handle(&eap_protos.Eap{Payload: testEapIdentityResp, Ctx: eapCtx})
 	if err != nil {
 		t.Fatalf("Error Handling Test EAP: %v", err)
 	}
-	if !reflect.DeepEqual([]byte(peap.GetPayload()), []byte(expectedResp)) {
+	if !reflect.DeepEqual([]byte(peap.GetPayload()), []byte(expectedTestEap)) {
 		t.Fatalf(
 			"Unexpected identityResponse EAP\n\tReceived: %.3v\n\tExpected: %.3v",
-			peap.GetPayload(), expectedResp)
+			peap.GetPayload(), expectedTestEap)
+	}
+	eapCtx = peap.GetCtx()
+	peap, err = client.Handle(&eap_protos.Eap{Payload: testEapChallengeResp, Ctx: eapCtx})
+	if err != nil {
+		t.Fatalf("Error Handling Test Challenge EAP: %v", err)
+	}
+	if !reflect.DeepEqual([]byte(peap.GetPayload()), []byte(successEAP)) {
+		t.Fatalf(
+			"Unexpected Challenge Response EAP\n\tReceived: %.3v\n\tExpected: %.3v",
+			peap.GetPayload(), expectedChallengeResp)
+	}
+
+	eapCtx = peap.GetCtx()
+	peap, err = client.Handle(&eap_protos.Eap{Payload: testEapChallengeResp, Ctx: eapCtx})
+	if err == nil {
+		t.Fatalf("Expected Error for removed Session ID: %s", eapCtx.SessionId)
+	}
+	grpcCode := status.Convert(err).Code()
+	if grpcCode != codes.FailedPrecondition {
+		t.Fatalf("Unexpected Error Copde: %d", grpcCode)
+	}
+
+	// Test timeout
+	aka.SetChallengeTimeout(time.Millisecond * 300)
+	eapCtx = &eap_protos.EapContext{SessionId: eap.CreateSessionId()}
+	peap, err = client.Handle(&eap_protos.Eap{Payload: testEapIdentityResp, Ctx: eapCtx})
+	if err != nil {
+		t.Fatalf("Error Handling second Test EAP: %v", err)
+	}
+
+	time.Sleep(aka.ChallengeTimeout() + time.Millisecond * 100)
+
+	eapCtx = peap.GetCtx()
+	peap, err = client.Handle(&eap_protos.Eap{Payload: testEapChallengeResp, Ctx: eapCtx})
+	if err == nil {
+		t.Fatalf("Expected Error for timed out Session ID: %s", eapCtx.SessionId)
 	}
 }
---
+++ feg/gateway/services/eap/providers/aka/servicers/handlers/aka_challenge.go
@@ -0,0 +1,129 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// Package handlers provided AKA Response handlers for supported AKA subtypes
+package handlers
+
+import (
+	"io"
+	"log"
+	"reflect"
+
+	"google.golang.org/grpc/codes"
+
+	"magma/feg/gateway/services/eap"
+	"magma/feg/gateway/services/eap/protos"
+	"magma/feg/gateway/services/eap/providers/aka"
+	"magma/feg/gateway/services/eap/providers/aka/servicers"
+)
+
+func init() {
+	servicers.AddHandler(aka.SubtypeChallenge, challengeResponse)
+}
+
+// challengeResponse implements handler for AKA Challenge Response,
+// see https://tools.ietf.org/html/rfc4187#page-49 for details
+func challengeResponse(s *servicers.EapAkaSrv, ctx *protos.EapContext, req eap.Packet) (eap.Packet, error) {
+	identifier := req.Identifier()
+	if ctx == nil {
+		return aka.EapErrorResPacket(identifier, aka.NOTIFICATION_FAILURE, codes.InvalidArgument, "Nil CTX")
+	}
+	if len(ctx.SessionId) == 0 {
+		return aka.EapErrorResPacket(identifier, aka.NOTIFICATION_FAILURE, codes.InvalidArgument, "Missing Session ID")
+	}
+	imsi, ok := s.FindAndRemoveSession(ctx.SessionId)
+	if !ok {
+		return aka.EapErrorResPacket(identifier, aka.NOTIFICATION_FAILURE, codes.FailedPrecondition,
+			"No Session found for ID: %s", ctx.SessionId)
+	}
+
+	p := make([]byte, len(req))
+	copy(p, req)
+	scanner, err := eap.NewAttributeScanner(p)
+	if err != nil {
+		return aka.EapErrorResPacket(identifier, aka.NOTIFICATION_FAILURE, codes.Aborted, err.Error())
+	}
+
+	uc := s.FindLockedUserCtx(imsi)
+	if uc == nil {
+		return aka.EapErrorResPacket(identifier, aka.NOTIFICATION_FAILURE, codes.FailedPrecondition,
+			"No IMSI '%s' found for SessionID: %s", imsi, ctx.SessionId)
+	}
+
+	// Delete CTX from state map & unlock CTX at the end
+	defer func() {
+		uc.SessionId = ""
+		s.DeleteUserCtx(uc)
+		uc.Unlock()
+	}()
+
+	var a, atMac, atRes eap.Attribute
+
+attrLoop:
+	for a, err = scanner.Next(); err == nil; a, err = scanner.Next() {
+		switch a.Type() {
+		case aka.AT_MAC:
+			atMac = a
+			if atRes != nil {
+				break attrLoop
+			}
+		case aka.AT_RES:
+			atRes = a
+			if atMac != nil {
+				break attrLoop
+			}
+		default:
+			log.Printf("Unexpected EAP-AKA Challenge Response Attribute type %d", a.Type())
+		}
+	}
+
+	if err != nil {
+		if err == io.EOF {
+			return aka.EapErrorResPacketWithMac(
+				identifier, aka.NOTIFICATION_FAILURE_AUTH, uc.K_aut, codes.InvalidArgument, "Missing AT_MAC | AT_RES")
+		}
+		return aka.EapErrorResPacketWithMac(
+			identifier, aka.NOTIFICATION_FAILURE_AUTH, uc.K_aut, codes.InvalidArgument, err.Error())
+	}
+
+	// Verify AT_RES
+	ueRes := atRes.Marshaled()[aka.ATT_HDR_LEN:]
+	if !reflect.DeepEqual(ueRes, uc.Xres) {
+		log.Printf("Invalid AT_RES for Session ID: %s; IMSI: %s\n\t%.3v !=\n\t%.3v", ctx.SessionId, imsi, ueRes, uc.Xres)
+		return aka.EapErrorResPacketWithMac(
+			identifier, aka.NOTIFICATION_FAILURE_AUTH, uc.K_aut, codes.Unauthenticated,
+			"Invalid AT_RES for Session ID: %s; IMSI: %s", ctx.SessionId, imsi)
+	}
+
+	// Verify MAC
+	macBytes := atMac.Marshaled()
+	if len(macBytes) < aka.ATT_HDR_LEN+aka.MAC_LEN {
+		return aka.EapErrorResPacketWithMac(
+			identifier, aka.NOTIFICATION_FAILURE_AUTH, uc.K_aut, codes.InvalidArgument, "Malformed AT_MAC")
+	}
+	ueMac := make([]byte, len(macBytes)-aka.ATT_HDR_LEN)
+	copy(ueMac, macBytes[aka.ATT_HDR_LEN:])
+
+	for i := aka.ATT_HDR_LEN; i < len(macBytes); i++ {
+		macBytes[i] = 0
+	}
+	mac := aka.GenMac(p, uc.K_aut)
+	if !reflect.DeepEqual(ueMac, mac) {
+		log.Printf(
+			"Invalid MAC for Session ID: %s; IMSI: %s; UE MAC: %x; Expected MAC: %x; EAP: %x",
+			ctx.SessionId, imsi, ueMac, mac, req)
+		return aka.EapErrorResPacketWithMac(
+			identifier, aka.NOTIFICATION_FAILURE_AUTH, uc.K_aut, codes.Unauthenticated,
+			"Invalid MAC for Session ID: %s; IMSI: %s", ctx.SessionId, imsi)
+	}
+
+	// All good, return
+	ctx.Msk = uc.MSK
+
+	return []byte{eap.SuccessCode, identifier, 0, 4}, nil
+}
