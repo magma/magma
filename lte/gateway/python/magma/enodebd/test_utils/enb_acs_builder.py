4479e019ac0414212c52fe9377d1c289bcd57be4
Andrei Lee <andreilee@fb.com>
2019-03-26 13:46:04 -0700

    Reorganize unit tests due to common functionality (#126)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/126
    
    Magma's enodebd service has an increasing number of tests with redundant logic to set up the tests. This revision refactors some unit tests to group together redundant logic into test utils.
    
    Reviewed By: sciencemanx
    
    Differential Revision: D14566517
    
    fbshipit-source-id: e71dbafc15369d539b4f44d05343ea5eb896bfd4

---
+++ lte/gateway/python/magma/enodebd/test_utils/enb_acs_builder.py
@@ -0,0 +1,43 @@
+"""
+Copyright (c) 2016-present, Facebook, Inc.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. An additional grant
+of patent rights can be found in the PATENTS file in the same directory.
+"""
+
+import asyncio
+from unittest import mock
+from magma.common.service import MagmaService
+from magma.enodebd.devices.device_map import get_device_handler_from_name
+from magma.enodebd.devices.device_utils import EnodebDeviceName
+from magma.enodebd.state_machines.enb_acs import EnodebAcsStateMachine
+from magma.enodebd.stats_manager import StatsManager
+from gateway.python.magma.enodebd.test_utils.config_builder import \
+    EnodebConfigBuilder
+
+
+class EnodebAcsStateMachineBuilder:
+    @classmethod
+    def build_acs_state_machine(
+        cls,
+        device: EnodebDeviceName = EnodebDeviceName.BAICELLS,
+    ) -> EnodebAcsStateMachine:
+        # Build the state_machine
+        stats_mgr = StatsManager()
+        service = cls.build_magma_service()
+        handler_class = get_device_handler_from_name(device)
+        acs_state_machine = handler_class(service, stats_mgr)
+        return acs_state_machine
+
+    @classmethod
+    def build_magma_service(cls) -> MagmaService:
+        event_loop = asyncio.get_event_loop()
+        mconfig = EnodebConfigBuilder.get_mconfig()
+        service_config = EnodebConfigBuilder.get_service_config()
+        with mock.patch('magma.common.service.MagmaService') as MockService:
+            MockService.config = service_config
+            MockService.mconfig = mconfig
+            MockService.loop = event_loop
+            return MockService
