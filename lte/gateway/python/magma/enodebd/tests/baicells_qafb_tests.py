272e4c6d19b7b8a950caadb6ca0ae658365910c3
Andrei Lee <andreilee@fb.com>
2019-03-26 13:46:06 -0700

    Reboot request will send after responding to Inform with InformResponse (#132)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/132
    
    While eNB will be able to respond to an empty http response when an Inform is sent to the AutoConfigurationServer (hosted by enodebd), it will not be able to respond to a Reboot. To handle this, enodebd will now make sure an InformResponse is sent back to the eNB before a Reboot request is sent.
    
    **eNB -> ACS**
      Inform
    
    **ACS -> eNB**
      InformResponse
    
    **eNB -> ACS**
      (empty HTTP request)
    
    **ACS -> eNB**
      Reboot
    
    **eNB -> ACS**
      RebootResponse
    
    **ACS -> eNB**
      (empty HTTP request)
    
    Reviewed By: sciencemanx
    
    Differential Revision: D14582361
    
    fbshipit-source-id: a2c8f35d9df4b610187045cf28617a55f9d28ee4

--- lte/gateway/python/magma/enodebd/tests/baicells_qafb_tests.py
+++ lte/gateway/python/magma/enodebd/tests/baicells_qafb_tests.py
@@ -18,6 +18,85 @@ from magma.enodebd.tests.test_utils.enb_acs_builder import \
 
 
 class BaicellsQAFBHandlerTests(TestCase):
+    def test_manual_reboot(self) -> None:
+        """
+        Test a scenario where a Magma user goes through the enodebd CLI to
+        reboot the Baicells eNodeB.
+
+        This checks the scenario where the command is not sent in the middle
+        of a TR-069 provisioning session.
+        """
+        acs_state_machine = \
+            EnodebAcsStateMachineBuilder \
+                .build_acs_state_machine(EnodebDeviceName.BAICELLS_QAFB)
+
+        # User uses the CLI tool to get eNodeB to reboot
+        acs_state_machine.reboot_asap()
+
+        # And now the Inform message arrives from the eNodeB
+        inform_msg = \
+            Tr069MessageBuilder.get_inform('48BF74',
+                                           'BaiBS_QAFBv123',
+                                           '120200002618AGP0003',
+                                           ['2 PERIODIC'])
+        resp = acs_state_machine.handle_tr069_message(inform_msg)
+        self.assertTrue(isinstance(resp, models.InformResponse),
+                        'In reboot sequence, state machine should still '
+                        'respond to an Inform with InformResponse.')
+        req = models.DummyInput()
+        resp = acs_state_machine.handle_tr069_message(req)
+        self.assertTrue(isinstance(resp, models.Reboot),
+                        'In reboot sequence, state machine should send a '
+                        'Reboot message.')
+        req = Tr069MessageBuilder.get_reboot_response()
+        resp = acs_state_machine.handle_tr069_message(req)
+        self.assertTrue(isinstance(resp, models.DummyInput),
+                        'State machine should end TR-069 session after '
+                        'receiving a RebootResponse')
+
+    def test_manual_reboot_during_provisioning(self) -> None:
+        """
+        Test a scenario where a Magma user goes through the enodebd CLI to
+        reboot the Baicells eNodeB.
+
+        This checks the scenario where the command is sent in the middle
+        of a TR-069 provisioning session.
+        """
+        acs_state_machine = \
+            EnodebAcsStateMachineBuilder \
+                .build_acs_state_machine(EnodebDeviceName.BAICELLS_QAFB)
+
+        # Send an Inform message, wait for an InformResponse
+        inform_msg = \
+            Tr069MessageBuilder.get_inform('48BF74',
+                                           'BaiBS_QAFBv123',
+                                           '120200002618AGP0003',
+                                           ['2 PERIODIC'])
+        resp = acs_state_machine.handle_tr069_message(inform_msg)
+        self.assertTrue(isinstance(resp, models.InformResponse),
+                        'Should respond with an InformResponse')
+
+        # Send an empty http request to kick off the rest of provisioning
+        req = models.DummyInput()
+        resp = acs_state_machine.handle_tr069_message(req)
+
+        # Expect a request for an optional parameter, three times
+        self.assertTrue(isinstance(resp, models.GetParameterValues),
+                        'State machine should be requesting param values')
+        req = Tr069MessageBuilder.get_fault()
+
+        # User uses the CLI tool to get eNodeB to reboot
+        acs_state_machine.reboot_asap()
+
+        resp = acs_state_machine.handle_tr069_message(req)
+        self.assertTrue(isinstance(resp, models.Reboot),
+                        'In reboot sequence, state machine should send a '
+                        'Reboot message.')
+        req = Tr069MessageBuilder.get_reboot_response()
+        resp = acs_state_machine.handle_tr069_message(req)
+        self.assertTrue(isinstance(resp, models.DummyInput),
+                        'State machine should end TR-069 session after '
+                        'receiving a RebootResponse')
     def test_provision(self) -> None:
         acs_state_machine = \
             EnodebAcsStateMachineBuilder \
