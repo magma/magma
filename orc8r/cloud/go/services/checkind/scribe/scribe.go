fae234659a79f51ea4ae6608349eeaffcf186245
Ian Hu <ianhu@fb.com>
2019-03-29 09:22:26 -0700

    Add new gateway status info to scribe
    Summary:
    This diff adds the new gateway status info to scribe. We will support backwards compatibility by continuing to fill deprecated fields.
    
    The diff also updates the unit test of scribe to perform a direct map compare, rather than asserting specific key-value pairs in the map.
    
    Reviewed By: amarpad
    
    Differential Revision: D14551323
    
    fbshipit-source-id: 2ff709e68a5d066ca62cfadb71f05f3e09030354

--- orc8r/cloud/go/services/checkind/scribe/scribe.go
+++ orc8r/cloud/go/services/checkind/scribe/scribe.go
@@ -9,6 +9,7 @@ LICENSE file in the root directory of this source tree.
 package scribe
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -53,7 +54,7 @@ func FormatScribeGwStatusMessage(
 		return nil, nil, fmt.Errorf("Checkin status is nil")
 	}
 	normalMsg := formatScribeGwStatusNormalMessage(status.Checkin.Status, status.Checkin, networkId, logicalId)
-	intMsg := formatScribeGwStatusIntMessage(status.Checkin.SystemStatus, status.CertExpirationTime)
+	intMsg := formatScribeGwStatusIntMessage(status.Checkin.SystemStatus, status.Checkin.GetMachineInfo(), status.CertExpirationTime)
 
 	return normalMsg, intMsg, nil
 }
@@ -68,7 +69,6 @@ func formatScribeGwStatusNormalMessage(
 	normalMsg["network_id"] = networkId
 	normalMsg["gateway_id"] = logicalId
 	normalMsg["hardware_id"] = checkin.GatewayId
-	normalMsg["magma_package_version"] = checkin.MagmaPkgVersion
 	normalMsg["enodeb_configured"] = getValueFromMetaMap("enodeb_configured", serviceStatus)
 	normalMsg["enodeb_connected"] = getValueFromMetaMap("enodeb_connected", serviceStatus)
 	normalMsg["gps_connected"] = getValueFromMetaMap("gps_connected", serviceStatus)
@@ -78,9 +78,20 @@ func formatScribeGwStatusNormalMessage(
 	normalMsg["opstate_enabled"] = getValueFromMetaMap("opstate_enabled", serviceStatus)
 	normalMsg["ptp_connected"] = getValueFromMetaMap("ptp_connected", serviceStatus)
 	normalMsg["rf_tx_on"] = getValueFromMetaMap("rf_tx_on", serviceStatus)
-	normalMsg["vpn_ip"] = checkin.VpnIp
-	normalMsg["kernel_version"] = checkin.KernelVersion
-	normalMsg["kernel_versions_installed"] = strings.Join(checkin.KernelVersionsInstalled, ",")
+	normalMsg["magma_package_version"] = getMagmaPkgVersion(checkin)
+	normalMsg["vpn_ip"] = getVpnIP(checkin)
+	normalMsg["kernel_version"] = getKernelVersion(checkin)
+	normalMsg["kernel_versions_installed"] = strings.Join(getKernelVersionsInstalled(checkin), ",")
+
+	if checkin.SystemStatus != nil {
+		addSystemStatusNormalMessage(normalMsg, checkin.SystemStatus)
+	}
+	if checkin.PlatformInfo != nil {
+		addPlatformInfoNormalMessage(normalMsg, checkin.PlatformInfo)
+	}
+	if checkin.MachineInfo != nil {
+		addMachineInfoNormalMessage(normalMsg, checkin.MachineInfo)
+	}
 	return normalMsg
 }
 
@@ -100,19 +111,94 @@ func getValueFromMetaMap(key string, serviceStatus *protos.ServiceStatus) string
 	}
 }
 
-func formatScribeGwStatusIntMessage(systemStatus *protos.SystemStatus, certExpTime int64) map[string]int64 {
-	if systemStatus == nil {
-		return map[string]int64{"cert_expiration_time": certExpTime}
+func getMagmaPkgVersion(checkin *protos.CheckinRequest) string {
+	packages := checkin.GetPlatformInfo().GetPackages()
+	if packages != nil {
+		for _, pkg := range packages {
+			if pkg.Name == "magma" {
+				return pkg.Version
+			}
+		}
 	}
-	return map[string]int64{
-		"cpu_idle":             int64(systemStatus.CpuIdle),
-		"cpu_system":           int64(systemStatus.CpuSystem),
-		"cpu_user":             int64(systemStatus.CpuUser),
-		"mem_available":        int64(systemStatus.MemAvailable),
-		"mem_free":             int64(systemStatus.MemFree),
-		"mem_total":            int64(systemStatus.MemTotal),
-		"mem_used":             int64(systemStatus.MemUsed),
-		"uptime_secs":          int64(systemStatus.UptimeSecs),
-		"cert_expiration_time": certExpTime,
+	return checkin.MagmaPkgVersion
+}
+
+func getVpnIP(checkin *protos.CheckinRequest) string {
+	if checkin.PlatformInfo != nil {
+		return checkin.PlatformInfo.VpnIp
 	}
+	return checkin.VpnIp
+}
+
+func getKernelVersion(checkin *protos.CheckinRequest) string {
+	if checkin.PlatformInfo != nil {
+		return checkin.PlatformInfo.KernelVersion
+	}
+	return checkin.KernelVersion
+}
+
+func getKernelVersionsInstalled(checkin *protos.CheckinRequest) []string {
+	if checkin.PlatformInfo != nil {
+		return checkin.PlatformInfo.KernelVersionsInstalled
+	}
+	return checkin.KernelVersionsInstalled
+}
+
+func addSystemStatusNormalMessage(normalMsg map[string]string, systemStatus *protos.SystemStatus) {
+	if systemStatus.GetDiskPartitions() != nil {
+		tryAddProtoNormalMessage(normalMsg, "disk_partitions", systemStatus.GetDiskPartitions())
+	}
+}
+
+func addPlatformInfoNormalMessage(normalMsg map[string]string, platformInfo *protos.PlatformInfo) {
+	normalMsg["platform_info.vpn_ip"] = platformInfo.GetVpnIp()
+	if platformInfo.GetPackages() != nil {
+		tryAddProtoNormalMessage(normalMsg, "platform_info.packages", platformInfo.GetPackages())
+	}
+	normalMsg["platform_info.kernel_version"] = platformInfo.GetKernelVersion()
+	if platformInfo.GetKernelVersionsInstalled() != nil {
+		tryAddProtoNormalMessage(normalMsg, "platform_info.kernel_versions_installed", platformInfo.GetKernelVersionsInstalled())
+	}
+}
+
+func addMachineInfoNormalMessage(normalMsg map[string]string, machineInfo *protos.MachineInfo) {
+	normalMsg["machine_info.cpu_info.architecture"] = machineInfo.GetCpuInfo().GetArchitecture()
+	normalMsg["machine_info.cpu_info.model_name"] = machineInfo.GetCpuInfo().GetModelName()
+	if machineInfo.GetNetworkInfo().GetNetworkInterfaces() != nil {
+		tryAddProtoNormalMessage(normalMsg, "machine_info.network_info.network_interfaces", machineInfo.GetNetworkInfo().GetNetworkInterfaces())
+	}
+	if machineInfo.GetNetworkInfo().GetRoutingTable() != nil {
+		tryAddProtoNormalMessage(normalMsg, "machine_info.network_info.routing_table", machineInfo.GetNetworkInfo().GetRoutingTable())
+	}
+}
+
+func tryAddProtoNormalMessage(normalMsg map[string]string, key string, message interface{}) {
+	marshalledJSON, err := json.Marshal(message)
+	if err == nil {
+		normalMsg[key] = string(marshalledJSON)
+	} else {
+		glog.Errorf("Failed to marshal json for key %s: %s\n", key, err)
+	}
+}
+
+func formatScribeGwStatusIntMessage(systemStatus *protos.SystemStatus, machineInfo *protos.MachineInfo, certExpTime int64) map[string]int64 {
+	intMessage := map[string]int64{"cert_expiration_time": certExpTime}
+	if systemStatus != nil {
+		intMessage["cpu_idle"] = int64(systemStatus.CpuIdle)
+		intMessage["cpu_system"] = int64(systemStatus.CpuSystem)
+		intMessage["cpu_user"] = int64(systemStatus.CpuUser)
+		intMessage["mem_available"] = int64(systemStatus.MemAvailable)
+		intMessage["mem_free"] = int64(systemStatus.MemFree)
+		intMessage["mem_total"] = int64(systemStatus.MemTotal)
+		intMessage["mem_used"] = int64(systemStatus.MemUsed)
+		intMessage["uptime_secs"] = int64(systemStatus.UptimeSecs)
+		intMessage["swap_total"] = int64(systemStatus.SwapTotal)
+		intMessage["swap_used"] = int64(systemStatus.SwapUsed)
+		intMessage["swap_free"] = int64(systemStatus.SwapFree)
+	}
+	if machineInfo.GetCpuInfo() != nil {
+		intMessage["machine_info.cpu_info.core_count"] = int64(machineInfo.GetCpuInfo().GetCoreCount())
+		intMessage["machine_info.cpu_info.threads_per_core"] = int64(machineInfo.GetCpuInfo().GetThreadsPerCore())
+	}
+	return intMessage
 }
