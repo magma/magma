1a0ae7252a99901f4c55f1e53447391bbe5a805e
Ian Hu <ianhu@fb.com>
2019-03-29 09:22:25 -0700

    Update magmad for new gateway status data model
    Summary:
    This diff updates the checkin manager in magmad for the new gateway status model. New information are aggregated with the following libraries:
    
    - CPU / Memory/ Disk partition: psutil
    - CPU hardware info: lscpu(D14510290)
    - Network information: netifaces and route(D14510290)
    
    Reviewed By: amarpad
    
    Differential Revision: D14469094
    
    fbshipit-source-id: 10118248296a919916faaf6f32f7bb19228d7290

--- orc8r/gateway/python/magma/magmad/checkin_manager.py
+++ orc8r/gateway/python/magma/magmad/checkin_manager.py
@@ -13,19 +13,43 @@ import platform
 import sys
 import time
 import asyncio
+import netifaces
 
 import grpc
 import psutil
 import snowflake
-from orc8r.protos.magmad_pb2 import CheckinRequest, SystemStatus
+from orc8r.protos.magmad_pb2 import (
+    CheckinRequest,
+    SystemStatus,
+    DiskPartition,
+    PlatformInfo,
+    MachineInfo,
+    Package,
+    CPUInfo,
+    NetworkInfo,
+    NetworkInterface,
+    Route,
+)
 from orc8r.protos.magmad_pb2_grpc import CheckindStub
 
-from magma.common.misc_utils import get_ip_from_if
+from magma.common.misc_utils import (
+    get_ip_from_if,
+    is_interface_up,
+    get_all_ips_from_if_cidr,
+    get_if_mac_address,
+    IpPreference,
+)
 from magma.common.rpc_utils import grpc_async_wrapper
 from magma.common.sdwatchdog import SDWatchdogTask
 from magma.common.service_registry import ServiceRegistry
 from magma.magmad.check.kernel_check.kernel_versions import (
     get_kernel_versions_async,
+)
+from magma.magmad.check.machine_check.cpu_info import (
+    get_cpu_info,
+)
+from magma.magmad.check.network_check.routing_table import (
+    get_routing_table,
 )
 from magma.magmad.metrics import CHECKIN_STATUS
 
@@ -60,7 +84,17 @@ class CheckinManager(SDWatchdogTask):
 
         # One time status info
         self._boot_time = psutil.boot_time()
+        self._kernel_name = platform.system()
         self._kernel_version = platform.uname().release
+        cpu_info = get_cpu_info()
+        if cpu_info.error is not None:
+            logging.error('Failed to get cpu info: %s', cpu_info.error)
+        self._cpu_info = CPUInfo(
+            core_count=cpu_info.core_count,
+            threads_per_core=cpu_info.threads_per_core,
+            architecture=cpu_info.architecture,
+            model_name=cpu_info.model_name,
+        )
 
         self._kernel_versions_installed = []
         self._periodically_check_kernel_versions = \
@@ -142,30 +176,14 @@ class CheckinManager(SDWatchdogTask):
             self._checkin_client = CheckindStub(chan)
 
         mconfig = self._service.mconfig
-        cpu = psutil.cpu_times()
-        mem = psutil.virtual_memory()
-        try:
-            gw_ip = get_ip_from_if('tun0')  # look for tun0 interface
-        except ValueError:
-            gw_ip = 'N/A'
 
         request = CheckinRequest(
             gateway_id=snowflake.snowflake(),
-            magma_pkg_version=self._service.version,
-            system_status=SystemStatus(
-                cpu_user=int(cpu.user * 1000),  # convert second to millisecond
-                cpu_system=int(cpu.system * 1000),
-                cpu_idle=int(cpu.idle * 1000),
-                mem_total=mem.total,
-                mem_available=mem.available,
-                mem_used=mem.used,
-                mem_free=mem.free,
-                uptime_secs=int(time.time() - self._boot_time),
-            ),
-            vpn_ip=gw_ip,
-            kernel_version=self._kernel_version,
-            kernel_versions_installed=self._kernel_versions_installed,
+            system_status=self._system_status(),
+            platform_info=self._platform_info(),
+            machine_info=self._machine_info(),
         )
+        logging.debug('Checkin request:\n%s', request)
 
         for statusmeta in service_statusmeta.values():
             request.status.meta.update(statusmeta)
@@ -179,6 +197,106 @@ class CheckinManager(SDWatchdogTask):
             self._checkin_done()
         except grpc.RpcError as err:
             self._checkin_error(err)
+
+    def _system_status(self):
+        cpu = psutil.cpu_times()
+        mem = psutil.virtual_memory()
+        swap = psutil.swap_memory()
+
+        def partition_gen():
+            for partition in psutil.disk_partitions():
+                usage = psutil.disk_usage(partition.mountpoint)
+                yield DiskPartition(
+                    device=partition.device,
+                    mount_point=partition.mountpoint,
+                    total=usage.total,
+                    used=usage.used,
+                    free=usage.free,
+                )
+
+        return SystemStatus(
+            time=int(time.time()),
+            uptime_secs=int(time.time() - self._boot_time),
+            cpu_user=int(cpu.user * 1000),  # convert second to millisecond
+            cpu_system=int(cpu.system * 1000),
+            cpu_idle=int(cpu.idle * 1000),
+            mem_total=mem.total,
+            mem_available=mem.available,
+            mem_used=mem.used,
+            mem_free=mem.free,
+            swap_total=swap.total,
+            swap_used=swap.used,
+            swap_free=swap.free,
+            disk_partitions=[partition for partition in partition_gen()],
+        )
+
+    def _platform_info(self):
+        try:
+            gw_ip = get_ip_from_if('tun0')  # look for tun0 interface
+        except ValueError:
+            gw_ip = 'N/A'
+
+        return PlatformInfo(
+            vpn_ip=gw_ip,
+            packages=[
+                Package(
+                    name='magma',
+                    version=self._service.version,
+                ),
+            ],
+            kernel_version=self._kernel_version,
+            kernel_versions_installed=self._kernel_versions_installed,
+        )
+
+    def _network_info(self):
+        def network_interface_gen():
+            for interface in netifaces.interfaces():
+                try:
+                    mac_address = get_if_mac_address(interface)
+                except ValueError:
+                    mac_address = None
+
+                try:
+                    ip_addresses = get_all_ips_from_if_cidr(
+                        interface, IpPreference.IPV4_ONLY)
+                except ValueError:
+                    ip_addresses = []
+
+                try:
+                    ipv6_addresses = get_all_ips_from_if_cidr(
+                        interface, IpPreference.IPV6_ONLY)
+                except ValueError:
+                    ipv6_addresses = []
+
+                yield NetworkInterface(
+                    network_interface_id=interface,
+                    status=NetworkInterface.UP if is_interface_up(
+                        interface) else NetworkInterface.DOWN,
+                    mac_address=mac_address,
+                    ip_addresses=ip_addresses,
+                    ipv6_addresses=ipv6_addresses,
+                )
+
+        routing_cmd_result = get_routing_table()
+        if routing_cmd_result.error is not None:
+            logging.error("Failed to get routing table: %s",
+                          routing_cmd_result.error)
+
+        return NetworkInfo(
+            network_interfaces=[network_interface for network_interface in
+                                network_interface_gen()],
+            routing_table=[Route(destination_ip=route.destination,
+                                 gateway_ip=route.gateway,
+                                 genmask=route.genmask,
+                                 network_interface_id=route.interface) for
+                           route in routing_cmd_result.routing_table],
+        )
+
+    def _machine_info(self):
+        return MachineInfo(
+            cpu_info=self._cpu_info,
+            network_info=self._network_info(),
+        )
 
     def _checkin_error(self, err):
         logging.error("Checkin Error! [%s] %s", err.code(), err.details())
