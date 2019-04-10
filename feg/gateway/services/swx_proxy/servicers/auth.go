da10d48037747cbc41c2375a19a26c48f6a65e92
Michael Germano <mgermano@fb.com>
2019-04-10 14:09:25 -0700

    Add option to retrieve user profile from HSS on Authentication Request in swx_proxy (#184)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/184
    
    Recent integrations have shown that possibility of needing to have the msisdn passed back from
    the authentication request in swx_proxy. This is implemented here by having a parameter in the request
    determine if an additional SAR should be sent to the HSS before returning.
    
    Reviewed By: emakeev, vikg-fb
    
    Differential Revision: D14792734
    
    fbshipit-source-id: 9bb6afa4e5c1854b74c6798647ab27d0f8c1f98b

--- feg/gateway/services/swx_proxy/servicers/auth.go
+++ feg/gateway/services/swx_proxy/servicers/auth.go
@@ -83,11 +83,17 @@ func (s *swxProxy) AuthenticateImpl(req *protos.AuthenticationRequest) (*protos.
 			return res, err
 		}
 
-		if s.config.VerifyAuthorization {
-			err = s.authorize(req.GetUserName())
+		shouldSendSar := s.config.VerifyAuthorization || req.GetRetrieveUserProfile()
+		if shouldSendSar {
+			profile, authorized, err := s.retrieveUserProfile(req.GetUserName())
 			if err != nil {
+				glog.Error(err)
+			}
+			// If user is unauthorized, don't send back auth vectors
+			if !authorized {
 				return res, err
 			}
+			res.UserProfile = profile
 		}
 		res.SipAuthVectors = getSIPAuthenticationVectors(maa.SIPAuthDataItems)
 
@@ -99,20 +105,27 @@ func (s *swxProxy) AuthenticateImpl(req *protos.AuthenticationRequest) (*protos.
 	return res, err
 }
 
-// authorize sends SAR over diameter with ServerAssignmentType set to
-// AAA_USER_DATA_REQUEST and ensures the user profile received back allows
-// for Non 3GPP IP Access
-func (s *swxProxy) authorize(userName string) error {
+// retrieveUserProfile sends SARs with ServerAssignmentType AAA_USER_DATA_REQUEST, receives back SAA
+// and returns the subscribers's Non-3GPP-User-Data profile
+func (s *swxProxy) retrieveUserProfile(userName string) (*protos.AuthenticationAnswer_UserProfile, bool, error) {
 	saa, err := s.sendSAR(userName, ServerAssignmentType_AAA_USER_DATA_REQUEST)
 	if err != nil {
-		return err
+		return nil, true, err
 	}
-	if saa.UserData.Non3GPPIPAccess != datatype.Enumerated(Non3GPPIPAccess_ENABLED) {
+	if s.config.VerifyAuthorization && saa.UserData.Non3GPPIPAccess != datatype.Enumerated(Non3GPPIPAccess_ENABLED) {
 		metrics.UnauthorizedAuthAttempts.Inc()
-		return status.Errorf(codes.PermissionDenied, "User %s is not authorized for Non-3GPP Subscription Access", userName)
+		return nil, false, status.Errorf(codes.PermissionDenied, "User %s is not authorized for Non-3GPP Subscription Access", userName)
 	}
-	// User is authorized
-	return nil
+	if saa.UserData.SubscriptionId.SubscriptionIdType != END_USER_E164 {
+		return &protos.AuthenticationAnswer_UserProfile{}, true, status.Error(
+			codes.Internal,
+			"Subscription ID type is not END_USER_E164; Cannot retrieve MSISDN",
+		)
+	}
+	userProfile := &protos.AuthenticationAnswer_UserProfile{
+		Msisdn: string(saa.UserData.SubscriptionId.SubscriptionIdData),
+	}
+	return userProfile, true, nil
 }
 
 // createMAR creates a Multimedia Authentication Request diameter msg with provided SessionID (sid)
