c243d59712ad94b3f9c975a015dbe9a3896ab08b
Alexander Lin <alexanderlin@fb.com>
2019-04-12 12:41:53 -0700

    Update sync rpc http server comments and formatting
    Summary: Small updates to comments and formatting in the Sync RPC HTTP server.
    
    Reviewed By: vikg-fb
    
    Differential Revision: D14570019
    
    fbshipit-source-id: 8e7b257a08c3aaeda87d0bab1c803ef5efcb784c

--- orc8r/cloud/go/services/dispatcher/httpserver/http_server.go
+++ orc8r/cloud/go/services/dispatcher/httpserver/http_server.go
@@ -6,13 +6,17 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
 */
 
-// This is a http/2 h2c server. It's run within the same process as SyncRPC grpc servicer.
-// When a client wants to send a grpc request to some service on gateway with hardwareId hwId,
-// it identifies the addr of the SyncRPC grpc servicer to which the gateway has a bidirectional stream to,
-// and sends a grpc request to the httpServer that's within the same process of that grpc servicer.
-// This httpServer converts httpRequest to GatewayRequest, send it over to grpc servicer using
-// GatewayRPCBroker, waits for a response, and converts the GatewayResponse to a HttpResponse and send it back
-// to the client.
+// Package httpserver is a http/2 h2c server. It's run within the same process
+// as SyncRPC grpc servicer.
+//
+// When a client wants to send a grpc request to some service on
+// gateway with hardwareId hwId, it identifies the addr of the SyncRPC grpc
+// servicer to which the gateway has a bidirectional stream to, and sends a grpc
+// request to the httpServer that's within the same process of that grpc servicer.
+//
+// This httpServer converts httpRequest to GatewayRequest, send it over to grpc
+// servicer using GatewayRPCBroker, waits for a response, and converts the
+// GatewayResponse to a HttpResponse and send it back to the client.
 package httpserver
 
 import (
@@ -36,7 +40,7 @@ import (
 )
 
 const (
-	DEFAULT_HTTP_RESPONSE_STATUS = 200
+	DefaultHttpResponseStatus = 200
 
 	responseTimeoutSecs = 15
 	maxCancelAttempts   = 5
@@ -64,12 +68,12 @@ func (server *SyncRPCHttpServer) rootHandler(responseWriter http.ResponseWriter,
 	respChan, err := server.sendRequest(req)
 	if err != nil {
 		glog.Errorf(err.Msg)
-		// also write to client
+		// Also write to client.
 		http2.WriteErrResponse(responseWriter, err)
 		return
 	}
 
-	// wait for response or timeout
+	// Wait for response or timeout.
 	for {
 		select {
 		case gwResponse := <-respChan:
@@ -82,15 +86,17 @@ func (server *SyncRPCHttpServer) rootHandler(responseWriter http.ResponseWriter,
 				return
 			}
 		case <-time.After(time.Second * responseTimeoutSecs):
-			http2.WriteErrResponse(responseWriter,
-				http2.NewHTTPGrpcError("Request timed out",
-					int(codes.DeadlineExceeded),
-					http.StatusRequestTimeout))
+			http2.WriteErrResponse(
+				responseWriter,
+				http2.NewHTTPGrpcError("Request timed out", int(codes.DeadlineExceeded), http.StatusRequestTimeout),
+			)
 			return
 		}
 	}
 }
 
+// sendRequest sends a SyncRPCRequest to the gateway and creates
+// a goroutine to notify the gateway when the context is done.
 func (server *SyncRPCHttpServer) sendRequest(req *http.Request) (chan *protos.GatewayResponse, *http2.HTTPGrpcError) {
 	gwReq, err := createRequest(req)
 	if err != nil {
@@ -125,15 +131,12 @@ func (server *SyncRPCHttpServer) sendRequest(req *http.Request) (chan *protos.Ga
 	return gwRespChannel.RespChan, nil
 }
 
+// createRequest converts a HTTP request to a GatewayRequest.
 func createRequest(req *http.Request) (*protos.GatewayRequest, *http2.HTTPGrpcError) {
 	headers := req.Header
 	gwIds := headers[gateway_registry.GATEWAYID_HEADER_KEY]
 	if len(gwIds) == 0 || len(gwIds[0]) == 0 {
-		str := fmt.Sprintf("No Gatewayid provided in metaData")
-		return nil, http2.NewHTTPGrpcError(
-			str,
-			int(codes.InvalidArgument),
-			http.StatusBadRequest)
+		return nil, http2.NewHTTPGrpcError("No Gatewayid provided in metaData", int(codes.InvalidArgument), http.StatusBadRequest)
 	}
 	gwId := gwIds[0]
 	delete(headers, gateway_registry.GATEWAYID_HEADER_KEY)
@@ -161,8 +164,7 @@ func createRequest(req *http.Request) (*protos.GatewayRequest, *http2.HTTPGrpcEr
 
 func getAuthority(host string) (string, *http2.HTTPGrpcError) {
 	if len(host) == 0 {
-		errMsg := "No authority provided"
-		return "", http2.NewHTTPGrpcError(errMsg, int(codes.InvalidArgument), http.StatusBadRequest)
+		return "", http2.NewHTTPGrpcError("No authority provided", int(codes.InvalidArgument), http.StatusBadRequest)
 	} else {
 		return host, nil
 	}
@@ -170,8 +172,7 @@ func getAuthority(host string) (string, *http2.HTTPGrpcError) {
 
 func getPath(url *url.URL) (string, *http2.HTTPGrpcError) {
 	if url == nil || len(url.Path) == 0 {
-		errMsg := "No url path provided"
-		return "", http2.NewHTTPGrpcError(errMsg, int(codes.InvalidArgument),
+		return "", http2.NewHTTPGrpcError("No url path provided", int(codes.InvalidArgument),
 			http.StatusBadRequest)
 	}
 	return url.Path, nil
@@ -182,10 +183,7 @@ func getPayload(body io.ReadCloser) ([]byte, *http2.HTTPGrpcError) {
 	defer body.Close()
 	if err != nil {
 		errMsg := fmt.Sprintf("err reading req body: %v", err)
-		return nil, http2.NewHTTPGrpcError(
-			errMsg,
-			int(codes.InvalidArgument),
-			http.StatusBadRequest)
+		return nil, http2.NewHTTPGrpcError(errMsg, int(codes.InvalidArgument), http.StatusBadRequest)
 	}
 	return payload, nil
 }
@@ -202,6 +200,8 @@ func concatenateHeaders(headers []string) string {
 	return strings.Join(headers, ",")
 }
 
+// processResponse converts the GatewayResponse to an HTTP response and sends it
+// back to the client.
 func processResponse(w http.ResponseWriter, gwResp *protos.GatewayResponse) *http2.HTTPGrpcError {
 	if gwResp.KeepConnActive {
 		return nil
@@ -209,13 +209,10 @@ func processResponse(w http.ResponseWriter, gwResp *protos.GatewayResponse) *htt
 	if gwResp == nil {
 		// Remains for backward compatibility, but it shouldn't get forwarded
 		// a nil GatewayResponse in new versions.
-		errMsg := "nil GatewayResponse"
-		return http2.NewHTTPGrpcError(errMsg,
-			int(codes.Internal), http.StatusInternalServerError)
+		return http2.NewHTTPGrpcError("nil GatewayResponse", int(codes.Internal), http.StatusInternalServerError)
 	}
 	if gwResp.Err != "" {
-		return http2.NewHTTPGrpcError(gwResp.Err, int(codes.Internal),
-			http.StatusInternalServerError)
+		return http2.NewHTTPGrpcError(gwResp.Err, int(codes.Internal), http.StatusInternalServerError)
 	}
 	headers := gwResp.GetHeaders()
 	writeHeadersToResponse(headers, w)
@@ -226,7 +223,7 @@ func processResponse(w http.ResponseWriter, gwResp *protos.GatewayResponse) *htt
 	}
 	w.(http.Flusher).Flush()
 	if err != nil {
-		// only log, and do not send to client
+		// Only log, and do not send to client.
 		glog.Errorf("%v\n", err)
 	}
 	return nil
@@ -235,12 +232,11 @@ func processResponse(w http.ResponseWriter, gwResp *protos.GatewayResponse) *htt
 func getHttpStatusFromGatewayResponse(gwRespStatus string) (int, error) {
 	httpStatus, err := strconv.Atoi(gwRespStatus)
 	if err != nil {
-		return DEFAULT_HTTP_RESPONSE_STATUS,
-			fmt.Errorf("cannot parse status of gatewayResponse: %v\n", err)
+		return DefaultHttpResponseStatus, fmt.Errorf("cannot parse status of gatewayResponse: %v\n", err)
 	}
 	// invalid status code, defaults to 200
 	if statusText := http.StatusText(httpStatus); len(statusText) == 0 {
-		return DEFAULT_HTTP_RESPONSE_STATUS, fmt.Errorf("Unrecognized httpStatus: %v\n", httpStatus)
+		return DefaultHttpResponseStatus, fmt.Errorf("Unrecognized httpStatus: %v\n", httpStatus)
 	}
 	return httpStatus, nil
 }
