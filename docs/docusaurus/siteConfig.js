09ae0ee693ff69dc73b057712bfb18170895c184
Vik Goel <vikg@fb.com>
2019-04-15 13:33:39 -0700

    Integrate docusaurus bot
    Summary: Currently the docs and the documentation website can get out of sync if the website is not updated. To prevent this from happening, the docusaurus bot will now automatically update the documentation website when the docs are changed. Docs will be stored on the gh-pages branch instead of inside the docs/web folder since this is required by the bot. A future change will remove the docs/web folder once we have switched to gh-pages.
    
    Reviewed By: rpraveen, sciencemanx
    
    Differential Revision: D14937365
    
    fbshipit-source-id: 1a8950fdcda90d77ce6f26ca394124240603c991

--- docs/docusaurus/siteConfig.js
+++ docs/docusaurus/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Magma', // Title for your website.
   tagline: 'Bring more people online by enabling operators with open, flexible, and extensible network solutions',
   url: 'https://facebookincubator.github.io', // Your website URL
-  baseUrl: '/magma/web/', // Base URL for your project */
+  baseUrl: '/magma/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Magma',
+  projectName: 'magma',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
