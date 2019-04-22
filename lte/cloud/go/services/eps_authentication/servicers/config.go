5498b0b34ae991049417185afa232491c15b115f
Marie Bremner <marwhal@fb.com>
2019-04-22 13:34:47 -0700

    Add servicers for the state service (#151)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/151
    
    This is a new state service that will manage states of services in the gateway, and is being created as part of the new orc8r reorganization. It uses `orc8r/cloud/blobstorage` interface to store states reported from services.
    The state service has 3 functionalities: `GetStates`, `ReportStates`, and `DeleteStates`.
    
    This diff implements a few things: servicer, test_service and main service.
    `servicer*`: Interacts with the storage interface to do the core work of storing/retrieving these states.
    `test_service_init.go`: Service that can be used for testing on top of an in-memory storage implementation.
    `main.go`: Starts the state service.
    
     I moved a function that fetches networkID from a context defined in eps_authentication to identity_helper.go in orc8r/cloud/go/protos. Since ReportStates does not take in a networkID, this function is useful.
    
    Next step: This state will gather states from different states.
    
    Reviewed By: xjtian
    
    Differential Revision: D14676093
    
    fbshipit-source-id: debdc14b99a3093f393716fe98eb41b630ca3e72

--- lte/cloud/go/services/eps_authentication/servicers/config.go
+++ lte/cloud/go/services/eps_authentication/servicers/config.go
@@ -1,13 +1,10 @@
 package servicers
 
 import (
-	"golang.org/x/net/context"
-
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	cellular "magma/lte/cloud/go/services/cellular/config"
 	cellular_protos "magma/lte/cloud/go/services/cellular/protos"
-	"magma/orc8r/cloud/go/protos"
 	"magma/orc8r/cloud/go/services/config"
 )
 
@@ -16,20 +13,6 @@ type EpsAuthConfig struct {
 	LteAuthOp   []byte
 	LteAuthAmf  []byte
 	SubProfiles map[string]*cellular_protos.NetworkEPCConfig_SubscriptionProfile
-}
-
-// getNetworkID returns the network id of the gateway sending a gRPC message
-func getNetworkID(ctx context.Context) (string, error) {
-	gw := protos.GetClientGateway(ctx)
-	if gw == nil {
-		err := status.Errorf(codes.PermissionDenied, "Missing Gateway Identity")
-		return "", err
-	}
-	if !gw.Registered() {
-		err := status.Errorf(codes.PermissionDenied, "Gateway is not registered")
-		return "", err
-	}
-	return gw.GetNetworkId(), nil
 }
 
 // getConfig returns the EpsAuthConfig config for a given networkId.
