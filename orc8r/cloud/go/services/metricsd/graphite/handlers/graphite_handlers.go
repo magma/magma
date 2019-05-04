7772884b9d583e788e1af9a233d42e9e252d2771
Scott Smith <smithscott@fb.com>
2019-05-03 17:03:30 -0700

    Restrict graphite queries with networkID (#235)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/235
    
    * Now that graphite queries can be parsed, we can traverse the tree to find all SeriesLists and restrict them
      * The restriction is done by replacing the seriesList with: `seriesByTag('name=~^metric_name$','networkID=<network>')`
      * More information about the `seriesByTag` function can be found here: https://graphite.readthedocs.io/en/latest/functions.html#graphite.render.functions.seriesByTag
    
      * This will allow both simple querying like was available before as well as the full functionality that graphite provides, including functions and composition of functions.
      * One more diff later will fill out the `graphite_functions.go` file with all of the graphite functions we want to support.
    
    Reviewed By: xjtian
    
    Differential Revision: D15004561
    
    fbshipit-source-id: 2379a533699f635fc8baf6d5bca15b38bba9bdbf

--- orc8r/cloud/go/services/metricsd/graphite/handlers/graphite_handlers.go
+++ orc8r/cloud/go/services/metricsd/graphite/handlers/graphite_handlers.go
@@ -11,13 +11,10 @@ package handlers
 import (
 	"fmt"
 	"net/http"
-	"regexp"
-	"sort"
-	"strings"
 	"time"
 
 	"magma/orc8r/cloud/go/obsidian/handlers"
-	"magma/orc8r/cloud/go/services/metricsd/graphite/exporters"
+	"magma/orc8r/cloud/go/services/metricsd/graphite/security"
 	"magma/orc8r/cloud/go/services/metricsd/graphite/third_party/api"
 	"magma/orc8r/cloud/go/services/metricsd/obsidian/utils"
 
@@ -29,17 +26,6 @@ const (
 
 	QueryURL = handlers.GRAPHITE_ROOT + handlers.URL_SEP + queryPart
 	Protocol = "http"
-
-	// matches graphite metric name. Alphanumeric plus '.' and '_'
-	metricNameRegexString = `[a-zA-Z_\.\+\*\d]+`
-)
-
-var (
-	tagRegexString = fmt.Sprintf(",%s=%s", metricNameRegexString, metricNameRegexString)
-	// regex to match an list of tags
-	tagListRegex = regexp.MustCompile(fmt.Sprintf("(%s)+", tagRegexString))
-	//regex to match a metric name followed by an optional list of tags
-	basicQueryRegex = regexp.MustCompile(fmt.Sprintf("^(%s)(%s)*$", metricNameRegexString, tagRegexString))
 )
 
 func GetQueryHandler(gc *api.Client) func(c echo.Context) error {
@@ -89,67 +75,15 @@ func prepareQuery(c echo.Context) (string, error) {
 	}
 
 	query := c.QueryParam(utils.ParamQuery)
-	if ok := validateQuery(query); !ok {
-		return "", fmt.Errorf("invalid query: %s", query)
+	restrictedQuery, err := security.RestrictQuery(query, networkID)
+	if err != nil {
+		return "", fmt.Errorf("Could not secure query: %v", err)
 	}
-
-	tags := parseTagsFromQuery(query)
-	tags.Insert(exporters.NetworkTagName, networkID)
-
-	metricName := extractMetricNameFromQuery(query)
-
-	request := buildTaggedQuery(metricName, tags)
-	return request, nil
-}
-
-func extractMetricNameFromQuery(query string) string {
-	firstTagIndex := strings.Index(query, ",")
-	if firstTagIndex != -1 {
-		return query[:firstTagIndex]
-	}
-	return query
-}
-
-func buildTaggedQuery(metricName string, tags exporters.TagSet) string {
-	nameSelector := fmt.Sprintf("'name=~^%s$'", metricName)
-	var keys []string
-	for key := range tags {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	var query strings.Builder
-	query.WriteString(nameSelector)
-	for _, key := range keys {
-		query.WriteString(fmt.Sprintf(",'%s=%s'", key, tags[key]))
-	}
-	return fmt.Sprintf("%s(%s)", "seriesByTag", query.String())
+	return restrictedQuery, nil
 }
 
 // GraphiteResultStruct carries the result and status
 type GraphiteResultStruct struct {
 	Status string       `json:"status"`
 	Result []api.Series `json:"result"`
-}
-
-func validateQuery(query string) bool {
-	// matches <metric_name>[,tag1=val1]*
-	return basicQueryRegex.MatchString(query)
-}
-
-func parseTagsFromQuery(query string) exporters.TagSet {
-	tags := make(exporters.TagSet)
-	tagString := tagListRegex.FindString(query)
-	if tagString == "" {
-		return tags
-	}
-
-	tagsList := strings.Split(tagString, ",")[1:]
-	for _, tag := range tagsList {
-		equalsIndex := strings.Index(tag, "=")
-		key := tag[:equalsIndex]
-		val := tag[equalsIndex+1:]
-		tags.Insert(key, val)
-	}
-	return tags
 }
