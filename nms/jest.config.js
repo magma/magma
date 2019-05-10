dc33897135571e3b530ace6c5ff0ada55e317f21
Joshua Braegger <jbraeg@fb.com>
2019-05-10 14:38:48 -0700

    Exclude generated thrift files from coverage results
    Summary:
    This file has a lot of functions that aren't even tested, and isn't helpful
    for our coverage results.  Further, we will be coming up with a better solution soon
    
    Also adding HTML output, because it makes things easy
    
    Reviewed By: aclave1
    
    Differential Revision: D15301624
    
    fbshipit-source-id: 458bb03cd47c6b8448cdc0e39dd70f56c238c9d2

--- nms/jest.config.js
+++ nms/jest.config.js
@@ -15,9 +15,10 @@ module.exports = {
     '**/fbcnms-packages/**/*.js',
     '!**/__mocks__/**',
     '!**/__tests__/**',
+    '!**/thrift/gen-nodejs/**',
     '!**/node_modules/**',
   ],
-  coverageReporters: ['json'],
+  coverageReporters: ['json', 'html'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
