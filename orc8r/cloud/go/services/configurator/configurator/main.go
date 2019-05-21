df3d6792bcbbc82bf4559c75c72c12cd4d6e3f5d
Marie Bremner <marwhal@fb.com>
2019-05-21 12:39:07 -0700

    Create a new magma service : 'configurator'
    Summary: Add a main file and register servicers to make configurator a running service on the cloud.
    
    Reviewed By: xjtian
    
    Differential Revision: D15349419
    
    fbshipit-source-id: 38ba8f1f221748fef73535a38b601c47e49dcb37

---
+++ orc8r/cloud/go/services/configurator/configurator/main.go
@@ -0,0 +1,60 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+/*
+Configurator is a dedicated Magma Cloud service which maintains configurations
+and meta data for the network and network entity structures.
+*/
+
+package main
+
+import (
+	"database/sql"
+
+	"magma/orc8r/cloud/go/datastore"
+	"magma/orc8r/cloud/go/orc8r"
+	"magma/orc8r/cloud/go/service"
+	"magma/orc8r/cloud/go/services/configurator"
+	"magma/orc8r/cloud/go/services/configurator/protos"
+	"magma/orc8r/cloud/go/services/configurator/servicers"
+	"magma/orc8r/cloud/go/services/configurator/storage"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	// Create the service
+	srv, err := service.NewOrchestratorService(orc8r.ModuleName, configurator.ServiceName)
+	if err != nil {
+		glog.Fatalf("Error creating service: %s", err)
+	}
+	db, err := sql.Open(datastore.SQL_DRIVER, datastore.DATABASE_SOURCE)
+	if err != nil {
+		glog.Fatalf("Failed to connect to database: %s", err)
+	}
+
+	factory := storage.NewSQLConfiguratorStorageFactory(db, &storage.DefaultIDGenerator{})
+	err = factory.InitializeServiceStorage()
+
+	nbServicer, err := servicers.NewNorthboundConfiguratorServicer(factory)
+	if err != nil {
+		glog.Fatalf("Failed to instantiate the device servicer: %v", nbServicer)
+	}
+	protos.RegisterNorthboundConfiguratorServer(srv.GrpcServer, nbServicer)
+
+	sbServicer, err := servicers.NewSouthboundConfiguratorServicer(factory)
+	if err != nil {
+		glog.Fatalf("Failed to instantiate the device servicer: %v", sbServicer)
+	}
+	protos.RegisterSouthboundConfiguratorServer(srv.GrpcServer, sbServicer)
+
+	err = srv.Run()
+	if err != nil {
+		glog.Fatalf("Failed to start configurator service: %v", err)
+	}
+}
