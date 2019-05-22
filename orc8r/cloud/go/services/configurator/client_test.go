d8640a74d4224a69e1bf9aac928616c0f600404c
Marie Bremner <marwhal@fb.com>
2019-05-22 08:54:29 -0700

    Implement Northbound Configurator servicers (Load, CreateAndUpdate for Network/NetworkEntity) (#297)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/297
    
    This diff implements the northbound portion of servicer functions defined in D15091643 .
    The major chunk of this diff (configurator_helper.go) is the translation between the protobuf structs used for grpc and the structs used for storage.
    
    As mentioned in https://fb.quip.com/SDsNADhTn7wj, the northbound servicers are meant for user interaction with the service. So an upcoming diff will implement the REST API portion that uses these client_api functions also defined in this diff.
    
    Validations for the values passed in will come soon.
    
    Delete functions will come in the next diff.
    
    Reviewed By: xjtian
    
    Differential Revision: D15286443
    
    fbshipit-source-id: 9d1265dafc0227e678642dc12198f6af617f910b

---
+++ orc8r/cloud/go/services/configurator/client_test.go
@@ -0,0 +1,206 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+package configurator_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"magma/orc8r/cloud/go/serde"
+	"magma/orc8r/cloud/go/services/configurator"
+	"magma/orc8r/cloud/go/services/configurator/protos"
+	"magma/orc8r/cloud/go/services/configurator/test_init"
+
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	networkID1 = "network_id_1"
+)
+
+func TestConfiguratorService(t *testing.T) {
+	test_init.StartTestService(t)
+	err := serde.RegisterSerdes(&FooSerde{})
+	assert.NoError(t, err)
+	err = serde.RegisterSerdes(&BarSerde{})
+	assert.NoError(t, err)
+
+	// Test Basic Network Interface
+	config := map[string][]byte{
+		"foo": []byte("world"),
+	}
+	// Create, Load
+	network1 := &protos.Network{
+		Id:          networkID1,
+		Name:        "test_network",
+		Description: "description",
+		Configs:     config,
+	}
+	_, err = configurator.CreateNetworks([]*protos.Network{network1})
+	assert.NoError(t, err)
+
+	networks, notFound, err := configurator.LoadNetworks([]string{networkID1}, true, true)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(notFound))
+	assert.Equal(t, 1, len(networks))
+
+	// Update, Load
+	newDesc := "Should be updated now"
+	toAddOrUpdate := map[string][]byte{}
+	barConfig := Bar{
+		Number: 42,
+	}
+	serializedBarConfig, err := json.Marshal(barConfig)
+	assert.NoError(t, err)
+	toAddOrUpdate["bar"] = serializedBarConfig
+	toDelete := []string{"foo"}
+	updateCriteria1 := &protos.NetworkUpdateCriteria{
+		Id:                   networkID1,
+		NewDescription:       strToStringValue(newDesc),
+		ConfigsToAddOrUpdate: toAddOrUpdate,
+		ConfigsToDelete:      toDelete,
+	}
+
+	err = configurator.UpdateNetworks([]*protos.NetworkUpdateCriteria{updateCriteria1})
+	networks, notFound, err = configurator.LoadNetworks([]string{networkID1}, true, true)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(notFound))
+	assert.Equal(t, 1, len(networks))
+	assert.Equal(t, newDesc, networks[networkID1].Description)
+	assert.Equal(t, []byte(nil), networks[networkID1].Configs["foo"])
+	assert.Equal(t, serializedBarConfig, networks[networkID1].Configs["bar"])
+
+	// Test Basic Entity Interface
+	entityID1 := &protos.EntityID{Type: "foo", Id: "bar"}
+	entity1 := &protos.NetworkEntity{
+		Type:        "foo",
+		Id:          "bar",
+		Name:        "foobar",
+		Description: "ent: foobar",
+		PhysicalId:  "1234",
+		Config:      []byte("hello"),
+	}
+	entityID2 := &protos.EntityID{Type: "foo", Id: "boo"}
+	entity2 := &protos.NetworkEntity{
+		Type:        "foo",
+		Id:          "boo",
+		Name:        "fooboo",
+		Description: "ent: fooboo",
+		PhysicalId:  "5678",
+		Config:      []byte("bye"),
+	}
+
+	// Create, Load
+	_, err = configurator.CreateEntities(networkID1, []*protos.NetworkEntity{entity1, entity2})
+	assert.NoError(t, err)
+
+	entities, entitiesNotFound, err := configurator.LoadEntities(
+		networkID1,
+		strPointer("foo"),
+		nil,
+		nil,
+		&protos.EntityLoadCriteria{
+			LoadMetadata: true,
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(entities))
+	assert.Equal(t, 0, len(entitiesNotFound))
+	assert.Equal(t, "foobar", entities[0].Name)
+	assert.Equal(t, "fooboo", entities[1].Name)
+
+	entityUpdateCriteria := &protos.EntityUpdateCriteria{
+		Type:              entityID1.Type,
+		Key:               entityID1.Id,
+		NewPhysicalID:     strToStringValue("4321"),
+		AssociationsToAdd: []*protos.EntityID{entityID2},
+	}
+
+	// Update, Load
+	_, err = configurator.UpdateEntities(networkID1, []*protos.EntityUpdateCriteria{entityUpdateCriteria})
+	assert.NoError(t, err)
+	entities, entitiesNotFound, err = configurator.LoadEntities(
+		networkID1,
+		strPointer("foo"),
+		nil,
+		nil,
+		&protos.EntityLoadCriteria{
+			LoadMetadata:   true,
+			LoadAssocsTo:   true,
+			LoadAssocsFrom: true,
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(entities))
+	assert.Equal(t, 0, len(entitiesNotFound))
+	assert.Equal(t, "foobar", entities[0].Name)
+	assert.Equal(t, "fooboo", entities[1].Name)
+	assert.Equal(t, "4321", entities[0].PhysicalId)
+	assert.Equal(t, 1, len(entities[0].Assocs))
+	assert.Equal(t, entityID2.Id, entities[0].Assocs[0].Id)
+}
+
+func strToStringValue(str string) *wrappers.StringValue {
+	return &wrappers.StringValue{Value: str}
+}
+
+func strPointer(str string) *string {
+	return &str
+}
+
+// Test Serdes
+type FooSerde struct {
+}
+
+func (*FooSerde) GetDomain() string {
+	return configurator.SerdeDomain
+}
+
+func (*FooSerde) GetType() string {
+	return "foo"
+}
+
+func (*FooSerde) Serialize(in interface{}) ([]byte, error) {
+	str, ok := in.(string)
+	if !ok {
+		return nil, fmt.Errorf("%v is not serializable by Foo", in)
+	}
+	return []byte(str), nil
+}
+
+func (*FooSerde) Deserialize(message []byte) (interface{}, error) {
+	return string(message), nil
+}
+
+type Bar struct {
+	// number
+	Number int `json:"age"`
+}
+
+type BarSerde struct {
+}
+
+func (*BarSerde) GetDomain() string {
+	return configurator.SerdeDomain
+}
+
+func (*BarSerde) GetType() string {
+	return "bar"
+}
+
+func (*BarSerde) Serialize(in interface{}) ([]byte, error) {
+	return json.Marshal(in)
+}
+
+func (*BarSerde) Deserialize(message []byte) (interface{}, error) {
+	res := Bar{}
+	err := json.Unmarshal(message, &res)
+	return res, err
+}
---
+++ orc8r/cloud/go/services/configurator/test_init/test_service_init.go
@@ -0,0 +1,40 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+package test_init
+
+import (
+	"testing"
+
+	"magma/orc8r/cloud/go/orc8r"
+	"magma/orc8r/cloud/go/services/configurator"
+	"magma/orc8r/cloud/go/services/configurator/protos"
+	"magma/orc8r/cloud/go/services/configurator/servicers"
+	"magma/orc8r/cloud/go/services/configurator/storage"
+	"magma/orc8r/cloud/go/sql_utils"
+	"magma/orc8r/cloud/go/test_utils"
+)
+
+func StartTestService(t *testing.T) {
+	db, err := sql_utils.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatalf("Could not initialize sqlite DB: %s", err)
+	}
+	idGenerator := storage.DefaultIDGenerator{}
+	storageFactory := storage.NewSQLConfiguratorStorageFactory(db, &idGenerator)
+	storageFactory.InitializeServiceStorage()
+
+	srv, lis := test_utils.NewTestService(t, orc8r.ModuleName, configurator.ServiceName)
+	nbServiser, err := servicers.NewNorthboundConfiguratorServicer(storageFactory)
+	if err != nil {
+		t.Fatalf("Failed to create checkin servisers: %s", err)
+	}
+	protos.RegisterNorthboundConfiguratorServer(srv.GrpcServer, nbServiser)
+
+	go srv.RunTest(lis)
+}
