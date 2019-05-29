17ce05f5f913dbc2d4a955e82544a62fa291d910
Michael Germano <mgermano@fb.com>
2019-05-29 09:52:50 -0700

    Implement bare CWF Orchestrator plugin
    Summary:
    This diff serves to add an orc8r module to magma
    for cwf (Carrier WiFi). This module currently does not
    have any explicit functionality, but this diff will
    serve to allow that functionality to be added.
    
    Reviewed By: rpraveen
    
    Differential Revision: D15487720
    
    fbshipit-source-id: f942ea45b00ea0b5e31599ac77d7dac264add5de

---
+++ cwf/cloud/go/go.mod
@@ -0,0 +1,11 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+//
+module magma/cwf/cloud/go
+
+replace magma/orc8r/cloud/go => ../../../orc8r/cloud/go
+
+require magma/orc8r/cloud/go v0.0.0
---
+++ cwf/cloud/go/plugin/plugin.go
@@ -0,0 +1,57 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+package plugin
+
+import (
+	"magma/cwf/cloud/go/cwf"
+	"magma/orc8r/cloud/go/obsidian/handlers"
+	"magma/orc8r/cloud/go/plugin"
+	"magma/orc8r/cloud/go/registry"
+	"magma/orc8r/cloud/go/serde"
+	srvconfig "magma/orc8r/cloud/go/service/config"
+	"magma/orc8r/cloud/go/service/serviceregistry"
+	"magma/orc8r/cloud/go/services/metricsd"
+	"magma/orc8r/cloud/go/services/streamer/mconfig/factory"
+	"magma/orc8r/cloud/go/services/streamer/providers"
+)
+
+// CwfOrchestratorPlugin implements OrchestratorPlugin for the CWF module
+type CwfOrchestratorPlugin struct{}
+
+func (*CwfOrchestratorPlugin) GetName() string {
+	return cwf.ModuleName
+}
+
+func (*CwfOrchestratorPlugin) GetServices() []registry.ServiceLocation {
+	serviceLocations, err := serviceregistry.LoadServiceRegistryConfig(cwf.ModuleName)
+	if err != nil {
+		return []registry.ServiceLocation{}
+	}
+	return serviceLocations
+}
+
+func (*CwfOrchestratorPlugin) GetSerdes() []serde.Serde {
+	return []serde.Serde{}
+}
+
+func (*CwfOrchestratorPlugin) GetMconfigBuilders() []factory.MconfigBuilder {
+	return []factory.MconfigBuilder{}
+}
+
+func (*CwfOrchestratorPlugin) GetMetricsProfiles(metricsConfig *srvconfig.ConfigMap) []metricsd.MetricsProfile {
+	return []metricsd.MetricsProfile{}
+}
+
+func (*CwfOrchestratorPlugin) GetObsidianHandlers(metricsConfig *srvconfig.ConfigMap) []handlers.Handler {
+	return plugin.FlattenHandlerLists()
+}
+
+func (*CwfOrchestratorPlugin) GetStreamerProviders() []providers.StreamProvider {
+	return []providers.StreamProvider{}
+}
