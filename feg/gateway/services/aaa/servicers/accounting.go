55f2a4b37aef0e4d6005a0ed81cae97f7bda6de3
Evgeniy Makeev <evgeniym@fb.com>
2019-06-07 16:18:35 -0700

    Magma AAA joint Session Table implementation
    Summary: Magma AAA joint Session Table implementation
    
    Reviewed By: anooptomar29
    
    Differential Revision: D15706174
    
    fbshipit-source-id: 3e01a90bd36c05125e5559140b1ef69e71a43fb3

--- feg/gateway/services/aaa/servicers/accounting.go
+++ feg/gateway/services/aaa/servicers/accounting.go
@@ -14,28 +14,57 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"magma/feg/gateway/services/aaa"
 	"magma/feg/gateway/services/aaa/protos"
 )
 
 type accountingService struct {
+	sessions aaa.SessionTable
 }
 
 // NewEapAuthenticator returns a new instance of EAP Auth service
-func NewAccountingService() (protos.AccountingServer, error) {
-	return &accountingService{}, nil
+func NewAccountingService(sessions aaa.SessionTable) (protos.AccountingServer, error) {
+	return &accountingService{sessions: sessions}, nil
 }
 
 // Start implements Radius Acct-Status-Type: Start endpoint
-func (s *accountingService) Start(_ context.Context, aaaCtx *protos.Context) (*protos.AcctResp, error) {
+func (srv *accountingService) Start(_ context.Context, aaaCtx *protos.Context) (*protos.AcctResp, error) {
+	if aaaCtx == nil {
+		return &protos.AcctResp{}, status.Errorf(codes.InvalidArgument, "Nil AAA Context")
+	}
+	sid := aaaCtx.GetSessionId()
+	s := srv.sessions.GetSession(sid)
+	if s == nil {
+		return &protos.AcctResp{}, status.Errorf(
+			codes.FailedPrecondition, "Accounting Start: Session %s was not authenticated", sid)
+	}
 	return &protos.AcctResp{}, status.Errorf(codes.Unimplemented, "Not Implemented")
 }
 
 // InterimUpdate implements Radius Acct-Status-Type: Interim-Update endpoint
-func (s *accountingService) InterimUpdate(_ context.Context, ur *protos.UpdateRequest) (*protos.AcctResp, error) {
+func (srv *accountingService) InterimUpdate(_ context.Context, ur *protos.UpdateRequest) (*protos.AcctResp, error) {
+	if ur == nil {
+		return &protos.AcctResp{}, status.Errorf(codes.InvalidArgument, "Nil AAA Context")
+	}
+	sid := ur.Ctx.GetSessionId()
+	s := srv.sessions.GetSession(sid)
+	if s == nil {
+		return &protos.AcctResp{}, status.Errorf(
+			codes.FailedPrecondition, "Accounting Update: Session %s was not authenticated", sid)
+	}
 	return &protos.AcctResp{}, status.Errorf(codes.Unimplemented, "Not Implemented")
 }
 
 // Stop implements Radius Acct-Status-Type: Stop endpoint
-func (s *accountingService) Stop(_ context.Context, _ *protos.Context) (*protos.AcctResp, error) {
-	return &protos.AcctResp{}, status.Errorf(codes.Unimplemented, "Implemented")
+func (srv *accountingService) Stop(_ context.Context, aaaCtx *protos.Context) (*protos.AcctResp, error) {
+	if aaaCtx == nil {
+		return &protos.AcctResp{}, status.Errorf(codes.InvalidArgument, "Nil AAA Context")
+	}
+	sid := aaaCtx.GetSessionId()
+	s := srv.sessions.RemoveSession(sid)
+	if s == nil {
+		return &protos.AcctResp{}, status.Errorf(
+			codes.FailedPrecondition, "Accounting Stop: Session %s cannot be found", sid)
+	}
+	return &protos.AcctResp{}, status.Errorf(codes.Unimplemented, "Not Implemented")
 }
