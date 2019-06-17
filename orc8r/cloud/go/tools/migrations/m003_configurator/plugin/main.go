b4b2f259773695c10aaac4d71a2724e726d40b2d
Jacky Tian <xjtian@fb.com>
2019-06-17 13:32:59 -0700

    Migrate network DNS configs
    Summary: see title
    
    Reviewed By: themarwhal
    
    Differential Revision: D15769532
    
    fbshipit-source-id: 659dea2bfb3281f8593a28c6f164f5675bf45d61

--- orc8r/cloud/go/tools/migrations/m003_configurator/plugin/main.go
+++ orc8r/cloud/go/tools/migrations/m003_configurator/plugin/main.go
@@ -8,16 +8,41 @@
 
 package main
 
-import "magma/orc8r/cloud/go/tools/migrations/m003_configurator/migration"
+import (
+	"magma/orc8r/cloud/go/tools/migrations/m003_configurator/migration"
+	"magma/orc8r/cloud/go/tools/migrations/m003_configurator/plugin/types"
+)
 
 func main() {}
 
 type plugin struct{}
 
 func (*plugin) GetConfigMigrators() []migration.ConfigMigrator {
-	return []migration.ConfigMigrator{}
+	return []migration.ConfigMigrator{
+		&networkDnsMigrator{},
+	}
 }
 
 func GetPlugin() migration.ConfiguratorMigrationPlugin {
 	return &plugin{}
+}
+
+// migrators
+
+type networkDnsMigrator struct{}
+
+func (*networkDnsMigrator) GetType() string {
+	return "dnsd_network"
+}
+
+func (*networkDnsMigrator) ToNewConfig(oldConfig []byte) ([]byte, error) {
+	oldMsg := &types.NetworkDNSConfig{}
+	err := migration.Unmarshal(oldConfig, oldMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	newModel := &types.NewNetworkDNSConfig{}
+	migration.FillIn(oldMsg, newModel)
+	return newModel.MarshalBinary()
 }
