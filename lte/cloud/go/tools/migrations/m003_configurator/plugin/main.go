82cde80e287e4d1c66a46a3b7e5554d21db33f8b
Jacky Tian <xjtian@fb.com>
2019-06-17 13:32:59 -0700

    Implement migration for cellular network configs
    Summary:
    - Duplicate a whole bunch of generated code over to the migration plugin for the lte module. Will delete the proto file when I'm sure the types are stable
    - Code is duplicated because the migration has to execute the exact same plan regardless of the release that it's run against
    - Implement a config migrator just for cellular network configs
    
    Reviewed By: themarwhal
    
    Differential Revision: D15732141
    
    fbshipit-source-id: 6828f148f7c9dd2ad2b4a3c896d6491b0d37058f

---
+++ lte/cloud/go/tools/migrations/m003_configurator/plugin/main.go
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+//go:generate bash -c "protoc -I . -I /usr/include --proto_path=$MAGMA_ROOT --go_out=plugins=grpc:. *.proto"
+package main
+
+import (
+	"fmt"
+
+	"magma/lte/cloud/go/tools/migrations/m003_configurator/plugin/types"
+	"magma/orc8r/cloud/go/tools/migrations/m003_configurator/migration"
+)
+
+func main() {}
+
+type plugin struct{}
+
+func (*plugin) GetConfigMigrators() []migration.ConfigMigrator {
+	return []migration.ConfigMigrator{
+		&cellularNetworkMigrator{},
+	}
+}
+
+func GetPlugin() migration.ConfiguratorMigrationPlugin {
+	return &plugin{}
+}
+
+// migrators
+
+type cellularNetworkMigrator struct{}
+
+func (*cellularNetworkMigrator) GetType() string {
+	return "cellular_network"
+}
+
+func (*cellularNetworkMigrator) ToNewConfig(oldConfig []byte) ([]byte, error) {
+	oldMsg := &types.CellularNetworkConfig{}
+	err := migration.Unmarshal(oldConfig, oldMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	newModel := &types.NetworkCellularConfigs{}
+	migration.FillIn(oldMsg, newModel)
+	newModel.FegNetworkID = oldMsg.FegNetworkId
+	newModel.Epc.RelayEnabled = oldMsg.Epc.RelayEnabled
+
+	if oldMsg.Epc.NetworkServices != nil {
+		for _, serviceEnum := range oldMsg.Epc.NetworkServices {
+			serviceName, ok := networkServiceEnumToNameMap[serviceEnum]
+			if !ok {
+				return nil, fmt.Errorf("Unknown network service enum: %s", serviceEnum)
+			}
+			newModel.Epc.NetworkServices = append(newModel.Epc.NetworkServices, serviceName)
+		}
+	}
+
+	return newModel.MarshalBinary()
+}
+
+var networkServiceEnumToNameMap = map[types.NetworkEPCConfig_NetworkServices]string{
+	types.NetworkEPCConfig_METERING:    "metering",
+	types.NetworkEPCConfig_DPI:         "dpi",
+	types.NetworkEPCConfig_ENFORCEMENT: "policy_enforcement",
+}
