de57843dc752db4573977f831e773772f46e5116
Sabrina Jiang <sabj@fb.com>
2019-06-18 09:51:35 -0700

    Add implementation of UE Simulator Service (#347)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/347
    
    Adds implementation of UE simulator service interface, which includes a `AddUE` method call. This allows configuration of the simulator.
    
    The implementation uses the in-memory UE storage from the previous diff.
    
    Reviewed By: mpgermano
    
    Differential Revision: D15717876
    
    fbshipit-source-id: 74bfb4c791be40413db75a5fc877d6058a461414

---
+++ cwf/gateway/services/uesim/main.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// This starts the user equipment (ue) service.
+package main
+
+import (
+	"magma/cwf/cloud/go/protos"
+	"magma/cwf/gateway/registry"
+	"magma/cwf/gateway/services/uesim/servicers"
+	"magma/orc8r/cloud/go/blobstore"
+	"magma/orc8r/cloud/go/service"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	srv, err := service.NewServiceWithOptions(registry.ModuleName, registry.UESIM)
+	if err != nil {
+		glog.Fatalf("Error creating UESIM service: %s", err)
+	}
+
+	store := blobstore.NewMemoryBlobStorageFactory()
+	servicer, err := servicers.NewUESimServer(store)
+	if err != nil {
+		glog.Fatalf("Error creating UE server: %s", err)
+	}
+	protos.RegisterUESimServer(srv.GrpcServer, servicer)
+
+	// Run the service
+	err = srv.Run()
+	if err != nil {
+		glog.Fatalf("Error running UE service: %s", err)
+	}
+}
