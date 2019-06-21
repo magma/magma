bfee2a0520298695f0f2d4622b8191cb7202fda6
Michael Germano <mgermano@fb.com>
2019-06-21 10:40:06 -0700

    Extend install_gateway.sh to support CWAG installation (#362)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/362
    
    This diff updates the installation and upgrade files at
    `magma/orc8r/tools/docker` to allow for installation of either
    a feg or cwag.
    
    Reviewed By: xjtian
    
    Differential Revision: D15905915
    
    fbshipit-source-id: efa79af1fad5453b2364c89007aba39ee0c19e9a

--- cwf/gateway/deploy/roles/cwag/files/create_gre_tunnel.sh
+++ cwf/gateway/deploy/roles/cwag/files/create_gre_tunnel.sh
@@ -7,8 +7,8 @@
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
 
-ovs-vsctl add-br br0
+ovs-vsctl --may-exist add-br br0
 sysctl net.ipv4.ip_forward=1
 iptables -t mangle -A FORWARD -i br0 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
 iptables -t mangle -A FORWARD -o br0 -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
-ovs-vsctl add-port br0 gre0 -- set interface gre0 type=gre ofport_request=32768 options:remote_ip=flow options:key=flow
+ovs-vsctl --may-exist add-port br0 gre0 -- set interface gre0 type=gre ofport_request=32768 options:remote_ip=flow options:key=flow
