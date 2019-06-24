7c4874ea4d1fe78e92ef09976a1cca487b2a4055
Joshua Braegger <jbraeg@fb.com>
2019-06-24 09:53:09 -0700

    Re-enable (+fix) tests that weren't running
    Summary:
    There were a few tests that got disabled when we moved to app/server code.
    This just fixes it.
    
    Reviewed By: tcirstea
    
    Differential Revision: D15951489
    
    fbshipit-source-id: cd57c2950e86e73f0b528c03c6cfcdc4b723b66a

--- nms/jest.config.js
+++ nms/jest.config.js
@@ -29,9 +29,11 @@ module.exports = {
       testEnvironment: 'node',
       testMatch: [
         '<rootDir>/fbcnms-projects/**/server/**/__tests__/*.js',
+        '<rootDir>/fbcnms-packages/fbcnms-auth/**/__tests__/*.js',
         '<rootDir>/fbcnms-packages/fbcnms-express-middleware/**/__tests__/*.js',
         '<rootDir>/fbcnms-packages/fbcnms-i18n/**/__tests__/*.js',
         // run app/server shared tests in both node and jsdom environments
+        '<rootDir>/fbcnms-packages/fbcnms-util/**/__tests__/*.js',
         '<rootDir>/fbcnms-projects/**/shared/**/__tests__/*.js',
       ],
       transform: {
@@ -43,7 +45,9 @@ module.exports = {
       testEnvironment: 'jsdom',
       testMatch: [
         '<rootDir>/fbcnms-projects/**/app/**/__tests__/*.js',
+        '<rootDir>/fbcnms-packages/fbcnms-ui/**/__tests__/*.js',
         // run app/server shared tests in both node and jsdom environments
+        '<rootDir>/fbcnms-packages/fbcnms-util/**/__tests__/*.js',
         '<rootDir>/fbcnms-projects/**/shared/**/__tests__/*.js',
       ],
       transform: {
