c8320458814ce8531eeec8013bf6ea0e9501d1dd
Naor Matania <naorm1991@fb.com>
2019-07-07 07:32:06 -0700

    add default user on fb-test when running in dev mode
    Summary:
    In pyinventory integration tests we would need user to connect to inventory with.
    Instead of creating it during the test let's create default one that will only be available in development env.
    This will also improve developer velocity and would save the need to run `setAdminPassword`
    
    Reviewed By: alexsn
    
    Differential Revision: D16145304
    
    fbshipit-source-id: e0bdd5fcef60789abd8160c10fa33d181a745208

---
+++ nms/fbcnms-packages/fbcnms-sequelize-models/migrations/20190707000000-populate-default-user.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {DataTypes, QueryInterface} from 'sequelize';
+import nullthrows from '@fbcnms/util/nullthrows';
+import bcrypt from 'bcryptjs';
+
+module.exports = {
+  up: async (queryInterface: QueryInterface, _Sequelize: DataTypes) => {
+    if (!process.env.FB_TEST_USER) {
+      return Promise.resolve(null);
+    }
+
+    const testuser = nullthrows(process.env.FB_TEST_USER);
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(testuser, salt);
+
+    return queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email: testuser,
+          organization: 'fb-test',
+          password: passwordHash,
+          role: 3,
+          createdAt: '2019-07-07 15:19:42',
+          updatedAt: '2019-07-07 15:19:42',
+          networkIDs: '["mpk_test"]',
+        },
+      ],
+      {},
+    );
+  },
+
+  down: (queryInterface: QueryInterface, _Sequelize: DataTypes) => {
+    if (!process.env.FB_TEST_USER) {
+      return Promise.resolve(null);
+    }
+    return queryInterface.bulkDelete('Users', null, {});
+  },
+};
