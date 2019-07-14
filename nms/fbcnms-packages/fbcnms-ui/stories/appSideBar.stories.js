472836908f9263f5f8a51eb26b63ef43c0e5f261
Dolev Hadar <dolevh@fb.com>
2019-07-14 06:40:07 -0700

    Expand/collapse locations tree
    Summary:
    Ability to expand / collapse the locations tree such that the properties card has more space (asked by cellcom)
    
    Behavior:
    * Only shown on the inventory tab
    * Only shown when hovering over either the locations tree or the side bar
    
    Reviewed By: naor9991
    
    Differential Revision: D16204790
    
    fbshipit-source-id: 8f00b38c23129d8a4d930c07469d0a40dac88c93

--- nms/fbcnms-packages/fbcnms-ui/stories/appSideBar.stories.js
+++ nms/fbcnms-packages/fbcnms-ui/stories/appSideBar.stories.js
@@ -12,8 +12,40 @@ import {storiesOf} from '@storybook/react';
 import AppSideBar from '../components/layout/AppSideBar.react';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import ListIcon from '@material-ui/icons/List';
-import React from 'react';
+import React, {useState} from 'react';
 import SettingsIcon from '@material-ui/icons/Settings';
+
+const ExpandablePanel = () => {
+  const [isExpanded, setIsExpanded] = useState();
+  const [showExpandButton, setShowExpandButton] = useState(false);
+  return (
+    <div style={{display: 'flex'}}>
+      <AppSideBar
+        user={{email: 'user@fb.com', isSuperUser: false}}
+        projects={[]}
+        mainItems={[
+          <AssignmentIcon color="primary" style={{margin: '8px'}} />,
+          <ListIcon color="primary" style={{margin: '8px'}} />,
+        ]}
+        secondaryItems={[]}
+        useExpandButton={true}
+        showExpandButton={showExpandButton}
+        onExpandClicked={() => setIsExpanded(!isExpanded)}
+        expanded={isExpanded}
+      />
+      <div
+        onMouseEnter={() => setShowExpandButton(true)}
+        onMouseLeave={() => setShowExpandButton(false)}
+        style={{
+          height: '100vh',
+          width: '300px',
+          backgroundColor: 'blue',
+          visibility: isExpanded ? 'visible' : 'hidden',
+        }}
+      />
+    </div>
+  );
+};
 
 storiesOf('AppSideBar', module)
   .add('default', () => (
@@ -76,4 +108,5 @@ storiesOf('AppSideBar', module)
       ]}
       secondaryItems={[]}
     />
-  ));
+  ))
+  .add('Expand Button', () => <ExpandablePanel />);
