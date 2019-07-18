cddffc42033f0ab553ca04e7aa42c7f94dbdcfda
Sabrina Jiang <sabj@fb.com>
2019-07-18 13:25:11 -0700

    Add UESim Client
    Summary: Adds a thin client for using the UE Simulator service so apps can interact with the service, without knowing about the RPC implementation. The TestRunner will use this client.
    
    Reviewed By: mpgermano
    
    Differential Revision: D16172439
    
    fbshipit-source-id: 15514b9b580c3a50168104e350356d153497f4d6

---
+++ cwf/gateway/services/uesim/client_api_test.go
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package uesim_test
+
+import (
+	"testing"
+
+	cwfprotos "magma/cwf/cloud/go/protos"
+	"magma/cwf/gateway/services/uesim"
+	"magma/lte/cloud/go/crypto"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// todo use a config
+const (
+	Op = "\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11"
+)
+
+func TestUESimClient(t *testing.T) {
+	imsi := "001010000000001"
+	key := make([]byte, 16)
+	opc, err := crypto.GenerateOpc(key, []byte(Op))
+	assert.NoError(t, err)
+	seq := uint64(0)
+
+	ue := &cwfprotos.UEConfig{Imsi: imsi, AuthKey: key, AuthOpc: opc[:], Seq: seq}
+	err = uesim.AddUE(ue)
+	assert.NoError(t, err)
+}
