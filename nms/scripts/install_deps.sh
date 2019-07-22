d320b2c59a2217dbc95bb05da7c49d5762f53808
Tudor Cirstea <tcirstea@fb.com>
2019-07-22 14:43:58 -0700

    Break apart FBC tests into tasks
    Summary:
    Break apart one giant script/task into multiple independent tasks.
    Allows seeing output of each step independently in sandcastle.
    
    {F170164531}
    
    Reviewed By: rckclmbr
    
    Differential Revision: D16389860
    
    fbshipit-source-id: f9ca1a128be582a04f0972f1992eee8f79f04bf8

--- nms/scripts/install_deps.sh
+++ nms/scripts/install_deps.sh
@@ -14,4 +14,10 @@ source "$ROOT_DIR/xplat/js/env-utils/setup_env_vars.sh"
 
 pushd "$ROOT_DIR/xplat/fbc" >/dev/null
   "$INSTALL_NODE_MODULES"
+
+  # manually install GRPC binaries
+  # TODO remove this when node-pre-gyp@0.13.1 is released (see D15591571)
+  mkdir -p node_modules/grpc/src/node/extension_binary/
+  tar -xzf "$ROOT_DIR/xplat/third-party/grpc/v1.20.3/node-v57-linux-x64-glibc.tar.gz" -C node_modules/grpc/src/node/extension_binary/
+  tar -xzf "$ROOT_DIR/xplat/third-party/grpc/v1.20.3/node-v64-linux-x64-glibc.tar.gz" -C node_modules/grpc/src/node/extension_binary/
 popd >/dev/null
--- nms/scripts/test_relay_build.sh
+++ nms/scripts/test_relay_build.sh
@@ -12,7 +12,11 @@ ROOT_DIR=$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && hg root)
 # shellcheck source=xplat/js/env-utils/setup_env_vars.sh
 source "$ROOT_DIR/xplat/js/env-utils/setup_env_vars.sh"
 
-pushd "$ROOT_DIR/xplat/fbc" >/dev/null
-  # Run tests
-  "$YARN_BINARY" run test "$@"
+pushd "$ROOT_DIR/xplat/fbc/fbcnms-projects/inventory" >/dev/null
+  "$YARN_BINARY" relay "$@"
+  repo_status=$(hg status | wc -l)
+  if [ "$repo_status" -ne 0 ]; then
+    echo "'yarn relay' modified changes. Please run 'yarn relay' from xplat/fbc/fbcnms-projects/inventory" >&2
+    exit 1
+  fi
 popd >/dev/null
