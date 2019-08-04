26799ff51dc0b80689e4827f1f9b3790dc6deef6
Sheer Amran <sheeramran@fb.com>
2019-08-04 09:14:03 -0700

    Add a satellite view to the map
    Summary: Add a street and satellite icon button to the map view,  enabling the user to switch between satellite/street view on the Inventory Map page.
    
    Reviewed By: noamsch
    
    Differential Revision: D16603358
    
    fbshipit-source-id: f623ec74964e3a4015992da8cc2dab0a5640b47a

---
+++ nms/fbcnms-packages/fbcnms-ui/components/map/MapToggleContainer.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import React from 'react';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  toggleContainer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+    background: theme.palette.background.default,
+    padding: 0,
+    border: 0,
+    margin: '10px',
+    position: 'absolute',
+    bottom: '0',
+    left: 0,
+    borderStyle: 'solid',
+    borderRadius: '4px',
+  },
+}));
+
+type Props = {children: any};
+
+const MapToggleContainer = (props: Props) => {
+  const classes = useStyles();
+  return <div className={classes.toggleContainer}>{props.children}</div>;
+};
+
+export default MapToggleContainer;
