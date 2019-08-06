7b39872ac3534814243cd49eae03426af676e872
Andrei Lee <andreilee@fb.com>
2019-08-06 15:42:47 -0700

    Add radiusd service, for getting metrics from radius server
    Differential Revision: D16286846
    
    fbshipit-source-id: e787a746f1180195cec81d838e51d0c4c8606811

---
+++ feg/gateway/services/radiusd/radiusd/main.go
@@ -0,0 +1,30 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+package main
+
+import (
+	"magma/feg/gateway/registry"
+	"magma/orc8r/cloud/go/service"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	// Create the service
+	srv, err := service.NewServiceWithOptions(registry.ModuleName, registry.RADIUSD)
+	if err != nil {
+		glog.Fatalf("Error creating RADIUSD service: %s", err)
+	}
+
+	// Run the service
+	err = srv.Run()
+	if err != nil {
+		glog.Fatalf("Error running service: %s", err)
+	}
+}
