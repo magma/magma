809165c96d5924ada098cb2b0403ccb9ff174ca2
Andrei Lee <andreilee@fb.com>
2019-08-06 15:45:36 -0700

    Add radiusd service, for getting metrics from radius server
    Summary:
    The radius server exports metrics in Prometheus format, and the 'radiusd' service will be a connector from that format to the way that orc8r handles service metrics.
    
    This revision implements a bare bones service with no functionality.
    
    Reviewed By: themarwhal
    
    Differential Revision: D16286846
    
    fbshipit-source-id: 9055c38a83266d48d60efbd84c6548c50b775886

---
+++ feg/gateway/services/radiusd/doc.go
@@ -0,0 +1,14 @@
+/*
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+// Package device contains the device service.
+// The device service is a simple blob-storage service for tracking
+// physical device.
+package radiusd
+
+const ServiceName = "RADIUSD"
