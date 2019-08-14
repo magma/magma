19619d2e1e60a6b313be71df2f05094a6a615758
Marie Bremner <marwhal@fb.com>
2019-08-14 15:22:59 -0700

    Change identity so that it reads from identity by default
    Summary:
    Removed references to magmad from the identity decorator. Now it will read from configurator by default.
    I've also modified the test to use the state servicer for decoration testing, since checkind will be removed soon.
    
    Modified some tests to also use the configurator to register network/gateway so that they still pass after the identity decorator change.
    
    Reviewed By: xjtian
    
    Differential Revision: D16813003
    
    fbshipit-source-id: f12b634505ebd03af48ec066bc08b0b5378300fb

--- orc8r/cloud/go/services/state/obsidian/handlers/stateh_test.go
+++ orc8r/cloud/go/services/state/obsidian/handlers/stateh_test.go
@@ -15,15 +15,13 @@ import (
 	"magma/orc8r/cloud/go/obsidian/tests"
 	"magma/orc8r/cloud/go/plugin"
 	"magma/orc8r/cloud/go/pluginimpl"
-	"magma/orc8r/cloud/go/protos"
+	"magma/orc8r/cloud/go/pluginimpl/models"
 	checkindTestUtils "magma/orc8r/cloud/go/services/checkind/test_utils"
-	"magma/orc8r/cloud/go/services/magmad"
-	magmadProtos "magma/orc8r/cloud/go/services/magmad/protos"
-	magmadTestInit "magma/orc8r/cloud/go/services/magmad/test_init"
+	configuratorTestInit "magma/orc8r/cloud/go/services/configurator/test_init"
+	configuratorTestUtils "magma/orc8r/cloud/go/services/configurator/test_utils"
+	deviceTestInit "magma/orc8r/cloud/go/services/device/test_init"
 	stateTestInit "magma/orc8r/cloud/go/services/state/test_init"
 	"magma/orc8r/cloud/go/services/state/test_utils"
-
-	"github.com/stretchr/testify/assert"
 )
 
 const testAgHwId = "Test-AGW-Hw-Id"
@@ -32,21 +30,14 @@ const testAgHwId = "Test-AGW-Hw-Id"
 // on cloud VM
 func TestState(t *testing.T) {
 	plugin.RegisterPluginForTests(t, &pluginimpl.BaseOrchestratorPlugin{})
-	magmadTestInit.StartTestService(t)
+	configuratorTestInit.StartTestService(t)
+	deviceTestInit.StartTestService(t)
 	stateTestInit.StartTestService(t)
 	restPort := tests.StartObsidian(t)
 
-	// create a test network with a single GW
-	testNetworkId, err := magmad.RegisterNetwork(
-		&magmadProtos.MagmadNetworkRecord{Name: "Test Network 1"},
-		"state_obsidian_test_network")
-	assert.NoError(t, err)
-	hwId := protos.AccessGatewayID{Id: testAgHwId}
-	_, err = magmad.RegisterGateway(
-		testNetworkId,
-		&magmadProtos.AccessGatewayRecord{HwId: &hwId, Name: "Test GW Name"},
-	)
-	assert.NoError(t, err)
+	testNetworkID := "state_obsidian_test_network"
+	configuratorTestUtils.RegisterNetwork(t, testNetworkID, "Test Network 1")
+	configuratorTestUtils.RegisterGateway(t, testNetworkID, testAgHwId, &models.GatewayDevice{HardwareID: testAgHwId})
 
 	// encode the appropriate certificate into context
 	ctx := test_utils.GetContextWithCertificate(t, testAgHwId)
@@ -55,8 +46,8 @@ func TestState(t *testing.T) {
 	gwStatus := checkindTestUtils.GetGatewayStatusSwaggerFixture(testAgHwId)
 	test_utils.ReportGatewayStatus(t, ctx, gwStatus)
 
-	getStateNoError(t, restPort, testNetworkId)
-	getStateNotFoundError(t, restPort, testNetworkId)
+	getStateNoError(t, restPort, testNetworkID)
+	getStateNotFoundError(t, restPort, testNetworkID)
 }
 
 func getURL(restPort int, networkID string, hwID string) string {
