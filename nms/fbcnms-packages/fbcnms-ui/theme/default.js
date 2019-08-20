0245ce7ae4765fbf72d65d22308387bc9ef357d7
Dolev Hadar <dolevh@fb.com>
2019-08-20 03:02:50 -0700

    remove shadows from all buttons
    Summary: See: https://docs.google.com/presentation/d/1HjDi-nGtALfreWkX-170uLzfktB-SOQYd4uArAQ3N6c/edit#slide=id.g5fcbad8339_0_56
    
    Reviewed By: noamsch
    
    Differential Revision: D16891216
    
    fbshipit-source-id: 5f40183dfdae201065a60b74dc7dd07082c354f3

--- nms/fbcnms-packages/fbcnms-ui/theme/default.js
+++ nms/fbcnms-packages/fbcnms-ui/theme/default.js
@@ -79,8 +79,14 @@ export default createMuiTheme({
       root: {
         borderRadius: 4,
         cursor: 'pointer',
-        fontSize: 14,
-        padding: '6px 30px',
+        fontSize: '14px',
+        textTransform: 'capitalize',
+        padding: '8px 12px',
+        fontWeight: 500,
+        lineHeight: '16px',
+      },
+      contained: {
+        boxShadow: 'none',
       },
     },
     MuiToggleButtonGroup: {
