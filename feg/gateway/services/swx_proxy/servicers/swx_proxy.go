0658952cafad934c8b1dd7507417448881a12ee3
Evgeniy Makeev <evgeniym@fb.com>
2019-09-11 11:11:23 -0700

    HSS UNREGISTER For SWx (#623)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/623
    
    IF IMSI was registered with a previous CWAG (AAA server) the concecutive MARs will fail.
    Handle this case by trying to deregister the user from HSS first using the previous AAA Server credentials & then retransmit MAR
    
    Reviewed By: mpgermano
    
    Differential Revision: D17306601
    
    fbshipit-source-id: e482b752b5f0ccbd53b550ab652799702c1cf120

--- feg/gateway/services/swx_proxy/servicers/swx_proxy.go
+++ feg/gateway/services/swx_proxy/servicers/swx_proxy.go
@@ -50,10 +50,12 @@ type swxProxy struct {
 }
 
 type SwxProxyConfig struct {
-	ClientCfg           *diameter.DiameterClientConfig
-	ServerCfg           *diameter.DiameterServerConfig
-	VerifyAuthorization bool // should we verify non-3gpp IP access is enabled for user
-	CacheTTLSeconds     uint32
+	ClientCfg             *diameter.DiameterClientConfig
+	ServerCfg             *diameter.DiameterServerConfig
+	VerifyAuthorization   bool // should we verify non-3gpp IP access is enabled for user
+	RegisterOnAuth        bool // should we send SAR REGISTER on every MAR/A
+	DeriveUnregisterRealm bool // use returned maa.AAAServerName to derive Origin Realm from
+	CacheTTLSeconds       uint32
 }
 
 // NewSwxProxy creates a new instance of the proxy with configured cache TTL
