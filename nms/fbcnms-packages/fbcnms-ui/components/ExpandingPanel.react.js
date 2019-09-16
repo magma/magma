6b787896ec49713f32f53dc94987fc55ddf53b48
Dolev Hadar <dolevh@fb.com>
2019-09-16 05:30:10 -0700

    Project types - first screens
    Summary: Just draft UI for defining a new project type
    
    Reviewed By: noamsch
    
    Differential Revision: D17394503
    
    fbshipit-source-id: f3e0349807e85c6a950cc2013b572fff0bd83781

---
+++ nms/fbcnms-packages/fbcnms-ui/components/ExpandingPanel.react.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import * as React from 'react';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  expansionPanel: {
+    padding: '24px',
+    borderRadius: '4px',
+    '&:before': {
+      content: 'none',
+    },
+    boxShadow: '0px 1px 4px 0px rgba(0,0,0,0.17)',
+  },
+  expansionPanelSummary: {
+    '&&': {
+      padding: '0px',
+      minHeight: 'auto',
+    },
+  },
+  expandIcon: {
+    padding: '0px',
+  },
+  summaryContent: {
+    '&&': {
+      margin: 0,
+    },
+  },
+  panelTitle: {
+    fontSize: '20px',
+    color: theme.palette.blueGrayDark,
+    lineHeight: '28px',
+    fontWeight: 500,
+  },
+  panelDetails: {
+    padding: 0,
+    marginTop: '16px',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+}));
+
+type Props = {
+  title: string,
+  children: React.Node,
+};
+
+const ExpandingPanel = ({children, title}: Props) => {
+  const classes = useStyles();
+  return (
+    <ExpansionPanel className={classes.expansionPanel} defaultExpanded={true}>
+      <ExpansionPanelSummary
+        className={classes.expansionPanelSummary}
+        classes={{
+          expandIcon: classes.expandIcon,
+          content: classes.summaryContent,
+        }}
+        expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.panelTitle}>{title}</Typography>
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails className={classes.panelDetails}>
+        {children}
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+};
+
+export default ExpandingPanel;
