d7e39b26941a61b646160dec70b86264a7f3473c
Joshua Braegger <jbraeg@fb.com>
2019-09-23 02:50:35 -0700

    Move rhino to magmalte
    Summary: Putting all code into magmalte, this isolates just rhino
    
    Reviewed By: tcirstea
    
    Differential Revision: D17505285
    
    fbshipit-source-id: b6254726555cdf4acc1f193a273818bd5427b559

--- nms/fbcnms-projects/magmalte/app/components/layout/useSections.js
+++ nms/fbcnms-projects/magmalte/app/components/layout/useSections.js
@@ -17,9 +17,10 @@ import NetworkContext from '../context/NetworkContext';
 import axios from 'axios';
 import {useContext, useEffect, useState} from 'react';
 
-import {CELLULAR} from '@fbcnms/types/network';
+import {CELLULAR, RHINO} from '@fbcnms/types/network';
 import {MagmaAPIUrls} from '../../common/MagmaAPI';
 import {getLteSections} from '../lte/LteSections';
+import {getRhinoSections} from '../rhino/RhinoSections';
 import {useFeatureFlag} from '@fbcnms/ui/hooks';
 
 export default function useSections(): SectionsConfigs {
@@ -40,6 +41,8 @@ export default function useSections(): SectionsConfigs {
   }
 
   switch (networkType) {
+    case RHINO:
+      return getRhinoSections();
     case CELLULAR:
     default:
       return getLteSections(alertsEnabled);
