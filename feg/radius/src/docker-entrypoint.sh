5ea75d1feb6cf86060795bdd4bd0d3e547aa8ff3
Kenneth Kahrs <kahrs@fb.com>
2019-09-23 13:56:45 -0700

    Add numerous missing copyright notices
    Summary: no functional changes, comments only
    
    Reviewed By: shahrahman-fb
    
    Differential Revision: D17506219
    
    fbshipit-source-id: 6eab521436ceaeee4ce6b5ad383397d80f659ce5

--- feg/radius/src/docker-entrypoint.sh
+++ feg/radius/src/docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+################################################################################
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+################################################################################
+
 /usr/bin/envsubst < ./radius.config.json.template > ./radius.config.json
 ./radius
--- orc8r/cloud/deploy/terraform/db.tf
+++ orc8r/cloud/deploy/terraform/db.tf
@@ -1,3 +1,11 @@
+################################################################################
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+################################################################################
+
 resource "aws_db_instance" "default" {
   identifier        = "orc8rdb"
   allocated_storage = 32
