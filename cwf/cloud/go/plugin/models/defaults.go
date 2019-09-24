681701fbcb12ed8c4e922e33a6db431534b2aad4
Andrei Lee <andreilee@fb.com>
2019-09-24 07:26:13 -0700

    Add unit tests for V1 API REST handlers
    Summary: Adding unit tests for carrier wifi v1 REST API handlers.
    
    Reviewed By: xjtian
    
    Differential Revision: D17512416
    
    fbshipit-source-id: 5b4afe996bdf39c8237e3b78db42fa0b6896426d

---
+++ cwf/cloud/go/plugin/models/defaults.go
@@ -0,0 +1,29 @@
+package models
+
+func NewDefaultNetworkCarrierWifiConfigs() *NetworkCarrierWifiConfigs {
+	defaultRuleID := "default_rule_1"
+	return &NetworkCarrierWifiConfigs{
+		AaaServer: &AaaServer{
+			AccountingEnabled:    false,
+			CreateSessionOnAuth:  false,
+			IDLESessionTimeoutMs: 21600000,
+		},
+		DefaultRuleID: &defaultRuleID,
+		EapAka: &EapAka{
+			PlmnIds: []string{"123456"},
+			Timeout: &EapAkaTimeout{
+				ChallengeMs:            20000,
+				ErrorNotificationMs:    10000,
+				SessionAuthenticatedMs: 5000,
+				SessionMs:              43200000,
+			},
+		},
+		NetworkServices: []string{"policy_enforcement"},
+	}
+}
+
+func NewDefaultModifiedNetworkCarrierWifiConfigs() *NetworkCarrierWifiConfigs {
+	configs := NewDefaultNetworkCarrierWifiConfigs()
+	configs.AaaServer.AccountingEnabled = true
+	return configs
+}
