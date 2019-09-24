c9f9a2deece1f78791d6c9cee96d655b5ff5a93b
jayashreeam <jayashree.manjunath@radisys.com>
2019-09-24 15:21:28 -0700

    S1SIM: Added New testcase for multi enb (#435)
    Summary:
    S1SIM: Added New testcase for multi enb
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/435
    
    Reviewed By: talkhasib
    
    Differential Revision: D16443289
    
    Pulled By: ssanadhya
    
    fbshipit-source-id: 4979a1cf57082683b11c304eb25869afd2433ae6

--- lte/gateway/python/integ_tests/s1aptests/s1ap_wrapper.py
+++ lte/gateway/python/integ_tests/s1aptests/s1ap_wrapper.py
@@ -11,14 +11,14 @@ import os
 import s1ap_types
 import time
 
-#from integ_tests.cloud.cloud_manager import CloudManager
+# from integ_tests.cloud.cloud_manager import CloudManager
 from integ_tests.common.mobility_service_client import MobilityServiceGrpc, \
-    MobilityServiceOAI #, MobilityServiceRest
+     MobilityServiceOAI  # MobilityServiceRest
 from integ_tests.common.service303_utils import GatewayServicesUtil
 from integ_tests.common.subscriber_db_client import SubscriberDbGrpc, \
-    SubscriberDbMySQL #, SubscriberDbRest
+     SubscriberDbMySQL  # SubscriberDbRest
 from integ_tests.s1aptests.s1ap_utils import MobilityUtil, S1ApUtil, \
-    SubscriberUtil
+     SubscriberUtil
 from integ_tests.s1aptests.util.traffic_util import TrafficUtil
 
 
@@ -37,7 +37,7 @@ class TestWrapper(object):
         self._s1_util = S1ApUtil()
         self._enBConfig()
 
-        #if self._test_cloud:
+        # if self._test_cloud:
         #    self._cloud_manager = CloudManager()
         #    self._cloud_manager.clean_up()
         #    subscriber_client = SubscriberDbRest(self._cloud_manager)
@@ -299,5 +299,39 @@ class TestWrapper(object):
 
         # Cloud cleanup needs to happen after cleanup for
         # subscriber util and mobility util
-        #if self._test_cloud:
+        # if self._test_cloud:
         #    self._cloud_manager.clean_up()
+
+    def multiEnbConfig(self, num_of_enbs, enb_list=None):
+        if enb_list is None:
+            enb_list = []
+        req = s1ap_types.multiEnbConfigReq_t()
+        req.numOfEnbs = num_of_enbs
+        # ENB Parameter column index initialization
+        PLMN_LENGTH = 6
+        CELLID_COL_IDX = 0
+        TAC_COL_IDX = 1
+        ENBTYPE_COL_IDX = 2
+        PLMNID_COL_IDX = 3
+
+        for idx1 in range(num_of_enbs):
+            req.multiEnbCfgParam[idx1].cell_id = \
+                    (enb_list[idx1][CELLID_COL_IDX])
+
+        for idx1 in range(num_of_enbs):
+            req.multiEnbCfgParam[idx1].tac = \
+                    (enb_list[idx1][TAC_COL_IDX])
+
+        for idx1 in range(num_of_enbs):
+            req.multiEnbCfgParam[idx1].enbType = \
+                    (enb_list[idx1][ENBTYPE_COL_IDX])
+
+        for idx1 in range(num_of_enbs):
+            for idx3 in range(PLMN_LENGTH):
+                val = enb_list[idx1][PLMNID_COL_IDX][idx3]
+                req.multiEnbCfgParam[idx1].plmn_id[idx3] = int(val)
+
+        print("***************** Sending Multiple Enb Config Request\n")
+        assert (
+            self._s1_util.issue_cmd(
+                 s1ap_types.tfwCmd.MULTIPLE_ENB_CONFIG_REQ, req) == 0)
