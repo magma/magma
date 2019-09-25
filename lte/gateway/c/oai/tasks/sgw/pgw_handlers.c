4c6f7ff60786e393136015a1f9916c20eb93659a
Pruthvi Hebbani <pruthvi.hebbani@radisys.com>
2019-09-25 11:23:59 -0700

    Pruthvi volte integ testing (#651)
    Summary:
    1. Removed usage of EPS_NETWORK_FEATURE_SUPPORT_IMS_VOICE_OVER_PS_SESSION_IN_S1  flag
    2. Verified sanity using S1 SIM
    3. Verified test_attach_detach_dedicated.py TC
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/651
    
    Reviewed By: ssanadhya
    
    Differential Revision: D17574498
    
    Pulled By: talkhasib
    
    fbshipit-source-id: ffb761fa5bdc42dda3dbd68ac8af6d8427a08d4e

--- lte/gateway/c/oai/tasks/sgw/pgw_handlers.c
+++ lte/gateway/c/oai/tasks/sgw/pgw_handlers.c
@@ -582,7 +582,6 @@ uint32_t pgw_handle_nw_initiated_bearer_deactv_req(
     if (hashtblP->nodes[i] != NULL) {
       node = hashtblP->nodes[i];
       spgw_ctxt_p = node->data;
-      pthread_mutex_unlock(&hashtblP->lock_nodes[i]);
       num_elements++;
       if (spgw_ctxt_p != NULL) {
         if (!strcmp(
@@ -604,6 +603,7 @@ uint32_t pgw_handle_nw_initiated_bearer_deactv_req(
         }
       }
     }
+    pthread_mutex_unlock(&hashtblP->lock_nodes[i]);
     i++;
   }
   OAILOG_INFO(
