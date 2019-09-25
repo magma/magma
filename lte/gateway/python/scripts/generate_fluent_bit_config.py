209b5a460f08632932bfb910ed9ca43ccdef94d5
Satya Bodduluri <skb@fb.com>
2019-09-25 05:11:37 -0700

    Added extra tags to fluentbit mconfig
    Summary: Added a FluentBit config section to mconfig that contains a single extra_tags map for arbitrary flags to pass to fluent-bit. By default, we pass in networkID and gatewayID for easier identification in ElasticSearch.
    
    Reviewed By: xjtian
    
    Differential Revision: D17570436
    
    fbshipit-source-id: f58453d0637e66bc755e721e3bccb945825a7950

--- lte/gateway/python/scripts/generate_fluent_bit_config.py
+++ lte/gateway/python/scripts/generate_fluent_bit_config.py
@@ -14,15 +14,27 @@ and the config/mconfig for the service.
 import logging
 
 from generate_service_config import generate_template_config
+from lte.protos.mconfig.mconfigs_pb2 import FluentBit
+from magma.configuration.mconfig_managers import load_service_mconfig
 
 CONFIG_OVERRIDE_DIR = "/var/opt/magma/tmp"
+
+
+def _get_extra_tags():
+    """
+    Get the extra_tags specified in the FluentBit mconfig.
+    """
+    return load_service_mconfig("td-agent-bit", FluentBit()).extra_tags
 
 
 def main():
     logging.basicConfig(
         level=logging.INFO, format="[%(asctime)s %(levelname)s %(name)s] %(message)s"
     )
-    generate_template_config("td-agent-bit", "td-agent-bit", CONFIG_OVERRIDE_DIR, {})
+    context = {"extra_tags": _get_extra_tags().items()}
+    generate_template_config(
+        "td-agent-bit", "td-agent-bit", CONFIG_OVERRIDE_DIR, context.copy()
+    )
 
 
 if __name__ == "__main__":
