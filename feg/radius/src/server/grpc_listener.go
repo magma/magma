546769bd89d9b6eb5a79ed0486c7ff9982e67c8c
Michael Germano <mgermano@fb.com>
2019-09-26 18:44:10 -0700

    Add Cisco-required attributes to GRPC CoA listener (#662)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/662
    
    Cisco require both `NAS_IP_ADDRESS` and `CALLING_STATION_ID` to appear in CoA-Request and Disconnect-Request in order to route it to the specific AP.
    
    Reviewed By: emakeev
    
    Differential Revision: D17601724
    
    fbshipit-source-id: 8278f891cec6f6d3d08b7be052391343a344306c

--- feg/radius/src/server/grpc_listener.go
+++ feg/radius/src/server/grpc_listener.go
@@ -21,6 +21,7 @@ import (
 	"net"
 
 	"fbc/lib/go/radius"
+	"fbc/lib/go/radius/rfc2865"
 	"fbc/lib/go/radius/rfc2866"
 
 	"github.com/mitchellh/mapstructure"
@@ -176,6 +177,7 @@ func (s *authorizationServer) handleCoaRequest(ctx *protos.Context, request *rad
 	// Add Acct-Session-Id attribute
 	request.Attributes = radius.Attributes{}
 	request.Set(rfc2866.AcctSessionID_Type, radius.Attribute(state.AcctSessionID))
+	request.Set(rfc2865.CallingStationID_Type, radius.Attribute(ctx.MacAddr))
 
 	// Set Identifier
 	request.Identifier = state.NextCoAIdentifier
