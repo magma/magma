c982f763c3773cfe989beca52448ada3530ce48c
David Bond <davidmokonbond@fb.com>
2019-10-10 09:27:41 -0700

    add ping timeouts to devmand (#701)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/701
    
    This commit adds ping timeouts to devmand.
    
    Reviewed By: al8
    
    Differential Revision: D17747578
    
    fbshipit-source-id: 94b0ed55b92c8b2eaea1305f66d07ef57d460357

--- devmand/gateway/src/devmand/channels/ping/Engine.cpp
+++ devmand/gateway/src/devmand/channels/ping/Engine.cpp
@@ -18,6 +18,8 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <algorithm>
+
 #include <folly/GLog.h>
 
 #include <devmand/channels/ping/Engine.h>
@@ -27,9 +29,14 @@ namespace devmand {
 namespace channels {
 namespace ping {
 
-Engine::Engine(folly::EventBase& _eventBase)
-    : folly::EventHandler(&_eventBase), eventBase(_eventBase) {
-  // TODO this socket needs to be moved to an engine...
+Engine::Engine(
+    folly::EventBase& _eventBase,
+    const std::chrono::milliseconds& pingTimeout_,
+    const std::chrono::milliseconds& timeoutFrequency_)
+    : folly::EventHandler(&_eventBase),
+      eventBase(_eventBase),
+      pingTimeout(pingTimeout_),
+      timeoutFrequency(timeoutFrequency_) {
   icmpSocket = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
   if (icmpSocket < 0) {
     throw std::system_error(errno, std::generic_category());
@@ -45,77 +52,123 @@ Engine::Engine(folly::EventBase& _eventBase)
   registerHandler(folly::EventHandler::READ | folly::EventHandler::PERSIST);
 }
 
+Engine::~Engine() {
+  unregisterHandler();
+}
+
+void Engine::start() {
+  EventBaseUtils::scheduleEvery(
+      eventBase, [this]() { timeout(); }, timeoutFrequency);
+}
+
+void Engine::timeout() {
+  sharedOutstandingRequests.withULockPtr([this](auto uOutstandingRequests) {
+    auto outstandingRequests = uOutstandingRequests.moveFromUpgradeToWrite();
+    LOG(INFO) << "Processing ping timeouts";
+    for (auto it = outstandingRequests->begin();
+         it != outstandingRequests->end();) {
+      utils::TimePoint end = utils::Time::now();
+      if ((end - it->second.start) > pingTimeout) {
+        LOG(ERROR) << "Ping request timed out";
+        it->second.promise.setValue(0);
+        it = outstandingRequests->erase(it);
+      } else {
+        ++it;
+      }
+    }
+  });
+}
+
 folly::Future<Rtt> Engine::ping(
     const icmphdr& hdr,
     const folly::IPAddress& destination) {
-  auto request = outstandingRequests.emplace(
-      std::piecewise_construct,
-      std::forward_as_tuple(std::make_pair(destination, hdr.un.echo.sequence)),
-      std::forward_as_tuple(Request{}));
-  if (request.second) {
-    sockaddr_storage dst;
-    destination.toSockaddrStorage(&dst);
-    request.first->second.start = utils::Time::now();
-    auto result = sendto(
-        icmpSocket,
-        &hdr,
-        sizeof(hdr),
-        0,
-        reinterpret_cast<const sockaddr*>(&dst),
-        sizeof(dst));
-    if (result <= 0) {
-      switch (result) {
-        case EAGAIN: // case EWOULDBLOCK:
-          // TODO if the ping fail because of a kernel buffer I'm not going to
-          // implement retry logic as something is filling up the buffers. We
-          // should probably alarm if this is the case.
-          LOG(ERROR) << "Buffer full so ping failed";
-          break;
-        default:
-          // TODO BOOTCAMP get errno string from syserror
-          LOG(ERROR) << "Failed to send packet with errno " << errno;
-          break;
+  return sharedOutstandingRequests.withULockPtr([this, &hdr, &destination](
+                                                    auto uOutstandingRequests) {
+    auto outstandingRequests = uOutstandingRequests.moveFromUpgradeToWrite();
+    auto request = outstandingRequests->emplace(
+        std::piecewise_construct,
+        std::forward_as_tuple(
+            std::make_pair(destination, hdr.un.echo.sequence)),
+        std::forward_as_tuple(Request{}));
+    if (request.second) {
+      sockaddr_storage dst;
+      destination.toSockaddrStorage(&dst);
+      request.first->second.start = utils::Time::now();
+      auto result = sendto(
+          icmpSocket,
+          &hdr,
+          sizeof(hdr),
+          0,
+          reinterpret_cast<const sockaddr*>(&dst),
+          sizeof(dst));
+      if (result <= 0) {
+        switch (result) {
+          case EAGAIN: // case EWOULDBLOCK:
+            // TODO if the ping fail because of a kernel buffer I'm not going to
+            // implement retry logic as something is filling up the buffers. We
+            // should probably alarm if this is the case.
+            LOG(ERROR) << "Buffer full so ping failed";
+            break;
+          default:
+            // TODO BOOTCAMP get errno string from syserror
+            LOG(ERROR) << "Failed to send packet with errno " << errno;
+            break;
+        }
+        outstandingRequests->erase(request.first);
+        return folly::makeFuture<Rtt>(0);
+      } else {
+        auto ret = request.first->second.promise.getFuture();
+        return ret;
       }
-      outstandingRequests.erase(request.first);
-      return folly::makeFuture<Rtt>(0);
     } else {
-      return request.first->second.promise.getFuture();
+      LOG(ERROR) << "ICMP Echo Id rollover with outstanding requests";
+      return folly::makeFuture<Rtt>(0);
     }
-  } else {
-    LOG(ERROR) << "ICMP Echo Id rollover with outstanding requests";
-    return folly::makeFuture<Rtt>(0);
-  }
-  // TODO implement timeout
+  });
+}
+
+IcmpPacket Engine::read() {
+  IcmpPacket pkt;
+  pkt.success = recvfrom(
+                    icmpSocket,
+                    &pkt.hdr,
+                    sizeof(pkt.hdr),
+                    0,
+                    reinterpret_cast<sockaddr*>(&pkt.src),
+                    &pkt.srcLen) > 0;
+  return pkt;
 }
 
 void Engine::handlerReady(uint16_t) noexcept {
   // TODO end time isn't really precise here as we don't have a kernel time
   // need to implement kernel timestamping
   utils::TimePoint end = utils::Time::now();
-  icmphdr hdr;
-  sockaddr_storage src;
-  socklen_t srcLen = sizeof(sockaddr_storage);
-  while (recvfrom(
-             icmpSocket,
-             &hdr,
-             sizeof(hdr),
-             0,
-             reinterpret_cast<sockaddr*>(&src),
-             &srcLen) > 0) {
-    LOG(INFO) << "Packet received with ICMP type " << static_cast<int>(hdr.type)
-              << " code " << static_cast<int>(hdr.code);
+  for (IcmpPacket pkt = read(); pkt.success; pkt = read()) {
+    bool processed{false};
+    if (pkt.hdr.type == 0 and pkt.hdr.code == 0) {
+      sharedOutstandingRequests.withULockPtr([this, &end, &pkt, &processed](
+                                                 auto uOutstandingRequests) {
+        auto outstandingRequests =
+            uOutstandingRequests.moveFromUpgradeToWrite();
+        auto request = outstandingRequests->find(std::make_pair(
+            folly::IPAddress(reinterpret_cast<sockaddr*>(&pkt.src)),
+            pkt.hdr.un.echo.sequence));
+        if (request != outstandingRequests->end()) {
+          auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+              end - request->second.start);
+          LOG(INFO) << "Received ICMP response after " << duration.count()
+                    << " microseconds";
+          request->second.promise.setValue(duration.count());
+          outstandingRequests->erase(request);
+          processed = true;
+        }
+      });
+    }
 
-    if (hdr.type == 0 and hdr.code == 0) {
-      auto request = outstandingRequests.find(std::make_pair(
-          folly::IPAddress(reinterpret_cast<sockaddr*>(&src)),
-          hdr.un.echo.sequence));
-      if (request != outstandingRequests.end()) {
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-            end - request->second.start);
-        LOG(INFO) << "Received ICMP response after " << duration.count()
-                  << " microseconds";
-        request->second.promise.setValue(duration.count());
-      }
+    if (not processed ) {
+      LOG(INFO) << "Packet received with ICMP type "
+                << static_cast<int>(pkt.hdr.type) << " code "
+                << static_cast<int>(pkt.hdr.code);
     }
   }
 }
