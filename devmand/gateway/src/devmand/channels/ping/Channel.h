6688b4f5433957b3d7ef872a14bd69363acf93fa
David Bond <davidmokonbond@fb.com>
2019-10-10 07:39:52 -0700

    start ping channel
    Summary: This commit starts the ping channel. This sends an unmonitored icmp request.
    
    Differential Revision: D17747585
    
    fbshipit-source-id: 6156f48f11c8283225e6e552c41e91526ad1cf0f

---
+++ devmand/gateway/src/devmand/channels/ping/Channel.h
@@ -0,0 +1,49 @@
+// Copyright (c) 2016-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#include <string>
+
+#include <netinet/ip_icmp.h>
+
+#include <folly/futures/Future.h>
+#include <folly/io/async/AsyncSocket.h>
+
+#include <devmand/channels/Channel.h>
+
+namespace devmand {
+namespace channels {
+namespace ping {
+
+using Rtt = uint32_t;
+
+class Channel : public channels::Channel {
+ public:
+  Channel(folly::EventBase& _eventBase, folly::IPAddress target_);
+  Channel() = delete;
+  ~Channel() override = default;
+  Channel(const Channel&) = delete;
+  Channel& operator=(const Channel&) = delete;
+  Channel(Channel&&) = delete;
+  Channel& operator=(Channel&&) = delete;
+
+ public:
+  folly::Future<Rtt> ping();
+
+ private:
+  uint16_t getSequence();
+  icmphdr makeIcmpPacket();
+
+ private:
+  folly::EventBase& eventBase;
+  folly::IPAddress target;
+  int icmpSocket{-1};
+  uint16_t sequence{0};
+};
+
+} // namespace ping
+} // namespace channels
+} // namespace devmand
