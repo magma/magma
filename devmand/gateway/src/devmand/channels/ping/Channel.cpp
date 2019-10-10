5bd584d93aedbad3039fe31bcf2599b8f2eaa2ea
David Bond <davidmokonbond@fb.com>
2019-10-10 07:40:09 -0700

    add ping engine in devmand (#698)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/698
    
    This commit makes a ping engine in devmand.
    
    Reviewed By: al8
    
    Differential Revision: D17747581
    
    fbshipit-source-id: b324fdbbaaf1c8aa0da89119dee168610d4ce373

--- devmand/gateway/src/devmand/channels/ping/Channel.cpp
+++ devmand/gateway/src/devmand/channels/ping/Channel.cpp
@@ -6,44 +6,17 @@
 // of patent rights can be found in the PATENTS file in the same directory.
 
 #include <arpa/inet.h>
-#include <fcntl.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/ip_icmp.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <time.h>
-#include <unistd.h>
 
 #include <folly/GLog.h>
 
 #include <devmand/channels/ping/Channel.h>
-#include <devmand/utils/Time.h>
 
 namespace devmand {
 namespace channels {
 namespace ping {
 
-Channel::Channel(folly::EventBase& _eventBase, folly::IPAddress target_)
-    : folly::EventHandler(&_eventBase), eventBase(_eventBase), target(target_) {
-  // TODO this socket needs to be moved to an engine...
-  icmpSocket = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
-  if (icmpSocket < 0) {
-    throw std::system_error(errno, std::generic_category());
-  }
-
-  if (fcntl(icmpSocket, F_SETFL, O_NONBLOCK) < 0) {
-    throw std::system_error(errno, std::generic_category());
-  }
-
-  folly::EventHandler::changeHandlerFD(
-      folly::NetworkSocket::fromFd(icmpSocket));
-
-  registerHandler(folly::EventHandler::READ | folly::EventHandler::PERSIST);
-}
+Channel::Channel(Engine& engine_, folly::IPAddress target_)
+    : engine(engine_), target(target_) {}
 
 folly::Future<Rtt> Channel::ping() {
   icmphdr hdr = makeIcmpPacket();
@@ -59,42 +32,7 @@ folly::Future<Rtt> Channel::ping() {
   }
   destination.sin_family = AF_INET;
 
-  auto request = outstandingRequests.emplace(
-      std::piecewise_construct,
-      std::forward_as_tuple(hdr.un.echo.sequence),
-      std::forward_as_tuple(Request{}));
-  if (request.second) {
-    request.first->second.start = utils::Time::now();
-    auto result = sendto(
-        icmpSocket,
-        &hdr,
-        sizeof(hdr),
-        0,
-        reinterpret_cast<sockaddr*>(&destination),
-        sizeof(destination));
-    if (result <= 0) {
-      switch (result) {
-        case EAGAIN: // case EWOULDBLOCK:
-          // TODO if the ping fail because of a kernel buffer I'm not going to
-          // implement retry logic as something is filling up the buffers. We
-          // should probably alarm if this is the case.
-          LOG(ERROR) << "Buffer full so ping failed to " << target.str();
-          break;
-        default:
-          // TODO BOOTCAMP get errno string from syserror
-          LOG(ERROR) << "Failed to send packet with errno " << errno;
-          break;
-      }
-      outstandingRequests.erase(request.first);
-      return folly::makeFuture<Rtt>(0);
-    } else {
-      return request.first->second.promise.getFuture();
-    }
-  } else {
-    LOG(ERROR) << "ICMP Echo Id rollover with outstanding requests";
-    return folly::makeFuture<Rtt>(0);
-  }
-  // TODO implement timeout
+  return engine.ping(hdr, destination);
 }
 
 RequestId Channel::getSequence() {
@@ -108,28 +46,6 @@ icmphdr Channel::makeIcmpPacket() {
   hdr.un.echo.sequence = getSequence();
   // hdr.checksum = 0; // Let the kernel fill in the checksum
   return hdr;
-}
-
-void Channel::handlerReady(uint16_t) noexcept {
-  // TODO end time isn't really precise here as we don't have a kernel time
-  // need to implement kernel timestamping
-  utils::TimePoint end = utils::Time::now();
-  icmphdr hdr;
-  while (recv(icmpSocket, &hdr, sizeof(hdr), 0) > 0) {
-    LOG(INFO) << "Packet received with ICMP type " << static_cast<int>(hdr.type)
-              << " code " << static_cast<int>(hdr.code);
-
-    if (hdr.type == 0 and hdr.code == 0) {
-      auto request = outstandingRequests.find(hdr.un.echo.sequence);
-      if (request != outstandingRequests.end()) {
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-            end - request->second.start);
-        LOG(INFO) << "Received ICMP response after " << duration.count()
-                  << " microseconds";
-        request->second.promise.setValue(duration.count());
-      }
-    }
-  }
 }
 
 } // namespace ping
---
+++ devmand/gateway/src/devmand/channels/ping/Engine.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2016-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#include <map>
+#include <string>
+
+#include <netinet/ip_icmp.h>
+
+#include <folly/futures/Future.h>
+#include <folly/io/async/AsyncSocket.h>
+
+#include <devmand/channels/Engine.h>
+#include <devmand/utils/Time.h>
+
+namespace devmand {
+namespace channels {
+namespace ping {
+
+using Rtt = uint64_t;
+
+struct Request {
+  utils::TimePoint start;
+  folly::Promise<Rtt> promise;
+};
+
+using RequestId = uint16_t;
+
+using OutstandingRequests = std::map<RequestId, Request>;
+
+class Engine : public channels::Engine, public folly::EventHandler {
+ public:
+  Engine(folly::EventBase& _eventBase);
+  Engine() = delete;
+  ~Engine() override = default;
+  Engine(const Engine&) = delete;
+  Engine& operator=(const Engine&) = delete;
+  Engine(Engine&&) = delete;
+  Engine& operator=(Engine&&) = delete;
+
+ public:
+  folly::Future<Rtt> ping(const icmphdr& hdr, const sockaddr_in& destination);
+
+ private:
+  virtual void handlerReady(uint16_t events) noexcept override;
+
+ private:
+  folly::EventBase& eventBase;
+  OutstandingRequests outstandingRequests;
+  int icmpSocket{-1};
+};
+
+} // namespace ping
+} // namespace channels
+} // namespace devmand
