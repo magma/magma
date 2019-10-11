c1f1d4a1fc7a4312002592b66427db2d1bd800c6
Nick Yurchenko <koolzz@fb.com>
2019-10-11 12:31:28 -0700

    Add pipelined resilience redirection rule test (#680)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/680
    
    Tests the redirection hack for pipelined setup
    
    Reviewed By: amarpad
    
    Differential Revision: D17697642
    
    fbshipit-source-id: 4d0517ba948e5dda62591ca71b5c18e6db9b522b

--- lte/gateway/python/magma/pipelined/tests/test_restart_resilience.py
+++ lte/gateway/python/magma/pipelined/tests/test_restart_resilience.py
@@ -8,13 +8,13 @@ of patent rights can be found in the PATENTS file in the same directory.
 """
 
 import unittest
-import subprocess
 from concurrent.futures import Future
 from unittest.mock import MagicMock
 
 import warnings
 from lte.protos.mconfig.mconfigs_pb2 import PipelineD
-from lte.protos.policydb_pb2 import FlowDescription, FlowMatch, PolicyRule
+from lte.protos.policydb_pb2 import FlowDescription, FlowMatch, PolicyRule, \
+    RedirectInformation
 from lte.protos.pipelined_pb2 import ActivateFlowsRequest, SetupFlowsRequest
 from magma.subscriberdb.sid import SIDUtils
 from magma.pipelined.app.enforcement import EnforcementController
@@ -24,7 +24,8 @@ from magma.pipelined.app.base import global_epoch
 from magma.pipelined.policy_converters import flow_match_to_magma_match
 from magma.pipelined.tests.app.flow_query import RyuDirectFlowQuery \
     as FlowQuery
-from magma.pipelined.tests.app.packet_builder import IPPacketBuilder
+from magma.pipelined.tests.app.packet_builder import IPPacketBuilder, \
+    TCPPacketBuilder
 from magma.pipelined.tests.app.packet_injector import ScapyPacketInjector
 from magma.pipelined.tests.app.start_pipelined import PipelinedController, \
     TestSetup
@@ -42,6 +43,7 @@ class RestartResilienceTest(unittest.TestCase):
     BRIDGE = 'testing_br'
     IFACE = 'testing_br'
     MAC_DEST = "5e:cc:cc:b1:49:4b"
+    BRIDGE_IP_ADDRESS = '192.168.128.1'
 
     def _wait_func(self, stat_names):
         def func():
@@ -99,7 +101,7 @@ class RestartResilienceTest(unittest.TestCase):
             },
             config={
                 'bridge_name': cls.BRIDGE,
-                'bridge_ip_address': '192.168.128.1',
+                'bridge_ip_address': cls.BRIDGE_IP_ADDRESS,
                 'enforcement': {'poll_interval': 5},
                 'nat_iface': 'eth2',
                 'enodeb_iface': 'eth1',
@@ -410,8 +412,114 @@ class RestartResilienceTest(unittest.TestCase):
                                              self.service_manager,
                                              'after_restart')
 
-        with snapshot_verifier:
+        with flow_verifier, snapshot_verifier:
             pass
+
+        self.assertEqual(stats[enf_stat_name[0]].bytes_tx,
+        num_pkts_tx_match * len(packet1))
+
+    def test_url_redirect(self):
+        """
+        Partial redirection test, checks if flows were added properly for url
+        based redirection.
+
+        Assert:
+            1 Packet is matched
+            Packet bypass flows are added
+            Flow learn action is triggered - another flow is added to the table
+        """
+        fake_controller_setup(self.enforcement_controller,
+            self.enforcement_stats_controller, self.startup_flows_contoller)
+        redirect_ips = ["185.128.101.5", "185.128.121.4"]
+        self.enforcement_controller._redirect_manager._dns_cache.get(
+            "about.sha.ddih.org", lambda: redirect_ips, max_age=42
+        )
+        imsi = 'IMSI010000000088888'
+        sub_ip = '192.168.128.74'
+        flow_list = [FlowDescription(match=FlowMatch())]
+        policy = PolicyRule(
+            id='redir_test', priority=3, flow_list=flow_list,
+            redirect=RedirectInformation(
+                support=1,
+                address_type=2,
+                server_address="http://about.sha.ddih.org/"
+            )
+        )
+
+        # ============================ Subscriber ============================
+        sub_context = RyuDirectSubscriberContext(
+            imsi, sub_ip, self.enforcement_controller, self._tbl_num
+        )
+
+        setup_flows_request = SetupFlowsRequest(
+            requests=[
+                ActivateFlowsRequest(
+                    sid=SIDUtils.to_pb(imsi),
+                    ip_addr=sub_ip,
+                    dynamic_rules=[policy]
+                ),
+            ],
+            epoch=global_epoch
+        )
+
+        fake_controller_setup(
+            self.enforcement_controller,
+            self.enforcement_stats_controller,
+            self.startup_flows_contoller,
+            setup_flows_request)
+
+        isolator = RyuDirectTableIsolator(
+            RyuForwardFlowArgsBuilder.from_subscriber(sub_context.cfg)
+                                     .build_requests(),
+            self.testing_controller
+        )
+        pkt_sender = ScapyPacketInjector(self.IFACE)
+        packet = TCPPacketBuilder()\
+            .set_tcp_layer(42132, 80, 321)\
+            .set_tcp_flags("S")\
+            .set_ip_layer('151.42.41.122', sub_ip)\
+            .set_ether_layer(self.MAC_DEST, "00:00:00:00:00:00")\
+            .build()
+
+        # Check if these flows were added (queries should return flows)
+        permit_outbound, permit_inbound = [], []
+        for ip in redirect_ips:
+            permit_outbound.append(FlowQuery(
+                self._tbl_num, self.testing_controller,
+                match=flow_match_to_magma_match(
+                    FlowMatch(ipv4_dst=ip, direction=FlowMatch.UPLINK))
+            ))
+            permit_inbound.append(FlowQuery(
+                self._tbl_num, self.testing_controller,
+                match=flow_match_to_magma_match(
+                    FlowMatch(ipv4_src=ip, direction=FlowMatch.DOWNLINK))
+            ))
+
+        learn_action_flow = flow_match_to_magma_match(
+            FlowMatch(ip_proto=6, direction=FlowMatch.DOWNLINK,
+                      ipv4_src=self.BRIDGE_IP_ADDRESS, ipv4_dst=sub_ip)
+        )
+        learn_action_query = FlowQuery(self._tbl_num, self.testing_controller,
+                                       learn_action_flow)
+
+        # =========================== Verification ===========================
+        # 1 packet sent, permit rules installed, learn action installed. Since
+        # the enforcement table is entered via the DPI table and the scratch
+        # enforcement table, the number of packets handled by the table is 2.
+        flow_verifier = FlowVerifier(
+            [FlowTest(FlowQuery(self._tbl_num, self.testing_controller), 2),
+             FlowTest(learn_action_query, 0, flow_count=1)]
+            + [FlowTest(query, 0, flow_count=1) for query in permit_outbound]
+            + [FlowTest(query, 0, flow_count=1) for query in permit_inbound],
+            lambda: wait_after_send(self.testing_controller))
+
+        snapshot_verifier = SnapshotVerifier(self, self.BRIDGE,
+                                             self.service_manager)
+
+        with isolator, sub_context, flow_verifier, snapshot_verifier:
+            pkt_sender.send(packet)
+
+        flow_verifier.verify()
 
     def test_clean_restart(self):
         """
