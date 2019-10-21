0125d7f84c7ae723ea2ac80713db7d165e056109
Nick Yurchenko <koolzz@fb.com>
2019-10-21 14:18:08 -0700

    Fix LTE access control default rule forwarding (#761)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/761
    
    The rule was accidentally added to the access control table, it should instead be added to the scratch. Also updates the unit test
    
    Differential Revision: D18047177
    
    fbshipit-source-id: 3df0154b9540b668cc80134b55a57b6f6baeb007

--- lte/gateway/python/magma/pipelined/tests/test_access_control.py
+++ lte/gateway/python/magma/pipelined/tests/test_access_control.py
@@ -11,6 +11,7 @@ import unittest
 import warnings
 from concurrent.futures import Future
 
+from lte.protos.mconfig.mconfigs_pb2 import PipelineD
 from magma.pipelined.app.access_control import \
     AccessControlController
 from magma.pipelined.openflow.magma_match import MagmaMatch
@@ -67,6 +68,7 @@ class AccessControlTest(unittest.TestCase):
                     testing_controller_reference
             },
             config={
+                'setup_type': 'LTE',
                 'bridge_name': cls.BRIDGE,
                 'bridge_ip_address': cls.BRIDGE_IP,
                 'nat_iface': 'eth2',
@@ -88,7 +90,9 @@ class AccessControlTest(unittest.TestCase):
                     ]
                 }
             },
-            mconfig=None,
+            mconfig=PipelineD(
+                allowed_gre_peers=[],
+            ),
             loop=None,
             service_manager=cls.service_manager,
             integ_test=False,
