d632480c4ea8b865ba9ad67719e5fae43cbe821f
Michael Germano <mgermano@fb.com>
2019-10-23 19:31:38 -0700

    Update Redis containers and serializers to store versioned state (#768)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/768
    
    This diff updates the RedisDict container to store a serialized proto alongside
    a uint64 version. By adding this, we now have version storage on the gateways and in
    the orchestrator. This will allow us to properly synchronize replicated state between
    the two components.
    
    Reviewed By: xjtian
    
    Differential Revision: D17964898
    
    fbshipit-source-id: 1ea6358b07ecaa1c6bb60b44524feef0e53b2701

--- orc8r/gateway/python/magma/common/redis/serializers.py
+++ orc8r/gateway/python/magma/common/redis/serializers.py
@@ -7,24 +7,32 @@ LICENSE file in the root directory of this source tree. An additional grant
 of patent rights can be found in the PATENTS file in the same directory.
 """
 
+from orc8r.protos.redis_pb2 import RedisState
 
 def get_proto_serializer():
     """
-    Return a basic proto serializer that simply serializes to a string with no
-    extra field manipulation
+    Return a proto serializer that serializes the proto, adds the associated
+    version, and then serializes the RedisState proto to a string
     """
-    def _serialize_proto(proto):
-        return proto.SerializeToString()
+    def _serialize_proto(proto, version):
+        serialized_proto = proto.SerializeToString()
+        redis_state = RedisState(
+            serialized_msg=serialized_proto,
+            version=version)
+        return redis_state.SerializeToString()
     return _serialize_proto
 
 
 def get_proto_deserializer(proto_class):
     """
-    Return a basic proto deserializer that takes in a proto type to deserialize
-    to
+    Return a proto deserializer that takes in a proto type to deserialize
+    the serialized msg stored in the RedisState proto
     """
     def _deserialize_proto(serialized_rule):
+        proto_wrapper = RedisState()
+        proto_wrapper.ParseFromString(serialized_rule)
+        serialized_proto = proto_wrapper.serialized_msg
         proto = proto_class()
-        proto.ParseFromString(serialized_rule)
+        proto.ParseFromString(serialized_proto)
         return proto
     return _deserialize_proto
