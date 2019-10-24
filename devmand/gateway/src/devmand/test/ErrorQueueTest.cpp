ca33319f93b61ee8520da1466298c1442e36ddc1
David Bond <davidmokonbond@fb.com>
2019-10-24 12:32:42 -0700

    run format on devmand
    Summary:
    We missed running format on some commits in devmand so this just gets
    it up to date.
    
    Differential Revision: D18042383
    
    fbshipit-source-id: 9208f2af429c484fad0b8cc236f36c72ea36d4b0

--- devmand/gateway/src/devmand/test/ErrorQueueTest.cpp
+++ devmand/gateway/src/devmand/test/ErrorQueueTest.cpp
@@ -42,8 +42,8 @@ TEST(ErrorQueueTest, SimpleEnqueueDequeue) {
   EXPECT_EQ(errs3, expected3);
 }
 
-static int doManyAdds(
-    int addCount, int getInterval, std::shared_ptr<ErrorQueue> errors) {
+static int
+doManyAdds(int addCount, int getInterval, std::shared_ptr<ErrorQueue> errors) {
   // "add" lots of errors
   for (int i = 0; i < addCount; ++i) {
     errors->add(std::move(folly::to<std::string>(i)));
