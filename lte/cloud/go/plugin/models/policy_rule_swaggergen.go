3f99dab53015d205aab3e9678d0b78888c1ebc18
Jacky Tian <xjtian@fb.com>
2019-11-13 15:44:08 -0800

    Update policy handlers with ability to associate BNRs and rules to subscribers (#857)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/857
    
    - Note: this diff breaks the legacy policy handlers (will stack fix on top of here to keep this reviewable)
    - Pull a `rule:` subfield out of the policy rule API entity to encapsulate all the rule fields, keeping only ID and assigned subscribers at the top level
    - Use the new subfield (PolicyRuleConfig) as config type for configurator backend entities - no data migration needed as this and the legacy type are JSON-compatible
    - Update top-level policy handlers in V1 to support editing BNR-subscriber and policy rule-subscriber mappings
    - Update policy streamer to use new backend entities
    
    Reviewed By: andreilee
    
    Differential Revision: D18467327
    
    fbshipit-source-id: 9d353a097f2dfab2577b98b282b5b65033a3ac55

--- lte/cloud/go/plugin/models/policy_rule_swaggergen.go
+++ lte/cloud/go/plugin/models/policy_rule_swaggergen.go
@@ -6,7 +6,6 @@ package models
 // Editing this file might prove futile when you re-run the swagger generate command
 
 import (
-	"encoding/json"
 	"strconv"
 
 	strfmt "github.com/go-openapi/strfmt"
@@ -21,35 +20,15 @@ import (
 type PolicyRule struct {
 
 	// Subscribers which have been assigned this policy not as part of a base name
-	AssignedSubscribers []SubscriberID `json:"assigned_subscribers"`
-
-	// flow list
-	// Required: true
-	FlowList []*FlowDescription `json:"flow_list"`
+	AssignedSubscribers []SubscriberID `json:"assigned_subscribers,omitempty"`
 
 	// id
 	// Required: true
 	ID PolicyID `json:"id"`
 
-	// monitoring key
-	MonitoringKey string `json:"monitoring_key,omitempty"`
-
-	// priority
+	// rule
 	// Required: true
-	Priority *uint32 `json:"priority"`
-
-	// qos
-	Qos *FlowQos `json:"qos,omitempty"`
-
-	// rating group
-	RatingGroup uint32 `json:"rating_group,omitempty"`
-
-	// redirect
-	Redirect *RedirectInformation `json:"redirect,omitempty"`
-
-	// tracking type
-	// Enum: [ONLY_OCS ONLY_PCRF OCS_AND_PCRF NO_TRACKING]
-	TrackingType string `json:"tracking_type,omitempty"`
+	Rule *PolicyRuleConfig `json:"rule"`
 }
 
 // Validate validates this policy rule
@@ -60,27 +39,11 @@ func (m *PolicyRule) Validate(formats strfmt.Registry) error {
 		res = append(res, err)
 	}
 
-	if err := m.validateFlowList(formats); err != nil {
-		res = append(res, err)
-	}
-
 	if err := m.validateID(formats); err != nil {
 		res = append(res, err)
 	}
 
-	if err := m.validatePriority(formats); err != nil {
-		res = append(res, err)
-	}
-
-	if err := m.validateQos(formats); err != nil {
-		res = append(res, err)
-	}
-
-	if err := m.validateRedirect(formats); err != nil {
-		res = append(res, err)
-	}
-
-	if err := m.validateTrackingType(formats); err != nil {
+	if err := m.validateRule(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -110,31 +73,6 @@ func (m *PolicyRule) validateAssignedSubscribers(formats strfmt.Registry) error 
 	return nil
 }
 
-func (m *PolicyRule) validateFlowList(formats strfmt.Registry) error {
-
-	if err := validate.Required("flow_list", "body", m.FlowList); err != nil {
-		return err
-	}
-
-	for i := 0; i < len(m.FlowList); i++ {
-		if swag.IsZero(m.FlowList[i]) { // not required
-			continue
-		}
-
-		if m.FlowList[i] != nil {
-			if err := m.FlowList[i].Validate(formats); err != nil {
-				if ve, ok := err.(*errors.Validation); ok {
-					return ve.ValidateName("flow_list" + "." + strconv.Itoa(i))
-				}
-				return err
-			}
-		}
-
-	}
-
-	return nil
-}
-
 func (m *PolicyRule) validateID(formats strfmt.Registry) error {
 
 	if err := m.ID.Validate(formats); err != nil {
@@ -147,95 +85,19 @@ func (m *PolicyRule) validateID(formats strfmt.Registry) error {
 	return nil
 }
 
-func (m *PolicyRule) validatePriority(formats strfmt.Registry) error {
+func (m *PolicyRule) validateRule(formats strfmt.Registry) error {
 
-	if err := validate.Required("priority", "body", m.Priority); err != nil {
+	if err := validate.Required("rule", "body", m.Rule); err != nil {
 		return err
 	}
 
-	return nil
-}
-
-func (m *PolicyRule) validateQos(formats strfmt.Registry) error {
-
-	if swag.IsZero(m.Qos) { // not required
-		return nil
-	}
-
-	if m.Qos != nil {
-		if err := m.Qos.Validate(formats); err != nil {
+	if m.Rule != nil {
+		if err := m.Rule.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
-				return ve.ValidateName("qos")
+				return ve.ValidateName("rule")
 			}
 			return err
 		}
-	}
-
-	return nil
-}
-
-func (m *PolicyRule) validateRedirect(formats strfmt.Registry) error {
-
-	if swag.IsZero(m.Redirect) { // not required
-		return nil
-	}
-
-	if m.Redirect != nil {
-		if err := m.Redirect.Validate(formats); err != nil {
-			if ve, ok := err.(*errors.Validation); ok {
-				return ve.ValidateName("redirect")
-			}
-			return err
-		}
-	}
-
-	return nil
-}
-
-var policyRuleTypeTrackingTypePropEnum []interface{}
-
-func init() {
-	var res []string
-	if err := json.Unmarshal([]byte(`["ONLY_OCS","ONLY_PCRF","OCS_AND_PCRF","NO_TRACKING"]`), &res); err != nil {
-		panic(err)
-	}
-	for _, v := range res {
-		policyRuleTypeTrackingTypePropEnum = append(policyRuleTypeTrackingTypePropEnum, v)
-	}
-}
-
-const (
-
-	// PolicyRuleTrackingTypeONLYOCS captures enum value "ONLY_OCS"
-	PolicyRuleTrackingTypeONLYOCS string = "ONLY_OCS"
-
-	// PolicyRuleTrackingTypeONLYPCRF captures enum value "ONLY_PCRF"
-	PolicyRuleTrackingTypeONLYPCRF string = "ONLY_PCRF"
-
-	// PolicyRuleTrackingTypeOCSANDPCRF captures enum value "OCS_AND_PCRF"
-	PolicyRuleTrackingTypeOCSANDPCRF string = "OCS_AND_PCRF"
-
-	// PolicyRuleTrackingTypeNOTRACKING captures enum value "NO_TRACKING"
-	PolicyRuleTrackingTypeNOTRACKING string = "NO_TRACKING"
-)
-
-// prop value enum
-func (m *PolicyRule) validateTrackingTypeEnum(path, location string, value string) error {
-	if err := validate.Enum(path, location, value, policyRuleTypeTrackingTypePropEnum); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (m *PolicyRule) validateTrackingType(formats strfmt.Registry) error {
-
-	if swag.IsZero(m.TrackingType) { // not required
-		return nil
-	}
-
-	// value enum
-	if err := m.validateTrackingTypeEnum("tracking_type", "body", m.TrackingType); err != nil {
-		return err
 	}
 
 	return nil
