15637e9d7d38805d51f540998db853dba9c25bfe
jmanjuna <Jayashree.Manjunath@radisys.com>
2019-11-14 04:23:08 +0530

    S1APTESTER: Integrity and ciphering testcases modified

--- lte/gateway/python/integ_tests/s1aptests/test_attach_detach_EIA1.py
+++ lte/gateway/python/integ_tests/s1aptests/test_attach_detach_EIA1.py
@@ -34,39 +34,45 @@ class TestAttachDetachEIA1(unittest.TestCase):
                        s1ap_types.ueDetachType_t.
                        UE_SWITCHOFF_DETACH.value]
         wait_for_s1 = [True, False]
-        self._s1ap_wrapper.configUEDevice(num_ues)
+        req = self._s1ap_wrapper._sub_util.add_sub(num_ues)
 
         for i in range(num_ues):
-            req = self._s1ap_wrapper.ue_req
-            req.ueNwCap_pr.pres = 1
-            req.ueNwCap_pr.eea2_128 = 0
-            req.ueNwCap_pr.eea1_128 = 1
-            req.ueNwCap_pr.eea0 = 1
-            req.ueNwCap_pr.eia2_128 = 0
-            req.ueNwCap_pr.eia1_128 = 1
-            req.ueNwCap_pr.eia0 = 1
+            print("************************* UE device config for ue_id ",
+                  req[i].ue_id)
+            req[i].ueNwCap_pr.pres = 1
+            req[i].ueNwCap_pr.eea2_128 = 0
+            req[i].ueNwCap_pr.eea1_128 = 1
+            req[i].ueNwCap_pr.eea0 = 1
+            req[i].ueNwCap_pr.eia2_128 = 0
+            req[i].ueNwCap_pr.eia1_128 = 1
+            req[i].ueNwCap_pr.eia0 = 1
 
-            self._s1ap_wrapper._s1_util.issue_cmd(
-                    s1ap_types.tfwCmd.UE_CONFIG, req)
+            assert (self._s1ap_wrapper._s1_util.issue_cmd(
+                s1ap_types.tfwCmd.UE_CONFIG, req[i]) == 0)
             response = self._s1ap_wrapper._s1_util.get_response()
-            self.assertTrue(response,
-                            s1ap_types.tfwCmd.UE_CONFIG_COMPLETE_IND.value)
+            assert (s1ap_types.tfwCmd.UE_CONFIG_COMPLETE_IND.value ==
+                    response.msg_type)
+            self._s1ap_wrapper._configuredUes.append(req[i])
+        self._s1ap_wrapper.check_gw_health_after_ue_load()
 
+        # self._s1ap_wrapper.configUEDevice(num_ues)
+
+        for i in range(num_ues):
             print("************************* Running End to End attach for ",
-                  "UE id ", req.ue_id)
+                  "UE id ", req[i].ue_id)
             # Now actually complete the attach
             self._s1ap_wrapper._s1_util.attach(
-                req.ue_id, s1ap_types.tfwCmd.UE_END_TO_END_ATTACH_REQUEST,
+                req[i].ue_id, s1ap_types.tfwCmd.UE_END_TO_END_ATTACH_REQUEST,
                 s1ap_types.tfwCmd.UE_ATTACH_ACCEPT_IND,
                 s1ap_types.ueAttachAccept_t)
 
             # Wait on EMM Information from MME
             self._s1ap_wrapper._s1_util.receive_emm_info()
             print("************************* Running UE detach for UE id ",
-                  req.ue_id)
+                  req[i].ue_id)
             # Now detach the UE
             self._s1ap_wrapper.s1_util.detach(
-                req.ue_id, detach_type[i], wait_for_s1[i])
+                req[i].ue_id, detach_type[i], wait_for_s1[i])
 
 
 if __name__ == "__main__":
