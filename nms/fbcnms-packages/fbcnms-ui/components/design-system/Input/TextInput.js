06467e2b55dc9013fcd685504a4a2fd4af4ce106
Amit Arbel <arbelamit@fb.com>
2019-11-13 02:57:02 -0800

    Comments pane in Work Order datails - Designing improvments - phase 2
    Summary:
    1. Limiting the hight of the comments box.
    2. Replacing the "Send" button with "Press Enter to send" capability.
    3. Making the commentsBox take all the card witdth (by adding required support in ExpandingPanel) - will be used for CommentsLog-scrolling-sadow effect defined in design.
    
    {F221090685}
    
    Reviewed By: dlvhdr
    
    Differential Revision: D18250111
    
    fbshipit-source-id: 5d8ccdc5b4c0caae1000b3a48c53eebb040f99bb

--- nms/fbcnms-packages/fbcnms-ui/components/design-system/Input/TextInput.js
+++ nms/fbcnms-packages/fbcnms-ui/components/design-system/Input/TextInput.js
@@ -17,8 +17,21 @@ import InputContext from './InputContext';
 import classNames from 'classnames';
 import {withStyles} from '@material-ui/core/styles';
 
+export const KEYBOARD_KEYS = {
+  CODES: {
+    ENTER: 13,
+  },
+  MODIFIERS: {
+    SHIFT: 'shift',
+  },
+};
+
 const styles = ({symphony}) => ({
   root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  inputContainer: {
     padding: '0px 8px',
     border: `1px solid ${symphony.palette.D100}`,
     borderRadius: '4px',
@@ -65,6 +78,10 @@ const styles = ({symphony}) => ({
     marginRight: '7px',
     marginLeft: '4px',
   },
+  hint: {
+    color: symphony.palette.D200,
+    paddingTop: '4px',
+  },
 });
 
 type Props = {
@@ -77,9 +94,11 @@ type Props = {
   disabled?: boolean,
   hasError?: boolean,
   prefix?: React.Node,
+  hint?: string,
   onChange?: (e: SyntheticInputEvent<HTMLInputElement>) => void,
   onFocus?: () => void,
   onBlur?: () => void,
+  onEnterPressed?: (e: KeyboardEvent) => void,
 } & WithStyles<typeof styles>;
 
 type State = {
@@ -121,6 +140,15 @@ class TextInput extends React.Component<Props, State> {
     onChange && onChange(e);
   };
 
+  _onKeyDown = (e: KeyboardEvent) => {
+    if (e.keyCode !== KEYBOARD_KEYS.CODES.ENTER) {
+      return;
+    }
+
+    const {onEnterPressed} = this.props;
+    onEnterPressed && onEnterPressed(e);
+  };
+
   render() {
     const {
       classes,
@@ -129,38 +157,39 @@ class TextInput extends React.Component<Props, State> {
       disabled: disabledProp,
       prefix,
       value,
+      hint,
       ...rest
     } = this.props;
     const {hasFocus} = this.state;
     const disabled = disabledProp ? disabledProp : this.context.disabled;
     const hasError = hasErrorProp ? hasErrorProp : this.context.hasError;
     return (
-      <div
-        className={classNames(
-          classes.root,
-          {
+      <div className={classNames(classes.root, className)}>
+        <div
+          className={classNames(classes.inputContainer, {
             [classes.hasFocus]: hasFocus,
             [classes.disabled]: disabled,
             [classes.hasError]: hasError,
-          },
-          className,
-        )}>
-        {prefix && (
-          <div className={classes.prefix}>
-            <InputContext.Provider value={{disabled, value: value ?? ''}}>
-              {prefix}
-            </InputContext.Provider>
-          </div>
-        )}
-        <input
-          className={classes.input}
-          disabled={disabled}
-          onFocus={this._onInputFocused}
-          onBlur={this._onInputBlurred}
-          onChange={this._onChange}
-          value={value}
-          {...rest}
-        />
+          })}>
+          {prefix && (
+            <div className={classes.prefix}>
+              <InputContext.Provider value={{disabled, value: value ?? ''}}>
+                {prefix}
+              </InputContext.Provider>
+            </div>
+          )}
+          <input
+            className={classes.input}
+            disabled={disabled}
+            onFocus={this._onInputFocused}
+            onBlur={this._onInputBlurred}
+            onChange={this._onChange}
+            onKeyDown={this._onKeyDown}
+            value={value}
+            {...rest}
+          />
+        </div>
+        {hint && <div className={classes.hint}>{hint}</div>}
       </div>
     );
   }
