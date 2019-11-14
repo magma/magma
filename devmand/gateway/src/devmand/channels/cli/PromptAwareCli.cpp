61e90ec2164ad96eeec11736e1f20a5a1ccbd52b
Maros Marsalek <mmarsalek@frinx.io>
2019-11-14 16:31:35 +0100

    Implement prompt aware Cli
    This implementation of Cli is able to resolve prompt
    and use the prompt to use as a delimiter when invoking commands.
    
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

---
+++ devmand/gateway/src/devmand/channels/cli/PromptAwareCli.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#include <devmand/channels/cli/Command.h>
+#include <devmand/channels/cli/PromptAwareCli.h>
+#include <devmand/channels/cli/SshSessionAsync.h>
+#include <folly/executors/IOThreadPoolExecutor.h>
+
+using devmand::channels::cli::CliInitializer;
+using devmand::channels::cli::Command;
+using devmand::channels::cli::PromptAwareCli;
+using devmand::channels::cli::PromptResolver;
+using std::string;
+
+namespace devmand {
+namespace channels {
+namespace cli {
+
+void PromptAwareCli::resolvePrompt() {
+  this->prompt = cliFlavour->resolver->resolvePrompt(session, cliFlavour->newline);
+}
+
+void PromptAwareCli::initializeCli() {
+  cliFlavour->initializer->initialize(session);
+}
+
+folly::Future<string> PromptAwareCli::executeAndRead(const Command& cmd) {
+  const string& command = cmd.toString();
+
+  return session->write(command)
+      .thenValue([=](...) { return session->readUntilOutput(command); })
+      .thenValue([=](const string & output) {
+          auto returnOutputParameter = [output] (...) { return output; };
+          return session->write(cliFlavour->newline).thenValue(returnOutputParameter);
+      })
+      .thenValue([=](const string & output) {
+          auto concatOutputParameter =  [output] (const string & readUntilOutput) {
+              return output + readUntilOutput;
+          };
+          return session->readUntilOutput(prompt).thenValue(concatOutputParameter);
+      });
+}
+
+PromptAwareCli::PromptAwareCli(
+    shared_ptr<SshSessionAsync> _session,
+    shared_ptr<CliFlavour> _cliFlavour)
+    : session(_session), cliFlavour(_cliFlavour) {}
+
+void PromptAwareCli::init( //TODO remove
+    const string hostname,
+    const int port,
+    const string username,
+    const string password) {
+  session->openShell(hostname, port, username, password).get();
+}
+
+folly::Future<std::string> PromptAwareCli::execute(const Command &cmd) {
+  const string& command = cmd.toString();
+  return session->write(command)
+       .thenValue([=](...) { return session->readUntilOutput(command); })
+       .thenValue([=](const string & output) {
+           return session->write(cliFlavour->newline)
+                   .thenValue([output, command] (...) { return output + command; });
+       });
+}
+} // namespace cli
+} // namespace channels
+}
---
+++ devmand/gateway/src/devmand/channels/cli/PromptAwareCli.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#pragma once
+
+#include <devmand/channels/cli/Cli.h>
+#include <devmand/channels/cli/CliFlavour.h>
+#include <devmand/channels/cli/SshSessionAsync.h>
+#include <folly/futures/Future.h>
+
+namespace devmand {
+namespace channels {
+namespace cli {
+
+using devmand::channels::cli::CliInitializer;
+using devmand::channels::cli::PromptResolver;
+using devmand::channels::cli::sshsession::SshSessionAsync;
+using std::shared_ptr;
+using std::string;
+
+class PromptAwareCli : public Cli {
+ private:
+  shared_ptr<SshSessionAsync> session;
+  shared_ptr<CliFlavour> cliFlavour;
+
+ public:
+  PromptAwareCli(shared_ptr<SshSessionAsync> session, shared_ptr<CliFlavour> cliFlavour);
+
+  string prompt;
+
+ public:
+  void init(
+      const string hostname,
+      const int port,
+      const string username,
+      const string password);
+  void resolvePrompt();
+  void initializeCli();
+  folly::Future<std::string> executeAndRead(const Command& cmd);
+
+  folly::Future<std::string> execute(const Command& cmd);
+};
+
+} // namespace cli
+} // namespace channels
+} // namespace devmand
