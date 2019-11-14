3c21c7c13b6b8801d10d679b62d6df38aadb9d16
Maros Marsalek <mmarsalek@frinx.io>
2019-11-14 16:44:53 +0100

    Add test utils for Cli
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

---
+++ devmand/gateway/src/devmand/test/cli/utils/Log.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#pragma once
+
+#define LOG_WITH_GLOG
+
+#include <magma_logging.h>
+#include <atomic>
+
+namespace devmand {
+namespace test {
+namespace utils {
+namespace log {
+
+using namespace std;
+
+extern atomic_bool loggingInitialized;
+
+extern void initLog();
+
+} // namespace ssh
+} // namespace utils
+} // namespace test
+} // namespace devmand
---
+++ devmand/gateway/src/devmand/test/cli/utils/Ssh.h
@@ -0,0 +1,77 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#pragma once
+
+#define LOG_WITH_GLOG
+
+#include <folly/executors/CPUThreadPoolExecutor.h>
+#include <folly/futures/Future.h>
+#include <libssh/callbacks.h>
+#include <libssh/libssh.h>
+#include <libssh/server.h>
+#include <magma_logging.h>
+#include <unistd.h>
+
+namespace devmand {
+namespace test {
+namespace utils {
+namespace ssh {
+
+using namespace std;
+using namespace folly;
+
+extern atomic_bool sshInitialized;
+
+extern void initSsh();
+
+extern shared_ptr<CPUThreadPoolExecutor> testExecutor;
+
+struct server {
+  string id;
+  ssh_bind sshbind = nullptr;
+  ssh_session session = nullptr;
+  Future<Unit> serverFuture;
+
+  void close() {
+    MLOG(MDEBUG) << "Closing server: " << id;
+    if (session != nullptr) {
+      if (ssh_is_connected(session) == 1) {
+        ssh_disconnect(session);
+      }
+      ssh_free(session);
+      session = nullptr;
+    }
+    if (sshbind != nullptr) {
+      shutdown(ssh_bind_get_fd(sshbind), SHUT_RDWR);
+
+      // Make sure the server thread finished before calling free
+      move(serverFuture).get();
+
+      ssh_bind_free(sshbind);
+      sshbind = nullptr;
+    }
+  }
+};
+
+/**
+ * Creates a disposable (use only once !) ssh server.
+ * Blocks 1 executor thread.
+ *
+ * Can be closed by closing return value or by sending ^C.
+ */
+extern shared_ptr<server> startSshServer(
+    shared_ptr<CPUThreadPoolExecutor> executor = testExecutor,
+    string address = "0.0.0.0",
+    uint port = 9999,
+    string rsaKey = "/etc/ssh/ssh_host_rsa_key",
+    string prompt = "PROMPT>");
+
+} // namespace ssh
+} // namespace utils
+} // namespace test
+} // namespace devmand
