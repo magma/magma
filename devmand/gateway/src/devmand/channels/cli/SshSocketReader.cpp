b73a52f6b9e4902704988898cbc7e1c394e8a44a
Maros Marsalek <mmarsalek@frinx.io>
2019-11-14 16:29:35 +0100

    Async ssh session implementation
    Ssh session implemented using libssh
    Async non-blocking adapter implemented using libevent
    
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

---
+++ devmand/gateway/src/devmand/channels/cli/SshSocketReader.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2019-present, Facebook, Inc.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. An additional grant
+// of patent rights can be found in the PATENTS file in the same directory.
+
+#include <devmand/channels/cli/SshSocketReader.h>
+#include <devmand/channels/cli/SshSessionAsync.h>
+#include <event2/event.h>
+#include <thread>
+#include <chrono>
+
+using devmand::channels::cli::sshsession::SshSessionAsync;
+
+void sshReadNotificationThread(struct event_base *base);
+void sshReadNotificationThread(struct event_base *base) {
+    while (true) {
+        int rv = event_base_dispatch(base);
+        if (rv == 1) {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            continue;
+        }
+        else {
+            const char *error = "event_base_dispatch catastrophic failure, terminating";
+            MLOG(MERROR) << error << " returned value: " << rv;
+            throw std::runtime_error(error);
+        }
+    }
+}
+
+devmand::channels::cli::SshSocketReader::SshSocketReader() {
+    base = event_base_new();
+    std::thread notificationThread(sshReadNotificationThread, base);
+    notificationThread.detach();
+}
+
+struct event * devmand::channels::cli::SshSocketReader::addSshReader(event_callback_fn callbackFn, socket_t fd, void *ptr) {
+    struct event *event_on_heap = event_new(this->base, fd, EV_READ|EV_PERSIST, callbackFn, ptr);
+    event_add(event_on_heap, nullptr);
+    return event_on_heap;
+}
+
+devmand::channels::cli::SshSocketReader::~SshSocketReader() {
+    event_base_free(base);
+}
+
+devmand::channels::cli::SshSocketReader &devmand::channels::cli::SshSocketReader::getInstance() {
+    static SshSocketReader instance;
+    return instance;
+}
