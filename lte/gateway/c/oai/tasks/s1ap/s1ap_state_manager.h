d037311ca8749745d575d1f6dd5eccaacb15f32b
Alejandro Rodriguez Aragonez <alexrod@fb.com>
2019-11-22 09:54:14 -0800

    Encapsulating s1ap state conversion functions (#903)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/903
    
    This diff updates s1ap task to encapsulate related state functions into its own state manager, state converter classes, to follow pattern of MME/NAS and SPGW tasks.
    
    - Adding `S1apStateManager` class
    - Adding `S1apStateConverter` class
    - Updating some s1ap mme handler functions, refactoring code
    - Moving `s1ap_state.proto` file from `lte/protos/` to `lte/gateway/c/oai/protos`
    - Moving db related functions from spgw state to spgw state manager
    
    Reviewed By: ssanadhya
    
    Differential Revision: D18010640
    
    fbshipit-source-id: 196998b74a8558f03cef799936fb041523af879c

---
+++ lte/gateway/c/oai/tasks/s1ap/s1ap_state_manager.h
@@ -0,0 +1,90 @@
+/*
+ *
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the Apache License, Version 2.0  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "mme_config.h"
+#include "s1ap_types.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "state_manager.h"
+#include "s1ap_state_converter.h"
+
+using magma::lte::gateway::s1ap::S1apState;
+
+namespace {
+constexpr char S1AP_STATE_TABLE[] = "s1ap_state";
+}
+
+namespace magma {
+namespace lte {
+
+/**
+ * S1apStateManager is a thread safe singleton class that contains functions
+ * to maintain S1AP task state, allocating and freeing related state structs.
+ */
+class S1apStateManager :
+  public StateManager<s1ap_state_t, S1apState, S1apStateConverter> {
+ public:
+  /**
+   * Returns an instance of S1apStateManager, guaranteed to be thread safe and
+   * initialized only once.
+   * @return S1apStateManager instance
+   */
+  static S1apStateManager& getInstance();
+
+  /**
+   * Function to initialize member variables
+   * @param mme_config mme_config_t struct
+   */
+  void init(uint32_t max_ues, uint32_t max_enbs, bool use_stateless);
+
+  // Copy constructor and assignment operator are marked as deleted functions
+  S1apStateManager(S1apStateManager const&) = delete;
+  S1apStateManager& operator=(S1apStateManager const&) = delete;
+
+  /**
+   * Frees all memory allocated on s1ap_state cache struct
+   */
+  void free_state() override;
+
+ private:
+  S1apStateManager();
+  ~S1apStateManager();
+
+  /**
+   * Allocates new s1ap_state_t struct and its properties
+   */
+  void create_state() override;
+
+  uint32_t max_ues_;
+  uint32_t max_enbs_;
+};
+} // namespace lte
+} // namespace magma
