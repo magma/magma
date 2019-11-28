273839a2bf479aaf3998974ba28947e6ac0b1b46
Alan Wang <alanw@fb.com>
2019-11-27 18:03:43 -0800

    Additional test coverage for DevicesUtils
    Summary:
    brings coverage of DevicesUtils.js up to 84% 	42/50
    
    I was originally not going to bother testing this function, but it was not very difficult to set up so I did it anyway
    
    Reviewed By: rckclmbr
    
    Differential Revision: D18647064
    
    fbshipit-source-id: 403eb7fcfa520691a66157e8cb6ee9e0771db757

--- symphony/app/fbcnms-projects/magmalte/app/components/devices/__tests__/DevicesUtils-test.js
+++ symphony/app/fbcnms-projects/magmalte/app/components/devices/__tests__/DevicesUtils-test.js
@@ -8,8 +8,11 @@
  * @format
  */
 
-import {RAW_AGENT} from '../test/DevicesMock';
-import {buildDevicesAgentFromPayload} from '../DevicesUtils';
+import {RAW_AGENT, RAW_DEVICES} from '../test/DevicesMock';
+import {
+  buildDevicesAgentFromPayload,
+  mergeAgentsDevices,
+} from '../DevicesUtils';
 
 it('disect agent', () => {
   const agent = buildDevicesAgentFromPayload(
@@ -29,5 +32,110 @@ it('disect agent', () => {
     up: true,
     version: 'Not Reported',
     rawAgent: RAW_AGENT,
+  });
+});
+
+it('test merging devices and agents calls', () => {
+  const agent = buildDevicesAgentFromPayload(
+    RAW_AGENT,
+    RAW_AGENT.status?.checkin_time,
+  );
+
+  if (agent.devmand_config?.managed_devices) {
+    const managed_devices = agent.devmand_config?.managed_devices || [];
+    managed_devices.push('nonexistdevice_but_ok_to_ignore');
+  }
+
+  const devicesAgents = mergeAgentsDevices([agent], Object.keys(RAW_DEVICES));
+
+  expect(devicesAgents).toEqual({
+    ens_switch_1: {
+      id: 'ens_switch_1',
+      agentIds: [],
+      config: null,
+      statusAgentId: null,
+      status: null,
+    },
+    localhost_snmpd: {
+      id: 'localhost_snmpd',
+      agentIds: [],
+      config: null,
+      statusAgentId: null,
+      status: null,
+    },
+    mikrotik: {
+      id: 'mikrotik',
+      agentIds: [],
+      config: null,
+      statusAgentId: null,
+      status: null,
+    },
+    ping_fb_dns_from_lab: {
+      id: 'ping_fb_dns_from_lab',
+      agentIds: ['fbbosfbcdockerengine'],
+      config: null,
+      statusAgentId: 'fbbosfbcdockerengine',
+      status: {
+        'fbc-symphony-device:system': {
+          'geo-location': {
+            'reference-frame': {
+              'astronomical-body': 'earth',
+              'geodetic-system': {'geodetic-datum': 'wgs-84'},
+            },
+            latitude: 0,
+            longitude: 0,
+            height: 0,
+          },
+          latencies: {
+            latency: [{type: 'ping', src: 'agent', dst: 'device', rtt: 11797}],
+          },
+          status: 'UP',
+        },
+      },
+    },
+    ping_fb_dns_ken_laptop: {
+      id: 'ping_fb_dns_ken_laptop',
+      agentIds: [],
+      config: null,
+      statusAgentId: null,
+      status: null,
+    },
+    ping_google_ipv6: {
+      id: 'ping_google_ipv6',
+      agentIds: ['fbbosfbcdockerengine'],
+      config: null,
+      statusAgentId: 'fbbosfbcdockerengine',
+      status: {
+        'fbc-symphony-device:system': {
+          status: 'UP',
+          latencies: {
+            latency: [{rtt: 12296, dst: 'device', src: 'agent', type: 'ping'}],
+          },
+          'geo-location': {
+            height: 0,
+            longitude: 0,
+            latitude: 0,
+            'reference-frame': {
+              'geodetic-system': {'geodetic-datum': 'wgs-84'},
+              'astronomical-body': 'earth',
+            },
+          },
+        },
+      },
+    },
+    ping_google_ipv6_ken_laptop: {
+      id: 'ping_google_ipv6_ken_laptop',
+      agentIds: [],
+      config: null,
+      statusAgentId: null,
+      status: null,
+    },
+    ubnt: {
+      id: 'ubnt',
+      agentIds: [],
+      config: null,
+      statusAgentId: null,
+      status: null,
+    },
   });
 });
