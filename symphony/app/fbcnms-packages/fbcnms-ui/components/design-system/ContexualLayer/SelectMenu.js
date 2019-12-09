1eaf214ae8573b4e564c4d6da7c45a16f775a3fc
Dolev Hadar <dolevh@fb.com>
2019-12-09 04:51:56 -0800

    Select menu
    Summary: Menu to select an item
    
    Reviewed By: noamsch
    
    Differential Revision: D18829967
    
    fbshipit-source-id: 47b2e6bfcc7a98c2dc12d585cb76a96730bcf819

---
+++ symphony/app/fbcnms-packages/fbcnms-ui/components/design-system/ContexualLayer/SelectMenu.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import * as React from 'react';
+import CheckIcon from '@material-ui/icons/Check';
+import Text from '../Text';
+import classNames from 'classnames';
+import symphony from '../../../theme/symphony';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: symphony.palette.white,
+    boxShadow: symphony.shadows.DP1,
+    borderRadius: '4px',
+  },
+  option: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '6px 8px',
+    cursor: 'pointer',
+    '&:hover': {
+      backgroundColor: symphony.palette.B50,
+    },
+  },
+  label: {
+    flexGrow: 1,
+  },
+  checkIcon: {
+    marginLeft: '6px',
+    color: symphony.palette.primary,
+  },
+});
+
+export type OptionProps<TValue> = {
+  label: React.Node,
+  value: TValue,
+};
+
+type Props<TValue> = {
+  className?: string,
+  onChange: (value: TValue) => void | (() => void),
+  options: Array<OptionProps<TValue>>,
+  selectedValue?: ?TValue,
+};
+
+const SelectMenu = <TValue>({
+  className,
+  options,
+  onChange,
+  selectedValue,
+}: Props<TValue>) => {
+  const classes = useStyles();
+  return (
+    <div className={classNames(classes.root, className)}>
+      {options.map(option => (
+        <div className={classes.option} onClick={() => onChange(option.value)}>
+          {typeof option.label === 'string' ? (
+            <Text className={classes.label} variant="body2">
+              {option.label}
+            </Text>
+          ) : (
+            option.label
+          )}
+          {option.value === selectedValue && (
+            <CheckIcon className={classes.checkIcon} fontSize="small" />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SelectMenu;
