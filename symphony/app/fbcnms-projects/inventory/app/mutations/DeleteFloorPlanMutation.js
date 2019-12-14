dcf9f551dcea9889f1267b755cd619bfae7b5c2e
Tudor Cirstea <tcirstea@fb.com>
2019-12-13 16:09:58 -0800

    Fix mutation lint
    Summary: Following up from D18942070, there will be some stragglers until those working on features notice the new pattern.
    
    Reviewed By: rckclmbr
    
    Differential Revision: D19052206
    
    fbshipit-source-id: 3efb9121147247749972f13616a707ef84368906

--- symphony/app/fbcnms-projects/inventory/app/mutations/DeleteFloorPlanMutation.js
+++ symphony/app/fbcnms-projects/inventory/app/mutations/DeleteFloorPlanMutation.js
@@ -16,6 +16,7 @@ import type {
   DeleteFloorPlanMutationVariables,
 } from './__generated__/DeleteFloorPlanMutation.graphql';
 import type {MutationCallbacks} from './MutationCallbacks.js';
+import type {StoreUpdater} from '../common/RelayEnvironment';
 
 const mutation = graphql`
   mutation DeleteFloorPlanMutation($id: ID!) {
@@ -26,7 +27,7 @@ const mutation = graphql`
 export default (
   variables: DeleteFloorPlanMutationVariables,
   callbacks?: MutationCallbacks<DeleteFloorPlanMutationResponse>,
-  updater?: (store: any) => void,
+  updater?: StoreUpdater,
 ) => {
   const {onCompleted, onError} = callbacks ? callbacks : {};
   commitMutation<DeleteFloorPlanMutation>(RelayEnvironment, {
