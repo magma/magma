4864c1177b095c0c93eba1467737cdfb3f2f7d77
Shruti Sanadhya <ssanadhya@fb.com>
2019-12-17 08:46:59 -0800

    Making ovs flows persists across MME restart
    Summary:
    The MME process always cleans up all the OVS when the switch connects to the
    controller. This change adds a check that flows are only removed when the
    `persist_state` flag is false. Instead of triggering the flow removal through
    BaseApplication, we can do it through the OpenflowController object itself.
    
    Also fixed some formatting.
    
    Reviewed By: koolzz
    
    Differential Revision: D18981364
    
    fbshipit-source-id: 7aa3b63247781c55eceef3214f8e09a6bd80f948

--- lte/gateway/c/oai/tasks/mme_app/mme_app_context.c
+++ lte/gateway/c/oai/tasks/mme_app/mme_app_context.c
@@ -392,8 +392,6 @@ void mme_app_ue_context_free_content(ue_mm_context_t *const ue_context_p)
       ue_context_p->sgs_context, ue_context_p->emm_context._imsi64);
     free_wrapper((void **) &(ue_context_p->sgs_context));
   }
-  // Release emm and esm context
-  _clear_emm_ctxt(&ue_context_p->emm_context);
   ue_context_p->ue_context_rel_cause = S1AP_INVALID_CAUSE;
 
   ue_context_p->send_ue_purge_request = false;
@@ -969,7 +967,8 @@ void mme_remove_ue_context(
 
   if (!lock_ue_contexts(ue_context_p)) {
     mme_app_ue_context_free_content(ue_context_p);
-
+    // Release emm and esm context
+    _clear_emm_ctxt(&ue_context_p->emm_context);
     // IMSI
     if (ue_context_p->emm_context._imsi64) {
       hash_rc = hashtable_uint64_ts_remove(
