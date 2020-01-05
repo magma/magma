a674164ed330c23a05fccfee49204929f65cb320
Ariel Mashraki <mashraki@fb.com>
2020-01-05 06:38:46 -0800

    symp/graph: add read-only user
    Summary: Pull Request resolved: https://github.com/facebookincubator/symphony/pull/204
    
    Reviewed By: alexsn
    
    Differential Revision: D19284873
    
    fbshipit-source-id: 40beb9ff669253759b168f223dc2cc055f0d4ed4

---
+++ symphony/graph/ent/readonly.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2004-present Facebook All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Code generated (@generated) by entc, DO NOT EDIT.
+package ent
+
+import (
+	"context"
+
+	"github.com/facebookincubator/ent/dialect"
+	"github.com/facebookincubator/symphony/graph/ent/migrate"
+)
+
+// ReadOnly returns a new readonly-client.
+//
+//	client := client.ReadOnly()
+//
+func (c *Client) ReadOnly() *Client {
+	cfg := config{driver: &readonly{Driver: c.driver}, log: c.log}
+	return &Client{
+		config:                      cfg,
+		Schema:                      migrate.NewSchema(cfg.driver),
+		ActionsRule:                 NewActionsRuleClient(cfg),
+		CheckListItem:               NewCheckListItemClient(cfg),
+		CheckListItemDefinition:     NewCheckListItemDefinitionClient(cfg),
+		Comment:                     NewCommentClient(cfg),
+		Customer:                    NewCustomerClient(cfg),
+		Equipment:                   NewEquipmentClient(cfg),
+		EquipmentCategory:           NewEquipmentCategoryClient(cfg),
+		EquipmentPort:               NewEquipmentPortClient(cfg),
+		EquipmentPortDefinition:     NewEquipmentPortDefinitionClient(cfg),
+		EquipmentPortType:           NewEquipmentPortTypeClient(cfg),
+		EquipmentPosition:           NewEquipmentPositionClient(cfg),
+		EquipmentPositionDefinition: NewEquipmentPositionDefinitionClient(cfg),
+		EquipmentType:               NewEquipmentTypeClient(cfg),
+		File:                        NewFileClient(cfg),
+		FloorPlan:                   NewFloorPlanClient(cfg),
+		FloorPlanReferencePoint:     NewFloorPlanReferencePointClient(cfg),
+		FloorPlanScale:              NewFloorPlanScaleClient(cfg),
+		Link:                        NewLinkClient(cfg),
+		Location:                    NewLocationClient(cfg),
+		LocationType:                NewLocationTypeClient(cfg),
+		Project:                     NewProjectClient(cfg),
+		ProjectType:                 NewProjectTypeClient(cfg),
+		Property:                    NewPropertyClient(cfg),
+		PropertyType:                NewPropertyTypeClient(cfg),
+		Service:                     NewServiceClient(cfg),
+		ServiceEndpoint:             NewServiceEndpointClient(cfg),
+		ServiceType:                 NewServiceTypeClient(cfg),
+		Survey:                      NewSurveyClient(cfg),
+		SurveyCellScan:              NewSurveyCellScanClient(cfg),
+		SurveyQuestion:              NewSurveyQuestionClient(cfg),
+		SurveyTemplateCategory:      NewSurveyTemplateCategoryClient(cfg),
+		SurveyTemplateQuestion:      NewSurveyTemplateQuestionClient(cfg),
+		SurveyWiFiScan:              NewSurveyWiFiScanClient(cfg),
+		Technician:                  NewTechnicianClient(cfg),
+		WorkOrder:                   NewWorkOrderClient(cfg),
+		WorkOrderDefinition:         NewWorkOrderDefinitionClient(cfg),
+		WorkOrderType:               NewWorkOrderTypeClient(cfg),
+	}
+}
+
+// ErrReadOnly returns when a readonly user tries to execute a write operation.
+var ErrReadOnly = &PermissionError{cause: "permission denied: read-only user"}
+
+// PermissionError represents a permission denied error.
+type PermissionError struct {
+	cause string
+}
+
+func (e PermissionError) Error() string { return e.cause }
+
+type readonly struct {
+	dialect.Driver
+}
+
+func (r *readonly) Exec(context.Context, string, interface{}, interface{}) error {
+	return ErrReadOnly
+}
+
+func (r *readonly) Tx(context.Context) (dialect.Tx, error) {
+	return nil, ErrReadOnly
+}
