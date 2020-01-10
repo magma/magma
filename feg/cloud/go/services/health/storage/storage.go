f3f5546c43c0930beeecbdafd0d17fe1ca7f6564
Michael Germano <mgermano@fb.com>
2020-01-10 15:17:37 -0800

    Migrate feg health service to use blobstore (#1050)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1050
    
    Frequent deadlock errors are occuring with the health
    service when running with Active/Standby fegs and using mariadb.
    This diff updates the orc8r health service to use blobstore
    rather than the legacy datastore.api implementation.
    
    Reviewed By: xjtian
    
    Differential Revision: D19036426
    
    fbshipit-source-id: 38e263d1b8dd2a7db140bd078a51b561ecc77bba

--- feg/cloud/go/services/health/storage/storage.go
+++ feg/cloud/go/services/health/storage/storage.go
@@ -32,3 +32,16 @@ type ClusterStorage interface {
 
 	DoesKeyExist(networkID string, clusterID string) (bool, error)
 }
+
+// HealthBlobstore defines a storage interface for the health service. This
+// interface defines create/update and read functionality while abstracting
+// away any underlying storage transaction mechanics for clients.
+type HealthBlobstore interface {
+	GetHealth(networkID string, gatewayID string) (*protos.HealthStats, error)
+
+	UpdateHealth(networkID string, gatewayID string, health *protos.HealthStats) error
+
+	GetClusterState(networkID string, clusterID string) (*protos.ClusterState, error)
+
+	UpdateClusterState(networkID string, clusterID string, logicalID string) error
+}
