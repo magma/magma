752854eb86ba4681203baf0268d25e3a3abfc0eb
Andrei Lee <andreilee@fb.com>
2020-01-17 14:20:11 -0800

    Add streamer callback functionality for receiving updates on IMSI -> policy mappings
    Differential Revision: D19167401
    
    fbshipit-source-id: e2d2795880ec19389f7cde9f87e18e9a9c51d48c

--- lte/gateway/python/magma/policydb/main.py
+++ lte/gateway/python/magma/policydb/main.py
@@ -8,17 +8,18 @@ of patent rights can be found in the PATENTS file in the same directory.
 """
 
 import logging
-
 from lte.protos.mconfig import mconfigs_pb2
+from lte.protos.session_manager_pb2_grpc import SessionProxyResponderStub
 from lte.protos.subscriberdb_pb2_grpc import SubscriberDBStub
 from magma.common.service import MagmaService
 from magma.common.service_registry import ServiceRegistry
 from magma.common.streamer import StreamerClient
 from magma.policydb.rpc_servicer import SessionRpcServicer
-
+from magma.policydb.basename_store import BaseNameDict
+from magma.policydb.reauth_handler import ReAuthHandler
+from magma.policydb.rule_map_store import RuleAssignmentsDict
 from .streamer_callback import PolicyDBStreamerCallback, \
-    RuleMappingsStreamerCallback, BaseNamesStreamerCallback
-from .basename_store import BaseNameDict
+    RuleMappingsStreamerCallback
 
 
 def main():
@@ -32,13 +33,22 @@ def main():
     session_servicer = SessionRpcServicer(service.mconfig, subscriberdb_stub)
     session_servicer.add_to_server(service.rpc_server)
 
+    assignments_dict = RuleAssignmentsDict()
+    basenames_dict = BaseNameDict()
+    sessiond_chan = ServiceRegistry.get_rpc_channel('sessiond',
+                                                    ServiceRegistry.LOCAL)
+    sessiond_stub = SessionProxyResponderStub(sessiond_chan)
+    reauth_handler = ReAuthHandler(assignments_dict, sessiond_stub)
+
     # Start a background thread to stream updates from the cloud
     if service.config['enable_streaming']:
         stream = StreamerClient(
             {
                 'policydb': PolicyDBStreamerCallback(),
-                'rule_mappings': RuleMappingsStreamerCallback(),
-                'base_names': BaseNamesStreamerCallback(BaseNameDict()),
+                'rule_mappings': RuleMappingsStreamerCallback(reauth_handler,
+                                                              basenames_dict,
+                                                              assignments_dict),
+
             },
             service.loop,
         )
