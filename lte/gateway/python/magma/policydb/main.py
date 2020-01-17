9f9a4042d540b753624cebf0e826085e787f8fc8
Andrei Lee <andreilee@fb.com>
2020-01-17 14:20:36 -0800

    Add PolicyDB service for local control of policy installation
    Differential Revision: D19172469
    
    fbshipit-source-id: 26a1353c233585591c5a8887e3330514a979fcd0

--- lte/gateway/python/magma/policydb/main.py
+++ lte/gateway/python/magma/policydb/main.py
@@ -10,14 +10,16 @@ of patent rights can be found in the PATENTS file in the same directory.
 import logging
 from lte.protos.mconfig import mconfigs_pb2
 from lte.protos.session_manager_pb2_grpc import SessionProxyResponderStub
-from lte.protos.subscriberdb_pb2_grpc import SubscriberDBStub
+from lte.protos.subscriberdb_pb2_grpc import SubscriberDBStub,\
+    PolicyAssignmentControllerStub
 from magma.common.service import MagmaService
 from magma.common.service_registry import ServiceRegistry
 from magma.common.streamer import StreamerClient
-from magma.policydb.rpc_servicer import SessionRpcServicer
 from magma.policydb.basename_store import BaseNameDict
 from magma.policydb.reauth_handler import ReAuthHandler
 from magma.policydb.rule_map_store import RuleAssignmentsDict
+from magma.policydb.servicers.policy_servicer import PolicyRpcServicer
+from magma.policydb.servicers.session_servicer import SessionRpcServicer
 from .streamer_callback import PolicyDBStreamerCallback, \
     RuleMappingsStreamerCallback
 
@@ -26,6 +28,13 @@ def main():
     """ main() for subscriberdb """
     service = MagmaService('policydb', mconfigs_pb2.PolicyDB())
 
+    assignments_dict = RuleAssignmentsDict()
+    basenames_dict = BaseNameDict()
+    sessiond_chan = ServiceRegistry.get_rpc_channel('sessiond',
+                                                    ServiceRegistry.LOCAL)
+    sessiond_stub = SessionProxyResponderStub(sessiond_chan)
+    reauth_handler = ReAuthHandler(assignments_dict, sessiond_stub)
+
     # Add all servicers to the server
     chan = ServiceRegistry.get_rpc_channel('subscriberdb',
                                            ServiceRegistry.LOCAL)
@@ -33,12 +42,12 @@ def main():
     session_servicer = SessionRpcServicer(service.mconfig, subscriberdb_stub)
     session_servicer.add_to_server(service.rpc_server)
 
-    assignments_dict = RuleAssignmentsDict()
-    basenames_dict = BaseNameDict()
-    sessiond_chan = ServiceRegistry.get_rpc_channel('sessiond',
-                                                    ServiceRegistry.LOCAL)
-    sessiond_stub = SessionProxyResponderStub(sessiond_chan)
-    reauth_handler = ReAuthHandler(assignments_dict, sessiond_stub)
+    orc8r_chan = ServiceRegistry.get_rpc_channel('subscriberdb',
+                                                 ServiceRegistry.CLOUD)
+    policy_stub = PolicyAssignmentControllerStub(orc8r_chan)
+    policy_servicer = PolicyRpcServicer(reauth_handler, basenames_dict,
+                                        policy_stub)
+    policy_servicer.add_to_server(service.rpc_server)
 
     # Start a background thread to stream updates from the cloud
     if service.config['enable_streaming']:
