41b128cb3b0f33b8253ec2d166d03e404e4c9b09
Murtadha Al-Tameemi <murtadha@fb.com>
2020-01-16 19:29:06 -0800

    Fix issue with gateway status
    Summary: Not sure this is the best way to fix this, we basically can't compute the status on the fly each time, so need to compute it when we fetch the data from the server
    
    Reviewed By: rckclmbr
    
    Differential Revision: D19401561
    
    fbshipit-source-id: cfa9e58bf64422b8014dd6477a2c2504468c2acd

---
+++ symphony/app/fbcnms-packages/fbcnms-ui/hooks/useInterval.js
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import {useEffect, useRef} from 'react';
+
+// from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export default function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef<?() => void>();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current && savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
