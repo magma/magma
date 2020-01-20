a995098ec0628c79289994be43eae2530743b226
Ido Shveki <idoshveki@fb.com>
2020-01-20 03:21:06 -0800

    integration tests (#218)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/218
    
    Added test that
    - Adds links through ent.
    - Export them.
    - Modifies the file
    - Re-Imports them
    
    Reviewed By: naor9991
    
    Differential Revision: D19410451
    
    fbshipit-source-id: dd547736ed2e1a8abdf29ee8d02cc7c6f11410d5

--- symphony/graph/exporter/link_export_import_match_test.go
+++ symphony/graph/exporter/link_export_import_match_test.go
@@ -6,17 +6,19 @@ package exporter
 
 import (
 	"bytes"
+	"context"
 	"encoding/csv"
 	"io"
 	"mime/multipart"
 	"strings"
 	"testing"
 
+	"github.com/facebookincubator/symphony/graph/ent/equipment"
 	"github.com/facebookincubator/symphony/graph/importer"
 	"github.com/stretchr/testify/require"
 )
 
-func writeModifiedLinksCSV(t *testing.T, r *csv.Reader) (*bytes.Buffer, string) {
+func writeModifiedLinksCSV(t *testing.T, r *csv.Reader, method method) (*bytes.Buffer, string) {
 	var newLine []string
 	var lines = make([][]string, 3)
 	var buf bytes.Buffer
@@ -35,11 +37,18 @@ func writeModifiedLinksCSV(t *testing.T, r *csv.Reader) (*bytes.Buffer, string) 
 		if i == 0 {
 			lines[0] = line
 		} else {
-			newLine = line
-			if line[1] == portName1 {
-				newLine[26] = "new-prop-value"
-				newLine[27] = "true"
-				newLine[28] = "10"
+			switch method {
+			case MethodAdd:
+				newLine = append([]string{""}, line[1:]...)
+			case MethodEdit:
+				newLine = line
+				if line[1] == portName1 {
+					newLine[26] = "new-prop-value"
+					newLine[27] = "true"
+					newLine[28] = "10"
+				}
+			default:
+				require.Fail(t, "method should be add or edit")
 			}
 			lines[i] = newLine
 		}
@@ -54,14 +63,14 @@ func writeModifiedLinksCSV(t *testing.T, r *csv.Reader) (*bytes.Buffer, string) 
 	return &buf, ct
 }
 
-func TestImportAndEditLinks(t *testing.T) {
+func TestExportAndEditLinks(t *testing.T) {
 	r, err := newExporterTestResolver(t)
 	require.NoError(t, err)
 	log := r.exporter.log
 	e := &exporter{log, linksRower{log}}
 	ctx, res := prepareLinksPortsAndExport(t, r, e)
 	defer res.Body.Close()
-	importLinksPortsFile(t, r.client, res.Body, importer.ImportEntityLink)
+	importLinksPortsFile(t, r.client, res.Body, importer.ImportEntityLink, MethodEdit)
 
 	locs := r.client.Location.Query().AllX(ctx)
 	require.Len(t, locs, 3)
@@ -81,4 +90,48 @@ func TestImportAndEditLinks(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestExportAndAddLinks(t *testing.T) {
+	r, err := newExporterTestResolver(t)
+	require.NoError(t, err)
+	log := r.exporter.log
+	e := &exporter{log, linksRower{log}}
+	ctx, res := prepareLinksPortsAndExport(t, r, e)
+	defer res.Body.Close()
+
+	locs := r.client.Location.Query().AllX(ctx)
+	require.Len(t, locs, 3)
+	// Deleting link and of side's equipment to verify it creates it on import
+	deleteLinkAndEquipmentForReImport(ctx, t, r)
+
+	equips := r.client.Equipment.Query().AllX(ctx)
+	require.Len(t, equips, 1)
+	importLinksPortsFile(t, r.client, res.Body, importer.ImportEntityLink, MethodAdd)
+
+	links, err := r.Query().LinkSearch(ctx, nil, nil)
+	require.NoError(t, err)
+	require.Equal(t, 1, links.Count)
+	for _, link := range links.Links {
+		props := link.QueryProperties().AllX(ctx)
+		for _, prop := range props {
+			switch prop.QueryType().OnlyX(ctx).Name {
+			case propNameInt:
+				require.Equal(t, 100, prop.IntVal)
+			case propNameBool:
+				require.Equal(t, false, prop.BoolVal)
+			case propNameStr:
+				require.Equal(t, "t1", prop.StringVal)
+			}
+		}
+	}
+}
+
+func deleteLinkAndEquipmentForReImport(ctx context.Context, t *testing.T, r *TestExporterResolver) {
+	l := r.client.Link.Query().OnlyX(ctx)
+	equipToDelete := l.QueryPorts().QueryParent().Where(equipment.Name(currEquip)).OnlyX(ctx)
+	_, err := r.Mutation().RemoveLink(ctx, l.ID, nil)
+	require.NoError(t, err)
+	_, err = r.Mutation().RemoveEquipment(ctx, equipToDelete.ID, nil)
+	require.NoError(t, err)
 }
