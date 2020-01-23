b9dd2f0b6706d0477d98af9d76a5c1ab44633db4
Scott Smith <smithscott@fb.com>
2020-01-23 09:57:22 -0800

    Alertmanager Configurer support single tenancy
    Summary:
    *Upgrade Required*
    To deal with a one-time data migration for the managed alertmanager configuration file, run `docker-compose -f docker-compose.metrics.yml exec alertmanager-configurer migration -alertmanager-conf=/etc/configs/alertmanager.yml` if running on Docker, or `kubectl exec -it -n magma <alertmanager-configurer-instance> -- migration -alertmanager-conf=/etc/configs/alertmanager.yml` if running on kubernetes.
    
    Refactor alertmanager-configurer to more naturally support single-tenant usage. Also remove all references to 'network' in favor of 'tenant'
    
    Reviewed By: xjtian
    
    Differential Revision: D19433574
    
    fbshipit-source-id: 61f1b79425d77e4e2c6151c6d03b658e210de4cc

--- orc8r/cloud/go/services/metricsd/prometheus/configmanager/alertmanager/server.go
+++ orc8r/cloud/go/services/metricsd/prometheus/configmanager/alertmanager/server.go
@@ -15,6 +15,7 @@ import (
 
 	"magma/orc8r/cloud/go/services/metricsd/prometheus/configmanager/alertmanager/receivers"
 	"magma/orc8r/cloud/go/services/metricsd/prometheus/configmanager/fsclient"
+	"magma/orc8r/cloud/go/services/metricsd/prometheus/configmanager/prometheus/alert"
 
 	"github.com/golang/glog"
 	"github.com/labstack/echo"
@@ -30,20 +31,20 @@ func main() {
 	port := flag.String("port", defaultPort, fmt.Sprintf("Port to listen for requests. Default is %s", defaultPort))
 	alertmanagerConfPath := flag.String("alertmanager-conf", defaultAlertmanagerConfigPath, fmt.Sprintf("Path to alertmanager configuration file. Default is %s", defaultAlertmanagerConfigPath))
 	alertmanagerURL := flag.String("alertmanagerURL", defaultAlertmanagerURL, fmt.Sprintf("URL of the alertmanager instance that is being used. Default is %s", defaultAlertmanagerURL))
+	matcherLabel := flag.String("multitenant-label", "", fmt.Sprintf("LabelName to use for enabling multitenancy through route matching. Leave empty for single tenant use cases."))
 	flag.Parse()
+
+	tenancy := &alert.TenancyConfig{
+		RestrictorLabel: *matcherLabel,
+	}
 
 	e := echo.New()
 
 	e.GET("/", statusHandler)
 
-	receiverClient := receivers.NewClient(*alertmanagerConfPath, *alertmanagerURL, fsclient.NewFSClient())
-	e.POST(ReceiverPath, GetReceiverPostHandler(receiverClient))
-	e.GET(ReceiverPath, GetGetReceiversHandler(receiverClient))
-	e.DELETE(ReceiverPath, GetDeleteReceiverHandler(receiverClient))
-	e.PUT(ReceiverPath+"/:"+ReceiverNamePathParam, GetUpdateReceiverHandler(receiverClient))
+	receiverClient := receivers.NewClient(*alertmanagerConfPath, *alertmanagerURL, tenancy, fsclient.NewFSClient())
 
-	e.POST(RoutePath, GetUpdateRouteHandler(receiverClient))
-	e.GET(RoutePath, GetGetRouteHandler(receiverClient))
+	RegisterV0Handlers(e, receiverClient)
 
 	glog.Infof("Alertmanager Config server listening on port: %s\n", *port)
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", *port)))
