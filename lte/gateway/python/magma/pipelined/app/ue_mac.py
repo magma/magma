25947ac4b5d657a19346632379eaa206da097077
Nick Yurchenko <koolzz@fb.com>
2020-01-23 08:38:41 -0800

    Add vlan learn table unit test (#1110)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1110
    
    Adding tests
    
    Reviewed By: xjtian
    
    Differential Revision: D19457297
    
    fbshipit-source-id: a65cb74f6c5e6d5edf89b78ab59f879a9f8dadc1

--- lte/gateway/python/magma/pipelined/app/ue_mac.py
+++ lte/gateway/python/magma/pipelined/app/ue_mac.py
@@ -5,7 +5,6 @@ All rights reserved.
 This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
 """
-from collections import namedtuple
 import threading
 
 from ryu.controller import ofp_event
