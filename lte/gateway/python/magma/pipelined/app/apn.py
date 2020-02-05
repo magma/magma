a4e7e3ad28b5c865db6db485e7a1e28d324c9e99
Vitalii <vitalij.kostenko@gmail.com>
2020-02-05 12:49:11 +0200

    updated APN tag functions according to comments

--- lte/gateway/python/magma/pipelined/app/apn.py
+++ lte/gateway/python/magma/pipelined/app/apn.py
@@ -8,9 +8,12 @@ of patent rights can be found in the PATENTS file in the same directory.
 """
 
 from magma.pipelined.app.base import MagmaController, ControllerType
+from magma.pipelined.openflow import flows
+from magma.pipelined.openflow.registers import Direction, APN_TAG_REG
+from magma.pipelined.openflow.magma_match import MagmaMatch
 from magma.pipelined.apn import encode_apn
 
-
+from ryu.controller.controller import  Datapath
 
 class APNController(MagmaController):
     """
@@ -28,7 +31,13 @@ class APNController(MagmaController):
     def __init__(self, *args, **kwargs):
         super(APNController, self).__init__(*args, **kwargs)
         self._clean_start = True # get from config file
-        # set Table_num
+        self._datapath = None
+
+        self.tbl_num = self._service_manager.get_table_num(self.APP_NAME)
+        self.next_table = \
+            self._service_manager.get_next_table_num(self.APP_NAME)
+        self.apn_tagging_scratch = \
+            self._service_manager.allocate_scratch_tables(self.APP_NAME, 1)[0]
 
 
     def initialize_on_connect(self, datapath):
@@ -38,6 +47,8 @@ class APNController(MagmaController):
         Args:
             datapath: ryu datapath struct
         """
+        self._datapath = datapath
+        self.delete_all_flows(datapath)
         # In case wee need to clean all existing  buggy / orphaned flows before start the controller
         # if self._clean_start
         #   self.delete_existing_flows()
@@ -51,13 +62,30 @@ class APNController(MagmaController):
             apn: APN UE is connected to with specified IP addr
         """
         # TODO(119vik): same IP is reused for several bearers connected to the same APN - take care about duplications
-        # For all packets which have:
-        #  - direction == Direction.IN
-        #  - ipv4_src == ue_ip_addr
-        #  Set
-        #  - registers.APN_TAG_REG to encode_apn(apn)
 
-        pass
+        parser = self._datapath.ofproto_parser
+
+        # Tag all downlink traffic
+        outbound_match = MagmaMatch(direction=Direction.OUT, ipv4_dst=ue_ip_addr)
+        actions = [
+            parser.NXActionRegLoad2(dst=APN_TAG_REG, value=encode_apn(apn)),
+            parser.NXActionResubmitTable(table_id=self.apn_tagging_scratch)
+        ]
+        flows.add_resubmit_next_service_flow(self._datapath, self.tbl_num,
+                                             outbound_match , actions,
+                                             priority=flows.MINIMUM_PRIORITY,
+                                             resubmit_table=self.next_table)
+
+        # Tag all uplink traffic
+        inbound_match = MagmaMatch(direction=Direction.IN, ipv4_src=ue_ip_addr)
+        actions = [
+            parser.NXActionRegLoad2(dst=APN_TAG_REG, value=encode_apn(apn)),
+            parser.NXActionResubmitTable(table_id=self.apn_tagging_scratch)
+        ]
+        flows.add_resubmit_next_service_flow(self._datapath, self.tbl_num,
+                                             inbound_match, actions,
+                                             priority=flows.MINIMUM_PRIORITY,
+                                             resubmit_table=self.next_table)
 
     def delete_apn_flow_for_ue(self, ue_ip_addr, apn):
         """ Delete flow been created in scope of add_apn_flow_for_ue.
@@ -70,9 +98,16 @@ class APNController(MagmaController):
         # flow delete
         pass
 
-    def delete_existing_flows(self):
-        """ Delete all flows which set APN register"""
+    def cleanup_on_disconnect(self, datapath):
+        """
+        Cleanup flows on datapath disconnect event.
 
-        # for flow in flows:
-        # delete flow
-        pass
+        Args:
+            datapath: ryu datapath struct
+        """
+        self.delete_all_flows(datapath)
+
+    def delete_all_flows(self, datapath):
+        """ Delete all flows which set APN register"""
+        flows.delete_all_flows_from_table(datapath, self.tbl_num)
+        flows.delete_all_flows_from_table(datapath, self.apn_tagging_scratch)
