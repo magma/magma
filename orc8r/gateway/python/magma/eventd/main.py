e67c9616d593974c03f0d965757e908ff6ddcf01
Vignesh Shankar <vgs@fb.com>
2020-02-12 12:39:36 -0800

    Send eventd events to FluentBit via local TCP
    Summary:
    - This takes the Event received by LogEvent and sends it as a JSON string to FluentBit
    - Encoding:
    	- Event.value is of type `bytes`, so `pickle` is used when making the RPC call
    	- UTF-8 encoding is used for sending the message over TCP
    	- Hexadecimal encoding is used for serializing the event bytes to a string
    - orc8r/gateway/config .yml file removed for clear source of truth for config
    
    Reviewed By: andreilee
    
    Differential Revision: D19846395
    
    fbshipit-source-id: d71443ef8c171b6c8536751aa1f76f6cd22455f4

--- orc8r/gateway/python/magma/eventd/main.py
+++ orc8r/gateway/python/magma/eventd/main.py
@@ -6,15 +6,18 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree. An additional grant
 of patent rights can be found in the PATENTS file in the same directory.
 """
+
 from magma.common.service import MagmaService
 from .rpc_servicer import EventDRpcServicer
-
 
 def main():
     """ main() for eventd """
     service = MagmaService('eventd', None)
 
-    eventd_servicer = EventDRpcServicer()
+    eventd_servicer = EventDRpcServicer(
+        service.config['fluent_bit_port'],
+        service.config['tcp_timeout'],
+    )
     eventd_servicer.add_to_server(service.rpc_server)
 
     # Run the service loop
