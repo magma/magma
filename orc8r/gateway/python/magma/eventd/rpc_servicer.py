0631068a29abb4e2c00ad3d6ccfe6dcd62c1e0ca
Vignesh Shankar <vgs@fb.com>
2020-02-12 16:19:33 -0800

    Send eventd events to FluentBit via local TCP (#1200)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1200
    
    - This takes the Event received by LogEvent and sends it as a JSON string to FluentBit
    - Encoding:
    	- Event.value is of type `bytes`, so `pickle` is used when making the RPC call
    	- UTF-8 encoding is used for sending the message over TCP
    	- Hexadecimal encoding is used for serializing the event bytes to a string
    - orc8r/gateway/config .yml file removed for clear source of truth for config
    
    Reviewed By: andreilee
    
    Differential Revision: D19846395
    
    fbshipit-source-id: 943f2058cd3768deea6eb5e1087931f5e72eff63

--- orc8r/gateway/python/magma/eventd/rpc_servicer.py
+++ orc8r/gateway/python/magma/eventd/rpc_servicer.py
@@ -7,23 +7,22 @@ LICENSE file in the root directory of this source tree. An additional grant
 of patent rights can be found in the PATENTS file in the same directory.
 """
 
-#  Copyright (c) Facebook, Inc. and its affiliates.
-#  All rights reserved.
-#
-#  This source code is licensed under the BSD-style license found in the
-#  LICENSE file in the root directory of this source tree.
-
+import json
 import logging
-
-from orc8r.protos import eventd_pb2_grpc
-
+import socket
+from contextlib import closing
 from magma.common.rpc_utils import return_void
+from orc8r.protos import eventd_pb2_grpc, eventd_pb2
 
 
 class EventDRpcServicer(eventd_pb2_grpc.EventServiceServicer):
     """
     gRPC based server for EventD.
     """
+    def __init__(self, fluent_bit_port: int, tcp_timeout: int):
+        self.fluent_bit_port = fluent_bit_port
+        self.tcp_timeout = tcp_timeout
+
     def add_to_server(self, server):
         """
         Add the servicer to a gRPC server
@@ -31,8 +30,20 @@ class EventDRpcServicer(eventd_pb2_grpc.EventServiceServicer):
         eventd_pb2_grpc.add_EventServiceServicer_to_server(self, server)
 
     @return_void
-    def LogEvent(self, request, context):
+    def LogEvent(self, request: eventd_pb2.Event, context):
         """
         Logs an event.
         """
         logging.error("Logging event: %s", request)
+        try:
+            with closing(socket.create_connection(
+                    ('localhost', self.fluent_bit_port),
+                    timeout=self.tcp_timeout)) as sock:
+                sock.sendall(json.dumps({
+                    'stream_name': request.stream_name,
+                    'event_type': request.event_type,
+                    'tag': request.tag,
+                    'value': request.value.hex()
+                }).encode('utf-8'))
+        except socket.error as e:
+            raise e
