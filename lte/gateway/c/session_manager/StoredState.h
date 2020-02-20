7598f28f6afab726dfc66816ad78994b0eaf10a8
Andrei Lee <andreilee@fb.com>
2020-02-19 16:06:16 -0800

    Create UpdateCriteria
    Differential Revision: D19963897
    
    fbshipit-source-id: d86b1e2f39bddf98596f7f0530bda5edc05b1f94

--- lte/gateway/c/session_manager/StoredState.h
+++ lte/gateway/c/session_manager/StoredState.h
@@ -125,4 +125,25 @@ struct StoredSessionState {
   uint32_t request_number;
 };
 
+// Update Criteria
+
+struct SessionCreditUpdateCriteria {
+  bool reporting;
+  bool is_final;
+  ReAuthState reauth_state;
+  ServiceState service_state;
+  std::time_t  expiry_time;
+  std::unordered_map<Bucket, uint64_t> bucket_deltas;
+};
+
+struct SessionStateUpdateCriteria {
+  std::vector<std::string> static_rules_to_install;
+  std::vector<std::string> static_rules_to_uninstall;
+  std::vector<PolicyRule> dynamic_rules_to_install;
+  std::vector<std::string> dynamic_rules_to_uninstall;
+  std::unordered_map<
+    CreditKey, SessionCreditUpdateCriteria,
+    decltype(&ccHash), decltype(&ccEqual)> charging_credit_map;
+  std::unordered_map<std::string, SessionCreditUpdateCriteria> monitor_credit_map;
+};
 }; // namespace magma
