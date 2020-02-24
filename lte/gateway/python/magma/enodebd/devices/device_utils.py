46f833be3432be9e27afb76b8da6277721f27bc6
Gustavo Junior Alves <gjalves@gjalves.com.br>
2020-02-24 11:51:29 -0300

    Add support for Baicells BaiBS_RTSH firmware

--- lte/gateway/python/magma/enodebd/devices/device_utils.py
+++ lte/gateway/python/magma/enodebd/devices/device_utils.py
@@ -61,6 +61,8 @@ def get_device_name(
             return EnodebDeviceName.BAICELLS
         elif sw_version.startswith('BaiBS_RTS_'):
             return EnodebDeviceName.BAICELLS_RTS
+        elif sw_version.startswith('BaiBS_RTSH_'):
+            return EnodebDeviceName.BAICELLS_RTS
         else:
             raise UnrecognizedEnodebError("Device %s unsupported: Software (%s)"
                                          % (device_oui, sw_version))
