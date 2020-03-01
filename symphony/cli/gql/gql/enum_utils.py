1bfbd91491c41aafbf52d6ba281d26dcbdcd431f
Naor Matania <naorm1991@fb.com>
2020-02-29 23:18:03 -0800

    Move field rendering of enums and datefield to gql library (#372)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/372
    
    This is first part of enabling pyre on the auto-generated files and on all the pyinventory library
    
    Reviewed By: vdorfman
    
    Differential Revision: D20181116
    
    fbshipit-source-id: 4331a5e8a9956c480e5a8f6d2af661c98c9cd329

---
+++ symphony/cli/gql/gql/enum_utils.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from dataclasses import field
+from functools import partial
+
+
+def enum_field(enum_type):
+    def encode_enum(value):
+        return value.value
+
+    def decode_enum(t, value):
+        return t(value)
+
+    return field(
+        metadata={
+            "dataclasses_json": {
+                "encoder": encode_enum,
+                "decoder": partial(decode_enum, enum_type),
+            }
+        }
+    )
