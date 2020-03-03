573af744dc811df64ca06560aac9d1981d602ad7
Vignesh Shankar <vgs@fb.com>
2020-03-03 14:18:27 -0800

    Change package name from swagger -> model (#1277)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1277
    
    Pull Request resolved: https://github.com/facebookexternal/fbc/pull/1984
    
    Following the other v1 APIs
    
    Reviewed By: andreilee
    
    Differential Revision: D20175063
    
    fbshipit-source-id: a3d8229c61db04f927f3d8dc0e70e8bad95f4129

--- orc8r/cloud/go/services/eventd/models/gen.go
+++ orc8r/cloud/go/services/eventd/models/gen.go
@@ -6,4 +6,4 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
 */
 //go:generate bash -c "swaggergen --target=swagger.v1.yml --root=$MAGMA_ROOT --template=$SWAGGER_V1_TEMPLATE"
-package swagger
+package models
