228da2003b8de4e6e50ea3fe0f573176baf7a3de
Marie Bremner <marwhal@fb.com>
2020-03-11 17:42:00 -0700

    Add MockDriver to keep track of expected requests into the core
    Summary:
    ## MockDriver
    - Keeps track of a queue of expectations
    - For each incoming request, it will take in a matcher function and the request and checks if the request matches the expectation at the top of the queue. If it matches, it will pop the expectation off the queue and returns the answer set in the expectation.
    
    Reviewed By: xjtian
    
    Differential Revision: D20210448
    
    fbshipit-source-id: 61a6910d2636046e01c5a71791e61ca3ae3d7b16

---
+++ feg/gateway/services/testcore/mock_driver/mock_driver.go
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package mock_driver
+
+import (
+	"fmt"
+
+	"magma/feg/cloud/go/protos"
+)
+
+type Expectation interface {
+	DoesMatch(interface{}) bool
+	GetAnswer() interface{}
+}
+
+type MockDriver struct {
+	expectationsSet bool
+	// Should not modified to maintain the expectation order
+	expectations              []Expectation
+	expectationIndex          int
+	resultByIndex             map[int]*protos.ExpectationResult
+	errorMessages             []*protos.ErrorByIndex
+	unexpectedRequestBehavior protos.UnexpectedRequestBehavior
+	defaultAnswer             interface{}
+}
+
+func NewMockDriver(
+	expectations []Expectation,
+	behavior protos.UnexpectedRequestBehavior,
+	defaultAnswer interface{}) *MockDriver {
+	resultByIndex := make(map[int]*protos.ExpectationResult, len(expectations))
+	for i := range expectations {
+		resultByIndex[i] = &protos.ExpectationResult{ExpectationIndex: int32(i), ExpectationMet: false}
+	}
+	return &MockDriver{
+		expectationsSet:           true,
+		expectations:              expectations,
+		expectationIndex:          0,
+		resultByIndex:             resultByIndex,
+		unexpectedRequestBehavior: behavior,
+		defaultAnswer:             defaultAnswer,
+		errorMessages:             []*protos.ErrorByIndex{},
+	}
+}
+
+// GetAnswerFromExpectations will use the message passed in to determine if
+// the message matches the next upcoming expectation. If it does, it will
+// return the answer specified in the expectation.
+// Otherwise, if the unexpected request behavior is set to CONTINUE_WITH_DEFAULT_ANSWER,
+// it will return the default answer.
+// Otherwise, it will return nil.
+func (e *MockDriver) GetAnswerFromExpectations(message interface{}) interface{} {
+	if !e.expectationsSet {
+		return nil
+	}
+	if len(e.expectations) == 0 {
+		return e.getAnswerForUnexpectedMessage()
+	}
+	expectation := e.expectations[e.expectationIndex]
+	doesMatch := expectation.DoesMatch(message)
+	if !doesMatch {
+		err := &protos.ErrorByIndex{Index: int32(e.expectationIndex), Error: fmt.Sprintf("Expected: %v, Received: %v", expectation, message)}
+		e.errorMessages = append(e.errorMessages, err)
+		return e.getAnswerForUnexpectedMessage()
+	}
+
+	e.resultByIndex[e.expectationIndex].ExpectationMet = true
+	e.expectationIndex++
+	return expectation.GetAnswer()
+}
+
+// AggregateResults will aggregate resultByIndex and errorsByIndex.
+func (e *MockDriver) AggregateResults() ([]*protos.ExpectationResult, []*protos.ErrorByIndex) {
+	results := make([]*protos.ExpectationResult, len(e.expectations))
+	for i := range e.expectations {
+		results[i] = e.resultByIndex[i]
+	}
+	e.expectationsSet = false
+	return results, e.errorMessages
+}
+
+func (e *MockDriver) getAnswerForUnexpectedMessage() interface{} {
+	switch e.unexpectedRequestBehavior {
+	case protos.UnexpectedRequestBehavior_CONTINUE_WITH_DEFAULT_ANSWER:
+		return e.defaultAnswer
+	default:
+		return nil
+	}
+}
---
+++ feg/gateway/services/testcore/mock_driver/mock_driver_test.go
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package mock_driver_test
+
+import (
+	"testing"
+
+	"magma/feg/cloud/go/protos"
+	"magma/feg/gateway/services/testcore/mock_driver"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestExpectation struct {
+	request string
+	answer  string
+}
+
+func (t TestExpectation) DoesMatch(iReq interface{}) bool {
+	request, ok := iReq.(string)
+	if !ok {
+		return false
+	}
+	return request == t.request
+}
+
+func (t TestExpectation) GetAnswer() interface{} {
+	return t.answer
+}
+
+func TestAllExpectationsMet(t *testing.T) {
+	// define some expectations
+	expectation1 := TestExpectation{request: "req1", answer: "ans1"}
+	expectation2 := TestExpectation{request: "req2", answer: "ans2"}
+	defaultAnswer := "default"
+
+	expectations := []mock_driver.Expectation{expectation1, expectation2}
+	// initialize expectations
+	md := mock_driver.NewMockDriver(expectations, protos.UnexpectedRequestBehavior_CONTINUE_WITH_DEFAULT_ANSWER, defaultAnswer)
+
+	// meet both expectations
+	request1 := "req1"
+	request2 := "req2"
+
+	answer := md.GetAnswerFromExpectations(request1)
+	assert.EqualValues(t, "ans1", answer)
+
+	answer = md.GetAnswerFromExpectations(request2)
+	assert.EqualValues(t, "ans2", answer)
+
+	result, errs := md.AggregateResults()
+	// assert there was no unexpected requests
+	assert.Empty(t, errs)
+	expectedResult := []*protos.ExpectationResult{
+		{ExpectationIndex: 0, ExpectationMet: true},
+		{ExpectationIndex: 1, ExpectationMet: true},
+	}
+	assert.ElementsMatch(t, expectedResult, result)
+}
+
+func TestSomeExpectationsNotMet(t *testing.T) {
+	// base case : no expectations
+	md := mock_driver.NewMockDriver(nil, protos.UnexpectedRequestBehavior_CONTINUE_WITH_DEFAULT_ANSWER, "default")
+
+	request1 := "req1"
+	answer := md.GetAnswerFromExpectations(request1)
+	assert.Equal(t, "default", answer)
+
+	result, errs := md.AggregateResults()
+	// assert there was no unexpected requests
+	assert.Empty(t, result)
+	assert.Empty(t, errs)
+
+	// Two expectations are set, we will meet only the first one
+	expectation1 := TestExpectation{request: "req1", answer: "ans1"}
+	expectation2 := TestExpectation{request: "req2", answer: "ans2"}
+	expectation4 := TestExpectation{request: "req4", answer: "ans4"}
+
+	expectations := []mock_driver.Expectation{expectation1, expectation2, expectation4}
+	md = mock_driver.NewMockDriver(expectations, protos.UnexpectedRequestBehavior_CONTINUE_WITH_ERROR, nil)
+
+	answer = md.GetAnswerFromExpectations(request1)
+	assert.EqualValues(t, "ans1", answer)
+	request3 := "bad-req2-1"
+	answer = md.GetAnswerFromExpectations(request3)
+	assert.EqualValues(t, nil, answer)
+
+	request4 := "bad-req2-2"
+	answer = md.GetAnswerFromExpectations(request4)
+	assert.EqualValues(t, nil, answer)
+
+	result, errs = md.AggregateResults()
+	expectedResult := []*protos.ExpectationResult{
+		{ExpectationIndex: 0, ExpectationMet: true},
+		{ExpectationIndex: 1, ExpectationMet: false},
+		{ExpectationIndex: 2, ExpectationMet: false},
+	}
+	assert.ElementsMatch(t, expectedResult, result)
+	expectedErrors := []*protos.ErrorByIndex{
+		{Index: 1, Error: "Expected: {req2 ans2}, Received: bad-req2-1"},
+		{Index: 1, Error: "Expected: {req2 ans2}, Received: bad-req2-2"},
+	}
+	assert.ElementsMatch(t, expectedErrors, errs)
+}
