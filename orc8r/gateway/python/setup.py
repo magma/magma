c7981c769915a18187bf35df6bb1561001f7f746
Astro <m.rahm7n@gmail.com>
2020-03-14 10:14:38 +0200

    [RESOLVE]: Conflict fix by removing orc8r logger service and usages in gateway.

--- orc8r/gateway/python/setup.py
+++ orc8r/gateway/python/setup.py
@@ -30,7 +30,6 @@ setup(
         "magma.magmad.check.kernel_check",
         "magma.magmad.check.machine_check",
         "magma.magmad.check.network_check",
-        "magma.magmad.logging",
         "magma.magmad.upgrade",
         "magma.state",
     ],
