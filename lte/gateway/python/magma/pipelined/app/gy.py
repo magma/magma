25628eba5b157d6ba1b172d1794938dcc11948c2
Nick Yurchenko <koolzz@fb.com>
2020-03-17 00:43:39 -0700

    Separate GY/GX pipelined processing into different controllers
    Differential Revision: D18506680
    
    fbshipit-source-id: cdb38b79bca4520a4032b62b94dbbdb77ab87a34

---
+++ lte/gateway/python/magma/pipelined/app/gy.py
@@ -0,0 +1,126 @@
+"""
+Copyright (c) 2016-present, Facebook, Inc.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. An additional grant
+of patent rights can be found in the PATENTS file in the same directory.
+"""
+
+from typing import List
+
+from ryu.ofproto.ofproto_v1_4_parser import OFPFlowStats
+
+from lte.protos.pipelined_pb2 import RuleModResult
+from magma.pipelined.app.base import MagmaController, ControllerType
+from magma.pipelined.app.policy_mixin import PolicyMixin
+from magma.pipelined.openflow import flows
+from magma.pipelined.openflow.magma_match import MagmaMatch
+from magma.pipelined.openflow.registers import Direction
+from magma.pipelined.redirect import RedirectionManager, RedirectException
+from ryu.lib.packet import ether_types
+
+
+class GYController(PolicyMixin, MagmaController):
+    """
+    GYController
+
+    The GY controller installs flows for enforcement of GY final actions, this
+    includes redirection and QoS(currently not supported)
+    """
+
+    APP_NAME = "gy"
+    APP_TYPE = ControllerType.LOGICAL
+
+    def __init__(self, *args, **kwargs):
+        super(GYController, self).__init__(*args, **kwargs)
+        self.tbl_num = self._service_manager.get_table_num(self.APP_NAME)
+        self.next_main_table = self._service_manager.get_next_table_num(
+            self.APP_NAME)
+        self.loop = kwargs['loop']
+        self._redirect_scratch = \
+            self._service_manager.allocate_scratch_tables(self.APP_NAME, 1)[0]
+        self._bridge_ip_address = kwargs['config']['bridge_ip_address']
+        self._redirect_manager = None
+
+    def initialize_on_connect(self, datapath):
+        """
+        Install the default flows on datapath connect event.
+
+        Args:
+            datapath: ryu datapath struct
+        """
+        self._delete_all_flows(datapath)
+        self._install_default_flows(datapath)
+        self._datapath = datapath
+
+        self._redirect_manager = RedirectionManager(
+            self._bridge_ip_address,
+            self.logger,
+            self.tbl_num,
+            self.next_main_table,
+            self._redirect_scratch,
+            self._session_rule_version_mapper)
+
+    def _install_flow_for_rule(self, imsi, ip_addr, rule):
+        rule_num = self._rule_mapper.get_or_create_rule_num(rule.id)
+        priority = rule.priority
+
+        if rule.redirect.support == rule.redirect.ENABLED:
+            # TODO currently if redirection is enabled we ignore other flows
+            # from rule.flow_list, confirm that this is the expected behaviour
+            redirect_request = RedirectionManager.RedirectRequest(
+                imsi=imsi,
+                ip_addr=ip_addr,
+                rule=rule,
+                rule_num=rule_num,
+                priority=priority)
+            try:
+                self._redirect_manager.handle_redirection(
+                    self._datapath, self.loop, redirect_request)
+                return RuleModResult.SUCCESS
+            except RedirectException as err:
+                self.logger.error(
+                    'Redirect Exception for imsi %s, rule.id - %s : %s',
+                    imsi, rule.id, err
+                )
+                return RuleModResult.FAILURE
+        else:
+            # TODO: Add support once sessiond implements restrict access QOS
+            return RuleModResult.FAILURE
+
+    def _install_default_flow_for_subscriber(self, imsi):
+        pass
+
+    def _delete_all_flows(self, datapath):
+        flows.delete_all_flows_from_table(datapath, self.tbl_num)
+        flows.delete_all_flows_from_table(datapath, self._redirect_scratch)
+
+    def _install_default_flows(self, datapath):
+        """
+        For each direction set the default flows to just forward to next app.
+        The enforcement flows for each subscriber would be added when the
+        IP session is created, by reaching out to the controller/PCRF.
+
+        Args:
+            datapath: ryu datapath struct
+        """
+        inbound_match = MagmaMatch(eth_type=ether_types.ETH_TYPE_IP,
+                                   direction=Direction.IN)
+        outbound_match = MagmaMatch(eth_type=ether_types.ETH_TYPE_IP,
+                                    direction=Direction.OUT)
+        flows.add_resubmit_next_service_flow(
+            datapath, self.tbl_num, inbound_match, [],
+            priority=flows.MINIMUM_PRIORITY,
+            resubmit_table=self.next_main_table)
+        flows.add_resubmit_next_service_flow(
+            datapath, self.tbl_num, outbound_match, [],
+            priority=flows.MINIMUM_PRIORITY,
+            resubmit_table=self.next_main_table)
+
+    def _install_redirect_flow(self, imsi, ip_addr, rule):
+        return None
+
+    def _install_default_flows_if_not_installed(self, datapath,
+            existing_flows: List[OFPFlowStats]) -> List[OFPFlowStats]:
+        return None
