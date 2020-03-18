065dd34aee8db1fc93a2a26c315b3e1a24951abf
Marie Bremner <marwhal@fb.com>
2020-03-17 17:44:31 -0700

    Minor refactoring on OCS credit testing (#1336)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1336
    
    Combining the two OCS credit exhaustion tests into one file.
    Taking out some common setup into a shared function
    
    Reviewed By: andreilee
    
    Differential Revision: D20494469
    
    fbshipit-source-id: b5c4da6bb667a3a340f6bad3caadcca04554f669

--- cwf/gateway/integ_tests/ocs_credit_exhaustion_test.go
+++ cwf/gateway/integ_tests/ocs_credit_exhaustion_test.go
@@ -10,13 +10,13 @@ package integ_tests
 
 import (
 	"fmt"
-	cwfprotos "magma/cwf/cloud/go/protos"
-	fegprotos "magma/feg/cloud/go/protos"
 	"reflect"
 	"testing"
 	"time"
 
 	"fbc/lib/go/radius/rfc2869"
+	cwfprotos "magma/cwf/cloud/go/protos"
+	fegprotos "magma/feg/cloud/go/protos"
 	"magma/feg/gateway/services/eap"
 	"magma/lte/cloud/go/plugin/models"
 
@@ -25,16 +25,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAuthenticateOcsCreditExhaustedWithoutCRRU(t *testing.T) {
-	fmt.Printf("\nRunning TestAuthenticateOcsCreditExhaustedWithoutCCRU...\n")
+const (
+	MaxUsageBytes = 5 * 1024 * KiloBytes
+	MaxUsageTime  = 1000 // in second
+	ValidityTime  = 60   // in second
+)
 
+func ocsCreditExhaustionTestSetup(t *testing.T) (*TestRunner, *RuleManager, *cwfprotos.UEConfig) {
 	tr := NewTestRunner()
 	ruleManager, err := NewRuleManager()
 	assert.NoError(t, err)
 
 	ues, err := tr.ConfigUEs(1)
 	assert.NoError(t, err)
-
 	setNewOCSConfig(
 		&fegprotos.OCSConfig{
 			MaxUsageOctets: &fegprotos.Octets{TotalOctets: MaxUsageBytes},
@@ -48,7 +51,7 @@ func TestAuthenticateOcsCreditExhaustedWithoutCRRU(t *testing.T) {
 		&fegprotos.CreditInfo{
 			Imsi:        ue.Imsi,
 			ChargingKey: 1,
-			Volume:      &fegprotos.Octets{TotalOctets: 4 * 1024 * KiloBytes},
+			Volume:      &fegprotos.Octets{TotalOctets: 7 * 1024 * KiloBytes},
 			UnitType:    fegprotos.CreditInfo_Bytes,
 		},
 	)
@@ -66,6 +69,73 @@ func TestAuthenticateOcsCreditExhaustedWithoutCRRU(t *testing.T) {
 	// Apply a dynamic rule that points to the static rules above
 	err = ruleManager.AddRulesToPCRF(ue.Imsi, []string{"static-pass-all-ocs1", "static-pass-all-ocs2"}, nil)
 	assert.NoError(t, err)
+
+	return tr, ruleManager, ues[0]
+}
+
+func TestAuthenticateOcsCreditExhaustedWithCRRU(t *testing.T) {
+	fmt.Printf("\nRunning TestAuthenticateOcsCreditExhaustedWithCRRU...\n")
+
+	tr, ruleManager, ue := ocsCreditExhaustionTestSetup(t)
+	setCreditOnOCS(
+		&fegprotos.CreditInfo{
+			Imsi:        ue.Imsi,
+			ChargingKey: 1,
+			Volume:      &fegprotos.Octets{TotalOctets: 7 * 1024 * KiloBytes},
+			UnitType:    fegprotos.CreditInfo_Bytes,
+		},
+	)
+
+	// Wait for rules propagation
+	time.Sleep(2 * time.Second)
+	radiusP, err := tr.Authenticate(ue.GetImsi())
+	assert.NoError(t, err)
+
+	eapMessage := radiusP.Attributes.Get(rfc2869.EAPMessage_Type)
+	assert.NotNil(t, eapMessage)
+	assert.True(t, reflect.DeepEqual(int(eapMessage[0]), eap.SuccessCode))
+	time.Sleep(2 * time.Second)
+
+	// we need to generate over 80% of the quota to trigger a CCR update
+	req := &cwfprotos.GenTrafficRequest{Imsi: ue.GetImsi(), Volume: &wrappers.StringValue{Value: *swag.String("5M")}}
+	_, err = tr.GenULTraffic(req)
+	assert.NoError(t, err)
+	time.Sleep(3 * time.Second)
+
+	// we need to generate over 100% of the quota to trigger a session termination
+	_, err = tr.GenULTraffic(req)
+	assert.NoError(t, err)
+
+	// Wait for traffic to go through
+	time.Sleep(5 * time.Second)
+
+	// Check that UE mac flow is removed
+	recordsBySubID, err := tr.GetPolicyUsage()
+	assert.NoError(t, err)
+	record := recordsBySubID["IMSI"+ue.GetImsi()]["static-pass-all-ocs2"]
+	assert.Nil(t, record, fmt.Sprintf("Policy usage record for imsi: %v was not removed", ue.GetImsi()))
+
+	// trigger disconnection
+	_, err = tr.Disconnect(ue.GetImsi())
+	assert.NoError(t, err)
+
+	// Clear hss, ocs, and pcrf
+	assert.NoError(t, ruleManager.RemoveInstalledRules())
+	assert.NoError(t, tr.CleanUp())
+}
+
+func TestAuthenticateOcsCreditExhaustedWithoutCRRU(t *testing.T) {
+	fmt.Printf("\nRunning TestAuthenticateOcsCreditExhaustedWithoutCCRU...\n")
+
+	tr, ruleManager, ue := ocsCreditExhaustionTestSetup(t)
+	setCreditOnOCS(
+		&fegprotos.CreditInfo{
+			Imsi:        ue.Imsi,
+			ChargingKey: 1,
+			Volume:      &fegprotos.Octets{TotalOctets: 4 * 1024 * KiloBytes},
+			UnitType:    fegprotos.CreditInfo_Bytes,
+		},
+	)
 
 	// Wait for rules propagation
 	time.Sleep(2 * time.Second)
