cff2317356a0bdcf126940049a55538ebd14f513
Naor Matania <naorm1991@fb.com>
2020-03-24 07:36:32 -0700

    Change store access through old login flow (#449)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/449
    
    BasicAuth doesn't work for accessing S3 in prod
    
    Reviewed By: vdorfman
    
    Differential Revision: D20620660
    
    fbshipit-source-id: 0e24f7af9fbc18db676dc073719750ab8a014112

--- symphony/cli/gql/gql/graphql_client.py
+++ symphony/cli/gql/gql/graphql_client.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 import requests
 from gql.gql import gql
 from gql.gql.client import Client
+from requests.auth import AuthBase
 
 from .reporter import DUMMY_REPORTER, Reporter
 from .transport.session import RequestsHTTPSessionTransport
@@ -16,6 +17,7 @@ class GraphqlClient:
         graphql_endpoint_address: str,
         session: requests.Session,
         client_name: str,
+        auth: Optional[AuthBase] = None,
         reporter: Reporter = DUMMY_REPORTER,
     ) -> None:
 
@@ -24,9 +26,7 @@ class GraphqlClient:
             Args:
                 graphql_endpoint_address (str): The graphql server address
                 auth (Optional[requests.auth.AuthBase], optional): Auth used
-                    to authenticate to graphql server
-                verify_ssl (bool): Used for testing environment where ssl
-                    certificate is invalid
+                    to authenticate to graphql server      
                 reporter (object, optional): Use reporter.InventoryReporter to
                             store reports on all successful and failed mutations
                             in inventory. The default is DummyReporter that
@@ -44,6 +44,7 @@ class GraphqlClient:
                     "Content-Type": "application/json",
                     "User-Agent": client_name,
                 },
+                auth=auth,
             ),
             fetch_schema_from_transport=True,
         )
