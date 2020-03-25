ea08aa3027f7d7b2f97fec34adb69d5cc35d4782
Jacky Tian <xjtian@fb.com>
2020-03-24 17:04:52 -0700

    Spring cleaning: Remove deprecated targets and defs from cloud Makefile
    Summary: - Get rid of all targets that we used to use for the systemd-based controller/proxy which have long been deprecated
    
    Reviewed By: uri200
    
    Differential Revision: D20634633
    
    fbshipit-source-id: 83ac4a26a4d401b45be4cb2ccb446a9e45a07d8c

--- orc8r/cloud/go/defs.mk
+++ orc8r/cloud/go/defs.mk
@@ -4,4 +4,3 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 #
-$(MODULE)_SERVICE_NAMES := accessd checkind metricsd streamer upgrade certifier bootstrapper magmad dispatcher config state device configurator
