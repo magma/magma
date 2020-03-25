fecfe81f0c60c5994936dc7273e3603541aa8205
vyas <otot@ereueomc.fr>
2020-03-25 12:56:02 +0100

    UDP Bug

--- lte/gateway/c/oai/oai_mme/oai_mme.c
+++ lte/gateway/c/oai/oai_mme/oai_mme.c
@@ -109,7 +109,7 @@ int main(int argc, char *argv[])
   CHECK_INIT_RETURN(sgw_init(&spgw_config, mme_config.use_stateless));
   CHECK_INIT_RETURN(pgw_init(&spgw_config));
 #else
-  CHECK_INIT_RETURN(udp_init(&mme_config));  
+  CHECK_INIT_RETURN(udp_init());  
   CHECK_INIT_RETURN(s11_mme_init(&mme_config));
 #endif
   CHECK_INIT_RETURN(s1ap_mme_init(&mme_config));
