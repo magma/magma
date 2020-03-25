17b67e885f1fd0c1b4f322934800e37aa3a93fa1
rsarwad <rashmi.sarwad@radisys.com>
2020-03-25 12:45:00 -0700

    Added new test case for sending dedicated bearer request with QCI,0 (#1349)
    Summary:
    Added new test case for sending dedicated bearer request with QCI,0
    Modified create_bearer(), such that qci value is sent from test case and default value is set to "1"
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1349
    
    Test Plan: Executed s1ap tester sanity test suite
    
    Reviewed By: ardzoht
    
    Differential Revision: D20536285
    
    Pulled By: ulaskozat
    
    fbshipit-source-id: ba86a90e4882aef7cc8c1dff491fc4ef0a394c27

--- lte/gateway/python/integ_tests/s1aptests/s1ap_utils.py
+++ lte/gateway/python/integ_tests/s1aptests/s1ap_utils.py
@@ -399,7 +399,7 @@ class SpgwUtil(object):
         """
         self._stub = SpgwServiceStub(get_rpc_channel("spgw_service"))
 
-    def create_bearer(self, imsi, lbi):
+    def create_bearer(self, imsi, lbi, qci_val=1):
         """
         Sends a CreateBearer Request to SPGW service
         """
@@ -410,7 +410,7 @@ class SpgwUtil(object):
             policy_rules=[
                 PolicyRule(
                     qos=FlowQos(
-                        qci=1,
+                        qci=qci_val,
                         gbr_ul=10000000,
                         gbr_dl=10000000,
                         max_req_bw_ul=10000000,
