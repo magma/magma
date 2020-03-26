f7156153d648213b6ca08c3414f30d2a2ee2ea02
Alejandro Rodriguez Aragonez <alexrod@fb.com>
2020-03-25 21:29:02 -0700

    Adding monitord service to AGW (#1351)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1351
    
    For CPE monitoring, we are adding a magma service that will pull valid subscribers, send ICMP ping to each one of them, and save responses in memory, so then they can be exported with `get_operational_states` to cloud.
    
    This diff:
    - Adds `monitord` service
    - Adds protobuf config files for service
    
    Next:
    - Save state responses in redis
    - Add get_operational_states service303 callback handler
    - Add metrics and tests
    
    Reviewed By: xjtian
    
    Differential Revision: D20527265
    
    fbshipit-source-id: 8dc0560454315c8aaf6ebb4c7e07c15cfdd70439

---
+++ lte/gateway/python/magma/monitord/icmp_monitoring.py
@@ -0,0 +1,122 @@
+#  Copyright (c) Facebook, Inc. and its affiliates.
+#  All rights reserved.
+#
+#  This source code is licensed under the BSD-style license found in the
+#  LICENSE file in the root directory of this source tree.
+import asyncio
+import ipaddress
+import logging
+from collections import defaultdict
+from datetime import datetime
+from typing import List, NamedTuple
+
+import grpc
+from lte.protos.mobilityd_pb2 import IPAddress
+from lte.protos.mobilityd_pb2_grpc import MobilityServiceStub
+from magma.common.job import Job
+from magma.common.rpc_utils import grpc_async_wrapper
+from magma.common.service_registry import ServiceRegistry
+from magma.configuration.service_configs import load_service_config
+from magma.magmad.check.network_check import ping
+from magma.magmad.check.network_check.ping import PingCommandResult
+from orc8r.protos.common_pb2 import Void
+
+NUM_PACKETS = 4
+DEFAULT_POLLING_INTERVAL = 60
+TIMEOUT_SECS = 10
+CHECKIN_INTERVAL = 10
+
+ICMPMonitoringResponse = NamedTuple('ICMPMonitoringResponse',
+                                    [('last_reported_time', int),
+                                     ('latency_ms', float)])
+
+
+def _get_addr_from_subscriber(sub) -> str:
+    return str(ipaddress.IPv4Address(
+        sub.ip.address) if sub.ip.version == 0 else \
+                   ipaddress.IPv6Address(sub.ip.address))
+
+
+class ICMPMonitoring(Job):
+    """
+    Class that handles main loop to send ICMP ping to valid subscribers.
+    """
+
+    def __init__(self, polling_interval: int, service_loop):
+        super().__init__(interval=CHECKIN_INTERVAL, loop=service_loop)
+        self._config = load_service_config("monitord")
+        self._MTR_PORT = self._config["mtr_interface"]
+        # Matching response time output to get latency
+        self._polling_interval = max(polling_interval,
+                                     DEFAULT_POLLING_INTERVAL)
+        # TODO: Save to redis
+        self._subscriber_state = defaultdict(ICMPMonitoringResponse)
+        self._loop = service_loop
+
+    async def _get_subscribers(self) -> List[IPAddress]:
+        """
+        Sends gRPC call to mobilityd to get all subscribers table.
+
+        Returns: List of [Subscriber ID => IP address, APN] entries
+        """
+        try:
+            mobilityd_chan = ServiceRegistry.get_rpc_channel('mobilityd',
+                                                             ServiceRegistry.LOCAL)
+            mobilityd_stub = MobilityServiceStub(mobilityd_chan)
+            response = await grpc_async_wrapper(
+                mobilityd_stub.GetSubscriberIPTable.future(Void(),
+                                                           TIMEOUT_SECS),
+                self._loop)
+            return response.entries
+        except grpc.RpcError as err:
+            logging.error(
+                "GetSubscribers Error for %s! %s", err.code(), err.details())
+            return []
+
+    async def _ping_subscribers(self, hosts: List[str], subscribers: List[IPAddress]):
+        """
+        Sends a count of ICMP pings to target IP address, returns response.
+        Args:
+            ip_addr: ip address to send ICMP ping
+
+        Returns: (stdout, stderr)
+        """
+        ping_params = [
+            ping.PingInterfaceCommandParams(host, NUM_PACKETS, self._MTR_PORT,
+                                            TIMEOUT_SECS) for host in hosts]
+        ping_results = await ping.ping_async(ping_params, self._loop)
+        ping_results_list = list(ping_results)
+        for sub, result in zip(subscribers, ping_results_list):
+            self._save_ping_response(sub.sid.id, result)
+
+    def _save_ping_response(self, sid: str,
+                            ping_resp: PingCommandResult) -> None:
+        """
+        Saves ping response to in-memory subscriber dict.
+        Args:
+            sid: subscriber ID
+            ping_resp: response of ICMP ping command
+        """
+        if ping_resp.error:
+            logging.debug('Failed to ping %s with error: %s',
+                          sid, ping_resp.error)
+        reported_time = datetime.now().timestamp()
+        self._subscriber_state[sid] = ICMPMonitoringResponse(
+            last_reported_time=int(reported_time),
+            latency_ms=ping_resp.stats.rtt_avg)
+        logging.info(
+            '{} => {}ms'.format(sid, self._subscriber_state[sid].latency_ms))
+
+    async def _run(self) -> None:
+        logging.info("Running on interface %s ..." % self._MTR_PORT)
+        while True:
+            try:
+                subscribers = await self._get_subscribers()
+                addresses = [_get_addr_from_subscriber(sub) for sub in
+                             subscribers]
+                await self._ping_subscribers(addresses, subscribers)
+                await asyncio.sleep(self._polling_interval, self._loop)
+            except AttributeError:
+                logging.warning('No subscribers found, retrying...')
+                await asyncio.sleep(self._polling_interval, self._loop)
+                continue
