66538f9f31726a276606b36082d86f417ab96204
Ken Garber <kengarber@fb.com>
2020-03-27 14:28:40 -0700

    temporarily remove pyhub from generation (#463)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/463
    
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1389
    
    Pull Request resolved: https://github.com/facebookexternal/fbc/pull/2120
    
    Temporarily removing pyhub from being generated to get the following diff through, as well as a re-org of hub. Once we need pyhub back we'll figure out how to get it to work again.
    
    Reviewed By: Mokon
    
    Differential Revision: D20706500
    
    fbshipit-source-id: fd920b3763cd6d710f8398f27088b3849c7c8b80

--- symphony/cli/build.sh
+++ symphony/cli/build.sh
@@ -3,5 +3,5 @@
 rm -r -f ./dist ./build ./pyinventory.egg-info/
 python3 -m pip install --user --upgrade setuptools wheel
 python3 setup_pyinventory.py sdist bdist_wheel
-python3 setup_pyhub.py sdist bdist_wheel
+# python3 setup_pyhub.py sdist bdist_wheel
 sudo rm -r -f ./build
