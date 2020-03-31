e76c5b92c099078af8a6fb5064817405ee805f7a
Michael Germano <mgermano@fb.com>
2020-03-31 16:33:40 -0700

    Implement initial cwf operator to control cwf helm deployments
    Summary:
    This diff implements the initial cwf operator and it's controller. Currently
    this controller is cwf helm deployments and health montoring
    In a subsequent diff, the reconcilation logic for the HACluster controller will be extended to implement active/standby logic for CWAGs.
    
    Most of the files provided below are the output of `operator-sdk generate ...`.
    
    Reviewed By: xjtian
    
    Differential Revision: D20682962
    
    fbshipit-source-id: de182be219af6c3b6459a9c3bd5818a5d5d2406e

---
+++ cwf/k8s/cwf_operator/pkg/controller/hacluster/hacluster_controller.go
@@ -0,0 +1,100 @@
+/*
+ Copyright 2018 The Operator-SDK Authors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+/*
+ Modifications:
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+*/
+
+package hacluster
+
+import (
+	"context"
+
+	magmav1alpha1 "magma/cwf/k8s/cwf_operator/pkg/apis/magma/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_hacluster")
+
+// Add creates a new HACluster Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileHACluster{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("hacluster-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource HACluster
+	return c.Watch(&source.Kind{Type: &magmav1alpha1.HACluster{}}, &handler.EnqueueRequestForObject{})
+}
+
+// blank assignment to verify that ReconcileHACluster implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileHACluster{}
+
+// ReconcileHACluster reconciles a HACluster object
+type ReconcileHACluster struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a HACluster object and makes changes based on the state read
+// and what is in the HACluster.Spec
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileHACluster) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling Cluster")
+
+	instance := &magmav1alpha1.HACluster{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			reqLogger.Info("HACluster resource not found. Ignoring since object must be deleted.")
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, err
+	}
+	reqLogger.Info("Reconciled request")
+	return reconcile.Result{}, nil
+}
