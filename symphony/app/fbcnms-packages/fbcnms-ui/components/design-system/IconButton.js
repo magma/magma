dec7cefa4e7e0ce0dc66b42fbc0b13a8a868c8e1
Dolev Hadar <dolevh@fb.com>
2020-04-01 04:57:25 -0700

    IconButton
    Summary: Created an icon button component.
    
    Reviewed By: idoshveki
    
    Differential Revision: D20763388
    
    fbshipit-source-id: 58bfe4fa3b5552f2a662e98b4b45ec061da6a53c

---
+++ symphony/app/fbcnms-packages/fbcnms-ui/components/design-system/IconButton.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+
+import type {ButtonSkin} from './Button';
+import type {SvgIconStyleProps} from './Icons/SvgIcon';
+
+import * as React from 'react';
+import Button from './Button';
+
+type Props = $ReadOnly<{|
+  className?: string,
+  icon: React.ComponentType<SvgIconStyleProps>,
+  onClick?: void | (() => void | Promise<void>),
+  skin?: ButtonSkin,
+  disabled?: boolean,
+|}>;
+
+const IconButton = ({icon: Icon, ...buttonProps}: Props) => {
+  return (
+    <Button variant="text" {...buttonProps}>
+      <Icon color="inherit" />
+    </Button>
+  );
+};
+
+export default IconButton;
