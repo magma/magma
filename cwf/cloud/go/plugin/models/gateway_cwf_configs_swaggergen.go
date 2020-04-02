f3c7b43dc07234cd3e47a293cf68f6007e990e70
Nick Yurchenko <koolzz@fb.com>
2020-04-01 23:56:57 -0700

    Expose LI imsi list to cloud (#1418)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1418
    
    only mirror specific list of imsis
    
    Reviewed By: themarwhal
    
    Differential Revision: D20774102
    
    fbshipit-source-id: f54e707692a7348ce0df0b921d5058d3d141b9a4

--- cwf/cloud/go/plugin/models/gateway_cwf_configs_swaggergen.go
+++ cwf/cloud/go/plugin/models/gateway_cwf_configs_swaggergen.go
@@ -23,6 +23,9 @@ type GatewayCwfConfigs struct {
 
 	// ipdr export dst
 	IPDRExportDst *IPDRExportDst `json:"ipdr_export_dst,omitempty"`
+
+	// li imsis
+	LiImsis LiImsis `json:"li_imsis,omitempty"`
 }
 
 // Validate validates this gateway cwf configs
@@ -34,6 +37,10 @@ func (m *GatewayCwfConfigs) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateIPDRExportDst(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateLiImsis(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -72,6 +79,22 @@ func (m *GatewayCwfConfigs) validateIPDRExportDst(formats strfmt.Registry) error
 			}
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (m *GatewayCwfConfigs) validateLiImsis(formats strfmt.Registry) error {
+
+	if swag.IsZero(m.LiImsis) { // not required
+		return nil
+	}
+
+	if err := m.LiImsis.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("li_imsis")
+		}
+		return err
 	}
 
 	return nil
