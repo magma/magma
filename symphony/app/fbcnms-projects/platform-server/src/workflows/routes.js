04dcfb509422cfb8ab79ffc73f5bbeacaa9b0cf8
Ken Garber <kengarber@fb.com>
2020-04-02 20:57:38 -0700

    adding workflows container
    Summary: This adds a simple container to the local symphony deployment, and sets up a platform-server to proxy requests to /workflows to the container. I closely followed the example of the "graph" container. This will allow independent workflow development without interfering with anything inventory related.
    
    Differential Revision: D20831913
    
    fbshipit-source-id: 27dd266b06431823e7f327b6c99c048cc3e5eec1

---
+++ symphony/app/fbcnms-projects/platform-server/src/workflows/routes.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+const express = require('express');
+const querystring = require('querystring');
+const proxy = require('http-proxy-middleware');
+const {WORKFLOWS_HOST} = require('../config');
+import {accessRoleToString} from '@fbcnms/auth/roles';
+import {oidcAccessToken} from '@fbcnms/auth/oidc/middleware';
+import type {ClientRequest} from 'http';
+import type {FBCNMSRequest} from '@fbcnms/auth/access';
+
+const router = express.Router();
+
+router.use(
+  '/',
+  proxy({
+    // hostname to the target server
+    target: 'http://' + WORKFLOWS_HOST,
+
+    // enable websocket proxying
+    ws: true,
+
+    // rewrite paths
+    pathRewrite: (path: string): string => path.replace(/^\/workflows/, ''),
+
+    // subscribe to http-proxy's proxyReq event
+    onProxyReq: (proxyReq: ClientRequest, req: FBCNMSRequest): void => {
+      if (req.user.organization) {
+        proxyReq.setHeader('x-auth-organization', req.user.organization);
+      }
+      proxyReq.setHeader('x-auth-user-email', req.user.email);
+      proxyReq.setHeader('x-auth-user-role', accessRoleToString(req.user.role));
+
+      const accessToken = oidcAccessToken(req);
+      if (accessToken != null) {
+        proxyReq.setHeader('authorization', 'Bearer ' + accessToken);
+      }
+
+      if (!req.body || !Object.keys(req.body).length) {
+        return;
+      }
+
+      const writeBody = (body: string) => {
+        proxyReq.setHeader(
+          'Content-Length',
+          Buffer.byteLength(body).toString(),
+        );
+        proxyReq.write(body);
+        proxyReq.end();
+      };
+
+      const contentType = proxyReq.getHeader('Content-Type');
+      if (contentType.includes('application/json')) {
+        writeBody(JSON.stringify(req.body));
+      } else if (contentType.includes('application/x-www-form-urlencoded')) {
+        writeBody(querystring.stringify(req.body));
+      }
+    },
+  }),
+);
+
+module.exports = router;
