d21f4b74fe24014700a18f0d3bee4c4393c28dae
Alejandro Rodriguez Aragonez <alexrod@fb.com>
2020-04-07 11:21:06 -0700

    Updating s1ap handlers logging to include IMSI
    Summary:
    - Updating s1ap functions log usages to include IMSI
    - Updating some over-logging on S1AP (INFO => DEBUG)
    
    Reviewed By: ssanadhya
    
    Differential Revision: D20808452
    
    fbshipit-source-id: 53735b36ab3024904701b46e3381e52e07eee314

--- lte/gateway/c/oai/tasks/s1ap/s1ap_mme.c
+++ lte/gateway/c/oai/tasks/s1ap/s1ap_mme.c
@@ -302,7 +302,8 @@ void* s1ap_mme_thread(__attribute__((unused)) void* args)
             if (
               (ue_ref_p = s1ap_state_get_ue_mmeid(state, mme_ue_s1ap_id)) ==
               NULL) {
-              OAILOG_WARNING(
+              OAILOG_WARNING_UE(
+                imsi64,
                 LOG_S1AP,
                 "Timer expired but no assoicated UE context for UE id %d\n",
                 mme_ue_s1ap_id);
@@ -314,8 +315,9 @@ void* s1ap_mme_thread(__attribute__((unused)) void* args)
               received_message_p->ittiMsg.timer_has_expired.timer_id ==
               ue_ref_p->s1ap_ue_context_rel_timer.id) {
               // UE context release complete timer expiry handler
-              OAILOG_INFO(
+              OAILOG_WARNING_UE(
                 LOG_S1AP,
+                imsi64,
                 "ue_context_release_command_timer_expired for UE id %d\n",
                 mme_ue_s1ap_id);
               increment_counter(
@@ -325,7 +327,8 @@ void* s1ap_mme_thread(__attribute__((unused)) void* args)
           } else if (timer_arg.timer_class == S1AP_ENB_TIMER) {
             sctp_assoc_id_t assoc_id = timer_arg.instance_id;
             if ((enb_ref_p = s1ap_state_get_enb(state, assoc_id)) == NULL) {
-              OAILOG_WARNING(
+              OAILOG_WARNING_UE(
+                imsi64,
                 LOG_S1AP,
                 "Timer expired but no assoicated eNB context for eNB assoc_id "
                 "%d\n",
@@ -337,7 +340,8 @@ void* s1ap_mme_thread(__attribute__((unused)) void* args)
             if (
               received_message_p->ittiMsg.timer_has_expired.timer_id ==
               enb_ref_p->s1ap_enb_assoc_clean_up_timer.id) {
-              OAILOG_INFO(
+              OAILOG_DEBUG_UE(
+                imsi64,
                 LOG_S1AP,
                 "enb_sctp_shutdown_ue_clean_up_timer_expired for enb assoc_id "
                 "%d\n",
@@ -347,8 +351,9 @@ void* s1ap_mme_thread(__attribute__((unused)) void* args)
               s1ap_enb_assoc_clean_up_timer_expiry(state, enb_ref_p);
             }
           } else {
-            OAILOG_WARNING(
+            OAILOG_WARNING_UE(
               LOG_S1AP,
+              imsi64,
               " S1AP Timer expired with invalid timer class  %u \n",
               timer_arg.timer_class);
           }
