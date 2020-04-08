8064160e56acc01331d118ebf2f9c45ff86c0012
Andrei Lee <andreilee@fb.com>
2020-04-08 10:16:51 -0700

    Add RedisStoreClient for persistent storage of SessionState
    Summary:
    This revision is part of a series of changes to make session_manager stateless.
    
    The `RedisStoreClient` is implemented with some assumptions that should always hold:
    1) `SessionStore` is always run in the main `EventBase` thread, so only one of its methods is executing at once
    2) Writes of `RedisStoreClient` can be assumed to be happening atomically
    3) Nothing other than `session_manager` can read the Redis table holding session state
    
    If these assumptions hold, then these guarantees can be provided:
    1) `read_sessions` will always return a set of sessions with sane state
    2) The Redis table holding session state should always have a sane view of session state which can be recovered from
    
    Reviewed By: themarwhal
    
    Differential Revision: D20732913
    
    fbshipit-source-id: 43ea2ac6ff6de470845ae9e251726d62f3ebe6fa

---
+++ lte/gateway/c/session_manager/RedisStoreClient.h
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#pragma once
+
+#include <cpp_redis/cpp_redis>
+#include <folly/Format.h>
+#include <folly/dynamic.h>
+#include <folly/json.h>
+
+#include "StoreClient.h"
+#include "StoredState.h"
+#include "ServiceConfigLoader.h"
+
+namespace magma {
+namespace lte {
+
+class RedisReadFailed : public std::exception {
+ public:
+  RedisReadFailed() = default;
+};
+
+/**
+ * Persistent StoreClient used to allow stateless session_manager to function
+ */
+class RedisStoreClient final : public StoreClient {
+ public:
+  RedisStoreClient(
+      cpp_redis::client& client,
+      const std::string& redis_table,
+      std::shared_ptr<StaticRuleStore> rule_store);
+
+  RedisStoreClient(RedisStoreClient const&) = delete;
+  RedisStoreClient(RedisStoreClient&&) = default;
+  ~RedisStoreClient() = default;
+
+  bool try_redis_connect();
+
+  SessionMap read_sessions(std::vector<std::string> subscriber_ids);
+
+  bool write_sessions(SessionMap session_map);
+
+ private:
+  cpp_redis::client& client_;
+  std::string redis_table_;
+  std::shared_ptr<StaticRuleStore> rule_store_;
+
+ private:
+  std::string serialize_session_vec(
+      std::vector<std::unique_ptr<SessionState>>& session_vec);
+
+  std::vector<std::unique_ptr<SessionState>> deserialize_session_vec(
+      std::string serialized);
+};
+
+} // namespace lte
+} // namespace magma
