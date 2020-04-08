ffb80f467b849d543d838838a468b703b83fa8ea
Scott Smith <smithscott@fb.com>
2020-04-08 11:11:11 -0700

    Add organization scripts to magmalte (#414)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/414
    
    Add scripts necessary for organization features to magmalte.
    * setPassword.js modified (taken from platform-server)
    
    Reviewed By: rckclmbr
    
    Differential Revision: D20473103
    
    fbshipit-source-id: 894ca3cdad2d283b54326cfa8f659110bd28ef4d

---
+++ symphony/app/fbcnms-projects/magmalte/scripts/createOrganization.js
@@ -0,0 +1,117 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+'use strict';
+
+import type {OrganizationType} from '@fbcnms/sequelize-models/models/organization';
+import type {Tab} from '@fbcnms/types/tabs';
+
+import {Organization} from '@fbcnms/sequelize-models';
+import {coerceToTab} from '@fbcnms/types/tabs';
+import {difference} from 'lodash';
+import {getProjectTabs} from '@fbcnms/magmalte/app/common/projects';
+import {union} from 'lodash';
+
+type OrganizationObject = {
+  name: string,
+  tabs: Array<Tab>,
+  networkIDs: Array<string>,
+  csvCharset: '',
+};
+
+async function updateOrganization(
+  organization: OrganizationType,
+  organizationObject: OrganizationObject,
+) {
+  console.log(
+    `Updating organization ${organizationObject.name} to: ` +
+      `tabs=${organizationObject.tabs.join(' ')}, ` +
+      `networkIDs=[${organizationObject.networkIDs.join(' ')}]`,
+  );
+  await organization.update({
+    tabs: organizationObject.tabs ?? ['inventory'],
+    networkIDs: union(
+      organization.networkIDs ?? [],
+      organizationObject.networkIDs,
+    ),
+  });
+}
+
+async function createOrganization(organizationObject: OrganizationObject) {
+  console.log(
+    `Creating a new organization: name=${organizationObject.name}, ` +
+      `tabs=${organizationObject.tabs.join(' ')}, ` +
+      `networkIDs=[${organizationObject.networkIDs.join(' ')}]`,
+  );
+  await Organization.create({
+    name: organizationObject.name,
+    tabs: organizationObject.tabs,
+    networkIDs: organizationObject.networkIDs,
+    csvCharset: '',
+    ssoCert: '',
+    ssoEntrypoint: '',
+    ssoIssuer: '',
+  });
+}
+
+async function createOrUpdateOrganization(
+  organizationObject: OrganizationObject,
+) {
+  const organization = await Organization.findOne({
+    where: {
+      name: organizationObject.name,
+    },
+  });
+  if (!organization) {
+    await Promise.all([createOrganization(organizationObject)]);
+  } else {
+    await updateOrganization(organization, organizationObject);
+  }
+}
+
+function main() {
+  const args = process.argv.slice(2);
+  if (args.length < 2) {
+    console.log(
+      'Usage: createOrganization.js <name> <tab>,<tab>,... <networkID>,<networkID>, ...',
+    );
+    process.exit(1);
+  }
+
+  const validTabs = getProjectTabs();
+  const tabs = args[1].split(',').map(tab => coerceToTab(tab));
+  const invalidTabs = difference(tabs, validTabs.map(tab => tab.id)).join(', ');
+  if (invalidTabs) {
+    console.log(
+      `tab should be one of: ${validTabs
+        .map(tab => tab.id)
+        .join(', ')}. Got: ${invalidTabs}`,
+    );
+    process.exit(1);
+  }
+
+  const networkIDs = (args[2] || '').split(',');
+  const organizationObject = {
+    name: args[0],
+    tabs: tabs,
+    networkIDs: networkIDs,
+    csvCharset: '',
+  };
+  createOrUpdateOrganization(organizationObject)
+    .then(_res => {
+      console.log('Success');
+      process.exit();
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+main();
--- symphony/app/fbcnms-projects/magmalte/scripts/setPassword.js
+++ symphony/app/fbcnms-projects/magmalte/scripts/setPassword.js
@@ -9,42 +9,50 @@
  */
 'use strict';
 
+import type {OrganizationType} from '@fbcnms/sequelize-models/models/organization';
+
 import bcrypt from 'bcryptjs';
 import {AccessRoles} from '@fbcnms/auth/roles';
-import {User} from '@fbcnms/sequelize-models';
+import {Organization, User} from '@fbcnms/sequelize-models';
 
 const SALT_GEN_ROUNDS = 10;
 
 type UserObject = {
+  organization: string,
   email: string,
   password: string,
-  superUser: boolean,
+  role: number,
 };
 
 async function updateUser(user: User, userObject: UserObject) {
-  const {password, superUser} = userObject;
+  const {password, role} = userObject;
   const salt = await bcrypt.genSalt(SALT_GEN_ROUNDS);
   const passwordHash = await bcrypt.hash(password, salt);
   await user.update({
     password: passwordHash,
-    role: superUser ? AccessRoles.SUPERUSER : AccessRoles.USER,
+    role,
   });
 }
 
 async function createUser(userObject: UserObject) {
-  const {email, password, superUser} = userObject;
+  const {organization, email, password, role} = userObject;
   const salt = await bcrypt.genSalt(SALT_GEN_ROUNDS);
   const passwordHash = await bcrypt.hash(password, salt);
+  const org = await createOrFetchOrganization(organization);
   await User.create({
     email: email.toLowerCase(),
     password: passwordHash,
-    role: superUser ? AccessRoles.SUPERUSER : AccessRoles.USER,
+    role,
+    networkIDs: [],
+    organization: org.name,
+    readOnly: false,
   });
 }
 
 async function createOrUpdateUser(userObject: UserObject) {
   const user = await User.findOne({
     where: {
+      organization: userObject.organization,
       email: userObject.email.toLowerCase(),
     },
   });
@@ -55,17 +63,50 @@ async function createOrUpdateUser(userObject: UserObject) {
   }
 }
 
+async function createOrFetchOrganization(
+  organization: string,
+): Promise<OrganizationType> {
+  let org = await Organization.findOne({
+    where: {
+      name: organization,
+    },
+  });
+  if (!org) {
+    console.log(`Creating a new Organization: name=${organization} `);
+    const [o] = await Promise.all([
+      Organization.create({
+        name: organization,
+        tab: ['inventory', 'nms'],
+        networkIDs: [],
+        csvCharset: '',
+        ssoCert: '',
+        ssoIssuer: '',
+        ssoEntrypoint: '',
+      }),
+    ]);
+    org = o;
+  }
+  return org;
+}
+
 function main() {
   const args = process.argv.slice(2);
-  if (args.length != 2) {
-    console.log('Usage: setPassword.js <email> <password>');
+  if (args.length !== 3) {
+    console.log('Usage: setPassword.js <organization> <email> <password>');
     process.exit(1);
   }
   const userObject = {
-    email: args[0],
-    password: args[1],
-    superUser: true,
+    organization: args[0],
+    email: args[1],
+    password: args[2],
+    role: AccessRoles.SUPERUSER,
   };
+  console.log(
+    'Creating a new user: email=' +
+      userObject.email +
+      ', password=' +
+      userObject.password,
+  );
   createOrUpdateUser(userObject)
     .then(_res => {
       console.log('Success');
