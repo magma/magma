731650b10a4499bf815b7840bf57ad68f2974f6c
Andrei Lee <andreilee@fb.com>
2020-04-10 16:17:09 -0700

    clang format SessionProxyResponderHandler
    Differential Revision: D20973981
    
    fbshipit-source-id: 1067b1304a7dd255dd706250c9c93b331f50a559

--- lte/gateway/c/session_manager/SessionProxyResponderHandler.cpp
+++ lte/gateway/c/session_manager/SessionProxyResponderHandler.cpp
@@ -17,77 +17,70 @@ using grpc::Status;
 namespace magma {
 
 SessionProxyResponderHandlerImpl::SessionProxyResponderHandlerImpl(
-  std::shared_ptr<LocalEnforcer> enforcer,
-  SessionStore& session_store):
-  enforcer_(enforcer),
-  session_store_(session_store)
-{
-}
+    std::shared_ptr<LocalEnforcer> enforcer, SessionStore& session_store)
+    : enforcer_(enforcer), session_store_(session_store) {}
 
 void SessionProxyResponderHandlerImpl::ChargingReAuth(
-  ServerContext *context,
-  const ChargingReAuthRequest *request,
-  std::function<void(Status, ChargingReAuthAnswer)> response_callback)
-{
-  auto &request_cpy = *request;
+    ServerContext* context, const ChargingReAuthRequest* request,
+    std::function<void(Status, ChargingReAuthAnswer)> response_callback) {
+  auto& request_cpy = *request;
   enforcer_->get_event_base().runInEventBaseThread(
-    [this, request_cpy, response_callback]() {
-      auto session_map = get_sessions_for_charging(request_cpy);
-      SessionUpdate update = SessionStore::get_default_session_update(session_map);
-      auto result = enforcer_->init_charging_reauth(session_map, request_cpy, update);
-      ChargingReAuthAnswer ans;
-      ans.set_result(result);
+      [this, request_cpy, response_callback]() {
+        auto session_map = get_sessions_for_charging(request_cpy);
+        SessionUpdate update =
+            SessionStore::get_default_session_update(session_map);
+        auto result =
+            enforcer_->init_charging_reauth(session_map, request_cpy, update);
+        ChargingReAuthAnswer ans;
+        ans.set_result(result);
 
-      bool update_success = session_store_.update_sessions(update);
-      if (update_success) {
-        response_callback(Status::OK, ans);
-      } else {
-        auto status = Status(
-            grpc::ABORTED,
-            "ChargingReAuth no longer valid due to another update that "
-            "updated the session first.");
-        response_callback(status, ans);
-      }
-    });
+        bool update_success = session_store_.update_sessions(update);
+        if (update_success) {
+          response_callback(Status::OK, ans);
+        } else {
+          auto status = Status(
+              grpc::ABORTED,
+              "ChargingReAuth no longer valid due to another update that "
+              "updated the session first.");
+          response_callback(status, ans);
+        }
+      });
 }
 
 void SessionProxyResponderHandlerImpl::PolicyReAuth(
-  ServerContext *context,
-  const PolicyReAuthRequest *request,
-  std::function<void(Status, PolicyReAuthAnswer)> response_callback)
-{
-  auto &request_cpy = *request;
+    ServerContext* context, const PolicyReAuthRequest* request,
+    std::function<void(Status, PolicyReAuthAnswer)> response_callback) {
+  auto& request_cpy = *request;
   enforcer_->get_event_base().runInEventBaseThread(
-    [this, request_cpy, response_callback]() {
-      PolicyReAuthAnswer ans;
-      auto session_map = get_sessions_for_policy(request_cpy);
-      SessionUpdate update = SessionStore::get_default_session_update(session_map);
-      enforcer_->init_policy_reauth(session_map, request_cpy, ans, update);
+      [this, request_cpy, response_callback]() {
+        PolicyReAuthAnswer ans;
+        auto session_map = get_sessions_for_policy(request_cpy);
+        SessionUpdate update =
+            SessionStore::get_default_session_update(session_map);
+        enforcer_->init_policy_reauth(session_map, request_cpy, ans, update);
 
-      bool update_success = session_store_.update_sessions(update);
-      if (update_success) {
-        response_callback(Status::OK, ans);
-      } else {
-        auto status = Status(
-            grpc::ABORTED,
-            "PolicyReAuth no longer valid due to another update that "
-            "updated the session first.");
-        response_callback(status, ans);
-      }
-    });
+        bool update_success = session_store_.update_sessions(update);
+        if (update_success) {
+          response_callback(Status::OK, ans);
+        } else {
+          auto status = Status(
+              grpc::ABORTED,
+              "PolicyReAuth no longer valid due to another update that "
+              "updated the session first.");
+          response_callback(status, ans);
+        }
+      });
 }
 
 SessionMap SessionProxyResponderHandlerImpl::get_sessions_for_charging(
-  const ChargingReAuthRequest& request)
-{
+    const ChargingReAuthRequest& request) {
   SessionRead req = {request.sid()};
   return session_store_.read_sessions(req);
 }
 
 SessionMap SessionProxyResponderHandlerImpl::get_sessions_for_policy(
-  const PolicyReAuthRequest& request)
-{
+    const PolicyReAuthRequest& request) {
   SessionRead req = {request.imsi()};
   return session_store_.read_sessions(req);
 }
-} // namespace magma
+}  // namespace magma
