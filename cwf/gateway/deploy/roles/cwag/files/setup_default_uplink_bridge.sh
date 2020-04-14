625dd72cf7f7095d0fcf3f6f03943f1ed8bd9850
Nick Yurchenko <koolzz@fb.com>
2020-04-14 12:53:10 -0700

    Change CWF uplink_br0 to a managed bridge
    Summary:
    Reason for change is NORMAL action kills our performance.
    For 2 NIC setups use group action with dp_hash decision
    For 1 NIC just egress to that NIC
    
    Reviewed By: pshelar
    
    Differential Revision: D20979664
    
    fbshipit-source-id: e0dfae66ca6e4243a8e65f4428e61950c1e504c7

--- cwf/gateway/deploy/roles/cwag/files/setup_default_uplink_bridge.sh
+++ cwf/gateway/deploy/roles/cwag/files/setup_default_uplink_bridge.sh
@@ -8,6 +8,9 @@
 # of patent rights can be found in the PATENTS file in the same directory.
 
 sudo ovs-vsctl add-br uplink_br0
+sudo ovs-vsctl set-fail-mode cwag_br0 secure
+sudo ovs-ofctl del-flows uplink_br0
+
 sudo ovs-vsctl --may-exist add-port uplink_br0 gw0 \
   -- set Interface gw0 type=internal \
   -- set interface gw0 ofport=1
@@ -17,10 +20,24 @@ sudo ovs-vsctl --may-exist add-port uplink_br0 uplink_patch \
   -- --may-exist add-port cwag_br0 cwag_patch \
   -- set Interface cwag_patch type=patch  options:peer=uplink_patch
 
+# gw0 configuration, TODO might want to avoid NORMAL for perf reasons
+ip_addr=$(sudo /sbin/ifconfig gw0 | grep -m 1 inet | awk '{ print $2}')
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, arp, nw_src=$ip_addr, actions=NORMAL"
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, arp, nw_dst=$ip_addr, actions=NORMAL"
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_src=$ip_addr, actions=NORMAL"
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_dst=$ip_addr, actions=NORMAL"
+
 # Some setups might not have 2 nics. In that case just use eth2
 if [ ! -d "/sys/class/net/eth3" ]
 then
   sudo ovs-vsctl --may-exist add-port uplink_br0 eth2
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=output:eth2"
 else
-  sudo ovs-vsctl --may-exist add-bond uplink_br0 uplink_bond eth2 eth3
+  sudo ovs-vsctl --may-exist add-port uplink_br0 eth2
+  sudo ovs-vsctl --may-exist add-port uplink_br0 eth3
+  sudo ovs-ofctl -O OpenFlow14 add-group uplink_br0 "group_id=42, type=select, selection_method=dp_hash, bucket=actions=output:eth2, bucket=actions=output:eth3"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth3, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=group:42"
 fi
