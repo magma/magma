f891ee8b2b49961dee620168359ce0def4419e53
Nick Yurchenko <koolzz@fb.com>
2020-04-15 11:32:00 -0700

    Fix cwf deploy to work with all interface configurations
    Summary: don't assume there is eth2
    
    Reviewed By: ardzoht, themarwhal, uri200
    
    Differential Revision: D21045171
    
    fbshipit-source-id: 6ae5e74e30501b9fdc9acc5ad5f0b022e4683b58

--- cwf/gateway/deploy/roles/cwag/files/setup_default_uplink_bridge.sh
+++ cwf/gateway/deploy/roles/cwag/files/setup_default_uplink_bridge.sh
@@ -14,6 +14,7 @@ sudo ovs-ofctl del-flows uplink_br0
 sudo ovs-vsctl --may-exist add-port uplink_br0 gw0 \
   -- set Interface gw0 type=internal \
   -- set interface gw0 ofport=1
+sudo ifconfig gw0 up
 
 sudo ovs-vsctl --may-exist add-port uplink_br0 uplink_patch \
   -- set Interface uplink_patch type=patch options:peer=cwag_patch \
@@ -28,16 +29,19 @@ sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_src=$ip_addr, 
 sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_dst=$ip_addr, actions=NORMAL"
 
 # Some setups might not have 2 nics. In that case just use eth2
-if [ ! -d "/sys/class/net/eth3" ]
+if [ -d "/sys/class/net/eth2" ] && [ -d "/sys/class/net/eth3" ]
 then
-  sudo ovs-vsctl --may-exist add-port uplink_br0 eth2
-  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
-  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=output:eth2"
-else
   sudo ovs-vsctl --may-exist add-port uplink_br0 eth2
   sudo ovs-vsctl --may-exist add-port uplink_br0 eth3
   sudo ovs-ofctl -O OpenFlow14 add-group uplink_br0 "group_id=42, type=select, selection_method=dp_hash, bucket=actions=output:eth2, bucket=actions=output:eth3"
   sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
   sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth3, actions=output:uplink_patch"
   sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=group:42"
+elif [ -d "/sys/class/net/eth2" ]
+then
+  sudo ovs-vsctl --may-exist add-port uplink_br0 eth2
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=output:eth2"
+else
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, actions=LOCAL"
 fi
