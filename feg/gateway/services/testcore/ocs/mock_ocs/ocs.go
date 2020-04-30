46dee3474c095a2f3c4124b030b140775c381946
Oriol Batalla <obatalla@fb.com>
2020-04-30 10:13:30 -0700

    Gx/Gy client fixed to use lastDiamMessageReceived from PCRF/OCS (#1587)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1587
    
    This diff exposes the last message received on PCRF and OCS. This way we can see the
    message we sent without the need to make the Gx/Gy methods publics just for test purposes
    
    Reviewed By: themarwhal
    
    Differential Revision: D21312513
    
    fbshipit-source-id: b82a3b9946e6cf5890c70e6bd3bdf68a5ab5116d

--- feg/gateway/services/testcore/ocs/mock_ocs/ocs.go
+++ feg/gateway/services/testcore/ocs/mock_ocs/ocs.go
@@ -60,12 +60,12 @@ type OCSConfig struct {
 
 // OCSDiamServer wraps an OCS storing subscriber accounts and their credit
 type OCSDiamServer struct {
-	diameterSettings    *diameter.DiameterClientConfig
-	ocsConfig           *OCSConfig
-	accounts            map[string]*SubscriberAccount // map of IMSI to subscriber account
-	mux                 *sm.StateMachine
-	LastMessageReceived *ccrMessage
-	mockDriver          *mock_driver.MockDriver
+	diameterSettings        *diameter.DiameterClientConfig
+	ocsConfig               *OCSConfig
+	accounts                map[string]*SubscriberAccount // map of IMSI to subscriber account
+	mux                     *sm.StateMachine
+	lastDiamMessageReceived *diam.Message
+	mockDriver              *mock_driver.MockDriver
 }
 
 // NewOCSDiamServer initializes an OCS with an empty account map
@@ -281,6 +281,15 @@ func (srv *OCSDiamServer) ReAuth(
 	case <-time.After(10 * time.Second):
 		return nil, fmt.Errorf("No RAA received")
 	}
+}
+
+// GetLastAVPreceived gets the last message in diam format received
+// Message gets overwriten every time a new CCR is sent
+func (srv *OCSDiamServer) GetLastAVPreceived() (*diam.Message, error) {
+	if srv.lastDiamMessageReceived == nil {
+		return nil, fmt.Errorf("No AVP message received")
+	}
+	return srv.lastDiamMessageReceived, nil
 }
 
 func sendRAR(state *SubscriberSessionState, ratingGroup *uint32, cfg *sm.Settings) error {
