88996593be671bf1a8b6d2f441096383b540b1f8
Marie Bremner <marwhal@fb.com>
2020-05-01 10:33:13 -0700

    Pass a reference of the update criteria in the map, not the variable (#1598)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1598
    
    In `terminate_service` the update criteria in session_update map was not properly passed down to `SessionState` etc.  Fixing to pass the actual reference of the update.
    
    Reviewed By: ulaskozat
    
    Differential Revision: D21348390
    
    fbshipit-source-id: bfd022357beaa69c9fbaf488d83e94bd5c60bda8

--- lte/gateway/c/session_manager/LocalEnforcer.cpp
+++ lte/gateway/c/session_manager/LocalEnforcer.cpp
@@ -254,8 +254,7 @@ void LocalEnforcer::terminate_service(
   }
 
   for (const auto &session : it->second) {
-    auto update_criteria = session_update[imsi][session->get_session_id()];
-
+    auto& update_criteria = session_update[imsi][session->get_session_id()];
     session->start_termination(update_criteria);
 
     // tell AAA service to terminate radius session if necessary
