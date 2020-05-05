ad4c1984bc1f8ea879b21233f2e3cc29d4faca38
Karthik Shyam Krishnan Subraveti <ksubraveti@fb.com>
2020-05-05 09:53:43 -0700

    Fetch orc8r state from NMS and update Gateway KPI tray
    Summary: Add handlers to pull data from orc8r and display connected and disconnected gateways. Still figuring out how to pull severe event related info.
    
    Reviewed By: Scott8440
    
    Differential Revision: D21371145
    
    fbshipit-source-id: 3330e723f1fd7a412146629892aebaee968dbe5b

--- symphony/app/fbcnms-projects/magmalte/app/components/GatewayKPIs.js
+++ symphony/app/fbcnms-projects/magmalte/app/components/GatewayKPIs.js
@@ -13,20 +13,39 @@ import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CellWifiIcon from '@material-ui/icons/CellWifi';
 import Grid from '@material-ui/core/Grid';
-import React from 'react';
+import MagmaV1API from '@fbcnms/magma-api/client/WebClient';
+import React, {useEffect, useState} from 'react';
 import Text from '@fbcnms/ui/components/design-system/Text';
+import nullthrows from '@fbcnms/util/nullthrows';
+import {useEnqueueSnackbar} from '@fbcnms/ui/hooks/useSnackbar';
+import {useRouter} from '@fbcnms/ui/hooks';
+import type {lte_gateway} from '@fbcnms/magma-api';
 
-function fetchGatewayKPIs() {
-  const mockKPIs = [
-    {category: 'Severe Events', value: 10},
-    {category: 'Connected', value: 20},
-    {category: 'Disconnected', value: 30},
-  ];
-  return mockKPIs;
-}
+const GATEWAY_KEEPALIVE_TIMEOUT_MS = 1000 * 5 * 60;
 
 export default function GatewayKPIs() {
-  const mockKPIs = fetchGatewayKPIs();
+  const [gatewaySt, setGatewaySt] = useState({});
+  const {match} = useRouter();
+  const networkId: string = nullthrows(match.params.networkId);
+  const enqueueSnackbar = useEnqueueSnackbar();
+
+  useEffect(() => {
+    const fetchSt = async () => {
+      try {
+        const resp = await MagmaV1API.getLteByNetworkIdGateways({
+          networkId: networkId,
+        });
+        setGatewaySt(resp);
+      } catch (error) {
+        enqueueSnackbar('Error getting gateway information', {
+          variant: 'error',
+        });
+      }
+    };
+    fetchSt();
+  }, [networkId, enqueueSnackbar]);
+
+  const [upCount, downCount] = gatewayStatus(gatewaySt);
   return (
     <Grid container alignItems="center">
       <Grid item>
@@ -37,16 +56,57 @@ export default function GatewayKPIs() {
           </CardContent>
         </Card>
       </Grid>
-      {mockKPIs.map((kpi, i) => (
-        <Grid item key={i}>
-          <Card variant="outlined">
-            <CardHeader title={kpi.category} />
-            <CardContent>
-              <Text variant="h6">{kpi.value}</Text>
-            </CardContent>
-          </Card>
-        </Grid>
-      ))}
+      <Grid item>
+        <Card variant="outlined">
+          <CardHeader title="Severe Events" />
+          <CardContent>
+            <Text variant="h6" data-testid="Severe Events">
+              0
+            </Text>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card variant="outlined">
+          <CardHeader title="Connected" />
+          <CardContent>
+            <Text variant="h6" data-testid="Connected">
+              {upCount ?? 0}
+            </Text>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item>
+        <Card variant="outlined">
+          <CardHeader title="Disconnected" />
+          <CardContent>
+            <Text variant="h6" data-testid="Disconnected">
+              {downCount ?? 0}
+            </Text>
+          </CardContent>
+        </Card>
+      </Grid>
     </Grid>
   );
+}
+
+function gatewayStatus(gatewaySt: {[string]: lte_gateway}): [number, number] {
+  let upCount = 0;
+  let downCount = 0;
+  Object.keys(gatewaySt)
+    .map((k: string) => gatewaySt[k])
+    .filter((g: lte_gateway) => g.cellular && g.id)
+    .map(function(gateway: lte_gateway) {
+      const {status} = gateway;
+      let isBackhaulDown = true;
+      if (status != null) {
+        const checkin = status.checkin_time;
+        if (checkin != null) {
+          const duration = Date.now() - checkin;
+          isBackhaulDown = duration > GATEWAY_KEEPALIVE_TIMEOUT_MS;
+        }
+      }
+      isBackhaulDown ? downCount++ : upCount++;
+    });
+  return [upCount, downCount];
 }
