8d485cb8b62ac52978511374654e6ec49219c4ac
Hunter Gatewood <hcgatewood@fb.com>
2020-05-05 14:53:39 -0700

    Update clock to support skipping time.Sleep calls.
    Summary: Our services supports async jobs (reindexing, metrics collection, etc) which run as interval-paced jobs. Testing these async jobs in a more e2e manner requires monkey-patching `time.Sleep` to sleep a negligible duration instead of the full batch interval.
    
    Reviewed By: xjtian
    
    Differential Revision: D21029262
    
    fbshipit-source-id: 631f195ce38a712cf87d1677c801941ce86ed77b

--- orc8r/cloud/go/clock/doc.go
+++ orc8r/cloud/go/clock/doc.go
@@ -6,7 +6,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// Package clock provides a simple abstraction around Golang's time.Now().
-// This package exposes the ability to set and "feeze" the wall clock in
-// test code.
+// Package clock provides a simple abstraction around the standard time package.
+//	- time.Now() -- ability to set and "feeze" the wall clock in test code
+//	- time.Sleep() -- ability to skip sleeps
 package clock
