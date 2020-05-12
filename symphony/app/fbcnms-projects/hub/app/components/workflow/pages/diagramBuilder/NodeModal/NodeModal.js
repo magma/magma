131c85459ca516f036d5bf94ad8aaae21f722b53
Maros Marsalek <mmarsalek@frinx.io>
2020-05-12 09:03:07 +0200

    Fix eslint errors in hub/**/workflow/pages/diagramBuilder/NodeModal
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- symphony/app/fbcnms-projects/hub/app/components/workflow/pages/diagramBuilder/NodeModal/NodeModal.js
+++ symphony/app/fbcnms-projects/hub/app/components/workflow/pages/diagramBuilder/NodeModal/NodeModal.js
@@ -1,12 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { Button, Modal, Tab, Tabs } from "react-bootstrap";
-import { hash } from "../builder-utils";
-import GeneralTab from "./GeneralTab";
-import InputsTab from "./InputsTab";
-import { HttpClient as http } from "../../../common/HttpClient";
-import { conductorApiUrlPrefix } from "../../../constants";
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ * @format
+ */
+import GeneralTab from './GeneralTab';
+import InputsTab from './InputsTab';
+import React, {useEffect, useState} from 'react';
+import {Button, Modal, Tab, Tabs} from 'react-bootstrap';
+import {conductorApiUrlPrefix} from '../../../constants';
+import {hash} from '../builder-utils';
+import {HttpClient as http} from '../../../common/HttpClient';
 
-const OBJECT_KEYWORDS = ["template", "body"];
+const OBJECT_KEYWORDS = ['template', 'body'];
 
 const renameObjKey = (oldObj, oldKey, newKey) => {
   return Object.keys(oldObj).reduce((acc, val) => {
@@ -26,15 +35,17 @@ function NodeModal(props) {
     setName(props.inputs.inputs.name);
     setInputs(props.inputs.inputs);
 
-    const { subWorkflowParam } = props.inputs.inputs;
+    const {subWorkflowParam} = props.inputs.inputs;
 
     if (subWorkflowParam) {
-      const { name, version } = subWorkflowParam;
+      const {name, version} = subWorkflowParam;
       setName(name);
       setVersion(version);
 
       http
-        .get(conductorApiUrlPrefix + "/metadata/workflow/" + name + "/" + version)
+        .get(
+          conductorApiUrlPrefix + '/metadata/workflow/' + name + '/' + version,
+        )
         .then(res => {
           setInputParameters(res.result.inputParameters);
         });
@@ -47,38 +58,38 @@ function NodeModal(props) {
   }
 
   function addNewInputParam(param) {
-    let inputParameters = { ...inputs.inputParameters };
+    const inputParameters = {...inputs.inputParameters};
 
-    let updatedInputs = {
+    const updatedInputs = {
       ...inputs,
       inputParameters: {
         ...inputParameters,
-        [param]: "${workflow.input." + param + "}"
-      }
+        [param]: '${workflow.input.' + param + '}',
+      },
     };
 
     setInputs(updatedInputs);
   }
 
   function addRemoveHeader(handleOperation, i) {
-    let updatedInputs = { ...inputs };
-    let headers = updatedInputs["inputParameters"]["http_request"]["headers"];
+    const updatedInputs = {...inputs};
+    const headers = updatedInputs['inputParameters']['http_request']['headers'];
 
     if (handleOperation) {
-      let key = "key_" + hash();
-      headers[key] = "value_" + hash();
+      const key = 'key_' + hash();
+      headers[key] = 'value_' + hash();
     } else {
       delete headers[Object.keys(headers)[i]];
     }
-    updatedInputs["inputParameters"]["http_request"]["headers"] = headers;
+    updatedInputs['inputParameters']['http_request']['headers'] = headers;
     setInputs(updatedInputs);
   }
 
   function updateInputParams(value, key, entry) {
-    let updatedInputs = { ...inputs };
-    let inputParameters = updatedInputs.inputParameters;
+    let updatedInputs = {...inputs};
+    const inputParameters = updatedInputs.inputParameters;
 
-    if (typeof key[1] === "object") {
+    if (typeof key[1] === 'object') {
       if (OBJECT_KEYWORDS.find(e => entry[0].includes(e))) {
         try {
           value = JSON.parse(value);
@@ -92,40 +103,40 @@ function NodeModal(props) {
       ...updatedInputs,
       inputParameters: {
         ...inputParameters,
-        [entry[0]]: value
-      }
+        [entry[0]]: value,
+      },
     };
 
     setInputs(updatedInputs);
   }
 
   function updateHTTPHeader(value, i, headerKey) {
-    let copiedInputs = { ...inputs };
-    let headers = copiedInputs["inputParameters"]["http_request"]["headers"];
-    let header = Object.keys(headers)[i];
+    const copiedInputs = {...inputs};
+    const headers = copiedInputs['inputParameters']['http_request']['headers'];
+    const header = Object.keys(headers)[i];
 
     if (headerKey) {
-      copiedInputs["inputParameters"]["http_request"]["headers"] = renameObjKey(
+      copiedInputs['inputParameters']['http_request']['headers'] = renameObjKey(
         headers,
         header,
-        value
+        value,
       );
     } else {
-      copiedInputs["inputParameters"]["http_request"]["headers"][
+      copiedInputs['inputParameters']['http_request']['headers'][
         header
       ] = value;
     }
 
-    return copiedInputs["inputParameters"]["http_request"]["headers"];
+    return copiedInputs['inputParameters']['http_request']['headers'];
   }
 
   function updateHTTPRequest(value, key, entry, i, headerKey) {
-    let copiedInputs = { ...inputs };
+    let copiedInputs = {...inputs};
     let http_request = copiedInputs.inputParameters.http_request;
-    let inputParameters = copiedInputs.inputParameters;
+    const inputParameters = copiedInputs.inputParameters;
 
-    if (typeof key[1] === "object") {
-      if (entry[0] === "headers") {
+    if (typeof key[1] === 'object') {
+      if (entry[0] === 'headers') {
         value = updateHTTPHeader(value, i, headerKey);
       } else if (OBJECT_KEYWORDS.find(e => entry[0].includes(e))) {
         try {
@@ -136,10 +147,10 @@ function NodeModal(props) {
       }
     }
 
-    if (entry[0] === "method") {
-      if (value === "PUT" || value === "POST")
-        http_request = { ...http_request, body: "${workflow.input.body}" };
-      else delete http_request["body"];
+    if (entry[0] === 'method') {
+      if (value === 'PUT' || value === 'POST')
+        http_request = {...http_request, body: '${workflow.input.body}'};
+      else delete http_request['body'];
     }
 
     copiedInputs = {
@@ -148,22 +159,22 @@ function NodeModal(props) {
         ...inputParameters,
         http_request: {
           ...http_request,
-          [entry[0]]: value
-        }
-      }
+          [entry[0]]: value,
+        },
+      },
     };
 
     setInputs(copiedInputs);
   }
 
   function updateDecisionCase(value) {
-    let copiedInputs = { ...inputs };
-    let decisionCases = { ...copiedInputs.decisionCases };
-    let keyNames = Object.keys(decisionCases);
-    let falseCase = decisionCases[keyNames[0]] || [];
+    const copiedInputs = {...inputs};
+    let decisionCases = {...copiedInputs.decisionCases};
+    const keyNames = Object.keys(decisionCases);
+    const falseCase = decisionCases[keyNames[0]] || [];
 
     decisionCases = {
-      [value]: falseCase
+      [value]: falseCase,
     };
 
     copiedInputs.decisionCases = decisionCases;
@@ -172,21 +183,21 @@ function NodeModal(props) {
 
   function handleInput(value, key, entry, i, headerKey) {
     switch (key[0]) {
-      case "inputParameters":
+      case 'inputParameters':
         updateInputParams(value, key, entry);
         break;
-      case "headers":
-      case "http_request":
+      case 'headers':
+      case 'http_request':
         updateHTTPRequest(value, key, entry, i, headerKey);
         break;
-      case "decisionCases":
+      case 'decisionCases':
         updateDecisionCase(value);
         break;
       default: {
-        let copiedInputs = { ...inputs };
+        let copiedInputs = {...inputs};
         copiedInputs = {
           ...copiedInputs,
-          [key]: value
+          [key]: value,
         };
         setInputs(copiedInputs);
         break;
@@ -197,17 +208,17 @@ function NodeModal(props) {
   return (
     <Modal size="lg" show={props.show} onHide={props.modalHandler}>
       <Modal.Header>
-        <Modal.Title style={{ fontSize: "20px" }}>
+        <Modal.Title style={{fontSize: '20px'}}>
           {name} / {version}
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body style={{ padding: "30px" }}>
-        <Tabs style={{ marginBottom: "20px" }}>
-          {name !== "RAW" && // Only display general settings for non RAW tasks
+      <Modal.Body style={{padding: '30px'}}>
+        <Tabs style={{marginBottom: '20px'}}>
+          {name !== 'RAW' && ( // Only display general settings for non RAW tasks
             <Tab eventKey={1} title="General">
               <GeneralTab inputs={inputs} handleInput={handleInput} />
             </Tab>
-          }
+          )}
           <Tab eventKey={2} title="Input parameters">
             <InputsTab
               inputs={inputs}
