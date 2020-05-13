93fcae259e153d62023dda7f5b1555e3b6cb0555
Naor Matania <naorm1991@fb.com>
2020-05-13 04:10:48 -0700

    Fix routing from jobrunner to graph in deployment (#619)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/619
    
    The name of graph host is different between local environment (`graph`) and production environment (`inventory-graph`)
    
    Reviewed By: idoshveki
    
    Differential Revision: D21542058
    
    fbshipit-source-id: dc4a2b3038b0ed03c7391be2b33640b8251fd434

--- symphony/jobrunner/job_runner.go
+++ symphony/jobrunner/job_runner.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/facebookincubator/symphony/graph/graphgrpc/schema"
@@ -18,14 +19,24 @@ import (
 )
 
 const (
-	grpcAddr = "graph:443"
-	jobsURL  = "http://graph/jobs"
+	grpcAddr         = "%s:443"
+	jobsURL          = "http://%s/jobs/%s"
+	graphHostEnv     = "GRAPH_HOST"
+	defaultGraphHost = "graph"
 )
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
 
 func getTenantList(client schema.TenantServiceClient) []string {
 	var tenantNames []string
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+	log.Printf("getting tenant list")
 	tenants, err := client.List(ctx, &empty.Empty{})
 	if err != nil {
 		log.Fatalf("failed to fetch list of tenants: %v", err)
@@ -45,6 +56,7 @@ func runJob(url, tenant string) error {
 	req.Header.Add("x-auth-organization", tenant)
 	req.Header.Add("x-auth-automation-name", "job_runner")
 	req.Header.Add("x-auth-user-role", "OWNER")
+	log.Printf("running job on url %s, tenant %s", url, tenant)
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("failed to get response: %v", err)
@@ -64,7 +76,10 @@ func runJob(url, tenant string) error {
 }
 
 func RunJobOnAllTenants(jobs ...string) {
-	conn, err := grpc.Dial(grpcAddr, grpc.WithInsecure(), grpc.WithBlock())
+	graphHost := getEnv(graphHostEnv, defaultGraphHost)
+	address := fmt.Sprintf(grpcAddr, graphHost)
+	log.Printf("connecting to grpc server %s", address)
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
@@ -73,7 +88,7 @@ func RunJobOnAllTenants(jobs ...string) {
 	tenants := getTenantList(client)
 	for _, tenant := range tenants {
 		for _, job := range jobs {
-			url := fmt.Sprintf("%s/%s", jobsURL, job)
+			url := fmt.Sprintf(jobsURL, graphHost, job)
 			err := runJob(url, tenant)
 			if err != nil {
 				log.Printf("failed connecting url %s: %v", url, err)
