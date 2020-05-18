cf7dbc838a0a6d5ce761bbb5a503a78a5c234803
Vitaly Dorfman <vdorfman@fb.com>
2020-05-18 00:29:12 -0700

    symphony/graph: add policy for comment (#627)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/627
    
    add policy for comment
    
    Reviewed By: naor9991
    
    Differential Revision: D21612375
    
    fbshipit-source-id: e6c756ab5cb9b1d978f5b6791346902b62f20395

--- symphony/graph/authz/comment_rules.go
+++ symphony/graph/authz/comment_rules.go
@@ -1,13 +1,18 @@
+// Copyright (c) 2004-present Facebook All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package authz
 
 import (
 	"context"
 
-	"github.com/facebookincubator/symphony/graph/ent/comment"
-
 	"github.com/facebookincubator/symphony/graph/ent/predicate"
+	"github.com/facebookincubator/symphony/graph/ent/project"
+	"github.com/facebookincubator/symphony/graph/ent/projecttype"
 
 	"github.com/facebookincubator/symphony/graph/ent"
+	"github.com/facebookincubator/symphony/graph/ent/comment"
 	"github.com/facebookincubator/symphony/graph/ent/privacy"
 )
 
@@ -30,6 +35,74 @@ func CommentReadPolicyRule() privacy.QueryRule {
 					comment.HasProjectWith(projectPredicate)))
 		}
 		q.Where(predicates...)
+		return privacy.Skip
+	})
+}
+
+// CommentWritePolicyRule grants write permission to comment based on policy.
+func CommentWritePolicyRule() privacy.MutationRule {
+	return privacy.CommentMutationRuleFunc(func(ctx context.Context, m *ent.CommentMutation) error {
+		commentId, exists := m.ID()
+		if !exists {
+			return privacy.Skip
+		}
+		comm, err := m.Client().Comment.Query().
+			Where(comment.ID(commentId)).
+			WithWorkOrder().
+			WithProject().
+			Only(ctx)
+
+		if err != nil {
+			if !ent.IsNotFound(err) {
+				return privacy.Denyf("failed to fetch comment: %w", err)
+			}
+			return privacy.Skip
+		}
+		p := FromContext(ctx)
+		switch {
+		case comm.Edges.WorkOrder != nil:
+			return allowOrSkipWorkOrder(ctx, p, comm.Edges.WorkOrder)
+		case comm.Edges.Project != nil:
+			projectTypeID, err := comm.Edges.Project.QueryType().OnlyID(ctx)
+			if err != nil {
+				return privacy.Denyf("failed to fetch project type id: %w", err)
+			}
+			return privacyDecision(checkWorkforce(p.WorkforcePolicy.Data.Update, nil, &projectTypeID))
+		}
+		return privacy.Skip
+	})
+}
+
+// CommentCreatePolicyRule grants create permission to comment based on policy.
+func CommentCreatePolicyRule() privacy.MutationRule {
+	return privacy.CommentMutationRuleFunc(func(ctx context.Context, m *ent.CommentMutation) error {
+		if !m.Op().Is(ent.OpCreate) {
+			return privacy.Skip
+		}
+		p := FromContext(ctx)
+
+		if workOrderID, exists := m.WorkOrderID(); exists {
+			workOrder, err := m.Client().WorkOrder.Get(ctx, workOrderID)
+			if err != nil {
+				if !ent.IsNotFound(err) {
+					return privacy.Denyf("failed to fetch work order: %w", err)
+				}
+				return privacy.Skip
+			}
+			return allowOrSkipWorkOrder(ctx, p, workOrder)
+		}
+		if projectID, exists := m.ProjectID(); exists {
+			projectTypeID, err := m.Client().ProjectType.Query().
+				Where(projecttype.HasProjectsWith(project.ID(projectID))).
+				OnlyID(ctx)
+			if err != nil {
+				if ent.IsNotFound(err) {
+					return privacy.Skip
+				}
+				return privacy.Denyf("failed to fetch project type id: %w", err)
+			}
+			return privacyDecision(checkWorkforce(p.WorkforcePolicy.Data.Update, nil, &projectTypeID))
+		}
 		return privacy.Skip
 	})
 }
