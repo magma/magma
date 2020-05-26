9529aa14e82c35ae0e7d831892607ee5b4596b4b
Vitaly Dorfman <vdorfman@fb.com>
2020-05-26 10:17:36 -0700

    symphony/graph: add policy for surveyCellScan (#677)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/symphony/pull/677
    
    add policy for surveyCellScan
    
    Reviewed By: naor9991
    
    Differential Revision: D21722614
    
    fbshipit-source-id: ef387c880ce617f907d664404e971201d71a7b7d

--- symphony/graph/ent/schema/surveycellscan.go
+++ symphony/graph/ent/schema/surveycellscan.go
@@ -8,6 +8,7 @@ import (
 	"github.com/facebookincubator/ent"
 	"github.com/facebookincubator/ent/schema/edge"
 	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/symphony/graph/authz"
 )
 
 // SurveyCellScan holds the schema definition for the SurveyCellScan entity.
@@ -85,7 +86,7 @@ func (SurveyCellScan) Fields() []ent.Field {
 	}
 }
 
-// Edges of the SurveyCellScan.
+// Edges returns SurveyCellScan edges.
 func (SurveyCellScan) Edges() []ent.Edge {
 	return []ent.Edge{
 		edge.To("checklist_item", CheckListItem.Type).
@@ -95,4 +96,17 @@ func (SurveyCellScan) Edges() []ent.Edge {
 		edge.To("location", Location.Type).
 			Unique(),
 	}
+}
+
+// Policy returns SurveyCellScan policy.
+func (SurveyCellScan) Policy() ent.Policy {
+	return authz.NewPolicy(
+		authz.WithQueryRules(
+			authz.SurveyCellScanReadPolicyRule(),
+		),
+		authz.WithMutationRules(
+			authz.SurveyCellScanWritePolicyRule(),
+			authz.SurveyCellScanCreatePolicyRule(),
+		),
+	)
 }
