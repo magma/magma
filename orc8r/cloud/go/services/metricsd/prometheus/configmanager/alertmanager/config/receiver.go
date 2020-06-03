53a39564290fc87e08d6d8e5e27bdfd8bf92ce5f
Julian Christensen <julianchr@fb.com>
2020-06-03 16:15:58 -0700

    Add PagerDuty Support to AlertManager (#10)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/prometheus-configmanager/pull/10
    
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1767
    
    This diff provides the functionality to use PagerDuty with AlertManager, in addition to unit testing and documentation for the new feature.
    
    Note: this did not have any strange fields like the duration field for the PushoverConfig. I used existing fields for the images and links fields (in the prometheus config), as all the underlying structures were string, and no modification was required.
    
    Reviewed By: Scott8440
    
    Differential Revision: D21871913
    
    fbshipit-source-id: 4e9db8b779a71fecfc8f769cce1a29af40cd0c0d

--- orc8r/cloud/go/services/metricsd/prometheus/configmanager/alertmanager/config/receiver.go
+++ orc8r/cloud/go/services/metricsd/prometheus/configmanager/alertmanager/config/receiver.go
@@ -22,10 +22,11 @@ import (
 type Receiver struct {
 	Name string `yaml:"name" json:"name"`
 
-	SlackConfigs    []*SlackConfig    `yaml:"slack_configs,omitempty" json:"slack_configs,omitempty"`
-	WebhookConfigs  []*WebhookConfig  `yaml:"webhook_configs,omitempty" json:"webhook_configs,omitempty"`
-	EmailConfigs    []*EmailConfig    `yaml:"email_configs,omitempty" json:"email_configs,omitempty"`
-	PushoverConfigs []*PushoverConfig `yaml:"pushover_configs,omitempty" json:"pushover_configs,omitempty"`
+	SlackConfigs     []*SlackConfig     `yaml:"slack_configs,omitempty" json:"slack_configs,omitempty"`
+	WebhookConfigs   []*WebhookConfig   `yaml:"webhook_configs,omitempty" json:"webhook_configs,omitempty"`
+	EmailConfigs     []*EmailConfig     `yaml:"email_configs,omitempty" json:"email_configs,omitempty"`
+	PagerDutyConfigs []*PagerDutyConfig `yaml:"pagerduty_configs,omitempty" json:"pagerduty_configs,omitempty"`
+	PushoverConfigs  []*PushoverConfig  `yaml:"pushover_configs,omitempty" json:"pushover_configs,omitempty"`
 }
 
 // ReceiverJSONWrapper uses custom (JSON compatible) notifier configs to allow
@@ -33,10 +34,11 @@ type Receiver struct {
 type ReceiverJSONWrapper struct {
 	Name string `yaml:"name" json:"name"`
 
-	SlackConfigs    []*SlackConfig         `yaml:"slack_configs,omitempty" json:"slack_configs,omitempty"`
-	WebhookConfigs  []*WebhookConfig       `yaml:"webhook_configs,omitempty" json:"webhook_configs,omitempty"`
-	EmailConfigs    []*EmailConfig         `yaml:"email_configs,omitempty" json:"email_configs,omitempty"`
-	PushoverConfigs []*PushoverJSONWrapper `yaml:"pushover_configs,omitempty" json:"pushover_configs,omitempty"`
+	SlackConfigs     []*SlackConfig         `yaml:"slack_configs,omitempty" json:"slack_configs,omitempty"`
+	WebhookConfigs   []*WebhookConfig       `yaml:"webhook_configs,omitempty" json:"webhook_configs,omitempty"`
+	EmailConfigs     []*EmailConfig         `yaml:"email_configs,omitempty" json:"email_configs,omitempty"`
+	PagerDutyConfigs []*PagerDutyConfig     `yaml:"pagerduty_configs,omitempty" json:"pagerduty_configs,omitempty"`
+	PushoverConfigs  []*PushoverJSONWrapper `yaml:"pushover_configs,omitempty" json:"pushover_configs,omitempty"`
 }
 
 // Secure replaces the receiver's name with a tenantID prefix
@@ -108,6 +110,25 @@ type EmailConfig struct {
 	RequireTLS   *bool             `yaml:"require_tls,omitempty" json:"require_tls,omitempty"`
 }
 
+// PagerDutyConfig uses string instead of Secret for the RoutingKey and ServiceKey
+// field so that it is mashaled as is instead of being obscured which is how
+// alertmanager handles secrets. Otherwise the secrets would be obscured on
+// write to the yml file, making it unusable.
+type PagerDutyConfig struct {
+	HTTPConfig *common.HTTPConfig `yaml:"http_config,omitempty" json:"http_config,omitempty"`
+
+	RoutingKey  string                   `yaml:"routing_key,omitempty" json:"routing_key,omitempty"`
+	ServiceKey  string                   `yaml:"service_key,omitempty" json:"service_key,omitempty"`
+	URL         string                   `yaml:"url,omitempty" json:"url,omitempty"`
+	Client      string                   `yaml:"client,omitempty" json:"client,omitempty"`
+	ClientURL   string                   `yaml:"client_url,omitempty" json:"client_url,omitempty"`
+	Description string                   `yaml:"description,omitempty" json:"description,omitempty"`
+	Severity    string                   `yaml:"severity,omitempty" json:"severity,omitempty"`
+	Details     map[string]string        `yaml:"details,omitempty" json:"details,omitempty"`
+	Images      []*config.PagerdutyImage `yaml:"images,omitempty" json:"images,omitempty"`
+	Links       []*config.PagerdutyLink  `yaml:"links,omitempty" json:"links,omitempty"`
+}
+
 // PushoverConfig uses string instead of Secret for the UserKey and Token
 // field so that it is mashaled as is instead of being obscured which is how
 // alertmanager handles secrets. Otherwise the secrets would be obscured on
@@ -145,10 +166,11 @@ type PushoverJSONWrapper struct {
 // complexities surrounding JSON unmarshalling)
 func (r *ReceiverJSONWrapper) ToReceiverFmt() (Receiver, error) {
 	receiver := Receiver{
-		Name:           r.Name,
-		SlackConfigs:   r.SlackConfigs,
-		WebhookConfigs: r.WebhookConfigs,
-		EmailConfigs:   r.EmailConfigs,
+		Name:             r.Name,
+		SlackConfigs:     r.SlackConfigs,
+		WebhookConfigs:   r.WebhookConfigs,
+		EmailConfigs:     r.EmailConfigs,
+		PagerDutyConfigs: r.PagerDutyConfigs,
 	}
 
 	for _, p := range r.PushoverConfigs {
