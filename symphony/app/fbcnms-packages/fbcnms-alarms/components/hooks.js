0d7f00818377447c9b0a17a3ff645a1156491572
Alex Clavelle <clavelle@fb.com>
2020-06-08 10:28:00 -0700

    (fix): use tg tenant id in receivers UI
    Summary: This fix is high-pri.
    
    Reviewed By: Scott8440
    
    Differential Revision: D21912940
    
    fbshipit-source-id: 05857093cfc41d71d88a48bbf4d680c2e6b450fe

--- symphony/app/fbcnms-packages/fbcnms-alarms/components/hooks.js
+++ symphony/app/fbcnms-packages/fbcnms-alarms/components/hooks.js
@@ -126,7 +126,7 @@ export function useAlertRuleReceiver({
   const saveReceiver = React.useCallback(async () => {
     let updatedRoutes: AlertRoutingTree = response || {
       routes: [],
-      receiver: `${match.params.networkId || '0'}_tenant_base_route`,
+      receiver: `${match.params.networkId || 'tg'}_tenant_base_route`,
     };
     if (
       (receiver == null || receiver.trim() === '') &&
