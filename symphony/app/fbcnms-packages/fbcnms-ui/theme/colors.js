54ed5a1ac8b6f0b22912b435d7798593c279ae7b
Eric Fuhrmann <ekfuhrmann@gmail.com>
2020-06-15 09:49:32 -0400

    Revert

--- symphony/app/fbcnms-packages/fbcnms-ui/theme/colors.js
+++ symphony/app/fbcnms-packages/fbcnms-ui/theme/colors.js
@@ -9,54 +9,54 @@
  */
 
 export const purple = {
-  main: "#6200ee",
+  main: '#6200ee',
 };
 
-export const black = "#000";
-export const blue05 = "#ecf3ff";
-export const blue30 = "#aac9ff";
-export const blue60 = "#3578e5";
-export const blue70 = "#3984ff";
-export const blue80 = "#1d3c78";
-export const blueGrayDark = "#303846";
-export const brown = "#5d4037";
-export const cherry = "#ad1457";
-export const cyan = "#00acc1";
-export const darkGreen = "#2e7d32";
-export const darkPink = "#9c27b0";
-export const darkPurple = "#673ab7";
-export const fadedBlue = "rgba(53, 135, 229, 0.1)";
-export const fadedSea = "#7cc8d0";
-export const gray0 = "#f2f3f5";
-export const gray00 = "#f5f6f7";
-export const gray1 = "#979797";
-export const gray2 = "#4d4d4e";
-export const gray3 = "#dadde1";
-export const gray4 = "rgba(0,0,0,0.5)";
-export const gray5 = "rgba(0,0,0,0.3)";
-export const gray6 = "#f0f0f0";
-export const gray7 = "#F2F2F2";
-export const gray8 = "#b8c2d3";
-export const gray9 = "#cbcbcb";
-export const gray10 = "rgba(0, 0, 0, 0.87)";
-export const gray11 = "#ccd0d5";
-export const gray12 = "#bcbcbc";
-export const gray13 = "#73839e";
-export const gray14 = "#9da9be";
-export const gray50 = "#b3b3b3";
-export const green30 = "#00af5b";
-export const green = "#66bb6a";
-export const lightBlue = "#42a5f5";
-export const orange = "#f9a825";
-export const pink = "#f48fb1";
-export const primaryText = "#1c1e21";
-export const red = "#e53935";
-export const redwood = "#d45C43";
-export const sea100 = "#6ca0b6";
-export const sea200 = "#0097a7";
-export const sea300 = "#455a64";
-export const white = "#fff";
-export const yellow = "#ffeb3b";
+export const black = '#000';
+export const blue05 = '#ecf3ff';
+export const blue30 = '#aac9ff';
+export const blue60 = '#3578e5';
+export const blue70 = '#3984ff';
+export const blue80 = '#1d3c78';
+export const blueGrayDark = '#303846';
+export const brown = '#5d4037';
+export const cherry = '#ad1457';
+export const cyan = '#00acc1';
+export const darkGreen = '#2e7d32';
+export const darkPink = '#9c27b0';
+export const darkPurple = '#673ab7';
+export const fadedBlue = 'rgba(53, 135, 229, 0.1)';
+export const fadedSea = '#7cc8d0';
+export const gray0 = '#f2f3f5';
+export const gray00 = '#f5f6f7';
+export const gray1 = '#979797';
+export const gray2 = '#4d4d4e';
+export const gray3 = '#dadde1';
+export const gray4 = 'rgba(0,0,0,0.5)';
+export const gray5 = 'rgba(0,0,0,0.3)';
+export const gray6 = '#f0f0f0';
+export const gray7 = '#F5F7FC';
+export const gray8 = '#b8c2d3';
+export const gray9 = '#cbcbcb';
+export const gray10 = 'rgba(0, 0, 0, 0.87)';
+export const gray11 = '#ccd0d5';
+export const gray12 = '#bcbcbc';
+export const gray13 = '#73839e';
+export const gray14 = '#9da9be';
+export const gray50 = '#b3b3b3';
+export const green30 = '#00af5b';
+export const green = '#66bb6a';
+export const lightBlue = '#42a5f5';
+export const orange = '#f9a825';
+export const pink = '#f48fb1';
+export const primaryText = '#1c1e21';
+export const red = '#e53935';
+export const redwood = '#d45C43';
+export const sea100 = '#6ca0b6';
+export const sea200 = '#0097a7';
+export const sea300 = '#455a64';
+export const white = '#fff';
+export const yellow = '#ffeb3b';
 
 export default {
   black,
