6d125b798ebe057a960a92d898f7b8996f980705
Alejandro Rodriguez Aragonez <alexrod@fb.com>
2020-06-23 12:29:15 -0700

    Adding event logging support to MME
    Summary:
    - This diff adds event logging support so it can be used by MME, S1AP and SPGW to send RPC calls to eventd using `AsyncEventdClient` that sessiond already uses
    - Minor updates to AsyncEventdClient as it was missing #pragma directive on header file  and some duplicate includes
    
    Differential Revision: D22064640
    
    fbshipit-source-id: fd15c224c7ba5b08a77de1b802574ae00babccd5

---
+++ lte/gateway/c/oai/include/mme_events.h
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the Apache License, Version 2.0  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#pragma once
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common_types.h"
+
+/**
+ * Helper function to initiate AsyncEventdClient in its own thread
+ */
+int event_client_init(void);
+
+#ifdef __cplusplus
+}
+#endif
