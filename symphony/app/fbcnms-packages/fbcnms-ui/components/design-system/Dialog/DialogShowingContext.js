bac9f5e77179c31d8f64c9af539fdf0b21faf1eb
Amit Arbel <arbelamit@fb.com>
2020-06-24 01:01:00 -0700

    Creating DialogShowingContext
    Summary:
    Supporting showing dialog by function call.
    (Same implementation like done for with MessageShowingContext)
    
    Reviewed By: dlvhdr
    
    Differential Revision: D22040335
    
    fbshipit-source-id: dae9f8d480282eb1d2581a2e5dfad2859e667cfd

---
+++ symphony/app/fbcnms-packages/fbcnms-ui/components/design-system/Dialog/DialogShowingContext.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+import type {BaseDialogComponentProps, BaseDialogProps} from './BaseDialog';
+
+import * as React from 'react';
+import BaseDialog from './BaseDialog';
+import emptyFunction from '@fbcnms/util/emptyFunction';
+import {createContext, useCallback, useContext, useMemo, useState} from 'react';
+
+export type DialogShowingContextValue = $ReadOnly<{|
+  showDialog: BaseDialogProps => void,
+  hideDialog: () => void,
+|}>;
+
+const DialogShowingContext = createContext<DialogShowingContextValue>({
+  showDialog: emptyFunction,
+  hideDialog: emptyFunction,
+});
+
+export function useDialogShowingContext() {
+  return useContext(DialogShowingContext);
+}
+
+type Props = $ReadOnly<{|
+  children: React.Node,
+|}>;
+
+const EMPTY_HIDDEN_DIALOG_PROPS: BaseDialogComponentProps = {
+  title: null,
+  children: null,
+  onClose: emptyFunction,
+  hidden: true,
+};
+
+export function DialogShowingContextProvider(props: Props) {
+  const [
+    baseDialogProps,
+    setBaseDialogProps,
+  ] = useState<BaseDialogComponentProps>(EMPTY_HIDDEN_DIALOG_PROPS);
+
+  const showDialog = useCallback(
+    (props: BaseDialogProps) => {
+      if (baseDialogProps.hidden !== true) {
+        return;
+      }
+      setBaseDialogProps(props);
+    },
+    [baseDialogProps],
+  );
+  const hideDialog = useCallback(() => {
+    setBaseDialogProps(currentDialogProps => ({
+      ...currentDialogProps,
+      hidden: true,
+    }));
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      showDialog,
+      hideDialog,
+    }),
+    [hideDialog, showDialog],
+  );
+
+  return (
+    <DialogShowingContext.Provider value={value}>
+      {props.children}
+      <BaseDialog {...baseDialogProps} />
+    </DialogShowingContext.Provider>
+  );
+}
+
+export default DialogShowingContext;
