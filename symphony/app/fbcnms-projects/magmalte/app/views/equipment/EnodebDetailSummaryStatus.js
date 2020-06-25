bd81f6f0c559a62a359d994e2ee81f30932ae346
ekfuhrmann <ekfuhrmann@gmail.com>
2020-06-25 18:12:37 -0400

    Updated the status grid

--- symphony/app/fbcnms-projects/magmalte/app/views/equipment/EnodebDetailSummaryStatus.js
+++ symphony/app/fbcnms-projects/magmalte/app/views/equipment/EnodebDetailSummaryStatus.js
@@ -12,21 +12,63 @@ import type {EnodebInfo} from '../../components/lte/EnodebUtils';
 
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import DeviceStatusCircle from '@fbcnms/ui/components/icons/DeviceStatusCircle';
+import DeviceStatusCircle from '../../theme/design-system/DeviceStatusCircle';
 import Grid from '@material-ui/core/Grid';
 import React from 'react';
 
+import {colors} from '../../theme/default';
 import {isEnodebHealthy} from '../../components/lte/EnodebUtils';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles(theme => ({
+  kpiHeaderBlock: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: 0,
+  },
+  kpiHeaderContent: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  kpiHeaderIcon: {
+    fill: colors.primary.comet,
+    marginRight: theme.spacing(1),
+  },
+  kpiBlock: {
+    boxShadow: `0 0 0 1px ${colors.primary.concrete}`,
+  },
+  kpiLabel: {
+    color: colors.primary.comet,
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    width: props => (props.hasStatus ? 'calc(100% - 16px)' : '100%'),
+  },
+  kpiValue: {
+    color: colors.primary.brightGray,
+  },
+  kpiBox: {
+    width: '100%',
+  },
+}));
 
 export function EnodebSummary({enbInfo}: {enbInfo: EnodebInfo}) {
+  const classes = useStyles();
   return (
-    <Card variant={'outlined'}>
+    <Card elevation={0}>
       <CardHeader
         data-testid="eNodeB Serial Number"
         title="eNodeB Serial Number"
         subheader={enbInfo.enb.serial}
-        titleTypographyProps={{variant: 'caption'}}
-        subheaderTypographyProps={{variant: 'body1'}}
+        titleTypographyProps={{
+          variant: 'body3',
+          className: classes.kpiLabel,
+          title: 'Health',
+        }}
+        subheaderTypographyProps={{
+          variant: 'body1',
+          className: classes.kpiValue,
+        }}
       />
     </Card>
   );
@@ -36,25 +78,44 @@ export function EnodebSummary({enbInfo}: {enbInfo: EnodebInfo}) {
 // disabled indicates if the status color is to be grayed out
 // up/down indicates if we have to display status to be in green or in red
 function StatusIndicator(disabled: boolean, up: boolean, val: string) {
+  const props = {hasStatus: true};
+  const classes = useStyles(props);
   return (
-    <Grid container>
+    <Grid container zeroMinWidth alignItems="center" xs={12}>
       <Grid item>
         <DeviceStatusCircle isGrey={disabled} isActive={up} isFilled={true} />
       </Grid>
-      <Grid item>{val}</Grid>
+      <Grid item className={classes.kpiLabel}>
+        {val}
+      </Grid>
     </Grid>
   );
 }
 
 export function EnodebStatus({enbInfo}: {enbInfo: EnodebInfo}) {
+  const classes = useStyles();
   const isEnbHealthy = isEnodebHealthy(enbInfo);
   return (
-    <Grid container>
-      <Grid item xs={6}>
-        <Card variant={'outlined'}>
+    <Card elevation={0}>
+      <Grid container>
+        <Grid
+          container
+          xs={6}
+          zeroMinWidth
+          className={classes.kpiBlock}
+          alignItems="center">
           <CardHeader
-            titleTypographyProps={{variant: 'caption'}}
-            subheaderTypographyProps={{variant: 'body1'}}
+            className={classes.kpiBox}
+            titleTypographyProps={{
+              variant: 'body3',
+              className: classes.kpiLabel,
+              title: 'Health',
+            }}
+            subheaderTypographyProps={{
+              variant: 'body1',
+              className: classes.kpiValue,
+              title: isEnbHealthy ? 'Good' : 'Bad',
+            }}
             data-testid="Health"
             title="Health"
             subheader={StatusIndicator(
@@ -63,13 +124,27 @@ export function EnodebStatus({enbInfo}: {enbInfo: EnodebInfo}) {
               isEnbHealthy ? 'Good' : 'Bad',
             )}
           />
-        </Card>
-      </Grid>
-      <Grid item xs={6}>
-        <Card variant={'outlined'}>
+        </Grid>
+        <Grid
+          container
+          xs={6}
+          zeroMinWidth
+          className={classes.kpiBlock}
+          alignItems="center">
           <CardHeader
-            titleTypographyProps={{variant: 'caption'}}
-            subheaderTypographyProps={{variant: 'body1'}}
+            className={classes.kpiBox}
+            titleTypographyProps={{
+              variant: 'body3',
+              className: classes.kpiLabel,
+              title: 'Transmit Enabled',
+            }}
+            subheaderTypographyProps={{
+              variant: 'body1',
+              className: classes.kpiValue,
+              title: enbInfo.enb.config.transmit_enabled
+                ? 'Enabled'
+                : 'Disabled',
+            }}
             data-testid="Transmit Enabled"
             title="Transmit Enabled"
             subheader={StatusIndicator(
@@ -78,14 +153,25 @@ export function EnodebStatus({enbInfo}: {enbInfo: EnodebInfo}) {
               enbInfo.enb.config.transmit_enabled ? 'Enabled' : 'Disabled',
             )}
           />
-        </Card>
-      </Grid>
-
-      <Grid item xs={6}>
-        <Card variant={'outlined'}>
+        </Grid>
+        <Grid
+          container
+          xs={6}
+          zeroMinWidth
+          className={classes.kpiBlock}
+          alignItems="center">
           <CardHeader
-            titleTypographyProps={{variant: 'caption'}}
-            subheaderTypographyProps={{variant: 'body1'}}
+            className={classes.kpiBox}
+            titleTypographyProps={{
+              variant: 'body3',
+              className: classes.kpiLabel,
+              title: 'Gateway ID',
+            }}
+            subheaderTypographyProps={{
+              variant: 'body1',
+              className: classes.kpiValue,
+              title: enbInfo.enb_state.reporting_gateway_id ?? '',
+            }}
             data-testid="Gateway ID"
             title="Gateway ID"
             subheader={StatusIndicator(
@@ -94,14 +180,27 @@ export function EnodebStatus({enbInfo}: {enbInfo: EnodebInfo}) {
               enbInfo.enb_state.reporting_gateway_id ?? '',
             )}
           />
-        </Card>
-      </Grid>
-
-      <Grid item xs={6}>
-        <Card variant={'outlined'}>
+        </Grid>
+        <Grid
+          container
+          xs={6}
+          zeroMinWidth
+          className={classes.kpiBlock}
+          alignItems="center">
           <CardHeader
-            titleTypographyProps={{variant: 'caption'}}
-            subheaderTypographyProps={{variant: 'body1'}}
+            className={classes.kpiBox}
+            titleTypographyProps={{
+              variant: 'body3',
+              className: classes.kpiLabel,
+              title: 'Mme Connected',
+            }}
+            subheaderTypographyProps={{
+              variant: 'body1',
+              className: classes.kpiValue,
+              title: enbInfo.enb_state.mme_connected
+                ? 'Connected'
+                : 'Disconnected',
+            }}
             data-testid="Mme Connected"
             title="Mme Connected"
             subheader={StatusIndicator(
@@ -110,8 +209,8 @@ export function EnodebStatus({enbInfo}: {enbInfo: EnodebInfo}) {
               enbInfo.enb_state.mme_connected ? 'Connected' : 'Disconnected',
             )}
           />
-        </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </Card>
   );
 }
