3d239df01f3ba4fd63520715dbc28a1238cb436b
Hunter Gatewood <hcgatewood@fb.com>
2020-06-25 21:40:17 -0700

    Add cosmetic and quality of life updates for deployment scripts
    Summary:
    - Missing copyright headers
    - Spelling/grammar
    - Better error reporting / usage indication in build and deploy scripts
    
    Reviewed By: xjtian
    
    Differential Revision: D22016422
    
    fbshipit-source-id: a9ba0111184efebba06aa8e5b9a71c8c04acf533

--- orc8r/cloud/deploy/scripts/self_sign_certs.sh
+++ orc8r/cloud/deploy/scripts/self_sign_certs.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2016-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+# self_sign_certs.sh generates a set of keys and self-signed certificates.
+
+usage() {
+  echo "Usage: $0 DOMAIN_NAME"
+  exit 2
+}
+
+domain="$1"
+if [[ ${domain} == "" ]]; then
+    usage
+fi
+
 echo "##################"
 echo "Creating root CA.."
 echo "##################"
 openssl genrsa -out rootCA.key 2048
 openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 \
-      -out rootCA.pem -subj "/C=US/CN=rootca.$1"
+      -out rootCA.pem -subj "/C=US/CN=rootca.$domain"
 
 echo "##########################"
 echo "Creating controller cert.."
 echo "##########################"
 openssl genrsa -out controller.key 2048
 openssl req -new -key controller.key -out controller.csr \
-      -subj "/C=US/CN=*.$1"
+      -subj "/C=US/CN=*.$domain"
 openssl x509 -req -in controller.csr -CA rootCA.pem -CAkey rootCA.key \
       -CAcreateserial -out controller.crt -days 3650 -sha256
 
