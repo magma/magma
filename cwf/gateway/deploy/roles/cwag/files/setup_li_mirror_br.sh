82d19387048824f94f570027ab7ad74e54dd2bed
Nick Yurchenko <koolzz@fb.com>
2020-06-26 22:11:13 -0700

    Make uplink/li ports configurable for CWF
    Summary: uplink and li ports are now provided to ansible playbook and not static
    
    Reviewed By: pshelar
    
    Differential Revision: D22269961
    
    fbshipit-source-id: 3b0cf1679215939e1110390f0c8e401eeffbec83

--- cwf/gateway/deploy/roles/cwag/files/setup_li_mirror_br.sh
+++ cwf/gateway/deploy/roles/cwag/files/setup_li_mirror_br.sh
@@ -11,15 +11,15 @@ sudo ovs-vsctl --may-exist add-port cwag_br0 li_port -- set Interface li_port ty
 sudo ifconfig li_port up
 
 # LI might not be enabled for all setups
-if [ -d "/sys/class/net/eth4" ]
+if [ -d "/sys/class/net/$2" ]
 then
-  sudo ovs-vsctl --may-exist add-port cwag_br0 eth4
+  sudo ovs-vsctl --may-exist add-port cwag_br0 "$2"
 
   # Setup tc rules to mirror traffic to li mirror bridge
-  ip_addr=$(sudo /sbin/ifconfig eth1 | grep -m 1 inet | awk '{ print $2}')
-  sudo tc qdisc add dev eth1 handle ffff: ingress
-  sudo tc filter add dev eth1 parent ffff: protocol ip u32 match ip protocol 17 0xff  match ip dport 1812 0xfffe match ip dst "$ip_addr" action mirred egress mirror dev li_port
+  ip_addr=$(sudo /sbin/ifconfig $1 | grep -m 1 inet | awk '{ print $2}')
+  sudo tc qdisc add dev "$1" handle ffff: ingress
+  sudo tc filter add dev "$1" parent ffff: protocol ip u32 match ip protocol 17 0xff  match ip dport 1812 0xfffe match ip dst "$ip_addr" action mirred egress mirror dev li_port
 
-  sudo tc qdisc add dev eth1 handle 1: root prio
-  sudo tc filter add dev eth1 parent 1: protocol ip u32 match ip protocol 17 0xff match ip sport 1812 0xfffe match ip src "$ip_addr" action mirred egress mirror dev li_port
+  sudo tc qdisc add dev "$1" handle 1: root prio
+  sudo tc filter add dev "$1" parent 1: protocol ip u32 match ip protocol 17 0xff match ip sport 1812 0xfffe match ip src "$ip_addr" action mirred egress mirror dev li_port
 fi
--- orc8r/tools/docker/install_gateway.sh
+++ orc8r/tools/docker/install_gateway.sh
@@ -89,7 +89,7 @@ if [ "$GW_TYPE" == "$CWAG" ] || [ "$GW_TYPE" == "$XWF" ]; then
   apt-add-repository -y ppa:ansible/ansible
   apt-get update -y
   apt-get -y install ansible
-  ANSIBLE_CONFIG="$INSTALL_DIR"/magma/"$MODULE_DIR"/gateway/ansible.cfg ansible-playbook "$INSTALL_DIR"/magma/"$MODULE_DIR"/gateway/deploy/cwag.yml -i "localhost," -c local -v
+  ANSIBLE_CONFIG="$INSTALL_DIR"/magma/"$MODULE_DIR"/gateway/ansible.cfg ansible-playbook "$INSTALL_DIR"/magma/"$MODULE_DIR"/gateway/deploy/cwag.yml -i "localhost," -c local -v -e ingress_port="eth1" -e uplink_port="eth2 eth3" -e li_port="eth4"
 fi
 
 if [ "$GW_TYPE" == "$XWF" ]; then
