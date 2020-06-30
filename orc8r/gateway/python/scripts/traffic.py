b3a1bff7ff4c3f70b4d7b1c004b1852a015765c8
Joey Jiemjitpolchai <joeyjiem@fb.com>
2020-06-30 12:01:29 -0700

    Updated traffic script to scan for wifi
    Differential Revision: D22286624
    
    fbshipit-source-id: 2b5f431fcb71f63a469c32b31d59a5c78bb35e5f

--- orc8r/gateway/python/scripts/traffic.py
+++ orc8r/gateway/python/scripts/traffic.py
@@ -7,20 +7,26 @@ All rights reserved.
 This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
 """
+import os
+import sys
 
 
-def scan_wifi():
-    pass
-
+def scan_wifi(interface):
+    out = os.system('nmcli device wifi list | grep {}'.format(interface))
+    if out != 0:
+        raise Exception()
 
 def send_traffic():
     pass
 
 
-def main():
-    scan_wifi()
+def main(argv):
+    try:
+        scan_wifi(argv[1])
+    except Exception:
+        print('Error interface {} not found'.format(argv[1]))
     send_traffic()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
