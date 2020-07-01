5b13d0bf2c91b5b12633a70394b5bb3329ab5edc
Karthik Shyam Krishnan Subraveti <ksubraveti@fb.com>
2020-06-30 18:14:26 -0700

    Adding network provisioning component (#1872)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1872
    
    Adding network provisioning components for adding/editing networks.
    
    Reviewed By: Scott8440
    
    Differential Revision: D22265549
    
    fbshipit-source-id: 98821f2dc23c203de9d2771c11f5a57079f33949

--- symphony/app/fbcnms-projects/magmalte/app/views/network/NetworkKPIs.js
+++ symphony/app/fbcnms-projects/magmalte/app/views/network/NetworkKPIs.js
@@ -8,94 +8,60 @@
  * @flow strict-local
  * @format
  */
+import type {
+  apn,
+  enodeb,
+  lte_gateway,
+  rule_id,
+  subscriber,
+} from '../../../../../fbcnms-packages/fbcnms-magma-api';
 
 import CellWifiIcon from '@material-ui/icons/CellWifi';
 import KPITray from '../../components/KPITray';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import LoadingFiller from '@fbcnms/ui/components/LoadingFiller';
-import MagmaV1API from '@fbcnms/magma-api/client/WebClient';
 import PeopleIcon from '@material-ui/icons/People';
 import React from 'react';
 import RssFeedIcon from '@material-ui/icons/RssFeed';
 import SettingsInputAntennaIcon from '@material-ui/icons/SettingsInputAntenna';
-import nullthrows from '@fbcnms/util/nullthrows';
-import useMagmaAPI from '@fbcnms/ui/magma/useMagmaAPI';
 
-import {useRouter} from '@fbcnms/ui/hooks';
+type Props = {
+  lteGatwayResp: ?{[string]: lte_gateway},
+  enb: ?{[string]: enodeb},
+  subscriber: ?{[string]: subscriber},
+  policyRules: ?Array<rule_id>,
+  apns: ?{[string]: apn},
+};
 
-export default function NetworkKPI() {
-  const {match} = useRouter();
-  const networkId: string = nullthrows(match.params.networkId);
-  const {response: lteGatwayResp, isLoading: isLteRespLoading} = useMagmaAPI(
-    MagmaV1API.getLteByNetworkIdGateways,
-    {
-      networkId: networkId,
-    },
-  );
-
-  const {response: enb, isLoading: isEnbRespLoading} = useMagmaAPI(
-    MagmaV1API.getLteByNetworkIdEnodebs,
-    {
-      networkId: networkId,
-    },
-  );
-
-  const {response: policyRules, isLoading: isPolicyLoading} = useMagmaAPI(
-    MagmaV1API.getNetworksByNetworkIdPoliciesRules,
-    {
-      networkId: networkId,
-    },
-  );
-
-  const {response: subscriber, isLoading: isSubscriberLoading} = useMagmaAPI(
-    MagmaV1API.getLteByNetworkIdSubscribers,
-    {
-      networkId: networkId,
-    },
-  );
-
-  const {response: apns, isLoading: isAPNsLoading} = useMagmaAPI(
-    MagmaV1API.getLteByNetworkIdApns,
-    {
-      networkId: networkId,
-    },
-  );
-  if (
-    isLteRespLoading ||
-    isEnbRespLoading ||
-    isPolicyLoading ||
-    isSubscriberLoading ||
-    isAPNsLoading
-  ) {
-    return <LoadingFiller />;
-  }
+export default function NetworkKPI(props: Props) {
   return (
     <KPITray
       data={[
         {
           icon: CellWifiIcon,
           category: 'Gateways',
-          value: lteGatwayResp ? Object.keys(lteGatwayResp).length : 0,
+          value: props.lteGatwayResp
+            ? Object.keys(props.lteGatwayResp).length
+            : 0,
         },
         {
           icon: SettingsInputAntennaIcon,
           category: 'eNodeBs',
-          value: enb ? Object.keys(enb).length : 0,
+          value: props.enb ? Object.keys(props.enb).length : 0,
         },
         {
           icon: PeopleIcon,
           category: 'Subscribers',
-          value: subscriber ? Object.keys(subscriber).length : 0,
+          value: props.subscriber ? Object.keys(props.subscriber).length : 0,
         },
         {
           icon: LibraryBooksIcon,
           category: 'Policies',
-          value: policyRules ? policyRules.length : 0,
+          value: props.policyRules ? props.policyRules.length : 0,
         },
         {
           icon: RssFeedIcon,
           category: 'APNs',
-          value: apns ? Object.keys(apns).length : 0,
+          value: props.apns ? Object.keys(props.apns).length : 0,
         },
       ]}
     />
