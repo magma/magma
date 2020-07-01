8c86c4692c7852b08a9cfa29494bb1aa92853ad4
Nick Yurchenko <koolzz@fb.com>
2020-07-01 16:03:08 -0700

    Make uplink/li ports configurable for CWF (#1873)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1873
    
    uplink and li ports are now provided to ansible playbook and not static
    
    Reviewed By: pshelar
    
    Differential Revision: D22269961
    
    fbshipit-source-id: 0f67f4fe39620fdead5a7abb1757d3b957bf2bfe

--- cwf/gateway/deploy/roles/cwag/files/add_uplink_bridge_flows.sh
+++ cwf/gateway/deploy/roles/cwag/files/add_uplink_bridge_flows.sh
@@ -8,26 +8,26 @@
 # of patent rights can be found in the PATENTS file in the same directory.
 
 # gw0 configuration, TODO might want to avoid NORMAL for perf reasons
-ip_addr=$(/sbin/ifconfig gw0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
-ovs-ofctl add-flow uplink_br0 "table=0, priority=999, arp, nw_src=$ip_addr, actions=NORMAL"
-ovs-ofctl add-flow uplink_br0 "table=0, priority=999, arp, nw_dst=$ip_addr, actions=NORMAL"
-ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_src=$ip_addr, actions=NORMAL"
-ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_dst=$ip_addr, actions=NORMAL"
+ip_addr=$(sudo /sbin/ifconfig gw0 | grep -m 1 inet | awk '{ print $2}')
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, arp, nw_src=$ip_addr, actions=NORMAL"
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, arp, nw_dst=$ip_addr, actions=NORMAL"
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_src=$ip_addr, actions=NORMAL"
+sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=999, ip, nw_dst=$ip_addr, actions=NORMAL"
 
-# Some setups might not have 2 nics. In that case just use eth2
-if [ -d "/sys/class/net/eth2" ] && [ -d "/sys/class/net/eth3" ]
+# Some setups might not have 2 nics. In that case just use $1
+if [ -n "$1" ] && [ -n "$2" ]
 then
-  ovs-vsctl --may-exist add-port uplink_br0 eth2
-  ovs-vsctl --may-exist add-port uplink_br0 eth3
-  ovs-ofctl -O OpenFlow14 add-group uplink_br0 "group_id=42, type=select, selection_method=dp_hash, bucket=actions=output:eth2, bucket=actions=output:eth3"
-  ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
-  ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth3, actions=output:uplink_patch"
-  ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=group:42"
-elif [ -d "/sys/class/net/eth2" ]
+  sudo ovs-vsctl --may-exist add-port uplink_br0 "$1"
+  sudo ovs-vsctl --may-exist add-port uplink_br0 "$2"
+  sudo ovs-ofctl -O OpenFlow14 add-group uplink_br0 "group_id=42, type=select, selection_method=dp_hash, bucket=actions=output:$1, bucket=actions=output:$2"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=$1, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=$2, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=group:42"
+elif [ -n "$1" ]
 then
-  ovs-vsctl --may-exist add-port uplink_br0 eth2
-  ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=eth2, actions=output:uplink_patch"
-  ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=output:eth2"
+  sudo ovs-vsctl --may-exist add-port uplink_br0 "$1"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=$1, actions=output:uplink_patch"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, in_port=uplink_patch, actions=output:$1"
 else
-  ovs-ofctl add-flow uplink_br0 "table=0, priority=10, actions=LOCAL"
+  sudo ovs-ofctl add-flow uplink_br0 "table=0, priority=10, actions=LOCAL"
 fi
