be140854cb7eebe5ed7924d5d96d3ecd81d0e3b1
Pravin Bhanudas Shelar <pbshelar@fb.com>
2020-07-16 01:43:20 -0700

    magma: pipelineD: setup Non-NAT config only for LTE setup (#2829)
    Summary:
    Pull Request resolved: https://github.com/facebookexternal/fbc/pull/2829
    
    Pull Request resolved: https://github.com/facebookincubator/magma/pull/1964
    
    Reviewed By: koolzz
    
    Differential Revision: D22566371
    
    fbshipit-source-id: fe4f04bf1e4e4e66b447f33deeb4a19e2a27ac5c

--- lte/gateway/python/magma/pipelined/app/inout.py
+++ lte/gateway/python/magma/pipelined/app/inout.py
@@ -53,7 +53,8 @@ class InOutController(MagmaController):
     InOutConfig = namedtuple(
         'InOutConfig',
         ['gtp_port', 'uplink_port_name', 'mtr_ip', 'mtr_port', 'li_port_name',
-         'enable_nat', 'non_mat_gw_probe_frequency', 'non_nat_arp_egress_port'],
+         'enable_nat', 'non_mat_gw_probe_frequency', 'non_nat_arp_egress_port',
+         'setup_type'],
     )
     ARP_PROBE_FREQUENCY = 300
     UPLINK_DPCP_PORT_NAME = 'dhcp0'
@@ -94,6 +95,7 @@ class InOutController(MagmaController):
         li_port_name = None
         port_name = config_dict.get('ovs_uplink_port_name',
                                     self.UPLINK_PORT_NAME)
+        setup_type = config_dict.get('setup_type', None)
 
         if 'mtr_ip' in config_dict:
             self._mtr_service_enabled = True
@@ -117,6 +119,7 @@ class InOutController(MagmaController):
             enable_nat=enable_nat,
             non_mat_gw_probe_frequency=non_mat_gw_probe_freq,
             non_nat_arp_egress_port=non_nat_arp_egress_port,
+            setup_type=setup_type,
         )
 
     def initialize_on_connect(self, datapath):
@@ -322,6 +325,9 @@ class InOutController(MagmaController):
         """
         if self.config.enable_nat is True:
             self.logger.info("Nat is on")
+            return
+        elif self.config.setup_type != 'LTE':
+            self.logger.info("No GW MAC probe for %s", self.config.setup_type)
             return
         else:
             self.logger.info("Non nat conf: Frequency:%s, egress port: %s, uplink: %s",
