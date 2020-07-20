44978dc7df2e6973a9226f919e59fbf05625f698
Pravin Bhanudas Shelar <pbshelar@fb.com>
2020-07-20 15:32:51 -0700

    mobilityd: set default GW for ip pool allocator type
    Summary:
    mobility: set default GW for ip pool allocator type
    
    PR: https://github.com/facebookincubator/magma/pull/1980
    
    Reviewed By: ardzoht
    
    Differential Revision: D22633661
    
    fbshipit-source-id: 74009ccf7db71af044fca3ab6964d49b45809489

---
+++ lte/gateway/python/magma/mobilityd/tests/test_uplink_gw.py
@@ -0,0 +1,41 @@
+"""
+Copyright (c) 2020-present, Facebook, Inc.
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. An additional grant
+of patent rights can be found in the PATENTS file in the same directory.
+"""
+
+
+import logging
+import unittest
+from collections import defaultdict
+import subprocess
+
+from magma.mobilityd.uplink_gw import UplinkGatewayInfo
+
+LOG = logging.getLogger('mobilityd.def_gw.test')
+LOG.isEnabledFor(logging.DEBUG)
+
+
+class DefGwTest(unittest.TestCase):
+    """
+    Validate default router setting.
+    """
+    def setUp(self):
+        self.gw_store = defaultdict(str)
+        self.dhcp_gw_info = UplinkGatewayInfo(self.gw_store)
+
+    def test_gw_ip_for_DHCP(self):
+        self.assertEqual(self.dhcp_gw_info.getIP(), None)
+        self.assertEqual(self.dhcp_gw_info.getMac(), None)
+
+    def test_gw_ip_for_Ip_pool(self):
+        self.dhcp_gw_info.read_default_gw()
+
+        def_gw_cmd = "ip route show |grep default| awk '{print $3}'"
+        p = subprocess.Popen([def_gw_cmd], stdout=subprocess.PIPE, shell=True)
+        def_ip = p.stdout.read().decode("utf-8").strip()
+        self.assertEqual(self.dhcp_gw_info.getIP(), str(def_ip))
+        self.assertEqual(self.dhcp_gw_info.getMac(), None)
