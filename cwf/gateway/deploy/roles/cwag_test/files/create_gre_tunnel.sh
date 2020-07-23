f2a24bed06d5c51fa6e322980289c35bcfbc0e1f
Shruti Sanadhya <ssanadhya@fb.com>
2020-07-22 23:00:37 -0700

    Updated cwf

--- cwf/gateway/deploy/roles/cwag_test/files/create_gre_tunnel.sh
+++ cwf/gateway/deploy/roles/cwag_test/files/create_gre_tunnel.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 #
-# Copyright (c) 2018-present, Facebook, Inc.
-# All rights reserved.
-#
+# Copyright 2020 The Magma Authors.
+
 # This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# LICENSE file in the root directory of this source tree.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 sudo ovs-vsctl --may-exist add-br cwag_test_br0
 sudo ovs-vsctl --may-exist add-port cwag_test_br0 gre0 -- set interface gre0 type=gre options:remote_ip=192.168.70.101
