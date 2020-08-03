73adb192e9f8e1236d88ed6562a690c3c6e5c4c5
Karthik Subraveti <karthikshyam@gmail.com>
2020-08-03 04:39:55 -0700

    Add gateway upgrade component changes (#2126)
    Signed-off-by: Karthik Subraveti <karthikshyam@gmail.com>

---
+++ nms/app/fbcnms-projects/magmalte/app/components/context/GatewayTierContext.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2020 The Magma Authors.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @flow strict-local
+ * @format
+ */
+import type {gateway_id, tier, tier_id} from '@fbcnms/magma-api';
+
+import React from 'react';
+
+export type GatewayTierContextType = {
+  tiers: {[string]: tier},
+  supportedVersions: Array<string>,
+  updateTier: (key: string, val: tier) => Promise<void>,
+  removeTier: (key: string) => Promise<void>,
+  updateGatewayTier: (gatewayId: gateway_id, tierId: tier_id) => Promise<void>,
+};
+
+export default React.createContext<GatewayTierContextType>({});
