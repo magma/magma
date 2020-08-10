01d8b446d8a06bf97b4366b117c8b2c390f524c0
HannaFar <hannafarag159@gmail.com>
2020-08-10 19:03:55 +0200

    Add Network JSON editor
    Signed-off-by: HannaFar <hannafarag159@gmail.com>

--- nms/app/packages/magmalte/app/views/network/NetworkDashboard.js
+++ nms/app/packages/magmalte/app/views/network/NetworkDashboard.js
@@ -20,8 +20,10 @@ import type {
 } from '@fbcnms/magma-api';
 
 import AddEditNetworkButton from './NetworkEdit';
+import Button from '@material-ui/core/Button';
 import CardTitleRow from '../../components/layout/CardTitleRow';
 import Grid from '@material-ui/core/Grid';
+import JsonEditor from '../../components/JsonEditor';
 import LoadingFiller from '@fbcnms/ui/components/LoadingFiller';
 import MagmaV1API from '@fbcnms/magma-api/client/WebClient';
 import NetworkEpc from './NetworkEpc';
@@ -35,54 +37,38 @@ import useMagmaAPI from '@fbcnms/ui/magma/useMagmaAPI';
 
 import {NetworkCheck} from '@material-ui/icons';
 import {Redirect, Route, Switch} from 'react-router-dom';
+import {colors, typography} from '../../theme/default';
 import {makeStyles} from '@material-ui/styles';
 import {useCallback, useState} from 'react';
+import {useEnqueueSnackbar} from '@fbcnms/ui/hooks/useSnackbar';
 import {useRouter} from '@fbcnms/ui/hooks';
 
 const useStyles = makeStyles(theme => ({
   dashboardRoot: {
     margin: theme.spacing(5),
   },
+  appBarBtn: {
+    color: colors.primary.white,
+    background: colors.primary.comet,
+    fontFamily: typography.button.fontFamily,
+    fontWeight: typography.button.fontWeight,
+    fontSize: typography.button.fontSize,
+    lineHeight: typography.button.lineHeight,
+    letterSpacing: typography.button.letterSpacing,
+
+    '&:hover': {
+      background: colors.primary.mirage,
+    },
+  },
 }));
 
 export default function NetworkDashboard() {
-  const {relativePath, relativeUrl} = useRouter();
-
-  return (
-    <>
-      <TopBar
-        header="Network"
-        tabs={[
-          {
-            label: 'Network',
-            to: '/network',
-            icon: NetworkCheck,
-            filters: (
-              <AddEditNetworkButton title={'Add Network'} isLink={false} />
-            ),
-          },
-        ]}
-      />
-
-      <Switch>
-        <Route
-          path={relativePath('/network')}
-          component={NetworkDashboardInternal}
-        />
-        <Redirect to={relativeUrl('/network')} />
-      </Switch>
-    </>
-  );
-}
-
-function NetworkDashboardInternal() {
-  const {match} = useRouter();
   const classes = useStyles();
+
+  const {history, match, relativePath, relativeUrl} = useRouter();
   const networkId: string = nullthrows(match.params.networkId);
 
   const [networkInfo, setNetworkInfo] = useState<network>({});
-  const [epcConfigs, setEpcConfigs] = useState<network_epc_configs>({});
-  const [lteRanConfigs, setLteRanConfigs] = useState<network_ran_configs>({});
 
   const {isLoading: isInfoLoading} = useMagmaAPI(
     MagmaV1API.getNetworksByNetworkId,
@@ -93,6 +79,112 @@ function NetworkDashboardInternal() {
       setNetworkInfo(networkInfo);
     }, []),
   );
+  if (isInfoLoading) {
+    return <LoadingFiller />;
+  }
+  return (
+    <>
+      <TopBar
+        header="Network"
+        tabs={[
+          {
+            label: 'Network',
+            to: '/network',
+            icon: NetworkCheck,
+            filters: (
+              <Grid
+                container
+                justify="flex-end"
+                alignItems="center"
+                spacing={2}>
+                <Grid item>
+                  <AddEditNetworkButton title={'Add Network'} isLink={false} />
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={classes.appBarBtn}
+                    onClick={() => {
+                      history.push(relativeUrl('/json'));
+                    }}>
+                    Edit JSON
+                  </Button>
+                </Grid>
+              </Grid>
+            ),
+          },
+        ]}
+      />
+
+      <Switch>
+        <Route
+          path={relativePath('/json')}
+          render={() => (
+            <NetworkJsonConfig
+              network={networkInfo}
+              onSave={network => setNetworkInfo(network)}
+            />
+          )}
+        />
+        <Route
+          path={relativePath('/network')}
+          render={() => (
+            <NetworkDashboardInternal
+              network={networkInfo}
+              onSave={network => {
+                setNetworkInfo(network);
+              }}
+            />
+          )}
+        />
+        <Redirect to={relativeUrl('/network')} />
+      </Switch>
+    </>
+  );
+}
+
+type Props = {
+  network: network,
+  onSave?: network => void,
+};
+
+export function NetworkJsonConfig(props: Props) {
+  const {match} = useRouter();
+  const [error, setError] = useState('');
+  const networkId: string = nullthrows(match.params.networkId);
+  const enqueueSnackbar = useEnqueueSnackbar();
+
+  if (!(Object.keys(props.network).length > 0)) {
+    return <LoadingFiller />;
+  }
+  return (
+    <JsonEditor
+      content={props.network}
+      error={error}
+      onSave={async networkInfo => {
+        try {
+          await MagmaV1API.putNetworksByNetworkId({
+            networkId: networkId,
+            network: networkInfo,
+          });
+          enqueueSnackbar('Network saved successfully', {
+            variant: 'success',
+          });
+          setError('');
+          props.onSave?.(networkInfo);
+        } catch (e) {
+          setError(e.response?.data?.message ?? e.message);
+        }
+      }}
+    />
+  );
+}
+
+export function NetworkDashboardInternal(props: Props) {
+  const {match} = useRouter();
+  const classes = useStyles();
+  const networkId: string = nullthrows(match.params.networkId);
+  const [epcConfigs, setEpcConfigs] = useState<network_epc_configs>({});
+  const [lteRanConfigs, setLteRanConfigs] = useState<network_ran_configs>({});
   const {isLoading: isEpcLoading} = useMagmaAPI(
     MagmaV1API.getLteByNetworkIdCellularEpc,
     {
@@ -146,7 +238,6 @@ function NetworkDashboardInternal() {
 
   if (
     isEpcLoading ||
-    isInfoLoading ||
     isRanLoading ||
     isLteRespLoading ||
     isEnbRespLoading ||
@@ -157,10 +248,12 @@ function NetworkDashboardInternal() {
     return <LoadingFiller />;
   }
   const editProps = {
-    networkInfo: networkInfo,
+    networkInfo: props.network,
     lteRanConfigs: lteRanConfigs,
     epcConfigs: epcConfigs,
-    onSaveNetworkInfo: setNetworkInfo,
+    onSaveNetworkInfo: networkInfo => {
+      props.onSave?.(networkInfo);
+    },
     onSaveEpcConfigs: setEpcConfigs,
     onSaveLteRanConfigs: setLteRanConfigs,
   };
@@ -203,7 +296,6 @@ function NetworkDashboardInternal() {
       />
     );
   }
-
   return (
     <div className={classes.dashboardRoot}>
       <Grid container spacing={4}>
@@ -221,7 +313,7 @@ function NetworkDashboardInternal() {
           <Grid container spacing={4}>
             <Grid item xs={12}>
               <CardTitleRow label="Network" filter={editNetwork} />
-              <NetworkInfo networkInfo={networkInfo} />
+              <NetworkInfo networkInfo={props.network} />
             </Grid>
             <Grid item xs={12}>
               <CardTitleRow label="RAN" filter={editRAN} />
