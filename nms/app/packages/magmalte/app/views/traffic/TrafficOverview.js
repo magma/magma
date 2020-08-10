2b5d6ee06fd64630b7c17cdca23e0bb3abb3d435
HannaFar <hannafarag159@gmail.com>
2020-08-10 14:52:18 +0200

    Add policy JSON editor (#2182)
    Signed-off-by: HannaFar <hannafarag159@gmail.com>

--- nms/app/packages/magmalte/app/views/traffic/TrafficOverview.js
+++ nms/app/packages/magmalte/app/views/traffic/TrafficOverview.js
@@ -13,10 +13,14 @@
  * @flow strict-local
  * @format
  */
+import type {policy_rule} from '@fbcnms/magma-api';
+
 import ApnOverview from './ApnOverview';
 import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
 import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import LoadingFiller from '@fbcnms/ui/components/LoadingFiller';
+import MagmaV1API from '@fbcnms/magma-api/client/WebClient';
 import NestedRouteLink from '@fbcnms/ui/components/NestedRouteLink';
 import PolicyOverview from './PolicyOverview';
 import React from 'react';
@@ -24,11 +28,15 @@ import RssFeedIcon from '@material-ui/icons/RssFeed';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import Text from '@fbcnms/ui/components/design-system/Text';
+import nullthrows from '@fbcnms/util/nullthrows';
+import useMagmaAPI from '@fbcnms/ui/magma/useMagmaAPI';
 
 import {GetCurrentTabPos} from '../../components/TabUtils.js';
+import {PolicyJsonConfig} from './PolicyOverview';
 import {Redirect, Route, Switch} from 'react-router-dom';
 import {colors, typography} from '../../theme/default';
 import {makeStyles} from '@material-ui/styles';
+import {useCallback, useState} from 'react';
 import {useRouter} from '@fbcnms/ui/hooks';
 
 const POLICY_TITLE = 'Policies';
@@ -96,7 +104,21 @@ const useStyles = makeStyles(theme => ({
 export default function TrafficDashboard() {
   const classes = useStyles();
   const {relativePath, relativeUrl, match} = useRouter();
+  const networkId: string = nullthrows(match.params.networkId);
+  const [policies, setPolicies] = useState<{[string]: policy_rule}>({});
 
+  const {isLoading} = useMagmaAPI(
+    MagmaV1API.getNetworksByNetworkIdPoliciesRulesViewFull,
+    {
+      networkId: networkId,
+    },
+    useCallback(response => {
+      setPolicies(response);
+    }, []),
+  );
+  if (isLoading) {
+    return <LoadingFiller />;
+  }
   return (
     <>
       <div className={classes.topBar}>
@@ -134,7 +156,19 @@ export default function TrafficDashboard() {
       </AppBar>
 
       <Switch>
-        <Route path={relativePath('/policy')} component={PolicyOverview} />
+        <Route
+          path={relativePath('/policy/:policyId/json')}
+          render={() => (
+            <PolicyJsonConfig
+              policies={policies}
+              onSave={policy => setPolicies({...policies, [policy.id]: policy})}
+            />
+          )}
+        />
+        <Route
+          path={relativePath('/policy')}
+          render={() => <PolicyOverview policies={policies} />}
+        />
         <Route path={relativePath('/apn')} component={ApnOverview} />
         <Redirect to={relativeUrl('/policy')} />
       </Switch>
