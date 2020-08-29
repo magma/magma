549c253a230e4d1ee1bac53466bbf2bf9fb1fa0c
HannaFar <hannafarag159@gmail.com>
2020-08-29 05:30:28 +0200

    Add alert table in network Dashboard (#2522)
    Signed-off-by: HannaFar <hannafarag159@gmail.com>

--- nms/app/packages/magmalte/app/components/DashboardAlertTable.js
+++ nms/app/packages/magmalte/app/components/DashboardAlertTable.js
@@ -13,24 +13,77 @@
  * @flow strict-local
  * @format
  */
-
-import type {RowData} from './TabbedTable';
 import type {prom_firing_alert} from '@fbcnms/magma-api';
 
+import ActionTable from './ActionTable';
 import CardTitleRow from './layout/CardTitleRow';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import LoadingFiller from '@fbcnms/ui/components/LoadingFiller';
 import MagmaV1API from '@fbcnms/magma-api/client/WebClient';
+import Paper from '@material-ui/core/Paper';
 import React from 'react';
-import TabbedTable from './TabbedTable';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Text from '../theme/design-system/Text';
 import nullthrows from '@fbcnms/util/nullthrows';
 import useMagmaAPI from '@fbcnms/ui/magma/useMagmaAPI';
 
 import {Alarm} from '@material-ui/icons';
+import {colors, typography} from '../theme/default';
+import {makeStyles} from '@material-ui/styles';
 import {useRouter} from '@fbcnms/ui/hooks';
+import {useState} from 'react';
+import {withStyles} from '@material-ui/core/styles';
 
-type AlertTable = {[string]: Array<RowData>};
+const useStyles = makeStyles(theme => ({
+  tab: {
+    backgroundColor: colors.primary.white,
+    borderRadius: '4px 4px 0 0',
+    boxShadow: `inset 0 -2px 0 0 ${colors.primary.concrete}`,
+    '& + &': {
+      marginLeft: '4px',
+    },
+  },
+  emptyTable: {
+    backgroundColor: colors.primary.white,
+    padding: theme.spacing(4),
+    minHeight: '96px',
+  },
+  emptyTableContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    color: colors.primary.comet,
+  },
+  rowTitle: {
+    color: colors.primary.brightGray,
+  },
+  rowText: {
+    color: colors.primary.comet,
+  },
+}));
+
+const MagmaTabs = withStyles({
+  indicator: {
+    backgroundColor: colors.secondary.dodgerBlue,
+  },
+})(Tabs);
+
+const MagmaTab = withStyles({
+  root: {
+    fontFamily: typography.body1.fontFamily,
+    fontWeight: typography.body1.fontWeight,
+    fontSize: typography.body1.fontSize,
+    lineHeight: typography.body1.lineHeight,
+    letterSpacing: typography.body1.letterSpacing,
+    color: colors.primary.brightGray,
+    textTransform: 'none',
+  },
+})(Tab);
 
 type Severity = 'Critical' | 'Major' | 'Minor' | 'Other';
+
 const severityMap: {[string]: Severity} = {
   critical: 'Critical',
   page: 'Critical',
@@ -39,52 +92,138 @@ const severityMap: {[string]: Severity} = {
   minor: 'Minor',
 };
 
+type AlertRowType = {
+  label: string,
+  labelInfo: string,
+  annotations: string,
+  status: string,
+  timingInfo: Date,
+};
+
+type AlertTable = {[Severity]: Array<AlertRowType>};
+
 export default function () {
   const {match} = useRouter();
   const networkId: string = nullthrows(match.params.networkId);
-
   const {isLoading, response} = useMagmaAPI(
     MagmaV1API.getNetworksByNetworkIdAlerts,
     {
       networkId,
     },
   );
-
   if (isLoading) {
     return <LoadingFiller />;
   }
 
-  const data: AlertTable = {Critical: [], Major: [], Minor: [], Other: []};
   const alerts: Array<prom_firing_alert> = response ?? [];
+  const data: AlertTable = {Critical: [], Major: [], Minor: [], Other: []};
+
   alerts.forEach(alert => {
-    const labelInfo = {
-      job: alert.labels['job'] || '',
-      instance: alert.labels['instance'] || '',
-    };
-
-    const timingInfo = {
-      startsAt: alert.startsAt || '',
-      endsAt: alert.endsAt || '',
-      updatedAt: alert.updatedAt || '',
-    };
-
     const sev: Severity = severityMap[alert.labels['severity']] || 'Other';
-
     data[sev].push({
-      name: alert.labels['alertname'],
-      cols: [
-        JSON.stringify(labelInfo),
-        JSON.stringify(alert.annotations),
-        JSON.stringify(alert.status),
-        JSON.stringify(timingInfo),
-      ],
+      label: alert.labels.alertname,
+      labelInfo: `${alert.labels.job} - ${alert.labels.instance}`,
+      annotations: `${alert.annotations.description} - ${alert.annotations.summary}`,
+      status: alert.status.state,
+      timingInfo: new Date(alert.startsAt),
     });
   });
-
   return (
     <>
       <CardTitleRow icon={Alarm} label={`Alerts (${alerts.length})`} />
-      <TabbedTable data={data} />
+      <AlertsTabbedTable alerts={data} />
+    </>
+  );
+}
+type TabPanelProps = {
+  alerts: Array<AlertRowType>,
+  label: string,
+};
+
+function TabPanel(props: TabPanelProps) {
+  const classes = useStyles();
+  const {history, match} = useRouter();
+
+  if (props.alerts.length === 0) {
+    return (
+      <Paper elevation={0}>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          className={classes.emptyTable}>
+          <Grid item xs={12} className={classes.emptyTableContent}>
+            <Text variant="body2">You have 0 {props.label} Alerts</Text>
+            <Text variant="body3">
+              To add alert triggers click
+              <Link
+                onClick={() => {
+                  history.push(
+                    match.url.replace(`dashboard/network`, `alerts/alerts`),
+                  );
+                }}>
+                {' '}
+                alert settings
+              </Link>
+            </Text>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  }
+
+  return (
+    <ActionTable
+      data={props.alerts}
+      columns={[
+        {title: 'Label', field: 'label'},
+        {title: 'Label Info', field: 'labelInfo'},
+        {title: 'Annotations', field: 'annotations'},
+        {title: 'Status', field: 'status'},
+        {title: 'Date', field: 'timingInfo', type: 'datetime'},
+      ]}
+      options={{
+        actionsColumnIndex: -1,
+        pageSizeOptions: [5, 10],
+        toolbar: false,
+        header: false,
+      }}
+      localization={{
+        header: {actions: ''},
+      }}
+    />
+  );
+}
+
+type Props = {
+  alerts: AlertTable,
+};
+
+function AlertsTabbedTable(props: Props) {
+  const classes = useStyles();
+  const [currTabIndex, setCurrTabIndex] = useState<number>(0);
+  const severityTabs: Array<Severity> = ['Critical', 'Major', 'Minor', 'Other'];
+
+  return (
+    <>
+      <MagmaTabs
+        value={currTabIndex}
+        onChange={(_, newIndex: number) => setCurrTabIndex(newIndex)}
+        variant="fullWidth">
+        {Object.keys(props.alerts).map((k: Severity, idx: number) => {
+          return (
+            <MagmaTab
+              key={idx}
+              label={`${props.alerts[k].length} ${k}`}
+              className={classes.tab}
+            />
+          );
+        })}
+      </MagmaTabs>
+      <TabPanel
+        label={severityTabs[currTabIndex]}
+        alerts={props.alerts[severityTabs[currTabIndex]]}
+      />
     </>
   );
 }
