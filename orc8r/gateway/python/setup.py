d470b9eab65ed9719997549730c4fb96202c701b
Shruti Sanadhya <ssanadhya@users.noreply.github.com>
2020-08-31 13:38:26 -0700

    [agw][dev VMs] Use setuptools version less than 50.0.0 (#2546)
    Signed-off-by: Shruti Sanadhya <ssanadhya@fb.com>

--- orc8r/gateway/python/setup.py
+++ orc8r/gateway/python/setup.py
@@ -56,6 +56,7 @@ setup(
         'scripts/show_gateway_info.py',
     ],
     install_requires=[
+        'setuptools==49.6.0',
         'Cython>=0.29.1',
         'pystemd==0.5.0',
         'docker>=4.0.2',
