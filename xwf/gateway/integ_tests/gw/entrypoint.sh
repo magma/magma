7baee80b7c07ecd74922e5ace134358bbcc9e730
aharonnovo <aharonnovo@gmail.com>
2020-09-03 11:00:00 +0300

    [xwfm] suppoprt Ubuntu for xwfm ci docker

--- xwf/gateway/integ_tests/gw/entrypoint.sh
+++ xwf/gateway/integ_tests/gw/entrypoint.sh
@@ -24,13 +24,28 @@ curl -X POST  https://graph.expresswifi.com/openflow/configxwfm?access_token=$AC
 sed -i '/^uplink_if/d'  /etc/xwfwhoami # TODO: remove this
 
 echo "run XWF ansible"
-ANSIBLE_CONFIG=xwf/gateway/ansible.cfg ansible-playbook -e xwf_ctrl_ip="${CtrlIP}" xwf/gateway/deploy/xwf.yml -i "localhost," --skip-tags "install,install_docker,no_ci" -c local -v
+if [ -f /etc/redhat-release ]; then
+  REPO=cwf-prod-redhat
+  DIST=centos
+else
+  REPO=cwf-prod
+  DIST=bionic
+fi
+ANSIBLE_CONFIG=xwf/gateway/ansible.cfg ansible-playbook -e xwf_ctrl_ip="${CtrlIP}" xwf/gateway/deploy/xwf.yml \
+    -i "localhost,"  -e repo="${REPO}" -e distribution="${DIST}" --skip-tags "install,install_docker,no_ci" -c local -v
 
 echo "run DNS server"
 dnsmasq
 
-echo "run DHCP server"
-/usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcpd -group dhcpd --no-pid gw0 &
+
+SERVICE="dhcpd"
+if pgrep -x "$SERVICE" >/dev/null
+then
+    echo "$SERVICE is running"
+else
+    echo "run $SERVICE server"
+    /usr/sbin/dhcpd -f -cf /etc/dhcp/dhcpd.conf -user dhcpd -group dhcpd --no-pid gw0 &
+fi
 
 echo "loop forever"
 tail -f /dev/null
