ae913f2ace6bda328288e4fda292a6794ee39580
Pravin Shelar <pbshelar@fb.com>
2020-09-02 20:20:39 -0700

    pipelined: automate SGi management IP address assignment (#2602)
    when switching between NAT and Bridged mode we could loose IP,
    this patch cleanup IP when switching between these modes.
    
    Signed-off-by: Pravin B Shelar <pbshelar@fb.com>

--- lte/gateway/python/magma/pipelined/app/uplink_bridge.py
+++ lte/gateway/python/magma/pipelined/app/uplink_bridge.py
@@ -11,6 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import subprocess
+import threading
 from collections import namedtuple
 
 from magma.pipelined.app.base import MagmaController, ControllerType
@@ -90,14 +91,13 @@ class UplinkBridgeController(MagmaController):
         self._delete_all_flows()
         self._add_eth_port()
         self._set_vlan_eth_port()
-        self._set_sgi_static_ip()
         self._setup_vlan_pop_dev()
         # flows to forward traffic between patch port to eth port
 
         # 1. DHCP traffic
         match = "in_port=%s,ip,udp,tp_dst=68" % self.config.uplink_eth_port_name
         actions = "output:%s,output:%s,output:LOCAL" % (self.config.dhcp_port,
-                                                     self.config.uplink_patch)
+                                                        self.config.uplink_patch)
         self._install_flow(flows.MAXIMUM_PRIORITY, match, actions)
 
         # 2.a. all egress traffic
@@ -146,6 +146,7 @@ class UplinkBridgeController(MagmaController):
 
         # everything else:
         self._install_flow(flows.MINIMUM_PRIORITY, "", "NORMAL")
+        self._set_sgi_ip_addr(self.config.uplink_bridge)
 
     def cleanup_on_disconnect(self, datapath):
         self._del_eth_port()
@@ -182,14 +183,16 @@ class UplinkBridgeController(MagmaController):
         if self.config.enable_nat is True or \
                 self.config.uplink_eth_port_name is None:
             return
-
+        self._cleanup_if(self.config.uplink_eth_port_name)
+        # Add eth interface to OVS.
         ovs_add_port = "ovs-vsctl --may-exist add-port %s %s" \
                        % (self.config.uplink_bridge, self.config.uplink_eth_port_name)
-        self.logger.info("Add uplink port: %s", ovs_add_port)
         try:
             subprocess.Popen(ovs_add_port, shell=True).wait()
         except subprocess.CalledProcessError as ex:
             raise Exception('Error: %s failed with: %s' % (ovs_add_port, ex))
+
+        self.logger.info("Add uplink port: %s", ovs_add_port)
 
     def _set_vlan_eth_port(self):
         if self.config.uplink_bridge is None:
@@ -197,7 +200,7 @@ class UplinkBridgeController(MagmaController):
 
         if self.config.sgi_management_iface_vlan == '':
             vlan_cmd = "ovs-vsctl clear port %s tag" \
-                    % self.config.uplink_bridge
+                       % self.config.uplink_bridge
         else:
             vlan_cmd = "ovs-vsctl set port %s tag=%s" \
                        % (self.config.uplink_bridge,
@@ -210,42 +213,51 @@ class UplinkBridgeController(MagmaController):
             raise Exception('Error: %s failed with: %s' % (vlan_cmd, ex))
 
     def _del_eth_port(self):
+        self._cleanup_if(self.config.uplink_bridge)
+
         ovs_rem_port = "ovs-vsctl --if-exists del-port %s %s" \
                        % (self.config.uplink_bridge, self.config.uplink_eth_port_name)
-        self.logger.info("Remove ovs uplink port: %s", ovs_rem_port)
         try:
             subprocess.Popen(ovs_rem_port, shell=True).wait()
         except subprocess.CalledProcessError as ex:
             self.logger.debug("ignore port del error: %s ", ex)
+        self.logger.info("Remove ovs uplink port: %s", ovs_rem_port)
 
-    def _set_sgi_static_ip(self):
+        self._set_sgi_ip_addr(self.config.uplink_eth_port_name)
+
+    def _set_sgi_ip_addr(self, if_name: str):
         self.logger.debug("self.config.sgi_management_iface_ip_addr %s",
-                         self.config.sgi_management_iface_ip_addr)
+                          self.config.sgi_management_iface_ip_addr)
         if self.config.sgi_management_iface_ip_addr is None or \
                 self.config.sgi_management_iface_ip_addr == "":
+            self._restart_dhclient(if_name)
             return
 
         try:
             # Kill dhclient if running.
             pgrep_out = subprocess.Popen(["pgrep", "-f",
-                                          "dhclient.*" + self.config.uplink_bridge],
+                                          "dhclient.*" + if_name],
                                          stdout=subprocess.PIPE)
             for pid in pgrep_out.stdout.readlines():
                 subprocess.check_call(["kill", pid.strip()])
 
             flush_ip = ["ip", "addr", "flush",
-                        "dev" , self.config.uplink_bridge]
+                        "dev", if_name]
             subprocess.check_call(flush_ip)
 
             set_ip_cmd = ["ip",
                           "addr", "add",
                           self.config.sgi_management_iface_ip_addr,
                           "dev",
-                          self.config.uplink_bridge]
+                          if_name]
             subprocess.check_call(set_ip_cmd)
             self.logger.debug("SGi ip address config: [%s]", set_ip_cmd)
         except subprocess.SubprocessError as e:
             self.logger.warning("Error while setting SGi IP: %s", e)
+
+    def _restart_dhclient(self, if_name):
+        # restart DHCP client can take loooong time, process it in separate thread:
+        threading.Thread(target=self._restart_dhclient_if(if_name))
 
     def _setup_vlan_pop_dev(self):
         if self.config.ovs_vlan_workaround:
@@ -260,3 +272,32 @@ class UplinkBridgeController(MagmaController):
             BridgeTools.add_ovs_port(self.config.uplink_bridge,
                                      self.config.dev_vlan_out, "71")
 
+    def _cleanup_if(self, if_name):
+        # Release eth IP first.
+        release_eth_ip = ["dhclient", "-r", if_name]
+        try:
+            subprocess.check_call(release_eth_ip)
+        except subprocess.CalledProcessError as ex:
+            self.logger.info("could not release dhcp lease: %s, %s",
+                             release_eth_ip, ex)
+
+        flush_eth_ip = ["ip", "addr", "flush", "dev", if_name]
+        try:
+            subprocess.check_call(flush_eth_ip)
+        except subprocess.CalledProcessError as ex:
+            self.logger.info("could not flush ip addr: %s, %s",
+                             flush_eth_ip, ex)
+
+        self.logger.info("SGi DHCP: port [%s] ip removed", if_name)
+
+    def _restart_dhclient_if(self, if_name):
+        self._cleanup_if(if_name)
+
+        setup_dhclient = ["dhclient", if_name]
+        try:
+            subprocess.check_call(setup_dhclient)
+        except subprocess.CalledProcessError as ex:
+            self.logger.info("could not release dhcp lease: %s, %s",
+                             setup_dhclient, ex)
+
+        self.logger.info("SGi DHCP: restart for %s done", if_name)
