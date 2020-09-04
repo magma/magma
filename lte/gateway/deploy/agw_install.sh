90e6ea0ec8bcb263410c1b0ed7c371a92e2c3dc8
Timothee Dzik <timdzik@fb.com>
2020-09-04 14:46:01 -0400

    Replace way to set up eth0 in agw_install.sh (#2630)
    * Replace way to set up eth0 in agw_install.sh
    
    Signed-off-by: Timothée Dzik <timdzik@fb.com>
    
    * [AGW][OVS]Bump version of specific packages
    
    Signed-off-by: Timothée Dzik <timdzik@fb.com>

--- lte/gateway/deploy/agw_install.sh
+++ lte/gateway/deploy/agw_install.sh
@@ -36,7 +36,8 @@ if [[ ! $INTERFACES == *'eth0'*  ]] || [[ ! $INTERFACES == *'eth1'* ]] || ! grep
   sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
   # changing interface name
   grub-mkconfig -o /boot/grub/grub.cfg
-  sed -i 's/enp1s0/eth0/g' /etc/network/interfaces
+  echo "auto eth0
+  iface eth0 inet dhcp" > /etc/network/interfaces.d/eth0
   # configuring eth1
   echo "auto eth1
   iface eth1 inet static
