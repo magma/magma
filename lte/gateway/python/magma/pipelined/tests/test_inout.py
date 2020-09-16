91cbee44a55b96a6114c4cc4f1ca2035e619f63d
Pravin B Shelar <pbshelar@fb.com>
2020-09-15 20:54:15 -0700

    AGW: pipelineD: use enodeB specific tunnel port
    MME can create enodeB specifc port. Start using it traffic
    towards UE. It uses Reg8 to get Tunnel port no.
    
    Signed-off-by: Pravin B Shelar <pbshelar@fb.com>

--- lte/gateway/python/magma/pipelined/tests/test_inout.py
+++ lte/gateway/python/magma/pipelined/tests/test_inout.py
@@ -90,5 +90,70 @@ class InOutTest(unittest.TestCase):
         assert_bridge_snapshot_match(self, self.BRIDGE, self.service_manager)
 
 
+class InOutTestLTE(unittest.TestCase):
+    BRIDGE = 'testing_br'
+    IFACE = 'testing_br'
+    MAC_DEST = "5e:cc:cc:b1:49:4b"
+    BRIDGE_IP = '192.168.128.1'
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Starts the thread which launches ryu apps
+
+        Create a testing bridge, add a port, setup the port interfaces. Then
+        launch the ryu apps for testing pipelined. Gets the references
+        to apps launched by using futures.
+        """
+        super(InOutTestLTE, cls).setUpClass()
+        warnings.simplefilter('ignore')
+        cls.service_manager = create_service_manager([])
+
+        inout_controller_reference = Future()
+        testing_controller_reference = Future()
+        test_setup = TestSetup(
+            apps=[PipelinedController.InOut,
+                  PipelinedController.Testing,
+                  PipelinedController.StartupFlows],
+            references={
+                PipelinedController.InOut:
+                    inout_controller_reference,
+                PipelinedController.Testing:
+                    testing_controller_reference,
+                PipelinedController.StartupFlows:
+                    Future(),
+            },
+            config={
+                'setup_type': 'LTE',
+                'bridge_name': cls.BRIDGE,
+                'bridge_ip_address': cls.BRIDGE_IP,
+                'ovs_gtp_port_number': 32768,
+                'clean_restart': True,
+                'enable_nat': True,
+                'uplink_gw_mac': '11:22:33:44:55:66',
+                'mtr_ip': '1.2.3.4',
+                'ovs_mtr_port_number': 211
+            },
+            mconfig=None,
+            loop=None,
+            service_manager=cls.service_manager,
+            integ_test=False,
+        )
+
+        BridgeTools.create_bridge(cls.BRIDGE, cls.IFACE)
+
+        cls.thread = start_ryu_app_thread(test_setup)
+        cls.inout_controller = inout_controller_reference.result()
+        cls.testing_controller = testing_controller_reference.result()
+
+    @classmethod
+    def tearDownClass(cls):
+        stop_ryu_app_thread(cls.thread)
+        BridgeTools.destroy_bridge(cls.BRIDGE)
+
+    def testFlowSnapshotMatch(self):
+        assert_bridge_snapshot_match(self, self.BRIDGE, self.service_manager)
+
+
 if __name__ == "__main__":
     unittest.main()
