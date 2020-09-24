3301cef825936da4a6ab81cdd2cb720ce43af112
Karthik Subraveti <karthikshyam@gmail.com>
2020-09-24 16:24:59 -0700

    Update MagmaApiBindings. Latest update disrupts existing qos code for policies. Updated the code to ensure that we can set the qos fields in cwf policy dialog
    Signed-off-by: Karthik Subraveti <ksubraveti@fb.com>

--- nms/app/packages/magmalte/app/components/network/PolicyQosFields.js
+++ nms/app/packages/magmalte/app/components/network/PolicyQosFields.js
@@ -13,7 +13,7 @@
  * @flow strict-local
  * @format
  */
-import type {flow_qos} from '@fbcnms/magma-api';
+import type {policy_qos_profile} from '@fbcnms/magma-api';
 
 import Checkbox from '@material-ui/core/Checkbox';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -24,84 +24,82 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import React, {useState} from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
 import {makeStyles} from '@material-ui/styles';
+
 const MAX_BW_SETTING = 1000000000; // 1 gbps
 
 const useStyles = makeStyles(_ => ({
   input: {width: '100%'},
 }));
 
-export type QosState = {
-  enabled: boolean,
-  qos: flow_qos,
-};
-
 type Props = {
-  qos?: flow_qos,
-  onChange: QosState => void,
+  qosProfile: policy_qos_profile,
+  setQosProfile: policy_qos_profile => void,
+  qosEnabled: boolean,
+  setIsQosEnabled: boolean => void,
 };
 
 export default function PolicyQosFields(props: Props) {
   const classes = useStyles();
-  const [maxUlBw, setMaxUlBw] = useState(props.qos?.max_req_bw_ul ?? 1);
-  const [maxDlBw, setMaxDlBw] = useState(props.qos?.max_req_bw_dl ?? 1);
-  const [enabled, setEnabled] = useState(props.qos ? true : false);
+  const {qosProfile, setQosProfile, qosEnabled, setIsQosEnabled} = props;
+
   const maxBwSetting = 1000000000;
   const err = bw =>
     bw > 0 && bw < MAX_BW_SETTING
       ? null
       : `value must be between 1-${maxBwSetting}`;
-
-  const ulError = err(maxUlBw);
-  const dlError = err(maxDlBw);
+  const ulError = qosProfile?.max_req_bw_ul
+    ? err(qosProfile.max_req_bw_ul)
+    : null;
+  const dlError = qosProfile?.max_req_bw_dl
+    ? err(qosProfile.max_req_bw_dl)
+    : null;
   return (
     <ExpansionPanel>
       <ExpansionPanelSummary
         expandIcon={<ExpandMoreIcon />}
         aria-label="Expand">
         <FormControlLabel
-          checked={enabled}
+          checked={qosEnabled}
           onFocus={event => event.stopPropagation()}
           control={<Checkbox />}
-          onChange={({target}) => {
-            setEnabled(target.checked);
-            props.onChange({
-              enabled: target.checked,
-              qos: {
-                max_req_bw_ul: maxUlBw,
-                max_req_bw_dl: maxDlBw,
-              },
-            });
-          }}
+          onChange={({target}) => setIsQosEnabled(target.checked)}
           label="Enable"
           input={<Input id="qos_enabled" />}
         />
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
         <Grid container spacing={2} justify="center">
-          <Grid item xs={6}>
+          <Grid item xs={12}>
+            <TextField
+              className={classes.input}
+              label="Profile ID"
+              value={qosProfile?.id}
+              onChange={({target}) =>
+                setQosProfile({
+                  ...qosProfile,
+                  id: target.value,
+                })
+              }
+            />
+          </Grid>
+          <Grid item xs={12}>
             <TextField
               error={ulError !== null}
               className={classes.input}
               label="Max UL B/W"
-              margin="normal"
               type="number"
-              value={maxUlBw}
+              value={qosProfile?.max_req_bw_ul ?? 1}
               helperText={ulError}
-              onChange={({target}) => {
-                const bw = parseInt(target.value);
-                setMaxUlBw(bw);
-                props.onChange({
-                  enabled: enabled,
-                  qos: {
-                    max_req_bw_ul: bw,
-                    max_req_bw_dl: maxDlBw,
-                  },
-                });
-              }}
+              onChange={({target}) =>
+                setQosProfile({
+                  ...qosProfile,
+                  max_req_bw_ul: parseInt(target.value),
+                })
+              }
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">bps</InputAdornment>
@@ -109,26 +107,20 @@ export default function PolicyQosFields(props: Props) {
               }}
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               className={classes.input}
               error={dlError !== null}
               label="Max DL B/W"
-              margin="normal"
               type="number"
-              value={maxDlBw}
+              value={qosProfile?.max_req_bw_dl ?? 1}
               helperText={dlError}
-              onChange={({target}) => {
-                const bw = parseInt(target.value);
-                setMaxDlBw(bw);
-                props.onChange({
-                  enabled: enabled,
-                  qos: {
-                    max_req_bw_ul: maxUlBw,
-                    max_req_bw_dl: bw,
-                  },
-                });
-              }}
+              onChange={({target}) =>
+                setQosProfile({
+                  ...qosProfile,
+                  max_req_bw_dl: parseInt(target.value),
+                })
+              }
               InputProps={{
                 endAdornment: (
                   <InputAdornment position="end">bps</InputAdornment>
