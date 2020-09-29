bc2b9adc52b52869ce9b4581a93d23b574eaa075
Shaddi Hasan <shasan@fb.com>
2020-09-29 12:43:41 -0700

    Add GW portion of sms_orc8r service (#2950)
    Signed-off-by: Shaddi Hasan <shasan@fb.com>

---
+++ lte/gateway/c/oai/tasks/sms_orc8r/sms_orc8r_messages.h
@@ -0,0 +1,25 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the terms found in the LICENSE file in the root of this source tree.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#ifndef SMS_ORC8R_MESSAGES_H_
+#define SMS_ORC8R_MESSAGES_H_
+
+#include "intertask_interface.h"
+
+extern task_zmq_ctx_t sms_orc8r_task_zmq_ctx;
+
+#endif
