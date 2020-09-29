9e76b9fb7b0dedda789f8f67061804db34bd5ceb
emakeev <evgeniym@fb.com>
2020-09-29 12:46:11 -0700

    Add cloud managed configs for EAP-SIM provider (#2935)
    Signed-off-by: Evgeniy Makeev <evgeniym@fb.com>

--- feg/cloud/go/services/feg/obsidian/models/network_federation_configs_swaggergen.go
+++ feg/cloud/go/services/feg/obsidian/models/network_federation_configs_swaggergen.go
@@ -28,6 +28,10 @@ type NetworkFederationConfigs struct {
 	// Required: true
 	EapAka *EapAka `json:"eap_aka"`
 
+	// eap sim
+	// Required: true
+	EapSim *EapSim `json:"eap_sim"`
+
 	// gx
 	// Required: true
 	Gx *Gx `json:"gx"`
@@ -70,6 +74,10 @@ func (m *NetworkFederationConfigs) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateEapAka(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateEapSim(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -153,6 +161,24 @@ func (m *NetworkFederationConfigs) validateEapAka(formats strfmt.Registry) error
 		if err := m.EapAka.Validate(formats); err != nil {
 			if ve, ok := err.(*errors.Validation); ok {
 				return ve.ValidateName("eap_aka")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *NetworkFederationConfigs) validateEapSim(formats strfmt.Registry) error {
+
+	if err := validate.Required("eap_sim", "body", m.EapSim); err != nil {
+		return err
+	}
+
+	if m.EapSim != nil {
+		if err := m.EapSim.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("eap_sim")
 			}
 			return err
 		}
