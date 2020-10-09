0e93044b67c2d0f0d94a724df2443864e209f4d2
Jacky Tian <xjtian@fb.com>
2020-10-09 15:12:33 -0700

    Revert "[nms][cwag] Add CWAG HA status to NMS CWAG page (#3131)"
    This reverts commit bf8f87962a63fb3bcd077e768169baefc85c3607.

--- nms/app/packages/fbcnms-magma-api/__generated__/MagmaAPIBindings.js
+++ nms/app/packages/fbcnms-magma-api/__generated__/MagmaAPIBindings.js
@@ -132,12 +132,7 @@ export type carrier_wifi_gateway_health_status = {
     description: string,
     status: "HEALTHY" | "UNHEALTHY",
 };
-export type carrier_wifi_ha_pair_state = {
-    gateway1_health ? : carrier_wifi_gateway_health_status,
-    gateway2_health ? : carrier_wifi_gateway_health_status,
-    ha_pair_status ? : carrier_wifi_ha_pair_status,
-};
-export type carrier_wifi_ha_pair_status = {
+export type carrier_wifi_network_cluster_status = {
     active_gateway: string,
 };
 export type challenge_key = {
@@ -173,7 +168,6 @@ export type cwf_ha_pair = {
     gateway_id_1: string,
     gateway_id_2: string,
     ha_pair_id: string,
-    state ? : carrier_wifi_ha_pair_state,
 };
 export type cwf_ha_pair_configs = {
     transport_virtual_ip: string,
@@ -728,7 +722,6 @@ export type mutable_cwf_ha_pair = {
     config: cwf_ha_pair_configs,
     gateway_id_1: string,
     gateway_id_2: string,
-    ha_pair_id: string,
 };
 export type mutable_enodebd_e2e_test = {
     config: enodebd_test_config,
@@ -2275,7 +2268,7 @@ export default class MagmaAPIBindings {
     static async postCwfByNetworkIdHaPairs(
         parameters: {
             'networkId': string,
-            'haPair': mutable_cwf_ha_pair,
+            'haPair': cwf_ha_pair,
         }
     ): Promise < "Success" > {
         let path = '/cwf/{network_id}/ha_pairs';
