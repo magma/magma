1562e9a655f32b77cf219602d27d308e8e3cbb31
emakeev <evgeniym@fb.com>
2020-10-19 20:17:28 -0700

    Add UTRAN Vectors support to HSS Lite (#3219)
    Signed-off-by: Evgeniy Makeev <evgeniym@fb.com>

--- feg/gateway/services/testcore/hss/servicers/ai_test.go
+++ feg/gateway/services/testcore/hss/servicers/ai_test.go
@@ -96,7 +96,7 @@ func TestNewAIA_SuccessfulResponse(t *testing.T) {
 
 func TestNewAIA_MultipleVectors(t *testing.T) {
 	server := test_utils.NewTestHomeSubscriberServer(t)
-	air := createAIRExtended("sub1", 3)
+	air := createAIRExtended("sub1", 3, 0)
 	response, err := hss.NewAIA(server, air)
 	assert.NoError(t, err)
 
@@ -182,7 +182,7 @@ func TestValidateAIR_MissingEUTRANInfo(t *testing.T) {
 	m.NewAVP(avp.UserName, avp.Mbit, 0, datatype.UTF8String("magma"))
 	m.NewAVP(avp.VisitedPLMNID, avp.Mbit|avp.Vbit, diameter.Vendor3GPP, datatype.Unsigned32(0))
 
-	assert.EqualError(t, hss.ValidateAIR(m), "Missing requested E-UTRAN authentication info in message")
+	assert.EqualError(t, hss.ValidateAIR(m), "Missing requested E-UTRAN and UTRAN/GERAN authentication info in message")
 }
 
 func TestValidateAIR_MissingSessionId(t *testing.T) {
@@ -208,6 +208,10 @@ func TestValidateAIR_MissingSessionId(t *testing.T) {
 func TestValidateAIR_Success(t *testing.T) {
 	air := createAIR("sub1")
 	assert.NoError(t, hss.ValidateAIR(air))
+	air = createAIRExtended("sub2", 0, 1)
+	assert.NoError(t, hss.ValidateAIR(air))
+	air = createAIRExtended("sub2", 1, 1)
+	assert.NoError(t, hss.ValidateAIR(air))
 }
 
 // createBaseAIR outputs a mock authentication information request with only a
@@ -222,12 +226,12 @@ func createBaseAIR() *diam.Message {
 
 // createAIR outputs a mock authentication information request.
 func createAIR(userName string) *diam.Message {
-	return createAIRExtended(userName, 1)
+	return createAIRExtended(userName, 1, 0)
 }
 
 // createAIRExtended outputs a mock authentication information request.
 // It allows specifying more options than createAIR.
-func createAIRExtended(userName string, numRequestedVectors uint32) *diam.Message {
+func createAIRExtended(userName string, numRequestedVectors, numRequestedUtranVectors uint32) *diam.Message {
 	m := createBaseAIR()
 	m.NewAVP(avp.SessionID, avp.Mbit, 0, datatype.UTF8String("magma;123_1234"))
 	m.NewAVP(avp.UserName, avp.Mbit, 0, datatype.UTF8String(userName))
@@ -244,6 +248,20 @@ func createAIRExtended(userName string, numRequestedVectors uint32) *diam.Messag
 		},
 	}
 	m.NewAVP(avp.RequestedEUTRANAuthenticationInfo, avp.Vbit|avp.Mbit, diameter.Vendor3GPP, authInfo)
+	if numRequestedUtranVectors > 0 {
+		authInfo := &diam.GroupedAVP{
+			AVP: []*diam.AVP{
+				diam.NewAVP(
+					avp.NumberOfRequestedVectors,
+					avp.Vbit|avp.Mbit,
+					diameter.Vendor3GPP,
+					datatype.Unsigned32(numRequestedUtranVectors)),
+				diam.NewAVP(
+					avp.ImmediateResponsePreferred, avp.Vbit|avp.Mbit, diameter.Vendor3GPP, datatype.Unsigned32(0)),
+			},
+		}
+		m.NewAVP(avp.RequestedUTRANGERANAuthenticationInfo, avp.Vbit|avp.Mbit, diameter.Vendor3GPP, authInfo)
+	}
 	return m
 }
 
@@ -251,7 +269,7 @@ func TestNewSuccessfulAIA(t *testing.T) {
 	server := test_utils.NewTestHomeSubscriberServer(t)
 	serverCfg := server.Config.Server
 
-	msg := createAIR("user1")
+	msg := createAIRExtended("user1", 1, 1)
 	var air definitions.AIR
 	err := msg.Unmarshal(&air)
 	assert.NoError(t, err)
@@ -260,9 +278,18 @@ func TestNewSuccessfulAIA(t *testing.T) {
 	copy(vector.Rand[:], []byte("\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f"))
 	copy(vector.Xres[:], []byte("\x2d\xaf\x87\x3d\x73\xf3\x10\xc6"))
 	copy(vector.Autn[:], []byte("o\xbf\xa3\x80\x1fW\x80\x00{\xdeY\x88n\x96\xe4\xfe"))
-	copy(vector.Kasme[:], []byte("\x87H\xc1\xc0\xa2\x82o\xa4\x05\xb1\xe2~\xa1\x04CJ\xe5V\xc7e\xe8\xf0a\xeb\xdb\x8a\xe2\x86\xc4F\x16\xc2"))
+	copy(vector.Kasme[:],
+		[]byte("\x87H\xc1\xc0\xa2\x82o\xa4\x05\xb1\xe2~\xa1\x04CJ\xe5V\xc7e\xe8\xf0a\xeb\xdb\x8a\xe2\x86\xc4F\x16\xc2"))
 
-	response := server.NewSuccessfulAIA(msg, air.SessionID, []*crypto.EutranVector{vector})
+	utranVector := &crypto.UtranVector{}
+	copy(utranVector.Rand[:], []byte("\xb0\x02\x7e\x16\x95\x59\xc4\x8a\x2d\xc8\x3d\xb6\xb7\x6b\x77\xb5"))
+	copy(utranVector.Xres[:], []byte("\xc8\x8e\x71\xfa\x5a\x1b\x41\x50"))
+	copy(utranVector.Autn[:], []byte("\x56\x63\xa4\x78\x7f\x61\x80\x00\x8d\x45\x64\x77\x96\x06\x39\x22"))
+	copy(utranVector.ConfidentialityKey[:], []byte("\x5e\x8d\xc7\x0e\x0d\xfa\xa5\xde\x81\x90\xe9\xc5\x98\x6a\xc2\x23"))
+	copy(utranVector.IntegrityKey[:], []byte("\xf2\xc1\x9c\x15\xf5\x5a\xf1\xe8\xbb\xdb\x76\x19\x30\xeb\xef\x7c"))
+
+	response := server.NewSuccessfulAIA(
+		msg, air.SessionID, []*crypto.EutranVector{vector}, []*crypto.UtranVector{utranVector})
 	var aia definitions.AIA
 	err = response.Unmarshal(&aia)
 	assert.NoError(t, err)
@@ -280,4 +307,11 @@ func TestNewSuccessfulAIA(t *testing.T) {
 	assert.Equal(t, datatype.OctetString(vector.Xres[:]), vec.XRES)
 	assert.Equal(t, datatype.OctetString(vector.Autn[:]), vec.AUTN)
 	assert.Equal(t, datatype.OctetString(vector.Kasme[:]), vec.KASME)
+
+	uvec := ai.UtranVectors[0]
+	assert.Equal(t, datatype.OctetString(utranVector.Rand[:]), uvec.RAND)
+	assert.Equal(t, datatype.OctetString(utranVector.Xres[:]), uvec.XRES)
+	assert.Equal(t, datatype.OctetString(utranVector.Autn[:]), uvec.AUTN)
+	assert.Equal(t, datatype.OctetString(utranVector.ConfidentialityKey[:]), uvec.CK)
+	assert.Equal(t, datatype.OctetString(utranVector.IntegrityKey[:]), uvec.IK)
 }
