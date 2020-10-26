407d544d8009bf24c3d3817827501c5f533a418c
Ulas Kozat <kozat@fb.com>
2020-10-26 16:45:25 -0700

    [lte][agw] Handle out of order E-RAB set up response for dedicated bearers (#3424)
    * Handle out of order ERAB response for dedicated bearers
    
    Signed-off-by: Ulas Kozat <kozat@fb.com>

--- lte/gateway/c/oai/tasks/nas/esm/sap/esm_recv.c
+++ lte/gateway/c/oai/tasks/nas/esm/sap/esm_recv.c
@@ -42,10 +42,6 @@
 #include "mme_app_itti_messaging.h"
 #include "mme_app_state.h"
 
-#define ERAB_SETUP_RSP_COUNTER_MAX 5
-// TODO - Make it configurable
-#define ERAB_SETUP_RSP_TMR 5  // In secs
-
 /****************************************************************************/
 /****************  E X T E R N A L    D E F I N I T I O N S  ****************/
 /****************************************************************************/
