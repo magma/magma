fc84315946382bdf97290511f431ab13be848aca
Hunter Gatewood <hgatewood@gmail.com>
2020-11-05 18:34:00 -0800

    [orc8r][mesh] Hoist orc8r services into top level containers
    Signed-off-by: Hunter Gatewood <hgatewood@gmail.com>

--- orc8r/cloud/docker/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-if [ "$1" == "-all" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.metrics.yml up -d
-elif [ "$1" == "-thanos" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.metrics.yml -f docker-compose.thanos.yml up -d
-else
-    docker-compose up -d
-fi
