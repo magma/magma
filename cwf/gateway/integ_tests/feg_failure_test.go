9b7386767e2fae9ee4dba0620e167cac9f744b9f
Marie Bremner <marwhal@fb.com>
2020-11-16 14:09:59 -0600

    Simulate mid-session feg disconnect
    Signed-off-by: Marie Bremner <marwhal@fb.com>

--- cwf/gateway/integ_tests/feg_failure_test.go
+++ cwf/gateway/integ_tests/feg_failure_test.go
@@ -19,8 +19,11 @@ import (
 	"fmt"
 	"testing"
 
-	fegprotos "magma/feg/cloud/go/protos"
+	cwfprotos "magma/cwf/cloud/go/protos"
+	"magma/feg/cloud/go/protos"
 
+	"github.com/fiorix/go-diameter/v4/diam"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -43,22 +46,71 @@ func TestLinkFailureCWAGtoFEG(t *testing.T) {
 	}()
 
 	ues, err := tr.ConfigUEs(1)
+	ue := ues[0]
+	imsi := ue.Imsi
 	assert.NoError(t, err)
 	setNewOCSConfig(
-		&fegprotos.OCSConfig{
-			MaxUsageOctets: &fegprotos.Octets{TotalOctets: GyMaxUsageBytes},
+		&protos.OCSConfig{
+			MaxUsageOctets: &protos.Octets{TotalOctets: GyMaxUsageBytes},
 			MaxUsageTime:   GyMaxUsageTime,
 			ValidityTime:   GyValidityTime,
 			UseMockDriver:  true,
 		},
 	)
 
-	ue := ues[0]
 	tr.WaitForPoliciesToSync()
-	tr.AuthenticateAndAssertFail(ue.Imsi)
+	tr.AuthenticateAndAssertFail(imsi)
 
 	// Since CCA-I was never received, there should be no rules installed
 	recordsBySubID, err := tr.GetPolicyUsage()
 	assert.NoError(t, err)
-	assert.Empty(t, recordsBySubID["IMSI"+ue.Imsi])
+	assert.Empty(t, recordsBySubID[prependIMSIPrefix(imsi)])
+}
+
+// FeG connection breaks mid way through a session
+func TestLinkFailureCWAGtoFEGMidSession(t *testing.T) {
+	fmt.Println("\nRunning TestLinkFailureCWAGtoFEGMidSession...")
+
+	tr := NewTestRunner(t)
+	ruleManager, err := NewRuleManager()
+	assert.NoError(t, err)
+
+	assert.NoError(t, usePCRFMockDriver())
+
+	defer func() {
+		// Clear hss, ocs, and pcrf
+		assert.NoError(t, clearPCRFMockDriver())
+		assert.NoError(t, ruleManager.RemoveInstalledRules())
+		assert.NoError(t, tr.CleanUp())
+		assert.NoError(t, tr.RestartService("ingress"))
+	}()
+	ues, err := tr.ConfigUEs(1)
+	assert.NoError(t, err)
+	ue := ues[0]
+	imsi := ue.GetImsi()
+	usageMonitorInfo := getUsageInformation("mkey1", 250*KiloBytes)
+
+	initRequest := protos.NewGxCCRequest(imsi, protos.CCRequestType_INITIAL)
+	initAnswer := protos.NewGxCCAnswer(diam.Success).
+		SetDynamicRuleInstall(getPassAllRuleDefinition("dynamic-pass-all", "mkey1", nil, 100)).
+		SetUsageMonitorInfo(usageMonitorInfo)
+	initExpectation := protos.NewGxCreditControlExpectation().Expect(initRequest).Return(initAnswer)
+	// return success with credit on unexpected requests
+	defaultAnswer := protos.NewGxCCAnswer(2001).SetUsageMonitorInfo(usageMonitorInfo)
+	assert.NoError(t, setPCRFExpectations([]*protos.GxCreditControlExpectation{initExpectation}, defaultAnswer))
+
+	tr.AuthenticateAndAssertSuccess(imsi)
+
+	req := &cwfprotos.GenTrafficRequest{Imsi: imsi, Volume: &wrappers.StringValue{Value: "100K"}}
+	_, err = tr.GenULTraffic(req)
+	assert.NoError(t, err)
+	tr.WaitForEnforcementStatsToSync()
+
+	assert.NoError(t, tr.PauseService("ingress"))
+
+	req = &cwfprotos.GenTrafficRequest{Imsi: imsi, Volume: &wrappers.StringValue{Value: "500K"}}
+	_, err = tr.GenULTraffic(req)
+	assert.NoError(t, err)
+	tr.WaitForEnforcementStatsToSync()
+	tr.DisconnectAndAssertSuccess(ue.GetImsi())
 }
