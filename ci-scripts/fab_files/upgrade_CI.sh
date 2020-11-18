3494e490ad978aa3972a7693ea3076f584b20a94
vaishaliCI <vagrawal@fb.com>
2020-11-18 15:05:03 -0800

    branch update
    Signed-off-by: vaishaliCI <vagrawal@fb.com>

--- ci-scripts/fab_files/upgrade_CI.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf test
-fab upgrade_teravm_agw:setup_2 >> test
-cat test| grep "Upgrade Status" |cut -d':' -f 1,2,3
-rm -rf test
-fab upgrade_teravm_feg:setup_2 >> test
-cat test| grep "Upgrade Status" |cut -d':' -f 1,2,3
-rm -rf test
-#fab run_3gpp_tests:setup_2
