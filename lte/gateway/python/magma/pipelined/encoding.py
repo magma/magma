8c35f7c5572a71e2128a50676aa689fc2228e007
Mykola Yurchenko <koolzz@fb.com>
2020-12-04 08:17:56 -1000

    Add Header Enrichment Header Encoding (#3874)
    * [orc8r] Add Swagger Header Enrichment Configuration
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * go fmt
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add pipelined HE encryption logic
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add swagger encoding key
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add test case
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>

---
+++ lte/gateway/python/magma/pipelined/encoding.py
@@ -0,0 +1,63 @@
+"""
+Copyright 2020 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import codecs
+import hashlib
+from Crypto.Cipher import ARC4
+from Crypto.Cipher import AES
+from Crypto.Hash import HMAC
+
+from lte.protos.mconfig.mconfigs_pb2 import PipelineD
+
+
+def encrypt_str(s: str, key: str, encryption_algorithm):
+    ret = ""
+    if encryption_algorithm == PipelineD.HEConfig.RC4:
+        cipher = ARC4.new(key)
+        ret = cipher.encrypt(s).hex()
+    elif encryption_algorithm == PipelineD.HEConfig.AES256_CBC_HMAC_MD5:
+        iv = os.urandom(16)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        ret = cipher.encrypt(s).hex()
+    elif encryption_algorithm == PipelineD.HEConfig.AES256_ECB_HMAC_MD5:
+        iv = os.urandom(16)
+        cipher = AES.new(key, AES.MODE_ECB, iv)
+        ret = cipher.encrypt(s).hex()
+    elif encryption_algorithm == PipelineD.HEConfig.GZIPPED_AES256_ECB_SHA1:
+    return ret
+
+
+def get_hash(s: str, hash_function):
+    hash_hex = ""
+    if hash_function == PipelineD.HEConfig.MD5:
+        m = hashlib.md5()
+        m.update(s.encode('utf-8'))
+        hash_hex = m.hexdigest()
+    elif hash_function == PipelineD.HEConfig.HEX:
+        hash_hex = s.encode('utf-8').hex()
+    elif hash_function == PipelineD.HEConfig.SHA256:
+        m = hashlib.sha256()
+        m.update(s.encode('utf-8'))
+        hash_hex = m.hexdigest()
+    return hash_hex
+
+
+def encode_str(s: str, encoding_type):
+    encoded = ""
+    if encoding_type == PipelineD.HEConfig.BASE64:
+        encoded = codecs.encode(codecs.decode(s, 'hex'), 'base64').decode()
+    elif encoding_type == PipelineD.HEConfig.HEX2BIN:
+        bits = len(s) * 4
+        encoded = bin(int(s, 16))[2:].zfill(bits)
+    return encoded.strip()
---
+++ lte/gateway/python/magma/pipelined/tests/test_encoding.py
@@ -0,0 +1,51 @@
+"""
+Copyright 2020 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+
+from magma.pipelined.encoding import encrypt_str, get_hash, encode_str
+from lte.protos.mconfig.mconfigs_pb2 import PipelineD
+
+
+class EncodingTest(unittest.TestCase):
+    def test_session_rule_version_mapper(self):
+        """
+        Example encoding:
+            MD5(C14r0315v0x)=37ee40eecb484166d68c29930e48313c
+            RC4(key=37ee40eecb484166d68c29930e48313c, msisdn=5521966054601))=>a4cc8cb22393a667611f4939b6
+            base64(a4cc8cb22393a667611f4939b6)=>pMyMsiOTpmdhH0k5tg==
+        """
+        msisdn = '5521966054601'
+        key = 'C14r0315v0x'
+
+        hash = get_hash(key, PipelineD.HEConfig.MD5)
+        self.assertEqual(hash, '37ee40eecb484166d68c29930e48313c')
+
+        encrypted = encrypt_str(msisdn, hash, PipelineD.HEConfig.RC4)
+        self.assertEqual(encrypted, 'a4cc8cb22393a667611f4939b6')
+
+        ret = encode_str(encrypted, PipelineD.HEConfig.BASE64)
+        self.assertEqual(ret, 'pMyMsiOTpmdhH0k5tg==')
+
+        # Not implemented yet
+        hash = get_hash(key, PipelineD.HEConfig.SHA256)
+        self.assertEqual(hash, '')
+        encrypted = encrypt_str(msisdn, hash,
+                                PipelineD.HEConfig.AES256_CBC_HMAC_MD5)
+        self.assertEqual(encrypted, '')
+        ret = encode_str(encrypted, PipelineD.HEConfig.BASE64)
+        self.assertEqual(ret, '')
+
+
+if __name__ == "__main__":
+    unittest.main()
