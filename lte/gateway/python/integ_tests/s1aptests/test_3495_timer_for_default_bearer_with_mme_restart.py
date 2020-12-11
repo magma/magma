5f7d59510f3cccbfe4b1e53b592b015929d4ee9f
rashmi <rashmi.sarwad@radisys.com>
2020-12-11 06:50:44 +0530

    Added testcase to validate for 3495 timer for both default and dedicated bearers
    Signed-off-by: rashmi <rashmi.sarwad@radisys.com>

---
+++ lte/gateway/python/integ_tests/s1aptests/test_3495_timer_for_default_bearer_with_mme_restart.py
@@ -0,0 +1,156 @@
+"""
+Copyright 2020 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+import s1ap_types
+import time
+
+from integ_tests.s1aptests import s1ap_wrapper
+from integ_tests.s1aptests.s1ap_utils import SpgwUtil
+from s1ap_utils import MagmadUtil
+
+
+class Test3495TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
+    def setUp(self):
+        self._s1ap_wrapper = s1ap_wrapper.TestWrapper(
+            stateless_mode=MagmadUtil.stateless_cmds.ENABLE
+        )
+        self._spgw_util = SpgwUtil()
+
+    def tearDown(self):
+        self._s1ap_wrapper.cleanup()
+
+    def test_3495_timer_for_default_bearer_with_mme_restart(self):
+        """ Test case validates the functionality of 3495 timer for
+            Default bearer while MME restarts
+        Step1: UE attaches to network
+        Step2: Creates a secondary PDN
+        Step3: Initiates PDN disconnect, as part of which mme sends
+               Deactivate EPS bearer context request and starts 3495 timer
+        Step4: TFW shall not respond to first Deactivate EPS bearer context
+               request message
+        Step5: Send command to Magma to restart mme service
+        Step6: TFW shall receive re-transmitted Deactivate EPS bearer context
+               Request message and send Deactivate EPS bearer Context Accept
+        Step7: TFW shall initiate Detach procedure.
+        """
+
+        num_ues = 1
+        self._s1ap_wrapper.configUEDevice(num_ues)
+        req = self._s1ap_wrapper.ue_req
+
+        # APN of the secondary PDN
+        ims = {
+            "apn_name": "ims",  # APN-name
+            "qci": 5,  # qci
+            "priority": 15,  # priority
+            "pre_cap": 0,  # preemption-capability
+            "pre_vul": 0,  # preemption-vulnerability
+            "mbr_ul": 200000000,  # MBR UL
+            "mbr_dl": 100000000,  # MBR DL
+        }
+
+        # APN list to be configured
+        apn_list = [ims]
+
+        self._s1ap_wrapper.configAPN(
+            "IMSI" + "".join([str(i) for i in req.imsi]), apn_list
+        )
+
+        print(
+            "********************** Running End to End attach for ",
+            "UE id ",
+            req.ue_id,
+        )
+        # Now actually complete the attach
+        self._s1ap_wrapper._s1_util.attach(
+            req.ue_id,
+            s1ap_types.tfwCmd.UE_END_TO_END_ATTACH_REQUEST,
+            s1ap_types.tfwCmd.UE_ATTACH_ACCEPT_IND,
+            s1ap_types.ueAttachAccept_t,
+        )
+
+        # Wait on EMM Information from MME
+        self._s1ap_wrapper._s1_util.receive_emm_info()
+
+        time.sleep(5)
+        print("*** Sending PDN connectivity Req ***")
+        # Send PDN Connectivity Request
+        apn = "ims"
+        self._s1ap_wrapper.sendPdnConnectivityReq(req.ue_id, apn)
+        # Receive PDN CONN RSP/Activate default EPS bearer context request
+        response = self._s1ap_wrapper.s1_util.get_response()
+        self.assertEqual(
+            response.msg_type, s1ap_types.tfwCmd.UE_PDN_CONN_RSP_IND.value
+        )
+        act_def_bearer_req = response.cast(s1ap_types.uePdnConRsp_t)
+
+        print(
+            "************************* Sending Activate default EPS bearer "
+            "context accept for UE id ",
+            req.ue_id,
+        )
+        time.sleep(5)
+
+        # Send PDN Disconnect
+        pdn_disconnect_req = s1ap_types.uepdnDisconnectReq_t()
+        pdn_disconnect_req.ue_Id = req.ue_id
+        pdn_disconnect_req.epsBearerId = (
+            act_def_bearer_req.m.pdnInfo.epsBearerId
+        )
+        self._s1ap_wrapper._s1_util.issue_cmd(
+            s1ap_types.tfwCmd.UE_PDN_DISCONNECT_REQ, pdn_disconnect_req
+        )
+        # Receive UE_DEACTIVATE_BER_REQ
+        response = self._s1ap_wrapper.s1_util.get_response()
+        self.assertEqual(
+            response.msg_type, s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value
+        )
+
+        print(
+            "******************* Received deactivate default eps bearer "
+            "context request"
+        )
+
+        # Do not send deactivate eps bearer context accept
+        print("************************* Restarting MME service on", "gateway")
+        self._s1ap_wrapper.magmad_util.restart_services(["mme"])
+
+        for j in range(30):
+            print("Waiting for", j, "seconds")
+            time.sleep(1)
+
+        response = self._s1ap_wrapper.s1_util.get_response()
+        self.assertEqual(
+            response.msg_type, s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value
+        )
+        deactv_bearer_req = response.cast(s1ap_types.UeDeActvBearCtxtReq_t)
+        self._s1ap_wrapper.sendDeactDedicatedBearerAccept(
+            req.ue_id, deactv_bearer_req.bearerId
+        )
+
+        time.sleep(2)
+
+        print(
+            "********************** Running UE detach for UE id ", req.ue_id,
+        )
+        # Now detach the UE
+        self._s1ap_wrapper.s1_util.detach(
+            req.ue_id,
+            s1ap_types.ueDetachType_t.UE_SWITCHOFF_DETACH.value,
+            False,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
