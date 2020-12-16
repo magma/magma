528f13abfe7ef0fbb40c26a2ce0009fad020408b
Mykola Yurchenko <koolzz@fb.com>
2020-12-16 13:32:55 -1000

    Add Header Enrichment Header Encoding (#3902)
    * Add Header Enrichment Header Encoding (#3874)
    
    * [orc8r] Add Swagger Header Enrichment Configuration
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * go fmt
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add pipelined HE encryption logic
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add swagger encoding key
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add test case
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Update gzipped code
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Add hmac key to api
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Change cwf docker to pycryptodome
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>
    
    * Handle unsupported alg
    
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>

--- lte/cloud/go/services/lte/servicers/builder_servicer.go
+++ lte/cloud/go/services/lte/servicers/builder_servicer.go
@@ -305,6 +305,7 @@ func getHEConfig(gwConfig *lte_models.GatewayHeConfig) *lte_mconfig.PipelineD_HE
 		HashFunction:           lte_mconfig.PipelineD_HEConfig_HashFunction(lte_mconfig.PipelineD_HEConfig_HashFunction_value[gwConfig.HeHashFunction]),
 		EncodingType:           lte_mconfig.PipelineD_HEConfig_EncodingType(lte_mconfig.PipelineD_HEConfig_EncodingType_value[gwConfig.HeEncodingType]),
 		EncryptionKey:          gwConfig.EncryptionKey,
+		HmacKey:                gwConfig.HmacKey,
 	}
 }
 
