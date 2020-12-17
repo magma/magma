697d5da5fbd8bc59087cae6bcc55e6b6344e0f83
Oriol Batalla <obatalla@fb.com>
2020-12-17 14:47:04 -0800

    [feg] S8 client with basic functionallity
    Signed-off-by: Oriol Batalla <obatalla@fb.com>

--- feg/gateway/services/s8_proxy/client_api.go
+++ feg/gateway/services/s8_proxy/client_api.go
@@ -12,3 +12,39 @@ limitations under the License.
 */
 
 package s8_proxy
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"magma/feg/cloud/go/protos"
+	"magma/feg/gateway/registry"
+
+	"github.com/golang/glog"
+)
+
+type s8ProxyClient struct {
+	protos.S8ProxyClient
+}
+
+func CreateSession(req *protos.CreateSessionRequestPgw) (*protos.CreateSessionResponsePgw, error) {
+	if req == nil {
+		return nil, errors.New("Invalid CreateSessionRequestPgw")
+	}
+	cli, err := getS8ProxyClient()
+	if err != nil {
+		return nil, err
+	}
+	return cli.CreateSession(context.Background(), req)
+}
+
+func getS8ProxyClient() (*s8ProxyClient, error) {
+	conn, err := registry.GetConnection(registry.S8_PROXY)
+	if err != nil {
+		errMsg := fmt.Sprintf("S8 Proxy client initialization error: %s", err)
+		glog.Error(errMsg)
+		return nil, errors.New(errMsg)
+	}
+	return &s8ProxyClient{protos.NewS8ProxyClient(conn)}, nil
+}
