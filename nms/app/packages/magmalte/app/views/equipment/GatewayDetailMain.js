45e26735cee0f288b6f4b7c627ba92c1fab08c39
Karthik Subraveti <ksubraveti@fb.com>
2020-12-17 12:39:36 -0800

    Add gateway command modal (#4011)
    Signed-off-by: Karthik Subraveti <ksubraveti@fb.com>

--- nms/app/packages/magmalte/app/views/equipment/GatewayDetailMain.js
+++ nms/app/packages/magmalte/app/views/equipment/GatewayDetailMain.js
@@ -16,11 +16,15 @@
 import type {WithAlert} from '@fbcnms/ui/components/Alert/withAlert';
 
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import Button from '@material-ui/core/Button';
 import CardTitleRow from '../../components/layout/CardTitleRow';
 import CellWifiIcon from '@material-ui/icons/CellWifi';
 import DashboardAlertTable from '../../components/DashboardAlertTable';
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '../../theme/design-system/DialogTitle';
 import EventsTable from '../../views/events/EventsTable';
 import GatewayConfig from './GatewayDetailConfig';
 import GatewayContext from '../../components/context/GatewayContext';
@@ -32,16 +36,23 @@ import GatewaySummary from './GatewaySummary';
 import GraphicEqIcon from '@material-ui/icons/GraphicEq';
 import Grid from '@material-ui/core/Grid';
 import ListAltIcon from '@material-ui/icons/ListAlt';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import MyLocationIcon from '@material-ui/icons/MyLocation';
 import PeopleIcon from '@material-ui/icons/People';
 import React from 'react';
 import SettingsIcon from '@material-ui/icons/Settings';
 import SettingsInputAntennaIcon from '@material-ui/icons/SettingsInputAntenna';
+import Text from '../../theme/design-system/Text';
 import TopBar from '../../components/TopBar';
 import nullthrows from '@fbcnms/util/nullthrows';
 import withAlert from '@fbcnms/ui/components/Alert/withAlert';
 
 import {GatewayJsonConfig} from './GatewayDetailConfig';
+import {
+  GenericCommandControls,
+  PingCommandControls,
+} from '../../components/GatewayCommandFields';
 import {Redirect, Route, Switch} from 'react-router-dom';
 import {RunGatewayCommands} from '../../state/lte/EquipmentState';
 import {colors, typography} from '../../theme/default';
@@ -49,6 +60,7 @@ import {makeStyles} from '@material-ui/styles';
 import {useContext} from 'react';
 import {useEnqueueSnackbar} from '@fbcnms/ui/hooks/useSnackbar';
 import {useRouter} from '@fbcnms/ui/hooks';
+import {withStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   dashboardRoot: {
@@ -73,43 +85,133 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-function GatewayRebootButtonInternal(props: WithAlert) {
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})(props => (
+  <Menu
+    data-testid="policy_menu"
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+type CommandProps = {
+  gatewayID: string,
+  open: boolean,
+  onClose: () => void,
+};
+
+function GatewayCommandDialog(props: CommandProps) {
+  return (
+    <Dialog open={props.open} onClose={props.onClose} scroll="body">
+      <DialogTitle onClose={props.onClose} label={'Run Gateway Commands'} />
+      <DialogContent>
+        <PingCommandControls gatewayID={props.gatewayID} />
+        <GenericCommandControls gatewayID={props.gatewayID} />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function GatewayMenuInternal(props: WithAlert) {
   const classes = useStyles();
   const {match} = useRouter();
   const networkId: string = nullthrows(match.params.networkId);
   const gatewayId: string = nullthrows(match.params.gatewayId);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
   const enqueueSnackbar = useEnqueueSnackbar();
 
-  const handleClick = () => {
-    props
-      .confirm(`Are you sure you want to reboot ${gatewayId}?`)
-      .then(async confirmed => {
-        if (!confirmed) {
-          return;
-        }
-        try {
-          await RunGatewayCommands({networkId, gatewayId, command: 'reboot'});
-          enqueueSnackbar('gateway reboot triggered successfully', {
-            variant: 'success',
-          });
-        } catch (e) {
-          enqueueSnackbar(e.response?.data?.message ?? e.message, {
-            variant: 'error',
-          });
-        }
-      });
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleGatewayMenuClick = (
+    command: 'reboot' | 'ping' | 'restartServices' | 'generic',
+    warnMsg: string,
+  ) => {
+    props.confirm(warnMsg).then(async confirmed => {
+      if (!confirmed) {
+        return;
+      }
+      try {
+        await RunGatewayCommands({
+          networkId,
+          gatewayId,
+          command: command,
+        });
+        enqueueSnackbar('command triggered successfully', {
+          variant: 'success',
+        });
+      } catch (e) {
+        enqueueSnackbar(e.response?.data?.message ?? e.message, {
+          variant: 'error',
+        });
+      }
+    });
   };
 
   return (
-    <Button
-      variant="contained"
-      className={classes.appBarBtn}
-      onClick={handleClick}>
-      Reboot
-    </Button>
+    <div>
+      <GatewayCommandDialog
+        gatewayID={gatewayId}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+      <Button
+        onClick={handleClick}
+        className={classes.appBarBtn}
+        endIcon={<ArrowDropDownIcon />}>
+        Actions
+      </Button>
+      <StyledMenu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}>
+        <MenuItem
+          data-testid="gatewayReboot"
+          onClick={() =>
+            handleGatewayMenuClick(
+              'reboot',
+              `Are you sure you want to reboot ${gatewayId}?`,
+            )
+          }>
+          <Text variant="subtitle2">Reboot</Text>
+        </MenuItem>
+        <MenuItem
+          data-testid="gatewayRestartServices"
+          onClick={() =>
+            handleGatewayMenuClick(
+              'restartServices',
+              `Are you sure you want to restart all services on ${gatewayId}?`,
+            )
+          }>
+          <Text variant="subtitle2">Restart Services</Text>
+        </MenuItem>
+        <MenuItem onClick={() => setOpen(true)}>
+          <Text variant="subtitle2">Command</Text>
+        </MenuItem>
+      </StyledMenu>
+    </div>
   );
 }
-const GatewayRebootButton = withAlert(GatewayRebootButtonInternal);
+const GatewayMenu = withAlert(GatewayMenuInternal);
 
 export function GatewayDetail() {
   const {relativePath, relativeUrl, match} = useRouter();
@@ -125,31 +227,31 @@ export function GatewayDetail() {
             label: 'Overview',
             to: '/overview',
             icon: DashboardIcon,
-            filters: <GatewayRebootButton />,
+            filters: <GatewayMenu />,
           },
           {
             label: 'Event',
             to: '/event',
             icon: MyLocationIcon,
-            filters: <GatewayRebootButton />,
+            filters: <GatewayMenu />,
           },
           {
             label: 'Logs',
             to: '/logs',
             icon: ListAltIcon,
-            filters: <GatewayRebootButton />,
+            filters: <GatewayMenu />,
           },
           {
             label: 'Alerts',
             to: '/alert',
             icon: AccessAlarmIcon,
-            filters: <GatewayRebootButton />,
+            filters: <GatewayMenu />,
           },
           {
             label: 'Config',
             to: '/config',
             icon: SettingsIcon,
-            filters: <GatewayRebootButton />,
+            filters: <GatewayMenu />,
           },
         ]}
       />
