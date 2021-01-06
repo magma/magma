b8604df96f13af1a8118f51659ad8b528dd8b885
Michael Germano <mgermano@fb.com>
2021-01-06 14:53:59 -0500

    Update orc8r deployment for version 1.4
    Signed-off-by: Michael Germano <mgermano@fb.com>

--- orc8r/cloud/deploy/terraform/orc8r-helm-aws/examples/remote/main.tf
+++ orc8r/cloud/deploy/terraform/orc8r-helm-aws/examples/remote/main.tf
@@ -63,7 +63,7 @@ data "aws_secretsmanager_secret_version" "root_secrets" {
   secret_id = data.aws_secretsmanager_secret.root_secrets.id
 }
 
-module orc8r {
+module "orc8r" {
   # Change this to pull from github with a specified ref
   source = "../../../orc8r-aws"
 
@@ -88,7 +88,7 @@ module orc8r {
   elasticsearch_ebs_volume_type = "gp2"
 }
 
-module orc8r-app {
+module "orc8r-app" {
   # Change this to pull from github with a specified ref
   source = "../.."
 
@@ -137,9 +137,9 @@ module orc8r-app {
 
   elasticsearch_endpoint = module.orc8r.es_endpoint
 
-  orc8r_chart_version = "1.4.36"
-  orc8r_tag           = "1.3.0"
-  deploy_nms          = true
+  orc8r_deployment_type = "fwa"
+  orc8r_tag             = "1.4.0"
+  deploy_nms            = true
 }
 
 output "nameservers" {
