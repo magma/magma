afbdfb4b72f41768fb67ccb5f45a71739aae40a1
Ulas Kozat <kozat@fb.com>
2021-01-08 12:15:44 -0800

    Remove paging timer when ue context is removed
    Signed-off-by: Ulas Kozat <kozat@fb.com>

--- lte/gateway/c/oai/tasks/mme_app/mme_app_context.c
+++ lte/gateway/c/oai/tasks/mme_app/mme_app_context.c
@@ -322,6 +322,22 @@ void mme_app_ue_context_free_content(ue_mm_context_t* const ue_context_p) {
     ue_context_p->ulr_response_timer.id = MME_APP_TIMER_INACTIVE_ID;
   }
 
+  // Stop paging response timer if running
+  if (ue_context_p->paging_response_timer.id != MME_APP_TIMER_INACTIVE_ID) {
+    timer_argP = NULL;
+    if (timer_remove(
+            ue_context_p->paging_response_timer.id, (void**) &timer_argP)) {
+      OAILOG_ERROR_UE(
+          LOG_MME_APP, ue_context_p->emm_context._imsi64,
+          "Failed to stop Paging Response timer for UE id %d \n",
+          ue_context_p->mme_ue_s1ap_id);
+    }
+    if (timer_argP) {
+      free_wrapper((void**) &timer_argP);
+    }
+    ue_context_p->paging_response_timer.id = MME_APP_TIMER_INACTIVE_ID;
+  }
+
   if (ue_context_p->sgs_context != NULL) {
     // free the sgs context
     mme_app_ue_sgs_context_free_content(
