ec9e3be92edc7ea7a3de9cce1363fdf84220ac7d
Michael Germano <mgermano@fb.com>
2021-01-29 03:35:16 -0800

    Fix tf install issues for v1.4
    Signed-off-by: Michael Germano <mgermano@fb.com>

--- orc8r/cloud/deploy/terraform/orc8r-helm-aws/examples/basic/main.tf
+++ orc8r/cloud/deploy/terraform/orc8r-helm-aws/examples/basic/main.tf
@@ -22,8 +22,9 @@ module "orc8r" {
   secretsmanager_orc8r_secret = "orc8r-secrets"
   orc8r_domain_name           = "orc8r.example.com"
 
-  vpc_name     = "orc8r"
-  cluster_name = "orc8r"
+  vpc_name        = "orc8r"
+  cluster_name    = "orc8r"
+  cluster_version = "1.17"
 
   deploy_elasticsearch          = true
   elasticsearch_domain_name     = "orc8r-es"
