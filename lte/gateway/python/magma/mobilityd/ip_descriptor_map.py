9472b61e81450b3820ca5df45b9ff8e83ada1e5c
Alex Rodriguez <alexrod@fb.com>
2021-02-01 09:32:33 -0800

    [lte][agw] Updating IP allocator pool to not do full look-up of IPs in free state (#4582)
    * Updating IP allocator pool to not do full look-up of FREE ips
    
    Signed-off-by: Alex Rodriguez <ardzoht@gmail.com>
    
    * Updating IP allocator pool to not do full look-up of FREE ips
    
    Signed-off-by: Alex Rodriguez <ardzoht@gmail.com>

--- lte/gateway/python/magma/mobilityd/ip_descriptor_map.py
+++ lte/gateway/python/magma/mobilityd/ip_descriptor_map.py
@@ -36,8 +36,7 @@ from __future__ import absolute_import, division, print_function, \
     unicode_literals
 
 from ipaddress import ip_address, ip_network
-from typing import Dict, List, Set
-from random import choice
+from typing import Dict, List, Optional, Set
 
 from magma.mobilityd.ip_descriptor import IPDesc, IPState
 
@@ -71,19 +70,26 @@ class IpDescriptorMap:
         ip_desc = self.ip_states[state].pop(ip.exploded, None)
         return ip_desc
 
-    def pop_ip_from_state(self, state: IPState) -> IPDesc:
+    def pop_ip_from_state(self, state: IPState) -> Optional[IPDesc]:
         """ Pop an IP from a internal dict """
         assert state in IPState, "unknown state %s" % state
 
-        ip_state_key = choice(list(self.ip_states[state].keys()))
-        ip_desc = self.ip_states[state].pop(ip_state_key)
-        return ip_desc
+        try:
+            _, ip_desc = self.ip_states[state].popitem()
+            return ip_desc
+        except KeyError:
+            return None
 
-    def get_ip_count(self, state: IPState) -> int:
-        """ Return number of IPs in a state """
+    def is_ip_state_map_empty(self, state: IPState) -> bool:
+        """
+        Args:
+            state: IP State to check for
+
+        Returns: True if IPs map is empty for a given state, else otherwise
+        """
         assert state in IPState, "unknown state %s" % state
 
-        return len(self.ip_states[state])
+        return bool(self.ip_states[state]) == False
 
     def test_ip_state(self, ip: ip_address, state: IPState) -> bool:
         """ check if IP is in state X """
@@ -117,7 +123,7 @@ class IpDescriptorMap:
         assert ip == ip_desc.ip, "Unmatching ip_desc for %s" % ip
 
         if ip_desc.state == IPState.FREE:
-            assert ip_desc.sid is None,\
+            assert ip_desc.sid is None, \
                 "Unexpected sid in a freed IPDesc {}".format(ip_desc)
         else:
             assert ip_desc.sid is not None, \
