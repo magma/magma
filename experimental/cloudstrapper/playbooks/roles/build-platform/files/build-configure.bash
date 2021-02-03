3516f81cd781353c9d6315cc37a16c2d252c8cbc
Ubuntu <ubuntu@ip-172-31-34-90.ap-northeast-1.compute.internal>
2021-02-03 14:57:18 +0000

    Adding experimental cloudstrapper

---
+++ experimental/cloudstrapper/playbooks/roles/build-platform/files/build-configure.bash
@@ -0,0 +1,5 @@
+export MAGMA_ROOT=~/magma-source/magma/
+export PUBLISH=$MAGMA_ROOT/orc8r/tools/docker/publish.sh
+export REGISTRY=registry.hub.docker.com/REGISTRY
+export MAGMA_TAG=1.3.0-master
+export PWD=`pwd`
