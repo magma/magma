0f14905d044c356c1f6cf6ce47be862dddc5eee4
Ulas Kozat <kozat@fb.com>
2021-02-04 13:29:09 -0800

    Sharding at file level and set workers to 10 in config
    Signed-off-by: Ulas Kozat <kozat@fb.com>

--- lte/gateway/python/magma/subscriberdb/store/sqlite.py
+++ lte/gateway/python/magma/subscriberdb/store/sqlite.py
@@ -12,7 +12,6 @@ limitations under the License.
 """
 
 import logging
-import random
 import sqlite3
 import threading
 import time
@@ -28,9 +27,8 @@ from .base import (
 )
 from .onready import OnDataReady
 
-EXECUTE_TIMEOUT = 5 # wait period in sec if db is locked
-RAND_UPPERBOUND = 250 # maximum delta wait in 2nd attempt (unnormalized)
-RAND_LOWERBOUND = 10 # minimum delta wait after failed first attempt (unnormalized)
+SID_DIGITS = 2 # number of least significant digits from IMSI to use as table buckets
+N_SHARDS = 10 ** SID_DIGITS # number of shards to distribute UEs
 
 class SqliteStore(BaseStore):
     """
@@ -41,27 +39,40 @@ class SqliteStore(BaseStore):
     """
 
     def __init__(self, db_location, loop=None):
-        self._db_location = db_location
-        self._tlocal = threading.local()
+        self._db_location = self._create_db_locations(db_location)
         self._create_store()
         self._on_ready = OnDataReady(loop=loop)
 
-    @property
-    def conn(self):
-        """
-        Returns a thread local connection to the sqlite db.
-        """
-        if not getattr(self._tlocal, 'conn', None):
-            self._tlocal.conn = sqlite3.connect(self._db_location, timeout=EXECUTE_TIMEOUT, uri=True)
-        return self._tlocal.conn
+
+    def _create_db_locations(self, db_location):
+        # db_location in subscriberdb.yml: file:/var/opt/magma/subscriber.db?cache=shared
+        i = db_location.find(".db")
+        db_location_list = []
+        # no sharding if it is not a file
+        if i == -1:
+            return [db_location]
+
+        # file name is passed, use it as a base
+        for n_shard in range(N_SHARDS):
+            if n_shard < 10: # prefix i with '0'
+                db_location_list.append(db_location[:i] + '0' + str(n_shard) + db_location[i:])
+            else:
+                db_location_list.append(db_location[:i] + str(n_shard) + db_location[i:])
+            logging.info("db location: %s", db_location_list[n_shard])
+        return db_location_list
 
     def _create_store(self):
         """
         Create the sqlite table if it doesn't exist already.
         """
-        with self.conn:
-            self.conn.execute("CREATE TABLE IF NOT EXISTS subscriberdb"
-                              "(subscriber_id text PRIMARY KEY, data text)")
+        for db_location in self._db_location:
+            conn = sqlite3.connect(db_location, uri=True)
+            try:
+                with conn:
+                    conn.execute("CREATE TABLE IF NOT EXISTS subscriberdb"
+                                      "(subscriber_id text PRIMARY KEY, data text)")
+            finally:
+                conn.close()
 
     def add_subscriber(self, subscriber_data):
         """
@@ -69,14 +80,19 @@ class SqliteStore(BaseStore):
         """
         sid = SIDUtils.to_str(subscriber_data.sid)
         data_str = subscriber_data.SerializeToString()
-        with self.conn:
-            res = self.conn.execute("SELECT data FROM subscriberdb WHERE "
-                                    "subscriber_id = ?", (sid, ))
-            if res.fetchone():
-                raise DuplicateSubscriberError(sid)
+        db_location = self._db_location[int(sid[SID_DIGITS:])]
+        conn = sqlite3.connect(db_location, uri=True)
+        try:
+            with conn:
+                res = conn.execute("SELECT data FROM subscriberdb WHERE "
+                                        "subscriber_id = ?", (sid, ))
+                if res.fetchone():
+                    raise DuplicateSubscriberError(sid)
 
-            self.conn.execute("INSERT INTO subscriberdb(subscriber_id, data) "
-                              "VALUES (?, ?)", (sid, data_str))
+                conn.execute("INSERT INTO subscriberdb(subscriber_id, data) "
+                                "VALUES (?, ?)", (sid, data_str))
+        finally:
+            conn.close()
         self._on_ready.add_subscriber(subscriber_data)
 
     @contextmanager
@@ -84,59 +100,70 @@ class SqliteStore(BaseStore):
         """
         Context manager to modify the subscriber data.
         """
-        with self.conn:
-            res = self.conn.execute(
-                "SELECT data FROM subscriberdb WHERE " "subscriber_id = ?",
-                (subscriber_id,),
-            )
-            row = res.fetchone()
-            if not row:
-                raise SubscriberNotFoundError(subscriber_id)
-            subscriber_data = SubscriberData()
-            subscriber_data.ParseFromString(row[0])
-            yield subscriber_data
-            data_str = subscriber_data.SerializeToString()
-            try:
-                self.conn.execute(
+        db_location = self._db_location[int(subscriber_id[SID_DIGITS:])]
+        conn = sqlite3.connect(db_location, uri=True)
+        try:
+            with conn:
+                res = conn.execute(
+                    "SELECT data FROM subscriberdb WHERE " "subscriber_id = ?",
+                    (subscriber_id,),
+                )
+                row = res.fetchone()
+                if not row:
+                    raise SubscriberNotFoundError(subscriber_id)
+                subscriber_data = SubscriberData()
+                subscriber_data.ParseFromString(row[0])
+                yield subscriber_data
+                data_str = subscriber_data.SerializeToString()
+                conn.execute(
                     "UPDATE subscriberdb SET data = ? " "WHERE subscriber_id = ?",
                     (data_str, subscriber_id),
                 )
-            except sqlite3.OperationalError:
-                logging.error("sqlite db error for subscriber: %s", subscriber_id)
-                time.sleep(random.randint(RAND_LOWERBOUND, RAND_UPPERBOUND)/100)
-                # Try one more time after random wait
-                self.conn.execute(
-                    "UPDATE subscriberdb SET data = ? " "WHERE subscriber_id = ?",
-                    (data_str, subscriber_id),
-                )
+        finally:
+            conn.close()
 
     def delete_subscriber(self, subscriber_id):
         """
         Method that deletes a subscriber, if present.
         """
-        with self.conn:
-            self.conn.execute(
-                "DELETE FROM subscriberdb WHERE " "subscriber_id = ?",
-                (subscriber_id,),
-            )
+        db_location = self._db_location[int(subscriber_id[SID_DIGITS:])]
+        conn = sqlite3.connect(db_location, uri=True)
+        try:
+            with conn:
+                conn.execute(
+                    "DELETE FROM subscriberdb WHERE " "subscriber_id = ?",
+                    (subscriber_id,),
+                )
+        finally:
+            conn.close()
 
     def delete_all_subscribers(self):
         """
         Method that removes all the subscribers from the store
         """
-        with self.conn:
-            self.conn.execute("DELETE FROM subscriberdb")
+        for db_location in self._db_location:
+            conn = sqlite3.connect(db_location, uri=True)
+            try:
+                with conn:
+                    conn.execute("DELETE FROM subscriberdb")
+            finally:
+                conn.close()
 
     def get_subscriber_data(self, subscriber_id):
         """
         Method that returns the auth key for the subscriber.
         """
-        with self.conn:
-            res = self.conn.execute("SELECT data FROM subscriberdb WHERE "
-                                    "subscriber_id = ?", (subscriber_id, ))
-            row = res.fetchone()
-            if not row:
-                raise SubscriberNotFoundError(subscriber_id)
+        db_location = self._db_location[int(subscriber_id[SID_DIGITS:])]
+        conn = sqlite3.connect(db_location, uri=True)
+        try:
+            with conn:
+                res = conn.execute("SELECT data FROM subscriberdb WHERE "
+                                        "subscriber_id = ?", (subscriber_id, ))
+                row = res.fetchone()
+                if not row:
+                    raise SubscriberNotFoundError(subscriber_id)
+        finally:
+            conn.close()
         subscriber_data = SubscriberData()
         subscriber_data.ParseFromString(row[0])
         return subscriber_data
@@ -145,9 +172,16 @@ class SqliteStore(BaseStore):
         """
         Method that returns the list of subscribers stored
         """
-        with self.conn:
-            res = self.conn.execute("SELECT subscriber_id FROM subscriberdb")
-            return [row[0] for row in res]
+        sub_list = []
+        for db_location in self._db_location:
+            conn = sqlite3.connect(db_location, uri=True)
+            try:
+                with conn:
+                    res = conn.execute("SELECT subscriber_id FROM subscriberdb")
+                    sub_list.extend([row[0] for row in res])
+            finally:
+                conn.close()
+        return sub_list
 
     def update_subscriber(self, subscriber_data):
         """
@@ -164,11 +198,16 @@ class SqliteStore(BaseStore):
         """
         sid = SIDUtils.to_str(subscriber_data.sid)
         data_str = subscriber_data.SerializeToString()
-        with self.conn:
-            res = self.conn.execute("UPDATE subscriberdb SET data = ? "
-                                    "WHERE subscriber_id = ?", (data_str, sid))
-            if not res.rowcount:
-                raise SubscriberNotFoundError(sid)
+        db_location = self._db_location[int(sid[SID_DIGITS:])]
+        conn = sqlite3.connect(db_location, uri=True)
+        try:
+            with conn:
+                res = conn.execute("UPDATE subscriberdb SET data = ? "
+                                        "WHERE subscriber_id = ?", (data_str, sid))
+                if not res.rowcount:
+                    raise SubscriberNotFoundError(sid)
+        finally:
+            conn.close()
 
     def resync(self, subscribers):
         """
@@ -179,26 +218,38 @@ class SqliteStore(BaseStore):
         Args:
             subscribers - list of subscribers to be in the store.
         """
-        with self.conn:
-            # Capture the current state of the subscribers
-            res = self.conn.execute("SELECT subscriber_id, data FROM subscriberdb")
+        for db_location in self._db_location:
+            conn = sqlite3.connect(db_location, uri=True)
             current_state = {}
-            for row in res:
-                sub = SubscriberData()
-                sub.ParseFromString(row[1])
-                current_state[row[0]] = sub.state
+            try:
+                with conn:
+                    # Capture the current state of the subscribers
+                    res = conn.execute("SELECT subscriber_id, data FROM subscriberdb")
 
-            # Clear all subscribers
-            self.conn.execute("DELETE FROM subscriberdb")
+                    for row in res:
+                        sub = SubscriberData()
+                        sub.ParseFromString(row[1])
+                        current_state[row[0]] = sub.state
 
-            # Add the subscribers with the current state
-            for sub in subscribers:
-                sid = SIDUtils.to_str(sub.sid)
-                if sid in current_state:
-                    sub.state.CopyFrom(current_state[sid])
-                data_str = sub.SerializeToString()
-                self.conn.execute("INSERT INTO subscriberdb(subscriber_id, data) "
-                                  "VALUES (?, ?)", (sid, data_str))
+                    # Clear all subscribers
+                    conn.execute("DELETE FROM subscriberdb")
+            finally:
+                conn.close()
+
+        # Add the subscribers with the current state
+        for sub in subscribers:
+            sid = SIDUtils.to_str(sub.sid)
+            db_location = self._db_location[int(sid[SID_DIGITS:])]
+            conn = sqlite3.connect(db_location, uri=True)
+            if sid in current_state:
+                sub.state.CopyFrom(current_state[sid])
+            data_str = sub.SerializeToString()
+            try:
+                with conn:
+                    conn.execute("INSERT INTO subscriberdb(subscriber_id, data) "
+                                        "VALUES (?, ?)", (sid, data_str))
+            finally:
+                conn.close()
         self._on_ready.resync(subscribers)
 
     async def on_ready(self):
