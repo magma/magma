6980f05fa2f2e99d2bf0f9a71c3aa657904a0a8a
Alex Rodriguez <alexrod@fb.com>
2021-02-05 08:45:20 -0800

    Adds logrotate conf for syslog to be checked hourly (#4758)
    Signed-off-by: Alex Rodriguez <ardzoht@gmail.com>

--- lte/gateway/release/build-magma.sh
+++ lte/gateway/release/build-magma.sh
@@ -355,6 +355,7 @@ ${CONTROL_PROXY_FILE}=/etc/magma/ \
 $(glob_files "${ANSIBLE_FILES}/magma_modules_load" /etc/modules-load.d/magma.conf) \
 $(glob_files "${ANSIBLE_FILES}/configure_envoy_namespace.sh" /usr/local/bin/ ) \
 $(glob_files "${ANSIBLE_FILES}/logrotate_oai.conf" /etc/logrotate.d/oai) \
+$(glob_files "${ANSIBLE_FILES}/logrotate_rsyslog.conf" /etc/logrotate.d/rsyslog) \
 $(glob_files "${ANSIBLE_FILES}/local-cdn/*" /var/www/local-cdn/) \
 ${ANSIBLE_FILES}/99-magma.conf=/etc/sysctl.d/ \
 ${ANSIBLE_FILES}/magma_ifaces_gtp=/etc/network/interfaces.d/gtp \
