36555f584c1b2ed3996b4f14bbda1ffd315ad57b
Pruthvi Hebbani <pruthvi.hebbani@radisys.com>
2021-02-08 18:40:37 +0530

    Fixed memory leak by clearing pco contents stored in sgpw context
    Signed-off-by: Pruthvi Hebbani <pruthvi.hebbani@radisys.com>

--- lte/gateway/c/oai/tasks/sgw/spgw_state.cpp
+++ lte/gateway/c/oai/tasks/sgw/spgw_state.cpp
@@ -82,7 +82,8 @@ void sgw_free_s11_bearer_context_information(
   if (*context_p) {
     sgw_free_pdn_connection(
         &(*context_p)->sgw_eps_bearer_context_information.pdn_connection);
-
+    clear_protocol_configuration_options(
+        &(*context_p)->sgw_eps_bearer_context_information.saved_message.pco);
     pgw_delete_procedures(*context_p);
     if ((*context_p)->pgw_eps_bearer_context_information.apns) {
       obj_hashtable_ts_destroy(
