11347a530f9a966f53582f6cebfdaf7c2255ed19
Ulas Kozat <kozat@fb.com>
2021-02-12 13:05:41 -0800

    Remove redundant mme_app_handle_modify_bearer_rsp and s1u counter
    Signed-off-by: Ulas Kozat <kozat@fb.com>

--- lte/gateway/c/oai/tasks/mme_app/mme_app_main.c
+++ lte/gateway/c/oai/tasks/mme_app/mme_app_main.c
@@ -141,10 +141,6 @@ static int handle_message(zloop_t* loop, zsock_t* reader, void* arg) {
           mme_app_handle_modify_bearer_rsp(
               &received_message_p->ittiMsg.s11_modify_bearer_response,
               ue_context_p);
-          update_mme_app_stats_s1u_bearer_add();
-          mme_app_handle_modify_bearer_rsp(
-              &received_message_p->ittiMsg.s11_modify_bearer_response,
-              ue_context_p);
         } else {
           mme_app_handle_path_switch_req_ack(
               &received_message_p->ittiMsg.s11_modify_bearer_response,
