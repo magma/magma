d428be274e23b2465f21e20337121b71820b3d27
ashish-pr <70095109+ashish-pr@users.noreply.github.com>
2021-02-17 04:00:40 +0530

    [5G: MVC][component: agw][type: enhancement] sctp and sctpd modifications to support eNB and gNB sctp connections #2035 (#3808)
    * sctp and sctpd modifications to support eNB and gNB sctp connections
    
    Signed-off-by: ashish-pr <ashish.p@altencalsoftlabs.com>

--- lte/gateway/c/oai/tasks/sctp/sctpd_downlink_client.cpp
+++ lte/gateway/c/oai/tasks/sctp/sctpd_downlink_client.cpp
@@ -149,7 +149,6 @@ int sctpd_init(sctp_init_t* init) {
     }
     req.add_ipv6_addrs(ipv6_str);
   }
-
   req.set_port(init->port);
   req.set_ppid(init->ppid);
 
@@ -182,10 +181,12 @@ int sctpd_init(sctp_init_t* init) {
 }
 
 // sendDl
-int sctpd_send_dl(uint32_t assoc_id, uint16_t stream, bstring payload) {
+int sctpd_send_dl(
+    uint32_t ppid, uint32_t assoc_id, uint16_t stream, bstring payload) {
   SendDlReq req;
   SendDlRes res;
 
+  req.set_ppid(ppid);
   req.set_assoc_id(assoc_id);
   req.set_stream(stream);
   req.set_payload(bdata(payload), blength(payload));
