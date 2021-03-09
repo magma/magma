b8e4f616750a36404987975e8b148e7f0330058a
Vyas20 <52447537+Vyas20@users.noreply.github.com>
2021-03-09 17:48:21 +0100

    [Converged-MME]ts8_timer edit (#4786)
    * replacing ts8_timer
    
    Signed-off-by: Mohit Vyas <mohit.vyas@openairinterface.org>

--- lte/gateway/c/oai/tasks/mme_app/mme_app_defs.h
+++ lte/gateway/c/oai/tasks/mme_app/mme_app_defs.h
@@ -165,7 +165,8 @@ int mme_app_handle_initial_paging_request(
 void mme_app_handle_paging_timer_expiry(void* args, imsi64_t* imsi64);
 void mme_app_handle_ulr_timer_expiry(void* args, imsi64_t* imsi64);
 
-void mme_app_handle_sgs_eps_detach_timer_expiry(void* args, imsi64_t* imsi64);
+int mme_app_handle_sgs_eps_detach_timer_expiry(
+    zloop_t* loop, int timer_id, void* args);
 void mme_app_handle_sgs_imsi_detach_timer_expiry(void* args, imsi64_t* imsi64);
 void mme_app_handle_sgs_implicit_imsi_detach_timer_expiry(
     void* args, imsi64_t* imsi64);
