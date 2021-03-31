3c61a6388406fe4c6bc6dc90665889cc575a24fc
Andrei Lee <andreilee@fb.com>
2021-03-31 15:41:27 -0700

    [1.5] Update NMS DB Migration script download reference
    Signed-off-by: Andrei Lee <andreilee@fb.com>

--- nms/app/packages/magmalte/scripts/fuji-upgrade/pre-upgrade-migration.sh
+++ nms/app/packages/magmalte/scripts/fuji-upgrade/pre-upgrade-migration.sh
@@ -12,7 +12,7 @@
 # limitations under the License.
 ################################################################################
 
-NMS_SCRIPT_URI="https://gist.githubusercontent.com/andreilee/7aa7d533e2e8f425222b1e6a016a6f5a/raw/5b3c9346dedca6dcfb8d0a13c5f3b4d9100b9279/runs-on-nms.sh"
+NMS_SCRIPT_URI="https://raw.githubusercontent.com/magma/magma/231fc6e27cf18a2f9c8bae94306897cbf4471e42/nms/app/packages/magmalte/scripts/fuji-upgrade/pre-upgrade-migration.sh"
 
 cat << EOF
 ================================================================================
