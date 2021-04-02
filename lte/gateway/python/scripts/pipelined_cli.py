0019f3a4c5f6113a2851bc9c2c55b641c99d0a0b
Nick Yurchenko <koolzz@fb.com>
2021-04-02 15:34:38 -0400

    [Pipelined]Add GHZ gRPC testing benchmark
    Signed-off-by: Nick Yurchenko <koolzz@fb.com>

--- lte/gateway/python/scripts/pipelined_cli.py
+++ lte/gateway/python/scripts/pipelined_cli.py
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import json
 import argparse
 import errno
 import time
@@ -22,6 +23,7 @@ from datetime import datetime
 from collections import namedtuple
 from pprint import pprint
 
+from google.protobuf import json_format
 from magma.common.rpc_utils import grpc_wrapper, grpc_async_wrapper
 from lte.protos.pipelined_pb2 import (
     SubscriberQuotaUpdate,
@@ -53,6 +55,22 @@ from magma.pipelined.tests.app.ng_set_session_msg import (
 
 from lte.protos.pipelined_pb2_grpc import PipelinedStub
 from lte.protos.policydb_pb2 import FlowMatch, FlowDescription, PolicyRule
+
+UEInfo = namedtuple('UEInfo', ['imsi_str', 'ipv4_src', 'ipv4_dst',
+                               'rule_id'])
+
+def _gen_ue_set(num_of_ues):
+    imsi = 123000000
+    ue_set = set()
+    for _ in range(0, num_of_ues):
+        imsi_str = "IMSI" + str(imsi)
+        ipv4_src = ".".join(str(random.randint(0, 255)) for _ in range(4))
+        ipv4_dst = ".".join(str(random.randint(0, 255)) for _ in range(4))
+        rule_id = "allow." + imsi_str
+        ue_set.add(UEInfo(imsi_str, ipv4_src, ipv4_dst, rule_id))
+        imsi = imsi + 1
+    return ue_set
+
 
 @grpc_wrapper
 def set_smf_session(client, args):
@@ -170,8 +188,6 @@ def get_policy_usage(client, _):
 @grpc_wrapper
 def stress_test_grpc(client, args):
     print("WARNING: DO NOT USE ON PRODUCTION SETUPS")
-    UEInfo = namedtuple('UEInfo', ['imsi_str', 'ipv4_src', 'ipv4_dst',
-                                   'rule_id'])
     delta_time = 1/args.attaches_per_sec
     print("Attach every ~{0} seconds".format(delta_time))
 
@@ -184,18 +200,6 @@ def stress_test_grpc(client, args):
             max_bandwidth_ul=1000000000,
             max_bandwidth_dl=1000000000,
         )
-
-    def _gen_ue_set(num_of_ues):
-        imsi = 123000000
-        ue_set = set()
-        for _ in range(0, num_of_ues):
-            imsi_str = "IMSI" + str(imsi)
-            ipv4_src = ".".join(str(random.randint(0, 255)) for _ in range(4))
-            ipv4_dst = ".".join(str(random.randint(0, 255)) for _ in range(4))
-            rule_id = "allow." + imsi_str
-            ue_set.add(UEInfo(imsi_str, ipv4_src, ipv4_dst, rule_id))
-            imsi = imsi + 1
-        return ue_set
 
     for i in range (0, args.test_iterations):
         print("Starting iteration {0} of attach/detach requests".format(i))
@@ -264,6 +268,92 @@ def stress_test_grpc(client, args):
                                                             duration))
         print("Actual detach rate = {0} UEs per sec",
               round(len(ue_dict)/duration))
+
+def _build_activate_flows_data(ue_dict, disable_qos):
+    activate_flow_reqs = []
+
+    if disable_qos:
+        print("QOS Disabled")
+        apn_ambr = None
+    else:
+        print("QOS Enabled")
+        apn_ambr = AggregatedMaximumBitrate(
+            max_bandwidth_ul=1000000000,
+            max_bandwidth_dl=1000000000,
+        )
+    for ue in ue_dict:
+        request = ActivateFlowsRequest(
+            sid=SIDUtils.to_pb(ue.imsi_str),
+            ip_addr=ue.ipv4_src,
+            dynamic_rules=[PolicyRule(
+                id=ue.rule_id,
+                priority=10,
+                flow_list=[
+                    FlowDescription(match=FlowMatch(
+                        ip_dst=convert_ipv4_str_to_ip_proto(ue.ipv4_src),
+                        direction=FlowMatch.UPLINK)),
+                    FlowDescription(match=FlowMatch(
+                        ip_src=convert_ipv4_str_to_ip_proto(ue.ipv4_dst),
+                        direction=FlowMatch.DOWNLINK)),
+                ],
+            )],
+            request_origin=RequestOriginType(type=RequestOriginType.GX),
+            apn_ambr=apn_ambr,
+        )
+        request_dict = json_format.MessageToDict(request)
+        # Dumping ActivateFlows request into json
+        activate_flow_reqs.append(request_dict)
+    with open('activate_flows.json', 'w') as file:
+        json.dump(activate_flow_reqs, file, separators=(',', ':'))
+
+def _build_deactivate_flows_data(ue_dict):
+    deactivate_flow_reqs = []
+
+    for ue in ue_dict:
+        request = DeactivateFlowsRequest(
+            sid=SIDUtils.to_pb(ue.imsi_str),
+            ip_addr=ue.ipv4_src,
+            rule_ids=[ue.rule_id],
+            request_origin=RequestOriginType(type=RequestOriginType.GX),
+            remove_default_drop_flows=True)
+        request_dict = json_format.MessageToDict(request)
+        # Dumping ActivateFlows request into json
+        deactivate_flow_reqs.append(request_dict)
+    with open('deactivate_flows.json', 'w') as file:
+        json.dump(deactivate_flow_reqs, file, separators=(',', ':'))
+
+
+# Building gHZ cmd and call subprocess with given params
+def _get_ghz_cmd_params(req_type: str, num_reqs: int):
+    req_name = 'magma.lte.Pipelined/%s' % req_type
+    file_name = ''
+    if req_type == 'ActivateFlows':
+        file_name = 'activate_flows.json'
+    elif req_type == 'DeactivateFlows':
+        file_name = 'deactivate_flows.json'
+    else:
+        print('Use valid request type (ActivateFlows/DeactivateFlows)')
+        return
+    cmd_list = ['/home/vagrant/magma/lte/gateway/ghz',
+                '--insecure', '--proto',
+                '/home/vagrant/magma/lte/protos/pipelined.proto',
+                '-i  /home/vagrant/magma/',
+                '--total', str(num_reqs), '--call', req_name,
+                '-D', file_name, '-O', 'html', '0.0.0.0:50063']
+
+    subprocess.call(cmd_list)
+
+@grpc_wrapper
+def ghz_attach_test(client, args):
+    ue_dict = _gen_ue_set(args.num_of_ues)
+    _build_activate_flows_data(ue_dict, args.disable_qos)
+    _build_deactivate_flows_data(ue_dict)
+    try:
+        # call grpc GHZ load test tool
+        _get_ghz_cmd_params(args.grpc_func_name, args.num_of_ues),
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print('Check if gRPC GHZ tool is installed')
 
 def create_ng_services_parser(apps):
     """
@@ -371,6 +461,16 @@ def create_enforcement_parser(apps):
     subcmd.add_argument('--disable_qos', help='If we want to disable QOS',
                         action="store_true")
     subcmd.set_defaults(func=stress_test_grpc)
+
+    subcmd = subparsers.add_parser('ghz_attach_test',
+        help='Sends a set of Activate grpc requests, followed by Deactivates')
+    subcmd.add_argument('--num_of_ues', help='Number of total UEs to atach',
+                        type=int, default=600)
+    subcmd.add_argument('--disable_qos', help='If we want to disable QOS',
+                        action="store_true")
+    subcmd.add_argument('--grpc_func_name', help='Function name',
+                        type=str, default='ActivateFlows')
+    subcmd.set_defaults(func=ghz_attach_test)
 
 # -------------
 # UE MAC APP
