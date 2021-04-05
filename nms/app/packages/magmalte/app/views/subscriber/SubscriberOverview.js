f0f290e3e5f3d6147b7dae70a63fb446193ddc14
HannaFar <hannafarag159@gmail.com>
2021-04-05 15:41:30 +0200

    Remove subscriber session state
    Signed-off-by: HannaFar <hannafarag159@gmail.com>

--- nms/app/packages/magmalte/app/views/subscriber/SubscriberOverview.js
+++ nms/app/packages/magmalte/app/views/subscriber/SubscriberOverview.js
@@ -23,43 +23,31 @@ import type {
 
 import ActionTable from '../../components/ActionTable';
 import AddSubscriberButton from './SubscriberAddDialog';
-import AutorefreshCheckbox from '../../components/AutorefreshCheckbox';
 import CardTitleRow from '../../components/layout/CardTitleRow';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
 import Link from '@material-ui/core/Link';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import MagmaV1API from '@fbcnms/magma-api/client/WebClient';
 import NetworkContext from '../../components/context/NetworkContext';
 import PeopleIcon from '@material-ui/icons/People';
 import React from 'react';
 import ReactJson from 'react-json-view';
 import SubscriberContext from '../../components/context/SubscriberContext';
 import SubscriberDetail from './SubscriberDetail';
-import Text from '../../theme/design-system/Text';
 import TopBar from '../../components/TopBar';
 import nullthrows from '@fbcnms/util/nullthrows';
 import withAlert from '@fbcnms/ui/components/Alert/withAlert';
 
 import {FEG_LTE} from '@fbcnms/types/network';
-import {FetchSubscriberState} from '../../state/lte/SubscriberState';
-// import {
-//   REFRESH_INTERVAL,
-//   useRefreshingContext,
-// } from '../../components/context/RefreshContext';
 import {Redirect, Route, Switch} from 'react-router-dom';
 import {colors, typography} from '../../theme/default';
+import {handleSubscriberQuery} from '../../state/lte/SubscriberState';
 import {makeStyles} from '@material-ui/styles';
 import {useContext, useState} from 'react';
 import {useEnqueueSnackbar} from '@fbcnms/ui/hooks/useSnackbar';
 import {useRouter} from '@fbcnms/ui/hooks';
 
 const TITLE = 'Subscribers';
-
 const useStyles = makeStyles(theme => ({
   dashboardRoot: {
     margin: theme.spacing(5),
@@ -105,7 +93,7 @@ export default function SubscriberDashboard() {
   );
 }
 
-type SubscriberRowType = {
+export type SubscriberRowType = {
   name: string,
   imsi: string,
   activeApns?: string,
@@ -115,15 +103,6 @@ type SubscriberRowType = {
   currentUsage: string,
   dailyAvg: string,
   lastReportedTime: Date | string,
-};
-
-type SubscriberSessionRowType = {
-  apnName: string,
-  sessionId: string,
-  ipAddr: string,
-  state: string,
-  activeDuration: string,
-  activePolicies: Array<string>,
 };
 
 type Props = {
@@ -160,21 +139,7 @@ function SubscriberInternal(props: WithAlert) {
   const networkId: string = nullthrows(match.params.networkId);
   const enqueueSnackbar = useEnqueueSnackbar();
   const ctx = useContext(SubscriberContext);
-  const [refresh, setRefresh] = useState(true);
   const classes = useStyles();
-  const [_lastRefreshTime, setLastRefreshTime] = useState(
-    new Date().toLocaleString(),
-  );
-  // Auto refresh subscribers every 30 seconds
-  // const state = useRefreshingContext({
-  //   context: SubscriberContext,
-  //   networkId,
-  //   type: 'subscriber',
-  //   interval: REFRESH_INTERVAL,
-  //   enqueueSnackbar,
-  //   refresh,
-  //   lastRefreshTime,
-  // });
   const state = ctx;
   const networkCtx = useContext(NetworkContext);
   // $FlowIgnore
@@ -183,6 +148,11 @@ function SubscriberInternal(props: WithAlert) {
   const sessionState: {[string]: subscriber_state} = state.sessionState;
   const subscriberMetrics = ctx.metrics;
   const [jsonDialog, setJsonDialog] = useState(false);
+  const [pageSize, setPageSize] = useState(10);
+  // first token (page 1) is an empty string
+  const [tokenList, setTokenList] = useState(['']);
+  const onClose = () => setJsonDialog(false);
+  const tableRef = React.useRef();
   const tableColumns = [
     {title: 'Name', field: 'name'},
     {
@@ -223,68 +193,6 @@ function SubscriberInternal(props: WithAlert) {
       width: 200,
     },
   ];
-  const [tokenList, setTokenList] = useState([]);
-
-  function handleSubscriberQuery(networkId, q) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const page = 100 < q.page * q.pageSize ? 100 / q.pageSize : q.page;
-        const subscriberQuery = await MagmaV1API.getLteByNetworkIdSubscribersV2(
-          {
-            networkId,
-            pageSize: 10,
-            pageToken: page === 0 ? '' : tokenList[page - 1],
-          },
-        );
-
-        const newTokenList = tokenList;
-        if (!newTokenList.includes(subscriberQuery.next_page_token)) {
-          newTokenList.push(subscriberQuery.next_page_token);
-        }
-        setTokenList([...newTokenList]);
-
-        const sessions = {};
-        Object.keys(subscriberQuery.subscribers).map(async imsi => {
-          const state = await FetchSubscriberState({networkId, id: imsi});
-          sessions[imsi] = state;
-        });
-        // set subscriber state with current subscriber rows
-        ctx.setState?.('', undefined, {
-          state: subscriberQuery.subscribers,
-          sessionState: sessions,
-        });
-        const tableData: Array<SubscriberRowType> = subscriberQuery
-          ? Object.keys(subscriberQuery.subscribers).map((imsi: string) => {
-              const subscriberInfo = subscriberQuery.subscribers[imsi] || {};
-              const metrics = subscriberMetrics?.[`${imsi}`];
-              return {
-                name: subscriberInfo.name ?? imsi,
-                imsi: imsi,
-                service: subscriberInfo.lte?.state || '',
-                currentUsage: metrics?.currentUsage ?? '0',
-                dailyAvg: metrics?.dailyAvg ?? '0',
-                lastReportedTime:
-                  subscriberInfo.monitoring?.icmp?.last_reported_time === 0
-                    ? new Date(
-                        subscriberInfo.monitoring?.icmp?.last_reported_time,
-                      )
-                    : '-',
-              };
-            })
-          : [];
-        resolve({
-          data: tableData,
-          page: page,
-          totalCount: 200,
-        });
-      } catch (e) {
-        reject(e?.message ?? 'error retrieving events');
-      }
-    });
-  }
-  const onClose = () => setJsonDialog(false);
-  const tableRef = React.useRef();
-
   return (
     <>
       <TopBar
@@ -294,26 +202,12 @@ function SubscriberInternal(props: WithAlert) {
             label: 'Subscribers',
             to: '/subscribersv2',
             icon: PeopleIcon,
-            filters: (
-              <AddSubscriberButton
-                onClose={() => setLastRefreshTime(new Date().toLocaleString())}
-              />
-            ),
+            filters: <AddSubscriberButton onClose={() => {}} />,
           },
         ]}
       />
       <div className={classes.dashboardRoot}>
-        <CardTitleRow
-          key="title"
-          icon={PeopleIcon}
-          label={TITLE}
-          filter={() => (
-            <AutorefreshCheckbox
-              autorefreshEnabled={refresh}
-              onToggle={() => setRefresh(current => !current)}
-            />
-          )}
-        />
+        <CardTitleRow key="title" icon={PeopleIcon} label={TITLE} />
         {subscriberMap || sessionState ? (
           <div>
             <JsonDialog
@@ -324,7 +218,15 @@ function SubscriberInternal(props: WithAlert) {
             <ActionTable
               tableRef={tableRef}
               data={(query: ActionQuery) => {
-                return handleSubscriberQuery(networkId, query);
+                return handleSubscriberQuery({
+                  networkId,
+                  query,
+                  ctx,
+                  tokenList,
+                  setTokenList,
+                  pageSize,
+                  subscriberMetrics,
+                });
               }}
               columns={tableColumns}
               handleCurrRow={(row: SubscriberRowType) => setCurrRow(row)}
@@ -374,7 +276,6 @@ function SubscriberInternal(props: WithAlert) {
                               try {
                                 await ctx.setState?.(currRow.imsi);
                                 tableRef.current?.onQueryChange();
-                                setLastRefreshTime(new Date().toLocaleString());
                               } catch (e) {
                                 enqueueSnackbar(
                                   'failed deleting subscriber ' + currRow.imsi,
@@ -388,86 +289,16 @@ function SubscriberInternal(props: WithAlert) {
                       },
                     ]
               }
+              onChangeRowsPerPage={newPageSize => {
+                setPageSize(newPageSize);
+                tableRef.current?.onQueryChange();
+              }}
               options={{
                 actionsColumnIndex: -1,
-                pageSize: 10,
+                pageSize: pageSize,
                 pageSizeOptions: [10, 20],
                 showFirstLastPageButtons: false,
               }}
-              detailPanel={
-                !Object.keys(sessionState).length
-                  ? []
-                  : [
-                      {
-                        icon: () => {
-                          return <ExpandMore data-testid="details" />;
-                        },
-                        openIcon: ExpandLess,
-                        render: rowData => {
-                          const subscriber =
-                            sessionState[rowData.imsi]?.subscriber_state || {};
-                          const subscriberSessionRows: Array<SubscriberSessionRowType> = [];
-                          Object.keys(subscriber).map((apn: string) => {
-                            subscriber[apn].map(infos => {
-                              subscriberSessionRows.push({
-                                apnName: apn,
-                                sessionId: infos.session_id,
-                                ipAddr: infos.ipv4 ?? '-',
-                                state: infos.lifecycle_state,
-                                activeDuration: `${infos.active_duration_sec} sec`,
-                                activePolicies: infos.active_policy_rules,
-                              });
-                            });
-                          });
-
-                          return (
-                            <ActionTable
-                              data-testid="detailPanel"
-                              title=""
-                              data={subscriberSessionRows}
-                              columns={[
-                                {title: 'APN Name', field: 'apnName'},
-                                {title: 'Session ID', field: 'sessionId'},
-                                {title: 'State', field: 'state'},
-                                {title: 'IP Address', field: 'ipAddr'},
-                                {
-                                  title: 'Active Duration',
-                                  field: 'activeDuration',
-                                },
-                                {
-                                  title: 'Active Policy IDs',
-                                  field: 'activePolicies',
-                                  render: currRow =>
-                                    currRow.activePolicies.length ? (
-                                      <List>
-                                        {currRow.activePolicies.map(policy => (
-                                          <ListItem key={policy.id}>
-                                            <Link>{policy.id} </Link>
-                                          </ListItem>
-                                        ))}
-                                      </List>
-                                    ) : (
-                                      <Text>{'-'}</Text>
-                                    ),
-                                },
-                              ]}
-                              options={{
-                                actionsColumnIndex: -1,
-                                pageSizeOptions: [5],
-                                toolbar: false,
-                                paging: false,
-                                rowStyle: {background: '#f7f7f7'},
-                                headerStyle: {
-                                  background: '#f7f7f7',
-                                  color: colors.primary.comet,
-                                },
-                              }}
-                            />
-                          );
-                        },
-                      },
-                    ]
-              }
             />
           </div>
         ) : (
