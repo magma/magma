cf0034eceea4fad3b339ba1309252b69821d9777
chandra <59682018+chandra-77@users.noreply.github.com>
2021-04-10 02:24:16 +0530

    Fix merge conflicts
    Signed-off-by: Ulas Kozat <kozat@fb.com>

--- lte/gateway/c/oai/include/amf_securityDef.h
+++ lte/gateway/c/oai/include/amf_securityDef.h
@@ -29,27 +29,25 @@ namespace magma5g {
 /*
  * Size of the authentication challenge parameters in bytes
  */
-#define AUTH_SQN_SIZE 6 /* Sequence number:          48 bits  */
-#define AUTH_AK_SIZE 6  /* Anonymity key:            48 bits  */
-#define AUTH_AMF_SIZE 2 /* Authentication Management Field:  16 bits  */
-#define AUTH_MAC_SIZE 8 /* Message Authentication Code:  64 bits  */
-#define AUTH_AUTN_SIZE                                                         \
-  16                          /* Authentication token:     128 bits            \
-                AUTN = (SQN ⊕ AK) || AMF || MAC        */
-#define AUTH_MACS_SIZE 8      /* Re-synchronization MAC:       64 bits  */
-#define AUTH_AUTS_SIZE 16     /* Re-synchronization AUT:       128 bits */
-#define AUTH_RAND_SIZE 16     /* Random challenge:         128 bits     */
-#define AUTH_CK_SIZE 16       /* Ciphering key:            128 bits     */
-#define AUTH_IK_SIZE 16       /* Integrity key:            128 bits     */
-#define AUTH_RES_SIZE 16      /* Authentication response:      128 bits */
-#define AUTH_SNID_SIZE 3      /* Serving network's identity:   24 bits  */
-#define AUTH_KASME_SIZE 32    /* KASME security key:        256 bits    */
-#define AUTH_KAMF_SIZE 32     /* KAMF secuirity key:        256 bits    */
-#define AUTH_KNAS_INT_SIZE 16 /* NAS integrity key     */
-#define AUTH_KNAS_ENC_SIZE 16 /* NAS cyphering key     */
-#define AUTH_KAMF_SIZE 32     /* KAMF secuirity key:        256 bits    */
-#define AUTH_KGNB_SIZE AUTH_KASME_SIZE     /* gNodeB security key   */
-#define AUTH_NEXT_HOP_SIZE AUTH_KASME_SIZE /* Next Hop security parameter*/
+#define AUTH_SQN_SIZE 6       /* Sequence number:                  48 bits */
+#define AUTH_AK_SIZE 6        /* Anonymity key:                    48 bits */
+#define AUTH_AMF_SIZE 2       /* Authentication Management Field:  16 bits */
+#define AUTH_MAC_SIZE 8       /* Message Authentication Code:      64 bits */
+#define AUTH_AUTN_SIZE 16     /* Authentication token:            128 bits */
+                              /* AUTN = (SQN ⊕ AK) || AMF || MAC           */
+#define AUTH_MACS_SIZE 8      /* Re-synchronization MAC:           64 bits */
+#define AUTH_AUTS_SIZE 16     /* Re-synchronization AUT:          128 bits */
+#define AUTH_RAND_SIZE 16     /* Random challenge:                128 bits */
+#define AUTH_CK_SIZE 16       /* Ciphering key:                   128 bits */
+#define AUTH_IK_SIZE 16       /* Integrity key:                   128 bits */
+#define AUTH_RES_SIZE 16      /* Authentication response:         128 bits */
+#define AUTH_SNID_SIZE 3      /* Serving network's identity:       24 bits */
+#define AUTH_KASME_SIZE 32    /* KASME security key:              256 bits */
+#define AUTH_KNAS_INT_SIZE 16 /* NAS integrity key                128 bits */
+#define AUTH_KNAS_ENC_SIZE 16 /* NAS cyphering key                128 bits */
+#define AUTH_KAMF_SIZE 32     /* KAMF secuirity key:              256 bits */
+#define AUTH_KGNB_SIZE AUTH_KASME_SIZE     /* gNodeB security key          */
+#define AUTH_NEXT_HOP_SIZE AUTH_KASME_SIZE /* Next Hop security parameter  */
 
 /* "Separation bit" of AMF field */
 #define AUTH_AMF_SEPARATION_BIT(a) ((a) &0x80)
