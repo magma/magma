7e6d349ab4090897c6f5b97bc97cd48c1933b1e6
ashish-acl <ashish.t@altencalsoftlabs.com>
2021-04-15 13:39:06 +0000

    cleanup debug logs
    Signed-off-by: ashish-acl <ashish.t@altencalsoftlabs.com>

--- lte/gateway/c/oai/tasks/nas5g/src/ies/M5GTAIList.cpp
+++ lte/gateway/c/oai/tasks/nas5g/src/ies/M5GTAIList.cpp
@@ -22,58 +22,33 @@ TAIListMsg::~TAIListMsg(){};
 int TAIListMsg::EncodeTAIListMsg(
     TAIListMsg* TAIList, uint8_t iei, uint8_t* buffer, uint32_t len) {
   uint8_t encoded = 0;
-  //  uint8_t ielen   = 0;
-  //  uint8_t* lenPtr;
-  // int i;
   if (iei > 0) {
     CHECK_IEI_ENCODER(iei, (unsigned char) TAIList->iei);
     *buffer = iei;
     MLOG(MDEBUG) << "iei = " << hex << int(*(buffer + encoded));
     encoded++;
   }
-  MLOG(MDEBUG) << "encoded = " << encoded;
-  MLOG(MDEBUG) << "iei-- =  " << hex << int(*(buffer - 2));
-  MLOG(MDEBUG) << "iei- = " << hex << int(*(buffer - 1));
-  MLOG(MDEBUG) << "iei = " << hex << int(*(buffer));
-  MLOG(MDEBUG) << "iei+ = " << hex << int(*(buffer + 1));
-
-  // lenPtr = (buffer + encoded);
   *(buffer + encoded) = TAIList->len;
   encoded++;
-  MLOG(MDEBUG) << "encoded = " << encoded;
-  MLOG(MDEBUG) << "iei = " << hex << int(*(buffer));
-  MLOG(MDEBUG) << "iei+ = " << hex << int(*(buffer + 1));
 
   *(buffer + encoded) = 0x00 | ((TAIList->list_type & 0x03) << 5) |
                         (TAIList->num_elements & 0x1f);
   encoded++;
-  MLOG(MDEBUG) << "encoded = " << encoded;
-  MLOG(MDEBUG) << "iei = " << hex << int(*(buffer));
-  MLOG(MDEBUG) << "iei+ = " << hex << int(*(buffer + 1));
   *(buffer + encoded) =
       0x00 | ((TAIList->mcc_digit2 & 0x0f) << 4) | (TAIList->mcc_digit1 & 0x0f);
   MLOG(MDEBUG) << "mcc_digit2 >mcc_digit1 type_of_identity = " << hex
                << int(*(buffer + encoded));
   encoded++;
-  MLOG(MDEBUG) << "encoded = " << encoded;
-  MLOG(MDEBUG) << "iei = " << hex << int(*(buffer));
-  MLOG(MDEBUG) << "iei+ = " << hex << int(*(buffer + 1));
   *(buffer + encoded) =
       0x00 | ((TAIList->mnc_digit3 & 0x0f) << 4) | (TAIList->mcc_digit3 & 0x0f);
   MLOG(MDEBUG) << "mnc_digit3 >mcc_digit3 type_of_identity = " << hex
                << int(*(buffer + encoded));
   encoded++;
-  MLOG(MDEBUG) << "encoded = " << encoded;
-  MLOG(MDEBUG) << "iei = " << hex << int(*(buffer));
-  MLOG(MDEBUG) << "iei+ = " << hex << int(*(buffer + 1));
   *(buffer + encoded) =
       0x00 | ((TAIList->mnc_digit2 & 0x0f) << 4) | (TAIList->mnc_digit1 & 0x0f);
   MLOG(MDEBUG) << "mnc_digit2 >mcc_digit1 type_of_identity = " << hex
                << int(*(buffer + encoded));
   encoded++;
-  MLOG(MDEBUG) << "encoded = " << encoded;
-  MLOG(MDEBUG) << "iei = " << hex << int(*(buffer));
-  MLOG(MDEBUG) << "iei+ = " << hex << int(*(buffer + 1));
 
   *(buffer + encoded) = TAIList->tac[0];
   encoded++;
@@ -81,8 +56,6 @@ int TAIListMsg::EncodeTAIListMsg(
   encoded++;
   *(buffer + encoded) = TAIList->tac[2];
   encoded++;
-
-  //*lenPtr = encoded - 1 - ((iei > 0) ? 1 : 0);
 
   return (encoded);
 }
