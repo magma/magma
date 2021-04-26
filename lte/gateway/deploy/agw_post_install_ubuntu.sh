7985b3493d8b7635e489e0071675cd0bcdbdb064
Pravin B Shelar <pbshelar@fb.com>
2021-04-26 11:10:31 -0700

    AGW: ubuntu: deploy: Fix post deploy script
    Following patch fixes a type and shellcheck warnings.
    
    Signed-off-by: Pravin B Shelar <pbshelar@fb.com>

--- lte/gateway/deploy/agw_post_install_ubuntu.sh
+++ lte/gateway/deploy/agw_post_install_ubuntu.sh
@@ -6,7 +6,7 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
+distributed under the License is distributed on an AS IS BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
@@ -46,8 +46,6 @@ if ! grep -q "$MAGMA_USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then
     addError "Restart installation following agw_install_ubuntu.sh, magma has to be sudoer"
 fi
 
-KVERS=$(uname -r)
-
 interfaces=("eth1" "eth0" "gtp_br0" "uplink_br0")
 for interface in "${interfaces[@]}"; do
     OPERSTATE_LOCATION="/sys/class/net/$interface/operstate"
@@ -86,7 +84,7 @@ done
 
 packages=("magma" "magma-cpp-redis" "magma-libfluid" "libopenvswitch" "openvswitch-datapath-dkms" "openvswitch-common" "openvswitch-switch")
 for package in "${packages[@]}"; do
-    PACKAGE_INSTALLED=$(dpkg-query -W -f='${Status}' $package  > /dev/null 2>&1 && echo "$SUCCESS_MESSAGE")
+    PACKAGE_INSTALLED=$(dpkg-query -W -f='${Status}' "$package"  > /dev/null 2>&1 && echo "$SUCCESS_MESSAGE")
     if [ "$PACKAGE_INSTALLED" != "$SUCCESS_MESSAGE" ]; then
         addError "$package hasn't been installed" "Rerun the agw_install.sh"
     fi
@@ -123,7 +121,7 @@ if [ -d "/var/opt/magma/configs/" ]; then
         echo "- Check control proxy content"
         cp_content=("cloud_address" "cloud_port" "bootstrap_address" "bootstrap_port" "rootca_cert" "fluentd_address" "fluentd_port")
         for content in "${cp_content[@]}"; do
-            if ! grep -q $content $CP; then
+            if ! grep -q "$content" $CP; then
                 echo "Missing $content in control proxy"
             fi
         done
