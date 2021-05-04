bb993e09e685677449843a097b81b84501f80216
Pravin Shelar <pshelar@ovn.org>
2021-05-04 11:28:50 -0700

    AGW: deploy: post install script cleanups (#6611)
    Following patch adds script download to install script to
    avoid manually downloading the post deploy script.
    It also fixes file permissions of all bash scripts.
    
    Signed-off-by: Pravin B Shelar <pbshelar@fb.com>

--- lte/gateway/deploy/agw_install_ubuntu.sh
+++ lte/gateway/deploy/agw_install_ubuntu.sh
@@ -168,11 +168,11 @@ if [ "$MAGMA_INSTALLED" != "$SUCCESS_MESSAGE" ]; then
   su - $MAGMA_USER -c "ansible-playbook -e \"MAGMA_ROOT='/home/$MAGMA_USER/magma' OUTPUT_DIR='/tmp'\" -i $DEPLOY_PATH/agw_hosts $DEPLOY_PATH/magma_deploy.yml"
 
   echo "Deleting boot script if it exists"
-  if [ -f "$AGW_INSTALL_CONFIG" ]; then
-    rm -rf $AGW_INSTALL_CONFIG
-  fi
+  rm -rf $AGW_INSTALL_CONFIG
   rm -rf /home/$MAGMA_USER/build
-  echo "AGW installation is done, make sure all services above are running correctly.. rebooting"
+  echo "AGW installation is done, Run agw_post_install_ubuntu.sh install script after reboot to finish installation"
+  wget https://raw.githubusercontent.com/magma/magma/"$MAGMA_VERSION"/lte/gateway/deploy/agw_post_install_ubuntu.sh
+
   reboot
 else
   echo "Magma already installed, skipping.."
