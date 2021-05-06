1925050acc08e5af2d7210f5aea5a6f2d60fb605
rashmi <rashmi.sarwad@radisys.com>
2021-05-07 04:11:45 +0530

    Fixed the compilation issue
    Signed-off-by: rashmi <rashmi.sarwad@radisys.com>

--- lte/gateway/c/oai/tasks/mme_app/mme_app_ip_imsi.cpp
+++ lte/gateway/c/oai/tasks/mme_app/mme_app_ip_imsi.cpp
@@ -1,7 +1,8 @@
 #include <unordered_map>
+#include <iostream>
 #include "mme_app_ip_imsi.h"
-
-typedef std::unordered_map<uint32_t, uint64_t> Ipv4Map;
+using namespace std;
+typedef unordered_map<string, uint64_t> Ipv4Map;
 
 Ipv4Map ipv4map;
 
@@ -14,18 +15,20 @@ void initialize_ipv4_map() {
 void mme_app_log_ipv4_imsi_map() {
   OAILOG_FUNC_IN(LOG_MME_APP);
   for (auto itr = ipv4map.begin(); itr != ipv4map.end(); ++itr) {
-    OAILOG_ERROR(
-        LOG_MME_APP, "key: %d \t value:%lu \n", itr->first, itr->second);
+    OAILOG_TRACE(
+        LOG_MME_APP, "key: %s \t value:%lu \n", itr->first.c_str(),
+        itr->second);
   }
   OAILOG_FUNC_OUT(LOG_MME_APP);
 }
 
 int mme_app_insert_ue_ipv4_addr(uint32_t ipv4_addr, uint64_t imsi64) {
   OAILOG_FUNC_IN(LOG_MME_APP);
-  auto rc = ipv4map.insert({ipv4_addr, imsi64});
+  char ipv4[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, (void*) &ipv4_addr, ipv4, INET_ADDRSTRLEN);
+  auto rc = ipv4map.insert({ipv4, imsi64});
   if (!rc.second) {
-    OAILOG_ERROR_UE(
-        LOG_MME_APP, imsi64, "Failed to insert ipv4_addr:%x \n", ipv4_addr);
+    OAILOG_ERROR_UE(LOG_MME_APP, imsi64, "Failed to insert ipv4:%x \n", ipv4);
     OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNerror);
   }
   mme_app_log_ipv4_imsi_map();
@@ -34,7 +37,9 @@ int mme_app_insert_ue_ipv4_addr(uint32_t ipv4_addr, uint64_t imsi64) {
 
 int mme_app_get_imsi_from_ipv4(uint32_t ipv4_addr, uint64_t* imsi64) {
   OAILOG_FUNC_IN(LOG_MME_APP);
-  auto itr = ipv4map.find(ipv4_addr);
+  char ipv4[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, (void*) &ipv4_addr, ipv4, INET_ADDRSTRLEN);
+  auto itr = ipv4map.find(ipv4);
   if (itr == ipv4map.end()) {
     OAILOG_ERROR(LOG_MME_APP, " No imsi found for ip:%x \n", ipv4_addr);
     OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNerror);
@@ -46,7 +51,9 @@ int mme_app_get_imsi_from_ipv4(uint32_t ipv4_addr, uint64_t* imsi64) {
 
 void mme_app_remove_ue_ipv4_addr(uint32_t ipv4_addr) {
   OAILOG_FUNC_IN(LOG_MME_APP);
-  if (!(ipv4map.erase(ipv4_addr))) {
+  char ipv4[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, (void*) &ipv4_addr, ipv4, INET_ADDRSTRLEN);
+  if (!(ipv4map.erase(ipv4))) {
     OAILOG_ERROR(
         LOG_MME_APP,
         "Failed to remove an entry for IP:%x from ipv4_imsi map \n", ipv4_addr);
