b257d0d6845e89ab771cf3fd7509d044a01f0c06
rashmi <rashmi.sarwad@radisys.com>
2021-05-07 04:10:17 +0530

    Fixed the compilation issue
    Signed-off-by: rashmi <rashmi.sarwad@radisys.com>

--- lte/gateway/c/oai/tasks/mme_app/mme_app_ip_imsi.cpp
+++ lte/gateway/c/oai/tasks/mme_app/mme_app_ip_imsi.cpp
@@ -1,6 +1,5 @@
 #include <unordered_map>
 #include "mme_app_ip_imsi.h"
-#include "magma_logging.h"
 
 typedef std::unordered_map<uint32_t, uint64_t> Ipv4Map;
 
@@ -12,6 +11,15 @@ void initialize_ipv4_map() {
   OAILOG_FUNC_OUT(LOG_MME_APP);
 }
 
+void mme_app_log_ipv4_imsi_map() {
+  OAILOG_FUNC_IN(LOG_MME_APP);
+  for (auto itr = ipv4map.begin(); itr != ipv4map.end(); ++itr) {
+    OAILOG_ERROR(
+        LOG_MME_APP, "key: %d \t value:%lu \n", itr->first, itr->second);
+  }
+  OAILOG_FUNC_OUT(LOG_MME_APP);
+}
+
 int mme_app_insert_ue_ipv4_addr(uint32_t ipv4_addr, uint64_t imsi64) {
   OAILOG_FUNC_IN(LOG_MME_APP);
   auto rc = ipv4map.insert({ipv4_addr, imsi64});
@@ -20,15 +28,8 @@ int mme_app_insert_ue_ipv4_addr(uint32_t ipv4_addr, uint64_t imsi64) {
         LOG_MME_APP, imsi64, "Failed to insert ipv4_addr:%x \n", ipv4_addr);
     OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNerror);
   }
+  mme_app_log_ipv4_imsi_map();
   OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNok);
-}
-
-void mme_app_log_ipv4_imsi_map() {
-  OAILOG_FUNC_IN(LOG_MME_APP);
-  for (auto itr = ipv4map.begin(); itr != ipv4map.end(); ++itr) {
-    MLOG(MDEBUG) << "Key: " << itr->first << "and value: " << itr->second;
-  }
-  OAILOG_FUNC_OUT(LOG_MME_APP);
 }
 
 int mme_app_get_imsi_from_ipv4(uint32_t ipv4_addr, uint64_t* imsi64) {
