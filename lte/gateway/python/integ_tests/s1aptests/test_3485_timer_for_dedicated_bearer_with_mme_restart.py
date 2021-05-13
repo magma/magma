b4b6b74f4df0bc2fbfa9c027ab93cc48a3209ab6
rashmi <rashmi.sarwad@radisys.com>
2021-05-13 07:48:30 +0530

    Modified script to address re-trasmitted activate default/dedicated bearer requests
    Signed-off-by: rashmi <rashmi.sarwad@radisys.com>

--- lte/gateway/python/integ_tests/s1aptests/test_3485_timer_for_dedicated_bearer_with_mme_restart.py
+++ lte/gateway/python/integ_tests/s1aptests/test_3485_timer_for_dedicated_bearer_with_mme_restart.py
@@ -12,9 +12,10 @@ limitations under the License.
 """
 
 import unittest
-import s1ap_types
 import time
+import ipaddress
 
+import s1ap_types
 from integ_tests.s1aptests import s1ap_wrapper
 from integ_tests.s1aptests.s1ap_utils import SpgwUtil
 from s1ap_utils import MagmadUtil
@@ -23,7 +24,7 @@ from s1ap_utils import MagmadUtil
 class Test3485TimerForDedicatedBearerWithMmeRestart(unittest.TestCase):
     def setUp(self):
         self._s1ap_wrapper = s1ap_wrapper.TestWrapper(
-            stateless_mode=MagmadUtil.stateless_cmds.ENABLE
+            stateless_mode=MagmadUtil.stateless_cmds.ENABLE,
         )
         self._spgw_util = SpgwUtil()
 
@@ -32,7 +33,7 @@ class Test3485TimerForDedicatedBearerWithMmeRestart(unittest.TestCase):
 
     def test_3485_timer_for_dedicated_bearer_with_mme_restart(self):
         """ Test case validates the functionality of 3485 timer for
-            Dedicated bearer while MME restarts
+        Dedicated bearer while MME restarts
         Step1: UE attaches to network
         Step2: Send an indication to S1ap stack to drop E-Rab Setup
                Request message, sent as part of secondary PDN activation
@@ -45,7 +46,7 @@ class Test3485TimerForDedicatedBearerWithMmeRestart(unittest.TestCase):
         Step6: TFW shall receive re-transmitted Actiavte Dedicated EPS
                Bearer Context Request and send Actiavte Dedicated EPS Bearer
                Context Accept
-        Step7: TFW shall initaite de-activation of deidicated bearer and then
+        Step7: TFW shall initiate de-activation of deidicated bearer and then
                initiate Detach procedure.
         """
 
@@ -57,121 +58,113 @@ class Test3485TimerForDedicatedBearerWithMmeRestart(unittest.TestCase):
         wait_for_s1 = [True, False]
         self._s1ap_wrapper.configUEDevice(num_ues)
 
-        for i in range(num_ues):
+        for idx in range(num_ues):
             req = self._s1ap_wrapper.ue_req
             print(
-                "********************** Running End to End attach for ",
-                "UE id ",
+                '********************** Running End to End attach for ',
+                'UE id ',
                 req.ue_id,
             )
             # Now actually complete the attach
-            self._s1ap_wrapper._s1_util.attach(
+            attach = self._s1ap_wrapper._s1_util.attach(
                 req.ue_id,
                 s1ap_types.tfwCmd.UE_END_TO_END_ATTACH_REQUEST,
                 s1ap_types.tfwCmd.UE_ATTACH_ACCEPT_IND,
                 s1ap_types.ueAttachAccept_t,
             )
 
+            addr = attach.esmInfo.pAddr.addrInfo
+            default_ip = ipaddress.ip_address(bytes(addr[:4]))
+
             # Wait on EMM Information from MME
             self._s1ap_wrapper._s1_util.receive_emm_info()
 
-            print("*** Sending indication to drop Erab Setup Req ***")
-            drop_erab_setup_req = s1ap_types.UeDropErabSetupReq_t()
-            drop_erab_setup_req.ue_Id = req.ue_id
-            drop_erab_setup_req.flag = True
-            self._s1ap_wrapper.s1_util.issue_cmd(
-                s1ap_types.tfwCmd.UE_DROP_ERAB_SETUP_REQ, drop_erab_setup_req
-            )
-
             time.sleep(2)
             print(
-                "********************** Adding dedicated bearer to IMSI",
-                "".join([str(i) for i in req.imsi]),
+                '********************** Adding dedicated bearer to IMSI',
+                ''.join([str(idx) for idx in req.imsi]),
             )
+
+            # Create default flow list
+            flow_list = self._spgw_util.create_default_ipv4_flows()
             self._spgw_util.create_bearer(
-                "IMSI" + "".join([str(i) for i in req.imsi]), 5
+                'IMSI' + ''.join([str(idx) for idx in req.imsi]),
+                attach.esmInfo.epsBearerId,
+                flow_list,
             )
+
             response = self._s1ap_wrapper.s1_util.get_response()
-            print("receved masg", s1ap_types.tfwCmd.UE_ERAB_DROP_IND.value)
-            print("receved masg1", response.msg_type)
             self.assertEqual(
-                response.msg_type, s1ap_types.tfwCmd.UE_ERAB_DROP_IND.value
+                response.msg_type, s1ap_types.tfwCmd.UE_ACT_DED_BER_REQ.value,
+            )
+            print(
+                '*******************Received first Activate dedicated bearer '
+                'request',
             )
 
-            print(
-                "*******************Received ERAB_DROP_IND For dedicated ",
-                "bearer request",
-            )
-
-            print(
-                "*** Sending indication to disbale dropping of Erab Setup ",
-                "Request ***",
-            )
-            drop_erab_setup_req = s1ap_types.UeDropErabSetupReq_t()
-            drop_erab_setup_req.ue_Id = req.ue_id
-            drop_erab_setup_req.flag = False
-            self._s1ap_wrapper.s1_util.issue_cmd(
-                s1ap_types.tfwCmd.UE_DROP_ERAB_SETUP_REQ, drop_erab_setup_req
-            )
-            print(
-                "************************* Restarting MME service on",
-                "gateway",
-            )
-            self._s1ap_wrapper.magmad_util.restart_services(["mme"])
+            print('***** Restarting MME service on gateway')
+            self._s1ap_wrapper.magmad_util.restart_services(['mme'])
 
             for j in range(20):
-                print("Waiting for", j, "seconds")
+                print('Waiting for', j, 'seconds')
                 time.sleep(1)
 
             response = self._s1ap_wrapper.s1_util.get_response()
-            self.assertEqual(
-                response.msg_type, s1ap_types.tfwCmd.UE_ACT_DED_BER_REQ.value
-            )
-            print(
-                "*******************Received Activate dedicated bearer "
-                "request"
-            )
             act_ded_ber_ctxt_req = response.cast(
-                s1ap_types.UeActDedBearCtxtReq_t
+                s1ap_types.UeActDedBearCtxtReq_t,
             )
-
             self._s1ap_wrapper.sendActDedicatedBearerAccept(
-                req.ue_id, act_ded_ber_ctxt_req.bearerId
+                req.ue_id, act_ded_ber_ctxt_req.bearerId,
             )
-            print("*******************Send Activate dedicated bearer accept")
+            print('*******************Send Activate dedicated bearer accept')
             time.sleep(4)
 
             print(
-                "********************** Deleting dedicated bearer for IMSI",
-                "".join([str(i) for i in req.imsi]),
+                '********************** Deleting dedicated bearer for IMSI',
+                ''.join([str(idx) for idx in req.imsi]),
             )
             self._spgw_util.delete_bearer(
-                "IMSI" + "".join([str(i) for i in req.imsi]), 5, 6
+                'IMSI' + ''.join([str(idx) for idx in req.imsi]), 5, 6,
             )
-
+            """During wait time, mme may send multiple times Activate EPS bearer
+               context request, after sending response one message ignore other
+               messages """
             response = self._s1ap_wrapper.s1_util.get_response()
+            while (
+                response.msg_type
+                != s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value
+            ):
+                response = self._s1ap_wrapper.s1_util.get_response()
             self.assertEqual(
                 response.msg_type,
                 s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value,
             )
-
-            print("******************* Received deactivate eps bearer context")
+            print('******************* Received deactivate eps bearer context')
 
             deactv_bearer_req = response.cast(s1ap_types.UeDeActvBearCtxtReq_t)
             self._s1ap_wrapper.sendDeactDedicatedBearerAccept(
-                req.ue_id, deactv_bearer_req.bearerId
+                req.ue_id, deactv_bearer_req.bearerId,
+            )
+            # Verify if the rule for dedicated bearer is deleted
+            dl_flow_rules = {
+                default_ip: [],
+            }
+            # 1 UL flow for default bearer
+            num_ul_flows = 1
+            self._s1ap_wrapper.s1_util.verify_flow_rules(
+                num_ul_flows, dl_flow_rules,
             )
 
             time.sleep(5)
             print(
-                "********************** Running UE detach for UE id ",
+                '********************** Running UE detach for UE id ',
                 req.ue_id,
             )
             # Now detach the UE
             self._s1ap_wrapper.s1_util.detach(
-                req.ue_id, detach_type[i], wait_for_s1[i]
+                req.ue_id, detach_type[idx], wait_for_s1[idx],
             )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- lte/gateway/python/integ_tests/s1aptests/test_3485_timer_for_default_bearer_with_mme_restart.py
+++ lte/gateway/python/integ_tests/s1aptests/test_3485_timer_for_default_bearer_with_mme_restart.py
@@ -13,7 +13,6 @@ limitations under the License.
 
 import unittest
 import time
-
 import s1ap_types
 import s1ap_wrapper
 from s1ap_utils import MagmadUtil
@@ -22,7 +21,7 @@ from s1ap_utils import MagmadUtil
 class Test3485TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
     def setUp(self):
         self._s1ap_wrapper = s1ap_wrapper.TestWrapper(
-            stateless_mode=MagmadUtil.stateless_cmds.ENABLE
+            stateless_mode=MagmadUtil.stateless_cmds.ENABLE,
         )
 
     def tearDown(self):
@@ -30,20 +29,21 @@ class Test3485TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
 
     def test_3485_timer_for_default_bearer_with_mme_restart(self):
         """ Test case validates the functionality of 3485 timer for
-            default bearer while MME restarts
+        default bearer while MME restarts
         Step1: UE attaches to network
         Step2: Send an indication to S1ap stack to drop E-Rab Setup
-               Request message, sent as part of secondary PDN activation
-               procedure.
-        Step3: Initaite activation of secondary PDN
+        Request message, sent as part of secondary PDN activation
+        procedure.
+        Step3: Initiate activation of secondary PDN
         Step4: Send an indication to S1ap stack to not to drop E-Rab Setup
-               Request message, so that re-transmitted message reaches to
-               TFW
+        Request message, so that re-transmitted message reaches to
+        TFW
         Step5: Send command to Magma to restart mme service
         Step6: TFW shall receive the PDN connectivity response
-        Step7: TFW shall initaite de-activation of secondary PDN and then
-               initiate Detach procedure.
+        Step7: TFW shall initiate de-activation of secondary PDN and then
+        initiate Detach procedure.
         """
+
         num_ue = 1
 
         self._s1ap_wrapper.configUEDevice(num_ue)
@@ -52,23 +52,23 @@ class Test3485TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
 
         # APN of the secondary PDN
         ims = {
-            "apn_name": "ims",  # APN-name
-            "qci": 5,  # qci
-            "priority": 15,  # priority
-            "pre_cap": 0,  # preemption-capability
-            "pre_vul": 0,  # preemption-vulnerability
-            "mbr_ul": 200000000,  # MBR UL
-            "mbr_dl": 100000000,  # MBR DL
+            'apn_name': 'ims',  # APN-name
+            'qci': 5,  # qci
+            'priority': 15,  # priority
+            'pre_cap': 0,  # preemption-capability
+            'pre_vul': 0,  # preemption-vulnerability
+            'mbr_ul': 200000000,  # MBR UL
+            'mbr_dl': 100000000,  # MBR DL
         }
 
         # APN list to be configured
         apn_list = [ims]
 
         self._s1ap_wrapper.configAPN(
-            "IMSI" + "".join([str(i) for i in req.imsi]), apn_list
+            'IMSI' + ''.join([str(idx) for idx in req.imsi]), apn_list,
         )
         print(
-            "************************* Running End to End attach for UE id ",
+            '************************* Running End to End attach for UE id ',
             ue_id,
         )
         # Attach
@@ -82,46 +82,47 @@ class Test3485TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
         # Wait on EMM Information from MME
         self._s1ap_wrapper._s1_util.receive_emm_info()
 
-        print("*** Sending indication to drop Erab Setup Req ***")
-        drop_erab_setup_req = s1ap_types.UeDropErabSetupReq_t()
-        drop_erab_setup_req.ue_Id = req.ue_id
-        drop_erab_setup_req.flag = True
+        print(
+            '*** Sending indication to drop Activate Default EPS bearer Ctxt'
+            ' Req ***',
+        )
+        drop_acctv_dflt_bearer_req = (
+            s1ap_types.UeDropActvDefaultEpsBearCtxtReq_t()
+        )
+        drop_acctv_dflt_bearer_req.ue_id = req.ue_id
+        drop_acctv_dflt_bearer_req.dropActDfltEpsBearCtxtReq = True
         self._s1ap_wrapper.s1_util.issue_cmd(
-            s1ap_types.tfwCmd.UE_DROP_ERAB_SETUP_REQ, drop_erab_setup_req
+            s1ap_types.tfwCmd.UE_DROP_ACTV_DEFAULT_EPS_BEARER_CTXT_REQ,
+            drop_acctv_dflt_bearer_req,
         )
 
         time.sleep(2)
-        print("*** Sending PDN connectivity Req ***")
+        print('*** Sending PDN connectivity Req ***')
         # Send PDN Connectivity Request
-        apn = "ims"
+        apn = 'ims'
         self._s1ap_wrapper.sendPdnConnectivityReq(ue_id, apn)
         # Receive PDN CONN RSP/Activate default EPS bearer context request
-        response = self._s1ap_wrapper.s1_util.get_response()
-        self.assertEqual(
-            response.msg_type, s1ap_types.tfwCmd.UE_ERAB_DROP_IND.value
-        )
-        print(
-            "*******************Received ERAB_DROP_IND for last received ",
-            "E-Rab Setup Request for secondary PDN's default bearer ",
-        )
 
-        print(
-            "*** Sending indication to disbale dropping of Erab Setup Req",
-            " ***",
-        )
-        drop_erab_setup_req = s1ap_types.UeDropErabSetupReq_t()
-        drop_erab_setup_req.ue_Id = req.ue_id
-        drop_erab_setup_req.flag = False
-        self._s1ap_wrapper.s1_util.issue_cmd(
-            s1ap_types.tfwCmd.UE_DROP_ERAB_SETUP_REQ, drop_erab_setup_req
-        )
+        print('************************* Restarting MME service on gateway')
+        self._s1ap_wrapper.magmad_util.restart_services(['mme'])
 
-        print("************************* Restarting MME service on gateway")
-        self._s1ap_wrapper.magmad_util.restart_services(["mme"])
-
-        for j in range(30):
-            print("Waiting for", j, "seconds")
+        for j in range(20):
+            print('Waiting for', j, 'seconds')
             time.sleep(1)
+
+        print(
+            '*** Sending indication to drop Activate Default EPS bearer Ctxt'
+            ' Req ***',
+        )
+        drop_acctv_dflt_bearer_req = (
+            s1ap_types.UeDropActvDefaultEpsBearCtxtReq_t()
+        )
+        drop_acctv_dflt_bearer_req.ue_id = req.ue_id
+        drop_acctv_dflt_bearer_req.dropActDfltEpsBearCtxtReq = False
+        self._s1ap_wrapper.s1_util.issue_cmd(
+            s1ap_types.tfwCmd.UE_DROP_ACTV_DEFAULT_EPS_BEARER_CTXT_REQ,
+            drop_acctv_dflt_bearer_req,
+        )
 
         retransmitted_response = self._s1ap_wrapper.s1_util.get_response()
         self.assertEqual(
@@ -129,7 +130,7 @@ class Test3485TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
             s1ap_types.tfwCmd.UE_PDN_CONN_RSP_IND.value,
         )
         act_def_bearer_req = retransmitted_response.cast(
-            s1ap_types.uePdnConRsp_t
+            s1ap_types.uePdnConRsp_t,
         )
 
         # Send PDN Disconnect
@@ -139,35 +140,39 @@ class Test3485TimerForDefaultBearerWithMmeRestart(unittest.TestCase):
             act_def_bearer_req.m.pdnInfo.epsBearerId
         )
         self._s1ap_wrapper._s1_util.issue_cmd(
-            s1ap_types.tfwCmd.UE_PDN_DISCONNECT_REQ, pdn_disconnect_req
+            s1ap_types.tfwCmd.UE_PDN_DISCONNECT_REQ, pdn_disconnect_req,
         )
 
         # Receive UE_DEACTIVATE_BER_REQ
         response = self._s1ap_wrapper.s1_util.get_response()
+        while (
+            response.msg_type != s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value
+        ):
+            response = self._s1ap_wrapper.s1_util.get_response()
         self.assertEqual(
-            response.msg_type, s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value
+            response.msg_type, s1ap_types.tfwCmd.UE_DEACTIVATE_BER_REQ.value,
         )
 
         print(
-            "******************* Received deactivate eps bearer context"
-            " request"
+            '******************* Received deactivate eps bearer context'
+            ' request',
         )
         # Send DeactDedicatedBearerAccept
         deactv_bearer_req = response.cast(s1ap_types.UeDeActvBearCtxtReq_t)
         self._s1ap_wrapper.sendDeactDedicatedBearerAccept(
-            req.ue_id, deactv_bearer_req.bearerId
+            req.ue_id, deactv_bearer_req.bearerId,
         )
 
         print(
-            "************************* Running UE detach (switch-off) for ",
-            "UE id ",
+            '************************* Running UE detach (switch-off) for ',
+            'UE id ',
             ue_id,
         )
         # Now detach the UE
         self._s1ap_wrapper.s1_util.detach(
-            ue_id, s1ap_types.ueDetachType_t.UE_SWITCHOFF_DETACH.value, False
+            ue_id, s1ap_types.ueDetachType_t.UE_SWITCHOFF_DETACH.value, False,
         )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
