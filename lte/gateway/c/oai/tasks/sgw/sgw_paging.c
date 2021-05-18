07e554a3eb9843db90fe77905366840efd77e35d
rsarwad <30685618+rsarwad@users.noreply.github.com>
2021-05-18 10:11:22 +0530

    Modified code to handle paging request for both roaming and non-roaming UEs (#6789)

--- lte/gateway/c/oai/tasks/sgw/sgw_paging.c
+++ lte/gateway/c/oai/tasks/sgw/sgw_paging.c
@@ -31,27 +31,18 @@
 #include "itti_types.h"
 #include "s11_messages_types.h"
 
-int sgw_send_paging_request(const struct in_addr* dest_ip) {
-  char* imsi;
-  int ret = get_subscriber_id_from_ipv4(dest_ip, &imsi);
-  if (ret > 0) {
-    char ip_str[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &(dest_ip->s_addr), ip_str, INET_ADDRSTRLEN);
-    OAILOG_ERROR(
-        TASK_SPGW_APP, "Subscriber could not be found for ip %s\n", ip_str);
-    return ret;
-  }
-  OAILOG_DEBUG(TASK_SPGW_APP, "Paging procedure initiated for IMSI%s\n", imsi);
+void sgw_send_paging_request(const struct in_addr* dest_ip) {
+  OAILOG_DEBUG(
+      TASK_SPGW_APP, "Paging procedure initiated for ue_ipv4: %x\n",
+      dest_ip->s_addr);
   MessageDef* message_p                       = NULL;
   itti_s11_paging_request_t* paging_request_p = NULL;
 
   message_p        = itti_alloc_new_message(TASK_SPGW_APP, S11_PAGING_REQUEST);
   paging_request_p = &message_p->ittiMsg.s11_paging_request;
   memset((void*) paging_request_p, 0, sizeof(itti_s11_paging_request_t));
-  paging_request_p->imsi = strdup(imsi);
-  free(imsi);
+  paging_request_p->ipv4_addr = *dest_ip;
 
-  IMSI_STRING_TO_IMSI64(imsi, &message_p->ittiMsgHeader.imsi);
-  ret = send_msg_to_task(&spgw_app_task_zmq_ctx, TASK_MME_APP, message_p);
-  return ret;
+  send_msg_to_task(&spgw_app_task_zmq_ctx, TASK_MME_APP, message_p);
+  return;
 }
