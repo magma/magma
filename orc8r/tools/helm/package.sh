1d82e5e843c32a9d9a5b59750d17e08edb77519e
quentinDERORY <15911421+quentinDERORY@users.noreply.github.com>
2021-05-26 14:16:55 -0700

    [ci][orc8r][helm][gha] push helm charts to artifactory (#7054)
    * (Devops) Add push of helm charts to jfrog Artifactory
    
    New needed environment variables are
    HELM_CHART_MUSEUM_URL for the artifactory URL
    HELM_CHART_MUSEUM_USERNAME for the artifactory username
    HELM_CHART_MUSEUM_TOKEN for the artifactory password token
    
    Legacy purpose of the script is still intact if not defining HELM_CHART_MUSEUM_URL
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orc8r][helm] Fix linting issues
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orc8r][helm] Add documentation to push helm charts to the artifactory
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orc8r][helm] Exit script if the push to artifactory is faling
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orce8r][helm] Address PR comments
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orc8r][ci] Add github action job to push helm charts to artifcatory
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [ci] Fix ShellCheck issues
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orc8r][helm][ci] Address PR comments
    
    Add long options name to script
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    
    * [orc8r][ci] documentations fixes and some more comments
    
    Signed-off-by: Quentin, Derory <15911421+quentinDERORY@users.noreply.github.com>
    Signed-off-by: Ramon Melero <ramonmelero@fb.com>

--- orc8r/tools/helm/package.sh
+++ orc8r/tools/helm/package.sh
@@ -21,8 +21,26 @@ FFWA="federated_fwa"
 ALL="all"
 ORC8R_VERSION="1.4"
 
+# package chart, update index.yaml and push it to artifactory
+update_and_send_to_artifactory () {
+  CHART_PATH="$1"
+  helm dependency update "$CHART_PATH"
+  # shellcheck disable=SC2086
+  # We want $VERSION to be split as this is an option to the helm command
+  ARTIFACT_PATH="$(helm package "$CHART_PATH" $VERSION | awk '{print $8}')"
+  helm repo index .
+  MD5_CHECKSUM="$(md5sum "$ARTIFACT_PATH" | awk '{print $1}')"
+  SHA1_CHECKSUM="$(shasum -a 1 "$ARTIFACT_PATH" | awk '{ print $1 }')"
+  SHA256_CHECKSUM="$(shasum -a 256 "$ARTIFACT_PATH" | awk '{ print $1 }')"
+  curl --user "$HELM_CHART_MUSEUM_USERNAME":"$HELM_CHART_MUSEUM_TOKEN" --fail \
+              --header "X-Checksum-MD5:${MD5_CHECKSUM}" \
+              --header "X-Checksum-Sha1:${SHA1_CHECKSUM}" \
+              --header "X-Checksum-Sha256:${SHA256_CHECKSUM}" \
+              --upload-file "$ARTIFACT_PATH" "$HELM_CHART_MUSEUM_URL/$(basename "$ARTIFACT_PATH")"
+}
+
 usage() {
-  echo "Usage: $0 -d DEPLOYMENT_TYPE"
+  echo "Usage: $0 [-v|--version V] [-d|--deployment-type $FWA|$FFWA|$ALL]"
   exit 2
 }
 
@@ -31,12 +49,29 @@ exitmsg() {
   exit 1
 }
 
-# Parse the args and declare defaults
-while getopts 'd:h' OPT; do
-  case "${OPT}" in
-    d) DEPLOYMENT_TYPE=${OPTARG} ;;
-    h|*) usage ;;
-  esac
+# Parse the args
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -v|--version)
+    VERSION="$2"
+    shift  # pass argument or value
+    ;;
+    -d|--deployment-type)
+    DEPLOYMENT_TYPE="$2"
+    shift
+    ;;
+    -h|--help)
+    usage
+    shift
+    ;;
+    *)
+    echo "Error: unknown cmdline option: $key"
+    usage
+    ;;
+esac
+shift  # past argument or value
 done
 
 # Check if the required args and env-vars present
@@ -47,78 +82,159 @@ if [ "$DEPLOYMENT_TYPE" != "$FWA" ] && [ "$DEPLOYMENT_TYPE" != "$FFWA" ] && [ "$
   ['$FWA', '$FFWA', '$ALL']"
 fi
 
-if [[ -z $GITHUB_REPO ]]; then
-  exitmsg "Environment variable GITHUB_REPO must be set"
+# Check for artifactory URL presence
+if [[ -z $HELM_CHART_ARTIFACTORY_URL ]]; then
+
+  if [[ -z $GITHUB_REPO ]]; then
+    exitmsg "Environment variable GITHUB_REPO must be set"
+  fi
+
+  if [[ -z $GITHUB_REPO_URL ]]; then
+    exitmsg "Environment variable GITHUB_REPO_URL must be set"
+  fi
+
+  if [[ -z $GITHUB_USERNAME ]]; then
+    exitmsg "Environment variable GITHUB_USERNAME must be set"
+  fi
+
+  if [[ -z $GITHUB_ACCESS_TOKEN ]]; then
+    exitmsg "Environment variable GITHUB_ACCESS_TOKEN must be set"
+  fi
+
+  if [[ -z $MAGMA_ROOT ]]; then
+    exitmsg "Environment variable MAGMA_ROOT must be set"
+  fi
+
+  # Set up repo for charts
+  mkdir -p ~/magma-charts && cd ~/magma-charts
+  git init
+
+  # Begin packaging necessary Helm charts
+  helm dependency update "$MAGMA_ROOT/orc8r/cloud/helm/orc8r/"
+  # shellcheck disable=SC2086
+  # We want $VERSION to be splitted as this is an option to the helm command
+  helm package "$MAGMA_ROOT/orc8r/cloud/helm/orc8r/" $VERSION && helm repo index .
+
+  if [ "$DEPLOYMENT_TYPE" == "$FWA" ]; then
+    helm dependency update "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/" $VERSION && helm repo index .
+  fi
+
+  if [ "$DEPLOYMENT_TYPE" == "$FFWA" ]; then
+    helm dependency update "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/" $VERSION && helm repo index .
+
+    helm dependency update "$MAGMA_ROOT/feg/cloud/helm/feg-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/feg/cloud/helm/feg-orc8r/" $VERSION && helm repo index .
+  fi
+
+  if  [ "$DEPLOYMENT_TYPE" == "$ALL" ]; then
+    helm dependency update "$MAGMA_ROOT/cwf/cloud/helm/cwf-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/cwf/cloud/helm/cwf-orc8r/" $VERSION && helm repo index .
+
+    helm dependency update "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/" $VERSION && helm repo index .
+
+    helm dependency update "$MAGMA_ROOT/feg/cloud/helm/feg-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/feg/cloud/helm/feg-orc8r/" $VERSION && helm repo index .
+
+    helm dependency update "$MAGMA_ROOT/fbinternal/cloud/helm/fbinternal-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/fbinternal/cloud/helm/fbinternal-orc8r/" $VERSION && helm repo index .
+
+    helm dependency update "$MAGMA_ROOT/wifi/cloud/helm/wifi-orc8r/"
+    # shellcheck disable=SC2086
+    helm package "$MAGMA_ROOT/wifi/cloud/helm/wifi-orc8r/" $VERSION && helm repo index .
+  fi
+
+  # Push charts
+  git add . && git commit -m "orc8r charts commit for version $ORC8R_VERSION"
+  git config remote.origin.url >&- || git remote add origin "$GITHUB_REPO_URL"
+  git push -u origin master
+
+  # Ensure push was successful
+  helm repo add "$GITHUB_REPO" --username "$GITHUB_USERNAME" --password "$GITHUB_ACCESS_TOKEN" \
+        "https://raw.githubusercontent.com/$GITHUB_USERNAME/$GITHUB_REPO/master/"
+  helm repo update
+
+  # The Helm command returns 0 even when no results are found. Search for err str
+  # instead
+  HELM_SEARCH_RESULTS="$(helm search repo "$GITHUB_REPO")" # should list the uploaded charts
+  if [ "$HELM_SEARCH_RESULTS" == "No results found" ]; then
+    exitmsg "Error! Unable to find uploaded orc8r charts"
+  fi
+else
+  if [[ -z $HELM_CHART_MUSEUM_REPO ]]; then
+    exitmsg "Environment variable $HELM_CHART_MUSEUM_REPO must be set"
+  fi
+
+  if [[ -z $HELM_CHART_MUSEUM_USERNAME ]]; then
+    exitmsg "Environment variable HELM_CHART_MUSEUM_USERNAME must be set"
+  fi
+
+  if [[ -z $HELM_CHART_MUSEUM_TOKEN ]]; then
+    exitmsg "Environment variable HELM_CHART_MUSEUM_TOKEN must be set"
+  fi
+
+  if [[ -z $MAGMA_ROOT ]]; then
+    exitmsg "Environment variable MAGMA_ROOT must be set"
+  fi
+
+  # Trim last backslash if exists
+  # shellcheck disable=SC2001
+  HELM_CHART_ARTIFACTORY_URL="$(echo "$HELM_CHART_ARTIFACTORY_URL" | sed 's:/$::')"
+
+  # Verify existence of the helm repo
+  RESPONSE_CODE_REPO="$(curl --output /dev/null --stderr /dev/null --silent --write-out "%{http_code}"  "$HELM_CHART_ARTIFACTORY_URL/$HELM_CHART_MUSEUM_REPO/" || :)"
+  if [ "$RESPONSE_CODE_REPO" != "200" ]; then
+    exitmsg "There was an error connecting to the artifactory repository $HELM_CHART_MUSEUM_ORIGIN_REPO, the http error code was $RESPONSE_CODE_REPO"
+  fi
+
+  HELM_CHART_MUSEUM_URL="$HELM_CHART_ARTIFACTORY_URL/$HELM_CHART_MUSEUM_REPO"
+  # Form API URL
+  HELM_CHART_MUSEUM_API_URL="$HELM_CHART_ARTIFACTORY_URL/api"
+
+  # Begin packaging necessary Helm charts
+  update_and_send_to_artifactory "$MAGMA_ROOT/orc8r/cloud/helm/orc8r/"
+
+  if [ "$DEPLOYMENT_TYPE" == "$FWA" ]; then
+    update_and_send_to_artifactory "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/"
+  fi
+
+  if [ "$DEPLOYMENT_TYPE" == "$FFWA" ]; then
+    update_and_send_to_artifactory "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/"
+    update_and_send_to_artifactory "$MAGMA_ROOT/feg/cloud/helm/feg-orc8r/"
+  fi
+
+  if  [ "$DEPLOYMENT_TYPE" == "$ALL" ]; then
+    update_and_send_to_artifactory "$MAGMA_ROOT/cwf/cloud/helm/cwf-orc8r/"
+    update_and_send_to_artifactory "$MAGMA_ROOT/lte/cloud/helm/lte-orc8r/"
+    update_and_send_to_artifactory "$MAGMA_ROOT/feg/cloud/helm/feg-orc8r/"
+    update_and_send_to_artifactory "$MAGMA_ROOT/fbinternal/cloud/helm/fbinternal-orc8r/"
+    update_and_send_to_artifactory "$MAGMA_ROOT/wifi/cloud/helm/wifi-orc8r/"
+  fi
+
+  # Refresh index.yaml
+  curl --request POST --user "$HELM_CHART_MUSEUM_USERNAME":"$HELM_CHART_MUSEUM_TOKEN" \
+              "$HELM_CHART_MUSEUM_API_URL/helm/$HELM_CHART_MUSEUM_REPO/reindex"
+
+  # Ensure push was successful
+  helm repo add "$(basename "$HELM_CHART_MUSEUM_URL")" "$HELM_CHART_MUSEUM_URL" --username "$HELM_CHART_MUSEUM_USERNAME" --password "$HELM_CHART_MUSEUM_TOKEN"
+  helm repo update
+
+  # The Helm command returns 0 even when no results are found. Search for err str
+  # instead
+  HELM_SEARCH_RESULTS="$(helm search repo "$(basename "$HELM_CHART_MUSEUM_URL")")" # should list the uploaded charts
+  if [ "$HELM_SEARCH_RESULTS" == "No results found" ]; then
+    exitmsg "Error! Unable to find uploaded orc8r charts"
+  fi
 fi
 
-if [[ -z $GITHUB_REPO_URL ]]; then
-  exitmsg "Environment variable GITHUB_REPO_URL must be set"
-fi
 
-if [[ -z $GITHUB_USERNAME ]]; then
-  exitmsg "Environment variable GITHUB_USERNAME must be set"
-fi
-
-if [[ -z $GITHUB_ACCESS_TOKEN ]]; then
-  exitmsg "Environment variable GITHUB_ACCESS_TOKEN must be set"
-fi
-
-if [[ -z $MAGMA_ROOT ]]; then
-  exitmsg "Environment variable MAGMA_ROOT must be set"
-fi
-
-# Set up repo for charts
-mkdir -p ~/magma-charts && cd ~/magma-charts
-git init
-
-# Begin packaging necessary helm charts
-helm dependency update $MAGMA_ROOT/orc8r/cloud/helm/orc8r/
-helm package $MAGMA_ROOT/orc8r/cloud/helm/orc8r/ && helm repo index .
-
-if [ "$DEPLOYMENT_TYPE" == "$FWA" ]; then
-  helm dependency update $MAGMA_ROOT/lte/cloud/helm/lte-orc8r/
-  helm package $MAGMA_ROOT/lte/cloud/helm/lte-orc8r/ && helm repo index .
-fi
-
-if [ "$DEPLOYMENT_TYPE" == "$FFWA" ]; then
-  helm dependency update $MAGMA_ROOT/lte/cloud/helm/lte-orc8r/
-  helm package $MAGMA_ROOT/lte/cloud/helm/lte-orc8r/ && helm repo index .
-
-  helm dependency update $MAGMA_ROOT/feg/cloud/helm/feg-orc8r/
-  helm package $MAGMA_ROOT/feg/cloud/helm/feg-orc8r/ && helm repo index .
-fi
-
-if  [ "$DEPLOYMENT_TYPE" == "$ALL" ]; then
-  helm dependency update $MAGMA_ROOT/cwf/cloud/helm/cwf-orc8r/
-  helm package $MAGMA_ROOT/cwf/cloud/helm/cwf-orc8r/ && helm repo index .
-
-  helm dependency update $MAGMA_ROOT/lte/cloud/helm/lte-orc8r/
-  helm package $MAGMA_ROOT/lte/cloud/helm/lte-orc8r/ && helm repo index .
-
-  helm dependency update $MAGMA_ROOT/feg/cloud/helm/feg-orc8r/
-  helm package $MAGMA_ROOT/feg/cloud/helm/feg-orc8r/ && helm repo index .
-
-  helm dependency update $MAGMA_ROOT/fbinternal/cloud/helm/fbinternal-orc8r/
-  helm package $MAGMA_ROOT/fbinternal/cloud/helm/fbinternal-orc8r/ && helm repo index .
-
-  helm dependency update $MAGMA_ROOT/wifi/cloud/helm/wifi-orc8r/
-  helm package $MAGMA_ROOT/wifi/cloud/helm/wifi-orc8r/ && helm repo index .
-fi
-
-# Push charts
-git add . && git commit -m "orc8r charts commit for version $ORC8R_VERSION"
-git config remote.origin.url >&- || git remote add origin $GITHUB_REPO_URL
-git push -u origin master
-
-# Ensure push was successful
-helm repo add $GITHUB_REPO --username $GITHUB_USERNAME --password $GITHUB_ACCESS_TOKEN \
-      "https://raw.githubusercontent.com/$GITHUB_USERNAME/$GITHUB_REPO/master/"
-helm repo update
-
-# The helm command returns 0 even when no results are found. Search for err str
-# instead
-HELM_SEARCH_RESULTS=$(helm search repo $GITHUB_REPO) # should list the uploaded charts
-if [ "$HELM_SEARCH_RESULTS" == "No results found" ]; then
-  exitmsg "Error! Unable to find uploaded orc8r charts"
-fi
 echo "Uploaded orc8r charts successfully."
