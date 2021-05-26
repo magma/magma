d8ef18dcc57251320e5bc138578b6d76411361cc
Timothée Dzik <timdzik@fb.com>
2021-05-26 15:26:58 -0400

    Bump agw from 1.5.0 to 1.5.1
    Signed-off-by: Timothée Dzik <timdzik@fb.com>

--- lte/gateway/release/build-magma.sh
+++ lte/gateway/release/build-magma.sh
@@ -20,8 +20,8 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 # Please update the version number accordingly for beta/stable builds
 # Test builds are versioned automatically by fabfile.py
-VERSION=1.5.0 # magma version number
-SCTPD_MIN_VERSION=1.5.0 # earliest version of sctpd with which this version is compatible
+VERSION=1.5.1 # magma version number
+SCTPD_MIN_VERSION=1.5.1 # earliest version of sctpd with which this version is compatible
 
 # RelWithDebInfo or Debug
 BUILD_TYPE=Debug
@@ -435,4 +435,3 @@ if grep -q stretch /etc/os-release; then
       "${SCRIPT_DIR}"/build-ovs.sh "${OUTPUT_DIR}"
   fi
 fi
-
