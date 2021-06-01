09e88ca8b1197944425baff33c5fa1974b373241
Yuanyuting Wang <yw3241@columbia.edu>
2021-06-01 16:28:28 -0700

    add golden file test for deterministic digest
    Signed-off-by: Yuanyuting Wang <yw3241@columbia.edu>

--- orc8r/cloud/go/mproto/mproto_test.go
+++ orc8r/cloud/go/mproto/mproto_test.go
@@ -14,6 +14,9 @@ limitations under the License.
 package mproto_test
 
 import (
+	"bytes"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"magma/orc8r/cloud/go/mproto"
@@ -23,11 +26,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestEncodeProtosDeterministic checks if EncodeProtosDeterministic truly enfores
-// deterministic encoding by comparing encoded protobuf messages containing the same data
-func TestEncodeProtosDeterministic(t *testing.T) {
-	// Encoding simple proto messages (no compound fields)
-	testDataObjs := []protos.TestData{
+var (
+	testDataObjs = []protos.TestData{
 		protos.TestData{Key: "12345", Value: 10},
 		protos.TestData{Key: "23456", Value: 15},
 		protos.TestData{Key: "34567", Value: 20},
@@ -35,20 +35,63 @@ func TestEncodeProtosDeterministic(t *testing.T) {
 		protos.TestData{Key: "56789", Value: 30},
 		protos.TestData{Key: "67890", Value: 35},
 	}
+	dataObjCount = len(testDataObjs)
 
-	dataObjCount := len(testDataObjs)
-	map1, map2 := map[string]proto.Message{}, map[string]proto.Message{}
+	testdataDir = "testdata"
+	goldenFilepath = filepath.Join(testdataDir, "determinisic-digest.golden")
+)
+
+// TestEncodeProtosDeterministic checks if EncodeProtosDeterministic truly enforces
+// deterministic encoding by comparing encoded protobuf messages containing the same data
+func TestEncodeProtosDeterministic(t *testing.T) {
+	// Encode simple proto messages (no compound fields)
+	protos1, protos2 := map[string]proto.Message{}, map[string]proto.Message{}
 	for i := 0; i < dataObjCount; i ++ {
 		ind1, ind2 := i, (i + 3) % dataObjCount
-		map1[testDataObjs[ind1].Key] = &testDataObjs[ind1]
-		map2[testDataObjs[ind2].Key] = &testDataObjs[ind2]
+		protos1[testDataObjs[ind1].Key] = &testDataObjs[ind1]
+		protos2[testDataObjs[ind2].Key] = &testDataObjs[ind2]
 	}
 
-	encoded1, _ := mproto.EncodeProtosDeterministic(map1)
-	encoded2, _ := mproto.EncodeProtosDeterministic(map2)
+	encoded1, _ := mproto.EncodeProtosDeterministic(protos1)
+	encoded2, _ := mproto.EncodeProtosDeterministic(protos2)
 	assert.Equal(t, encoded1, encoded2)
 
-	// Encoding compound proto messages (with repeated & map fields)
+	// Encode compound proto messages (with repeated & map fields)
+	testDataCollections := prepareTestDataCollections()
+	dataCollectionCount := len(testDataCollections)
+	protos3, protos4 := map[string]proto.Message{}, map[string]proto.Message{}
+	for i := 0; i < dataCollectionCount; i ++ {
+		ind1, ind2 := i, dataCollectionCount - i - 1
+		protos3[testDataCollections[ind1].ID] = &testDataCollections[ind1]
+		protos4[testDataCollections[ind2].ID] = &testDataCollections[ind2]
+	}
+
+	encoded3, _ := mproto.EncodeProtosDeterministic(protos3)
+	encoded4, _ := mproto.EncodeProtosDeterministic(protos4)
+	assert.Equal(t, encoded3, encoded4)
+}
+
+// TestEncodeProtosDeterministicGoldenFile checks if EncodeProtosDeterministic enforces
+// deterministic encoding consistently over time by conducting a golden file test
+func TestEncodeProtoDeterministicGoldenFile(t *testing.T) {
+	testDataCollections := prepareTestDataCollections()
+	dataCollectionCount := len(testDataCollections)
+	protos := map[string]proto.Message{}
+	for i := 0; i < dataCollectionCount; i ++ {
+		protos[testDataCollections[i].ID] = &testDataCollections[i]
+	}
+	encoded, _ := mproto.EncodeProtosDeterministic(protos)
+
+	// Compare resultant digest to encoded content stored in the golden file
+	absGoldenFilepath, err := filepath.Abs(goldenFilepath)
+	assert.NoError(t, err)
+	goldenFileContent, err2 := ioutil.ReadFile(absGoldenFilepath)
+	assert.NoError(t, err2)
+
+	assert.Equal(t, 0, bytes.Compare(goldenFileContent, encoded))
+}
+
+func prepareTestDataCollections() []protos.TestDataCollection {
 	submap1, submap2, submap3 := map[string]*protos.TestData{}, map[string]*protos.TestData{}, map[string]*protos.TestData{}
 	for i := 0; i < dataObjCount; i ++ {
 		submap1[testDataObjs[i].Key] = &testDataObjs[i]
@@ -61,10 +104,9 @@ func TestEncodeProtosDeterministic(t *testing.T) {
 		subslice3 = append(subslice3, &testDataObjs[i])
 		if i % 2 == 0 { subslice2 = append(subslice2, &testDataObjs[i]) }
 		if i % 3 == 0 { subslice1 = append(subslice1, &testDataObjs[i]) }
-
 	}
 
-	testDataCollections := []protos.TestDataCollection{
+	return []protos.TestDataCollection{
 		protos.TestDataCollection{
 			ID: "c1",
 			SingleData: &testDataObjs[0],
@@ -84,15 +126,4 @@ func TestEncodeProtosDeterministic(t *testing.T) {
 			DataSlice: subslice3,
 		},
 	}
-
-	map3, map4 := map[string]proto.Message{}, map[string]proto.Message{}
-	for i := 0; i < 3; i ++ {
-		ind1, ind2 := i, 3 - i - 1
-		map3[testDataCollections[ind1].ID] = &testDataCollections[ind1]
-		map4[testDataCollections[ind2].ID] = &testDataCollections[ind2]
-	}
-
-	encoded3, _ := mproto.EncodeProtosDeterministic(map3)
-	encoded4, _ := mproto.EncodeProtosDeterministic(map4)
-	assert.Equal(t, encoded3, encoded4)
 }
