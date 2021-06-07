696cc6b95e90f08514ef8e8d4918e90e46dfa325
Oriol Batalla <obatalla@fb.com>
2021-06-07 13:26:53 -0700

    [feg] s8_proxy add APN operator suffix
    Signed-off-by: Oriol Batalla <obatalla@fb.com>

--- feg/gateway/services/s8_proxy/servicers/config_test.go
+++ feg/gateway/services/s8_proxy/servicers/config_test.go
@@ -1,6 +1,7 @@
 package servicers_test
 
 import (
+	"os"
 	"testing"
 
 	"magma/feg/gateway/services/s8_proxy/servicers"
@@ -13,12 +14,24 @@ func TestGetS8ProxyConfig(t *testing.T) {
 	conf := generateS8Mconfig(t, config)
 	assert.Equal(t, ":2222", conf.ClientAddr)
 	assert.Equal(t, "10.0.0.1:9999", conf.ServerAddr.String())
+	assert.Equal(t, ".operator.test", conf.ApnOperatorSuffix)
 }
 
 func TestGetS8ProxyConfig_noPgw(t *testing.T) {
 	conf := generateS8Mconfig(t, config_noPGW)
 	assert.Equal(t, ":3333", conf.ClientAddr)
+	assert.Equal(t, "", conf.ApnOperatorSuffix)
 	assert.Nil(t, conf.ServerAddr)
+}
+
+func TestGetS8ProxyConfig_EnvVars(t *testing.T) {
+	os.Setenv(servicers.ClientAddrEnv, ":4444")
+	os.Setenv(servicers.ServerAddrEnv, "9.9.9.9:99")
+	os.Setenv(servicers.ApnOperatorSuffix, ".another.operator.com")
+	conf := generateS8Mconfig(t, config)
+	assert.Equal(t, ":4444", conf.ClientAddr)
+	assert.Equal(t, "9.9.9.9:99", conf.ServerAddr.String())
+	assert.Equal(t, ".another.operator.com", conf.ApnOperatorSuffix)
 }
 
 func generateS8Mconfig(t *testing.T, configString string) *servicers.S8ProxyConfig {
@@ -34,7 +47,8 @@ var (
 				"@type": "type.googleapis.com/magma.mconfig.S8Config",
 				"logLevel": "INFO",
 				"localAddress": ":2222",
-				"pgwAddress": "10.0.0.1:9999"
+				"pgwAddress": "10.0.0.1:9999",
+				"apnOperatorSuffix": ".operator.test"
 			}
 		}
 	}`
