9e1bdde413fc11103b0286acab071db887cdadfc
lionelgo <29477918+lionelgo@users.noreply.github.com>
2021-06-09 10:32:21 +0200

    [LTE][AGW] S6a/Diameter/Supported Features AVP change to Magma header for new files
    Signed-off-by: lionelgo <29477918+lionelgo@users.noreply.github.com>

--- lte/gateway/c/core/oai/tasks/s6a/s6a_supported_features.c
+++ lte/gateway/c/core/oai/tasks/s6a/s6a_supported_features.c
@@ -1,26 +1,19 @@
-/*
- * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under
- * the terms found in the LICENSE file in the root of this source tree.
+/**
+ * Copyright 2020 The Magma Authors.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *-------------------------------------------------------------------------------
- * For more information about the OpenAirInterface (OAI) Software Alliance:
- *      contact@openairinterface.org
  */
-
-/*! \file s6a_supported_features.c
-  \brief
-  \author Lionel Gauthier
-  \company Eurecom
-  \email: lionel.gauthier@eurecom.fr
-*/
+/****************************************************************************
+  Subsystem   MME
+  Description Handles the Supported Features Diameter AVP
+*****************************************************************************/
 
 #include <stdint.h>
 #include <netinet/in.h>
