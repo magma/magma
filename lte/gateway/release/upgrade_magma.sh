f3b2ac7a89fada63848d87113201e8d3c0f773a5
Timothée Dzik <timdzik@fb.com>
2021-06-11 22:42:33 +0200

    [AGW] Set up force in upgrade script
    Signed-off-by: Timothée Dzik <timdzik@fb.com>

--- lte/gateway/release/upgrade_magma.sh
+++ lte/gateway/release/upgrade_magma.sh
@@ -36,4 +36,4 @@ apt-key add /tmp/public
 echo "deb https://artifactory.magmacore.org/artifactory/debian $OS_VERSION-$MAGMA_VERSION main" >> /etc/apt/sources.list.d/magma.list
 
 apt update
-apt install -y magma
+apt install -y magma -o Dpkg::Options::="--force-overwrite"
