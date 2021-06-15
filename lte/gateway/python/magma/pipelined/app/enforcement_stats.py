c47b242da7d45e27233305c90fd8b3ebf0f4e3aa
veshkemburu <veshkemburu@fb.com>
2021-06-15 19:17:18 -0400

    Syntax adjustments and adding config flag across all pipelined.yml
    Signed-off-by: veshkemburu <veshkemburu@fb.com>

--- lte/gateway/python/magma/pipelined/app/enforcement_stats.py
+++ lte/gateway/python/magma/pipelined/app/enforcement_stats.py
@@ -91,7 +91,7 @@ class EnforcementStatsController(PolicyMixin, RestartMixin, MagmaController):
         self._last_poll_time = datetime.now()
         self._last_report_timestamp = datetime.now()
         self._bridge_name = kwargs['config']['bridge_name']
-        self._periodic_stats_reporting = kwargs['config']['periodic_stats_reporting']
+        self._periodic_stats_reporting = kwargs['config'].get('periodic_stats_reporting', True)
         if self._print_grpc_payload is None:
             self._print_grpc_payload = \
                 kwargs['config'].get('magma_print_grpc_payload', False)
@@ -300,7 +300,7 @@ class EnforcementStatsController(PolicyMixin, RestartMixin, MagmaController):
             # Still send an empty report -> for pipelined setup
             self._report_usage({})
             hub.sleep(self.INIT_SLEEP_TIME)
-        if (self._periodic_stats_reporting == False):
+        if not self._periodic_stats_reporting:
             return
         while True:
             hub.sleep(poll_interval)
