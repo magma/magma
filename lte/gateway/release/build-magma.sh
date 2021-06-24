cfc6a287ef267f20a3bfd091b57141032b408c51
Timothee Dzik <timdzik@fb.com>
2021-06-24 04:27:16 +0200

    Bump to 1.7.0 (#7739)
    Signed-off-by: Timothée Dzik <timdzik@fb.com>
    Signed-off-by: Ramon Melero <ramonmelero@fb.com>

--- lte/gateway/release/build-magma.sh
+++ lte/gateway/release/build-magma.sh
@@ -20,8 +20,8 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 # Please update the version number accordingly for beta/stable builds
 # Test builds are versioned automatically by fabfile.py
-VERSION=1.6.0 # magma version number
-SCTPD_MIN_VERSION=1.6.0 # earliest version of sctpd with which this version is compatible
+VERSION=1.7.0 # magma version number
+SCTPD_MIN_VERSION=1.7.0 # earliest version of sctpd with which this version is compatible
 
 # RelWithDebInfo or Debug
 BUILD_TYPE=Debug
@@ -439,4 +439,3 @@ if grep -q stretch /etc/os-release; then
       "${SCRIPT_DIR}"/build-ovs.sh "${OUTPUT_DIR}"
   fi
 fi
-
