53061c831741a6ae1a7fff9df827fef05a43f701
Pravin Shelar <pbshelar@fb.com>
2021-06-24 20:04:04 -0700

    fix(mobilityd): handle def gw changes. (#7763)
    Pediorically read def gw to handle changes to default gw IP address.
    
    Signed-off-by: Pravin B Shelar <pbshelar@fb.com>

--- lte/gateway/python/magma/mobilityd/uplink_gw.py
+++ lte/gateway/python/magma/mobilityd/uplink_gw.py
@@ -12,6 +12,7 @@ limitations under the License.
 """
 import ipaddress
 import logging
+import threading
 from typing import List, MutableMapping, Optional
 
 import netifaces
@@ -48,6 +49,8 @@ class UplinkGatewayInfo:
             gw_info_map: map to store GW info.
         """
         self._backing_map = gw_info_map
+        self._read_default_gw_timer = None
+        self._read_default_gw_interval_seconds = 120
 
     def get_gw_ip(self, vlan_id: Optional[str] = "") -> Optional[str]:
         """
@@ -62,6 +65,17 @@ class UplinkGatewayInfo:
             return str(ip)
 
     def read_default_gw(self):
+        self._do_read_default_gw()
+
+        if self._read_default_gw_timer is not None:
+            return
+        self._read_default_gw_timer = threading.Timer(
+            self._read_default_gw_interval_seconds,
+            self._do_read_default_gw,
+        )
+        self._read_default_gw_timer.start()
+
+    def _do_read_default_gw(self):
         gws = netifaces.gateways()
         logging.info("Using GW info: %s", gws)
         if gws is not None:
