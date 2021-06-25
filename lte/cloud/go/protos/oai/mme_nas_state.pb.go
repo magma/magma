magma/magma

commit 118e113b35dad0fe16864716e9178de7caeef414

Currently service303 reaches across service boundaries to read (#7710)
mme_app state. Fix this by pushing the stats over an ITTI message
Also remove the check for MME service as the timer is not started
in the SPGW context (as it is only run in the MME_APP)

Also may close #7421 where the nas datastructure is null as there is probably
races with clearing out the state_p.

Also, addresses race in initialization where the timer was started outside
mme_app_thread thus the zmq context might not be initialized

This reverts commit ab6d9cbe3dcd403d569df6b9142fe5c181e240ca.

Signed-off-by: Amar Padmanabhan <amar@freedomfi.com>

Co-authored-by: Amar Padmanabhan <amar@freedomfi.com>

lte/gateway/c/core/oai/include/service303.h
@@ -17,6 +17,8 @@
 
 #pragma once
 
+#include "service303_messages_types.h"
+
 #include "bstrlib.h"
 #define SERVICE303_MME_PACKAGE_NAME "mme"
 #define SERVICE303_MME_PACKAGE_VERSION "1.0"
@@ -27,7 +29,7 @@
 #define NO_LABELS 0
 #define EPC_STATS_TIMER_MSEC 60000  // In milliseconds
 
-void service303_statistics_read(void);
+void service303_mme_statistics_read(application_mme_stats_msg_t* stats_msg_p);
 
 // service303 conf type added to be able to use same task interface for MME and
 // SPGW while passing configs from mme_config and spgw_config types

lte/gateway/c/core/oai/include/service303_messages_def.h
@@ -22,3 +22,6 @@ MESSAGE_DEF(
 MESSAGE_DEF(
     APPLICATION_UNHEALTHY_MSG, application_unhealthy_msg_t,
     application_unhealthy_msg)
+MESSAGE_DEF(
+    APPLICATION_STATS_MSG, application_mme_stats_msg_t,
+    application_mme_stats_msg)

lte/gateway/c/core/oai/include/service303_messages_types.h
@@ -33,4 +33,11 @@ typedef struct application_unhealthy_msg {
   uint8_t unused;
 } application_unhealthy_msg_t;
 
+// Message capturing stats as communicated by the mme_app
+typedef struct application_mme_stats_msg {
+  uint32_t nb_enb_connected;
+  uint32_t nb_ue_attached;
+  uint32_t nb_ue_connected;
+} application_mme_stats_msg_t;
+
 #endif /* FILE_SERVICE303_MESSAGES_TYPES_SEEN */

lte/gateway/c/core/oai/lib/message_utils/service303_message_utils.c
@@ -35,3 +35,21 @@ int send_app_health_to_service303(
   }
   return send_msg_to_task(task_zmq_ctx_p, TASK_SERVICE303, message_p);
 }
+
+int send_stats_to_service303(
+    task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id,
+    application_mme_stats_msg_t* stats_msg) {
+  MessageDef* message_p =
+      itti_alloc_new_message(origin_id, APPLICATION_STATS_MSG);
+  if (message_p == NULL) {
+    OAILOG_ERROR(LOG_MME_APP, "Unable to allocate memory");
+    OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNerror);
+  }
+  message_p->ittiMsg.application_mme_stats_msg.nb_enb_connected =
+      stats_msg->nb_enb_connected;
+  message_p->ittiMsg.application_mme_stats_msg.nb_ue_attached =
+      stats_msg->nb_ue_attached;
+  message_p->ittiMsg.application_mme_stats_msg.nb_ue_connected =
+      stats_msg->nb_ue_connected;
+  return send_msg_to_task(task_zmq_ctx_p, TASK_SERVICE303, message_p);
+}

lte/gateway/c/core/oai/lib/message_utils/service303_message_utils.h
@@ -26,4 +26,8 @@
 int send_app_health_to_service303(
     task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id, bool healthy);
 
+int send_stats_to_service303(
+    task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id,
+    application_mme_stats_msg_t* stats_msg);
+
 #endif /* FILE_SERVICE303_MESSAGE_UTILS */

lte/gateway/c/core/oai/tasks/mme_app/mme_app_main.c
@@ -55,6 +55,7 @@
 static void check_mme_healthy_and_notify_service(void);
 static bool is_mme_app_healthy(void);
 static void mme_app_exit(void);
+static void start_stats_timer(void);
 
 bool mme_hss_associated = false;
 bool mme_sctp_bounded   = false;
@@ -62,6 +63,7 @@ task_zmq_ctx_t mme_app_task_zmq_ctx;
 bool mme_congestion_control_enabled = true;
 long mme_app_last_msg_latency;
 long pre_mme_task_msg_latency;
+static long epc_stats_timer_id;
 
 mme_congestion_params_t mme_congestion_params;
 
@@ -520,6 +522,7 @@ static void* mme_app_thread(__attribute__((unused)) void* args) {
 
   // Service started, but not healthy yet
   send_app_health_to_service303(&mme_app_task_zmq_ctx, TASK_MME_APP, false);
+  start_stats_timer();
 
   zloop_start(mme_app_task_zmq_ctx.event_loop);
   mme_app_exit();
@@ -566,6 +569,22 @@ status_code_e mme_app_init(const mme_config_t* mme_config_p) {
   OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNok);
 }
 
+static int handle_stats_timer(zloop_t* loop, int id, void* arg) {
+  mme_app_desc_t* mme_app_desc_p = get_mme_nas_state(false);
+  application_mme_stats_msg_t stats_msg;
+  stats_msg.nb_ue_attached   = mme_app_desc_p->nb_ue_attached;
+  stats_msg.nb_ue_connected  = mme_app_desc_p->nb_ue_connected;
+  stats_msg.nb_enb_connected = mme_app_desc_p->nb_enb_connected;
+  return send_stats_to_service303(
+      &mme_app_task_zmq_ctx, TASK_MME_APP, &stats_msg);
+}
+
+static void start_stats_timer(void) {
+  epc_stats_timer_id = start_timer(
+      &mme_app_task_zmq_ctx, EPC_STATS_TIMER_MSEC, TIMER_REPEAT_FOREVER,
+      handle_stats_timer, NULL);
+}
+
 static void check_mme_healthy_and_notify_service(void) {
   if (is_mme_app_healthy()) {
     send_app_health_to_service303(&mme_app_task_zmq_ctx, TASK_MME_APP, true);
@@ -578,6 +597,7 @@ static bool is_mme_app_healthy(void) {
 
 //------------------------------------------------------------------------------
 static void mme_app_exit(void) {
+  stop_timer(&mme_app_task_zmq_ctx, epc_stats_timer_id);
   mme_app_edns_exit();
   clear_mme_nas_state();
   // Clean-up NAS module

lte/gateway/c/core/oai/tasks/service303/service303_mme_stats.c
@@ -21,16 +21,10 @@
 #include "mme_app_state.h"
 #include "service303.h"
 
-static void service303_mme_statistics_read(void) {
-  size_t label                   = 0;
-  mme_app_desc_t* mme_app_desc_p = get_mme_nas_state(false);
-  set_gauge("enb_connected", mme_app_desc_p->nb_enb_connected, label);
-  set_gauge("ue_registered", mme_app_desc_p->nb_ue_attached, label);
-  set_gauge("ue_connected", mme_app_desc_p->nb_ue_connected, label);
-  return;
-}
-
-void service303_statistics_read(void) {
-  service303_mme_statistics_read();
+void service303_mme_statistics_read(application_mme_stats_msg_t* stats_msg_p) {
+  size_t label = 0;
+  set_gauge("enb_connected", stats_msg_p->nb_enb_connected, label);
+  set_gauge("ue_registered", stats_msg_p->nb_ue_attached, label);
+  set_gauge("ue_connected", stats_msg_p->nb_ue_connected, label);
   return;
 }

commit 02a56b7f8b95568eaf87b503750a2cb156f50b93

Move stats collection of eNB stats into s1ap thread
The current access pattern violates the ITTI framework so
1 - Create a stats timer for S1ap to send stats to service303
2 - Remove fields from mme_app structure relating to eNB (s1).
3 - Move the increment of enb stats to post s1setup request vs when the
association gets enabled. This makes more sense and maps to where the
increment used to happen for the cross thread stats
Also was missing a break for handling APPLICATION_MME_APP_STATS_MSG which
triggered the premature termination of the service303 thread, regression
introduced in by me in 118e113b35. Fixed that as well.

Signed-off-by: Amar Padmanabhan <amar@freedomfi.com>

lte/cloud/go/protos/oai/mme_nas_state.pb.go
@@ -1030,7 +1030,7 @@ func (m *MmeUeContext) GetGutiUeIdHtbl() map[string]uint64 {
 // mme_app_desc_t
 type MmeNasState struct {
 	MmeUeContexts           *MmeUeContext `protobuf:"bytes,1,opt,name=mme_ue_contexts,json=mmeUeContexts,proto3" json:"mme_ue_contexts,omitempty"`
-	NbEnbConnected          uint32        `protobuf:"varint,2,opt,name=nb_enb_connected,json=nbEnbConnected,proto3" json:"nb_enb_connected,omitempty"`
+	NbEnbConnected          uint32        `protobuf:"varint,2,opt,name=nb_enb_connected,json=nbEnbConnected,proto3" json:"nb_enb_connected,omitempty"` // Deprecated: Do not use.
 	NbUeAttached            uint32        `protobuf:"varint,3,opt,name=nb_ue_attached,json=nbUeAttached,proto3" json:"nb_ue_attached,omitempty"`
 	NbUeConnected           uint32        `protobuf:"varint,4,opt,name=nb_ue_connected,json=nbUeConnected,proto3" json:"nb_ue_connected,omitempty"`
 	NbDefaultEpsBearers     uint32        `protobuf:"varint,5,opt,name=nb_default_eps_bearers,json=nbDefaultEpsBearers,proto3" json:"nb_default_eps_bearers,omitempty"`
@@ -1079,6 +1079,7 @@ func (m *MmeNasState) GetMmeUeContexts() *MmeUeContext {
 	return nil
 }
 
+// Deprecated: Do not use.
 func (m *MmeNasState) GetNbEnbConnected() uint32 {
 	if m != nil {
 		return m.NbEnbConnected
@@ -1182,12 +1183,12 @@ func init() {
 func init() { proto.RegisterFile("lte/protos/oai/mme_nas_state.proto", fileDescriptor_87e46b68105575f0) }
 
 var fileDescriptor_87e46b68105575f0 = []byte{
-	// 2543 bytes of a gzipped FileDescriptorProto
+	// 2548 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x58, 0xdd, 0x73, 0xdb, 0x36,
 	0x12, 0x3f, 0x37, 0x8e, 0x2d, 0xad, 0x2c, 0x7f, 0xc0, 0x76, 0xc2, 0xb8, 0x69, 0xec, 0x2a, 0x4d,
 	0x9a, 0x5e, 0x52, 0x3b, 0x6a, 0x9a, 0x4e, 0xda, 0x69, 0x9a, 0x2a, 0x8a, 0x9b, 0x6a, 0xda, 0xb4,
 	0x0e, 0x65, 0xb7, 0x37, 0x77, 0x0f, 0x18, 0x90, 0x84, 0x68, 0x4c, 0x48, 0x10, 0x21, 0x40, 0x3b,
-	0xbe, 0xbf, 0xe9, 0x9e, 0xee, 0x9f, 0xb8, 0xef, 0xef, 0x7f, 0xe4, 0x9e, 0x6f, 0xee, 0xe1, 0x06,
+	0xbe, 0xbf, 0xe9, 0x9e, 0xee, 0x9f, 0xb8, 0xef, 0xef, 0x7f, 0xe5, 0x9e, 0x6e, 0xee, 0xe1, 0x06,
 	0x1f, 0xa4, 0x68, 0xd9, 0x51, 0xae, 0x0f, 0x9a, 0xa1, 0x76, 0xf7, 0xf7, 0x03, 0x16, 0xd8, 0x5d,
 	0x2c, 0x00, 0x9d, 0x44, 0xd1, 0x1d, 0x91, 0x67, 0x2a, 0x93, 0x3b, 0x19, 0x61, 0x3b, 0x69, 0x4a,
 	0x31, 0x27, 0x12, 0x4b, 0x45, 0x14, 0xdd, 0x36, 0x0a, 0xd4, 0x4e, 0x49, 0x9c, 0x92, 0xed, 0x44,
@@ -1325,21 +1326,22 @@ var fileDescriptor_87e46b68105575f0 = []byte{
 	0xf8, 0x12, 0xd0, 0xd9, 0xc5, 0xf9, 0x49, 0x0c, 0x8f, 0x61, 0xed, 0xbc, 0x15, 0xf8, 0x49, 0x1c,
 	0x8f, 0x60, 0xe5, 0x8c, 0xa7, 0x75, 0x82, 0xe6, 0x1b, 0x08, 0x3a, 0xff, 0x9e, 0x85, 0xd6, 0xb3,
 	0x94, 0x7e, 0x47, 0xa4, 0xbd, 0xc8, 0xf4, 0x61, 0xc9, 0xdd, 0x0a, 0xaa, 0x16, 0xc7, 0xc6, 0xf6,
-	0xdb, 0x53, 0x56, 0xdb, 0x6f, 0xa7, 0xb5, 0x7f, 0x12, 0xdd, 0x82, 0x65, 0x1e, 0x60, 0x1d, 0x16,
-	0x61, 0xc6, 0x39, 0x0d, 0x75, 0x8a, 0xd8, 0x9b, 0xd6, 0x22, 0x0f, 0x76, 0x79, 0xd0, 0x2f, 0xa5,
-	0xe8, 0x3d, 0x58, 0xb4, 0xc1, 0x63, 0xaf, 0x14, 0xee, 0x59, 0xa3, 0xed, 0x2f, 0xe8, 0x85, 0xe9,
-	0x39, 0x99, 0x3e, 0xb7, 0xad, 0xd5, 0x98, 0xce, 0x5e, 0xc0, 0xda, 0xda, 0x6c, 0xcc, 0x66, 0x5b,
-	0xc2, 0xea, 0x9e, 0x2d, 0xe4, 0xc4, 0x2b, 0xce, 0x2a, 0x0f, 0x9e, 0xb8, 0x1b, 0xb7, 0x90, 0xe5,
-	0x83, 0x8e, 0x9d, 0x82, 0xec, 0x16, 0x95, 0xf1, 0x5c, 0x39, 0x85, 0x61, 0xb7, 0x28, 0xad, 0xee,
-	0x00, 0xd2, 0x3d, 0x3f, 0x93, 0x8a, 0x85, 0x2e, 0xf9, 0xdd, 0xed, 0x6b, 0xd6, 0x5f, 0xae, 0x34,
-	0x26, 0xb3, 0x07, 0x11, 0xea, 0x40, 0xdb, 0x4e, 0x38, 0x25, 0x9c, 0xc4, 0x34, 0x72, 0xaf, 0x1e,
-	0x2d, 0x3d, 0xdd, 0x67, 0x56, 0x84, 0xae, 0x02, 0x94, 0x79, 0xe3, 0xde, 0x38, 0xda, 0x7e, 0xc3,
-	0xc6, 0x43, 0x42, 0xf5, 0x78, 0x3c, 0x28, 0x67, 0x54, 0xd1, 0xd8, 0xfb, 0xd7, 0x32, 0x0f, 0xdc,
-	0xb4, 0x4a, 0xae, 0xbb, 0xb0, 0xee, 0xae, 0x85, 0x8c, 0x87, 0x14, 0x27, 0x44, 0xda, 0x27, 0x2a,
-	0x77, 0x17, 0x5b, 0x31, 0xb7, 0x43, 0xad, 0xfa, 0x96, 0x48, 0xf3, 0x58, 0x85, 0x3e, 0x83, 0x8d,
-	0x1a, 0xff, 0x24, 0xcc, 0x3e, 0x4f, 0x5c, 0xaa, 0xc6, 0x39, 0x8d, 0x7d, 0x08, 0x57, 0x75, 0x8c,
-	0x10, 0x21, 0x6a, 0xb7, 0x47, 0x1c, 0x53, 0x4e, 0x73, 0xd3, 0xfc, 0xac, 0xd9, 0x97, 0xce, 0x34,
-	0xa5, 0x3d, 0x21, 0xca, 0xab, 0xe4, 0xd3, 0x52, 0xfd, 0x78, 0xf3, 0x97, 0xef, 0x98, 0x50, 0xda,
-	0x49, 0x14, 0xdd, 0x09, 0x93, 0xac, 0x88, 0x76, 0xe2, 0xac, 0xf6, 0x66, 0x1e, 0xcc, 0x99, 0xef,
-	0x7b, 0xff, 0x0b, 0x00, 0x00, 0xff, 0xff, 0x11, 0x17, 0x6c, 0x8f, 0xe7, 0x17, 0x00, 0x00,
+	0xdb, 0x53, 0x56, 0xdb, 0x6f, 0xa7, 0xb5, 0x7f, 0xfa, 0x8a, 0xba, 0xcc, 0x03, 0xac, 0xc3, 0x22,
+	0xcc, 0x38, 0xa7, 0xa1, 0x4e, 0x11, 0x73, 0xd3, 0x7a, 0xfc, 0x96, 0x37, 0xe3, 0x2f, 0xf2, 0x60,
+	0x97, 0x07, 0xfd, 0x52, 0x83, 0xde, 0x83, 0x45, 0x1b, 0x40, 0xf6, 0x5a, 0xe1, 0x9e, 0x36, 0xda,
+	0xfe, 0x82, 0x5e, 0x9c, 0x9e, 0x93, 0xe9, 0xb3, 0xdb, 0x5a, 0x8d, 0x29, 0xed, 0x25, 0xac, 0xad,
+	0xcd, 0xc6, 0x6c, 0xb6, 0x2d, 0xac, 0xee, 0xda, 0x42, 0x4e, 0xbc, 0xe4, 0xac, 0xf2, 0xe0, 0x89,
+	0xbb, 0x75, 0x0b, 0x59, 0x3e, 0xea, 0xd8, 0x29, 0xc8, 0x6e, 0x51, 0x19, 0xcf, 0x95, 0x53, 0x18,
+	0x76, 0x8b, 0xd2, 0xea, 0x0e, 0x20, 0xdd, 0xf7, 0x33, 0xa9, 0x58, 0xe8, 0x0a, 0x80, 0xbb, 0x81,
+	0xcd, 0xfa, 0xcb, 0x95, 0xc6, 0x64, 0xf7, 0x20, 0x42, 0x1d, 0x68, 0xdb, 0x09, 0xa7, 0x84, 0x93,
+	0x98, 0x46, 0xee, 0xe5, 0xa3, 0xa5, 0xa7, 0xfb, 0xcc, 0x8a, 0xd0, 0x55, 0x80, 0x32, 0x77, 0xdc,
+	0x3b, 0x47, 0xdb, 0x6f, 0xd8, 0x98, 0x48, 0xa8, 0x1e, 0x8f, 0x07, 0xe5, 0x8c, 0x2a, 0x1a, 0x7b,
+	0x07, 0x5b, 0xe6, 0x81, 0x9b, 0x56, 0xc9, 0x75, 0x17, 0xd6, 0xdd, 0xd5, 0x90, 0xf1, 0x90, 0xe2,
+	0x84, 0x48, 0xfb, 0x4c, 0xe5, 0xee, 0x63, 0x2b, 0xe6, 0x86, 0xa8, 0x55, 0xdf, 0x12, 0x69, 0x1e,
+	0xac, 0xd0, 0x67, 0xb0, 0x51, 0xe3, 0x9f, 0x84, 0xd9, 0x27, 0x8a, 0x4b, 0xd5, 0x38, 0xa7, 0xb1,
+	0x0f, 0xe1, 0xaa, 0x8e, 0x13, 0x22, 0x44, 0xed, 0x06, 0x89, 0x63, 0xca, 0x69, 0x6e, 0x1a, 0xa0,
+	0x35, 0xfb, 0xda, 0x99, 0xa6, 0xb4, 0x27, 0x44, 0x79, 0x9d, 0x7c, 0x5a, 0xaa, 0x1f, 0x6f, 0xfe,
+	0xf2, 0x1d, 0x13, 0x4e, 0x3b, 0x89, 0xa2, 0x3b, 0x61, 0x92, 0x15, 0xd1, 0x4e, 0x9c, 0xd5, 0xde,
+	0xcd, 0x83, 0x39, 0xf3, 0x7d, 0xef, 0x7f, 0x01, 0x00, 0x00, 0xff, 0xff, 0x38, 0xee, 0xa4, 0xf5,
+	0xeb, 0x17, 0x00, 0x00,
 }

lte/gateway/c/core/oai/include/mme_app_desc.h
@@ -38,7 +38,6 @@ typedef struct mme_app_desc_s {
    * number of S1_U bearers,number of PDN sessions
    */
 
-  uint32_t nb_enb_connected;
   uint32_t nb_ue_attached;
   uint32_t nb_ue_connected;
   uint32_t nb_default_eps_bearers;
@@ -52,8 +51,6 @@ typedef struct mme_app_desc_s {
   uint32_t nb_ue_disconnected_since_last_stat;
   uint32_t nb_eps_bearers_established_since_last_stat;
   uint32_t nb_eps_bearers_released_since_last_stat;
-  uint32_t nb_enb_connected_since_last_stat;
-  uint32_t nb_enb_released_since_last_stat;
   uint32_t nb_s1u_bearers_released_since_last_stat;
   uint32_t nb_s1u_bearers_established_since_last_stat;
   uint32_t nb_ue_managed;

lte/gateway/c/core/oai/include/mme_app_statistics.h
@@ -22,8 +22,6 @@ int mme_app_statistics_display(void);
 
 /*********************************** Utility Functions to update
  * Statistics**************************************/
-void update_mme_app_stats_connected_enb_add(void);
-void update_mme_app_stats_connected_enb_sub(void);
 void update_mme_app_stats_connected_ue_add(void);
 void update_mme_app_stats_connected_ue_sub(void);
 void update_mme_app_stats_s1u_bearer_add(void);

lte/gateway/c/core/oai/include/service303.h
@@ -29,7 +29,9 @@
 #define NO_LABELS 0
 #define EPC_STATS_TIMER_MSEC 60000  // In milliseconds
 
-void service303_mme_statistics_read(application_mme_stats_msg_t* stats_msg_p);
+void service303_mme_app_statistics_read(
+    application_mme_app_stats_msg_t* stats_msg_p);
+void service303_s1ap_statistics_read(application_s1ap_stats_msg_t* stats_msg_p);
 
 // service303 conf type added to be able to use same task interface for MME and
 // SPGW while passing configs from mme_config and spgw_config types

lte/gateway/c/core/oai/include/service303_messages_def.h
@@ -23,5 +23,8 @@ MESSAGE_DEF(
     APPLICATION_UNHEALTHY_MSG, application_unhealthy_msg_t,
     application_unhealthy_msg)
 MESSAGE_DEF(
-    APPLICATION_STATS_MSG, application_mme_stats_msg_t,
-    application_mme_stats_msg)
+    APPLICATION_MME_APP_STATS_MSG, application_mme_app_stats_msg_t,
+    application_mme_app_stats_msg)
+MESSAGE_DEF(
+    APPLICATION_S1AP_STATS_MSG, application_s1ap_stats_msg_t,
+    application_s1ap_stats_msg)

lte/gateway/c/core/oai/include/service303_messages_types.h
@@ -34,10 +34,13 @@ typedef struct application_unhealthy_msg {
 } application_unhealthy_msg_t;
 
 // Message capturing stats as communicated by the mme_app
-typedef struct application_mme_stats_msg {
-  uint32_t nb_enb_connected;
+typedef struct application_mme_app_stats_msg {
   uint32_t nb_ue_attached;
   uint32_t nb_ue_connected;
-} application_mme_stats_msg_t;
+} application_mme_app_stats_msg_t;
+
+typedef struct application_s1ap_stats_msg {
+  uint32_t nb_enb_connected;
+} application_s1ap_stats_msg_t;
 
 #endif /* FILE_SERVICE303_MESSAGES_TYPES_SEEN */

lte/gateway/c/core/oai/lib/message_utils/service303_message_utils.c
@@ -36,20 +36,32 @@ int send_app_health_to_service303(
   return send_msg_to_task(task_zmq_ctx_p, TASK_SERVICE303, message_p);
 }
 
-int send_stats_to_service303(
+int send_mme_app_stats_to_service303(
     task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id,
-    application_mme_stats_msg_t* stats_msg) {
+    application_mme_app_stats_msg_t* stats_msg) {
   MessageDef* message_p =
-      itti_alloc_new_message(origin_id, APPLICATION_STATS_MSG);
+      itti_alloc_new_message(origin_id, APPLICATION_MME_APP_STATS_MSG);
   if (message_p == NULL) {
     OAILOG_ERROR(LOG_MME_APP, "Unable to allocate memory");
     OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNerror);
   }
-  message_p->ittiMsg.application_mme_stats_msg.nb_enb_connected =
-      stats_msg->nb_enb_connected;
-  message_p->ittiMsg.application_mme_stats_msg.nb_ue_attached =
+  message_p->ittiMsg.application_mme_app_stats_msg.nb_ue_attached =
       stats_msg->nb_ue_attached;
-  message_p->ittiMsg.application_mme_stats_msg.nb_ue_connected =
+  message_p->ittiMsg.application_mme_app_stats_msg.nb_ue_connected =
       stats_msg->nb_ue_connected;
+  return send_msg_to_task(task_zmq_ctx_p, TASK_SERVICE303, message_p);
+}
+
+int send_s1ap_stats_to_service303(
+    task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id,
+    application_s1ap_stats_msg_t* stats_msg) {
+  MessageDef* message_p =
+      itti_alloc_new_message(origin_id, APPLICATION_S1AP_STATS_MSG);
+  if (message_p == NULL) {
+    OAILOG_ERROR(LOG_MME_APP, "Unable to allocate memory");
+    OAILOG_FUNC_RETURN(LOG_MME_APP, RETURNerror);
+  }
+  message_p->ittiMsg.application_s1ap_stats_msg.nb_enb_connected =
+      stats_msg->nb_enb_connected;
   return send_msg_to_task(task_zmq_ctx_p, TASK_SERVICE303, message_p);
 }

lte/gateway/c/core/oai/lib/message_utils/service303_message_utils.h
@@ -26,8 +26,12 @@
 int send_app_health_to_service303(
     task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id, bool healthy);
 
-int send_stats_to_service303(
+int send_mme_app_stats_to_service303(
     task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id,
-    application_mme_stats_msg_t* stats_msg);
+    application_mme_app_stats_msg_t* stats_msg);
+
+int send_s1ap_stats_to_service303(
+    task_zmq_ctx_t* task_zmq_ctx_p, task_id_t origin_id,
+    application_s1ap_stats_msg_t* stats_msg);
 
 #endif /* FILE_SERVICE303_MESSAGE_UTILS */

lte/gateway/c/core/oai/tasks/mme_app/mme_app_main.c
@@ -571,11 +571,10 @@ status_code_e mme_app_init(const mme_config_t* mme_config_p) {
 
 static int handle_stats_timer(zloop_t* loop, int id, void* arg) {
   mme_app_desc_t* mme_app_desc_p = get_mme_nas_state(false);
-  application_mme_stats_msg_t stats_msg;
-  stats_msg.nb_ue_attached   = mme_app_desc_p->nb_ue_attached;
-  stats_msg.nb_ue_connected  = mme_app_desc_p->nb_ue_connected;
-  stats_msg.nb_enb_connected = mme_app_desc_p->nb_enb_connected;
-  return send_stats_to_service303(
+  application_mme_app_stats_msg_t stats_msg;
+  stats_msg.nb_ue_attached  = mme_app_desc_p->nb_ue_attached;
+  stats_msg.nb_ue_connected = mme_app_desc_p->nb_ue_connected;
+  return send_mme_app_stats_to_service303(
       &mme_app_task_zmq_ctx, TASK_MME_APP, &stats_msg);
 }
 

lte/gateway/c/core/oai/tasks/mme_app/mme_app_state_converter.cpp
@@ -754,7 +754,6 @@ void MmeNasStateConverter::proto_to_ue_mm_context(
 void MmeNasStateConverter::state_to_proto(
     const mme_app_desc_t* mme_nas_state_p, oai::MmeNasState* state_proto) {
   OAILOG_FUNC_IN(LOG_MME_APP);
-  state_proto->set_nb_enb_connected(mme_nas_state_p->nb_enb_connected);
   state_proto->set_nb_ue_attached(mme_nas_state_p->nb_ue_attached);
   state_proto->set_nb_ue_connected(mme_nas_state_p->nb_ue_connected);
   state_proto->set_nb_default_eps_bearers(
@@ -794,9 +793,8 @@ void MmeNasStateConverter::state_to_proto(
 void MmeNasStateConverter::proto_to_state(
     const oai::MmeNasState& state_proto, mme_app_desc_t* mme_nas_state_p) {
   OAILOG_FUNC_IN(LOG_MME_APP);
-  mme_nas_state_p->nb_enb_connected = state_proto.nb_enb_connected();
-  mme_nas_state_p->nb_ue_attached   = state_proto.nb_ue_attached();
-  mme_nas_state_p->nb_ue_connected  = state_proto.nb_ue_connected();
+  mme_nas_state_p->nb_ue_attached  = state_proto.nb_ue_attached();
+  mme_nas_state_p->nb_ue_connected = state_proto.nb_ue_connected();
   mme_nas_state_p->nb_default_eps_bearers =
       state_proto.nb_default_eps_bearers();
   mme_nas_state_p->nb_s1u_bearers        = state_proto.nb_s1u_bearers();

lte/gateway/c/core/oai/tasks/mme_app/mme_app_statistics.c
@@ -32,14 +32,6 @@ int mme_app_statistics_display(void) {
       "since last display |\n");
   OAILOG_DEBUG(
       LOG_MME_APP,
-      "Connected eNBs | %10u      |     %10u              |    %10u            "
-      "  "
-      " |\n",
-      mme_app_desc_p->nb_enb_connected,
-      mme_app_desc_p->nb_enb_connected_since_last_stat,
-      mme_app_desc_p->nb_enb_released_since_last_stat);
-  OAILOG_DEBUG(
-      LOG_MME_APP,
       "Attached UEs   | %10u      |     %10u              |    %10u            "
       "  "
       " |\n",
@@ -76,8 +68,6 @@ int mme_app_statistics_display(void) {
       "============================================\n\n");
 
   // resetting stats for next display
-  mme_app_desc_p->nb_enb_connected_since_last_stat           = 0;
-  mme_app_desc_p->nb_enb_released_since_last_stat            = 0;
   mme_app_desc_p->nb_ue_connected_since_last_stat            = 0;
   mme_app_desc_p->nb_ue_disconnected_since_last_stat         = 0;
   mme_app_desc_p->nb_s1u_bearers_established_since_last_stat = 0;
@@ -92,25 +82,6 @@ int mme_app_statistics_display(void) {
 
 /*********************************** Utility Functions to update
  * Statistics**************************************/
-
-// Number of Connected eNBs
-void update_mme_app_stats_connected_enb_add(void) {
-  mme_app_desc_t* mme_app_desc_p = get_mme_nas_state(false);
-  // These stats will be persisted by mme_app task clocked
-  // by its incoming messages
-  (mme_app_desc_p->nb_enb_connected)++;
-  (mme_app_desc_p->nb_enb_connected_since_last_stat)++;
-  return;
-}
-void update_mme_app_stats_connected_enb_sub(void) {
-  mme_app_desc_t* mme_app_desc_p = get_mme_nas_state(false);
-  // These stats will be persisted by mme_app task clocked
-  // by its incoming messages
-  if (mme_app_desc_p->nb_enb_connected != 0)
-    (mme_app_desc_p->nb_enb_connected)--;
-  (mme_app_desc_p->nb_enb_released_since_last_stat)++;
-  return;
-}
 
 /*****************************************************/
 // Number of Connected UEs

lte/gateway/c/core/oai/tasks/s1ap/s1ap_mme.c
@@ -39,12 +39,12 @@
 #include "hashtable.h"
 #include "log.h"
 #include "assertions.h"
-#include "mme_app_statistics.h"
 #include "s1ap_mme_decoder.h"
 #include "s1ap_mme_handlers.h"
 #include "s1ap_mme_nas_procedures.h"
 #include "s1ap_mme_itti_messaging.h"
 #include "service303.h"
+#include "service303_message_utils.h"
 #include "dynamic_memory_check.h"
 #include "mme_config.h"
 #include "timer.h"
@@ -74,6 +74,9 @@
 bool s1ap_dump_ue_hash_cb(
     const hash_key_t keyP, void* const ue_void, void* parameter,
     void** unused_res);
+static void start_stats_timer(void);
+static int handle_stats_timer(zloop_t* loop, int id, void* arg);
+static long epc_stats_timer_id;
 
 bool hss_associated = false;
 static int indent   = 0;
@@ -362,12 +365,13 @@ static int handle_message(zloop_t* loop, zsock_t* reader, void* arg) {
 static void* s1ap_mme_thread(__attribute__((unused)) void* args) {
   itti_mark_task_ready(TASK_S1AP);
   init_task_context(
-      TASK_S1AP, (task_id_t[]){TASK_MME_APP, TASK_SCTP}, 2, handle_message,
-      &s1ap_task_zmq_ctx);
+      TASK_S1AP, (task_id_t[]){TASK_MME_APP, TASK_SCTP, TASK_SERVICE303}, 3,
+      handle_message, &s1ap_task_zmq_ctx);
 
   if (s1ap_send_init_sctp() < 0) {
     OAILOG_ERROR(LOG_S1AP, "Error while sendind SCTP_INIT_MSG to SCTP \n");
   }
+  start_stats_timer();
 
   zloop_start(s1ap_task_zmq_ctx.event_loop);
   s1ap_mme_exit();
@@ -409,6 +413,7 @@ status_code_e s1ap_mme_init(const mme_config_t* mme_config_p) {
 //------------------------------------------------------------------------------
 void s1ap_mme_exit(void) {
   OAILOG_DEBUG(LOG_S1AP, "Cleaning S1AP\n");
+  stop_timer(&s1ap_task_zmq_ctx, epc_stats_timer_id);
 
   put_s1ap_state();
   put_s1ap_imsi_map();
@@ -495,8 +500,6 @@ enb_description_t* s1ap_new_enb(s1ap_state_t* state) {
    * * * * TODO: Notify eNB with a cause like Hardware Failure.
    */
   DevAssert(enb_ref != NULL);
-  // Update number of eNB associated
-  state->num_enbs++;
   bstring bs = bfromcstr("s1ap_ue_coll");
   hashtable_uint64_ts_init(&enb_ref->ue_id_coll, mme_config.max_ues, NULL, bs);
   bdestroy_wrapper(&bs);
@@ -597,7 +600,7 @@ void s1ap_remove_ue(s1ap_state_t* state, ue_description_t* ue_ref) {
       OAILOG_INFO(LOG_S1AP, "Moving eNB state to S1AP_INIT \n");
       enb_ref->s1_state = S1AP_INIT;
       set_gauge("s1_connection", 0, 1, "enb_name", enb_ref->enb_name);
-      update_mme_app_stats_connected_enb_sub();
+      state->num_enbs--;
     } else if (enb_ref->s1_state == S1AP_SHUTDOWN) {
       OAILOG_INFO(LOG_S1AP, "Deleting eNB \n");
       set_gauge("s1_connection", 0, 1, "enb_name", enb_ref->enb_name);
@@ -615,5 +618,18 @@ void s1ap_remove_enb(s1ap_state_t* state, enb_description_t* enb_ref) {
   hashtable_uint64_ts_destroy(&enb_ref->ue_id_coll);
   hashtable_ts_free(&state->enbs, enb_ref->sctp_assoc_id);
   state->num_enbs--;
-  update_mme_app_stats_connected_enb_sub();
+}
+
+static int handle_stats_timer(zloop_t* loop, int id, void* arg) {
+  s1ap_state_t* s1ap_state_p = get_s1ap_state(false);
+  application_s1ap_stats_msg_t stats_msg;
+  stats_msg.nb_enb_connected = s1ap_state_p->num_enbs;
+  return send_s1ap_stats_to_service303(
+      &s1ap_task_zmq_ctx, TASK_S1AP, &stats_msg);
+}
+
+static void start_stats_timer(void) {
+  epc_stats_timer_id = start_timer(
+      &s1ap_task_zmq_ctx, EPC_STATS_TIMER_MSEC, TIMER_REPEAT_FOREVER,
+      handle_stats_timer, NULL);
 }

lte/gateway/c/core/oai/tasks/s1ap/s1ap_mme_handlers.c
@@ -46,7 +46,6 @@
 #include "s1ap_mme.h"
 #include "s1ap_mme_ta.h"
 #include "s1ap_mme_handlers.h"
-#include "mme_app_statistics.h"
 #include "mme_events.h"
 #include "3gpp_23.003.h"
 #include "3gpp_24.008.h"
@@ -664,7 +663,7 @@ status_code_e s1ap_mme_handle_s1_setup_request(
   s1ap_dump_enb(enb_association);
   rc = s1ap_generate_s1_setup_response(state, enb_association);
   if (rc == RETURNok) {
-    update_mme_app_stats_connected_enb_add();
+    state->num_enbs++;
     set_gauge("s1_connection", 1, 1, "enb_name", enb_association->enb_name);
     increment_counter("s1_setup", 1, 1, "result", "success");
     s1_setup_success_event(enb_name, enb_id);
@@ -3608,7 +3607,7 @@ status_code_e s1ap_handle_sctp_disconnection(
       OAILOG_INFO(
           LOG_S1AP, "Moving eNB with assoc_id %u to INIT state\n", assoc_id);
       enb_association->s1_state = S1AP_INIT;
-      update_mme_app_stats_connected_enb_sub();
+      state->num_enbs--;
     } else {
       OAILOG_INFO(
           LOG_S1AP,

lte/gateway/c/core/oai/tasks/s1ap/s1ap_mme_itti_messaging.c
@@ -26,7 +26,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <mme_app_ue_context.h>
-#include <mme_app_state.h>
 
 #include "bstrlib.h"
 #include "log.h"

lte/gateway/c/core/oai/tasks/service303/service303_mme_stats.c
@@ -21,10 +21,17 @@
 #include "mme_app_state.h"
 #include "service303.h"
 
-void service303_mme_statistics_read(application_mme_stats_msg_t* stats_msg_p) {
+void service303_mme_app_statistics_read(
+    application_mme_app_stats_msg_t* stats_msg_p) {
   size_t label = 0;
-  set_gauge("enb_connected", stats_msg_p->nb_enb_connected, label);
   set_gauge("ue_registered", stats_msg_p->nb_ue_attached, label);
   set_gauge("ue_connected", stats_msg_p->nb_ue_connected, label);
+  return;
+}
+
+void service303_s1ap_statistics_read(
+    application_s1ap_stats_msg_t* stats_msg_p) {
+  size_t label = 0;
+  set_gauge("enb_connected", stats_msg_p->nb_enb_connected, label);
   return;
 }

lte/gateway/c/core/oai/tasks/service303/service303_task.c
@@ -81,9 +81,13 @@ static int handle_service_message(zloop_t* loop, zsock_t* reader, void* arg) {
     case APPLICATION_UNHEALTHY_MSG: {
       service303_set_application_health(APP_UNHEALTHY);
     } break;
-    case APPLICATION_STATS_MSG: {
-      service303_mme_statistics_read(
-          &received_message_p->ittiMsg.application_mme_stats_msg);
+    case APPLICATION_MME_APP_STATS_MSG: {
+      service303_mme_app_statistics_read(
+          &received_message_p->ittiMsg.application_mme_app_stats_msg);
+    } break;
+    case APPLICATION_S1AP_STATS_MSG: {
+      service303_s1ap_statistics_read(
+          &received_message_p->ittiMsg.application_s1ap_stats_msg);
     } break;
     case TERMINATE_MESSAGE:
       free(received_message_p);

lte/protos/oai/mme_nas_state.proto
@@ -158,7 +158,7 @@ message MmeUeContext {
 // mme_app_desc_t
 message MmeNasState {
   MmeUeContext mme_ue_contexts = 1; // mme_ue_context_t
-  uint32 nb_enb_connected = 2;
+  uint32 nb_enb_connected = 2 [deprecated=true];
   uint32 nb_ue_attached = 3;
   uint32 nb_ue_connected = 4;
   uint32 nb_default_eps_bearers = 5;