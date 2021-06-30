73de0d828068a3836378f97480611206c795ba70
Timothée Dzik <timdzik@fb.com>
2021-06-30 20:43:18 +0200

    [AGW] Bump OVS to 2.14.3-7
    Signed-off-by: Timothée Dzik <timdzik@fb.com>

--- lte/gateway/release/build-magma.sh
+++ lte/gateway/release/build-magma.sh
@@ -183,10 +183,10 @@ if [[ "$OS" == "debian" ]]; then
 else
     OVS_DEPS=(
         "magma-libfluid >= 0.1.0.6"
-        "libopenvswitch >= 2.14"
-        "openvswitch-switch >= 2.14"
-        "openvswitch-common >= 2.14"
-        "openvswitch-datapath-dkms >= 2.14"
+        "libopenvswitch >= 2.14.3-7"
+        "openvswitch-switch >= 2.14.3-7"
+        "openvswitch-common >= 2.14.3-7"
+        "openvswitch-datapath-dkms >= 2.14.3-7"
         )
 fi
 
