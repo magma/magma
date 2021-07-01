e85e2182c6abc5634c59253a0680ab3983ddd2ca
Timothée Dzik <timdzik@fb.com>
2021-07-01 19:26:47 +0200

    Bump master to 1.6 installation
    Signed-off-by: Timothée Dzik <timdzik@fb.com>

--- lte/gateway/release/upgrade_magma.sh
+++ lte/gateway/release/upgrade_magma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WHOAMI=$(whoami)
-MAGMA_VERSION="1.5.2"
+MAGMA_VERSION="1.6.0"
 # Default is focal
 OS_VERSION="focal"
 
@@ -22,6 +22,10 @@ done
 
 if grep -q 'Debian' /etc/issue; then
   OS_VERSION="stretch"
+  # We don't support 1.6.0 for debian so bumping down to 1.5.2
+  if [ "$MAGMA_VERSION" == "1.6.0" ]; then
+    MAGMA_VERSION="1.5.2"
+  fi
 fi
 
 apt update
