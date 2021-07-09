b18aee90a2a9cafdc3eccd061b7196550a73672e
Pravin Shelar <pbshelar@fb.com>
2021-07-09 16:10:12 -0700

    fix(mme): add config flag to control GTP-U echo (#7980)
    This is turned off by default to investigate perf
    regression.
    
    Signed-off-by: Pravin B Shelar <pbshelar@fb.com>

--- lte/gateway/c/core/oai/tasks/gtpv1-u/gtp_tunnel_openflow.c
+++ lte/gateway/c/core/oai/tasks/gtpv1-u/gtp_tunnel_openflow.c
@@ -154,14 +154,21 @@ static uint32_t get_gtp_port_no(char port_name[]) {
  */
 static uint32_t create_gtp_port(struct in_addr enb_addr, char port_name[]) {
   char gtp_port_create[512];
+  char* gtp_echo;
   int rc;
+
+  if (spgw_config.sgw_config.ovs_config.gtp_echo) {
+    gtp_echo = "true";
+  } else {
+    gtp_echo = "false";
+  }
   rc = snprintf(
       gtp_port_create, sizeof(gtp_port_create),
       "sudo ovs-vsctl --may-exist add-port gtp_br0 %s -- set Interface %s "
       "type=%s "
       "options:remote_ip=%s options:key=flow "
-      "bfd:enable=true",
-      port_name, port_name, ovs_gtp_type, inet_ntoa(enb_addr));
+      "bfd:enable=%s",
+      port_name, port_name, ovs_gtp_type, inet_ntoa(enb_addr), gtp_echo);
   if (rc < 0) {
     OAILOG_ERROR(LOG_GTPV1U, "gtp-port create: format error %d", rc);
     return rc;
