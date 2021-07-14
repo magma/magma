b9f5fc2dcc21d9b282aeef4697d815cf19a114b7
Ramon Melero <ramonmelero@fb.com>
2021-07-13 23:49:22 -0700

    WIP AGW containerization, mobilityd service
    Signed-off-by: Ramon Melero <ramonmelero@fb.com>

---
+++ lte/gateway/docker/services/build/Dockerfile.c
@@ -0,0 +1,11 @@
+FROM mme_builder:latest
+
+ARG MAGMA_VERSION=master
+
+RUN git clone https://github.com/magma/magma.git && \
+    cd magma && \
+    git checkout $MAGMA_VERSION
+
+WORKDIR /magma/lte/gateway
+
+RUN make build_common build_oai build_sctpd
