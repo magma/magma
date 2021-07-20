259b5430d38f1204fbfbfff4974764f7af7d80d8
chandhu-wavelabs <chandhu.naga@wavelabs.ai>
2021-07-20 18:36:01 +0000

    Registration reject support
    Signed-off-by: chandhu-wavelabs <chandhu.naga@wavelabs.ai>

--- lte/gateway/c/core/oai/tasks/amf/amf_recv.cpp
+++ lte/gateway/c/core/oai/tasks/amf/amf_recv.cpp
@@ -101,8 +101,10 @@ int amf_handle_registration_request(
    */
   OAILOG_DEBUG(LOG_NAS_AMF, "Processing REGITRATION_REQUEST message\n");
   if (amf_cause != AMF_CAUSE_SUCCESS) {
-    rc = amf_proc_registration_reject(ue_id, amf_cause);
-    OAILOG_DEBUG(LOG_NAS_AMF, "Processing REGITRATION_REQUEST message\n");
+    amf_cause = AMF_CAUSE_SUCCESS;
+    OAILOG_INFO(LOG_NAS_AMF, "Processing REGITRATION_REJECT  message\n");
+    rc        = amf_proc_registration_reject(ue_id, amf_cause);
+    OAILOG_FUNC_RETURN(LOG_NAS_AMF, rc);
   }
   amf_registration_request_ies_t* params =
       new (amf_registration_request_ies_t)();
