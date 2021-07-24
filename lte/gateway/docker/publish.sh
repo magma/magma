31febd852cf08954f367d7990c419ba48746d354
Ramon Melero <ramonmelero@fb.com>
2021-07-23 19:23:32 -0700

    WIP AGW containerization, mobilityd service
    Signed-off-by: Ramon Melero <ramonmelero@fb.com>

--- lte/gateway/docker/publish.sh
+++ lte/gateway/docker/publish.sh
@@ -1,38 +1,26 @@
-
-
+REGISTRY=$1
+VERSION=latest
 publish (){
-# aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/z2g3r6f7
-# docker tag control_proxy:latest public.ecr.aws/z2g3r6f7/control_proxy:latest
-# docker push public.ecr.aws/z2g3r6f7/control_proxy:latest
-  docker tag $1:latest public.ecr.aws/z2g3r6f7/$1:latest
-  docker push public.ecr.aws/z2g3r6f7/$1:latest
+  docker tag $1:latest ${REGISTRY}$1:${VERSION}
+  docker push ${REGISTRY}$1:${VERSION}
 }
 
-
-
-
 publish mobilityd
-# build enodebd
-# build health
-# build policydb
-# build smsd
-# build subscriberdb
-# build ctraced
-# build magmad
-# build state
-# build directoryd
-# build pipelined
-# build eventd
-# build control_proxy
-#
-# # build container for C services
-# cd ../../../
-# docker build . -f lte/gateway/docker/mme/Dockerfile.ubuntu20.04 -t mme_builder:latest
-# cd lte/gateway/docker
-# docker build . -f services/build/Dockerfile.c -t cbuilder:latest
-#
-# # C services
-# build mme
-# build sctpd
-# build sessiond
-# build envoy_controller
+publish enodebd
+publish health
+publish policydb
+publish smsd
+publish subscriberdb
+publish ctraced
+publish magmad
+publish state
+publish directoryd
+publish pipelined
+publish eventd
+publish control_proxy
+
+# C services
+publish mme
+publish sctpd
+publish sessiond
+publish envoy_controller
