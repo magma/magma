012590d7a3b5b67ef2303ec31282ed2e2e292726
vinay <vinay.tokala@radisys.com>
2021-09-01 21:25:04 +0530

    fix(amf): addressed code review comments
    Signed-off-by: vinay <vinay.tokala@radisys.com>

--- lte/gateway/c/core/oai/tasks/amf/amf_app_handler.cpp
+++ lte/gateway/c/core/oai/tasks/amf/amf_app_handler.cpp
@@ -765,7 +765,6 @@ int amf_app_handle_pdu_session_accept(
       M5G_SESSION_MANAGEMENT_MESSAGES;
   smf_msg->header.pdu_session_id = pdu_session_resp->pdu_session_id;
   smf_msg->header.message_type   = PDU_SESSION_ESTABLISHMENT_ACCEPT;
-  // smf_msg->header.procedure_transaction_id = smf_ctx->smf_proc_data.pti.pti;
   smf_msg->header.procedure_transaction_id = smf_ctx->smf_proc_data.pti.pti;
   smf_msg->msg.pdu_session_estab_accept.extended_protocol_discriminator
       .extended_proto_discriminator = M5G_SESSION_MANAGEMENT_MESSAGES;
@@ -773,15 +772,10 @@ int amf_app_handle_pdu_session_accept(
       pdu_session_resp->pdu_session_id;
   smf_msg->msg.pdu_session_estab_accept.pti.pti =
       smf_ctx->smf_proc_data.pti.pti;
-  ;
-  // smf_ctx->smf_proc_data.pti.pti;
   smf_msg->msg.pdu_session_estab_accept.message_type.msg_type =
       PDU_SESSION_ESTABLISHMENT_ACCEPT;
   smf_msg->msg.pdu_session_estab_accept.pdu_session_type.type_val = 1;
-  // smf_msg->msg.pdu_session_estab_accept.pdu_session_type.type_val =
-  // pdu_session_resp->pdu_session_type;
   smf_msg->msg.pdu_session_estab_accept.ssc_mode.mode_val = 1;
-  // smf_msg->msg.pdu_session_estab_accept.ssc_mode.mode_val = SSC_MODE_ONE;
 
   memset(
       &(smf_msg->msg.pdu_session_estab_accept.pdu_address.address_info), 0, 12);
