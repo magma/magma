9c7b2b553f6bb9c55e01febc0cc994e10725a5b9
root <369888042@qq.com>
2021-09-07 06:22:25 -0400

    ho algorithm parameters
    Signed-off-by: root <369888042@qq.com>

--- orc8r/tools/fab/dev_utils.py
+++ orc8r/tools/fab/dev_utils.py
@@ -214,7 +214,8 @@ def is_hw_id_registered(
     """
     # gateways is a dict mapping gw ID to full resource
     gateways = cloud_get(
-        f'networks/{network_id}/gateways',
+        #f'networks/{network_id}/gateways',
+        f'lte/{network_id}/gateways',
         admin_cert=admin_cert,
     )
     for gw in gateways.values():
