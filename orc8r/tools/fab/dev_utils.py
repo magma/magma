9f55630cb651d4366c1c0fc06b1b900534f1f0aa
root <369888042@qq.com>
2021-09-07 22:51:24 -0400

    ho algorithm parameters

--- orc8r/tools/fab/dev_utils.py
+++ orc8r/tools/fab/dev_utils.py
@@ -214,12 +214,10 @@ def is_hw_id_registered(
     """
     # gateways is a dict mapping gw ID to full resource
     gateways = cloud_get(
-        #f'networks/{network_id}/gateways',
         f'lte/{network_id}/gateways',
         admin_cert=admin_cert,
     )
     for gw in gateways.values():
-        print(gw)
         if gw['device']['hardware_id'] == hw_id:
             return True, gw['id']
     return False, ''
