6fff66796d265c80cdd4bdb94ae77d36e0ac450b
root <369888042@qq.com>
2021-09-15 23:16:36 -0400

    ho algorithm parameters
    Signed-off-by: root <369888042@qq.com>

--- nms/packages/magmalte/app/views/equipment/EnodebDetailConfig.js
+++ nms/packages/magmalte/app/views/equipment/EnodebDetailConfig.js
@@ -245,8 +245,9 @@ function EnodebManagedRanConfig({
     [
       {
         category: 'A1 Threshold Rsrp',
+
         value: enbInfo.enb.enodeb_config?.managed_config?.ho_algorithm_config
-          ?.a1_threshold_rsrp ?? '-',
+            ?.a1_threshold_rsrp ?? '-',
       },
     ],
   ];
