14c12440b4815f2d275e153a154ad0cebdacb396
Scott Moeller <electronjoe@gmail.com>
2021-09-17 09:52:16 -0400

    refactor(agw): refactor MConfigLoader to enable test, add tests (#9057)
    # Summary
    
    This refactoring PR was necessary to enable testing of e.g. #9047.
    
    The general points of refactoring were as follows.
    
    - Removed the Class definition
      - As it was unused and extraneous
    - Moved the bulk of logic to a public `load_service_mconfig` function that takes in an istream
      - To allow tests that do not require filesystem read/write and associated complexities
    - Moved non-public functions to anonymous namespace
      - Preferred over static declaration that (sometimes) existed
    
    # Outstanding Work
    
    When we have access to Google's abseil library, we should make use of a couple helpers there.
    
    - Consider use of the absl::Status return type instead of Boolean
      - Enables more fidelity in error reporting, error joining, etc
    - Shall use absl::Cleanup to ensure closure of opened resources (e.g. the file.close())
    
    # Behavioral Change
    
    In authoring tests, it was discovered that many mal-formed json mconfigs would actually result in an uncaught exception being thrown by our third party json library.  I now catch this exception in the MConfigLoader library and pass back a status instead (failure notice via the boolean return value).
    
    # Bazel Build based Test
    
    Our CMake project architecture seems to make it ~impossible to access our protobuf definitions for mconfig (in /lte/gateway/..) from the MConfigLoader library that uses them (in /orc8r/gateway/c/..).
    
    This PR includes a Bazel BUILD file which has no issue with this. Hopefully we'll go the way of Bazel.
    
    # Test Strategy
    
    Authored new unit tests to confirm behavior for the following. Note this omits testing for the file i/o portions of the library.
    
    - Empty input ("")
      - Should return failure status (false) on function call
    - Input with proper MME mconfig shape and a couple fields set
      - Validate fields are parsed and in expected proto result
    
    The PR has also passed [lte-integ-test](https://app.circleci.com/pipelines/github/magma/magma/32696/workflows/d9495b91-538b-4ed3-ab98-92fa8596441f/jobs/380159).
    
    Signed-off-by: Scott Harrison Moeller <smoeller@fb.com>

--- orc8r/gateway/c/common/config/includes/MConfigLoader.h
+++ orc8r/gateway/c/common/config/includes/MConfigLoader.h
@@ -12,41 +12,28 @@
  */
 #pragma once
 
-#include <iosfwd>  // for ifstream
-#include <string>  // for string
-namespace google {
-namespace protobuf {
-class Message;
-}
-}  // namespace google
+#include <istream>                    // for istream
+#include <string>                     // for string
+#include <google/protobuf/message.h>  // for Message
 
 namespace magma {
-
 /**
- * MConfigLoader is used to load mconfig files for service configurations
+ * load_service_mconfig_from_file loads an mconfig from the statically defined
+ * files.
+ * @param service_name - name of service to load
+ * @param message - pointer to protobuf message to load file to. Note that
+ *                  this should match the message defined in mconfigs.proto.
+ * @returns true if message was parsed successfully, false if the file reading
+ *          failed, the service configuration is missing, or the message
+ *          passed is not the right type
  */
-class MConfigLoader {
- public:
-  /**
-   * load_service_mconfig loads an mconfig from the statically defined files.
-   * @param service_name - name of service to load
-   * @param message - pointer to protobuf message to load file to. Note that
-   *                  this should match the message defined in mconfigs.proto.
-   * @returns true if message was parsed successfully, false if the file reading
-   *          failed, the service configuration is missing, or the message
-   *          passed is not the right type
-   */
-  bool load_service_mconfig(
-      const std::string& service_name, google::protobuf::Message* message);
+// TODO(#6151): migrate to an absl::Status return type.
+bool load_service_mconfig_from_file(
+    const std::string& service_name, google::protobuf::Message* message);
 
- private:
-  static constexpr const char* DYNAMIC_MCONFIG_PATH =
-      "/var/opt/magma/configs/gateway.mconfig";
-  static constexpr const char* CONFIG_DIR        = "/etc/magma";
-  static constexpr const char* MCONFIG_FILE_NAME = "gateway.mconfig";
+bool load_service_mconfig(
+    const std::string& service_name, std::istream* config_stream,
+    google::protobuf::Message* message);
 
- private:
-  void get_mconfig_file(std::ifstream* file);
-};
-
+void get_mconfig_file(std::ifstream* file);
 }  // namespace magma
