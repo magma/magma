788b9ca102a12c3519e5378bd8db81232689f511
Regina Wang <rwang3495@gmail.com>
2021-10-06 16:17:16 -0700

    Remove WiFi Module references in NMS
    Signed-off-by: Regina Wang <rwang3495@gmail.com>

--- nms/packages/magmalte/server/apicontroller/routes.js
+++ nms/packages/magmalte/server/apicontroller/routes.js
@@ -152,7 +152,7 @@ router.use(
   }),
 );
 
-const networkTypeRegex = '(cwf|feg|lte|feg_lte|symphony|wifi)';
+const networkTypeRegex = '(cwf|feg|lte|feg_lte|symphony)';
 router.use(
   `/magma/v1/:networkType(${networkTypeRegex})/:networkID`,
   proxy(API_HOST, {
