a41d26513e53985b02aed018a560e39ed8b21daa
Scott Harrison Moeller <smoeller@fb.com>
2021-10-06 09:58:58 +0000

    fix(agw): adds cleanup of stray unix domain socket to agwd
    Signed-off-by: Scott Harrison Moeller <smoeller@fb.com>

---
+++ src/go/agwd/server/server_windows_test.go
@@ -0,0 +1,74 @@
+// Copyright 2021 The Magma Authors.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//+build windows
+
+package server
+
+import (
+	"errors"
+	"net"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/magma/magma/src/go/internal/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func removeNoError(string) error {
+	return nil
+}
+
+func osFileNoError(string) (os.FileInfo, error) {
+	return nil, nil
+}
+
+func netDialNoError(network, address string, timeout time.Duration) (net.Conn, error) {
+	return nil, nil
+}
+
+func netDialError(network, address string, timeout time.Duration) (net.Conn, error) {
+	return nil, errors.New("any error will do")
+}
+
+// TestCleanupExistingUnusedSock ensures that should an un-used existing socket exist, we clean it up
+// and log a warning.
+func TestCleanupExistingUnusedSock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	logger, _ := testutil.NewTestLogger()
+
+	err := cleanupUnixSocket(logger, osFileNoError, removeNoError, netDialError, "arbitrary_file_path.sock")
+
+	assert.NotNil(t, err)
+	wantErrMsg := "found pre-existing socket at path, but does not support cleanup in GOOS == windows"
+	assert.Containsf(t,
+		err.Error(), wantErrMsg, "expected error containing %q, got %s", wantErrMsg, err)
+}
+
+// TestCleanupExistingConnectedSock validates that should an existing server be hosting on a unix
+// domain socket, we do not clean it up but instead log an error.
+func TestCleanupExistingConnectedSock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	logger, _ := testutil.NewTestLogger()
+
+	err := cleanupUnixSocket(logger, osFileNoError, removeNoError, netDialNoError, "arbitrary_file_path.sock")
+
+	assert.NotNil(t, err)
+	wantErrMsg := "found pre-existing socket at path, but does not support cleanup in GOOS == windows"
+	assert.Containsf(t,
+		err.Error(), wantErrMsg, "expected error containing %q, got %s", wantErrMsg, err)
+}
