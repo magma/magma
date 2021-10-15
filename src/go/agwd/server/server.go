0892fd9d6301c08491e88b1c0f347fa410fb349b
Scott Moeller <electronjoe@gmail.com>
2021-10-15 13:19:44 -0400

    fix(agw): adds cleanup of stray unix domain socket to agwd (#9635)
    ## Summary
    
    This PR adds more graceful handling of dangling / leftover unix domain socket handling by the Golang `agwd` binary.
    
    - If an existing socket exists at the specified path
      - But a Dial attempt indicates no Listener -> Log warning and delete old sock
      - But a Dial attempt indicates a Listener -> Return error (ultimately panic)
    
    ## Test Plan
    
    Authored new unit tests to cover the majority of the code path for socket creation.
    
    Signed-off-by: Scott Harrison Moeller <smoeller@fb.com>

--- src/go/agwd/server/server.go
+++ src/go/agwd/server/server.go
@@ -12,7 +12,11 @@
 package server
 
 import (
+	"fmt"
 	"net"
+	"os"
+	"runtime"
+	"time"
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
@@ -41,10 +45,53 @@ func newServiceRouter(cfgr config.Configer) service.Router {
 	)
 }
 
+func cleanupUnixSocket(
+	logger log.Logger,
+	osStat func(string) (os.FileInfo, error),
+	osRemove func(string) error,
+	netDialTimeout func(network, address string, timeout time.Duration) (net.Conn, error),
+	path string) error {
+	_, err := osStat(path)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return errors.Wrapf(err, "os.Stat(%s)", path)
+	}
+
+	// attempt to connect to see if someone is still bound to the socket.
+	if runtime.GOOS == "windows" {
+		return fmt.Errorf("cleanupUnixSocket(logger=_,path=%q) found pre-existing socket at path, but does not support cleanup in GOOS == windows", path)
+	}
+	_, err = netDialTimeout("unix", path, time.Second)
+	if err == nil {
+		return fmt.Errorf("os.Stat(%s): existing listener on socket file", path)
+	}
+	logger.Warning().Printf("Removing existing socket file; previous unclean shutdown?")
+	if err := osRemove(path); err != nil {
+		return errors.Wrapf(err, "os.Stat(%s)", path)
+	}
+
+	return nil
+}
+
+func cleanupUnixSocketOrDie(logger log.Logger, path string) {
+	if err := cleanupUnixSocket(logger, os.Stat, os.Remove, net.DialTimeout, path); err != nil {
+		panic(errors.Wrapf(
+			err,
+			"cleanupUnixSocket(logger=_, target.Endpoint=%s)",
+			path))
+	}
+}
+
 func startSctpdDownlinkServer(
 	cfgr config.Configer, logger log.Logger, sr service.Router,
 ) {
 	target := config.ParseTarget(cfgr.Config().GetMmeSctpdDownstreamServiceTarget())
+	if target.Scheme == "unix" {
+		cleanupUnixSocketOrDie(logger, target.Endpoint)
+	}
+
 	listener, err := net.Listen(
 		target.Scheme, target.Endpoint)
 	if err != nil {
