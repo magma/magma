05d9f875679db179e9da5358282cff61dff21bd7
HannaFar <hannafarag159@gmail.com>
2021-10-20 18:28:41 +0200

    feat(nms): Add subscriber bulk add new workflow
    Signed-off-by: HannaFar <hannafarag159@gmail.com>

--- nms/flow-typed/npm/@material-ui/lab/index.js
+++ nms/flow-typed/npm/@material-ui/lab/index.js
@@ -13,7 +13,6 @@ type ToggleButton = {
 declare module "@material-ui/lab/ToggleButton" {
   declare module.exports: React$ComponentType<ToggleButton>;
 }
-
 declare module "@material-ui/lab/ToggleButtonGroup" {
   declare module.exports: React$ComponentType<{
     children?: React$Node,
@@ -24,12 +23,33 @@ declare module "@material-ui/lab/ToggleButtonGroup" {
     value?: any,
   }>;
 }
+
+
+type Alert = {
+  severity: 'error' | 'info' | 'success' | 'warning',
+  variant?: 'filled' | 'outlined' | 'standard',
+  children?: React$Node,
+  classes?: Object,
+}
+type AlertTitle = {
+  children?: React$Node,
+  classes?: Object,
+}
+declare module "@material-ui/lab/Alert" {
+  declare module.exports: React$ComponentType<Alert>;
+}
+declare module "@material-ui/lab/AlertTitle" {
+  declare module.exports: React$ComponentType<AlertTitle>;
+}
   
 declare module "@material-ui/lab" {
   declare module.exports: {
       Autocomplete: $Exports<"@material-ui/lab/Autocomplete">,
       ToggleButton: $Exports<"@material-ui/lab/ToggleButton">,
       ToggleButtonGroup: $Exports<"@material-ui/lab/ToggleButtonGroup">,
+      Alert: $Exports<"@material-ui/lab/Alert">,
+      AlertTitle: $Exports<"@material-ui/lab/AlertTitle">,
+
   };
 }
   
--- nms/packages/magmalte/app/views/subscriber/SubscriberOverview.js
+++ nms/packages/magmalte/app/views/subscriber/SubscriberOverview.js
@@ -13,14 +13,9 @@
  * @flow strict-local
  * @format
  */
-import AddSubscriberButton, {
-  BulkEditSubscriberButton,
-  validateSubscriberInfo,
-} from './SubscriberAddDialog';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import PeopleIcon from '@material-ui/icons/People';
 import React from 'react';
@@ -31,17 +26,13 @@ import SubscriberDetail from './SubscriberDetail';
 import SubscriberStateTable from './SubscriberStateTable';
 import SubscriberTable from './SubscriberTable';
 import TopBar from '../../components/TopBar';
-import type {SubscriberInfo} from './SubscriberAddDialog';
 import type {
   mutable_subscriber,
-  mutable_subscribers,
   subscriber,
 } from '../../../generated/MagmaAPIBindings';
 
 import {Redirect, Route, Switch} from 'react-router-dom';
-import {hexToBase64, isValidHex} from '@fbcnms/util/strings';
-import {useContext, useRef, useState} from 'react';
-import {useEnqueueSnackbar} from '@fbcnms/ui/hooks/useSnackbar';
+import {useContext} from 'react';
 import {useRouter} from '@fbcnms/ui/hooks';
 
 const TITLE = 'Subscribers';
@@ -82,149 +73,9 @@ type Props = {
   onClose?: () => void,
   imsi: string,
 };
-type refreshProps = {
-  refreshContext: () => void,
-};
-const SUBSCRIBERS_CHUNK_SIZE = 1000;
-
-function SubscriberActions(props: refreshProps) {
-  const [_error, setError] = useState('');
-  const enqueueSnackbar = useEnqueueSnackbar();
-  const ctx = useContext(SubscriberContext);
-  const successCountRef = useRef(0);
-  const [open, setOpen] = useState(false);
-
-  const saveSubscribers = async (
-    subscribers: Array<SubscriberInfo>,
-    edit: boolean,
-  ) => {
-    let addedSubscribers = [];
-    let subscriberErrors = '';
-    for (const [i, subscriber] of subscribers.entries()) {
-      try {
-        const err = validateSubscriberInfo(subscriber, ctx.state);
-        if (err.length > 0) {
-          throw err;
-        }
-        const authKey =
-          subscriber.authKey && isValidHex(subscriber.authKey)
-            ? hexToBase64(subscriber.authKey)
-            : '';
-
-        const authOpc =
-          subscriber.authOpc !== undefined && isValidHex(subscriber.authOpc)
-            ? hexToBase64(subscriber.authOpc)
-            : '';
-        const newSubscriber = {
-          active_apns: subscriber.apns,
-          active_policies: subscriber.policies,
-          id: subscriber.imsi,
-          name: subscriber.name,
-          lte: {
-            auth_algo: 'MILENAGE',
-            auth_key: authKey,
-            auth_opc: authOpc,
-            state: subscriber.state,
-            sub_profile: subscriber.dataPlan,
-          },
-        };
-        if (edit) {
-          ctx.setState?.(subscriber.imsi, newSubscriber);
-        } else {
-          addedSubscribers.push(newSubscriber);
-          // bulk add chunked subscribers
-          if (
-            addedSubscribers.length == SUBSCRIBERS_CHUNK_SIZE ||
-            i == subscribers.length - 1
-          ) {
-            const success = await bulkAdd(addedSubscribers, subscriberErrors);
-            if (success) {
-              successCountRef.current =
-                successCountRef.current + addedSubscribers.length;
-              addedSubscribers = [];
-            } else {
-              enqueueSnackbar('Saving subscribers to the api failed: ', {
-                variant: 'error',
-              });
-              return;
-            }
-          }
-        }
-      } catch (e) {
-        const errMsg = e.response?.data?.message ?? e.message ?? e;
-        subscriberErrors +=
-          'error saving ' + subscriber.imsi + ' : ' + errMsg + '\n';
-        //report saved errors if we reach end of loop without calling bulkadd.
-        if (i == subscribers.length - 1) {
-          setError(subscriberErrors);
-          enqueueSnackbar('Saving subscribers to the api failed: ', {
-            variant: 'error',
-          });
-          return;
-        }
-      }
-    }
-    enqueueSnackbar(
-      ` Subscriber${successCountRef.current > 1 ? 's' : ''} saved successfully`,
-      {
-        variant: 'success',
-      },
-    );
-    setOpen(false);
-    props.refreshContext();
-  };
-  const bulkAdd = async (
-    addedSubscribers: mutable_subscribers,
-    subscriberErrors: string,
-  ) => {
-    let success = true;
-    try {
-      if (subscriberErrors.length > 0) {
-        setError(subscriberErrors);
-        return false;
-      }
-      await ctx.setState?.('', addedSubscribers);
-      return success;
-    } catch (e) {
-      const errMsg = e.response?.data?.message ?? e.message ?? e;
-      setError('error saving subscribers: ' + errMsg);
-      success = false;
-      return success;
-    }
-  };
-  return (
-    <Grid container alignItems="center" spacing={1}>
-      <Grid item>
-        <BulkEditSubscriberButton
-          onClose={() => {
-            props.refreshContext();
-          }}
-          onSave={subscribers => {
-            saveSubscribers(subscribers, true);
-          }}
-        />
-      </Grid>
-      <Grid item>
-        <AddSubscriberButton
-          onClose={() => {
-            props.refreshContext();
-          }}
-          onSave={subscribers => {
-            saveSubscribers(subscribers, false);
-          }}
-          isOpen={open}
-          handleOpen={isOpen => {
-            setOpen(isOpen);
-          }}
-        />
-      </Grid>
-    </Grid>
-  );
-}
 
 export function SubscribersOverview() {
   const {relativePath, relativeUrl} = useRouter();
-  const [refresh, setRefresh] = useState(false);
 
   return (
     <>
@@ -235,9 +86,6 @@ export function SubscribersOverview() {
             label: 'Config',
             to: '/config',
             icon: SettingsIcon,
-            filters: (
-              <SubscriberActions refreshContext={() => setRefresh(!refresh)} />
-            ),
           },
           {
             label: 'Sessions',
@@ -249,7 +97,7 @@ export function SubscribersOverview() {
       <Switch>
         <Route
           path={relativePath('/config')}
-          component={() => <SubscriberTable refresh />}
+          component={() => <SubscriberTable />}
         />
         <Route
           path={relativePath('/sessions')}
