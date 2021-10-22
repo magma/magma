3a84550e852c1c9858e23493135a7be344624293
Pruthvi Hebbani <pruthvi.hebbani@radisys.com>
2021-10-22 15:51:30 +0530

    Removed unused messages
    Signed-off-by: Pruthvi Hebbani <pruthvi.hebbani@radisys.com>

--- lte/gateway/c/core/oai/include/s11_messages_def.h
+++ lte/gateway/c/core/oai/include/s11_messages_def.h
@@ -99,9 +99,3 @@ MESSAGE_DEF(
     S11_DOWNLINK_DATA_NOTIFICATION_ACKNOWLEDGE,
     itti_s11_downlink_data_notification_acknowledge_t,
     s11_downlink_data_notification_acknowledge)
-MESSAGE_DEF(
-    S11_MME_INIT_DEACTIVATE_BEARER_REQ, itti_s11_mme_initiated_deactivate_bearer_req_t,
-    itti_s11_mme_initiated_deactivate_bearer_req)
-MESSAGE_DEF(
-    S11_MME_INIT_DEACTIVATE_BEARER_RSP, itti_s11_mme_initiated_deactivate_bearer_rsp_t,
-    itti_s11_mme_initiated_deactivate_bearer_rsp)
