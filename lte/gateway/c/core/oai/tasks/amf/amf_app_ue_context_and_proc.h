fa5909e249e64bf32a68627f25cbf38de76f862c
ganeshg87 <ganesh.gedela@wavelabs.ai>
2021-10-26 14:58:03 +0000

    fix(mme): PDUSessionResourceSetupReq and PDUSessionEstablishmentAccept Clubbed into Single NGAP Message
    Test Cases:
      - verified PDUSession setup with UERANSIM
    
    Signed-off-by: ganeshg87 <ganesh.gedela@wavelabs.ai>

--- lte/gateway/c/core/oai/tasks/amf/amf_app_ue_context_and_proc.h
+++ lte/gateway/c/core/oai/tasks/amf/amf_app_ue_context_and_proc.h
@@ -788,7 +788,7 @@ void amf_smf_context_cleanup_pdu_session(ue_m5gmm_context_s* ue_context);
 // PDU session related communication to gNB
 int pdu_session_resource_setup_request(
     ue_m5gmm_context_s* ue_context, amf_ue_ngap_id_t amf_ue_ngap_id,
-    std::shared_ptr<smf_context_t>);
+    std::shared_ptr<smf_context_t>, bstring);
 void amf_app_handle_resource_setup_response(
     itti_ngap_pdusessionresource_setup_rsp_t session_seup_resp);
 int pdu_session_resource_release_request(
