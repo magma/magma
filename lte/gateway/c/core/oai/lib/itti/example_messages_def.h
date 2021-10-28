0a541ace6c565761a59ebb9ffd085ef1527bf885
Scott Moeller <electronjoe@gmail.com>
2021-10-28 14:07:33 +0000

    chore(mme): fully specify include paths in oai/lib/itti
    Signed-off-by: Scott Moeller <electronjoe@gmail.com>

--- lte/gateway/c/core/oai/lib/itti/example_messages_def.h
+++ lte/gateway/c/core/oai/lib/itti/example_messages_def.h
@@ -30,7 +30,7 @@
 
 // These messages files are mandatory and must always be placed in first
 // position
-#include "intertask_messages_def.h"
+#include "lte/gateway/c/core/oai/lib/itti/intertask_messages_def.h"
 
 // Dummy file for the generic intertask interface definition. Actual definition
 // should be in top level build directory.
