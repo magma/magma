e4f67d4be62093943e917c4c6703a590db363dcd
Sebastian Thomas <sebastian.thomas@tngtech.com>
2021-10-29 15:35:48 +0200

    fix(agw): Enable opt-in based logging to Sentry
    Makes it possible to select only a subset of log messages to be
    sent to Sentry instead of everything above a certain log level.
    In Python services there are now three possible configuration
    settings for Sentry:
    * send all errors
    * send only selected errors (opt-in)
    * disabled
    
    Also adds a configurable decorator to send all uncaught errors
    to Sentry, which is useful in the opt-in case. It is used for
    all gRPC endpoints of mobilityd.
    
    Signed-off-by: Sebastian Thomas <sebastian.thomas@tngtech.com>
    Signed-off-by: Jan Heidbrink <jan.heidbrink@tngtech.com>
    Signed-off-by: Marie Bremner <marwhal@fb.com>

---
+++ orc8r/gateway/python/magma/common/tests/sentry_tests.py
@@ -0,0 +1,52 @@
+"""
+Copyright 2021 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import logging
+import unittest
+
+from magma.common.sentry import (
+    _ignore_if_not_marked,
+    send_uncaught_errors_to_monitoring,
+)
+
+
+class SentryTests(unittest.TestCase):
+    """
+    Tests for Sentry monitoring
+    """
+
+    def test_ignore_if_not_marked(self):
+        """Test ignored events that are not sent to Sentry"""
+        self.assertIsNone(_ignore_if_not_marked({"key": "value", "extra": {"something_else": 3}}, {}))
+
+    def test_do_not_ignore_and_remove_tag_if_marked(self):
+        """Test marked events that are sent to Sentry"""
+        returned_event = _ignore_if_not_marked({"key": "value", "extra": {"send_to_monitoring": True}}, {})
+        self.assertEqual(returned_event, {"key": "value", "extra": {}})
+
+    def test_uncaught_error_wrapper(self):
+        """Test enabled wrapper logs an error"""
+        @send_uncaught_errors_to_monitoring(True)
+        def raise_error():
+            raise ValueError("Something went wrong")
+
+        with self.assertLogs() as captured:
+            try:
+                raise_error()
+            except ValueError:
+                pass
+
+        self.assertEqual(len(captured.records), 1)
+        log_record = captured.records[0]
+        self.assertEqual(log_record.message, "Uncaught error")
+        self.assertEqual(log_record.levelno, logging.ERROR)
