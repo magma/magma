2bf714aae7cbb24a2464bc5c8657c99c8ef2c879
RahulKalsangra <rahul.kalsangra@wavelabs.ai>
2021-10-29 21:33:39 +0530

    fix(amf): Changing the default parameter for amfName in gate
    Signed-off-by: RahulKalsangra <rahul.kalsangra@wavelabs.ai>

--- lte/gateway/c/core/oai/test/mme_app_task/test_mme_app_config.cpp
+++ lte/gateway/c/core/oai/test/mme_app_task/test_mme_app_config.cpp
@@ -155,7 +155,7 @@ const char* kHealthyConfig =
     {
       # outcome drop timer value (seconds)
       #  S1AP_OUTCOME_TIMER = 10;
-      AMF_NAME = "AMF_1"
+      AMF_NAME = "MAGMAAMF1"
     };
     # AMF served GUAMFIs
     # AMF code DEFAULT  size = 8 bits
