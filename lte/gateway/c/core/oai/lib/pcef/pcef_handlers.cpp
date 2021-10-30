953a10fd7b95d892c57d315e716e5b35e96d8ca6
Ulas Kozat <kozat@fb.com>
2021-10-29 17:42:07 -0700

    fix(mme): Handle failed teid update by deactivating default bearer (#9968)
    * fix(mme): handle failed TEID update
    
    Signed-off-by: Ulas Kozat <kozat@fb.com>

--- lte/gateway/c/core/oai/lib/pcef/pcef_handlers.cpp
+++ lte/gateway/c/core/oai/lib/pcef/pcef_handlers.cpp
@@ -200,8 +200,33 @@ void pcef_update_teids(
   request.set_agw_teid(agw_teid);
 
   magma::PCEFClient::update_teids(
-      request, [&](grpc::Status status,
-                   magma::UpdateTunnelIdsResponse response) { return; });
+      request,
+      [request](grpc::Status status, magma::UpdateTunnelIdsResponse response) {
+        if (status.error_code() == grpc::ABORTED) {
+          MessageDef* message_p = DEPRECATEDitti_alloc_new_message_fatal(
+              TASK_GRPC_SERVICE, GX_NW_INITIATED_DEACTIVATE_BEARER_REQ);
+          itti_gx_nw_init_deactv_bearer_request_t* itti_msg =
+              &message_p->ittiMsg.gx_nw_init_deactv_bearer_request;
+          std::string imsi = request.sid().id();
+          OAILOG_INFO(
+              LOG_UTIL,
+              "Received grpc::ABORTED for update_teids RPC for %s with error "
+              "msg: %s. Deactivating bearer %u.",
+              imsi.c_str(), status.error_message().c_str(),
+              request.bearer_id());
+          // strip off "IMSI" prefix
+          imsi                  = imsi.substr(4, std::string::npos);
+          itti_msg->imsi_length = imsi.size();
+          strcpy(itti_msg->imsi, imsi.c_str());
+          itti_msg->lbi           = request.bearer_id();
+          itti_msg->no_of_bearers = 1;
+          itti_msg->ebi[0]        = request.bearer_id();
+          send_msg_to_task(
+              &grpc_service_task_zmq_ctx, TASK_SPGW_APP, message_p);
+        }
+
+        return;
+      });
 }
 
 /*
