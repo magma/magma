771b64c6a4190c6365b599169cfe6db5cc867279
Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>
2021-11-12 18:20:06 +0100

    test(spgw): experimenting with different data sources
    Signed-off-by: Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>

--- lte/gateway/c/core/oai/test/spgw_task/mock_spgw_op.cpp
+++ lte/gateway/c/core/oai/test/spgw_task/mock_spgw_op.cpp
@@ -11,13 +11,14 @@ int mock_read_spgw_ue_state_db() {
   int number_of_samples = 1;
   for (int i = 0; i < number_of_samples; ++i) {
     oai::SpgwUeContext ue_proto = oai::SpgwUeContext();
-    std::string name_of_sample = "/home/vagrant/magma/lte/gateway/c/core/oai/test/spgw_task/data/sample.bin.v8";
+    std::string name_of_sample = "/home/vagrant/magma/lte/gateway/c/core/oai/test/spgw_task/data/sample.bin.v6";
     std::fstream input(name_of_sample.c_str(), std::ios::in | std::ios::binary);
     if (!ue_proto.ParseFromIstream(&input)) {
       std::cerr << "Failed to parse the sample: " << name_of_sample << std::endl;
       return -1;
     }
     
+  
   spgw_ue_context_t* ue_context_p =
       (spgw_ue_context_t*) calloc(1, sizeof(spgw_ue_context_t));
   SpgwStateConverter::proto_to_ue(ue_proto, ue_context_p);
--- lte/gateway/c/core/oai/test/spgw_task/test_spgw_procedures_with_injected_state.cpp
+++ lte/gateway/c/core/oai/test/spgw_task/test_spgw_procedures_with_injected_state.cpp
@@ -83,20 +83,20 @@ class SPGWAppInjectedStateProcedureTest : public ::testing::Test {
 
  protected:
   std::shared_ptr<MockMmeAppHandler> mme_app_handler;
-  std::string test_imsi_str          = "001010000000001";
-  unsigned long long int test_imsi64 = 1010000000001;
-  plmn_t test_plmn                   = {.mcc_digit2 = 5,
-                      .mcc_digit1 = 5,
-                      .mnc_digit3 = 0x06,
-                      .mcc_digit3 = 4,
-                      .mnc_digit2 = 7,
-                      .mnc_digit1 = 7};
+  std::string test_imsi_str          = "001010000000002";
+  unsigned long long int test_imsi64 = 1010000000002;
+  plmn_t test_plmn                   = {.mcc_digit2 = 0,
+                      .mcc_digit1 = 0,
+                      .mnc_digit3 = 0x00,
+                      .mcc_digit3 = 0,
+                      .mnc_digit2 = 0,
+                      .mnc_digit1 = 0};
   bearer_context_to_be_created_t sample_default_bearer_context = {
       .eps_bearer_id    = 5,
       .bearer_level_qos = {.pci = 1,
-                           .pl  = 3,
+                           .pl  = 15,
                           //  .pvi = 0,
-                           .qci = 5,
+                           .qci = 9,
                            .gbr = {},
                            }}; // .mbr = {} .br_ul = 200000000, .br_dl = 100000000
 };
@@ -106,7 +106,8 @@ TEST_F(SPGWAppInjectedStateProcedureTest, TestIPAllocFailure) {
 
   itti_s11_create_session_request_t sample_session_req_p = {};
   fill_create_session_request(
-      &sample_session_req_p, test_imsi_str, DEFAULT_BEARER_INDEX,
+      &sample_session_req_p, test_imsi_str, DEFAULT_MME_S11_TEID, 
+      DEFAULT_BEARER_INDEX,
       sample_default_bearer_context, test_plmn);
 
   // Verify that a UE context exists in SPGW state after CSR is received
@@ -140,60 +141,6 @@ TEST_F(SPGWAppInjectedStateProcedureTest, TestIPAllocFailure) {
   // check that IP address is not allocated
   ASSERT_TRUE(eps_bearer_ctxt_p->paa.ipv4_address.s_addr == UNASSIGNED_UE_IP);
 
-  // Sleep to ensure that messages are received and contexts are released
-  std::this_thread::sleep_for(std::chrono::milliseconds(END_OF_TEST_SLEEP_MS));
-}
-
-TEST_F(SPGWAppInjectedStateProcedureTest, TestCreateSessionRequestSuccess) {
-  spgw_state_t* spgw_state = get_spgw_state(false);
-  // expect call to MME create session response
-  itti_s11_create_session_request_t sample_session_req_p = {};
-  fill_create_session_request(
-      &sample_session_req_p, test_imsi_str, DEFAULT_BEARER_INDEX,
-      sample_default_bearer_context, test_plmn);
-
-  // Verify that a UE context exists in SPGW state after CSR is received
-  spgw_ue_context_t* ue_context_p = spgw_get_ue_context(test_imsi64);
-  ASSERT_TRUE(ue_context_p != nullptr);
-
-  // Verify that teid is created
-  ASSERT_FALSE(LIST_EMPTY(&ue_context_p->sgw_s11_teid_list));
-  teid_t ue_sgw_teid =
-      LIST_FIRST(&ue_context_p->sgw_s11_teid_list)->sgw_s11_teid;
-
-  // Verify that no IP address is allocated for this UE
-  s_plus_p_gw_eps_bearer_context_information_t* spgw_eps_bearer_ctxt_info_p =
-      sgw_cm_get_spgw_context(ue_sgw_teid);
-
-  sgw_eps_bearer_ctxt_t* eps_bearer_ctxt_p = sgw_cm_get_eps_bearer_entry(
-      &spgw_eps_bearer_ctxt_info_p->sgw_eps_bearer_context_information
-           .pdn_connection,
-      DEFAULT_EPS_BEARER_ID);
-
-  ASSERT_TRUE(eps_bearer_ctxt_p->paa.ipv4_address.s_addr == UNASSIGNED_UE_IP);
-
-  // send an IP alloc response to SPGW
-  itti_ip_allocation_response_t test_ip_alloc_resp = {};
-  fill_ip_allocation_response(
-      &test_ip_alloc_resp, SGI_STATUS_OK, ue_sgw_teid, DEFAULT_EPS_BEARER_ID,
-      DEFAULT_UE_IP, DEFAULT_VLAN);
-  status_code_e ip_alloc_rc = sgw_handle_ip_allocation_rsp(
-      spgw_state, &test_ip_alloc_resp, test_imsi64);
-
-  // check if IP address is allocated after this message is done
-  ASSERT_TRUE(eps_bearer_ctxt_p->paa.ipv4_address.s_addr == DEFAULT_UE_IP);
-
-  // send pcef create session response to SPGW
-  itti_pcef_create_session_response_t sample_pcef_csr_resp;
-  fill_pcef_create_session_response(
-      &sample_pcef_csr_resp, PCEF_STATUS_OK, ue_sgw_teid, DEFAULT_EPS_BEARER_ID,
-      SGI_STATUS_OK);
-
-  // check if MME gets a create session response
-  EXPECT_CALL(*mme_app_handler, mme_app_handle_create_sess_resp()).Times(1);
-
-  spgw_handle_pcef_create_session_response(
-      spgw_state, &sample_pcef_csr_resp, test_imsi64);
   // Sleep to ensure that messages are received and contexts are released
   std::this_thread::sleep_for(std::chrono::milliseconds(END_OF_TEST_SLEEP_MS));
 }
