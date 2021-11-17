0cca2ce04921a7a7a19178cdd5a745e76cfaacf4
milap joshi <milap.joshi@gmail.com>
2021-11-17 00:28:25 +0000

    python format error resolved
    Signed-off-by: milap joshi <milap.joshi@gmail.com>

--- ci-scripts/test_agent/Tester.py
+++ ci-scripts/test_agent/Tester.py
@@ -10,9 +10,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import threading
-import subprocess
 import pickle
+import subprocess
+import threading
 
 
 class TesterState:
