5d2ab7d2eaecc9da9f6c0b206b88a27136f387a0
RahulKalsangra <rahul.kalsangra@wavelabs.ai>
2021-11-17 20:58:51 +0530

    feat(amf): agw changes to accept amf configs
    Signed-off-by: RahulKalsangra <rahul.kalsangra@wavelabs.ai>

--- lte/gateway/python/scripts/generate_oai_config.py
+++ lte/gateway/python/scripts/generate_oai_config.py
@@ -287,8 +287,7 @@ def _get_default_slice_service_type_config(service_mconfig: object) -> str:
 
     if enable_default_service_slice_type is not None:
         if isinstance(enable_default_service_slice_type, int):
-            return str(enable_default_service_slice_type)
-        
+            return str(enable_default_service_slice_type)       
         return enable_default_service_slice_type
 
     return service_mconfig.default_slice_service_type or DEFAULT_NGAP_S_NSSAI_SST
