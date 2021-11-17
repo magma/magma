a1e319ece2e2a013c4aa8ccbb323efaf40c4be7f
milap joshi <milap.joshi@gmail.com>
2021-11-17 17:56:14 +0000

    pep8 first line error
    Signed-off-by: milap joshi <milap.joshi@gmail.com>

--- ci-scripts/test_agent/main.py
+++ ci-scripts/test_agent/main.py
@@ -36,10 +36,10 @@ def test_done_callback(tester_id, workload, verdict, report):
 
 
 def do_token_refresh():
-    """
-    This function is used to run in its own thread and
+    """This function is used to run in its own thread and
     refresh token every 30 min
     """
+
     print("Refreshing Firebase Token:", datetime.now())
     db_client.user = db_client.auth.refresh(db_client.user["refreshToken"])
     threading.Timer(RefreshTokenTime, do_token_refresh).start()
