magma/magma

commit 2e3ac4b8a2cdef374f34ffdd3bcfbc6704c4f928

fix(feg): Incorrect conversion between integer types (#10278)
* Added check if variable is within the allowable bounds

Signed-off-by: Kristijan <spikey979@gmail.com>

* Added check if variable is in bounds of Unsigned32 type (0-4294967295)

Signed-off-by: Kristijan <spikey979@gmail.com>

feg/gateway/tools/swx_cli/main.go
@@ -235,6 +235,10 @@ func sendSar(addr string, client swxClient) int {
 }
 
 func sendMar(addr string, client swxClient) int {
+	if numVectors < 0 || numVectors > 4294967295 {
+		fmt.Printf("numVectors %d is outside the bounds of unit32 type", numVectors)
+		return 2
+	}
 	req := &protos.AuthenticationRequest{
 		UserName:             imsi,
 		SipNumAuthVectors:    uint32(numVectors),

feg/gateway/tools/hello_cli/main.go
@@ -45,6 +45,9 @@ func main() {
 	defer conn.Close()
 	cl := protos.NewHelloClient(conn)
 	fmt.Printf("Sending  Greeting: '%s', Code: %d\n", msg, code)
+	if code < 0 || code > 4294967295 {
+		log.Fatal(fmt.Errorf("Code %d is outside the bounds of unit32 type", code))
+	}
 	res, err := cl.SayHello(context.Background(), &protos.HelloRequest{Greeting: msg, GrpcErrCode: uint32(code)})
 	if err != nil {
 		log.Fatal(err)

feg/gateway/tools/s6a_cli/main.go
@@ -321,6 +321,9 @@ func getPlmnID(imsi string, mncLen int) ([3]byte, error) {
 		if err != nil {
 			return [3]byte{}, fmt.Errorf("Invalid Digit '%s' in IMSI '%s': %v", imsi[i:i+1], imsi, err)
 		}
+		if v < 0 || v > 255 {
+			return [3]byte{}, fmt.Errorf("Number %d is outside the boundaries of byte type", v)
+		}
 		imsiBytes[i] = byte(v)
 	}
 	// see https://www.arib.or.jp/english/html/overview/doc/STD-T63v10_70/5_Appendix/Rel11/29/29272-bb0.pdf#page=73

commit f0b36084a5185ab4c07cb9f23116691aff17beb6

fix(feg): address linting error on numVectors size comparison (#10416)
Fixes `feg-workflow` CI on master which presently blocks new PRs. Regression introduced in #10278, in which for some reason `feg-workflow` was not executed by CI.

Linting notes that `uint64` cannot be negative so the first half of this if condition is a no-op.

While we are here, use Golang built-in for maximum `uint32` size comparison instead of magic value.

Signed-off-by: Scott Moeller <electronjoe@gmail.com>

feg/gateway/tools/swx_cli/main.go
@@ -17,6 +17,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -235,7 +236,7 @@ func sendSar(addr string, client swxClient) int {
 }
 
 func sendMar(addr string, client swxClient) int {
-	if numVectors < 0 || numVectors > 4294967295 {
+	if numVectors > math.MaxUint32 {
 		fmt.Printf("numVectors %d is outside the bounds of unit32 type", numVectors)
 		return 2
 	}