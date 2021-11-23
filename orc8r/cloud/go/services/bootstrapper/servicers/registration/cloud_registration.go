669c2ce93420d3be340c500b40c62180b03e2be1
Regina Wang <rwang3495@gmail.com>
2021-11-23 07:25:36 -0800

    address code review
    Signed-off-by: Regina Wang <rwang3495@gmail.com>

--- orc8r/cloud/go/services/bootstrapper/servicers/registration/cloud_registration.go
+++ orc8r/cloud/go/services/bootstrapper/servicers/registration/cloud_registration.go
@@ -26,13 +26,18 @@ const (
 
 type cloudRegistrationServicer struct {
 	store Store
+	rootCA string
 }
 
 func NewCloudRegistrationServicer(store Store) (protos.CloudRegistrationServer, error) {
 	if store == nil {
-		return nil, fmt.Errorf("Storage store is nil")
+		return nil, fmt.Errorf("storage store is nil")
 	}
-	return &cloudRegistrationServicer{store}, nil
+	rootCA, err := getRootCA()
+	if err != nil {
+		return nil, err
+	}
+	return &cloudRegistrationServicer{store, rootCA}, nil
 }
 
 func (crs *cloudRegistrationServicer) GetToken(c context.Context, request *protos.GetTokenRequest) (*protos.GetTokenResponse, error) {
@@ -41,7 +46,9 @@ func (crs *cloudRegistrationServicer) GetToken(c context.Context, request *proto
 
 	tokenInfo, err := crs.store.GetTokenInfoFromLogicalID(networkId, logicalId)
 	if err != nil {
-		glog.Infof("Could not get tokenInfo for networkID %v and logicalID %v: %v", networkId, logicalId, err)
+		// error is not bubbled up since the tokenInfo is only important if the token exists and is expired
+		// if GetTokenInfoFromLogicalID fails, we can just ignore and continue
+		glog.V(2).Infof("could not get tokenInfo for networkID %v and logicalID %v: %v", networkId, logicalId, err)
 	}
 
 	refresh := request.Refresh || tokenInfo == nil || isTokenExpired(tokenInfo)
@@ -49,24 +56,22 @@ func (crs *cloudRegistrationServicer) GetToken(c context.Context, request *proto
 		// TODO(reginawang3495) add a test with tokenInfo = nil to make sure it works
 		tokenInfo, err = crs.generateAndSaveTokenInfo(networkId, logicalId)
 		if err != nil {
-			return nil, status.Errorf(codes.Internal, "Could not generate and save tokenInfo for networkID %v and logicalID %v: %v", networkId, logicalId, err)
+			return nil, status.Errorf(codes.Internal, "could not generate and save tokenInfo for networkID %v and logicalID %v: %v", networkId, logicalId, err)
 		}
 	}
 
-	return &protos.GetTokenResponse{Timeout: tokenInfo.Timeout, Token: nonceToToken(tokenInfo.Nonce)}, nil
+	res := &protos.GetTokenResponse{Timeout: tokenInfo.Timeout, Token: nonceToToken(tokenInfo.Nonce)}
+	return res, nil
 }
 
 func (crs *cloudRegistrationServicer) GetGatewayRegistrationInfo(c context.Context, request *protos.GetGatewayRegistrationInfoRequest) (*protos.GetGatewayRegistrationInfoResponse, error) {
-	rootCA, err := getRootCA()
-	if err != nil {
-		return nil, err
-	}
 	domainName := getDomainName()
 
-	return &protos.GetGatewayRegistrationInfoResponse{
-		RootCa:               rootCA,
+	res := &protos.GetGatewayRegistrationInfoResponse{
+		RootCa:               crs.rootCA,
 		DomainName:           domainName,
-	}, nil
+	}
+	return res, nil
 }
 
 func (crs *cloudRegistrationServicer) GetGatewayDeviceInfo(c context.Context, request *protos.GetGatewayDeviceInfoRequest) (*protos.GetGatewayDeviceInfoResponse, error) {
@@ -74,48 +79,48 @@ func (crs *cloudRegistrationServicer) GetGatewayDeviceInfo(c context.Context, re
 
 	tokenInfo, err := crs.store.GetTokenInfoFromNonce(nonce)
 	if err != nil {
-		return &protos.GetGatewayDeviceInfoResponse{
+		res := &protos.GetGatewayDeviceInfoResponse{
 			Response: &protos.GetGatewayDeviceInfoResponse_Error{
-				Error: fmt.Sprintf("Could not get token info from nonce %v: %v", nonce, err),
+				Error: fmt.Sprintf("could not get token info from nonce %v: %v", nonce, err),
 			},
-		}, nil
+		}
+		return res, nil
 	}
 
-	return &protos.GetGatewayDeviceInfoResponse{
+	res := &protos.GetGatewayDeviceInfoResponse{
 		Response:             &protos.GetGatewayDeviceInfoResponse_GatewayDeviceInfo{
 			GatewayDeviceInfo: &protos.GatewayDeviceInfo{
 				NetworkId:           tokenInfo.GatewayDeviceInfo.NetworkId,
 				LogicalId:            tokenInfo.GatewayDeviceInfo.LogicalId,
 			},
 		},
-	}, nil
+	}
+	return res, nil
 }
 
 func (crs *cloudRegistrationServicer) generateAndSaveTokenInfo(networkID string, logicalID string) (*protos.TokenInfo, error) {
-	nonce := generateSecureNonce(nonceLength)
+	nonce := generateNonce(nonceLength)
 
 	t := time.Now().Add(timeoutDuration)
-	timeout :=
-		&timestamp.Timestamp{
-			Seconds: int64(t.Second()),
-			Nanos:   int32(t.Unix()),
-		}
 
-	tokenInfo := protos.TokenInfo{
+	tokenInfo := &protos.TokenInfo{
 		GatewayDeviceInfo: &protos.GatewayDeviceInfo{
 			NetworkId: networkID,
 			LogicalId: logicalID,
 		},
 		Nonce:   nonce,
-		Timeout: timeout,
+		Timeout: &timestamp.Timestamp{
+			Seconds: int64(t.Second()),
+			Nanos:   int32(t.Unix()),
+		},
 	}
 
-	err := crs.store.SetTokenInfo(tokenInfo)
+	err := crs.store.SetTokenInfo(*tokenInfo)
 	if err != nil {
 		return nil, err
 	}
 
-	return &tokenInfo, nil
+	return tokenInfo, nil
 }
 
 func getRootCA() (string, error) {
@@ -128,5 +133,5 @@ func getRootCA() (string, error) {
 
 // TODO(#10437)
 func getDomainName() string {
-	return "localhost"
+	return "warning: not implemented"
 }
--- orc8r/cloud/go/services/bootstrapper/servicers/registration/helpers.go
+++ orc8r/cloud/go/services/bootstrapper/servicers/registration/helpers.go
@@ -3,7 +3,6 @@ package registration
 import (
 	"math/rand"
 	"time"
-	"unsafe"
 
 	"magma/orc8r/cloud/go/services/bootstrapper"
 	"magma/orc8r/lib/go/protos"
@@ -23,29 +22,12 @@ func isTokenExpired(info *protos.TokenInfo) bool {
 
 // ========================================================================= //
 // Sourced from https://stackoverflow.com/a/31832326
-const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func generateSecureNonce(length int) string {
-	src := rand.NewSource(time.Now().UnixNano())
-
-	b := make([]byte, length)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := length-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
+func generateNonce(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
-
-	return *(*string)(unsafe.Pointer(&b))
+	return string(b)
 }
