c5d0c30951563151f4e5b0351d96dadc2351a3a8
Felix Tian <fytian@fb.com>
2021-11-23 18:29:36 +0000

    add destructor to ProxyConnector
    Signed-off-by: Felix Tian <fytian@fb.com>

--- lte/gateway/c/li_agent/src/main.cpp
+++ lte/gateway/c/li_agent/src/main.cpp
@@ -106,17 +106,14 @@ int main(void) {
       interface_name, std::move(pkt_generator));
   if (interface_watcher->init_interface_monitor() < 0) {
     MLOG(MERROR) << "Coudn't setup interface sniffing, terminating";
-    proxy_connector->cleanup();
     return -1;
   }
 
   if (interface_watcher->start_capture() < 0) {
     MLOG(MERROR) << "Coudn't start interface sniffing, terminating";
-    proxy_connector->cleanup();
     return -1;
   }
 
   MLOG(MERROR) << "CleanUP " << reinterpret_cast<void*>(&pcap_dispatch);
-  proxy_connector->cleanup();
   return 0;
 }
