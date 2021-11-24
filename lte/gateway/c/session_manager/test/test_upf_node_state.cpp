f8d4a853a7157d783dea3e6ab47aeb0f565f3f2c
prabinakpattnaik <69528542+prabinakpattnaik@users.noreply.github.com>
2021-11-25 00:20:14 +0530

    feat(sessiond): Support usage reporting and charging for 5G (#10239)
    Co-authored-by: sreedharkumartn <sreedhar.kumar@wavelabs.ai>

--- lte/gateway/c/session_manager/test/test_upf_node_state.cpp
+++ lte/gateway/c/session_manager/test/test_upf_node_state.cpp
@@ -42,11 +42,13 @@ class SetUPFNodeState : public ::testing::Test {
  public:
   virtual void SetUp() {
     rule_store    = std::make_shared<StaticRuleStore>();
+    reporter      = std::make_shared<MockSessionReporter>();
     session_store = std::make_shared<SessionStore>(
         rule_store, std::make_shared<MeteringReporter>());
     std::unordered_multimap<std::string, uint32_t> pdr_map;
     auto pipelined_client = std::make_shared<magma::AsyncPipelinedClient>();
     amf_srv_client        = std::make_shared<magma::AsyncAmfServiceClient>();
+    events_reporter       = std::make_shared<MockEventsReporter>();
 
     magma::mconfig::SessionD mconfig;
     mconfig.set_log_level(magma::orc8r::LogLevel::INFO);
@@ -56,7 +58,8 @@ class SetUPFNodeState : public ::testing::Test {
 
     session_enforcer = std::make_shared<magma::SessionStateEnforcer>(
         rule_store, *session_store, pdr_map, pipelined_client, amf_srv_client,
-        mconfig, config["session_force_termination_timeout_ms"].as<int32_t>(),
+        reporter.get(), events_reporter, mconfig,
+        config["session_force_termination_timeout_ms"].as<int32_t>(),
         session_max_rtx_count);
 
     set_interface_for_up_mock =
@@ -73,6 +76,8 @@ class SetUPFNodeState : public ::testing::Test {
   std::shared_ptr<AsyncAmfServiceClient> amf_srv_client;
   std::shared_ptr<MockSetInterfaceForUserPlane> set_interface_for_up_mock;
   std::shared_ptr<UPFSessionConfigState> sess_config;
+  std::shared_ptr<MockSessionReporter> reporter;
+  std::shared_ptr<MockEventsReporter> events_reporter;
 };  // End of class
 
 // Testing the functionality of SetUPFNodeState
