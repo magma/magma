68ba6c827d3a48b6b5f49c40d4e60f8915ab6e62
Lucas Gonze <lucas@gonze.com>
2021-12-03 15:31:40 -0800

    chore: postcss update to 7.0.36 or later
    Fixes https://github.com/ospoco/magma-issue-tracker/issues/17
    
    Signed-off-by: Lucas Gonze <lucas@gonze.com>

--- nms/packages/magmalte/config/webpack.config.js
+++ nms/packages/magmalte/config/webpack.config.js
@@ -97,7 +97,9 @@ module.exports = {
                   // Necessary for external CSS imports to work
                   // https://github.com/facebookincubator/create-react-app/issues/2677
                   ident: 'postcss',
-                  plugins: () => [require('postcss-flexbugs-fixes')],
+                  postcssOptions: {
+                    plugins: () => [require('postcss-flexbugs-fixes')]
+                  },
                 },
               },
             ],
