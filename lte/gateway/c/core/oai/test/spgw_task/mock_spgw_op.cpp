f33f0a7a420512dee04ae2b24c8c1678edcb7f41
Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>
2022-01-11 16:16:31 +0100

    test(mme): change name of some variables to meaningful names
    Signed-off-by: Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>

--- lte/gateway/c/core/oai/test/spgw_task/mock_spgw_op.cpp
+++ lte/gateway/c/core/oai/test/spgw_task/mock_spgw_op.cpp
@@ -24,30 +24,30 @@ namespace lte {
 std::vector<std::string> load_file_into_vector_of_line_content(
     const std::string& data_folder_path, const std::string& file_name) {
   std::fstream file_content(file_name.c_str(), std::ios_base::in);
-  std::string line;
-  std::vector<std::string> vector_of_lines;
+  std::string data_file_name;
+  std::vector<std::string> vector_of_file_names;
   if (file_content) {
-    while (std::getline(file_content, line)) {
-      line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
-      vector_of_lines.push_back(data_folder_path + "/" + line);
+    while (std::getline(file_content, data_file_name)) {
+      data_file_name.erase(std::remove(data_file_name.begin(), data_file_name.end(), '\n'), data_file_name.end());
+      vector_of_file_names.push_back(data_folder_path + "/" + data_file_name);
     }
   } else {
     std::cerr << "couldn't open file: " << file_name << std::endl;
   }
 
   file_content.close();
-  return vector_of_lines;
+  return vector_of_file_names;
 }
 
 // mocking the reading spgw ue states from redis database by injecting local
 // samples
 status_code_e mock_read_spgw_ue_state_db(
     const std::vector<std::string>& ue_samples) {
-  for (const auto& name_of_sample : ue_samples) {
+  for (const auto& name_of_sample_file : ue_samples) {
     oai::SpgwUeContext ue_proto = oai::SpgwUeContext();
-    std::fstream input(name_of_sample.c_str(), std::ios::in | std::ios::binary);
+    std::fstream input(name_of_sample_file.c_str(), std::ios::in | std::ios::binary);
     if (!ue_proto.ParseFromIstream(&input)) {
-      std::cerr << "Failed to parse the sample: " << name_of_sample
+      std::cerr << "Failed to parse the sample: " << name_of_sample_file
                 << std::endl;
       return RETURNerror;
     }
