56a3cde55b372385c67a677fdc0018661578a0dc
Kristijan <spikey979@gmail.com>
2022-01-14 12:45:43 +0100

    chore(lte): Upgrade aiohttp from 3.6.2 to 3.8.0
    Signed-off-by: Kristijan <spikey979@gmail.com>

--- lte/gateway/python/setup.py
+++ lte/gateway/python/setup.py
@@ -125,7 +125,7 @@ setup(
         'freezegun>=0.3.15',
         'pycryptodome>=3.9.9',
         'pyroute2==0.5.14',
-        'aiohttp==3.6.2',
+        'aiohttp==3.8.0',
         'jsonpointer>=1.14',
         'ovs>=2.13',
         'prometheus-client>=0.3.1',
