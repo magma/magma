nikhilc149/magma_v1.7

feat(amf): Serialization and Deserialization of Smf Context
Signed-off-by: shashidhar-patil <shashidhar.patil@wavelabs.ai>

lte/gateway/c/core/oai/tasks/amf/amf_app_state_converter.cpp
@@ -208,4 +208,296 @@ void AmfNasStateConverter::proto_to_ue_m5gmm_context(
   // Actual implementation logic will be added as part of upcoming pr
 }
 
+void AmfNasStateConverter::smf_proc_data_to_proto(
+    const smf_proc_data_t* state_smf_proc_data,
+    magma::lte::oai::Smf_Proc_Data* smf_proc_data_proto) {
+  smf_proc_data_proto->set_pdu_session_id(state_smf_proc_data->pdu_session_id);
+  smf_proc_data_proto->set_pti(state_smf_proc_data->pti);
+  smf_proc_data_proto->set_message_type(state_smf_proc_data->message_type);
+  smf_proc_data_proto->set_max_uplink(state_smf_proc_data->max_uplink);
+  smf_proc_data_proto->set_max_downlink(state_smf_proc_data->max_downlink);
+  smf_proc_data_proto->set_pdu_session_type(
+      state_smf_proc_data->pdu_session_type);
+  smf_proc_data_proto->set_ssc_mode(state_smf_proc_data->ssc_mode);
+}
+void AmfNasStateConverter::proto_to_smf_proc_data(
+    const magma::lte::oai::Smf_Proc_Data& smf_proc_data_proto,
+    smf_proc_data_t* state_smf_proc_data) {
+  state_smf_proc_data->pdu_session_id = smf_proc_data_proto.pdu_session_id();
+  state_smf_proc_data->pti            = smf_proc_data_proto.pti();
+  state_smf_proc_data->message_type   = smf_proc_data_proto.message_type();
+  state_smf_proc_data->max_uplink     = smf_proc_data_proto.max_uplink();
+  state_smf_proc_data->max_downlink   = smf_proc_data_proto.max_downlink();
+  state_smf_proc_data->pdu_session_type =
+      smf_proc_data_proto.pdu_session_type();
+  state_smf_proc_data->ssc_mode = smf_proc_data_proto.ssc_mode();
+}
+
+void AmfNasStateConverter::s_nssai_to_proto(
+    const s_nssai_t* state_s_nssai, magma::lte::oai::SNssai* snassi_proto) {
+  snassi_proto->set_sst(state_s_nssai->sst);
+  snassi_proto->set_sd(*(uint32_t*) &state_s_nssai->sd);
+}
+void AmfNasStateConverter::proto_to_s_nssai(
+    const magma::lte::oai::SNssai& snassi_proto, s_nssai_t* state_s_nssai) {
+  state_s_nssai->sst              = snassi_proto.sst();
+  *(uint32_t*) &state_s_nssai->sd = snassi_proto.sd();
+}
+
+void AmfNasStateConverter::pco_protocol_or_container_id_to_proto(
+    const protocol_configuration_options_t&
+        state_protocol_configuration_options,
+    magma::lte::oai::ProtocolConfigurationOptions*
+        protocol_configuration_options_proto) {
+  for (int i = 0;
+       i < state_protocol_configuration_options.num_protocol_or_container_id;
+       i++) {
+    pco_protocol_or_container_id_t state_pco_protocol_or_container_id =
+        state_protocol_configuration_options.protocol_or_container_ids[i];
+    auto pco_protocol_or_container_id_proto =
+        protocol_configuration_options_proto->add_proto_or_container_id();
+    pco_protocol_or_container_id_proto->set_id(
+        state_pco_protocol_or_container_id.id);
+    pco_protocol_or_container_id_proto->set_length(
+        state_pco_protocol_or_container_id.length);
+    if (state_pco_protocol_or_container_id.contents) {
+      BSTRING_TO_STRING(
+          state_pco_protocol_or_container_id.contents,
+          pco_protocol_or_container_id_proto->mutable_contents());
+    }
+  }
+}
+
+void AmfNasStateConverter::proto_to_pco_protocol_or_container_id(
+    const magma::lte::oai::ProtocolConfigurationOptions&
+        protocol_configuration_options_proto,
+    protocol_configuration_options_t* state_protocol_configuration_options) {
+  auto proto_pco_ids =
+      protocol_configuration_options_proto.proto_or_container_id();
+  int i = 0;
+  for (auto ptr = proto_pco_ids.begin(); ptr < proto_pco_ids.end(); ptr++) {
+    pco_protocol_or_container_id_t* state_pco_protocol_or_container_id =
+        &state_protocol_configuration_options->protocol_or_container_ids[i];
+    state_pco_protocol_or_container_id->id     = ptr->id();
+    state_pco_protocol_or_container_id->length = ptr->length();
+    if (ptr->contents().length()) {
+      state_pco_protocol_or_container_id->contents = bfromcstr_with_str_len(
+          ptr->contents().c_str(), ptr->contents().length());
+    }
+    i++;
+  }
+}
+
+void AmfNasStateConverter::protocol_configuration_options_to_proto(
+    const protocol_configuration_options_t&
+        state_protocol_configuration_options,
+    magma::lte::oai::ProtocolConfigurationOptions*
+        protocol_configuration_options_proto) {
+  protocol_configuration_options_proto->set_ext(
+      state_protocol_configuration_options.ext);
+  protocol_configuration_options_proto->set_spare(
+      state_protocol_configuration_options.spare);
+  protocol_configuration_options_proto->set_config_protocol(
+      state_protocol_configuration_options.configuration_protocol);
+  protocol_configuration_options_proto->set_num_protocol_or_container_id(
+      state_protocol_configuration_options.num_protocol_or_container_id);
+
+  AmfNasStateConverter::pco_protocol_or_container_id_to_proto(
+      state_protocol_configuration_options,
+      protocol_configuration_options_proto);
+}
+
+void AmfNasStateConverter::proto_to_protocol_configuration_options(
+    const magma::lte::oai::ProtocolConfigurationOptions&
+        protocol_configuration_options_proto,
+    protocol_configuration_options_t* state_protocol_configuration_options) {
+  state_protocol_configuration_options->ext =
+      protocol_configuration_options_proto.ext();
+  state_protocol_configuration_options->spare =
+      protocol_configuration_options_proto.spare();
+  state_protocol_configuration_options->configuration_protocol =
+      protocol_configuration_options_proto.config_protocol();
+  state_protocol_configuration_options->num_protocol_or_container_id =
+      protocol_configuration_options_proto.num_protocol_or_container_id();
+  AmfNasStateConverter::proto_to_pco_protocol_or_container_id(
+      protocol_configuration_options_proto,
+      state_protocol_configuration_options);
+}
+
+void AmfNasStateConverter::session_ambr_to_proto(
+    const session_ambr_t& state_session_ambr,
+    magma::lte::oai::Ambr* ambr_proto) {
+  ambr_proto->set_br_ul(state_session_ambr.ul_session_ambr);
+  ambr_proto->set_br_dl(state_session_ambr.dl_session_ambr);
+  ambr_proto->set_br_unit(static_cast<magma::lte::oai::Ambr::BitrateUnitsAMBR>(
+      state_session_ambr.dl_ambr_unit));
+}
+void AmfNasStateConverter::proto_to_session_ambr(
+    const magma::lte::oai::Ambr& ambr_proto,
+    session_ambr_t* state_session_ambr) {
+  state_session_ambr->dl_ambr_unit    = ambr_proto.br_unit();
+  state_session_ambr->dl_session_ambr = ambr_proto.br_dl();
+  state_session_ambr->ul_ambr_unit    = ambr_proto.br_unit();
+  state_session_ambr->ul_session_ambr = ambr_proto.br_ul();
+}
+
+void AmfNasStateConverter::qos_flow_level_parameters_to_proto(
+    const qos_flow_level_qos_parameters& state_qos_flow_parameters,
+    magma::lte::oai::QosFlowParameters* qos_flow_parameters_proto) {
+  qos_flow_parameters_proto->set_fiveqi(
+      state_qos_flow_parameters.qos_characteristic.non_dynamic_5QI_desc.fiveQI);
+  qos_flow_parameters_proto->set_priority_level(
+      state_qos_flow_parameters.alloc_reten_priority.priority_level);
+  qos_flow_parameters_proto->set_preemption_vulnerability(
+      state_qos_flow_parameters.alloc_reten_priority.pre_emption_vul);
+  qos_flow_parameters_proto->set_preemption_capability(
+      state_qos_flow_parameters.alloc_reten_priority.pre_emption_cap);
+}
+
+void AmfNasStateConverter::proto_to_qos_flow_level_parameters(
+    const magma::lte::oai::QosFlowParameters& qos_flow_parameters_proto,
+    qos_flow_level_qos_parameters* state_qos_flow_parameters) {
+  state_qos_flow_parameters->qos_characteristic.non_dynamic_5QI_desc.fiveQI =
+      qos_flow_parameters_proto.fiveqi();
+  state_qos_flow_parameters->alloc_reten_priority.priority_level =
+      qos_flow_parameters_proto.priority_level();
+  state_qos_flow_parameters->alloc_reten_priority.pre_emption_vul =
+      static_cast<pre_emption_vulnerability>(
+          qos_flow_parameters_proto.preemption_vulnerability());
+  state_qos_flow_parameters->alloc_reten_priority.pre_emption_cap =
+      static_cast<pre_emption_capability>(
+          qos_flow_parameters_proto.preemption_capability());
+}
+
+void AmfNasStateConverter::qos_flow_setup_request_item_to_proto(
+    const qos_flow_setup_request_item& state_qos_flow_request_item,
+    magma::lte::oai::M5GQosFlowItem* qos_flow_item_proto) {
+  qos_flow_item_proto->set_qfi(state_qos_flow_request_item.qos_flow_identifier);
+  AmfNasStateConverter::qos_flow_level_parameters_to_proto(
+      state_qos_flow_request_item.qos_flow_level_qos_param,
+      qos_flow_item_proto->mutable_qos_flow_param());
+}
+
+void AmfNasStateConverter::proto_to_qos_flow_setup_request_item(
+    const magma::lte::oai::M5GQosFlowItem& qos_flow_item_proto,
+    qos_flow_setup_request_item* state_qos_flow_request_item) {
+  state_qos_flow_request_item->qos_flow_identifier = qos_flow_item_proto.qfi();
+  AmfNasStateConverter::proto_to_qos_flow_level_parameters(
+      qos_flow_item_proto.qos_flow_param(),
+      &state_qos_flow_request_item->qos_flow_level_qos_param);
+}
+// smf_context to proto and proto to smf_context
+void AmfNasStateConverter::smf_context_to_proto(
+    const smf_context_t* state_smf_context,
+    magma::lte::oai::SmfContext* smf_context_proto) {
+  smf_context_proto->set_sm_session_state(state_smf_context->pdu_session_state);
+  smf_context_proto->set_pdu_session_version(
+      state_smf_context->pdu_session_version);
+  smf_context_proto->set_active_pdu_sessions(state_smf_context->n_active_pdus);
+  smf_context_proto->set_is_emergency(state_smf_context->is_emergency);
+  AmfNasStateConverter::session_ambr_to_proto(
+      state_smf_context->selected_ambr,
+      smf_context_proto->mutable_selected_ambr());
+
+  smf_context_proto->set_gnb_gtp_teid(
+      state_smf_context->gtp_tunnel_id.gnb_gtp_teid);
+
+  char gnb_gtp_teid_ip_addr_str[16] = {0};
+  inet_ntop(
+      AF_INET, state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr,
+      gnb_gtp_teid_ip_addr_str, INET_ADDRSTRLEN);
+  smf_context_proto->set_gnb_gtp_teid_ip_addr(gnb_gtp_teid_ip_addr_str);
+
+  smf_context_proto->set_upf_gtp_teid(
+      *(uint32_t*) &state_smf_context->gtp_tunnel_id.upf_gtp_teid);
+
+  char upf_gtp_teid_ip_addr_str[16] = {0};
+  inet_ntop(
+      AF_INET, state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr,
+      upf_gtp_teid_ip_addr_str, INET_ADDRSTRLEN);
+  smf_context_proto->set_upf_gtp_teid_ip_addr(upf_gtp_teid_ip_addr_str);
+
+  bstring bstr_buffer = paa_to_bstring(&state_smf_context->pdu_address);
+  BSTRING_TO_STRING(bstr_buffer, smf_context_proto->mutable_paa());
+  bdestroy(bstr_buffer);
+
+  StateConverter::ambr_to_proto(
+      state_smf_context->apn_ambr, smf_context_proto->mutable_apn_ambr());
+
+  AmfNasStateConverter::smf_proc_data_to_proto(
+      &state_smf_context->smf_proc_data,
+      smf_context_proto->mutable_smf_proc_data());
+  smf_context_proto->set_retransmission_count(
+      state_smf_context->retransmission_count);
+  AmfNasStateConverter::protocol_configuration_options_to_proto(
+      state_smf_context->pco, smf_context_proto->mutable_pco());
+  smf_context_proto->set_dnn_in_use(state_smf_context->dnn);
+
+  AmfNasStateConverter::s_nssai_to_proto(
+      &state_smf_context->requested_nssai,
+      smf_context_proto->mutable_requested_nssai());
+
+  AmfNasStateConverter::qos_flow_setup_request_item_to_proto(
+      state_smf_context->subscribebed_qos_profile.qos_flow_req_item,
+      smf_context_proto->mutable_qos_flow_list());
+}
+
+void AmfNasStateConverter::proto_to_smf_context(
+    const magma::lte::oai::SmfContext& smf_context_proto,
+    smf_context_t* state_smf_context) {
+  state_smf_context->pdu_session_state =
+      (SMSessionFSMState) smf_context_proto.sm_session_state();
+  state_smf_context->pdu_session_version =
+      smf_context_proto.pdu_session_version();
+  state_smf_context->n_active_pdus = smf_context_proto.active_pdu_sessions();
+  state_smf_context->is_emergency  = smf_context_proto.is_emergency();
+  AmfNasStateConverter::proto_to_session_ambr(
+      smf_context_proto.selected_ambr(), &state_smf_context->selected_ambr);
+  state_smf_context->gtp_tunnel_id.gnb_gtp_teid =
+      smf_context_proto.gnb_gtp_teid();
+
+  memset(
+      &state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr, '\0',
+      sizeof(state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr));
+  inet_pton(
+      AF_INET, smf_context_proto.gnb_gtp_teid_ip_addr().c_str(),
+      &(state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr));
+
+  *(uint32_t*) &state_smf_context->gtp_tunnel_id.upf_gtp_teid =
+      smf_context_proto.upf_gtp_teid();
+
+  memset(
+      &state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr, '\0',
+      sizeof(state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr));
+  inet_pton(
+      AF_INET, smf_context_proto.upf_gtp_teid_ip_addr().c_str(),
+      &(state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr));
+
+  bstring bstr_buffer;
+  STRING_TO_BSTRING(smf_context_proto.paa(), bstr_buffer);
+  bstring_to_paa(bstr_buffer, &state_smf_context->pdu_address);
+  bdestroy(bstr_buffer);
+
+  StateConverter::proto_to_ambr(
+      smf_context_proto.apn_ambr(), &state_smf_context->apn_ambr);
+
+  AmfNasStateConverter::proto_to_smf_proc_data(
+      smf_context_proto.smf_proc_data(), &state_smf_context->smf_proc_data);
+
+  state_smf_context->retransmission_count =
+      smf_context_proto.retransmission_count();
+
+  AmfNasStateConverter::proto_to_protocol_configuration_options(
+      smf_context_proto.pco(), &state_smf_context->pco);
+
+  state_smf_context->dnn = smf_context_proto.dnn_in_use();
+
+  AmfNasStateConverter::proto_to_s_nssai(
+      smf_context_proto.requested_nssai(), &state_smf_context->requested_nssai);
+
+  AmfNasStateConverter::proto_to_qos_flow_setup_request_item(
+      smf_context_proto.qos_flow_list(),
+      &state_smf_context->subscribebed_qos_profile.qos_flow_req_item);
+}
+
 }  // namespace magma5g

magma/magma

lte/gateway/c/core/oai/tasks/amf/amf_app_state_converter.cpp
@@ -437,4 +437,301 @@ void AmfNasStateConverter::proto_to_amf_security_context(
       emm_security_context_proto.direction_decode();
 }
 
+void AmfNasStateConverter::smf_proc_data_to_proto(
+    const smf_proc_data_t* state_smf_proc_data,
+    magma::lte::oai::Smf_Proc_Data* smf_proc_data_proto) {
+  smf_proc_data_proto->set_pdu_session_id(state_smf_proc_data->pdu_session_id);
+  smf_proc_data_proto->set_pti(state_smf_proc_data->pti);
+  smf_proc_data_proto->set_message_type(
+      static_cast<uint32_t>(state_smf_proc_data->message_type));
+  smf_proc_data_proto->set_max_uplink(state_smf_proc_data->max_uplink);
+  smf_proc_data_proto->set_max_downlink(state_smf_proc_data->max_downlink);
+  smf_proc_data_proto->set_pdu_session_type(
+      (magma::lte::oai::M5GPduSessionType)
+          state_smf_proc_data->pdu_session_type);
+  smf_proc_data_proto->set_ssc_mode(state_smf_proc_data->ssc_mode);
+}
+void AmfNasStateConverter::proto_to_smf_proc_data(
+    const magma::lte::oai::Smf_Proc_Data& smf_proc_data_proto,
+    smf_proc_data_t* state_smf_proc_data) {
+  state_smf_proc_data->pdu_session_id = smf_proc_data_proto.pdu_session_id();
+  state_smf_proc_data->pti            = smf_proc_data_proto.pti();
+  state_smf_proc_data->message_type =
+      static_cast<M5GMessageType>(smf_proc_data_proto.message_type());
+  state_smf_proc_data->max_uplink   = smf_proc_data_proto.max_uplink();
+  state_smf_proc_data->max_downlink = smf_proc_data_proto.max_downlink();
+  state_smf_proc_data->pdu_session_type =
+      static_cast<M5GPduSessionType>(smf_proc_data_proto.pdu_session_type());
+  state_smf_proc_data->ssc_mode = smf_proc_data_proto.ssc_mode();
+}
+
+void AmfNasStateConverter::s_nssai_to_proto(
+    const s_nssai_t* state_s_nssai, magma::lte::oai::SNssai* snassi_proto) {
+  snassi_proto->set_sst(state_s_nssai->sst);
+  snassi_proto->set_sd(*(uint32_t*) &state_s_nssai->sd);
+}
+void AmfNasStateConverter::proto_to_s_nssai(
+    const magma::lte::oai::SNssai& snassi_proto, s_nssai_t* state_s_nssai) {
+  state_s_nssai->sst              = snassi_proto.sst();
+  *(uint32_t*) &state_s_nssai->sd = snassi_proto.sd();
+}
+
+void AmfNasStateConverter::pco_protocol_or_container_id_to_proto(
+    const protocol_configuration_options_t&
+        state_protocol_configuration_options,
+    magma::lte::oai::ProtocolConfigurationOptions*
+        protocol_configuration_options_proto) {
+  for (int i = 0;
+       i < state_protocol_configuration_options.num_protocol_or_container_id;
+       i++) {
+    pco_protocol_or_container_id_t state_pco_protocol_or_container_id =
+        state_protocol_configuration_options.protocol_or_container_ids[i];
+    auto pco_protocol_or_container_id_proto =
+        protocol_configuration_options_proto->add_proto_or_container_id();
+    pco_protocol_or_container_id_proto->set_id(
+        state_pco_protocol_or_container_id.id);
+    pco_protocol_or_container_id_proto->set_length(
+        state_pco_protocol_or_container_id.length);
+    if (state_pco_protocol_or_container_id.contents) {
+      BSTRING_TO_STRING(
+          state_pco_protocol_or_container_id.contents,
+          pco_protocol_or_container_id_proto->mutable_contents());
+    }
+  }
+}
+
+void AmfNasStateConverter::proto_to_pco_protocol_or_container_id(
+    const magma::lte::oai::ProtocolConfigurationOptions&
+        protocol_configuration_options_proto,
+    protocol_configuration_options_t* state_protocol_configuration_options) {
+  auto proto_pco_ids =
+      protocol_configuration_options_proto.proto_or_container_id();
+  int i = 0;
+  for (auto ptr = proto_pco_ids.begin(); ptr < proto_pco_ids.end(); ptr++) {
+    pco_protocol_or_container_id_t* state_pco_protocol_or_container_id =
+        &state_protocol_configuration_options->protocol_or_container_ids[i];
+    state_pco_protocol_or_container_id->id     = ptr->id();
+    state_pco_protocol_or_container_id->length = ptr->length();
+    if (ptr->contents().length()) {
+      state_pco_protocol_or_container_id->contents = bfromcstr_with_str_len(
+          ptr->contents().c_str(), ptr->contents().length());
+    }
+    i++;
+  }
+}
+
+void AmfNasStateConverter::protocol_configuration_options_to_proto(
+    const protocol_configuration_options_t&
+        state_protocol_configuration_options,
+    magma::lte::oai::ProtocolConfigurationOptions*
+        protocol_configuration_options_proto) {
+  protocol_configuration_options_proto->set_ext(
+      state_protocol_configuration_options.ext);
+  protocol_configuration_options_proto->set_spare(
+      state_protocol_configuration_options.spare);
+  protocol_configuration_options_proto->set_config_protocol(
+      state_protocol_configuration_options.configuration_protocol);
+  protocol_configuration_options_proto->set_num_protocol_or_container_id(
+      state_protocol_configuration_options.num_protocol_or_container_id);
+
+  AmfNasStateConverter::pco_protocol_or_container_id_to_proto(
+      state_protocol_configuration_options,
+      protocol_configuration_options_proto);
+}
+
+void AmfNasStateConverter::proto_to_protocol_configuration_options(
+    const magma::lte::oai::ProtocolConfigurationOptions&
+        protocol_configuration_options_proto,
+    protocol_configuration_options_t* state_protocol_configuration_options) {
+  state_protocol_configuration_options->ext =
+      protocol_configuration_options_proto.ext();
+  state_protocol_configuration_options->spare =
+      protocol_configuration_options_proto.spare();
+  state_protocol_configuration_options->configuration_protocol =
+      protocol_configuration_options_proto.config_protocol();
+  state_protocol_configuration_options->num_protocol_or_container_id =
+      protocol_configuration_options_proto.num_protocol_or_container_id();
+  AmfNasStateConverter::proto_to_pco_protocol_or_container_id(
+      protocol_configuration_options_proto,
+      state_protocol_configuration_options);
+}
+
+void AmfNasStateConverter::session_ambr_to_proto(
+    const session_ambr_t& state_session_ambr,
+    magma::lte::oai::Ambr* ambr_proto) {
+  ambr_proto->set_br_ul(state_session_ambr.ul_session_ambr);
+  ambr_proto->set_br_dl(state_session_ambr.dl_session_ambr);
+  ambr_proto->set_br_unit(static_cast<magma::lte::oai::Ambr::BitrateUnitsAMBR>(
+      state_session_ambr.dl_ambr_unit));
+}
+void AmfNasStateConverter::proto_to_session_ambr(
+    const magma::lte::oai::Ambr& ambr_proto,
+    session_ambr_t* state_session_ambr) {
+  state_session_ambr->dl_ambr_unit =
+      static_cast<M5GSessionAmbrUnit>(ambr_proto.br_unit());
+  state_session_ambr->dl_session_ambr = ambr_proto.br_dl();
+  state_session_ambr->ul_ambr_unit =
+      static_cast<M5GSessionAmbrUnit>(ambr_proto.br_unit());
+  state_session_ambr->ul_session_ambr = ambr_proto.br_ul();
+}
+
+void AmfNasStateConverter::qos_flow_level_parameters_to_proto(
+    const qos_flow_level_qos_parameters& state_qos_flow_parameters,
+    magma::lte::oai::QosFlowParameters* qos_flow_parameters_proto) {
+  qos_flow_parameters_proto->set_fiveqi(
+      state_qos_flow_parameters.qos_characteristic.non_dynamic_5QI_desc.fiveQI);
+  qos_flow_parameters_proto->set_priority_level(
+      state_qos_flow_parameters.alloc_reten_priority.priority_level);
+  qos_flow_parameters_proto->set_preemption_vulnerability(
+      state_qos_flow_parameters.alloc_reten_priority.pre_emption_vul);
+  qos_flow_parameters_proto->set_preemption_capability(
+      state_qos_flow_parameters.alloc_reten_priority.pre_emption_cap);
+}
+
+void AmfNasStateConverter::proto_to_qos_flow_level_parameters(
+    const magma::lte::oai::QosFlowParameters& qos_flow_parameters_proto,
+    qos_flow_level_qos_parameters* state_qos_flow_parameters) {
+  state_qos_flow_parameters->qos_characteristic.non_dynamic_5QI_desc.fiveQI =
+      qos_flow_parameters_proto.fiveqi();
+  state_qos_flow_parameters->alloc_reten_priority.priority_level =
+      qos_flow_parameters_proto.priority_level();
+  state_qos_flow_parameters->alloc_reten_priority.pre_emption_vul =
+      static_cast<pre_emption_vulnerability>(
+          qos_flow_parameters_proto.preemption_vulnerability());
+  state_qos_flow_parameters->alloc_reten_priority.pre_emption_cap =
+      static_cast<pre_emption_capability>(
+          qos_flow_parameters_proto.preemption_capability());
+}
+
+void AmfNasStateConverter::qos_flow_setup_request_item_to_proto(
+    const qos_flow_setup_request_item& state_qos_flow_request_item,
+    magma::lte::oai::M5GQosFlowItem* qos_flow_item_proto) {
+  qos_flow_item_proto->set_qfi(state_qos_flow_request_item.qos_flow_identifier);
+  AmfNasStateConverter::qos_flow_level_parameters_to_proto(
+      state_qos_flow_request_item.qos_flow_level_qos_param,
+      qos_flow_item_proto->mutable_qos_flow_param());
+}
+
+void AmfNasStateConverter::proto_to_qos_flow_setup_request_item(
+    const magma::lte::oai::M5GQosFlowItem& qos_flow_item_proto,
+    qos_flow_setup_request_item* state_qos_flow_request_item) {
+  state_qos_flow_request_item->qos_flow_identifier = qos_flow_item_proto.qfi();
+  AmfNasStateConverter::proto_to_qos_flow_level_parameters(
+      qos_flow_item_proto.qos_flow_param(),
+      &state_qos_flow_request_item->qos_flow_level_qos_param);
+}
+// smf_context to proto and proto to smf_context
+void AmfNasStateConverter::smf_context_to_proto(
+    const smf_context_t* state_smf_context,
+    magma::lte::oai::SmfContext* smf_context_proto) {
+  smf_context_proto->set_sm_session_state(state_smf_context->pdu_session_state);
+  smf_context_proto->set_pdu_session_version(
+      state_smf_context->pdu_session_version);
+  smf_context_proto->set_active_pdu_sessions(state_smf_context->n_active_pdus);
+  smf_context_proto->set_is_emergency(state_smf_context->is_emergency);
+  AmfNasStateConverter::session_ambr_to_proto(
+      state_smf_context->selected_ambr,
+      smf_context_proto->mutable_selected_ambr());
+
+  smf_context_proto->set_gnb_gtp_teid(
+      state_smf_context->gtp_tunnel_id.gnb_gtp_teid);
+
+  char gnb_gtp_teid_ip_addr_str[16] = {0};
+  inet_ntop(
+      AF_INET, state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr,
+      gnb_gtp_teid_ip_addr_str, INET_ADDRSTRLEN);
+  smf_context_proto->set_gnb_gtp_teid_ip_addr(gnb_gtp_teid_ip_addr_str);
+
+  smf_context_proto->set_upf_gtp_teid(
+      *(uint32_t*) &state_smf_context->gtp_tunnel_id.upf_gtp_teid);
+
+  char upf_gtp_teid_ip_addr_str[16] = {0};
+  inet_ntop(
+      AF_INET, state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr,
+      upf_gtp_teid_ip_addr_str, INET_ADDRSTRLEN);
+  smf_context_proto->set_upf_gtp_teid_ip_addr(upf_gtp_teid_ip_addr_str);
+
+  bstring bstr_buffer = paa_to_bstring(&state_smf_context->pdu_address);
+  BSTRING_TO_STRING(bstr_buffer, smf_context_proto->mutable_paa());
+  bdestroy(bstr_buffer);
+
+  StateConverter::ambr_to_proto(
+      state_smf_context->apn_ambr, smf_context_proto->mutable_apn_ambr());
+
+  AmfNasStateConverter::smf_proc_data_to_proto(
+      &state_smf_context->smf_proc_data,
+      smf_context_proto->mutable_smf_proc_data());
+  smf_context_proto->set_retransmission_count(
+      state_smf_context->retransmission_count);
+  AmfNasStateConverter::protocol_configuration_options_to_proto(
+      state_smf_context->pco, smf_context_proto->mutable_pco());
+  smf_context_proto->set_dnn_in_use(state_smf_context->dnn);
+
+  AmfNasStateConverter::s_nssai_to_proto(
+      &state_smf_context->requested_nssai,
+      smf_context_proto->mutable_requested_nssai());
+
+  AmfNasStateConverter::qos_flow_setup_request_item_to_proto(
+      state_smf_context->subscribed_qos_profile.qos_flow_req_item,
+      smf_context_proto->mutable_qos_flow_list());
+}
+
+void AmfNasStateConverter::proto_to_smf_context(
+    const magma::lte::oai::SmfContext& smf_context_proto,
+    smf_context_t* state_smf_context) {
+  state_smf_context->pdu_session_state =
+      (SMSessionFSMState) smf_context_proto.sm_session_state();
+  state_smf_context->pdu_session_version =
+      smf_context_proto.pdu_session_version();
+  state_smf_context->n_active_pdus = smf_context_proto.active_pdu_sessions();
+  state_smf_context->is_emergency  = smf_context_proto.is_emergency();
+  AmfNasStateConverter::proto_to_session_ambr(
+      smf_context_proto.selected_ambr(), &state_smf_context->selected_ambr);
+  state_smf_context->gtp_tunnel_id.gnb_gtp_teid =
+      smf_context_proto.gnb_gtp_teid();
+
+  memset(
+      &state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr, '\0',
+      sizeof(state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr));
+  inet_pton(
+      AF_INET, smf_context_proto.gnb_gtp_teid_ip_addr().c_str(),
+      &(state_smf_context->gtp_tunnel_id.gnb_gtp_teid_ip_addr));
+
+  *(uint32_t*) &state_smf_context->gtp_tunnel_id.upf_gtp_teid =
+      smf_context_proto.upf_gtp_teid();
+
+  memset(
+      &state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr, '\0',
+      sizeof(state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr));
+  inet_pton(
+      AF_INET, smf_context_proto.upf_gtp_teid_ip_addr().c_str(),
+      &(state_smf_context->gtp_tunnel_id.upf_gtp_teid_ip_addr));
+
+  bstring bstr_buffer;
+  STRING_TO_BSTRING(smf_context_proto.paa(), bstr_buffer);
+  bstring_to_paa(bstr_buffer, &state_smf_context->pdu_address);
+  bdestroy(bstr_buffer);
+
+  StateConverter::proto_to_ambr(
+      smf_context_proto.apn_ambr(), &state_smf_context->apn_ambr);
+
+  AmfNasStateConverter::proto_to_smf_proc_data(
+      smf_context_proto.smf_proc_data(), &state_smf_context->smf_proc_data);
+
+  state_smf_context->retransmission_count =
+      smf_context_proto.retransmission_count();
+
+  AmfNasStateConverter::proto_to_protocol_configuration_options(
+      smf_context_proto.pco(), &state_smf_context->pco);
+
+  state_smf_context->dnn = smf_context_proto.dnn_in_use();
+
+  AmfNasStateConverter::proto_to_s_nssai(
+      smf_context_proto.requested_nssai(), &state_smf_context->requested_nssai);
+
+  AmfNasStateConverter::proto_to_qos_flow_setup_request_item(
+      smf_context_proto.qos_flow_list(),
+      &state_smf_context->subscribed_qos_profile.qos_flow_req_item);
+}
+
 }  // namespace magma5g