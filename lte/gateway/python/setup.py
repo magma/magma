7f07352e2aa9c921e30a90b89655cc286da294f4
Kristijan <spikey979@gmail.com>
2022-02-03 13:09:00 +0100

    chore(deps): Upgrade eventlet from v0.30.2 to v0.31.0
    Signed-off-by: Kristijan <spikey979@gmail.com>

--- lte/gateway/python/setup.py
+++ lte/gateway/python/setup.py
@@ -120,7 +120,7 @@ setup(
         'idna==2.8',
         'python-dateutil==2.8.1',
         'six>=1.12.0',
-        'eventlet==0.30.2',
+        'eventlet==0.31.0',
         'h2>=3.2.0',
         'hpack>=3.0',
         'freezegun>=0.3.15',
