20baa576d597afd9c73ce411d5abede0a9521131
alexzurbonsen <82914459+alexzurbonsen@users.noreply.github.com>
2022-02-09 17:46:24 +0100

    chore(connectiond): enable sentry for connectiond (#11460)
    Signed-off-by: Alexander zur Bonsen <alexander.zur.bonsen@tngtech.com>

--- orc8r/gateway/c/common/sentry/includes/SentryWrapper.h
+++ orc8r/gateway/c/common/sentry/includes/SentryWrapper.h
@@ -19,6 +19,7 @@ extern "C" {
 
 #define MAX_URL_LENGTH 255
 #define SENTRY_TAG_MME "MME"
+#define SENTRY_TAG_CONNECTIOND "ConnectionD"
 #define SENTRY_TAG_SESSIOND "SessionD"
 #define SENTRY_TAG_SCTPD "SctpD"
 #define SENTRY_TAG_LEN 16
