5f7c8911be67a9e91d8b93bb7ed21c5064a8d302
rsarwad <30685618+rsarwad@users.noreply.github.com>
2022-02-23 11:22:39 +0530

    fix(agw): Modified code to handle TAU Req for connected UEs (#11245)
    * Fixed following issues
    * On reception of TAU Req in initial ue message for connected UEs, send UE context release request
    * After TAU handling, eps_bearer_context status flag was not reset, now it's corrected to reset this flag, after sending TAU Accept
    * Release s1ap signaling contexts on reception of ue context release complete
    * Added ue_context_release timer to track s1ap-ue_context_release_complete from eNB
    * Fixed the unit test case for ue context release command
    * Adding timer functionality for S1ap
    Signed-off-by: Rashmi <rashmi.sarwad@radisys.com>

--- lte/gateway/c/core/oai/tasks/s1ap/s1ap_mme_handlers.c
+++ lte/gateway/c/core/oai/tasks/s1ap/s1ap_mme_handlers.c
@@ -95,6 +95,7 @@
 #include "lte/gateway/c/core/oai/include/mme_app_messages_types.h"
 #include "orc8r/gateway/c/common/service303/includes/MetricsHelpers.h"
 #include "lte/gateway/c/core/oai/include/s1ap_state.h"
+#include "lte/gateway/c/core/oai/tasks/s1ap/s1ap_timer.h"
 
 struct S1ap_E_RABItem_s;
 struct S1ap_E_RABSetupItemBearerSURes_s;
@@ -105,6 +106,7 @@ status_code_e s1ap_generate_s1_setup_response(
     s1ap_state_t* state, enb_description_t* enb_association);
 
 bool is_all_erabId_same(S1ap_PathSwitchRequest_t* container);
+static int handle_ue_context_rel_timer_expiry(zloop_t* loop, int id, void* arg);
 
 /* Handlers matrix. Only mme related procedures present here.
  */
@@ -1320,6 +1322,10 @@ status_code_e s1ap_mme_generate_ue_context_release_command(
       cause_type = S1ap_Cause_PR_radioNetwork;
       cause_value = S1ap_CauseRadioNetwork_load_balancing_tau_required;
       break;
+    case S1AP_INVALID_ENB_ID:
+      cause_type = S1ap_Cause_PR_radioNetwork;
+      cause_value = S1ap_CauseRadioNetwork_unknown_enb_ue_s1ap_id;
+      break;
     default:
       // Freeing ie and pdu data since it will not be encoded
       free_wrapper((void**)&ie);
@@ -1338,10 +1344,14 @@ status_code_e s1ap_mme_generate_ue_context_release_command(
   free(buffer);
   rc = s1ap_mme_itti_send_sctp_request(&b, assoc_id, stream, mme_ue_s1ap_id);
 
-  // Dont remove UE context if UE handed over to another eNB
-  if (ue_ref_p != NULL && ue_ref_p->sctp_assoc_id == assoc_id) {
+  if (rc == RETURNok) {
+    // Start timer to track UE context release complete from eNB
     ue_ref_p->s1_ue_state = S1AP_UE_WAITING_CRR;
-    // We can safely remove UE context now, no need for timer
+    ue_ref_p->s1ap_ue_context_rel_timer.id = s1ap_start_timer(
+        ue_ref_p->s1ap_ue_context_rel_timer.msec, TIMER_REPEAT_ONCE,
+        handle_ue_context_rel_timer_expiry, mme_ue_s1ap_id);
+  } else {
+    // Remove UE context and inform MME_APP.
     s1ap_mme_release_ue_context(state, ue_ref_p, imsi64);
   }
   OAILOG_FUNC_RETURN(LOG_S1AP, rc);
@@ -1497,8 +1507,7 @@ status_code_e s1ap_handle_ue_context_release_command(
      */
 
     if (ue_context_release_command_pP->cause == S1AP_IMPLICIT_CONTEXT_RELEASE ||
-        ue_context_release_command_pP->cause == S1AP_SCTP_SHUTDOWN_OR_RESET ||
-        ue_context_release_command_pP->cause == S1AP_INVALID_ENB_ID) {
+        ue_context_release_command_pP->cause == S1AP_SCTP_SHUTDOWN_OR_RESET) {
       s1ap_remove_ue(state, ue_ref_p);
     } else {
       rc = s1ap_mme_generate_ue_context_release_command(
@@ -1566,11 +1575,6 @@ status_code_e s1ap_mme_handle_ue_context_release_complete(
   }
 
   if ((ue_ref_p = s1ap_state_get_ue_mmeid(mme_ue_s1ap_id)) == NULL) {
-    /*
-     * The UE context has already been deleted when the UE context release
-     * command was sent
-     * Ignore this message.
-     */
     OAILOG_DEBUG(
         LOG_S1AP,
         " UE Context Release commplete: S1 context cleared. Ignore message for "
@@ -1582,12 +1586,21 @@ status_code_e s1ap_mme_handle_ue_context_release_complete(
       /* This is an error scenario, the S1 UE context should have been deleted
        * when UE context release command was sent
        */
-      OAILOG_ERROR(
-          LOG_S1AP,
-          " UE Context Release commplete: S1 context should have been cleared "
-          "for ueid " MME_UE_S1AP_ID_FMT "\n",
-          (uint32_t)mme_ue_s1ap_id);
-      OAILOG_FUNC_RETURN(LOG_S1AP, RETURNerror);
+      OAILOG_ERROR(LOG_S1AP,
+                   "UE Context Release complete: clearing S1 context for "
+                   "ueid " MME_UE_S1AP_ID_FMT "\n",
+                   (uint32_t)mme_ue_s1ap_id);
+      // Remove UE context only if UE is not handed over to another eNB
+      imsi64_t imsi64 = INVALID_IMSI64;
+      s1ap_imsi_map_t* imsi_map = get_s1ap_imsi_map();
+      hashtable_uint64_ts_get(imsi_map->mme_ue_id_imsi_htbl,
+                              (const hash_key_t)mme_ue_s1ap_id, &imsi64);
+
+      ue_ref_p->s1_ue_state = S1AP_UE_WAITING_CRR;
+      // We can safely remove UE context now and stop timer
+      s1ap_mme_release_ue_context(state, ue_ref_p, imsi64);
+
+      OAILOG_FUNC_RETURN(LOG_S1AP, RETURNok);
     } else {
       /*
        * UE Context Release commplete received from a different eNB. This could
@@ -3755,9 +3768,12 @@ void s1ap_mme_release_ue_context(s1ap_state_t* state,
   if (ue_ref_p == NULL) {
     OAILOG_ERROR(LOG_S1AP, "ue_ref_p is NULL\n");
   }
-
+  // Stop the ue context release timer
+  s1ap_stop_timer(ue_ref_p->s1ap_ue_context_rel_timer.id);
+  ue_ref_p->s1ap_ue_context_rel_timer.id = S1AP_TIMER_INACTIVE_ID;
   OAILOG_DEBUG_UE(LOG_S1AP, imsi64, "Releasing UE Context for UE id  %d \n",
                   ue_ref_p->mme_ue_s1ap_id);
+
   /*
    * Remove UE context and inform MME_APP.
    */
@@ -5279,4 +5295,40 @@ status_code_e s1ap_send_mme_ue_context_release(s1ap_state_t* state,
 
   message_p->ittiMsgHeader.imsi = imsi64;
   return send_msg_to_task(&s1ap_task_zmq_ctx, TASK_MME_APP, message_p);
+}
+
+//------------------------------------------------------------------------------
+static int handle_ue_context_rel_timer_expiry(zloop_t* loop, int timer_id,
+                                              void* arg) {
+  OAILOG_FUNC_IN(LOG_S1AP);
+  ue_description_t* ue_ref_p = NULL;
+  mme_ue_s1ap_id_t mme_ue_s1ap_id = 0;
+  imsi64_t imsi64 = INVALID_IMSI64;
+  s1ap_state_t* state = NULL;
+  if (!s1ap_pop_timer_arg_ue_id(timer_id, &mme_ue_s1ap_id)) {
+    OAILOG_WARNING(LOG_S1AP, "Invalid Timer Id expiration, Timer Id: %u\n",
+                   timer_id);
+    OAILOG_FUNC_RETURN(LOG_S1AP, RETURNerror);
+  }
+  if ((ue_ref_p = s1ap_state_get_ue_mmeid(mme_ue_s1ap_id)) == NULL) {
+    OAILOG_ERROR(
+        LOG_S1AP,
+        "Failed to find UE context for mme_ue_s1ap_id " MME_UE_S1AP_ID_FMT,
+        mme_ue_s1ap_id);
+    OAILOG_FUNC_RETURN(LOG_S1AP, RETURNerror);
+  }
+
+  state = get_s1ap_state(false);
+  ue_ref_p->s1ap_ue_context_rel_timer.id = S1AP_TIMER_INACTIVE_ID;
+  s1ap_imsi_map_t* imsi_map = get_s1ap_imsi_map();
+  hashtable_uint64_ts_get(imsi_map->mme_ue_id_imsi_htbl,
+                          (const hash_key_t)mme_ue_s1ap_id, &imsi64);
+
+  OAILOG_DEBUG_UE(LOG_S1AP, imsi64,
+                  "Expired- UE Context Release Timer for "
+                  "mme_ue_s1ap_id " MME_UE_S1AP_ID_FMT,
+                  ue_ref_p->mme_ue_s1ap_id);
+  // Remove UE context and inform MME_APP.
+  s1ap_mme_release_ue_context(state, ue_ref_p, imsi64);
+  OAILOG_FUNC_OUT(LOG_S1AP);
 }
