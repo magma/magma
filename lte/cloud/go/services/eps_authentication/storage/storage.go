e3edad6455900c73d089c98c1e9dd50616ef0000
Evgeniy Makeev <evgeniym@fb.com>
2022-02-23 16:41:08 -0800

    feat(orc8r): EPS Authentication Service - a gRPC version of S6a type authentication
    Signed-off-by: Evgeniy Makeev <evgeniym@fb.com>

---
+++ lte/cloud/go/services/eps_authentication/storage/storage.go
@@ -0,0 +1,136 @@
+/*
+Copyright 2022 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package storage
+
+import (
+	"context"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"magma/lte/cloud/go/lte"
+	lteprotos "magma/lte/cloud/go/protos"
+	"magma/lte/cloud/go/serdes"
+	"magma/lte/cloud/go/services/subscriberdb/obsidian/models"
+	"magma/orc8r/cloud/go/blobstore"
+	"magma/orc8r/cloud/go/services/configurator"
+	"magma/orc8r/cloud/go/storage"
+	"magma/orc8r/lib/go/protos"
+)
+
+const (
+	EpsAuthStateStore = "eps_auth_state_store"
+	EpsAuthType       = "eps_auth_state_type"
+)
+
+type SubscriberDBStorage interface {
+	// GetSubscriberData returns subscriber data of given subscriber ID
+	GetSubscriberData(*lteprotos.SubscriberID, *protos.NetworkID) (*lteprotos.SubscriberData, error)
+	// UpdateSubscriberAuthNextSeq sets subscriber's AuthNextSeq to the provided value
+	UpdateSubscriberAuthNextSeq(*lteprotos.SubscriberData) (*protos.Void, error)
+	// IncrementSubscriberAuthNextSeq increments subscriber's AuthNextSeq
+	IncrementSubscriberAuthNextSeq(*lteprotos.SubscriberData) (*protos.Void, error)
+}
+
+// needs to be implemented
+type subscriberDBStorageImpl struct {
+	blobstore.StoreFactory
+}
+
+func NewSubscriberDBStorage(storeFactory blobstore.StoreFactory) SubscriberDBStorage {
+	return &subscriberDBStorageImpl{StoreFactory: storeFactory}
+}
+
+// GetSubscriberData returns subscriber data of given subscriber ID
+func (s *subscriberDBStorageImpl) GetSubscriberData(
+	sid *lteprotos.SubscriberID, nid *protos.NetworkID) (*lteprotos.SubscriberData, error) {
+
+	lc := configurator.EntityLoadCriteria{
+		PageSize:           0,
+		PageToken:          "",
+		LoadConfig:         true,
+		LoadAssocsToThis:   false,
+		LoadAssocsFromThis: false,
+	}
+
+	ent, err := configurator.LoadEntity(
+		context.Background(), nid.GetId(), lte.SubscriberEntityType, sid.GetId(), lc, serdes.Entity)
+
+	if err != nil {
+		return nil, status.Errorf(codes.Unavailable, "error loading subscriber entity: %v", err)
+	}
+	subData := &lteprotos.SubscriberData{
+		Sid:       sid,
+		NetworkId: nid,
+	}
+	if ent.Config == nil {
+		return nil, status.Error(codes.NotFound, "missing subscriber configuration")
+	}
+	if cfg := ent.Config.(*models.SubscriberConfig); cfg != nil {
+		subData.Lte = &lteprotos.LTESubscription{
+			State: lteprotos.LTESubscription_LTESubscriptionState(
+				lteprotos.LTESubscription_LTESubscriptionState_value[cfg.Lte.State]),
+			AuthAlgo: lteprotos.LTESubscription_LTEAuthAlgo(
+				lteprotos.LTESubscription_LTEAuthAlgo_value[cfg.Lte.AuthAlgo]),
+			AuthKey: cfg.Lte.AuthKey,
+			AuthOpc: cfg.Lte.AuthOpc,
+		}
+	}
+	return subData, nil
+}
+
+// UpdateSubscriberAuthNextSeq sets subscriber's AuthNextSeq to the provided value
+// Note: the implementation uses the blob version as the next sequence
+func (s *subscriberDBStorageImpl) UpdateSubscriberAuthNextSeq(sd *lteprotos.SubscriberData) (*protos.Void, error) {
+	ret := &protos.Void{}
+	if sd == nil {
+		return ret, status.Errorf(codes.InvalidArgument, "nil Subscriber Data")
+	}
+	store, err := s.StartTransaction(&storage.TxOptions{})
+	if err != nil {
+		return ret, status.Errorf(codes.Unavailable, "error starting update nex seq transaction: %v", err)
+	}
+	defer store.Rollback()
+
+	err = store.Write(sd.GetNetworkId().GetId(), []blobstore.Blob{{
+		Type:    EpsAuthType,
+		Key:     sd.GetSid().GetId(),
+		Value:   []byte{},
+		Version: sd.GetState().GetLteAuthNextSeq(),
+	}})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "state write error: %v", err)
+	}
+	return ret, store.Commit()
+}
+
+// IncrementSubscriberAuthNextSeq increments subscriber's AuthNextSeq
+// Note: the implementation uses the blob version as the next sequence
+func (s *subscriberDBStorageImpl) IncrementSubscriberAuthNextSeq(sd *lteprotos.SubscriberData) (*protos.Void, error) {
+	ret := &protos.Void{}
+	if sd == nil {
+		return ret, status.Errorf(codes.InvalidArgument, "nil Subscriber Data")
+	}
+	store, err := s.StartTransaction(&storage.TxOptions{})
+	if err != nil {
+		return ret, status.Errorf(codes.Unavailable, "error starting increment nex seq transaction: %v", err)
+	}
+	defer store.Rollback()
+
+	err = store.IncrementVersion(sd.GetNetworkId().GetId(), storage.TK{Type: EpsAuthType, Key: sd.GetSid().GetId()})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "auth next seq increment error: %v", err)
+	}
+	return ret, store.Commit()
+}
