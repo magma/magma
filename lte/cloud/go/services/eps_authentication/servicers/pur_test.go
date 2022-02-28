9e20c7b9522ecd3ac54df259a4cb49372c8d6602
emakeev <evgeniym@fb.com>
2022-02-28 10:42:21 -0800

    feat(orc8r): EPS Authentication Service - a gRPC version of S6a type authentication (#11773)
    Signed-off-by: Evgeniy Makeev <evgeniym@fb.com>

---
+++ lte/cloud/go/services/eps_authentication/servicers/pur_test.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2022 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package servicers
+
+import "magma/lte/cloud/go/protos"
+
+func (suite *EpsAuthTestSuite) TestPurgeUE_UnknownSubscriber() {
+	purge := &protos.PurgeUERequest{UserName: "sub_unknown"}
+	answer, err := suite.PurgeUE(purge)
+	suite.EqualError(
+		err, "rpc error: code = NotFound desc = Error fetching subscriber: IMSIsub_unknown, No record for query")
+	suite.Equal(protos.ErrorCode_USER_UNKNOWN, answer.ErrorCode)
+}
+
+func (suite *EpsAuthTestSuite) TestPurgeUE_Success() {
+	purge := &protos.PurgeUERequest{UserName: "sub1"}
+	answer, err := suite.PurgeUE(purge)
+	suite.NoError(err)
+	suite.Equal(protos.ErrorCode_SUCCESS, answer.ErrorCode)
+}
