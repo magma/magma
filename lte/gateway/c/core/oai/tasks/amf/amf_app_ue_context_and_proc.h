767eaab28ce0529666433433e00deb3070f628c6
Rahul Kalsangra <51331971+RahulKalsangra@users.noreply.github.com>
2022-03-18 13:43:24 +0530

    fix(amf): Re-Attempt Authentication during subscriberdb lock (#11454)
    * fix(amf): handle and proper response the error code from subscriberdb during authentication
    
    Signed-off-by: RahulKalsangra <rahul.kalsangra@wavelabs.ai>
    
    * Addressed review comment (R1)
    
    Signed-off-by: RahulKalsangra <rahul.kalsangra@wavelabs.ai>
    
    * Addressed review comment (R2)
    
    Signed-off-by: RahulKalsangra <rahul.kalsangra@wavelabs.ai>

--- lte/gateway/c/core/oai/tasks/amf/amf_app_ue_context_and_proc.h
+++ lte/gateway/c/core/oai/tasks/amf/amf_app_ue_context_and_proc.h
@@ -105,7 +105,7 @@ struct amf_procedures_t;
 #define PDU_ADDR_IPV4_LEN 0x4
 #define GNB_IPV4_ADDR_LEN 4
 #define GNB_TEID_LEN 4
-
+#define DIAMETER_TOO_BUSY 3004
 #define NON_AMF_3GPP_ACCESS 2
 #define AMF_3GPP_ACCESS_AND_NON_AMF_3GPP_ACCESS 3
 
@@ -351,6 +351,8 @@ typedef struct amf_context_s {
   apn_config_profile_t apn_config_profile;
 
   amf_nas_message_decode_status_t decode_status;
+  nas5g_timer_t auth_retry_timer;
+  uint32_t auth_retry_count = 0;
 } amf_context_t;
 
 // Amf-Map Declarations:
