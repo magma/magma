e558401b57bbd70a9e920df71893d4425387bde9
Christine Wang <christinewang@fb.com>
2022-03-27 22:23:15 -0400

    changed EnodebStateLookup to protected
    Signed-off-by: Christine Wang <christinewang@fb.com>

--- lte/cloud/go/services/lte/test_init/test_service_init.go
+++ lte/cloud/go/services/lte/test_init/test_service_init.go
@@ -54,7 +54,7 @@ func StartTestServiceWithConfig(t *testing.T, serviceConfig lte_service.Config) 
 		orc8r.StreamProviderStreamsAnnotation: strings.Join(streams, orc8r.AnnotationFieldSeparator),
 	}
 
-	srv, lis, _ := test_utils.NewTestOrchestratorService(t, lte.ModuleName, lte_service.ServiceName, labels, annotations)
+	srv, lis, plis := test_utils.NewTestOrchestratorService(t, lte.ModuleName, lte_service.ServiceName, labels, annotations)
 	builder_protos.RegisterMconfigBuilderServer(srv.GrpcServer, protected_servicers.NewBuilderServicer(serviceConfig))
 	provider_protos.RegisterStreamProviderServer(srv.GrpcServer, servicers.NewProviderServicer())
 
@@ -65,8 +65,8 @@ func StartTestServiceWithConfig(t *testing.T, serviceConfig lte_service.Config) 
 	assert.NoError(t, enbStateStore.Initialize())
 
 	// Add servicers
-	lte_protos.RegisterEnodebStateLookupServer(srv.GrpcServer, protected_servicers.NewLookupServicer(enbStateStore))
+	lte_protos.RegisterEnodebStateLookupServer(srv.ProtectedGrpcServer, protected_servicers.NewLookupServicer(enbStateStore))
 	state_protos.RegisterIndexerServer(srv.GrpcServer, protected_servicers.NewIndexerServicer())
 
-	go srv.RunTest(lis, nil)
+	go srv.RunTest(lis, plis)
 }
