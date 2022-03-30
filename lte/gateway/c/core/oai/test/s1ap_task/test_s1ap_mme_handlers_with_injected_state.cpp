eb76319108fae664aa7de304b77384ba3015ce3c
Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>
2022-03-30 09:36:55 +0200

    test(mme): correct the header path with new merge from master
    Signed-off-by: Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>

--- lte/gateway/c/core/oai/test/s1ap_task/test_s1ap_mme_handlers_with_injected_state.cpp
+++ lte/gateway/c/core/oai/test/s1ap_task/test_s1ap_mme_handlers_with_injected_state.cpp
@@ -16,7 +16,7 @@
 #include "lte/gateway/c/core/oai/test/mock_tasks/mock_tasks.h"
 
 extern "C" {
-#include "lte/gateway/c/core/oai/common/dynamic_memory_check.h"
+#include "lte/gateway/c/core/common/dynamic_memory_check.h"
 #include "lte/gateway/c/core/oai/common/log.h"
 #include "lte/gateway/c/core/oai/include/mme_config.h"
 #include "lte/gateway/c/core/oai/include/s1ap_state.h"
