41db1fd532bcd873326824425f6333dffcdac3d2
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-04-06 16:01:41 +0530

    Added operator docker image
    Signed-off-by: Shubham Tatvamasi <shubhamtatvamasi@gmail.com>

--- orc8r/tools/docker/promote.sh
+++ orc8r/tools/docker/promote.sh
@@ -10,7 +10,7 @@ declare -A repositories=(
   [feg]="gateway_go gateway_python"
   [orc8r]="controller magmalte nginx"
   [agw]="agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python"
-  [cwf]="cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond" # operator
+  [cwf]="cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator"
 )
 
 for repo in ${!repositories[@]}; do
