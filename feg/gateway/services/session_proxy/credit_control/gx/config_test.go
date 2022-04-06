9af197bb080ee545bc1d1f0cd50b748cff5f81a9
Vikram G <50407462+vikramgreddy@users.noreply.github.com>
2022-04-06 23:42:00 +0530

    fix(feg): config_test.go in session_proxy (#12319)

--- feg/gateway/services/session_proxy/credit_control/gx/config_test.go
+++ feg/gateway/services/session_proxy/credit_control/gx/config_test.go
@@ -23,30 +23,41 @@ import (
 	"magma/gateway/mconfig"
 )
 
-func TestGxClientConfig(t *testing.T) {
-	// Create tmp mconfig test file & load configs from it
-	fegConfigFmt := `{
-		"configsByKey": {
-			"session_proxy": {
-				"@type": "type.googleapis.com/magma.mconfig.SessionProxyConfig",
-				"gx": {
-					"servers": [{
-						"protocol": "sctp",
-						"address": "1.1.1.1:9999",
-						"retransmits": 5,
-						"watchdog_interval": 1,
-						"retry_count": 5,
-						"product_name": "magma_test",
-						"realm": "local.openair4G.eur",
-						"host": "local.magma-oai.openair4G.eur",
-						"dest_host": "magma-oai.openair4G.eur",
-						"dest_realm": "openair4G.eur",
-						"request_timeout": 10
-					}]
-				}
+// Create tmp mconfig test file & load configs from it
+var fegConfigFmt = `{
+	"configsByKey": {
+		"session_proxy": {
+			"@type": "type.googleapis.com/magma.mconfig.SessionProxyConfig",
+			"gx": {
+				"DisableGx": false,
+				"OverwriteApn": "apn.magma.com",
+				"servers": [{
+					"protocol": "sctp",
+					"address": "1.1.1.1:9999",
+					"retransmits": 5,
+					"watchdog_interval": 1,
+					"retry_count": 5,
+					"local_address": "2.2.2.2:9999",
+					"product_name": "magma_test",
+					"realm": "local.openair4G.eur",
+					"host": "local.magma-oai.openair4G.eur",
+					"dest_host": "magma-oai.openair4G.eur",
+					"dest_realm": "openair4G.eur",
+					"disable_dest_host": true,
+					"overwrite_dest_host": false,
+					"request_timeout": 10
+				}],
+				"virtual_apn_rules": [{
+					"apn_filter": ".*",
+					"charging_characteristics_filter": "1*",
+					"apn_overwrite": "vApnGy.magma-fedgw.magma.com"
+				}]
 			}
 		}
-	}`
+	}
+}`
+
+func TestGxClientConfig(t *testing.T) {
 
 	os.Setenv(GxSupportedVendorIDsEnv, "example-vendor-id")
 	defer os.Unsetenv(GxSupportedVendorIDsEnv)
@@ -64,4 +75,33 @@ func TestGxClientConfig(t *testing.T) {
 	assert.Equal(t, uint(5), cliConfig.RetryCount)
 	assert.Equal(t, uint(5), cliConfig.Retransmits)
 	assert.Equal(t, uint(10), cliConfig.RequestTimeout)
+}
+
+func TestGxPCRFConfig(t *testing.T) {
+
+	err := mconfig.CreateLoadTempConfig(fegConfigFmt)
+	assert.NoError(t, err)
+	pcrfConfig := GetPCRFConfiguration()[0]
+
+	assert.Equal(t, "1.1.1.1:9999", pcrfConfig.Addr)
+	assert.Equal(t, "sctp", pcrfConfig.Protocol)
+	assert.Equal(t, "2.2.2.2:9999", pcrfConfig.LocalAddr)
+	assert.Equal(t, "magma-oai.openair4G.eur", pcrfConfig.DestHost)
+	assert.Equal(t, "openair4G.eur", pcrfConfig.DestRealm)
+	assert.Equal(t, bool(true), pcrfConfig.DisableDestHost)
+	assert.Equal(t, bool(false), pcrfConfig.OverwriteDestHost)
+}
+
+func TestGxGlobalConfig(t *testing.T) {
+
+	err := mconfig.CreateLoadTempConfig(fegConfigFmt)
+	assert.NoError(t, err)
+	globalConfig := GetGxGlobalConfig()
+	vApnRules := globalConfig.VirtualApnRules[0]
+
+	assert.Equal(t, "apn.magma.com", globalConfig.PCFROverwriteApn)
+	assert.Equal(t, bool(false), globalConfig.DisableGx)
+	assert.Regexp(t, ".*", vApnRules.ApnFilter)
+	assert.Regexp(t, "1*", vApnRules.ChargingCharacteristicsFilter)
+	assert.Equal(t, "vApnGy.magma-fedgw.magma.com", vApnRules.ApnOverwrite)
 }
