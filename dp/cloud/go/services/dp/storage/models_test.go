f95c74521f571abf6e9c3c4345a045123febd82a
Kuba Marciniszyn <kuba@freedomfi.com>
2022-04-07 12:45:29 +0200

    chore(dp): Remove unused models
    After recent changes some of dp orc8r models have become unused
    
    Signed-off-by: Kuba Marciniszyn <kuba@freedomfi.com>

--- dp/cloud/go/services/dp/storage/models_test.go
+++ dp/cloud/go/services/dp/storage/models_test.go
@@ -29,30 +29,6 @@ func TestFields(t *testing.T) {
 		expected []string
 	}{
 		{
-			name:     "check field names for DBRequestType",
-			model:    &storage.DBRequestType{},
-			expected: []string{"id", "name"},
-		},
-		{
-			name:     "check field names for DBRequestState",
-			model:    &storage.DBRequestState{},
-			expected: []string{"id", "name"},
-		},
-		{
-			name:  "check field names for DBRequest",
-			model: &storage.DBRequest{},
-			expected: []string{
-				"id", "type_id", "state_id", "cbsd_id", "payload",
-			},
-		},
-		{
-			name:  "check field names for DBResponse",
-			model: &storage.DBResponse{},
-			expected: []string{
-				"id", "request_id", "grant_id", "response_code", "payload",
-			},
-		},
-		{
 			name:     "check field names for DBGrantState",
 			model:    &storage.DBGrantState{},
 			expected: []string{"id", "name"},
@@ -84,26 +60,9 @@ func TestFields(t *testing.T) {
 			},
 		},
 		{
-			name:  "check field names for DBChannel",
-			model: &storage.DBChannel{},
-			expected: []string{
-				"id", "cbsd_id", "low_frequency", "high_frequency",
-				"channel_type", "rule_applied", "max_eirp",
-			},
-		},
-		{
 			name:     "check field names for DBActiveModeConfig",
 			model:    &storage.DBActiveModeConfig{},
 			expected: []string{"id", "cbsd_id", "desired_state_id"},
-		},
-		{
-			name:  "check field names for DBLog",
-			model: &storage.DBLog{},
-			expected: []string{
-				"id", "network_id", "log_from", "log_to", "log_name",
-				"log_message", "cbsd_serial_number", "fcc_id",
-				"response_code", "created_date",
-			},
 		},
 	}
 	for _, tc := range testCases {
@@ -125,45 +84,6 @@ func TestGetMetadata(t *testing.T) {
 		model    db.Model
 		expected db.ModelMetadata
 	}{
-		{
-			name:  "check ModelMetadata structure for DBRequestType",
-			model: &storage.DBRequestType{},
-			expected: db.ModelMetadata{
-				Table:     storage.RequestTypeTable,
-				Relations: map[string]string{},
-			},
-		},
-		{
-			name:  "check ModelMetadata structure for DBRequestState",
-			model: &storage.DBRequestState{},
-			expected: db.ModelMetadata{
-				Table:     storage.RequestStateTable,
-				Relations: map[string]string{},
-			},
-		},
-		{
-			name:  "check ModelMetadata structure for DBRequest",
-			model: &storage.DBRequest{},
-			expected: db.ModelMetadata{
-				Table: storage.RequestTable,
-				Relations: map[string]string{
-					storage.RequestStateTable: "state_id",
-					storage.RequestTypeTable:  "type_id",
-					storage.CbsdTable:         "cbsd_id",
-				},
-			},
-		},
-		{
-			name:  "check ModelMetadata structure for DBResponse",
-			model: &storage.DBResponse{},
-			expected: db.ModelMetadata{
-				Table: storage.ResponseTable,
-				Relations: map[string]string{
-					storage.RequestTable: "request_id",
-					storage.GrantTable:   "grant_id",
-				},
-			},
-		},
 		{
 			name:  "check ModelMetadata structure for DBGrantState",
 			model: &storage.DBGrantState{},
@@ -202,16 +122,6 @@ func TestGetMetadata(t *testing.T) {
 			},
 		},
 		{
-			name:  "check ModelMetadata structure for DBChannel",
-			model: &storage.DBChannel{},
-			expected: db.ModelMetadata{
-				Table: storage.ChannelTable,
-				Relations: map[string]string{
-					storage.CbsdTable: "cbsd_id",
-				},
-			},
-		},
-		{
 			name:  "check ModelMetadata structure for DBActiveModeConfig",
 			model: &storage.DBActiveModeConfig{},
 			expected: db.ModelMetadata{
@@ -220,13 +130,6 @@ func TestGetMetadata(t *testing.T) {
 					storage.CbsdTable:      "cbsd_id",
 					storage.CbsdStateTable: "desired_state_id",
 				},
-			},
-		},
-		{
-			name:  "check ModelMetadata structure for DBLog",
-			model: &storage.DBLog{},
-			expected: db.ModelMetadata{
-				Table: storage.LogTable,
 			},
 		},
 	}
