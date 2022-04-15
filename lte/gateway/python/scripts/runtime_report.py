afaca71ef9bcb0fdf48bb8daee5fe3b55f9b026a
Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>
2022-04-15 16:00:03 +0200

    test(mme): update method to combine multiple test reports into a single file
    Signed-off-by: Hung NGUYEN <nguyen.thanh.hung.91@gmail.com>

--- lte/gateway/python/scripts/runtime_report.py
+++ lte/gateway/python/scripts/runtime_report.py
@@ -16,50 +16,77 @@ import argparse
 import glob
 import os
 from pathlib import Path
+from datetime import datetime
+from pprint import pprint
 import xml.etree.ElementTree as ET
 
-def parsing_and_display(report_list):
+
+def merge_all_report(list_xml_report_paths, output_path):
     """ 
-    Reading the XML report from google test, parsing the information and
-    displaying it in the desired format to determine the test takes the most time.  
+    Given the paths of all xml report files, merge them all into a single report files
+    Args:
+        list_xml_report_paths: list of all the report xml files
+        output_path : path of output report file in xml format 
     """
-    header = ['Testsuites', 'Testsuite', 'Testcase', 'Failed', 'Runtime']
-    reformatted_report = [header,]
-    for report_file_name in report_list:
-        test_result = ET.parse(report_file_name)
+    testsuites_node = ET.Element("testsuites")
+
+    testsuites_node.set("name", "AllTests")
+    num_all_failures = 0
+    num_all_disabled = 0
+    num_all_errors = 0
+    total_time = 0
+    num_all_tests = 0
+    init_time = datetime.max 
+    c = 0
+
+    ## iterate through files
+    for xml_file_path in list_xml_report_paths:
+        test_result = ET.parse(xml_file_path)
         test_suites_data = test_result.getroot()
-        testuites = [Path(report_file_name).stem, '', '', test_suites_data.attrib['failures'] + "/" + test_suites_data.attrib['tests'], test_suites_data.attrib['time']]
-        reformatted_report.append(testuites)
 
+        num_all_failures += int(test_suites_data.attrib['failures'])
+        num_all_tests+=int(test_suites_data.attrib['tests'])
+        total_time+=float(test_suites_data.attrib['time'])
+        num_all_errors+=int(test_suites_data.attrib['errors'])
+        num_all_disabled+=int(test_suites_data.attrib['disabled'])
+        init_time = min(init_time, datetime.fromisoformat(test_suites_data.attrib['timestamp']))
+        
         for single_test_suite_data in test_suites_data:
-            test_suite_name = single_test_suite_data.attrib['name']
-            number_failed_case = single_test_suite_data.attrib['failures'] + "/" + single_test_suite_data.attrib['tests']
-            reformatted_report.append(['', test_suite_name, '', number_failed_case, single_test_suite_data.attrib['time']])
-            for test_case in single_test_suite_data:
-                failed = '0/1' if test_case.attrib['result'] == 'completed' else '1/1'
-                reformatted_report.append(['', '', test_case.attrib['name'], failed, test_case.attrib['time']]) 
+            testsuites_node.append(single_test_suite_data)
 
-    max_len_per_col = [max(len(row[i]) for row in reformatted_report) for i in range(len(reformatted_report[0]))]
+    # adding node here
+    testsuites_node.set("timestamp", str(init_time))
+    testsuites_node.set("time", str(total_time))
+    testsuites_node.set("errors", str(num_all_errors))
+    testsuites_node.set("disabled", str(num_all_disabled))
+    testsuites_node.set("failures", str(num_all_failures))
+    testsuites_node.set("tests", str(num_all_tests))
 
-    format_row = ''.join(["{:>" + str(x + 3)+ "}"  for x in max_len_per_col])
-    print(' ')
-    print("RUNTIME REPORT:")
-    for row in reformatted_report:
-        print(format_row.format(*row))
-    print(" ")
+    tree = ET.ElementTree()
+    tree._setroot(testsuites_node)
+    tree.write(output_path)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("xml_report")
+
+    parser = argparse.ArgumentParser(description="Merging all the .xml unittest reports into a single file")
+    parser.add_argument('-i','--input', nargs='+', help='<Required> List of xml files', required=True)
+    parser.add_argument('-o','--output', help='Output of xml report', required=False)    
     args = parser.parse_args()
 
     print(f"Value of env variable GTEST_OUTPUT = {os.environ['GTEST_OUTPUT']}")
-    print(f"Processing test reports at : {args.xml_report}")
-
+    print(f"Processing test reports at :")
+    pprint(args.input)
     print("=" * 50)
-    print("NOTE: THIS REPORT IS ONLY FOR FINISHED TESTS. FOR THE ABORTED TESTS, WE DO NOT HAVE DATA TO REPORT!")
-    files = glob.glob(args.xml_report + "/*.xml")
-    if len(files) < 1:
+
+    output_path = args.output if args.output else "./report_all_tests.xml"
+    if len(args.input) < 1:
         print("No report is generated")
     else:
-        parsing_and_display(files)
+        merge_all_report(args.input, output_path)
+    print(f"Final report is generated at: {output_path}")
+    
+
+
+
+
+    
