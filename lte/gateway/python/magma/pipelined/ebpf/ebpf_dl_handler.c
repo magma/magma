802e92770e84ad63b8112ea854498300685da56c
Cameron Voisey <32741139+voisey@users.noreply.github.com>
2022-05-04 02:27:41 +0200

    fix(agw): Format lte/gateway/python with clang-format-11 (#12572)
    Signed-off-by: Cameron Voisey <cameron.voisey@tngtech.com>

--- lte/gateway/python/magma/pipelined/ebpf/ebpf_dl_handler.c
+++ lte/gateway/python/magma/pipelined/ebpf/ebpf_dl_handler.c
@@ -21,16 +21,15 @@
 
 // The map is pinned so that it can be accessed by pipelined or debugging tool
 // to examine datapath state.
-BPF_TABLE_PINNED(
-    "hash", struct dl_map_key, struct dl_map_info, dl_map, 1024 * 512,
-    "/sys/fs/bpf/dl_map");
+BPF_TABLE_PINNED("hash", struct dl_map_key, struct dl_map_info, dl_map,
+                 1024 * 512, "/sys/fs/bpf/dl_map");
 
 struct cfg_array_info {
   u32 if_idx;
 };
 
-BPF_TABLE_PINNED(
-    "array", u32, struct cfg_array_info, cfg_array, 1, "/sys/fs/bpf/cfg_array");
+BPF_TABLE_PINNED("array", u32, struct cfg_array_info, cfg_array, 1,
+                 "/sys/fs/bpf/cfg_array");
 
 // Ingress handler for Uplink traffic.
 int gtpu_egress_handler(struct __sk_buff* skb) {
@@ -42,20 +41,20 @@ int gtpu_egress_handler(struct __sk_buff* skb) {
   int ip_hdr = sizeof(struct ethhdr) + sizeof(struct iphdr);
 
   // 1. a. check pkt length
-  data     = (void*) (long) skb->data;
-  data_end = (void*) (long) skb->data_end;
-  int len  = data_end - data;
+  data = (void*)(long)skb->data;
+  data_end = (void*)(long)skb->data_end;
+  int len = data_end - data;
 
   if ((data + ip_hdr) > data_end) {
-    bpf_trace_printk(
-        "ERR: truncated packet: len: %d, data sz %d\n", skb->len, len);
+    bpf_trace_printk("ERR: truncated packet: len: %d, data sz %d\n", skb->len,
+                     len);
     return TC_ACT_OK;
   }
 
   // 1. b. check UE map
-  iph                          = data + sizeof(struct ethhdr);
+  iph = data + sizeof(struct ethhdr);
   struct dl_map_key lookup_key = {iph->daddr};
-  struct dl_map_info* fwd      = dl_map.lookup(&lookup_key);
+  struct dl_map_info* fwd = dl_map.lookup(&lookup_key);
   if (!fwd) {
     bpf_trace_printk("ERR: UE for IP %x not found\n", iph->daddr);
     return TC_ACT_OK;
@@ -65,17 +64,19 @@ int gtpu_egress_handler(struct __sk_buff* skb) {
   struct bpf_tunnel_key tun_info;
   __builtin_memset(&tun_info, 0x0, sizeof(tun_info));
   tun_info.remote_ipv4 = fwd->remote_ipv4;
-  tun_info.tunnel_id   = fwd->tunnel_id;
-  tun_info.tunnel_ttl  = 64;
+  tun_info.tunnel_id = fwd->tunnel_id;
+  tun_info.tunnel_ttl = 64;
 
-  ret = bpf_skb_set_tunnel_key(skb, &tun_info, sizeof(tun_info), BPF_F_ZERO_CSUM_TX);
+  ret = bpf_skb_set_tunnel_key(skb, &tun_info, sizeof(tun_info),
+                               BPF_F_ZERO_CSUM_TX);
   if (ret < 0) {
-      bpf_trace_printk("ERR: bpf_skb_set_tunnel_key failed with %d", ret);
-      return TC_ACT_SHOT;
+    bpf_trace_printk("ERR: bpf_skb_set_tunnel_key failed with %d", ret);
+    return TC_ACT_SHOT;
   }
-  bpf_trace_printk("INFO: set: key %d remote ip 0x%x ret = %d\n", tun_info.tunnel_id, tun_info.remote_ipv4, ret);
+  bpf_trace_printk("INFO: set: key %d remote ip 0x%x ret = %d\n",
+                   tun_info.tunnel_id, tun_info.remote_ipv4, ret);
 
-  u32  cfg_key   = 0;
+  u32 cfg_key = 0;
   struct cfg_array_info* cfg = cfg_array.lookup(&cfg_key);
   if (!cfg) {
     bpf_trace_printk("ERR: Config array lookup failed\n");
