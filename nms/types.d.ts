b71aa8cf76f4c1047654c65e8ecd22abd0c1d6fa
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-05-23 17:41:51 +0200

    refactor(nms): Migrate AppContext to TypeScript
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/types.d.ts
+++ nms/types.d.ts
@@ -9,14 +9,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @format
  */
 
-declare let global: {
-  CONFIG: {
-    appData: {
-      enabledFeatures: Array<string>;
+import {EmbeddedData} from './shared/types/embeddedData';
+
+declare global {
+  interface Window {
+    CONFIG: {
+      appData: EmbeddedData;
     };
-  };
-};
+  }
+}
