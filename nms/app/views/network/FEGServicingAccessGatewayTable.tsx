efe042bfeb8c101a30af19dc074e01acea760621
Thomas Schmitt <thomas.schmitt@tngtech.com>
2022-05-23 20:40:11 +0200

    refactor(nms): Migrate LoadingFiller to TypeScript
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/app/views/network/FEGServicingAccessGatewayTable.tsx
+++ nms/app/views/network/FEGServicingAccessGatewayTable.tsx
@@ -13,6 +13,7 @@
 
 import ActionTable from '../../components/ActionTable';
 import Link from '@material-ui/core/Link';
+// $FlowFixMe migrated to typescript
 import LoadingFiller from '../../components/LoadingFiller';
 import React, {useEffect, useState} from 'react';
 import nullthrows from '../../../shared/util/nullthrows';
