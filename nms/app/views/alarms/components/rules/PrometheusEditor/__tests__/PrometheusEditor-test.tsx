2405d6f90f71dfd53443806f67904090af08e513
Thomas Schmitt <thomas.schmitt@tngtech.com>
2022-06-02 17:36:02 +0200

    refactor(nms): Migrate PrometheusEditor-test to TypeScript
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/app/views/alarms/components/rules/PrometheusEditor/__tests__/PrometheusEditor-test.tsx
+++ nms/app/views/alarms/components/rules/PrometheusEditor/__tests__/PrometheusEditor-test.tsx
@@ -9,33 +9,24 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow
- * @format
  */
 
 import * as React from 'react';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import PrometheusEditor from '../PrometheusEditor';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import {alarmTestUtil} from '../../../../test/testHelpers';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+import {MockApiUtil, alarmTestUtil} from '../../../../test/testHelpers';
 import {parseTimeString} from '../PrometheusEditor';
 import {render} from '@testing-library/react';
 
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+import {TextFieldProps} from '@material-ui/core/TextField/TextField';
 import type {AlarmsWrapperProps} from '../../../../test/testHelpers';
-// $FlowFixMe migrated to typescript
 import type {AlertConfig} from '../../../AlarmAPIType';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import type {ApiUtil} from '../../../AlarmsApi';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import type {GenericRule} from '../../RuleInterface';
 
 // TextField select is difficult to test so replace it with an Input
 jest.mock('@material-ui/core/TextField', () => {
-  const Input = require('@material-ui/core/Input').default;
-  return ({children: _, InputProps: __, label, ...props}) => (
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+  const Input = jest.requireActual('@material-ui/core/Input').default;
+  return ({children, InputProps, label, ...props}: TextFieldProps) => (
     <label>
       {label}
       <Input {...props} />
@@ -44,8 +35,8 @@ jest.mock('@material-ui/core/TextField', () => {
 });
 
 describe('PrometheusEditor', () => {
-  let AlarmsWrapper: React.ComponentType<$Shape<AlarmsWrapperProps>>;
-  let apiUtil: ApiUtil;
+  let AlarmsWrapper: React.ComponentType<Partial<AlarmsWrapperProps>>;
+  let apiUtil: MockApiUtil;
 
   beforeEach(() => {
     ({apiUtil, AlarmsWrapper} = alarmTestUtil());
@@ -58,8 +49,8 @@ describe('PrometheusEditor', () => {
     onRuleSaved: jest.fn(),
   };
 
-  test('editing a threshold alert opens the PrometheusEditor with the threshold expression editor enabled', async () => {
-    jest.spyOn(apiUtil, 'getMetricSeries').mockResolvedValue([]);
+  test('editing a threshold alert opens the PrometheusEditor with the threshold expression editor enabled', () => {
+    jest.spyOn(apiUtil, 'getMetricSeries').mockReturnValue([]);
     const testThresholdRule: GenericRule<AlertConfig> = {
       severity: '',
       ruleType: '',
@@ -78,7 +69,7 @@ describe('PrometheusEditor', () => {
     expect(getByDisplayValue('123')).toBeInTheDocument();
   });
 
-  test('editing a non-threshold alert opens the PrometheusEditor with the advanced editor enabled', async () => {
+  test('editing a non-threshold alert opens the PrometheusEditor with the advanced editor enabled', () => {
     const testThresholdRule: GenericRule<AlertConfig> = {
       severity: '',
       ruleType: '',
@@ -104,7 +95,7 @@ describe('PrometheusEditor', () => {
       ['hour', '1h', {hours: 1, minutes: 0, seconds: 0}],
       ['minute', '1m', {hours: 0, minutes: 1, seconds: 0}],
       ['second', '1s', {hours: 0, minutes: 0, seconds: 1}],
-    ];
+    ] as const;
     test.each(testCases)('%s', (name, input, expectedDuration) => {
       expect(parseTimeString(input)).toEqual(expectedDuration);
     });
