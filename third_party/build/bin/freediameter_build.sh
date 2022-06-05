073cb3f51a9ec05ae7f0e8688028240b4d0f2ee6
Gustavo Junior Alves <gustavo@radtonics.com>
2022-06-05 14:58:10 -0300

    fix(agw): Fix freeDiameter patching
    Signed-off-by: Gustavo Junior Alves <gustavo@radtonics.com>

--- third_party/build/bin/freediameter_build.sh
+++ third_party/build/bin/freediameter_build.sh
@@ -59,7 +59,8 @@ git clone "${GIT_URL}" freediameter
 cd freediameter
 git checkout "${GIT_COMMIT}"
 
-patch -p1 < $MAGMA_ROOT/lte/gateway/c/oai/patches/0001-opencoord.org.freeDiameter.patch
+patch -p1 < $MAGMA_ROOT/lte/gateway/c/core/oai/patches/0001-opencoord.org.freeDiameter.patch
+patch -p1 < $MAGMA_ROOT/lte/gateway/c/core/oai/patches/0002-opencoord.org.freeDiameter.patch
 
 awk '{if (/^DISABLE_SCTP/) gsub(/OFF/, "ON"); print}' CMakeCache.txt > tmp && mv tmp CMakeCache.txt
 
