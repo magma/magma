a89615d9455774f57788bb7ff4e32c95c6173817
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-06-13 11:20:06 +0200

    refactor(nms): Migrate GatewayLogChart to TypeScript
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/app/views/equipment/GatewayLogChart.tsx
+++ nms/app/views/equipment/GatewayLogChart.tsx
@@ -9,41 +9,31 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow strict-local
- * @format
  */
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import type {Dataset, DatasetType} from '../../components/CustomMetrics';
 
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import CustomHistogram from '../../components/CustomMetrics';
-// $FlowFixMe migrated to typescript
 import LoadingFiller from '../../components/LoadingFiller';
-import MagmaV1API from '../../../generated/WebClient';
+import MagmaAPI from '../../../api/MagmaAPI';
 import React from 'react';
 import moment from 'moment';
-// $FlowFixMe migrated to typescript
 import nullthrows from '../../../shared/util/nullthrows';
-
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+import {TimeUnit} from 'chart.js';
 import {colors} from '../../theme/default';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import {getQueryRanges} from '../../components/CustomMetrics';
 import {useEffect, useState} from 'react';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import {useEnqueueSnackbar} from '../../../app/hooks/useSnackbar';
+import {useEnqueueSnackbar} from '../../hooks/useSnackbar';
 import {useParams} from 'react-router-dom';
+import type {Dataset, DatasetType} from '../../components/CustomMetrics';
 
 type Props = {
-  start: moment,
-  end: moment,
-  delta: number,
-  unit: string,
-  format: string,
-  setLogCount: number => void,
+  start: moment.Moment;
+  end: moment.Moment;
+  delta: number;
+  unit: TimeUnit;
+  format: string;
+  setLogCount: (arg0: number) => void;
 };
 
 export default function LogChart(props: Props) {
@@ -62,37 +52,40 @@ export default function LogChart(props: Props) {
     hoverBorderColor: 'black',
     data: [],
   });
-
   useEffect(() => {
-    // build queries
-    let requestError = '';
     const queries = getQueryRanges(start, end, delta, unit);
-    const requests = queries.map(async (query, _) => {
+    const requests = queries.map(async query => {
       try {
         const [s, e] = query;
-        const response = await MagmaV1API.getNetworksByNetworkIdLogsCount({
-          networkId: networkId,
-          filters: `gateway_id:${gatewayId}`,
-          start: s.toISOString(),
-          end: e.toISOString(),
-        });
+        const response = (
+          await MagmaAPI.logs.networksNetworkIdLogsCountGet({
+            networkId: networkId,
+            filters: `gateway_id:${gatewayId}`,
+            start: s.toISOString(),
+            end: e.toISOString(),
+          })
+        ).data;
         return response;
-      } catch (error) {
-        requestError = error;
+      } catch {
+        enqueueSnackbar('Error getting log counts', {variant: 'error'});
       }
+
       return null;
     });
 
     Promise.all(requests)
       .then(allResponses => {
         const data: Array<DatasetType> = allResponses.map((r, index) => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           const [_, e] = queries[index];
+
           if (r === null || r === undefined) {
             return {
               t: e.unix() * 1000,
               y: 0,
             };
           }
+
           return {
             t: e.unix() * 1000,
             y: r,
@@ -112,16 +105,10 @@ export default function LogChart(props: Props) {
         setLogCount(data.reduce((a, b) => a + b.y, 0));
         setIsLoading(false);
       })
-      .catch(error => {
-        requestError = error;
+      .catch(() => {
+        enqueueSnackbar('Error getting log counts', {variant: 'error'});
         setIsLoading(false);
       });
-
-    if (requestError) {
-      enqueueSnackbar('Error getting log counts', {
-        variant: 'error',
-      });
-    }
   }, [
     setLogCount,
     start,
