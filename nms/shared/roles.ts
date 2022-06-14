95707b8257733ca8a29084880ee66d7549d4b48a
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-06-14 15:42:04 +0200

    refactor(nms): Migrate shared/roles to TypeScript
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/shared/roles.ts
+++ nms/shared/roles.ts
@@ -9,19 +9,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow strict-local
- * @format
  */
 
 export type AccessRoleLevel = 0 | 1 | 3;
-
 export const AccessRoles = {
   USER: 0,
   READ_ONLY_USER: 1,
   SUPERUSER: 3,
 };
-
 export const UserRoles = {
   USER: 0,
   READ_ONLY_USER: 1,
