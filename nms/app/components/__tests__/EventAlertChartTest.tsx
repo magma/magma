facd35eac80bf833b54214fd06fdb2caeba95b3a
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-06-15 11:50:17 +0200

    refactor(nms): Migrate EventAlertChartTest to TypeScript
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/app/components/__tests__/EventAlertChartTest.tsx
+++ nms/app/components/__tests__/EventAlertChartTest.tsx
@@ -9,25 +9,21 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow strict-local
- * @format
  */
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+
 import EventAlertChart from '../EventAlertChart';
-import MagmaAPIBindings from '../../../generated/MagmaAPIBindings';
+import MagmaAPI from '../../../api/MagmaAPI';
 import MuiStylesThemeProvider from '@material-ui/styles/ThemeProvider';
 import React from 'react';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import defaultTheme from '../../theme/default';
 import moment from 'moment';
-
 import {MemoryRouter, Route, Routes} from 'react-router-dom';
 import {MuiThemeProvider} from '@material-ui/core/styles';
+import {mockAPI} from '../../util/TestUtils';
 import {render, wait} from '@testing-library/react';
-import type {promql_return_object} from '../../../generated/MagmaAPIBindings';
+import type {PromqlReturnObject} from '../../../generated-ts';
 
-const mockMetricSt: promql_return_object = {
+const mockMetricSt: PromqlReturnObject = {
   status: 'success',
   data: {
     resultType: 'matrix',
@@ -41,20 +37,22 @@ const mockMetricSt: promql_return_object = {
 };
 
 jest.mock('axios');
-jest.mock('../../../generated/MagmaAPIBindings');
 jest.mock('../../../app/hooks/useSnackbar');
 
 // chart component was failing here so mocking this out
 // this shouldn't affect the prop verification part in the react
 // chart component
+// @ts-ignore
 window.HTMLCanvasElement.prototype.getContext = () => {};
 
 describe('<EventAlertChart/>', () => {
   beforeEach(() => {
-    // eslint-disable-next-line max-len
-    MagmaAPIBindings.getNetworksByNetworkIdPrometheusQueryRange.mockResolvedValue(
+    mockAPI(
+      MagmaAPI.metrics,
+      'networksNetworkIdPrometheusQueryRangeGet',
       mockMetricSt,
     );
+    mockAPI(MagmaAPI.events, 'eventsNetworkIdAboutCountGet');
   });
 
   const testCases = [
@@ -97,31 +95,22 @@ describe('<EventAlertChart/>', () => {
         </MuiThemeProvider>
       </MemoryRouter>
     );
+
     render(<Wrapper />);
     await wait();
-    if (tc.valid) {
-      expect(
-        MagmaAPIBindings.getNetworksByNetworkIdPrometheusQueryRange,
-      ).toHaveBeenCalledTimes(1);
-      expect(
-        MagmaAPIBindings.getNetworksByNetworkIdPrometheusQueryRange.mock
-          .calls[0][0].start,
-      ).toEqual(tc.startDate.toISOString());
-      expect(
-        MagmaAPIBindings.getNetworksByNetworkIdPrometheusQueryRange.mock
-          .calls[0][0].end,
-      ).toEqual(tc.endDate.toISOString());
-      expect(
-        MagmaAPIBindings.getNetworksByNetworkIdPrometheusQueryRange.mock
-          .calls[0][0].step,
-      ).toEqual(tc.step);
-    } else {
-      // negative test for invalid start end use default timerange
-      const defaultStep = '5m';
-      expect(
-        MagmaAPIBindings.getNetworksByNetworkIdPrometheusQueryRange.mock
-          .calls[0][0].step,
-      ).toEqual(defaultStep);
-    }
+
+    const currentStep = tc.valid ? tc.step : '5m';
+    expect(
+      MagmaAPI.metrics.networksNetworkIdPrometheusQueryRangeGet,
+    ).toHaveBeenCalledTimes(1);
+    expect(
+      MagmaAPI.metrics.networksNetworkIdPrometheusQueryRangeGet,
+    ).toBeCalledWith({
+      start: tc.startDate.toISOString(),
+      end: tc.endDate.toISOString(),
+      step: currentStep,
+      networkId: 'mynetwork',
+      query: 'sum(ALERTS)',
+    });
   });
 });
