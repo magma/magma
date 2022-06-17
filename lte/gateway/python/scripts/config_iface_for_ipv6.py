82b26e266734345bf7a98e960ca66c8c06d634b6
Pruthvi Hebbani <pruthvi.hebbani@radisys.com>
2022-06-17 15:43:01 +0530

    Addressed review comments
    Signed-off-by: Pruthvi Hebbani <pruthvi.hebbani@radisys.com>

--- lte/gateway/python/scripts/config_iface_for_ipv6.py
+++ lte/gateway/python/scripts/config_iface_for_ipv6.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Copyright 2020 The Magma Authors.
+Copyright 2022 The Magma Authors.
 
 This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree.
