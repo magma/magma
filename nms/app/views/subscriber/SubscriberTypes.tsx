91283698e780fcf84d53954c63660eed3416b457
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-06-20 11:40:25 +0200

    refactor(nms): Migrate SubscriberTypes to TypeScript
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/app/views/subscriber/SubscriberTypes.tsx
+++ nms/app/views/subscriber/SubscriberTypes.tsx
@@ -9,95 +9,89 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow strict-local
- * @format
  */
 
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Link from '@material-ui/core/Link';
+import React from 'react';
 import ReactJson from 'react-json-view';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import SubscriberContext from '../../components/context/SubscriberContext';
 import {useContext} from 'react';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import type {SubscriberActionType, SubscriberInfo} from './SubscriberUtils';
-import type {
-  mutable_subscriber,
-  subscriber,
-} from '../../../generated/MagmaAPIBindings';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import Link from '@material-ui/core/Link';
 import {useNavigate} from 'react-router-dom';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+import type {MutableSubscriber, Subscriber} from '../../../generated-ts';
+import type {SubscriberActionType, SubscriberInfo} from './SubscriberUtils';
 import type {SubscriberRowType} from '../../state/lte/SubscriberState';
 
 export type subscriberStaticIpsRowType = {
-  apnName: string,
-  staticIp: string,
+  apnName: string;
+  staticIp: string;
 };
 
 export type subscriberForbiddenNetworkTypes = {
-  nwTypes: string,
+  nwTypes: string;
 };
 
 export type EditSubscriberProps = {
-  subscriberState: subscriber,
-  onSubscriberChange: (key: string, val: string | number | {}) => void,
-  inputClass: string,
+  subscriberState: Subscriber;
+  onSubscriberChange: (key: string, val: string | number | undefined) => void;
+  inputClass: string;
   onTrafficPolicyChange: (
     key: string,
-    val: string | number | {},
+    val: string | number | undefined,
     index: number,
-  ) => void,
-  onDeleteApn: (apn: {}) => void,
-  onAddApnStaticIP: () => void,
-  subProfiles: {},
-  subscriberStaticIPRows: Array<subscriberStaticIpsRowType>,
-  forbiddenNetworkTypes: Array<subscriberForbiddenNetworkTypes>,
-  authKey: string,
-  authOpc: string,
-  setAuthKey: (key: string) => void,
-  setAuthOpc: (key: string) => void,
+  ) => void;
+  onDeleteApn: (apn: undefined) => void;
+  onAddApnStaticIP: () => void;
+  subProfiles: undefined;
+  subscriberStaticIPRows: Array<subscriberStaticIpsRowType>;
+  forbiddenNetworkTypes: Array<subscriberForbiddenNetworkTypes>;
+  authKey: string;
+  authOpc: string;
+  setAuthKey: (key: string) => void;
+  setAuthOpc: (key: string) => void;
 };
 
 export type SubscribersDialogDetailProps = {
   // Subscribers to add, edit or delete
-  setSubscribers: (Array<SubscriberInfo>) => void,
-  subscribers: Array<SubscriberInfo>,
+  setSubscribers: (arg0: Array<SubscriberInfo>) => void;
+  subscribers: Array<SubscriberInfo>;
   // Formatting error (eg: field missing, wrong IMSI format)
-  setAddError: (Array<string>) => void,
-  addError: Array<string>,
+  setAddError: (arg0: Array<string>) => void;
+  addError: Array<string>;
   // Display dropzone if set to true
-  setUpload: boolean => void,
-  upload: boolean,
-  onClose: () => void,
+  setUpload: (arg0: boolean) => void;
+  upload: boolean;
+  onClose: () => void;
   // Add, edit or delete subscribers
-  onSave: (Array<SubscriberInfo>, selectedSubscribers?: Array<string>) => void,
-  error?: string,
+  onSave: (
+    arg0: Array<SubscriberInfo>,
+    selectedSubscribers?: Array<string>,
+  ) => void;
+  error?: string;
   // Row added with the Add New Row button
-  rowAdd: boolean,
-  setRowAdd: boolean => void,
+  rowAdd: boolean;
+  setRowAdd: (arg0: boolean) => void;
   // Delete, Edit or Add subscriber
-  subscriberAction: SubscriberActionType,
+  subscriberAction: SubscriberActionType;
 };
 
 type JsonProps = {
-  open: boolean,
-  onClose?: () => void,
-  imsi: string,
+  open: boolean;
+  onClose?: () => void;
+  imsi: string;
 };
 
 export function JsonDialog(props: JsonProps) {
   const ctx = useContext(SubscriberContext);
   const sessionState = ctx.sessionState[props.imsi] || {};
   const configuredSubscriberState = ctx.state[props.imsi];
-  const subscriber: mutable_subscriber = {
+  // TODO[ts-migration] The state composition needs attention in more detail
+  const subscriber: MutableSubscriber = {
     ...configuredSubscriberState,
     state: sessionState,
-  };
+  } as MutableSubscriber;
   return (
     <Dialog open={props.open} onClose={props.onClose} fullWidth={true}>
       <DialogTitle>{props.imsi}</DialogTitle>
@@ -113,8 +107,8 @@ export function JsonDialog(props: JsonProps) {
 }
 
 type RenderLinkType = {
-  subscriberConfig: subscriber,
-  currRow: SubscriberRowType,
+  subscriberConfig: Subscriber;
+  currRow: SubscriberRowType;
 };
 
 export function RenderLink(props: RenderLinkType) {
