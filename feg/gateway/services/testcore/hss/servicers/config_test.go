c1cb7adce599b591ac7f3f55d882a47dd823558f
Christian Krämer <christian.kraemer@tngtech.com>
2022-06-21 14:03:44 +0200

    chore(feg) Added test for configuration loading. Refactored hss main method into smaller pieces.
    Signed-off-by: Christian Kraemer <christian.kraemer@tngtech.com>

---
+++ feg/gateway/services/testcore/hss/servicers/config_test.go
@@ -0,0 +1,47 @@
+package servicers_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"magma/feg/cloud/go/protos/mconfig"
+	"magma/feg/gateway/services/testcore/hss/servicers"
+	gwMconfig "magma/gateway/mconfig"
+)
+
+func TestGetHssProxyConfig(t *testing.T) {
+	conf := generateHssMconfig(t, test_config)
+	assert.Equal(t, "magma-fedgw.magma.com", conf.Server.DestHost)
+}
+
+func generateHssMconfig(t *testing.T, configString string) *mconfig.HSSConfig {
+	err := gwMconfig.CreateLoadTempConfig(configString)
+	assert.NoError(t, err)
+	config, err := servicers.GetHSSConfig()
+	assert.NoError(t, err)
+	return config
+}
+
+var (
+	test_config = `{
+	  "configsByKey": {
+		"hss": {
+		  "@type": "type.googleapis.com/magma.mconfig.HSSConfig",
+		  "server": {
+			"protocol": "tcp",
+			"address": "localhost:3768",
+			"local_address": "localhost:3767",
+			"dest_host": "magma-fedgw.magma.com",
+			"dest_realm": "magma.com"
+		  },
+		  "lte_auth_op": "",
+		  "lte_auth_amf": "",
+		  "default_sub_profile": {
+			"max_ul_bit_rate": 10000000,
+			"max_dl_bit_rate": 20000000
+		  }
+		}
+	  }
+	}`
+)
