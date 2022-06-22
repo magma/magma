f82a1762375ce4a9520cd1b51d0edf0c31203cd9
Thomas Schmitt <thomas.schmitt@tngtech.com>
2022-06-22 10:30:30 +0200

    refactor(nms): Migrate Section to TypeScript
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/app/components/layout/useSections.js
+++ nms/app/components/layout/useSections.js
@@ -17,7 +17,8 @@
 import type {NetworkContextType} from '../context/NetworkContext';
 // $FlowFixMe migrated to typescript
 import type {NetworkType} from '../../../shared/types/network';
-import type {SectionsConfigs} from '../layout/Section';
+// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+import type {SectionsConfigs} from './Section';
 
 // $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import AppContext from '../../../app/components/context/AppContext';
