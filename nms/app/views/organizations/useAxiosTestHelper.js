3604623c6dc8840c68af3a71c5240742da8e9ea4
Thomas Schmitt <thomas.schmitt@tngtech.com>
2022-06-22 08:57:35 +0200

    refactor(nms): Add missing FlowFixMe after rebase
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/app/views/organizations/useAxiosTestHelper.js
+++ nms/app/views/organizations/useAxiosTestHelper.js
@@ -14,6 +14,7 @@
  * @format
  */
 
+// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import useAxios from '../../hooks/useAxios';
 
 // $FlowIgnore because responses contains multiple types
