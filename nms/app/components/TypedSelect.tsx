b5acde6fa1ebe600cf4c4b4c12cbe37ac7687fa7
Thomas Schmitt <thomas.schmitt@tngtech.com>
2022-06-22 19:08:24 +0200

    refactor(nms): Migrate PolicyRuleEditDialog to TypeScript
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/app/components/TypedSelect.tsx
+++ nms/app/components/TypedSelect.tsx
@@ -14,6 +14,7 @@
 import MenuItem from '@material-ui/core/MenuItem';
 import React from 'react';
 import Select from '@material-ui/core/Select';
+import {InputBaseComponentProps} from '@material-ui/core/InputBase/InputBase';
 
 type Props<T extends string | number> = {
   value: T;
@@ -23,6 +24,7 @@ type Props<T extends string | number> = {
   input?: React.ReactElement<any, any>;
   disabled?: boolean;
   fullWidth?: boolean;
+  inputProps?: InputBaseComponentProps;
 };
 
 export default function TypedSelect<T extends string | number>(
