ec297a877ed9b65aca26d22e3c570ea8bb125af0
Varun Madathil <varun27@fb.com>
2022-06-23 16:02:21 -0700

    removing unnecessary logs in ul
    Signed-off-by: Varun Madathil <varun27@fb.com>

--- lte/cloud/go/services/eps_authentication/servicers/ul.go
+++ lte/cloud/go/services/eps_authentication/servicers/ul.go
@@ -58,7 +58,6 @@ func (srv *EPSAuthServer) UpdateLocation(
 
 
 	networkID, err := identity.GetClientNetworkID(ctx)
-	glog.V(2).Infof("Network id is '%s'", networkID)
 	if err != nil {
 		glog.V(2).Infof("could not lookup networkID: %v", err.Error())
 		metrics.NetworkIDErrors.Inc()
@@ -66,15 +65,12 @@ func (srv *EPSAuthServer) UpdateLocation(
 	}
 
 	config, err := getConfig(networkID)
-	glog.Info("Config id is '%s'", config)
 	if err != nil {
 		glog.V(2).Infof("could not lookup config for networkID '%s': %v", networkID, err.Error())
 		metrics.ConfigErrors.Inc()
 		return nil, err
 	}
-
 	subscriber, errorCode, err := srv.lookupSubscriber(ulr.UserName, networkID)
-	glog.Info("Subscriber  is '%s'", subscriber)
 	if err != nil {
 		glog.V(2).Infof("failed to lookup subscriber '%s': %v", ulr.UserName, err.Error())
 		metrics.UnknownSubscribers.Inc()
