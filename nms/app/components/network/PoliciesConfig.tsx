c8b1f239ec0ca94e7184cd6048ce606b885f74e6
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-06-23 17:05:35 +0200

    refactor(nms): Migrate PoliciesConfig to TypeScript
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/app/components/network/PoliciesConfig.tsx
+++ nms/app/components/network/PoliciesConfig.tsx
@@ -9,29 +9,18 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow
- * @format
  */
 
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
-import type {WithAlert} from '../Alert/withAlert';
-// $FlowFixMe migrated to typescript
 import LoadingFiller from '../LoadingFiller';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import LoadingFillerBackdrop from '../LoadingFillerBackdrop';
-import MagmaV1API from '../../../generated/WebClient';
-// $FlowFixMe migrated to typescript
+import MagmaAPI from '../../../api/MagmaAPI';
 import NestedRouteLink from '../NestedRouteLink';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import PolicyBaseNameDialog from './PolicyBaseNameDialog';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import PolicyRuleEditDialog from './PolicyRuleEditDialog';
 import React from 'react';
 import Table from '@material-ui/core/Table';
@@ -39,19 +28,16 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import Text from '../../theme/design-system/Text';
 import Toolbar from '@material-ui/core/Toolbar';
-
-// $FlowFixMe migrated to typescript
 import nullthrows from '../../../shared/util/nullthrows';
-import useMagmaAPI from '../../../api/useMagmaAPIFlow';
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
+import useMagmaAPI from '../../../api/useMagmaAPI';
 import withAlert from '../Alert/withAlert';
 import {Route, Routes, useNavigate, useParams} from 'react-router-dom';
 import {findIndex} from 'lodash';
 import {makeStyles} from '@material-ui/styles';
 import {useState} from 'react';
+import type {WithAlert} from '../Alert/withAlert';
 
 const useStyles = makeStyles(() => ({
   header: {
@@ -66,22 +52,27 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
   const classes = useStyles();
   const navigate = useNavigate();
   const params = useParams();
-  const [ruleIDs, setRuleIDs] = useState();
-  const [baseNames, setBaseNames] = useState();
-
+  const [ruleIDs, setRuleIDs] = useState<Array<string>>();
+  const [baseNames, setBaseNames] = useState<Array<string>>();
   const networkID = nullthrows(params.networkId);
+
   useMagmaAPI(
-    MagmaV1API.getNetworksByNetworkIdPoliciesRules,
-    {networkId: networkID},
+    MagmaAPI.policies.networksNetworkIdPoliciesRulesGet,
+    {
+      networkId: networkID,
+    },
     setRuleIDs,
   );
   useMagmaAPI(
-    MagmaV1API.getNetworksByNetworkIdPoliciesBaseNames,
-    {networkId: networkID},
+    MagmaAPI.policies.networksNetworkIdPoliciesBaseNamesGet,
+    {
+      networkId: networkID,
+    },
     setBaseNames,
   );
+
   const {response: qosProfiles} = useMagmaAPI(
-    MagmaV1API.getLteByNetworkIdPolicyQosProfiles,
+    MagmaAPI.policies.lteNetworkIdPolicyQosProfilesGet,
     {
       networkId: networkID,
     },
@@ -91,7 +82,7 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
     return <LoadingFiller />;
   }
 
-  const onDelete = id => {
+  const onDelete = (id: string) => {
     const newRuleIDs = [...nullthrows(ruleIDs)];
     newRuleIDs.splice(
       findIndex(newRuleIDs, id2 => id2 === id),
@@ -100,7 +91,7 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
     setRuleIDs(newRuleIDs);
   };
 
-  const deleteBaseName = async name => {
+  const deleteBaseName = async (name: string) => {
     const confirmed = await props.confirm(
       `Are you sure you want to remove the base name "${name}"?`,
     );
@@ -114,11 +105,15 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
       ];
 
       if (props.mirrorNetwork) {
-        data.push({networkId: props.mirrorNetwork, baseName: name});
+        data.push({
+          networkId: props.mirrorNetwork,
+          baseName: name,
+        });
       }
+
       await Promise.all(
         data.map(d =>
-          MagmaV1API.deleteNetworksByNetworkIdPoliciesBaseNamesByBaseName(d),
+          MagmaAPI.policies.networksNetworkIdPoliciesBaseNamesBaseNameDelete(d),
         ),
       );
 
@@ -166,10 +161,6 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
               qosProfiles={qosProfiles ?? {}}
               mirrorNetwork={props.mirrorNetwork}
               onCancel={() => navigate('')}
-              onSave={ruleID => {
-                setRuleIDs([...nullthrows(ruleIDs), ruleID]);
-                navigate('');
-              }}
             />
           }
         />
@@ -202,7 +193,7 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
                     <EditIcon />
                   </IconButton>
                 </NestedRouteLink>
-                <IconButton onClick={() => deleteBaseName(name)}>
+                <IconButton onClick={() => void deleteBaseName(name)}>
                   <DeleteIcon />
                 </IconButton>
               </TableCell>
@@ -239,25 +230,28 @@ function PoliciesConfig(props: WithAlert & {mirrorNetwork?: string}) {
 }
 
 type Props = WithAlert & {
-  ruleID: string,
-  onDelete: () => void,
-  mirrorNetwork?: string,
+  ruleID: string;
+  onDelete: (id: string) => void;
+  mirrorNetwork?: string;
 };
 
 const RuleRow = withAlert((props: Props) => {
-  const [lastRefreshTime, setLastRefreshTime] = useState(new Date().getTime());
+  const [lastRefreshTime] = useState(new Date().getTime());
   const navigate = useNavigate();
   const params = useParams();
   const networkID = nullthrows(params.networkId);
 
   const {response: rule} = useMagmaAPI(
-    MagmaV1API.getNetworksByNetworkIdPoliciesRulesByRuleId,
-    {networkId: networkID, ruleId: encodeURIComponent(props.ruleID)},
+    MagmaAPI.policies.networksNetworkIdPoliciesRulesRuleIdGet,
+    {
+      networkId: networkID,
+      ruleId: encodeURIComponent(props.ruleID),
+    },
     undefined,
     lastRefreshTime,
   );
   const {response: qosProfiles} = useMagmaAPI(
-    MagmaV1API.getLteByNetworkIdPolicyQosProfiles,
+    MagmaAPI.policies.lteNetworkIdPolicyQosProfilesGet,
     {
       networkId: networkID,
     },
@@ -280,19 +274,21 @@ const RuleRow = withAlert((props: Props) => {
         ruleId: props.ruleID,
       },
     ];
+
     if (props.mirrorNetwork) {
       data.push({
         networkId: props.mirrorNetwork,
         ruleId: props.ruleID,
       });
     }
+
     await Promise.all(
       data.map(d =>
-        MagmaV1API.deleteNetworksByNetworkIdPoliciesRulesByRuleId(d),
+        MagmaAPI.policies.networksNetworkIdPoliciesRulesRuleIdDelete(d),
       ),
     );
 
-    props.onDelete();
+    props.onDelete(props.ruleID);
   };
 
   const editPath = `edit/${props.ruleID}/`;
@@ -308,7 +304,7 @@ const RuleRow = withAlert((props: Props) => {
             <EditIcon />
           </IconButton>
         </NestedRouteLink>
-        <IconButton onClick={onDeleteRule}>
+        <IconButton onClick={() => void onDeleteRule()}>
           <DeleteIcon />
         </IconButton>
         <Routes>
@@ -321,10 +317,6 @@ const RuleRow = withAlert((props: Props) => {
                   mirrorNetwork={props.mirrorNetwork}
                   rule={rule}
                   onCancel={() => navigate('')}
-                  onSave={() => {
-                    setLastRefreshTime(new Date().getTime());
-                    navigate('');
-                  }}
                 />
               ) : (
                 <LoadingFillerBackdrop />
