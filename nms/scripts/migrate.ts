1f8047e754bcad72db88a830d1c123c1469a53dd
Thomas Schmitt <thomas.schmitt@tngtech.com>
2022-06-27 17:26:07 +0200

    refactor(nms): Migrate migrate.js, runMigrations and sequelizerc to TypeScript
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/scripts/migrate.ts
+++ nms/scripts/migrate.ts
@@ -9,25 +9,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @flow strict-local
- * @format
  */
 
-// $FlowFixMe[cannot-resolve-module] for TypeScript migration
 import {LOG_FORMAT, LOG_LEVEL} from '../config/config';
 
 // This must be done before any module imports to configure
 // logging correctly
-// $FlowFixMe migrated to typescript
-const logging = require('../shared/logging.ts');
+import logging from '../shared/logging';
 logging.configure({
   LOG_FORMAT,
   LOG_LEVEL,
 });
 
-const {runMigrations} = require('./runMigrations');
+import {runMigrations} from './runMigrations';
 
 runMigrations()
-  .then(_ => console.log('Ran migrations successfully'))
+  .then(() => console.log('Ran migrations successfully'))
   .catch(err => console.error('Failed to run migrations', err));
