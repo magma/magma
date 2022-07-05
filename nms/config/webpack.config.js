22437fa4d12bf36b56f7a66e7798bd37b6aa4aa9
Thomas Schmitt <102796295+thmsschmitt@users.noreply.github.com>
2022-07-05 14:37:09 +0200

    refactor(nms): Add TypeScript support (#13194)
    Signed-off-by: Thomas Schmitt <thomas.schmitt@tngtech.com>

--- nms/config/webpack.config.js
+++ nms/config/webpack.config.js
@@ -29,6 +29,9 @@ module.exports = {
     login: entry([paths.loginJs]),
     host: entry([paths.hostJs]),
   },
+  resolve: {
+    extensions: ['.ts','.tsx', '.js', '.json']
+  },
   externals: [
     {
       xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}',
@@ -62,7 +65,7 @@ module.exports = {
           },
           // Process JS with Babel.
           {
-            test: /\.(js|jsx|mjs)$/,
+            test: /\.(js|jsx|mjs|ts|tsx)$/,
             loader: require.resolve('babel-loader'),
             options: {
               rootMode: 'upward',
@@ -112,7 +115,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file"
             // loader. Also exclude `html` and `json` extensions so they get
             // processed by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs|ts|tsx)$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- nms/config/webpack.production.config.js
+++ nms/config/webpack.production.config.js
@@ -31,6 +31,9 @@ module.exports = {
     login: [paths.loginJs],
     host: [paths.hostJs],
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
   externals: [
     {
       xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}',
@@ -65,7 +68,7 @@ module.exports = {
               // rebuilds.
               cacheDirectory: true,
             },
-            test: /\.(js|jsx|mjs)$/,
+            test: /\.(js|jsx|mjs|ts|tsx)$/,
           },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as
@@ -106,7 +109,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file"
             // loader. Also exclude `html` and `json` extensions so they get
             // processed by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs|ts|tsx)$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
