c39dcc8939636618af478a0dcfcac09912e14892
Pruthvi Hebbani <pruthvi.hebbani@radisys.com>
2022-07-11 10:26:42 +0530

    cpp migration
    Signed-off-by: Pruthvi Hebbani <pruthvi.hebbani@radisys.com>

--- lte/gateway/c/core/oai/tasks/sgw/pgw_handlers.cpp
+++ lte/gateway/c/core/oai/tasks/sgw/pgw_handlers.cpp
@@ -34,8 +34,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include "lte/gateway/c/core/oai/common/log.h"
 #include "lte/gateway/c/core/oai/common/common_types.h"
+#include "lte/gateway/c/core/oai/common/log.h"
 #include "lte/gateway/c/core/common/assertions.h"
 #include "lte/gateway/c/core/common/dynamic_memory_check.h"
 #include "lte/gateway/c/core/oai/include/sgw_context_manager.h"
