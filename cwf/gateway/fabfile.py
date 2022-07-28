f600893a40b0830c9841e068b4c95bb0e030ff7b
Marco Pfirrmann <marco.pfirrmann@tngtech.com>
2022-07-28 08:17:58 +0200

    chore(agw): `magma_trfserver` is upgraded to Ubuntu 20.04 (#13427)
    * test(agw): magma_trfserver playbooks are adapted for Ubuntu 20.04
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    * test(cwg): starting the trfserver is adapted for Ubuntu
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    * test(agw): general packing of Ubuntu focal VMs is improved
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    * test(agw): packing of magma_trfserver is adapted to Ubuntu 20.04
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    * test(agw): Vagrantfile is adapted to Ubuntu focal trfserver
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    * test(agw): Add license files for adapted packer scripts
    
    Signed-off-by: Sebastian Thomas <sebastian.thomas@tngtech.com>
    
    * chore(agw): versions of cached vagrant boxes are adapted to new trfserver version
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    * chore(agw): additional packing files are movet into separate folders
    
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>
    
    Co-authored-by: Sebastian Thomas <sebastian.thomas@tngtech.com>

--- cwf/gateway/fabfile.py
+++ cwf/gateway/fabfile.py
@@ -459,7 +459,7 @@ def _start_ue_simulator():
 
 def _start_trfserver():
     """ Starts the traffic gen server"""
-    run('nohup iperf3 -s --json -B %s > /dev/null &' % TRF_SERVER_IP, pty=False)
+    run('nohup iperf3 -s --json -B %s > /dev/null &' % TRF_SERVER_IP)
 
 
 def _run_unit_tests():
