34d80388c9a232e0ae67b6b66476068f6a89b212
Sagar Arora <sagar.arora@eurecom.fr>
2022-08-18 18:22:43 +0200

    feat(ci): Publishing feg-integ test result in firebase
    Signed-off-by: Sagar Arora <sagar.arora@eurecom.fr>

--- ci-scripts/firebase_publish_report.py
+++ ci-scripts/firebase_publish_report.py
@@ -74,6 +74,25 @@ def lte_integ_test(args):
     publish_report('lte_integ_test', args.build_id, verdict, report)
 
 
+def feg_integ_test(args):
+    """Prepare and publish FEG Integ Test report"""
+    report = url_to_html_redirect(args.run_id, args.url)
+    # Possible args.verdict values are success, failure, or canceled
+    verdict = 'inconclusive'
+
+    # As per the recent change, CI process runs all integ tests ignoring the
+    # failing test cases, because of which CI report always shows feg integ
+    # test as success. Here we read the CI status from file for more accurate
+    # feg integ test execution status
+    if os.path.exists("test_status.txt"):
+        with open('test_status.txt', 'r') as file:
+            status_file_content = file.read().rstrip()
+            expected_verdict_list = ["pass", "fail"]
+            if status_file_content in expected_verdict_list:
+                verdict = status_file_content
+    publish_report('feg_integ_test', args.build_id, verdict, report)
+
+
 def cwf_integ_test(args):
     """Prepare and publish CWF Integ Test report"""
     report = url_to_html_redirect(args.run_id, args.url)
