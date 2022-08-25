92fae0215f7540139c5196a3c64158e914de1ac1
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-08-25 16:52:05 +0530

    removed pd from helm test file
    Signed-off-by: Shubham Tatvamasi <shubhamtatvamasi@gmail.com>

--- orc8r/cloud/deploy/orc8r_deployer/docker/root/scripts/test_helm_charts_sync.py
+++ orc8r/cloud/deploy/orc8r_deployer/docker/root/scripts/test_helm_charts_sync.py
@@ -26,7 +26,6 @@ charts_fn_map = {
     'feg-orc8r': 'feg/cloud/helm/feg-orc8r/Chart.yaml',
     'lte-orc8r': 'lte/cloud/helm/lte-orc8r/Chart.yaml',
     'cwf-orc8r': 'cwf/cloud/helm/cwf-orc8r/Chart.yaml',
-    'domain-proxy': 'dp/cloud/helm/dp/charts/domain-proxy/Chart.yaml',
 }
 
 def read_all_chart_versions(constants: dict) -> dict:
