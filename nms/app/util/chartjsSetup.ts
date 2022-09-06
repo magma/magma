5635e197e7ab22b60452df5c21228cdc731dfaed
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-09-06 14:47:48 +0200

    refactor(nms): use date-fns instead of moment for chart.js
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- nms/app/util/chartjsSetup.ts
+++ nms/app/util/chartjsSetup.ts
@@ -10,7 +10,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import 'chartjs-adapter-moment';
+import 'chartjs-adapter-date-fns';
 
 import {
   ArcElement,
