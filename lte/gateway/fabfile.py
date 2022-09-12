86f32501c36e756083ef58fea2ecdcabb5e33e7c
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2022-09-12 16:29:05 +0200

    chore(ci): testing new workflow for containerized AGW - test_VM test + typo
    Signed-off-by: Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>

--- lte/gateway/fabfile.py
+++ lte/gateway/fabfile.py
@@ -545,12 +545,12 @@ def integ_test_containerized(
 
     # Setup the trfserver: use the provided trfserver if given, else default to the
     # vagrant machine
-    _setup_vm(trf_host, "magma_trfserver", "trfserver", "magma_trfserver.yml", destroy_vm, provision_vm)
-    execute(_start_trfserver)
+    #_setup_vm(trf_host, "magma_trfserver", "trfserver", "magma_trfserver.yml", destroy_vm, provision_vm)
+    #execute(_start_trfserver)
 
     # Run the tests: use the provided test machine if given, else default to
     # the vagrant machine
-    #_setup_vm(test_host, "magma_test", "test", "magma_test.yml", destroy_vm, provision_vm)
+    _setup_vm(test_host, "magma_test", "test", "magma_test.yml", destroy_vm, provision_vm)
     #execute(_make_integ_tests)
     #execute(_run_integ_tests, gateway_ip)
 
