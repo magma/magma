d5d14d9277e46b986754b1f40434f7ac4d4d678d
Moritz Huebner <moritz.huebner@tngtech.com>
2022-09-26 14:09:22 +1000

    feat(ci): Add docker promote script and workflow
    Taken from @ShubhamTatvamasi's repo (https://github.com/ShubhamTatvamasi/magma-artifactory-docker-promote/blob/master/.github/workflows/docker-promote.yml).
    
    Signed-off-by: Moritz Huebner <moritz.huebner@tngtech.com>

---
+++ orc8r/tools/docker/promote.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -ex
+
+MAGMA_TAG=$0
+NEW_MAGMA_TAG=$1
+MAGMA_ARTIFACTORY=$2
+
+declare -A repositories=(
+  [orc8r]="controller magmalte nginx active-mode-controller configuration-controller radio-controller db-service"
+  [feg]="gateway_go gateway_python"
+  [agw]="agw_gateway_c agw_gateway_python ghz_gateway_c ghz_gateway_python agw_gateway_c_arm agw_gateway_python_arm"
+  [cwf]="cwag_go gateway_go gateway_pipelined gateway_python gateway_sessiond operator"
+)
+
+for repo in ${!repositories["$@"]}; do
+  for image in ${repositories[${repo}]}; do
+
+    # Change docker URL to Artifactory
+    sed -i "s/docker/${repo}-prod/g" ~/.docker/config.json
+
+    # Pull docker image from test registry
+    docker pull ${repo}-test.${MAGMA_ARTIFACTORY}/${image}:${MAGMA_TAG}
+
+    # Tag docker image with new tag
+    docker tag ${repo}-test.${MAGMA_ARTIFACTORY}/${image}:${MAGMA_TAG} ${repo}-prod.${MAGMA_ARTIFACTORY}/${image}:${NEW_MAGMA_TAG}
+    docker tag ${repo}-test.${MAGMA_ARTIFACTORY}/${image}:${MAGMA_TAG} ${repo}-prod.${MAGMA_ARTIFACTORY}/${image}:latest
+
+    # Push docker image to prod registry
+    docker push ${repo}-prod.${MAGMA_ARTIFACTORY}/${image}:${NEW_MAGMA_TAG}
+    docker push ${repo}-prod.${MAGMA_ARTIFACTORY}/${image}:latest
+
+    # Remove uploaded image
+    docker rmi ${repo}-test.${MAGMA_ARTIFACTORY}/${image}:${MAGMA_TAG}
+    docker rmi ${repo}-prod.${MAGMA_ARTIFACTORY}/${image}:${NEW_MAGMA_TAG}
+    docker rmi ${repo}-prod.${MAGMA_ARTIFACTORY}/${image}:latest
+
+    # Change docker URL back to docker
+    sed -i "s/${repo}-prod/docker/g" ~/.docker/config.json
+
+  done
+done
