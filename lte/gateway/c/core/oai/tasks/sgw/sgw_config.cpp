634f31c3d61a32701209956d4a024bdee4ca0124
rsarwad <30685618+rsarwad@users.noreply.github.com>
2022-10-12 11:17:07 +0530

    fix(agw): Modified code to replace state_teid_hash list to protobuf map in spgw task (#13843)
    * fix(agw): Modified code to replace state_teid_hash list to protobuf map in spgw task
    Signed-off-by: Rashmi <rashmi.sarwad@radisys.com>

--- lte/gateway/c/core/oai/tasks/sgw/sgw_config.cpp
+++ lte/gateway/c/core/oai/tasks/sgw/sgw_config.cpp
@@ -37,14 +37,14 @@ extern "C" {
 #endif
 #include "lte/gateway/c/core/oai/common/log.h"
 #include "lte/gateway/c/core/common/assertions.h"
-#include "lte/gateway/c/core/common/dynamic_memory_check.h"
+#include "lte/gateway/c/core/oai/lib/bstr/bstrlib.h"
 #ifdef __cplusplus
 }
 #endif
 
+#include "lte/gateway/c/core/common/dynamic_memory_check.h"
 #include "lte/gateway/c/core/common/common_defs.h"
 #include "lte/gateway/c/core/oai/include/sgw_config.h"
-#include "lte/gateway/c/core/oai/lib/bstr/bstrlib.h"
 
 #ifdef LIBCONFIG_LONG
 #define libconfig_int long
