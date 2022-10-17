7e369a1152a407c43e38444d469ee73b0df7a9ff
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2022-10-17 10:56:20 +0530

    updated kubernetes cluster version
    Signed-off-by: Shubham Tatvamasi <shubhamtatvamasi@gmail.com>

--- orc8r/cloud/deploy/terraform/orc8r-aws/variables.tf
+++ orc8r/cloud/deploy/terraform/orc8r-aws/variables.tf
@@ -79,7 +79,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster."
   type        = string
-  default     = "1.22"
+  default     = "1.23"
 }
 
 variable "eks_worker_group_key" {
