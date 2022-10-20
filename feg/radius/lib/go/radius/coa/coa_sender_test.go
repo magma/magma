221650d89d8c915ec71229e9724a6c3b8e5f7576
Moritz Huebner <moritz.huebner@tngtech.com>
2022-10-20 16:11:37 +1100

    reactor(feg): Removed ruckus
    Signed-off-by: Moritz Huebner <moritz.huebner@tngtech.com>

--- feg/radius/lib/go/radius/coa/coa_sender_test.go
+++ feg/radius/lib/go/radius/coa/coa_sender_test.go
@@ -20,7 +20,6 @@ import (
 	"testing"
 	"time"
 
-	"fbc/lib/go/radius/dictionaries/ruckus"
 	"fbc/lib/go/radius/dictionaries/xwf"
 
 	"fbc/lib/go/radius"
@@ -52,29 +51,6 @@ var coaPacketXWFCertified = Params{
 	VendorName: VendorXWFCertified,
 }
 
-var coaPacketRuckus = Params{
-	NASIdentifier:       "XWF-C-TLV",
-	AcctInterimInterval: 60,
-	AcctSessionID:       "00-04-56-91-C4-F8-55-E4-EA-92-87-D1-9A-5A-51-43",
-	CallingStationID:    "30:07:4d:9f:33:8b",
-	CaptivePortalToken:  "eyJyYWRpdXNfc2Vzc2lvbl92YW5pbGxhX2lkIjoyNTYyNDU4NTQ5MDc4MTJ9",
-	TrafficClasses: []AuthorizeTrafficClasses{
-		{
-			AuthorizeClassName: "xwf",
-			AuthorizeBytesLeft: 0,
-		},
-		{
-			AuthorizeClassName: "fbs",
-			AuthorizeBytesLeft: 0,
-		},
-		{
-			AuthorizeClassName: "internet",
-			AuthorizeBytesLeft: 103905273,
-		},
-	},
-	VendorName: VendorRuckus,
-}
-
 var disconnectPacket = DisconnectParams{
 	NASIdentifier:    "XWF-C-TLV",
 	AcctSessionID:    "00-04-56-91-C4-F8-55-E4-EA-92-87-D1-9A-5A-51-43",
@@ -86,19 +62,7 @@ const XWFAuthorizeClassInternet = "internet"
 func serverCoAHandlerXWFCertified(traffic []xwf.XWFAuthorizeTrafficClasses, t *testing.T) {
 	for _, val := range traffic {
 		if val.XWFAuthorizeClassName == XWFAuthorizeClassInternet {
-			require.NotEqual(t, val.XWFAuthorizeBytesLeft, coaPacketRuckus.TrafficClasses[2].AuthorizeBytesLeft)
 			require.Equal(t, val.XWFAuthorizeBytesLeft, coaPacketXWFCertified.TrafficClasses[2].AuthorizeBytesLeft)
-			return
-		}
-	}
-	require.Fail(t, "Missing CoA traffic class internt")
-}
-
-func serverCoAHandlerRuckus(traffic []ruckus.RuckusTCAttrIdsWithQuota, t *testing.T) {
-	for _, val := range traffic {
-		if val.RuckusTCNameQuota == XWFAuthorizeClassInternet {
-			require.NotEqual(t, val.RuckusTCQuota, coaPacketXWFCertified.TrafficClasses[2].AuthorizeBytesLeft)
-			require.Equal(t, val.RuckusTCQuota, coaPacketRuckus.TrafficClasses[2].AuthorizeBytesLeft)
 			return
 		}
 	}
@@ -108,16 +72,10 @@ func serverCoAHandlerRuckus(traffic []ruckus.RuckusTCAttrIdsWithQuota, t *testin
 func serverCoAHandler(w radius.ResponseWriter, r *radius.Request, t *testing.T) {
 	xwfTCAttr, err := xwf.XWFAuthorizeTrafficClasses_Gets(r.Packet)
 	require.NoError(t, err, "Failed to get XWF traffic classes")
-	ruckesTCAttr, err := ruckus.RuckusTCAttrIdsWithQuota_Gets(r.Packet)
-	require.NoError(t, err, "Failed to get Ruckus traffic classes")
-	switch {
-	case len(xwfTCAttr) > 0:
+	if len(xwfTCAttr) > 0 {
 		require.Equal(t, 1, len(xwfTCAttr), "Number of authorized traffic classes don't fit")
 		serverCoAHandlerXWFCertified(xwfTCAttr[0], t)
-	case len(ruckesTCAttr) > 0:
-		require.Equal(t, 1, len(ruckesTCAttr), "Number of authorized traffic classes don't fit")
-		serverCoAHandlerRuckus(ruckesTCAttr[0], t)
-	default:
+	} else {
 		require.Fail(t, "Unexpected CoA package")
 		err := w.Write(r.Response(radius.CodeCoANAK))
 		require.NoError(t, err)
@@ -165,8 +123,6 @@ func TestCoA(t *testing.T) {
 
 	firstCoAPacket, err := CreateCoARequest(coaPacketXWFCertified, secret)
 	require.NoError(t, err)
-	secondCoAPacket, err := CreateCoARequest(coaPacketRuckus, secret)
-	require.NoError(t, err)
 	disconnectPacket, err := CreateCoADisconnect(disconnectPacket, secret)
 	require.NoError(t, err)
 
@@ -189,7 +145,6 @@ func TestCoA(t *testing.T) {
 		defer wg.Done()
 		defer server.Shutdown(context.Background())
 		sendPacket(ctx, t, firstCoAPacket, client, pc.LocalAddr().String())
-		sendPacket(ctx, t, secondCoAPacket, client, pc.LocalAddr().String())
 		sendPacket(ctx, t, disconnectPacket, client, pc.LocalAddr().String())
 		t.Log("Done")
 	}()
--- feg/radius/src/loader/static_loader.go
+++ feg/radius/src/loader/static_loader.go
@@ -14,12 +14,13 @@ limitations under the License.
 package loader
 
 import (
+	"fmt"
+
 	"fbc/cwf/radius/config"
 	"fbc/cwf/radius/filters"
 	filtlballocate "fbc/cwf/radius/filters/lballocate"
 	filtlbcanary "fbc/cwf/radius/filters/lbcanary"
 	"fbc/cwf/radius/modules"
-	modadaptruckus "fbc/cwf/radius/modules/adaptruckus"
 	modmsisdn "fbc/cwf/radius/modules/addmsisdn"
 	modalwaysaccept "fbc/cwf/radius/modules/alwaysaccept"
 	modan "fbc/cwf/radius/modules/analytics"
@@ -35,7 +36,6 @@ import (
 	testsessionstorage "fbc/cwf/radius/modules/testsessionstorage"
 	modxwfv3 "fbc/cwf/radius/modules/xwfv3"
 	"fbc/lib/go/radius"
-	"fmt"
 
 	"go.uber.org/zap"
 )
@@ -70,7 +70,6 @@ var CWFModuleMap = ModuleNameMap{
 	"coafixedip":         func() modules.Module { return NewModule(modcoafixed.Init, modcoafixed.Handle) },
 	"coanas":             func() modules.Module { return NewModule(modcoanas.Init, modcoanas.Handle) },
 	"coadynamic":         func() modules.Module { return NewModule(modcoadynamic.Init, modcoadynamic.Handle) },
-	"adaptruckus":        func() modules.Module { return NewModule(modadaptruckus.Init, modadaptruckus.Handle) },
 	"alwaysaccept":       func() modules.Module { return NewModule(modalwaysaccept.Init, modalwaysaccept.Handle) },
 	"magmaacct":          func() modules.Module { return NewModule(modmagmaacct.Init, modmagmaacct.Handle) },
 	"testsessionstorage": func() modules.Module { return NewModule(testsessionstorage.Init, testsessionstorage.Handle) },
