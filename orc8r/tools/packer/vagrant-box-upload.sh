e57980e64b386c45a3a7b003dd71bffe26977039
Marco Pfirrmann <marco.pfirrmann@tngtech.com>
2023-02-09 09:07:07 +0100

    chore(agw): bump version number of AGW VMs (#14981)
    Signed-off-by: Marco Pfirrmann <marco.pfirrmann@tngtech.com>

--- orc8r/tools/packer/vagrant-box-upload.sh
+++ orc8r/tools/packer/vagrant-box-upload.sh
@@ -7,7 +7,7 @@ FORCE="--no-force"
 
 USER=magmacore
 BOX=$(basename $BOX_FILE | cut -d_ -f1-2 | cut -d. -f1)
-VERSION="1.2.$(date +"%Y%m%d")"
+VERSION="1.3.$(date +"%Y%m%d")"
 BOX_PROVIDER=virtualbox
 if echo $BOX_FILE | grep -q libvirt; then
   BOX_PROVIDER=libvirt
