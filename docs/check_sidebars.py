f305c5f38d4ec53b2712ee5ba35557ffd77c934a
Moritz Huebner <moritz.huebner@tngtech.com>
2023-03-16 11:30:17 +1100

    ci: Reviewdog stuff
    Signed-off-by: Moritz Huebner <moritz.huebner@tngtech.com>

--- docs/check_sidebars.py
+++ docs/check_sidebars.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+"""
+Copyright 2023 The Magma Authors.
 
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import json
 import os
+from typing import Set
 
 exceptions = {
     "proposals/README",
@@ -23,7 +35,17 @@ exceptions = {
 }
 
 
-def get_implemented_sidebar_pages(version="latest"):
+def get_implemented_sidebar_pages(version: str = "latest") -> Set[str]:
+    """
+    Scrape the relevant sidebar.json file to get all the pages
+    that are implemented in the sidebar.
+
+    Args:
+        version (str): The version of the docs to check. Defaults to "latest".
+
+    Returns:
+        Set[str]: A set of sidebar pages that are implemented.
+    """
     version_prefix = _get_version_prefix(version)
     implemented_sidebar_pages = _extract_sidebar_pages(
         sidebar_json_path=_get_sidebar_json_path(version),
@@ -36,10 +58,19 @@ def get_implemented_sidebar_pages(version="latest"):
     return implemented_sidebar_pages
 
 
-def get_all_pages(version="latest"):
+def get_all_pages(version: str = "latest") -> Set[str]:
+    """
+    Scrape the relevant docs folder to get all relevant page ids.
+
+    Args:
+        version (str): The version of the docs to check. Defaults to "latest".
+
+    Returns:
+        Set[str]: A set of all pages that are available for this version.
+    """
     readme_path = _get_readme_path(version)
     all_pages = set()
-    for root, dirs, filenames in os.walk(readme_path):
+    for root, _, filenames in os.walk(readme_path):
         for filename in filenames:
             if filename.endswith('.md'):
                 doc_id = _extract_doc_id(filename, root, _get_version_prefix(version))
@@ -51,7 +82,7 @@ def _extract_sidebar_pages(sidebar_json_path, version_prefix):
     implemented_sidebar_pages = set()
     with open(sidebar_json_path) as f:
         sidebars = json.load(f)[f'{version_prefix}docs']
-        for k, v in sidebars.items():
+        for v in sidebars.values():
             if isinstance(v[0], str):
                 implemented_sidebar_pages = implemented_sidebar_pages.union(set(v))
             else:
@@ -70,25 +101,20 @@ def _remove_version_prefix(implemented_sidebar_pages, version_prefix):
 
 def _get_readme_path(version):
     if version == 'latest':
-        readme_path = 'readmes'
-    else:
-        readme_path = f'docusaurus/versioned_docs/version-{version}'
-    return readme_path
+        return 'readmes'
+    return f'docusaurus/versioned_docs/version-{version}'
 
 
 def _get_sidebar_json_path(version):
     if version == 'latest':
-        sidebar_json_path = 'docusaurus/sidebars.json'
-    else:
-        sidebar_json_path = f'docusaurus/versioned_sidebars/version-{version}-sidebars.json'
-    return sidebar_json_path
+        return 'docusaurus/sidebars.json'
+    return f'docusaurus/versioned_sidebars/version-{version}-sidebars.json'
 
 
 def _get_version_prefix(version):
     if version == 'latest':
         return ''
-    else:
-        return f'version-{version}-'
+    return f'version-{version}-'
 
 
 def _extract_doc_id(filename, root, version_prefix):
@@ -96,7 +122,7 @@ def _extract_doc_id(filename, root, version_prefix):
     doc_id = ""
     with open(path) as f:
         lines = f.readlines()
-        if len(lines) > 0 and lines[0].startswith('---'):
+        if lines and lines[0].startswith('---'):
             for line in lines:
                 if line.startswith('id: '):
                     doc_id = line.replace(f'id: {version_prefix}', '').rstrip('\n')
@@ -106,10 +132,10 @@ def _extract_doc_id(filename, root, version_prefix):
     return doc_id
 
 
-if __name__ == '__main__':
-    versions = ["latest", "1.8.0", "1.7.0"]
+def main():
+    versions = ("latest", "1.8.0", "1.7.0")
     pages_not_implemented = {v: set() for v in versions}
-    for v in ["latest", "1.8.0", "1.7.0"]:
+    for v in versions:
         all_pages = get_all_pages(version=v)
         sidebar_pages = get_implemented_sidebar_pages(version=v)
         for doc in sorted(all_pages):
@@ -118,8 +144,12 @@ if __name__ == '__main__':
 
     sidebars_missing = False
     for v in versions:
-        if len(pages_not_implemented[v]) > 0:
+        if pages_not_implemented[v]:
             sidebars_missing = True
             print(f"Missing pages for {v}: {pages_not_implemented[v]}")
     if sidebars_missing:
         exit(1)
+
+
+if __name__ == '__main__':
+    main()
