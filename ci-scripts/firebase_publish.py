84658a66358b10a507dbc9b79316e3c6aab78831
Nils Semmelrock <nils.semmelrock@tngtech.com>
2023-03-24 14:36:24 +0100

    fix: script does not try to publish removed agw meta data
    Signed-off-by: Nils Semmelrock <nils.semmelrock@tngtech.com>

--- ci-scripts/firebase_publish.py
+++ ci-scripts/firebase_publish.py
@@ -56,7 +56,7 @@ def main():
         print(f"Decoding build_metadata_env JSON failed: {build_metadata_env}")
     build_info = {"metadata": build_metadata}
 
-    builds = ["AGW", "FEG", "ORC8R", "CWAG", "NMS"]
+    builds = ["FEG", "ORC8R", "CWAG", "NMS"]
     for build in builds:
         artifact_env = os.environ[f"{build}_ARTIFACTS"]
         artifacts = {}
@@ -68,8 +68,6 @@ def main():
                 f"has failed: {artifact_env}",
             )
             artifacts = {"packages": [], "valid": False}
-        if build == "AGW":
-            artifacts = _set_download_uri(artifacts)
         build_info[f"{build.lower()}"] = artifacts
 
     # Prepare workload
