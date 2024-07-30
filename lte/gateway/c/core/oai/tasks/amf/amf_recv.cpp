494f360597e8aa3de683ec294038d6b8fc7207d4
Bruno Faria <brunohcfaria@gmail.com>
2024-07-30 21:48:58 +0000

    fix(amf): Fixing lint errors
    Signed-off-by: Bruno Faria <brunohcfaria@gmail.com>

--- lte/gateway/c/core/oai/tasks/amf/amf_recv.cpp
+++ lte/gateway/c/core/oai/tasks/amf/amf_recv.cpp
@@ -549,14 +549,16 @@ status_code_e amf_handle_registration_request(
           (msg->m5gs_mobile_identity.mobile_identity.guti.tmsi1
            << BIT_SHIFT_TMSI1);
     }
-  } else if (params->m5gsregistrationtype == AMF_REGISTRATION_TYPE_MOBILITY_UPDATING) {
+  } else if (params->m5gsregistrationtype ==
+             AMF_REGISTRATION_TYPE_MOBILITY_UPDATING) {
     OAILOG_DEBUG(
         LOG_NAS_AMF,
         "AMF_REGISTRATION_TYPE_MOBILITY_UPDATING processing"
         "identity type = %d ",
         msg->m5gs_mobile_identity.mobile_identity.imsi.type_of_identity);
     if (msg->m5gs_mobile_identity.mobile_identity.guti.type_of_identity ==
-        M5GSMobileIdentityMsg_GUTI && !(is_amf_ctx_new)) {
+            M5GSMobileIdentityMsg_GUTI &&
+        !(is_amf_ctx_new)) {
       for (uint8_t i = 0; i < amf_config.guamfi.nb; i++) {
         if (PLMN_ARE_EQUAL(msg->m5gs_mobile_identity.mobile_identity.guti,
                            amf_config.guamfi.guamfi[i].plmn)) {
@@ -566,12 +568,13 @@ status_code_e amf_handle_registration_request(
       if (!is_plmn_present) {
         delete params;
         amf_cause = AMF_CAUSE_INVALID_MANDATORY_INFO;
-        OAILOG_ERROR(LOG_NAS_AMF,
-                     "UE PLMN mismatch"
-                     "AMF rejecting the REGISTRATION_TYPE_MOBILITY_UPDATING with "
-                     "cause : %d for UE "
-                     "ID: " AMF_UE_NGAP_ID_FMT,
-                     amf_cause, ue_id);
+        OAILOG_ERROR(
+            LOG_NAS_AMF,
+            "UE PLMN mismatch"
+            "AMF rejecting the REGISTRATION_TYPE_MOBILITY_UPDATING with "
+            "cause : %d for UE "
+            "ID: " AMF_UE_NGAP_ID_FMT,
+            amf_cause, ue_id);
         rc = amf_proc_registration_reject(ue_id, amf_cause);
         amf_free_ue_context(ue_context);
         OAILOG_FUNC_RETURN(LOG_NAS_AMF, rc);
@@ -598,13 +601,12 @@ status_code_e amf_handle_registration_request(
 
       OAILOG_DEBUG(LOG_NAS_AMF,
                    "In process of registration mobility update: "
-                   "GUAMFI: " PLMN_FMT".%u.%u.%u - "
+                   "GUAMFI: " PLMN_FMT
+                   ".%u.%u.%u - "
                    "new 5G-TMSI value 0x%08" PRIx32 "\n",
                    PLMN_ARG(&amf_guti.guamfi.plmn),
-                   amf_guti.guamfi.amf_regionid,
-                   amf_guti.guamfi.amf_set_id,
-                   amf_guti.guamfi.amf_pointer,
-                   amf_guti.m_tmsi);
+                   amf_guti.guamfi.amf_regionid, amf_guti.guamfi.amf_set_id,
+                   amf_guti.guamfi.amf_pointer, amf_guti.m_tmsi);
 
       amf_ue_context_on_new_guti(ue_context,
                                  reinterpret_cast<guti_m5_t*>(&amf_guti));
@@ -619,10 +621,10 @@ status_code_e amf_handle_registration_request(
     } else {
       // UE context is new and/or UE identity type is not GUTI
       // add log message.
-      OAILOG_ERROR(
-          LOG_AMF_APP,
-          "UE context does not exist or UE identity type is not GUTI "
-          "Mobility Registration Update failed. Sending Registration Reject message\n");
+      OAILOG_ERROR(LOG_AMF_APP,
+                   "UE context does not exist or UE identity type is not GUTI "
+                   "Mobility Registration Update failed. Sending Registration "
+                   "Reject message\n");
       amf_cause = AMF_CAUSE_UE_ID_CAN_NOT_BE_DERIVED;
       rc = amf_proc_registration_reject(ue_id, amf_cause);
       // Haven't received 5G TMSI. Temporary context created to be cleaned up
