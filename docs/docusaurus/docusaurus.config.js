23af39902e638ad24bbe7e1e120b3fecc645795e
conrado.gobato <conradogob07@gmail.com>
2024-10-23 10:41:29 -0300

    fix: dockerfile and old version name

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/icon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://magmacore.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
