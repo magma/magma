c5ce132c5023eeab4e2f53d9dba50f4243ae8597
vratnak <ratnak.n@gmail.com>
2024-11-02 13:41:07 +0530

    removed trailing spaces

--- third_party/build/bin/libczmq-dev_build.sh
+++ third_party/build/bin/libczmq-dev_build.sh
@@ -17,12 +17,12 @@ source "$SCRIPT_DIR/../lib/util.sh"
 
 
 function buildrequires() {
-  echo  comerr-dev cppzmq-dev fontconfig-config fonts-dejavu-core icu-devtools krb5-multidev libabsl20220623 libaom3 libavif15 libbrotli1 \ 
-        libbsd-dev libbsd0 libc-dev-bin libc-devtools libc6-dev libcrypt-dev libdav1d6 libde265-0 libdeflate0 libexpat1 libfontconfig1 libfreetype6 \ 
-        libgav1-1 libgd3 libgssrpc4 libheif1 libicu-dev libicu72 libjbig0 libjpeg62-turbo libkadm5clnt-mit12 libkadm5srv-mit12 \ 
-        libkdb5-10 libkrb5-dev liblerc4 libmd-dev libnorm-dev libnorm1 libnsl-dev libnsl2 libnspr4 libnspr4-dev libnss3 libnss3-dev libnuma1 \ 
-        libpgm-5.3-0 libpgm-dev libpng16-16 librav1e0 libsodium-dev libsodium23 libsqlite3-0 libsvtav1enc1 libtiff6 libtirpc-common libtirpc-dev \ 
-        libtirpc3 libwebp7 libx11-6 libx11-data libx265-199 libxau6 libxcb1 libxdmcp6 libxml2 libxml2-dev libxpm4 libyuv0 libzmq3-dev libzmq5 \ 
+  echo  comerr-dev cppzmq-dev fontconfig-config fonts-dejavu-core icu-devtools krb5-multidev libabsl20220623 libaom3 libavif15 libbrotli1 \
+        libbsd-dev libbsd0 libc-dev-bin libc-devtools libc6-dev libcrypt-dev libdav1d6 libde265-0 libdeflate0 libexpat1 libfontconfig1 libfreetype6 \
+        libgav1-1 libgd3 libgssrpc4 libheif1 libicu-dev libicu72 libjbig0 libjpeg62-turbo libkadm5clnt-mit12 libkadm5srv-mit12 \
+        libkdb5-10 libkrb5-dev liblerc4 libmd-dev libnorm-dev libnorm1 libnsl-dev libnsl2 libnspr4 libnspr4-dev libnss3 libnss3-dev libnuma1 \
+        libpgm-5.3-0 libpgm-dev libpng16-16 librav1e0 libsodium-dev libsodium23 libsqlite3-0 libsvtav1enc1 libtiff6 libtirpc-common libtirpc-dev \
+        libtirpc3 libwebp7 libx11-6 libx11-data libx265-199 libxau6 libxcb1 libxdmcp6 libxml2 libxml2-dev libxpm4 libyuv0 libzmq3-dev libzmq5 \
         linux-libc-dev manpages manpages-dev rpcsvc-proto uuid-dev
 }
 
