c6f5297621efd4db4b19b6d6592ea41b6f828ee8
mohinali <94956303+mohinali@users.noreply.github.com>
2024-11-08 18:46:46 +0530

    Update td-agent-bit_build.sh

--- third_party/build/bin/td-agent-bit_build.sh
+++ third_party/build/bin/td-agent-bit_build.sh
@@ -10,11 +10,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-PKGNAME=td-agent-bit-dev
-WORK_DIR=/tmp/build-${PKGNAME}
+
 set -e
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 source "$SCRIPT_DIR/../lib/util.sh"
+
+PKGNAME=td-agent-bit-dev
+WORK_DIR=/tmp/build-${PKGNAME}
 
 if_subcommand_exec
 
