magma/magma

commit 0f84c81edf27640c2f17d855011cbcd00e9d72c3

fix(orc8r): fix for new ssl directive (#15542)
Changes:
  - Updated Conf file as per directive

Signed-off-by: shashidhar-patil <shashidhar.patil@veltris.com>
Co-authored-by: shashidhar-patil <shashidhar.patil@veltris.com>

orc8r/cloud/helm/orc8r/charts/nms/templates/etc/_nginx_proxy_ssl.conf.tpl
@@ -1,8 +1,7 @@
 server {
-  listen 443;
-  ssl on;
-  ssl_certificate /etc/nginx/conf.d/nms_nginx.pem;
-  ssl_certificate_key /etc/nginx/conf.d/nms_nginx.key.pem;
+  listen 443 ssl;
+  ssl_certificate /var/opt/magma/certs/nms/tls.crt;
+  ssl_certificate_key /var/opt/magma/certs/nms/tls.key;
   location / {
      proxy_pass http://magmalte:8081;
      proxy_set_header Host $http_host;

orc8r/cloud/helm/orc8r/charts/nms/templates/etc/_certs_nginx_proxy_ssl.conf.tpl
@@ -1,6 +1,5 @@
 server {
-  listen 443;
-  ssl on;
+  listen 443 ssl;
   ssl_certificate /var/opt/magma/certs/nms/tls.crt;
   ssl_certificate_key /var/opt/magma/certs/nms/tls.key;
   location / {

commit d2e1dfa14b86a05c0f2cec00927d659d2fd53d66

fix(orc8r): fix for error introduced in PR #15542 new ssl
Signed-off-by: Jim Blakley <jblake1@andrew.cmu.edu>

orc8r/cloud/helm/orc8r/charts/nms/templates/etc/_nginx_proxy_ssl.conf.tpl
@@ -1,7 +1,7 @@
 server {
   listen 443 ssl;
-  ssl_certificate /var/opt/magma/certs/nms/tls.crt;
-  ssl_certificate_key /var/opt/magma/certs/nms/tls.key;
+  ssl_certificate /etc/nginx/conf.d/nms_nginx.pem;
+  ssl_certificate_key /etc/nginx/conf.d/nms_nginx.key.pem;
   location / {
      proxy_pass http://magmalte:8081;
      proxy_set_header Host $http_host;