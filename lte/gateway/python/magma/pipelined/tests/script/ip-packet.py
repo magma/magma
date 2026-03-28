cfce0fe36104c94dd49fdf3127c7a041867c8eb7
Shah Rahman <shahrahman@meta.com>
2026-03-27 18:13:04 -0700

    chore: add missing BSD-3-Clause license headers to 57 source files
    Add the standard Magma BSD-3-Clause license header to 50 Go files
    and 7 Python files that were missing it entirely.
    
    Skipped:
    - Generated files (*.pb.go, *_swaggergen.go, *.gen.go)
    - Mock files (*/mocks/*)
    - Empty __init__.py files
    - Alembic migration files (auto-generated)
    - Files with legacy Facebook copyright (separate cleanup)
    - Files in open PRs (AVOID list)
    
    Part of P027 Phase 6: Code Quality (#15838)
    
    Signed-off-by: Shah Rahman <shahrahman@meta.com>

--- lte/gateway/python/magma/pipelined/tests/script/ip-packet.py
+++ lte/gateway/python/magma/pipelined/tests/script/ip-packet.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+Copyright 2020 The Magma Authors.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
 # sudo /home/vagrant/build/python/bin/python gtp-cmd.py 192.168.128.12 192.168.129.42 eth1
 
 import sys
