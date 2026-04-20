44c4b29a6979397233a5a7bb908e0650d26b1eeb
panagiotisevaggelou <panagiotisevaggelou02@gmail.com>
2026-04-20 20:34:49 +0000

    chore: [6.5] Phase 2 - Fix inconsistent headers - Batch 2
    Change the standard Magma BSD-3-Clause license header from block-style to Doxygen-style
    
    Files updated:
    - ./core/oai/tasks/grpc_service/HaServiceImpl.hpp
    - ./core/oai/tasks/grpc_service/HaServiceImpl.cpp
    - ./core/oai/tasks/grpc_service/S8ServiceImpl.cpp
    - ./core/oai/tasks/grpc_service/S8ServiceImpl.hpp
    - ./core/oai/tasks/async_grpc_service/grpc_async_service_task.hpp
    - ./core/oai/tasks/mme_app/mme_app_ip_imsi.hpp
    - ./core/oai/tasks/mme_app/mme_app_timer.hpp
    - ./core/oai/tasks/mme_app/mme_app_timer_management.hpp
    - ./core/oai/tasks/mme_app/mme_app_ip_imsi.cpp
    - ./core/oai/tasks/mme_app/mme_app_timer_management.cpp
    - ./core/oai/tasks/mme_app/mme_app_ueip_imsi_map.hpp
    - ./core/oai/tasks/amf/amf_app_timer_management.cpp
    - ./core/oai/tasks/amf/amf_app_timer_management.hpp
    - ./core/oai/tasks/nas5g/include/M5GRegistrationComplete.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GTAIList.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GExtendedProtocolDiscriminator.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GMMCause.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GNSSAI.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GSRegistrationResult.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GAuthenticationFailureIE.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GSMobileIdentity.hpp
    - ./core/oai/tasks/nas5g/include/ies/M5GSDeRegistrationType.hpp
    - ./core/oai/tasks/nas5g/include/M5GAuthenticationFailure.hpp
    - ./core/oai/tasks/nas5g/src/ies/M5GPDUSessionStatus.cpp
    - ./core/oai/tasks/nas5g/src/ies/M5GUplinkDataStatus.cpp
    - ./core/oai/tasks/nas5g/src/ies/M5GSDeRegistrationType.cpp
    - ./core/oai/tasks/nas5g/src/ies/M5GMaxNumOfSupportedPacketFilters.cpp
    - ./core/oai/tasks/nas5g/src/ies/M5GSMCause.cpp
    - ./core/oai/tasks/nas5g/src/ies/M5GNSSAI.cpp
    - ./core/oai/tasks/nas5g/src/ies/M5GPDUSessionReActivationResult.cpp
    - ./core/oai/tasks/ha/HaClient.cpp
    - ./core/oai/tasks/ha/ha_task.cpp
    - ./core/oai/tasks/ha/ha_defs.hpp
    - ./core/oai/tasks/ha/ha_service_handler.cpp
    - ./core/oai/tasks/s1ap/s1ap_timer.hpp
    - ./core/oai/tasks/s1ap/s1ap_timer_management.hpp
    - ./core/oai/tasks/s1ap/s1ap_timer_management.cpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_s11_handlers.hpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_state_manager.cpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_state_converter.cpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_state_manager.hpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_state_converter.hpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_task.cpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_handlers.cpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_state.cpp
    - ./core/oai/tasks/sgw_s8/sgw_s8_defs.hpp
    
    Refs: #15838
    
    Signed-off-by: panagiotisevaggelou <panagiotisevaggelou02@gmail.com>

--- lte/gateway/c/core/oai/tasks/nas5g/src/ies/M5GPDUSessionReActivationResult.cpp
+++ lte/gateway/c/core/oai/tasks/nas5g/src/ies/M5GPDUSessionReActivationResult.cpp
@@ -1,13 +1,15 @@
-/*
-Copyright 2020 The Magma Authors.
-This source code is licensed under the BSD-style license found in the
-LICENSE file in the root directory of this source tree.
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+/**
+ * Copyright 2020 The Magma Authors.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <iostream>
 #include <sstream>
@@ -17,8 +19,8 @@ limitations under the License.
 #include "lte/gateway/c/core/oai/tasks/nas5g/include/ies/M5GPDUSessionReActivationResult.hpp"
 
 namespace magma5g {
-M5GPDUSessionReActivationResult::M5GPDUSessionReActivationResult() {};
-M5GPDUSessionReActivationResult::~M5GPDUSessionReActivationResult() {};
+M5GPDUSessionReActivationResult::M5GPDUSessionReActivationResult(){};
+M5GPDUSessionReActivationResult::~M5GPDUSessionReActivationResult(){};
 
 int M5GPDUSessionReActivationResult::EncodePDUSessionReActivationResult(
     M5GPDUSessionReActivationResult* pduSessionReActivationStatus, uint8_t iei,
